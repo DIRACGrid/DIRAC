2602f16937d26bdfc0d29988fe491db62d201b78
Ricardo Graciani <graciani@ecm.ub.es>
2007-03-14 06:36:36 +0000

    use DIRAC.Tests.run
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4033 4525493e-7705-40b1-a816-d608a930855b

--- ConfigurationSystem/test/test_gCFG.py
+++ ConfigurationSystem/test/test_gCFG.py
@@ -1,49 +1,22 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ConfigurationSystem/test/test_gCFG.py,v 1.1 2007/03/09 15:22:42 rgracian Exp $
-__RCSID__ = "$Id: test_gCFG.py,v 1.1 2007/03/09 15:22:42 rgracian Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ConfigurationSystem/test/test_gCFG.py,v 1.2 2007/03/14 06:31:29 rgracian Exp $
+__RCSID__ = "$Id: test_gCFG.py,v 1.2 2007/03/14 06:31:29 rgracian Exp $"
 
 from dirac import DIRAC
 
-DIRAC.gLogger.setName('test_gConfig')
+DIRAC.gLogger.initialize('test_gConfig','/testSection')
 
-# DIRAC.gConfig.loadFile('test.cfg')
-# import os
-
-# print os.path.abspath( os.curdir )
+DIRAC.gConfig.loadFile('./test.cfg')
 
 testList = [{ 'method'    : DIRAC.gConfig.getOption,
               'arguments' : ( '/testSection/test', ),
-              'output'    : True
+              'output'    : {'OK': True, 'Value': 'test'}
             },             
              ]
 
 testdict = { 'DIRAC.gConfig'               : testList,}
 
-def runTests( testDict, name=None ):
-
-  if name: print "Running tests for %s ..." % name
-  for module in testDict:
-    testFailed = False
-    # print module, testDict[module]
-    for test in testDict[module]:
-        # print "Running Test %s" % module
-        retVal = apply( test['method'], test['arguments'] )
-        if retVal <> test['output']:
-          print "Test for %s Failed" % module
-          print "Error %s%s should return '%s', " % ( test['method'].__name__, test['arguments'], test['output'])
-          print "It returns '%s'" % retVal, type( retVal )
-          print "Failed"
-          testFailed = True
-          break
-    if testFailed:
-      DIRAC.gLogger.exception('')
-      DIRAC.sys.exit(-1)
-    print "Test for %s OK" % module
-
-  if name : print "Tests for %s OK" % name
-  return True
-
   
-runTests( testdict, 'DIRAC.Information.Logger' )
+DIRAC.Tests.run( testdict, 'DIRAC.gConfig' )
 
 
 DIRAC.exit()
--- LoggingSystem/test/test_Logger.py
+++ LoggingSystem/test/test_Logger.py
@@ -1,8 +1,11 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/LoggingSystem/test/test_Logger.py,v 1.1 2007/03/09 15:45:53 rgracian Exp $
-__RCSID__ = "$Id: test_Logger.py,v 1.1 2007/03/09 15:45:53 rgracian Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/LoggingSystem/test/test_Logger.py,v 1.2 2007/03/14 06:36:36 rgracian Exp $
+__RCSID__ = "$Id: test_Logger.py,v 1.2 2007/03/14 06:36:36 rgracian Exp $"
 """
 """
 from dirac import DIRAC
+
+DIRAC.gLogger.initialize('test_gLogger','/testSectionDebug')
+
 
 try:
   for i in None:
@@ -17,11 +20,11 @@ testList = [{ 'method'    : DIRAC.gLogger.always,
               'arguments' : ( ( "This is a always message" ), ),
               'output'    : True
             },
-            { 'method'    : DIRAC.gLogger.vital,
+            { 'method'    : DIRAC.gLogger.info,
               'arguments' : ( ( "This is a vital message" ), ),
               'output'    : True
             },
-            { 'method'    : DIRAC.gLogger.info,
+            { 'method'    : DIRAC.gLogger.verbose,
               'arguments' : ( ( "This is a info message" ), ),
               'output'    : True
             },
@@ -45,37 +48,10 @@ testList = [{ 'method'    : DIRAC.gLogger.always,
               'arguments' : ( ( "This is a fatal message" ), ),
               'output'    : True
             },
-            { 'method'    : DIRAC.gLogger.setName,
-              'arguments' : ( "test_Logger" , ),
-              'output'    : "test_Logger"
-            },
             ]
 testdict = { 'DIRAC.gLogger'               : testList,}
 
-def runTests( testDict, name=None ):
-
-  if name: print "Running tests for %s ..." % name
-  for module in testDict:
-    testFailed = False
-    # print module, testDict[module]
-    for test in testDict[module]:
-        # print "Running Test %s" % module
-        retVal = apply( test['method'], test['arguments'] )
-        if retVal <> test['output']:
-          print "Test for %s Failed" % module
-          print "Error %s%s should return '%s', " % ( test['method'].__name__, test['arguments'], test['output'])
-          print "It returns '%s'" % retVal, type( retVal )
-          print "Failed"
-          testFailed = True
-          break
-    if testFailed:
-      DIRAC.sys.exit(-1)
-    print "Test for %s OK" % module
-
-  if name : print "Tests for %s OK" % name
-  return True
-
   
-runTests( testdict, 'DIRAC.Information.Logger' )
+DIRAC.Tests.run( testdict, 'DIRAC.Information.Logger' )
 
 DIRAC.exit()
