9a5d5bbe5721e480980315b5d8319f4563ab3ef4
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2007-05-13 21:19:41 +0000

    Import of the __init__.py package stamp
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4195 4525493e-7705-40b1-a816-d608a930855b

---
+++ WorkloadManagementSystem/__init__.py
@@ -0,0 +1,9 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/__init__.py,v 1.1 2007/05/13 21:19:41 atsareg Exp $
+############################################################
+
+"""
+   DIRAC.WorkloadManagementSystem package
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2007/05/13 21:19:41 atsareg Exp $"
