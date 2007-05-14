65c86f5af1996e718859a91f91a7a855ca8d4136
Adria Casajus <adria@ecm.ub.es>
2007-05-14 13:27:00 +0000

    Id nice
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4201 4525493e-7705-40b1-a816-d608a930855b

--- LoggingSystem/__init__.py
+++ LoggingSystem/__init__.py
@@ -1,5 +1,5 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/LoggingSystem/__init__.py,v 1.1 2007/03/09 15:45:54 rgracian Exp $
-__RCSID__ = "$Id: __init__.py,v 1.1 2007/03/09 15:45:54 rgracian Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/LoggingSystem/__init__.py,v 1.2 2007/05/14 13:27:00 acasajus Exp $
+__RCSID__ = "$Id: __init__.py,v 1.2 2007/05/14 13:27:00 acasajus Exp $"
 """
-   DIRAC.LoggingSystem package  
+   DIRAC.LoggingSystem package
 """
