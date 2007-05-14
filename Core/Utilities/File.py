d97ee2f2a5afe148da7922df89e1b0475a0ff23c
Gennady Kuznetsov <G.Kuznetsov@rl.ac.uk>
2007-05-14 16:21:35 +0000

    Header test
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4221 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/File.py
+++ Core/Utilities/File.py
@@ -1,5 +1,5 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/File.py,v 1.9 2007/05/14 16:15:32 gkuznets Exp $
-# $Id: File.py,v 1.9 2007/05/14 16:15:32 gkuznets Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/File.py,v 1.10 2007/05/14 16:21:35 gkuznets Exp $
+# $Id: File.py,v 1.10 2007/05/14 16:21:35 gkuznets Exp $
 """
-test
+test test
 """
