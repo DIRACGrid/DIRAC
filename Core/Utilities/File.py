2250baadd6a7af9ec56270e9d37a0455908e3b1c
Gennady Kuznetsov <G.Kuznetsov@rl.ac.uk>
2007-05-14 15:28:09 +0000

    Header test
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4207 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/File.py
+++ Core/Utilities/File.py
@@ -1,18 +1,18 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/File.py,v 1.1 2007/03/09 15:33:19 rgracian Exp $
-__RCSID__ = "$Id: File.py,v 1.1 2007/03/09 15:33:19 rgracian Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/File.py,v 1.2 2007/05/14 15:28:09 gkuznets Exp $
 """
    Collection of DIRAC useful file related modules
    by default on Error they return None
 """
+__RCSID__ = "$Release: $"
 
 import md5
 import random
 
 def makeGuid( fileName=None ):
   """
-     Utility to create GUID's, if a filename it is provided the 
+     Utility to create GUID's, if a filename it is provided the
      GUID will correspond to its hexadecimal md5 checksum
-     the format is capitalized 8-4-4-4-12, otherwise a random seed it is 
+     the format is capitalized 8-4-4-4-12, otherwise a random seed it is
      used to create a GUID
   """
   myMd5 = md5.new()
@@ -25,14 +25,14 @@ def makeGuid( fileName=None ):
     except:
       return None
   else:
-    myMd5.update( str( random.getrandbits( 128 ) ) )  
-    
+    myMd5.update( str( random.getrandbits( 128 ) ) )
+
   md5HexString = myMd5.hexdigest()
-  
+
   md5String = "%s-%s-%s-%s-%s" % ( md5HexString[0:8],
                                    md5HexString[8:12],
                                    md5HexString[12:16],
                                    md5HexString[16:20],
                                    md5HexString[20:32] )
-                                   
+
   return md5String.upper()
