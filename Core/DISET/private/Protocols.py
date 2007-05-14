14018dfeffde47049f1c967c1d6ca5dc23133bb0
Adria Casajus <adria@ecm.ub.es>
2007-05-14 16:02:43 +0000

    Final protocol names. dips and dip :)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4218 4525493e-7705-40b1-a816-d608a930855b

--- Core/DISET/private/Protocols.py
+++ Core/DISET/private/Protocols.py
@@ -1,10 +1,10 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/DISET/private/Protocols.py,v 1.5 2007/05/10 18:44:59 acasajus Exp $
-__RCSID__ = "$Id: Protocols.py,v 1.5 2007/05/10 18:44:59 acasajus Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/DISET/private/Protocols.py,v 1.6 2007/05/14 16:02:43 acasajus Exp $
+__RCSID__ = "$Id: Protocols.py,v 1.6 2007/05/14 16:02:43 acasajus Exp $"
 
 from DIRAC.Core.DISET.private.Transports import PlainTransport, SSLTransport
 
-gProtocolDict = { 'dirac' : PlainTransport.PlainTransport,
-                  'diset' : SSLTransport.SSLTransport
+gProtocolDict = { 'dip' : PlainTransport.PlainTransport,
+                  'dips' : SSLTransport.SSLTransport
                  }
 
-gDefaultProtocol = 'dirac'
+gDefaultProtocol = 'dips'
