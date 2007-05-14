d4efaa4d1771aaf3afe32aa8206885a6c5d83c0e
Gennady Kuznetsov <G.Kuznetsov@rl.ac.uk>
2007-05-14 16:25:33 +0000

    Header test
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4222 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/File.py
+++ Core/Utilities/File.py
@@ -1,5 +1,5 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/File.py,v 1.10 2007/05/14 16:21:35 gkuznets Exp $
-# $Id: File.py,v 1.10 2007/05/14 16:21:35 gkuznets Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/File.py,v 1.11 2007/05/14 16:25:33 gkuznets Exp $
+# $Id: File.py,v 1.11 2007/05/14 16:25:33 gkuznets Exp $
 """
 test test
 """
--- Core/Utilities/List.py
+++ Core/Utilities/List.py
@@ -1,5 +1,5 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/List.py,v 1.2 2007/03/30 16:06:35 rgracian Exp $
-__RCSID__ = "$Id: List.py,v 1.2 2007/03/30 16:06:35 rgracian Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/List.py,v 1.3 2007/05/14 16:22:39 gkuznets Exp $
+__RCSID__ = "$Id: List.py,v 1.3 2007/05/14 16:22:39 gkuznets Exp $"
 """
    Collection of DIRAC useful list related modules
    by default on Error they return None
@@ -20,7 +20,7 @@ def uniqueElements( list ):
         newList.append( i )
     return newList
   except:
-    return None  
+    return None
 
 def fromChar( inputString, sepChar = "," ):
   """
@@ -28,10 +28,10 @@ def fromChar( inputString, sepChar = "," ):
      resulting string items are stripped and empty items are removed
   """
   if not ( type( inputString ) in StringTypes and
-           type( sepChar ) in StringTypes and 
-           sepChar ): # to prevent getting an empty String as argument  
+           type( sepChar ) in StringTypes and
+           sepChar ): # to prevent getting an empty String as argument
     return None
-  
+
   return [ fieldString.strip() for fieldString in inputString.split( sepChar ) if len( fieldString.strip() ) > 0 ]
 
 def randomize( initialList ):
