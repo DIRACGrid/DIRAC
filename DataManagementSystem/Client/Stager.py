a0c8fc5e7c57a842b6b3e3e79413078e5931d95f
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2007-05-18 11:20:26 +0000

    Moved from WMS System
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4444 4525493e-7705-40b1-a816-d608a930855b

---
+++ DataManagementSystem/Client/Stager.py
@@ -0,0 +1,111 @@
+"""Set of utilities and classes to handle Stager Database"""
+
+from types import *
+from DIRAC import gLogger, gConfig, S_OK, S_ERROR
+from DIRAC.Core.DISET.Client import RPCClient
+
+class StagerClient:
+
+  def __init__(self,useCertificates = False):
+    """ Constructor of the StagerDBDBClient class
+    """
+    self.server = RPCClient('/DataManagentSytem/Stager',useCertificates)
+
+  def stageFiles(self,jobid,sites,replicas):
+    try:
+      result = self.server.stageFiles(jobid,sites,replicas)
+      return result
+    except Exception, x:
+      errorStr = "StagerDBClient.stageFiles failed"
+      gLogger.exception(errorStr,x)
+      return S_ERROR(errorStr+": "+str(x))
+
+  def getFilesForState(self,site,state,limit=0):
+    try:
+      result = self.server.getFilesForState(site,state,limit)
+      return result
+    except Exception, x:
+      errorStr = "StagerDBClient.getFilesForState failed"
+      gLogger.exception(errorStr,x)
+      return S_ERROR(errorStr+": "+str(x))
+
+  def setFilesState(self,lfns,site,state):
+    try:
+      result = self.server.setFilesState(lfns,site,state)
+      return result
+    except Exception, x:
+      errorStr = "StagerDBClient.setFilesState failed"
+      gLogger.exception(errorStr,x)
+      return S_ERROR(errorStr+": "+str(x))
+
+  def getJobsForState(self,site,state,limit=0):
+    try:
+      result = self.server.getJobsForState(site,state,limit)
+      return result
+    except Exception, x:
+      errorStr = "StagerDBClient.getJobsForState failed"
+      gLogger.exception(errorStr,x)
+      return S_ERROR(errorStr+": "+str(x))
+
+  def getStageTimeAtSite(self,lfns,site):
+    try:
+      result = self.server.getStageTimeAtSite(lfns,site)
+      return result
+    except Exception, x:
+      errorStr = "StagerDBClient.getStageTimeAtSite failed"
+      gLogger.exception(errorStr,x)
+      return S_ERROR(errorStr+": "+str(x))
+
+  def setJobsDone(self,jobIDs):
+    try:
+      result = self.server.setJobsDone(jobIDs)
+      return result
+    except Exception, x:
+      errorStr = "StagerDBClient.setJobsDone failed"
+      gLogger.exception(errorStr,x)
+      return S_ERROR(errorStr+": "+str(x))
+
+  def resetStageRequest(self,site,timeout):
+    try:
+      result = self.server.resetStageRequest(site,timeout)
+      return result
+    except Exception, x:
+      errorStr = "StagerDBClient.resetStageRequest failed"
+      gLogger.exception(errorStr,x)
+      return S_ERROR(errorStr+": "+str(x))
+
+  def getLFNsForJob(self,jobid):
+    try:
+      result = self.server.getLFNsForJob(jobid)
+      return result
+    except Exception, x:
+      errorStr = "StagerDBClient.getLFNsForJob failed"
+      gLogger.exception(errorStr,x)
+      return S_ERROR(errorStr+": "+str(x))
+
+  def getJobsForRetry(self,retry,site):
+    try:
+      result = self.server.getJobsForRetry(retry,site)
+      return result
+    except Exception, x:
+      errorStr = "StagerDBClient.getJobsForRetry failed"
+      gLogger.exception(errorStr,x)
+      return S_ERROR(errorStr+": "+str(x))
+
+  def getAllJobs(self,site):
+    try:
+      result = self.server.getAllJobs(site)
+      return result
+    except Exception, x:
+      errorStr = "StagerDBClient.getAllJobs failed"
+      gLogger.exception(errorStr,x)
+      return S_ERROR(errorStr+": "+str(x))
+
+  def setTiming(self,site,cmd,time,files):
+    try:
+      result = self.server.setTiming(site,cmd,time,files)
+      return result
+    except Exception, x:
+      errorStr = "StagerDBClient.setTiming failed"
+      gLogger.exception(errorStr,x)
+      return S_ERROR(errorStr+": "+str(x))
