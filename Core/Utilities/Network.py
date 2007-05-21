e8290a3afbdf7a85c0393e3d4340d66eb9d05f27
Adria Casajus <adria@ecm.ub.es>
2007-05-21 15:19:07 +0000

    Moved fcntl to under tht function scope.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4447 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/Network.py
+++ Core/Utilities/Network.py
@@ -1,5 +1,5 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/Network.py,v 1.3 2007/05/03 18:59:48 acasajus Exp $
-__RCSID__ = "$Id: Network.py,v 1.3 2007/05/03 18:59:48 acasajus Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/Network.py,v 1.4 2007/05/21 15:19:07 acasajus Exp $
+__RCSID__ = "$Id: Network.py,v 1.4 2007/05/21 15:19:07 acasajus Exp $"
 """
    Collection of DIRAC useful network related modules
    by default on Error they return None
@@ -7,13 +7,13 @@ __RCSID__ = "$Id: Network.py,v 1.3 2007/05/03 18:59:48 acasajus Exp $"
    getAllInterfaces and getAddressFromInterface do not work in MAC
 """
 import socket
-import fcntl
 import struct
 import array
 import os
 from DIRAC.Core.Utilities.ReturnValues import S_OK, S_ERROR
 
 def getAllInterfaces():
+  import fcntl
   max_possible = 128  # arbitrary. raise if needed.
   bytes = max_possible * 32
   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -32,6 +32,7 @@ def getAllInterfaces():
   return [namestr[i:i+32].split('\0', 1)[0] for i in range(0, outbytes, 32)]
 
 def getAddressFromInterface( ifName ):
+  import fcntl
   try:
     s = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
     return socket.inet_ntoa( fcntl.ioctl(
