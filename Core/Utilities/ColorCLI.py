d0fd9771bd846da18a374b7fda6a6f5f202bb363
Adria Casajus <adria@ecm.ub.es>
2007-05-22 18:24:43 +0000

    delete print
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4458 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/ColorCLI.py
+++ Core/Utilities/ColorCLI.py
@@ -1,5 +1,5 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/ColorCLI.py,v 1.2 2007/05/22 18:21:32 acasajus Exp $
-__RCSID__ = "$Id: ColorCLI.py,v 1.2 2007/05/22 18:21:32 acasajus Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/ColorCLI.py,v 1.3 2007/05/22 18:24:43 acasajus Exp $
+__RCSID__ = "$Id: ColorCLI.py,v 1.3 2007/05/22 18:24:43 acasajus Exp $"
 
 import os
 import types
@@ -8,7 +8,6 @@ gColors = { 'red':1, 'green':2, 'yellow':3, 'blue':4 }
 
 def colorEnabled():
   if os.environ.has_key('TERM'):
-    print os.environ[ 'TERM' ]
     if os.environ['TERM'] in ( 'xterm', 'xterm-color' ):
       return True
   return False
