ac1562df52934d5809a2c047beff37b58c37e2a9
Gennady Kuznetsov <G.Kuznetsov@rl.ac.uk>
2007-05-25 13:13:44 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4486 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Workflow/WFEditor/__init__.py
@@ -0,0 +1,2 @@
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Workflow/WFEditor/__init__.py,v 1.2 2007/05/25 13:13:26 gkuznets Exp $
+__RCSID__ = "$Id: __init__.py,v 1.2 2007/05/25 13:13:26 gkuznets Exp $"
--- Core/Workflow/__init__.py
+++ Core/Workflow/__init__.py
@@ -1,2 +1,4 @@
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Workflow/__init__.py,v 1.3 2007/05/25 13:13:26 gkuznets Exp $
+__RCSID__ = "$Id: __init__.py,v 1.3 2007/05/25 13:13:26 gkuznets Exp $"
 
 
