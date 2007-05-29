be76dbcc203eacada87b81acf36f5d749a57e795
Adria Casajus <adria@ecm.ub.es>
2007-05-29 15:02:47 +0000

    Added functions to get a tuple from a message and a message from a tuple
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4500 4525493e-7705-40b1-a816-d608a930855b

--- LoggingSystem/private/Message.py
+++ LoggingSystem/private/Message.py
@@ -1,57 +1,72 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/LoggingSystem/private/Message.py,v 1.2 2007/03/14 06:35:39 rgracian Exp $
-__RCSID__ = "$Id: Message.py,v 1.2 2007/03/14 06:35:39 rgracian Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/LoggingSystem/private/Message.py,v 1.3 2007/05/29 15:02:47 acasajus Exp $
+__RCSID__ = "$Id: Message.py,v 1.3 2007/05/29 15:02:47 acasajus Exp $"
+
+from DIRAC.Core.Utilities import Time
+
+def tupleToMessage( varTuple ):
+  varList = list( varTuple )
+  varList[ 2 ] = time.fromString( varList[ 2 ] )
+  return Message( *varList )
 
 class Message:
-  
-  def __init__( self, systemName, level, time, msgText, variableText, frameInfo ):
+
+  def __init__( self, systemName, level, time, msgText, variableText, frameInfo, subSystemName = False ):
     self.systemName = systemName
     self.level = level
     self.time = time
     self.msgText = str( msgText )
     self.variableText = str( variableText )
     self.frameInfo = frameInfo
-    self.subSystemName = False
-    
+    self.subSystemName = subSystemName
+
   def getName( self ):
     return self.systemName
-  
+
   def setName( self, systemName ):
     self.systemName = systemName
-    
+
   def getSubSystemName( self ):
     return self.subSystemName
-  
+
   def setSubSystemName( self, subSystemName ):
     self.subSystemName = subSystemName
-  
+
   def getLevel( self ):
     return self.level
-  
+
   def getTime( self ):
     return self.time
-  
+
   def getMessage( self ):
     messageString = "%s %s" % ( self.getFixedMessage(), self.getVariableMessage() )
     return messageString.strip()
-    
+
   def getFixedMessage( self ):
     return self.msgText
-  
+
   def getVariableMessage( self ):
     if self.variableText:
       return self.variableText
     else:
       return ""
-  
+
   def getFrameInfo( self ):
     return self.frameInfo
-  
+
   def __str__( self ):
     messageString = ""
     for lineString in self.getMessage().split( "\n" ):
       messageString += "%s %s %s: %s" % ( str( self.getTime() ),
                                self.getName(),
-                               self.getLevel().rjust( 6 ), 
+                               self.getLevel().rjust( 6 ),
                                lineString )
     return messageString
-  
+
+  def toTuple( self ):
+    return ( self.systemName,
+             self.level,
+             Time.toString( self.time ),
+             self.msgText,
+             self.variableText,
+             self.frameInfo,
+             self.subSystemName )
