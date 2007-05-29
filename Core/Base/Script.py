7769b74e34152700bdeda28c3bcce2ac8bace7e6
Adria Casajus <adria@ecm.ub.es>
2007-05-29 16:23:19 +0000

    Possibility to specify script name and ignore errors (for the web site)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4504 4525493e-7705-40b1-a816-d608a930855b

--- Core/Base/Script.py
+++ Core/Base/Script.py
@@ -1,5 +1,5 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Base/Script.py,v 1.2 2007/05/16 14:51:19 acasajus Exp $
-__RCSID__ = "$Id: Script.py,v 1.2 2007/05/16 14:51:19 acasajus Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Base/Script.py,v 1.3 2007/05/29 16:23:19 acasajus Exp $
+__RCSID__ = "$Id: Script.py,v 1.3 2007/05/29 16:23:19 acasajus Exp $"
 
 import sys
 import os.path
@@ -8,18 +8,18 @@ from DIRAC.LoggingSystem.Client.Logger import gLogger
 
 localCfg = LocalConfiguration()
 
-def parseCommandLine():
+def parseCommandLine( scriptName = False, ignoreErrors = False ):
   global localCfg
 
-  scriptName = os.path.basename( sys.argv[0] )
+  if not scriptName:
+    scriptName = os.path.basename( sys.argv[0] )
   scriptSection = localCfg.setConfigurationForScript( scriptName )
   localCfg.addMandatoryEntry( "/DIRAC/Setup" )
   resultDict = localCfg.loadUserData()
 
-  if not resultDict[ 'OK' ]:
+  if not ignoreErrors and not resultDict[ 'OK' ]:
     gLogger.error( "There were errors when loading configuration", resultDict[ 'Message' ] )
     sys.exit(1)
-
 
 def registerSwitch( showKey, longKey, helpString, callback = False ):
   global localCfg
