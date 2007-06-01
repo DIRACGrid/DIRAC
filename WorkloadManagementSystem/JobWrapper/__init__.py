dbbbae72d2409e988b702647fe1ea60cdedd13fc
Stuart K. Paterson <stuart.paterson@cern.ch>
2007-06-01 15:50:22 +0000

    Corrected CVS headers...
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4529 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/JobWrapper/__init__.py
+++ WorkloadManagementSystem/JobWrapper/__init__.py
@@ -1,9 +1,9 @@
 ############################################################
-# $Header:
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/JobWrapper/__init__.py,v 1.2 2007/06/01 15:50:22 paterson Exp $
 ############################################################
 
 """
    DIRAC.WorkloadManagementSystem.JobWrapper package
 """
 
-__RCSID__ = "$Id: "
+__RCSID__ = "$Id: __init__.py,v 1.2 2007/06/01 15:50:22 paterson Exp $"
