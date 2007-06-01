00e980165ef7683846c12677992334edc689b3df
Stuart K. Paterson <stuart.paterson@cern.ch>
2007-06-01 16:13:39 +0000

    Watchdog init file.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4530 4525493e-7705-40b1-a816-d608a930855b

---
+++ WorkloadManagementSystem/JobWrapper/Watchdog/__init__.py
@@ -0,0 +1,9 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/JobWrapper/Watchdog/Attic/__init__.py,v 1.1 2007/06/01 16:13:39 paterson Exp $
+############################################################
+
+"""
+   DIRAC.WorkloadManagementSystem.JobWrapper.Watchdog package
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2007/06/01 16:13:39 paterson Exp $"
