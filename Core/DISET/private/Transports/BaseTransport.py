fe240d78634ae030dcb0b003ac6b30dea8948cd7
Adria Casajus <adria@ecm.ub.es>
2007-06-13 19:28:59 +0000

    Packetized data and S_ERROR on fail
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4568 4525493e-7705-40b1-a816-d608a930855b

--- Core/DISET/private/Transports/BaseTransport.py
+++ Core/DISET/private/Transports/BaseTransport.py
@@ -1,5 +1,5 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/DISET/private/Transports/BaseTransport.py,v 1.9 2007/06/13 14:51:36 acasajus Exp $
-__RCSID__ = "$Id: BaseTransport.py,v 1.9 2007/06/13 14:51:36 acasajus Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/DISET/private/Transports/BaseTransport.py,v 1.10 2007/06/13 19:28:59 acasajus Exp $
+__RCSID__ = "$Id: BaseTransport.py,v 1.10 2007/06/13 19:28:59 acasajus Exp $"
 
 from DIRAC.Core.DISET.private.Transports.DEncode import encode, decode
 from DIRAC.LoggingSystem.Client.Logger import gLogger
@@ -52,25 +52,29 @@ class BaseTransport:
     sCodedData = encode( uData )
     dataToSend = "%s:%s" % ( len( sCodedData ), sCodedData )
     for index in range( 0, len( dataToSend ), self.packetSize ):
-      self.oSocket.write( dataToSend[ index : index + self.packetSize ] )
+      self.oSocket.send( dataToSend[ index : index + self.packetSize ] )
 
   def receiveData( self, iMaxLength = 0 ):
-    iSeparatorPosition = self.byteStream.find( ":" )
-    while iSeparatorPosition == -1:
-      sReadData = self._read()
-      if sReadData == "":
-        break
-      self.byteStream += sReadData
+    try:
       iSeparatorPosition = self.byteStream.find( ":" )
-      if iMaxLength and len( self.byteStream ) > iMaxLength and iSeparatorPosition == -1 :
-        raise RuntimeError( "Read limit exceeded (%s chars)" % iMaxLength )
-    size = int( self.byteStream[ :iSeparatorPosition ] )
-    self.byteStream = self.byteStream[ iSeparatorPosition+1: ]
-    while len( self.byteStream ) < size:
-      self.byteStream += self._read()
-      if iMaxLength and len( self.byteStream ) > iMaxLength:
-        raise RuntimeError( "Read limit exceeded (%s chars)" % iMaxLength )
-    data = self.byteStream[ :size ]
-    self.byteStream = self.byteStream[ size + 1: ]
-    return decode( data )[0]
+      while iSeparatorPosition == -1:
+        sReadData = self._read()
+        if sReadData == "":
+          break
+        self.byteStream += sReadData
+        iSeparatorPosition = self.byteStream.find( ":" )
+        if iMaxLength and len( self.byteStream ) > iMaxLength and iSeparatorPosition == -1 :
+          raise RuntimeError( "Read limit exceeded (%s chars)" % iMaxLength )
+      size = int( self.byteStream[ :iSeparatorPosition ] )
+      self.byteStream = self.byteStream[ iSeparatorPosition+1: ]
+      while len( self.byteStream ) < size:
+        self.byteStream += self._read()
+        if iMaxLength and len( self.byteStream ) > iMaxLength:
+          raise RuntimeError( "Read limit exceeded (%s chars)" % iMaxLength )
+      data = self.byteStream[ :size ]
+      self.byteStream = self.byteStream[ size + 1: ]
+      return decode( data )[0]
+    except Exception, e:
+      gLogger.exception( "Network error while receiving data" )
+      return S_ERROR( "Network error while receiving data: %s" % str( e ) )
 
