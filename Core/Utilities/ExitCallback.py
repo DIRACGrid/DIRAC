934a7166fd282b3224eb89637852e855279d52fe
Adria Casajus <adria@ecm.ub.es>
2007-06-14 13:47:37 +0000

    register Callbacks for exiting nicely
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4574 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Utilities/ExitCallback.py
@@ -0,0 +1,35 @@
+import signal
+import os
+
+from DIRAC.LoggingSystem.Client.Logger import gLogger
+
+gCallbackList = []
+
+def registerSignals():
+  """
+  Registers signal handlers
+  """
+  for sigNum in ( signal.SIGINT, signal.SIGQUIT, signal.SIGKILL, signal.SIGTERM ):
+    try:
+      signal.signal( sigNum, execute )
+    except:
+      pass
+
+def execute( exitCode, frame ):
+  """
+  Executes the callback list
+  """
+  for callback in gCallbackList:
+    try:
+      callback( exitCode )
+    except:
+      gLogger.exception( "Exception while calling callback" )
+  os._exit( exitCode )
+
+def registerExitCallback( function ):
+  """
+  Adds a new callback to the list
+  """
+  global gCallbackList
+  if not function in gCallbackList:
+    exitCallbackList.append( function )
