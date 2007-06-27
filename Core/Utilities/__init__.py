f43e1e2fb6a514349884843009dd8c82d6b6c8bb
Adria Casajus <adria@ecm.ub.es>
2007-06-27 15:01:16 +0000

    Missing import of ExitCallback
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4629 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/__init__.py
+++ Core/Utilities/__init__.py
@@ -1,5 +1,5 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/__init__.py,v 1.2 2007/03/12 10:03:20 rgracian Exp $
-__RCSID__ = "$Id: __init__.py,v 1.2 2007/03/12 10:03:20 rgracian Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/__init__.py,v 1.3 2007/06/27 15:01:16 acasajus Exp $
+__RCSID__ = "$Id: __init__.py,v 1.3 2007/06/27 15:01:16 acasajus Exp $"
 """
    DIRAC.Core.Utils package
 """
@@ -10,4 +10,5 @@ from DIRAC.Core.Utilities.Os                 import *
 from DIRAC.Core.Utilities.Subprocess         import shellCall, systemCall, pythonCall
 from DIRAC.Core.Utilities.Time               import *
 from DIRAC.Core.Utilities.ThreadPool         import *
-from DIRAC.Core.Utilities.Tests              import * 
+from DIRAC.Core.Utilities.Tests              import *
+from DIRAC.Core.Utilities.ExitCallback       import *
