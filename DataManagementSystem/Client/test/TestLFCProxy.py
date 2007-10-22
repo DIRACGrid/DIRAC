7606e23b154257235db3a439e3d0a903528a5167
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2007-10-22 13:09:05 +0000

    This is a very basic test script to see whether the LFCProxy service is actually working.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@4979 4525493e-7705-40b1-a816-d608a930855b

---
+++ DataManagementSystem/Client/test/TestLFCProxy.py
@@ -0,0 +1,22 @@
+import unittest,types,time
+from DIRAC.DataManagementSystem.Client.LcgFileCatalogProxyClient import LcgFileCatalogProxyClient
+
+class LFCProxyTestCase(unittest.TestCase):
+  """ Base class for the TransferDB test cases
+  """
+  def setUp(self):
+    self.LFCProxy = LcgFileCatalogProxyClient()
+
+class TestInitCase(LFCProxyTestCase):
+
+  def test_init(self):
+    lfns = ['/lhcb/production/DC06/phys-lumi2/00001620/DST/0000/00001620_00000597_5.dst','/lhcb/production/DC06/phys-lumi2/00001620/DST/0000/00001620_00000598_5.dst','/lhcb/production/DC06/phys-lumi2/00001620/DST/0000/00001620_00000600_5.dst']
+    print lfns
+    res = self.LFCProxy.getPfnsByLfnList(lfns)
+    print res
+
+if __name__ == '__main__':
+
+  suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestInitCase)
+  testResult = unittest.TextTestRunner(verbosity=2).run(suite)
+
