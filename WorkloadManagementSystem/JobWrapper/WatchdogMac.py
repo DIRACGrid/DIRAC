b9ffb69ec61b051cab9fdf295c1e292835b870f0
Stuart K. Paterson <stuart.paterson@cern.ch>
2007-10-29 17:35:45 +0000

    First import of Watchdog instance prototype (for mac).
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5069 4525493e-7705-40b1-a816-d608a930855b

---
+++ WorkloadManagementSystem/JobWrapper/WatchdogMac.py
@@ -0,0 +1,131 @@
+########################################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/JobWrapper/WatchdogMac.py,v 1.1 2007/10/29 17:35:45 paterson Exp $
+# Author: Stuart Paterson
+# eMail : Stuart.Paterson@cern.ch
+########################################################################
+
+"""  The Watchdog class is used by the Job Wrapper to resolve and monitor
+     the system CPU and memory consumed.  The Watchdog can determine if
+     a running job is stalled and indicate this to the Job Wrapper.
+
+     This is the Mac compatible Watchdog subclass.
+"""
+
+from DIRAC.Core.Base.Agent                              import Agent
+from DIRAC.WorkloadManagementSystem.JobWrapper.Watchdog import Watchdog
+from DIRAC.Core.Utilities.Subprocess                    import shellCall
+from DIRAC                                              import S_OK, S_ERROR
+
+import string,re
+
+class WatchdogMac(Watchdog):
+
+  def __init__(self, pid, thread,systemFlag='mac'):
+    """ Constructor, takes system flag as argument.
+    """
+    Watchdog.__init__(self,pid,thread,systemFlag)
+    self.systemFlag = systemFlag
+    self.thread = thread
+    self.pid = pid
+
+  #############################################################################
+  def getNodeInformation(self):
+    """Try to obtain system HostName, CPU, Model, cache and memory.  This information
+       is not essential to the running of the jobs but will be reported if
+       available.
+    """
+    result = S_OK()
+    result['Value']={}
+    comm = 'sysctl -a'
+    parameterDict = shellCall(5,comm)
+    if parameterDict['OK']:
+      info = parameterDict['Value'][1].split('\n')
+      for val in info:
+        if re.search('^kern.hostname',val):
+          hostname = val.split('=')[1].strip()
+          result['Value']['HostName']=hostname
+        if re.search('^hw.model',val):
+          model = val.split('=')[1].strip()
+          result['Value']['Model']=model
+        if re.search('^hw.machine',val):
+          cpu = val.split('=')[1].strip()
+          result['Value']['CPU']=cpu
+        if re.search('^hw.cachelinesize =',val):
+          cache = val.split('=')[1].strip()+'KB'
+          result['Value']['Cache']=cache
+        if re.search('^hw.memsize =',val):
+          memory = str(int(val.split('=')[1].strip())/2**20)+'MB'
+          result['Value']['Memory']=memory
+    else:
+      result = S_ERROR('Could not obtain system information')
+
+    return result
+
+  #############################################################################
+  def getLoadAverage(self):
+    """Obtains the load average.
+    """
+    result = S_OK()
+    comm = 'sysctl vm.loadavg'
+    loadAvgDict = shellCall(5,comm)
+    if loadAvgDict['OK']:
+      la = float(string.split(loadAvgDict['Value'][1])[1])
+      result['Value'] = la
+    else:
+      result = S_ERROR('Could not obtain load average')
+
+    return result
+
+  #############################################################################
+  def getMemoryUsed(self):
+    """Obtains the memory used.
+    """
+    result = S_OK()
+    comm = 'sysctl vm.swapusage'
+    memDict = shellCall(5,comm)
+    if memDict['OK']:
+      mem = string.split(memDict['Value'][1]) [6]
+      if re.search('M$',mem):
+        mem = float(mem.replace('M',''))
+        mem = 2**20*mem
+
+      result['Value'] = float(mem)
+    else:
+      result = S_ERROR('Could not obtain memory used')
+
+    return result
+
+ #############################################################################
+  def getDiskSpace(self):
+    """Obtains the available disk space.
+    """
+    result = S_OK()
+    comm = 'df -P -m .'
+    spaceDict = shellCall(5,comm)
+    if spaceDict['OK']:
+      space = string.split(spaceDict['Value'][1]) [10]
+      result['Value'] = float(space)  # MB
+    else:
+      result = S_ERROR('Could not obtain disk usage')
+
+    return result
+
+ #############################################################################
+  def getCPUConsumed(self,pid):
+    """Obtains the CPU consumed via PID.
+    """
+    result = S_OK()
+    comm = ' ps -p '+str(pid)+' -o time | grep -v TIME'  #ps -p 294 -o cpu | grep -v CPU
+    cpuDict = shellCall(5,comm)
+    if cpuDict['OK']:
+      cpu = string.split(cpuDict['Value'][1]) [0]
+      if re.search('.',cpu):
+        cpu = cpu.replace('.',':')
+      result['Value'] = cpu
+    else:
+      result = S_ERROR('Could not obtain CPU consumed')
+
+    return result
+
+  #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
+
