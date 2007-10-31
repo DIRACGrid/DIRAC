af4fca8d624c2a94b726d45e49f5c9f1b4386d80
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2007-10-31 20:42:03 +0000

    Added tests for new functionalities:
    ls()
    remove()
    removeDir()
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5089 4525493e-7705-40b1-a816-d608a930855b

--- Core/Storage/test/TestStorageFactory.py
+++ Core/Storage/test/TestStorageFactory.py
@@ -4,12 +4,15 @@ from DIRAC.Core.Storage.StorageFactory import StorageFactory
 class StorageFactoryTestCase(unittest.TestCase):
   """ Base class for the StorageFactory test cases
   """
+
+  """
   def test_getStorageName(self):
     factory = StorageFactory()
     initialName = 'RAWFileDestination'
     res = factory.getStorageName(initialName)
     self.assert_(res['OK'])
     self.assertEqual(res['Value'],'CERN-tape')
+  """
 
   def test_getStorage(self):
 
@@ -60,7 +63,7 @@ class StorageFactoryTestCase(unittest.TestCase):
     storageDict['Host'] = 'ccsrmtestv2.in2p3.fr'
     storageDict['Port'] = '8443/srm/managerv2?SFN='
     storageDict['Path'] = '/pnfs/in2p3.fr/data'
-    storageDict['SpaceToken'] = 'LHCb_FAKE'
+    storageDict['SpaceToken'] = 'LHCb_RAW'
     res = storageStub.getParameters()
     self.assert_(res['OK'])
     parameterDict = res['Value']
@@ -81,9 +84,19 @@ class StorageFactoryTestCase(unittest.TestCase):
     self.assertEqual(res['Value'],'srm://ccsrmtestv2.in2p3.fr:8443/srm/managerv2?SFN=/pnfs/in2p3.fr/data/lhcb/production/DC06/test.file')
 
     res = storageStub.remove('srm://ccsrmtestv2.in2p3.fr:8443/srm/managerv2?SFN=/pnfs/in2p3.fr/data/lhcb/production/DC06/test.file')
+    listOfDirs = ['srm://ccsrmtestv2.in2p3.fr:8443/srm/managerv2?SFN=/pnfs/in2p3.fr/data/lhcb/production/DC06/v1-lumi2/00001368/DIGI']
+    res = storageStub.ls(listOfDirs)
+
+    #directoryPath = 'srm://ccsrmtestv2.in2p3.fr:8443/srm/managerv2?SFN=/pnfs/in2p3.fr/data/lhcb/production/DC06/v1-lumi2/1368'
+    #res = storageStub.removeDir(directoryPath)
+
+    destFile = 'srm://ccsrmtestv2.in2p3.fr:8443/srm/managerv2?SFN=/pnfs/in2p3.fr/data/lhcb/production/DC06/v1-lumi2/1368/dirac_directory.7'
+    res = storageStub.put(destFile)
+    print res
+
+    res = storageStub.get(destFile)
 
 if __name__ == '__main__':
   suite = unittest.defaultTestLoader.loadTestsFromTestCase(StorageFactoryTestCase)
   #suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(CreateFTSReqCase))
   testResult = unittest.TextTestRunner(verbosity=2).run(suite)
-
