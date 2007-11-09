68863a186c1edacd8f00cae4ed9b2a8e175808d3
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2007-11-09 13:22:29 +0000

    Using bulk transfer for the whole sandbox
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5168 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/DB/SandboxDB.py
+++ WorkloadManagementSystem/DB/SandboxDB.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/DB/SandboxDB.py,v 1.2 2007/06/27 12:36:17 atsareg Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/DB/SandboxDB.py,v 1.3 2007/11/09 13:22:29 atsareg Exp $
 ########################################################################
 """ SandboxDB class is a simple storage using MySQL as a container for
     relatively small sandbox files. The file size is limited to 16MB.
@@ -7,10 +7,10 @@
 
     addLoggingRecord()
     getJobLoggingInfo()
-    getWMSTimeStamps()    
-"""    
+    getWMSTimeStamps()
+"""
 
-__RCSID__ = "$Id: SandboxDB.py,v 1.2 2007/06/27 12:36:17 atsareg Exp $"
+__RCSID__ = "$Id: SandboxDB.py,v 1.3 2007/11/09 13:22:29 atsareg Exp $"
 
 import re, os, sys
 import time, datetime
@@ -18,7 +18,7 @@ from types import *
 
 from DIRAC  import gLogger, S_OK, S_ERROR
 from DIRAC.ConfigurationSystem.Client.Config import gConfig
-from DIRAC.Core.Base.DB import DB  
+from DIRAC.Core.Base.DB import DB
 
 #############################################################################
 class SandboxDB(DB):
@@ -29,20 +29,28 @@ class SandboxDB(DB):
 
     DB.__init__(self,'ISandboxDB','WorkloadManagement/SandboxDB',maxQueueSize)
 
+    self.maxSize = 16
+    result = gConfig.getOption( self.cs_path+'/MaxSize')
+    if not result['OK']:
+      self.log.warn('Failed to get the Maximum Size limit')
+      self.log.error('Using default value '+str(self.maxSize))
+    else:
+      self.maxSize = int(result['Value'])
+
 #############################################################################
-  def storeSandboxFile(self,jobID,filename,body,sandbox='in'):  
+  def storeFile(self,jobID,filename,body,sandbox='Input'):
     """ Store input sandbox ASCII file for jobID with the name filename which
-        is given with its string body 
+        is given with its string body
     """
-    
-    if len(body) > 16*1024*1024:
+
+    if len(body) > self.Maxsize*1024*1024:
       return S_ERROR('File size too large %.2f MB for file %s' % \
                      (len(body)/1024./1024.,filename))
-    
+
     prefix = "O"
-    if sandbox == 'in':
+    if sandbox == 'Input':
       prefix = "I"
-      
+
     # Check that the file does not exist already
     req = "SELECT FileName FROM %sSandbox WHERE JobID=%d AND FileName='%s'" % \
           (prefix,int(jobID),filename)
@@ -53,56 +61,54 @@ class SandboxDB(DB):
       # Remove the already existing file - overwrite
       self.gLogger.warn('Overwriting file %s for job %d' % (filename,int(jobID)))
       req = "DELETE FROM %sSandbox WHERE JobID=%d AND FileName='%s'" % \
-            (prefix,int(jobID),filename)    
-      result = self._update(req)       
+            (prefix,int(jobID),filename)
+      result = self._update(req)
       if not result['OK']:
-        return result           
-    
-    body = body.replace("\\","\\\\")
-    body = body.replace('\0','\\0')            
-    body = body.replace("'","\\'")
-    body = body.replace('"','\\"')  
-    req = "INSERT INTO %sSandbox(JobId,FileName,FileBody) VALUES (%s,'%s','%s')" % \
-          (prefix,jobID,filename,body)
-    return self._update(req) 
+        return result
+
+    inFields = ['JobID','FileName','FileBody']
+    inValues = [jobID,fname,fileString]
+
+    result = sandboxDB._insert(sandbox_type,inFields,inValues)
+    return result
 
 #############################################################################
-  def getSandboxFile(self,jobID,filename,sandbox='in'):  
+  def getSandboxFile(self,jobID,filename,sandbox='in'):
     """ Store input sandbox ASCII file for jobID with the name filename which
-        is given with its string body 
+        is given with its string body
     """
-    
+
     prefix = "O"
     if sandbox == 'in':
       prefix = "I"
-    
+
     req = "SELECT FileBody FROM %sSandbox WHERE JobID=%d AND FileName='%s'" % \
           (prefix, int(jobID), filename)
-          
+
     result = self._query(req)
     if not result['OK']:
       return result
     if len(result['Value']) == 0:
-      return S_ERROR('Sandbox file not found')       
-      
+      return S_ERROR('Sandbox file not found')
+
     body = result['Value'][0][0]
     return S_OK(body)
-    
+
 #############################################################################
-  def getFileNames(self,jobID,sandbox='in'):     
+  def getFileNames(self,jobID,sandbox='in'):
     """ Get file names for a given job in a given sandbox
-    """ 
-    
+    """
+
     prefix = "O"
     if sandbox == 'in':
       prefix = "I"
-      
+
     req = "SELECT FileName FROM %sSandbox WHERE JobID=%d" % (prefix,int(jobID))
     result = self._query(req)
     if not result['OK']:
       return result
     if len(result['Value']) == 0:
       return S_ERROR('No files found for job %d' % int(jobID))
-      
-    fileList = [ x[0] for x in result['Value']]         
+
+    fileList = [ x[0] for x in result['Value']]
     return S_OK(fileList)
--- WorkloadManagementSystem/Service/SandboxHandler.py
+++ WorkloadManagementSystem/Service/SandboxHandler.py
@@ -1,18 +1,18 @@
 ########################################################################
-# $Id: SandboxHandler.py,v 1.3 2007/11/09 08:13:50 atsareg Exp $
+# $Id: SandboxHandler.py,v 1.4 2007/11/09 13:22:27 atsareg Exp $
 ########################################################################
 
 """ SandboxHandler is the implementation of the Sandbox service
     in the DISET framework
-    
+
     The following methods are available in the Service interface
-    
+
     submitJob()
     rescheduleJob()
-    
+
 """
 
-__RCSID__ = "$Id: SandboxHandler.py,v 1.3 2007/11/09 08:13:50 atsareg Exp $"
+__RCSID__ = "$Id: SandboxHandler.py,v 1.4 2007/11/09 13:22:27 atsareg Exp $"
 
 from types import *
 from DIRAC.Core.DISET.RequestHandler import RequestHandler
@@ -28,17 +28,18 @@ sandbox_type = "Unknown"
 
 def initializeSandboxHandler(serviceInfo):
 
-  global sandboxDB 
+  global sandboxDB
+  global jobDB
   global sandbox_type
-  
+
   sandbox = serviceInfo["serviceName"].split('/')[1]
   if sandbox == "InputSandbox":
     sandbox_type = 'ISandbox'
   elif sandbox == "OutputSandbox":
-    sandbox_type = 'OSandbox'  
+    sandbox_type = 'OSandbox'
   else:
-    return S_ERROR('Uknown sandbox service '+sandbox_type)  
-  
+    return S_ERROR('Uknown sandbox service '+sandbox_type)
+
   sandboxDB = SandboxDB()
   jobDB = JobDB()
   return S_OK()
@@ -48,43 +49,71 @@ class SandboxHandler(RequestHandler):
   def __parsefileID(self, fileID):
     """
     """
-    
-    ind = fileID.find(':')
+
+    ind = fileID.find('::')
     if ind == -1:
       return S_ERROR("Can not get the JobID")
-          
-    try:  
+
+    try:
       jobID = int(fileID[:ind])
-      fname = fileID[ind+1:]
+      fname = fileID[ind+2:]
     except Exception, x:
-      return S_ERROR("Can not get the JobID") 
-      
-    return S_OK((jobID,fname))  
+      return S_ERROR("Can not get the JobID")
+
+    return S_OK((jobID,fname))
 
   def transfer_fromClient( self, fileID, token, fileSize, fileHelper ):
     """ Method to receive bytes from clients.
         fileSize can be Xbytes or -1 if unknown.
     """
 
-    result = self.__parsefileID(fileID) 
+    result = self.__parsefileID(fileID)
     if not result['OK']:
       return result
-      
-    jobID,fname = result['Value']  
-    
+
+    jobID,fname = result['Value']
+
     result = fileHelper.networkToString()
     if result['OK']:
       fileString = result['Value']
-      gLogger.info('Received file %s of size %d for job %d' % 
-                   (fname,len(fileString), jobID)) 
+      gLogger.info('Received file %s of size %d for job %d' %
+                   (fname,len(fileString), jobID))
     else:
-      return result  
-  
-      
-    inFields = ['JobID','FileName','FileBody']  
+      return result
+
+
+    inFields = ['JobID','FileName','FileBody']
     inValues = [jobID,fname,fileString]
-      
-    result = sandboxDB._insert(sandbox_type,inFields,inValues) 
+
+    result = sandboxDB._insert(sandbox_type,inFields,inValues)
+    if not result['OK']:
+      if result['Message'].find('Duplicate entry') != -1:
+        return S_ERROR('InputSandbox file %s for job %d already exists' % (fname,jobID))
+    return result
+
+  def transfer_bulkFromClient( self, fileID, token, fileHelper ):
+    """ Receive files packed into a tar archive by the fileHelper logic.
+        token is used for access rights confirmation.
+    """
+
+    result = self.__parsefileID(fileID)
+    if not result['OK']:
+      return result
+
+    jobID,fname = result['Value']
+
+    result = fileHelper.networkToString(dir_path)
+    if result['OK']:
+      fileString = result['Value']
+      gLogger.info('Received file %s of size %d for job %d' %
+                   (fname,len(fileString), jobID))
+    else:
+      return result
+
+    inFields = ['JobID','FileName','FileBody']
+    inValues = [jobID,fname,fileString]
+
+    result = sandboxDB.storeFile(jobID,fname,fileString,sandbox_type)
     if not result['OK']:
       if result['Message'].find('Duplicate entry') != -1:
         return S_ERROR('InputSandbox file %s for job %d already exists' % (fname,jobID))
@@ -94,49 +123,49 @@ class SandboxHandler(RequestHandler):
     """ Method to send bytes to clients.
     """
 
-    result = self.__parsefileID(fileID) 
+    result = self.__parsefileID(fileID)
     if not result['OK']:
       return result
-      
-    jobID,fname = result['Value']     
-    
+
+    jobID,fname = result['Value']
+
     req = "SELECT FileBody from %s where JobID=%d and FileName='%s'" % \
           (sandbox_type,jobID,fname)
-    
+
     result = sandboxDB._query(req)
     if not result['OK']:
       return result
-      
+
     if len(result['Value']) > 0:
-      fileString = result['Value'][0][0]    
+      fileString = result['Value'][0][0]
     else:
       fileString = ''
-    
+
     result = fileHelper.stringToNetwork(fileString)
-    
+
     if not fileString:
-      return S_ERROR('File not found')  
-    
+      return S_ERROR('File not found')
+
     if result['OK']:
-      gLogger.info('Sent file %s of size %d' % (fileID,len(fileString))) 
+      gLogger.info('Sent file %s of size %d' % (fileID,len(fileString)))
     else:
       return result
-    
+
     return S_OK()
-    
+
   types_setSandboxReady = [ IntType, StringType ]
   def export_setSandboxReady(self,jobID,stype="Input"):
     """  Set the sandbox ready for the job with jobID
-    """   
-    
+    """
+
     return jobDB.setSandboxReady(jobID,stype)
-    
+
   types_removeSandbox = [ IntType ]
-  def export_removeFile(self,jobID,stype="Input"):  
+  def export_removeSandbox(self,jobID,stype="Input"):
     """ Remove sandbox for the given job
     """
-    
+
     return sandboxDB.removeSandbox(jobID,fname,stype)
-    
-    
-    
+
+
+
