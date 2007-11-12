58527d7e8841893bae5641028bb6bf35ca9697fe
Stuart K. Paterson <stuart.paterson@cern.ch>
2007-11-12 21:56:11 +0000

    Now I know why my unit test wasn't working ;)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5212 4525493e-7705-40b1-a816-d608a930855b

---
+++ Interfaces/__init__.py
@@ -0,0 +1,10 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Interfaces/__init__.py,v 1.1 2007/11/12 21:56:11 paterson Exp $
+############################################################
+
+"""
+DIRAC.Interfaces package
+
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2007/11/12 21:56:11 paterson Exp $"
