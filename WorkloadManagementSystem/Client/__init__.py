2686b40d45c6b20bd95a2365458fceb5abd5b9cc
Stuart K. Paterson <stuart.paterson@cern.ch>
2007-11-15 12:52:54 +0000

    To make WMS job submission work :)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5217 4525493e-7705-40b1-a816-d608a930855b

---
+++ WorkloadManagementSystem/Client/__init__.py
@@ -0,0 +1,9 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Client/__init__.py,v 1.1 2007/11/15 12:52:54 paterson Exp $
+############################################################
+
+"""
+   DIRAC.WorkloadManagementSystem.Client package
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2007/11/15 12:52:54 paterson Exp $"
