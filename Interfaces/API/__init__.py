48005fa228aec20b5db89bc21ddb8752b160f4a2
Stuart K. Paterson <stuart.paterson@cern.ch>
2007-11-15 21:37:32 +0000

    API package __init__ file.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5225 4525493e-7705-40b1-a816-d608a930855b

---
+++ Interfaces/API/__init__.py
@@ -0,0 +1,29 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Interfaces/API/__init__.py,v 1.1 2007/11/15 21:37:32 paterson Exp $
+############################################################
+
+"""
+DIRAC.Interfaces.API package
+
+DIRAC - Distributed Infrastructure With Remote Agent Control
+
+   DIRAC is a system which allows to integrate various distributed
+   computing resources in a single system which is used for MC data
+   production, data reprocessing and final user analysis. It consists
+   of the following main subsystems:
+
+     - Workload Management System
+     - Data Management System
+     - Information Services
+     - Tools and utilities
+
+   The DIRAC API provides a transparent and secure way for users to submit
+   jobs to the Grid. While it may be exploited directly by users, the DIRAC API also
+   serves as the interface for the Ganga Grid front-end to perform distributed user
+   analysis for LHCb.  DIRAC Jobs are submitted through the API and are constructed
+   using the DIRAC Job(), Step() and Module() infrastructure.
+
+
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2007/11/15 21:37:32 paterson Exp $"
