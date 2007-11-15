2664cc082d2058e20ec516f7f3542940383834bc
Adria Casajus <adria@ecm.ub.es>
2007-11-15 11:10:42 +0000

    Much faster encoding and decoding
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5216 4525493e-7705-40b1-a816-d608a930855b

--- Core/DISET/private/Transports/DEncode.py
+++ Core/DISET/private/Transports/DEncode.py
@@ -1,5 +1,5 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/DISET/private/Transports/Attic/DEncode.py,v 1.4 2007/09/20 17:50:37 acasajus Exp $
-__RCSID__ = "$Id: DEncode.py,v 1.4 2007/09/20 17:50:37 acasajus Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/DISET/private/Transports/Attic/DEncode.py,v 1.5 2007/11/15 11:10:42 acasajus Exp $
+__RCSID__ = "$Id: DEncode.py,v 1.5 2007/11/15 11:10:42 acasajus Exp $"
 
 # Encoding and decoding for dirac
 #
@@ -29,99 +29,108 @@ g_dEncodeFunctions = {}
 g_dDecodeFunctions = {}
 
 #Encoding and decoding ints
-def encodeInt( iValue ):
-  return "i%se" % iValue
+def encodeInt( iValue, eList ):
+  eList.extend( ( "i", str( iValue ), "e" ) )
 
-def decodeInt( sStream, iIndex ):
-  iEndPos = sStream[ iIndex: ].find( "e" )
-  sNumberString = sStream[ iIndex + 1: iIndex + iEndPos ]
-  return ( int( sNumberString ), iIndex + iEndPos + 1 )
+def decodeInt( buffer, i ):
+  i += 1
+  end  = buffer.index( 'e', i )
+  n = int( buffer[i:end] )
+  return ( n, end + 1 )
 
 g_dEncodeFunctions[ types.IntType ] = encodeInt
 g_dDecodeFunctions[ "i" ] = decodeInt
 
 #Encoding and decoding longs
-def encodeLong( iValue ):
-  return "I%se" % iValue
+def encodeLong( iValue, eList ):
+  eList.extend( ( "l", str( iValue ), "e" ) )
 
-def decodeLong( sStream, iIndex ):
-  iEndPos = sStream[ iIndex: ].find( "e" )
-  sNumberString = sStream[ iIndex + 1: iIndex + iEndPos ]
-  return ( long( sNumberString ), iIndex + iEndPos + 1 )
+def decodeLong( buffer, i ):
+  i += 1
+  end  = buffer.index( 'e', i )
+  n = long( buffer[i:end] )
+  return ( n, end + 1 )
 
 g_dEncodeFunctions[ types.LongType ] = encodeLong
 g_dDecodeFunctions[ "I" ] = decodeLong
 
 #Encoding and decoding floats
-def encodeFloat( fValue ):
-  return "f%se" % fValue
+def encodeFloat( iValue, eList ):
+  eList.extend( ( "f", str( iValue ), "e" ) )
 
-def decodeFloat( sStream, iIndex ):
-  iEndPos = sStream[ iIndex: ].find( "e" )
-  sNumberString = sStream[ iIndex + 1: iIndex + iEndPos ]
-  return ( float( sNumberString ), iIndex + iEndPos + 1 )
+def decodeFloat( buffer, i ):
+  i += 1
+  end  = buffer.index( 'e', i )
+  n = float( buffer[i:end] )
+  return ( n, end + 1 )
 
 g_dEncodeFunctions[ types.FloatType ] = encodeFloat
 g_dDecodeFunctions[ "f" ] = decodeFloat
 
 #Encoding and decoding booleand
-def encodeBool( bValue ):
+def encodeBool( bValue, eList ):
   if bValue:
-    return "b1"
+    eList.append( "b1" )
   else:
-    return "b0"
+    eList.append( "b0" )	
 
-def decodeBool( sStream, iIndex ):
-  if sStream[ iIndex + 1 ] == "0":
-    return ( False, iIndex + 2 )
+def decodeBool( buffer, i ):
+  if buffer[ i + 1 ] == "0":
+    return ( False, i + 2 )
   else:
-    return ( True, iIndex + 2 )
+    return ( True, i + 2 )
 
 g_dEncodeFunctions[ types.BooleanType ] = encodeBool
 g_dDecodeFunctions[ "b" ] = decodeBool
 
 #Encoding and decoding strings
-def encodeString( sValue ):
-  return "s%s:%s" % ( len( sValue), sValue )
+def encodeString( sValue, eList ):
+  eList.extend( ( 's', str( len( sValue ) ), ':', sValue ) ) 
 
-def decodeString( sStream, iIndex ):
-  iSeparatorPosition = sStream[ iIndex + 1: ].find( ":" )
-  iLength = int( sStream[ iIndex + 1 : iIndex + 1 + iSeparatorPosition ] )
-  iStringStart = iIndex + 2 + iSeparatorPosition
-  iStringEnd = iIndex + 2 + iSeparatorPosition + iLength
-  return ( sStream[ iStringStart: iStringEnd ], iStringEnd )
+def decodeString( buffer, i ):
+  i += 1
+  colon = buffer.index( ":", i )
+  n = int( buffer[ i : colon ] )
+  colon += 1
+  end = colon + n
+  return ( buffer[ colon : end] , end )
 
 g_dEncodeFunctions[ types.StringType ] = encodeString
 g_dDecodeFunctions[ "s" ] = decodeString
 
 #Encoding and decoding datetime
-def encodeDateTime( oValue ):
+def encodeDateTime( oValue, eList ):
   prefix = "z"
   if type( oValue ) == _dateTimeType:
     tDateTime = ( oValue.year, oValue.month, oValue.day, \
                       oValue.hour, oValue.minute, oValue.second, \
                       oValue.microsecond, oValue.tzinfo )
-    return "%sa%s" % ( prefix, encode( tDateTime ) )
+    eList.append( "za" )
+    encode( tDateTime, eList )
   elif type( oValue ) == _dateType:
     tData = ( oValue.year, oValue.month, oValue. day )
-    return "%sd%s" % ( prefix, encode( tData ) )
+    eList.append( "zd" )
+    encode( tData, eList )
   elif type( oValue ) == _timeType:
     tTime = ( oValue.hour, oValue.minute, oValue.second, oValue.microsecond, oValue.tzinfo )
-    return "%st%s" % ( prefix, encode( tTime ) )
+    eList.append( "zt" )
+    encode( tTime, eList )
   else:
     raise Exception( "Unexpected type %s while encoding a datetime object" % str( type( oValue ) ) )
 
-def decodeDateTime( sStream, iIndex ):
-  tupleObject, endIndex = decode( sStream, iIndex + 2 )
-  if sStream[ iIndex + 1] == 'a':
+def decodeDateTime( buffer, i ):
+  i += 1
+  type = buffer[i]
+  tupleObject, i = decode( buffer, i + 1 )
+  if type == 'a':
     dtObject = datetime.datetime( *tupleObject )
-  elif sStream[ iIndex + 1 ] == 'd':
+  elif stype == 'd':
     dtObject = datetime.date( *tupleObject )
-  elif sStream[ iIndex + 1 ] == 't':
+  elif type == 't':
     dtObject = datetime.time( *tupleObject )
   else:
-    raise Exception( "Unexpected type %s while decoding a datetime object" % sStream[ iIndex + 1 ] )
-  return ( dtObject, endIndex )
+    raise Exception( "Unexpected type %s while decoding a datetime object" % type )
+  return ( dtObject, i )
 
 g_dEncodeFunctions[ _dateTimeType ] = encodeDateTime
 g_dEncodeFunctions[ _dateType ] = encodeDateTime
@@ -129,65 +138,62 @@ g_dEncodeFunctions[ _timeType ] = encodeDateTime
 g_dDecodeFunctions[ 'z' ] = decodeDateTime
 
 #Encoding and decoding None
-def encodeNone( oValue ):
-  return "n"
+def encodeNone( oValue, eList ):
+  eList.append( "n" )
 
-def decodeNone( sStream, iIndex ):
-  return ( None, iIndex + 1 )
+def decodeNone( buffer, i ):
+  return ( None, i + 1 )
 
 g_dEncodeFunctions[ types.NoneType ] = encodeNone
 g_dDecodeFunctions[ 'n' ] = decodeNone
 
 #Encode and decode a list
-def encodeList( lValue ):
-  sListString = "l"
+def encodeList( lValue, eList ):
+  eList.append( "l" )
   for uObject in lValue:
-    sListString += encode( uObject )
-  return "%se" % sListString
+    g_dEncodeFunctions[ type( uObject ) ]( uObject, eList ) 
+  eList.append( "e" )
 
-def decodeList( sStream, iIndex ):
-  lObjects = []
-  iIndex += 1
-  while sStream[ iIndex ] != "e":
-    uObject, iIndex = decode( sStream, iIndex )
-    lObjects.append( uObject )
-  return( lObjects, iIndex + 1 )
-
+def decodeList( buffer, i ):
+  oL = []
+  i += 1
+  while buffer[ i ] != "e":
+    ob, i = g_dDecodeFunctions[ buffer[ i ] ]( buffer, i )
+    oL.append( ob )
+  return( oL, i + 1 )
 
 g_dEncodeFunctions[ types.ListType ] = encodeList
 g_dDecodeFunctions[ "l" ] = decodeList
 
-
 #Encode and decode a tuple
-def encodeTuple( lValue ):
-    sTupleString = "t"
-    for uObject in lValue:
-      sTupleString += encode( uObject )
-    return "%se" % sTupleString
+def encodeTuple( lValue, eList ):
+  eList.append( "t" )
+  for uObject in lValue:
+    g_dEncodeFunctions[ type( uObject ) ]( uObject, eList )
+  eList.append( "e" )
 
-def decodeTuple( sStream, iIndex ):
-  lObjects, iIndex = decodeList( sStream, iIndex )
-  return ( tuple( lObjects ), iIndex )
+def decodeTuple( buffer, i ):
+  oL, i = decodeList( buffer, i )
+  return ( tuple( oL ), i )
 
 g_dEncodeFunctions[ types.TupleType ] = encodeTuple
 g_dDecodeFunctions[ "t" ] = decodeTuple
 
 #Encode and decode a dictionary
-def encodeDict( dValue ):
-  sDictString = "d"
-  for uKey in dValue.keys():
-    sDictString += encode( uKey )
-    sDictString += encode( dValue[ uKey ] )
-  return "%se" % sDictString
+def encodeDict( dValue, eList ):
+  eList.append( "d" )
+  for key, value in dValue.items():
+    g_dEncodeFunctions[ type( key ) ]( key, eList )
+    g_dEncodeFunctions[ type( value ) ]( value, eList )	
+  eList.append( "e" )
 
-def decodeDict( sStream, iIndex ):
-  dObjects = {}
-  iIndex += 1
-  while sStream[ iIndex ] != "e":
-    uKey, iIndex = decode( sStream, iIndex )
-    uValue, iIndex = decode( sStream, iIndex )
-    dObjects[ uKey ] = uValue
-  return ( dObjects, iIndex + 1 )
+def decodeDict( buffer, i ):
+  oD = {}
+  i += 1
+  while buffer[ i ] != "e":
+    k, i = g_dDecodeFunctions[ buffer[ i ] ]( buffer, i )
+    oD[ k ], i = g_dDecodeFunctions[ buffer[ i ] ]( buffer, i )
+  return ( oD, i + 1 )
 
 g_dEncodeFunctions[ types.DictType ] = encodeDict
 g_dDecodeFunctions[ "d" ] = decodeDict
@@ -196,27 +202,26 @@ g_dDecodeFunctions[ "d" ] = decodeDict
 #Encode function
 def encode( uObject ):
   try:
+    eList = []
     #print "ENCODE FUNCTION : %s" % g_dEncodeFunctions[ type( uObject ) ]
-    return g_dEncodeFunctions[ type( uObject ) ]( uObject )
+    g_dEncodeFunctions[ type( uObject ) ]( uObject, eList )
+    return "".join( eList )
   except Exception, e:
     raise
 
-def decode( sStream, iIndex = 0 ):
+def decode( buffer ):
   try:
     #print "DECODE FUNCTION : %s" % g_dDecodeFunctions[ sStream [ iIndex ] ]
-    return g_dDecodeFunctions[ sStream[ iIndex ] ]( sStream, iIndex )
+    return g_dDecodeFunctions[ buffer[ 0 ] ]( buffer, 0 )
   except Exception, e:
     raise
 
 
 if __name__=="__main__":
-  uObject = {
-    1 : { 2 : 3 },
-    4 : { 5 : 6 }
-   }
-  print uObject
+  uObject = {2:"3", True : (3,None) }
+  print "Initial: %s" % uObject
   sData = encode( uObject )
-  print sData
-  print decode( sData )
+  print "Encoded: %s" % sData
+  print "Decoded: %s, [%s]" % decode( sData )
 
 
