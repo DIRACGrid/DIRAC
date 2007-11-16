afb958d9204ef35a5b437f2397ca6ca9d5261554
Stuart K. Paterson <stuart.paterson@cern.ch>
2007-11-16 12:50:25 +0000

    Update of JobSanityAgent from eclipse before testing.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5239 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/Agent/JobSanityAgent.py
+++ WorkloadManagementSystem/Agent/JobSanityAgent.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Agent/JobSanityAgent.py,v 1.3 2007/11/07 17:25:19 paterson Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Agent/JobSanityAgent.py,v 1.4 2007/11/16 12:50:25 paterson Exp $
 # File :   JobSanityAgent.py
 # Author : Stuart Paterson
 ########################################################################
@@ -13,7 +13,7 @@
        - Input sandbox not correctly uploaded.
 """
 
-__RCSID__ = "$Id: JobSanityAgent.py,v 1.3 2007/11/07 17:25:19 paterson Exp $"
+__RCSID__ = "$Id: JobSanityAgent.py,v 1.4 2007/11/16 12:50:25 paterson Exp $"
 
 from DIRAC.WorkloadManagementSystem.DB.JobDB        import JobDB
 from DIRAC.Core.Utilities.ClassAd.ClassAdLight      import ClassAd
@@ -40,6 +40,8 @@ class JobSanityAgent(Agent):
     """
     result = Agent.initialize(self)
     self.jobDB = JobDB()
+    self.optimizerName     = 'JobSanity'
+    self.nextOptimizerName = 'InputData'
 
     self.dbg = False
     if gConfig.getValue(self.section+'/LogLevel','INFO') == 'DEBUG':
@@ -47,20 +49,36 @@ class JobSanityAgent(Agent):
       self.log.setLevel(logging.DEBUG)
 
     #Test control flags N.B. JDL check is mandatory
-    self.inputDataCheck    = gConfig.getValue(self.section+'/InputDataCheck',0)
+    self.inputDataCheck    = gConfig.getValue(self.section+'/InputDataCheck',1)
     self.outputDataCheck   = gConfig.getValue(self.section+'/OutputDataCheck',0)
     self.inputSandboxCheck = gConfig.getValue(self.section+'/InputSandboxCheck',0)
-    self.platformCheck     = gConfig.getValue(self.section+'/PlatformCheck',1)
+    self.platformCheck     = gConfig.getValue(self.section+'/PlatformCheck',0)
     #Other parameters
-    self.pollingTime     = gConfig.getValue(self.section+'/PollingTime',60)
-    self.initialJobState = gConfig.getValue(self.section+'/InitialJobStatus','received')
-    self.finalJobState   = gConfig.getValue(self.section+'/FinalJobStatus','checked')
-    self.successStatus   = gConfig.getValue(self.section+'/SuccessfulJobStatus','outputready')
-    self.maxDataPerJob   = gConfig.getValue(self.section+'/MaxInputDataPerJob',200)
-    self.jobCheckDelay   = gConfig.getValue(self.section+'/JobCheckingDelay',10)
+    self.pollingTime          = gConfig.getValue(self.section+'/PollingTime',10)
+    self.jobStatus            = gConfig.getValue(self.section+'/JobStatus','Checking')
+    self.minorStatus          = gConfig.getValue(self.section+'/InitialJobMinorStatus',self.optimizerName)
+    self.nextOptMinorStatus   = gConfig.getValue(self.section+'/FinalJobMinorStatus',self.nextOptimizerName)
+    self.successStatus        = gConfig.getValue(self.section+'/SuccessfulJobStatus','OutputReady')
+    self.maxDataPerJob        = gConfig.getValue(self.section+'/MaxInputDataPerJob',200)
+    self.jobCheckDelay        = gConfig.getValue(self.section+'/JobCheckingDelay',5)
+    self.failedJobStatus      = gConfig.getValue(self.section+'/FailedJobStatus','failed')
+
+    infosys = gConfig.getValue(self.section,'LCG_GFAL_INFOSYS','lcg-bdii.cern.ch:2170')
+    host    = gConfig.getValue(self.section,'LFC_HOST','lhcb-lfc.cern.ch')
+
+#needed for the output data check
+#    try:
+#      from DIRAC.DataManagement.Client.LcgFileCatalogCombinedClient import LcgFileCatalogCombinedClient
+#      self.FileCatalog = LcgFileCatalogCombinedClient()
+#      self.log.debug("Instantiating LFC File Catalog in mode %s %s %s" % (mode,host,infosys) )
+#    except Exception,x:
+#      msg = "Failed to create LcgFileCatalogClient"
+#      self.log.fatal(msg)
+#      self.log.fatal(str(x))
+#      result = S_ERROR(msg)
 
     self.log.debug( '==========================================='          )
-    self.log.debug( 'DIRAC Job Sanity Agent is started with     '          )
+    self.log.debug( 'DIRAC '+self.optimizerName+' Agent is started with '  )
     self.log.debug( 'the following parameters and checks:'                 )
     self.log.debug( '==========================================='          )
     self.log.debug( 'JDL Check          ==>  Enabled'                      )
@@ -82,11 +100,12 @@ class JobSanityAgent(Agent):
       self.log.debug( 'Platform Check  ==>  Disabled'                      )
     self.log.debug( '==========================================='          )
     self.log.debug( 'Polling Time       ==> %s' % self.pollingTime         )
-    self.log.debug( 'Initial Job Status ==> %s' % self.initialJobState     )
-    self.log.debug( 'Final Job Status   ==> %s' % self.finalJobState       )
-    self.log.debug( 'Max Data Per Job   ==> %s' % self.initialJobState     )
+    self.log.debug( 'Job Status         ==> %s' % self.jobStatus           )
+    self.log.debug( 'Job Minor Status   ==> %s' % self.minorStatus         )
+    self.log.debug( 'Max Data Per Job   ==> %s' % self.maxDataPerJob       )
     self.log.debug( 'Successful Status  ==> %s' % self.successStatus       )
     self.log.debug( 'Job Check Delay    ==> %s' % self.jobCheckDelay       )
+    self.log.debug( 'Failed Job Status  ==> %s' % self.failedJobStatus     )
     self.log.debug( '==========================================='          )
 
     return result
@@ -105,19 +124,21 @@ class JobSanityAgent(Agent):
         conditions such as a delay before checking to
         be inserted for all tests.
     """
-    status = self.initialJobState
     delay = self.jobCheckDelay
-    result = self.jobDB.selectJobWithStatus(status)
-    if self.dbg:
-      print result
 
+    condition = {'Status':self.jobStatus,'MinorStatus':self.minorStatus}
+    result = self.jobDB.selectJobs(condition)
     if not result['OK']:
-      self.log.error('Error retrieving job information from JobDB')
-      return result
+      self.log.error('Failed to get a job list from the JobDB')
+      return S_ERROR('Failed to get a job list from the JobDB')
+
+    if not len(result['Value']):
+      self.log.debug('No pending jobs to process')
+      return S_OK('No work to do')
 
     jobs = result['Value']
     for job in jobs:
-      jobAttributes = self.jobDB.getAllJobAttributes(job)
+      jobAttributes = self.jobDB.getJobAttributes(job)
       if jobAttributes['OK']:
         timeDict = jobAttributes['Value']
         print timeDict
@@ -132,9 +153,9 @@ class JobSanityAgent(Agent):
             msg = 'Job %s, submitted less than %s seconds ago' % (job,delay)
             self.log.info(msg)
         else:
-          self.log.error('No job attributes found for job '+str(job))
+          self.log.error('No job attributes found for job %s' %(job))
       else:
-        self.log.warn('JobDB returned error for job '+str(job))
+        self.log.warn('JobDB returned error for job %s' %(job))
         print jobAttributes
 
     return S_OK('Successfully checked jobs')
@@ -147,7 +168,7 @@ class JobSanityAgent(Agent):
         any other potential checks.
     """
 
-     #Job JDL check
+    #Job JDL check
     message = 'Job: '+str(job)+' '
     self.log.debug('Checking Loop Starts for job: '+str(job))
     checkJDL = self.checkJDL(job)
@@ -156,16 +177,16 @@ class JobSanityAgent(Agent):
     else:
       res = 'Job: '+str(job)+' Failed JDL check.'
       minorStatus = checkJDL['Value']
-      self.updateJobStatus(job,'failed',minorStatus)
+      self.updateJobStatus(job,self.failedJobStatus,minorStatus)
       self.log.info(res)
       return
 
     jdl = checkJDL['JDL']
     classadJob = ClassAd('['+jdl+']')
-   # jobType = classadJob.get_expression('JobType').replace('"','')
+    jobType = classadJob.get_expression('JobType').replace('"','')
 
     #Input data check
-    if self.inputDataCheck: # disabled
+    if self.inputDataCheck:
       inputData = self.checkInputData(job)
       if inputData['OK']:
         number = inputData['Value']
@@ -173,12 +194,12 @@ class JobSanityAgent(Agent):
       else:
         res = 'Job: '+str(job)+' Failed input data check.'
         minorStatus = inputData['Value']
-        self.updateJobStatus(job,'failed',minorStatus)
+        self.updateJobStatus(job,self.failedJobStatus,minorStatus)
         self.log.info(message)
         self.log.info(res)
         return
 
-    #Platform check
+    #Platform check # disabled
     if self.platformCheck:
       platform = self.checkPlatformSupported(job,jdl)
       if platform['OK']:
@@ -187,7 +208,7 @@ class JobSanityAgent(Agent):
       else:
         res = 'No supported platform for job '+str(job)+'.'
         minorStatus = platform['Value']
-        self.updateJobStatus(job,'failed',minorStatus)
+        self.updateJobStatus(job,self.failedJobStatus,minorStatus)
         self.log.info(message)
         self.log.info(res)
         return
@@ -212,7 +233,7 @@ class JobSanityAgent(Agent):
         else:
           res = 'Job: '+str(job)+' Failed since output data exists.'
           minorStatus=outputData['Value']
-          self.updateJobStatus(job,'failed', minorStatus)
+          self.updateJobStatus(job,self.failedJobStatus, minorStatus)
           self.log.info(message)
           self.log.info(res)
           return
@@ -226,131 +247,54 @@ class JobSanityAgent(Agent):
       else:
         res = 'Job: '+str(job)+' Failed since input sandbox not uploaded.'
         minorStatus=inputSandbox['Value']
-        self.updateJobStatus(job,'failed', minorStatus)
+        self.updateJobStatus(job,self.failedJobStatus, minorStatus)
         self.log.info(message)
         self.log.info(res)
         return
 
-    success = self.finalJobState
     self.log.info(message)
     self.setJobParam(job,'JobSanityCheck',message)
-    self.updateJobStatus(job,success)
-
-
-  #############################################################################
-  def updateJobStatus(self,job,status,minorstatus=None):
-    """This method updates the job status in the JobDB.
-    """
-    self.log.debug("self.jobDB.setJobAttribute("+str(job)+",Status,"+status+" update=True)")
-   # self.jobDB.setJobAttribute(job,'Status',status, update=True)
-    if minorstatus:
-      self.log.debug("self.jobDB.setJobAttribute("+str(job)+","+minorstatus+",update=True)")
-    #  self.jobDB.setJobAttribute(job,'ApplicationStatus',minorstatus,update=True)
-
-  #############################################################################
-  def setJobParam(self,job,reportName,value):
-    """This method updates a job parameter in the JobDB.
-    """
-    self.log.debug("self.jobDB.setJobParameter("+str(job)+","+reportName+","+value+")")
-    self.jobDB.setJobParameter(job,reportName,value)
+    result = self.updateJobStatus(job,self.jobStatus,self.nextOptMinorStatus)
+    if not result['OK']:
+      self.log.error(result['Message'])
 
   #############################################################################
   def checkJDL(self,job):
     """Checks JDL is OK for Job.
     """
-    jobID = str(job)
-    self.log.debug("Checking JDL for job: "+jobID)
-    retVal = self.jobDB.getJobJDL(jobID)
-    if not retVal['OK']:    
+    self.log.debug("Checking JDL for job: %s" %(job))
+    retVal = self.jobDB.getJobJDL(job)
+    if not retVal['OK']:
       self.log.warn(retVal['Message'])
-      retVal = self.jobDB.getJobJDL(jobID,original=True)
-      self.log.warn('Could not get current JDL from JobDB, trying original')      
+      retVal = self.jobDB.getJobJDL(job,original=True)
+      self.log.warn('Could not get current JDL from JobDB, trying original')
       if not retVal['OK']:
-        self.log.warn(retVal['Message'])  
+        self.log.warn(retVal['Message'])
 
     if not retVal['OK']:
       result = S_ERROR()
       result['Value'] = "Job JDL not found in JobDB"
       return result
 
-    jdl = retVal['Value'] 
-      
+    jdl = retVal['Value']
+
     if not jdl:
-      self.log.debug("Warning: JDL not found for job "+jobID+", job will be marked problematic")
+      self.log.debug("Warning: JDL not found for job %s, will be marked problematic" % (job))
       result = S_ERROR()
       result['Value'] = "Job JDL Not Found"
       return result
 
     classadJob = ClassAd('['+jdl+']')
     if not classadJob.isOK():
-      self.log.debug("Warning: illegal JDL for job"+jobID+", job will be marked problematic")
+      self.log.debug("Warning: illegal JDL for job %s, will be marked problematic" % (job))
       result = S_ERROR()
       result['Value'] = "Illegal Job JDL"
       return result
     else:
-      result = S_OK()
+      param = classadJob
+
+      result = S_OK('JDL OK')
       result['JDL'] = jdl
-      return result
-
-  #############################################################################
-  def checkPlatformSupported(self,job,jdl):
-    """This method queries the CS for available platforms
-       supported by DIRAC and will check these against what
-       the job requests.
-    """
-    classadJob = ClassAd('['+jdl+']')
-    architecture = classadJob.get_expression("CompatiblePlatforms").replace('"','')
-    if not architecture:
-      result = S_OK()
-      msg = 'No architecture requirement'
-      result['Value'] = msg
-      return result
-
-    if architecture == 'ANY':
-      result = S_OK()
-      result['Value'] = architecture
-      return result
-    else:
-      okPlatforms = cfgSvc.get('OS_Compatibility',architecture,'NA')
-      if okPlatforms != 'NA':
-        result = S_OK()
-        result['Value'] = architecture
-        return result
-      else:
-        result = S_ERROR()
-        result['Value'] = architecture+" not supported"
-        return result
-
-  #############################################################################
-  def checkInputSandbox(self,job,jdl):
-    """The number of input sandbox files, as specified in the job
-       JDL are checked in the JobDB.
-    """
-    result =  self.jobDB.getInputSandboxFileCount(job)
-    if self.dbg:
-      print result
-    if result['OK']:
-      dbCount = result['Value'][0][0]
-      self.log.debug('Found '+str(dbCount)+' files in DB for job '+str(job))
-
-      classadJob = ClassAd('['+jdl+']')
-      sandbox = ClassAddStringListToPythonList(classadJob.get_expression("InputSandbox"))
-      if self.dbg:
-        print sandbox
-      jdlCount = len(sandbox)
-      self.log.debug('Found '+str(jdlCount)+' files in JDL for job '+str(job))
-
-      if jdlCount > dbCount:
-        result = S_ERROR()
-        message = 'Input Sandbox not uploaded '+str(dbCount)+' / '+str(jdlCount)
-        result['Value'] = message
-        return result
-      else:
-        result = S_OK()
-        number = str(dbCount)+' / '+str(jdlCount)
-        result['Value'] = number
-        return result
-    else:
       return result
 
   #############################################################################
@@ -359,8 +303,60 @@ class JobSanityAgent(Agent):
        datasets for the job and whether the LFN conventions
        are correct.
     """
-    #To implement
-    return S_OK()
+    setup = self.setup
+    maxData = int(self.maxDataPerJob)
+    totalData = 0
+    slashFlag = 0
+    incorrectDataFlag = 0
+
+    result = self.jobDB.getInputData(job)
+
+    if not result['OK']:
+      self.log.error('Failed to get input data from JobdB for %s' %(job) )
+      self.log.error(result['Message'])
+
+    if not result['Value']:
+      self.log.debug('Job %s has no input data requirement' % (job))
+      if not result['OK']:
+        self.log.error(result['Message'])
+        return result
+
+    self.log.debug('Job %s has an input data requirement and will be checked' % (job))
+    inputData = result['Value']
+    self.log.debug('Data is: ')
+    if self.dbg:
+      for i in data: print i
+
+    totalData = len(inputData)
+    if totalData > maxData:
+      message = '%s datasets selected. Max limit is %s.'  % (totalData,maxData)
+      self.setJobParam(job,'DatasetCheck',message)
+      result = S_ERROR()
+      result['Value'] = "Exceeded maximum dataset limit"
+      return result
+
+    if totalData:
+      for i in data:
+        j = i.replace('LFN:','')
+        if not re.search('^/'+setup+'/',j):
+          incorrectDataFlag += 1
+        if re.search('//',j):
+          slashFlag +=1
+
+    if incorrectDataFlag:
+      result = S_ERROR()
+      result['Value'] = "Input data not corrrectly specified"
+      return result
+
+    if slashFlag:
+      result = S_ERROR()
+      result['Value'] = "Input data contains //"
+      return result
+
+    number = str(totalData)
+    result = S_OK()
+    result['Value'] = number+' LFNs OK'
+    return result
 
   #############################################################################
   def  checkOutputDataExists(self, job, jdl):
@@ -370,5 +366,43 @@ class JobSanityAgent(Agent):
     """
     #To implement
     return S_OK()
+
+  #############################################################################
+  def checkPlatformSupported(self,job,jdl):
+    """This method queries the CS for available platforms
+       supported by DIRAC and will check these against what
+       the job requests.
+    """
+    #To implement
+    return S_OK()
+
+  #############################################################################
+  def checkInputSandbox(self,job,jdl):
+    """The number of input sandbox files, as specified in the job
+       JDL are checked in the JobDB.
+    """
+    #To implement
+    return S_OK()
+
+  #############################################################################
+  def updateJobStatus(self,job,status,minorstatus=None):
+    """This method updates the job status in the JobDB.
+    """
+    self.log.debug("self.jobDB.setJobAttribute("+str(job)+",Status,"+status+" update=True)")
+    result = self.jobDB.setJobAttribute(job,'Status',status, update=True)
+    if result['OK']:
+      if minorstatus:
+        self.log.debug("self.jobDB.setJobAttribute("+str(job)+","+minorstatus+",update=True)")
+        result = self.jobDB.setJobAttribute(job,'MinorStatus',minorstatus,update=True)
+
+    return result
+
+  #############################################################################
+  def setJobParam(self,job,reportName,value):
+    """This method updates a job parameter in the JobDB.
+    """
+    self.log.debug("self.jobDB.setJobParameter("+str(job)+","+reportName+","+value+")")
+    result = self.jobDB.setJobParameter(job,reportName,value)
+    return result
 
   #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
