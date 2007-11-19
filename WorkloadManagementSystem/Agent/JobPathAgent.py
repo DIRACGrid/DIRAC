36cf87147d3ba3cabd691e466c6399ae0b2426c6
Stuart K. Paterson <stuart.paterson@cern.ch>
2007-11-19 10:07:40 +0000

    Extensively updated to use Optimizer base class. Now set full optimizer chain from job JDL and allow jobs to define their own path :)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5269 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/Agent/JobPathAgent.py
+++ WorkloadManagementSystem/Agent/JobPathAgent.py
@@ -1,110 +1,122 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Agent/JobPathAgent.py,v 1.2 2007/11/16 11:51:51 paterson Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Agent/JobPathAgent.py,v 1.3 2007/11/19 10:07:40 paterson Exp $
 # File :   JobPathAgent.py
 # Author : Stuart Paterson
 ########################################################################
 
-"""   The Job Path Agent determines the chain of Agents that must work on the job
-      prior to the scheduling decision.
+"""   The Job Path Agent determines the chain of Optimizing Agents that must 
+      work on the job prior to the scheduling decision.
 
       Initially this takes jobs in the received state and starts the jobs on the
       optimizer chain.  The next development will be to explicitly specify the 
       path through the optimizers. 
 
 """
-__RCSID__ = "$Id: JobPathAgent.py,v 1.2 2007/11/16 11:51:51 paterson Exp $"
+__RCSID__ = "$Id: JobPathAgent.py,v 1.3 2007/11/19 10:07:40 paterson Exp $"
 
-from DIRAC.WorkloadManagementSystem.DB.JobDB        import JobDB
-#from DIRAC.WorkloadManagementSystem.DB.JobLoggingDB import JobLoggingDB
-from DIRAC.Core.Utilities.ClassAd.ClassAdLight      import ClassAd
-from DIRAC.Core.Base.Agent                          import Agent
-from DIRAC.ConfigurationSystem.Client.Config        import gConfig
-from DIRAC                                          import S_OK, S_ERROR
+from DIRAC.WorkloadManagementSystem.Agent.Optimizer        import Optimizer
+from DIRAC.ConfigurationSystem.Client.Config               import gConfig
+from DIRAC.Core.Utilities.ClassAd.ClassAdLight             import ClassAd
+from DIRAC                                                 import S_OK, S_ERROR
+import string,re
 
-AGENT_NAME = 'WorkloadManagement/JobPathAgent'
+OPTIMIZER_NAME = 'JobPath'
 
-class JobPathAgent(Agent):
+class JobPathAgent(Optimizer):
 
   #############################################################################
   def __init__(self):
-    """ Standard constructor
+    """ Constructor, takes system flag as argument.
     """
-    Agent.__init__(self,AGENT_NAME)
-
-  #############################################################################
+    Optimizer.__init__(self,OPTIMIZER_NAME,'Received',enableFlag=True)
+    
+  #############################################################################    
   def initialize(self):
-    """ Initialization of the Agent.
-    """
-
-    result = Agent.initialize(self)
-    self.jobDB = JobDB()
-   # self.logDB = JobLoggingDB()
-    self.FileCatalog        = None
-    self.optimizerName      = 'JobPath'
-    self.nextOptimizerName  = 'JobSanity'
-
-    self.pollingTime          = gConfig.getValue(self.section+'/PollingTime',10)
-    self.jobStatus            = gConfig.getValue(self.section+'/JobStatus','Received')
-    self.optStatus            = gConfig.getValue(self.section+'/OptimizerJobStatus','Checking')
-    self.nextOptMinorStatus   = gConfig.getValue(self.section+'/OptimizerMinorStatus',self.nextOptimizerName)
-
-    self.log.debug( '==========================================='           )
-    self.log.debug( 'DIRAC '+self.optimizerName+' Agent is started with'    )
-    self.log.debug( 'the following parameters:'                             )
-    self.log.debug( '==========================================='           )
-    self.log.debug( 'Polling Time        ==> %s' % self.pollingTime         )
-    self.log.debug( 'Job Status          ==> %s' % self.jobStatus           )
-    self.log.debug( 'Opt Status          ==> %s' % self.optStatus           )
-    self.log.debug( 'Opt Minor Status    ==> %s' % self.nextOptMinorStatus  )
-    self.log.debug( '==========================================='           )
-
-    return result
-
-  #############################################################################
-  def execute(self):
-    """ The main agent execution method
-    """
-    condition = {'Status':self.jobStatus}
-    result = self.jobDB.selectJobs(condition)
-    if not result['OK']:
-      self.log.error('Failed to get a job list from the JobDB')
-      return S_ERROR('Failed to get a job list from the JobDB')
-
-    if not len(result['Value']):
-      self.log.debug('No pending jobs to process')
-      return S_OK('No work to do')
-
-    jobList = result['Value']
-    for job in jobList:
-      result = self.checkJob(job)
-      if not result['OK']:
-        return result
-
-    return result
+    """Initialize specific parameters for JobPathAgent.
+    """ 
+    result = Optimizer.initialize(self)  
+    self.basePath  = gConfig.getValue(self.section+'/BasePath','JobPath,JobSanity,')
+    self.ancestors = gConfig.getValue(self.section+'/Ancestors','AncestorFiles')
+    self.inputData = gConfig.getValue(self.section+'/InputData','InputData')
+    self.procDB    = gConfig.getValue(self.section+'/ProcDB','ProcessingDB')
+    self.endPath   = gConfig.getValue(self.section+'/EndPath','JobScheduling,TaskQueue')    
+    return result                               
 
   #############################################################################
   def checkJob(self,job):
     """This method controls the checking of the job.
     """
-    self.log.info('Job %s will be processed' % (job))
-    result = self.updateJobStatus(job,self.optStatus,self.nextOptMinorStatus)
-    if not result['OK']:
-      self.log.error(result['Message'])
+    self.log.info('Job %s will be processed by %sAgent' % (job,self.optimizerName))
+    jdl = self.jobDB.getJobJDL(job,original=True)
+    if jdl['OK'] and len(jdl['Value']):
+      jdl = jdl['Value']
+      result = self.setJobPath(job,jdl)
+    else:
+      self.log.error('Could not obtain original JDL for job %s' %(job))
+      return S_ERROR('JDL not found in JobDB')
 
     return result
 
   #############################################################################
-  def updateJobStatus(self,job,status,minorstatus=None):
-    """This method updates the job status in the JobDB.
+  def setJobPath(self,job,jdl):
+    """This method controls the checking of the job.
     """
-    self.log.debug("self.jobDB.setJobAttribute("+str(job)+",Status,"+status+" update=True)")
-    result = S_OK()
-    result = self.jobDB.setJobAttribute(job,'Status',status, update=True)
-    if result['OK']:
-      if minorstatus:
-        self.log.debug("self.jobDB.setJobAttribute("+str(job)+","+minorstatus+",update=True)")
-        result = self.jobDB.setJobAttribute(job,'MinorStatus',minorstatus,update=True)
+    classadJob = ClassAd('['+jdl+']')
+    if not classadJob.isOK():
+      self.log.debug("Warning: illegal JDL for job %s, will be marked problematic" % (job))
+      result = S_ERROR('Problematic JDL')
 
+    #Check if job defines a path itself
+    jobPath = classadJob.get_expression('JobPath').replace('"','').replace('Unknown','')
+    if jobPath:
+      self.log.info('Job %s defines its own optimizer chain %s' %(job,jobPath))
+      result = self.processJob(job,string.split(jobPath,',')) 
+      return S_OK('Job defines own path') #in this case next job will be checked before returning 
+
+    #If no path, construct based on JDL 
+    path = self.basePath
+    
+    result = self.jobDB.getInputData(job)
+    if not result['OK']:
+      self.log.error('Failed to get input data from JobdB for %s' % (job) )
+      self.log.error(result['Message'])
+      return S_OK()
+
+    if len(result['Value']):
+      self.log.info('Job %s has an input data requirement' % (job))   
+      ancestorDepth = classadJob.get_expression('AncestorDepth').replace('"','').replace('Unknown','')
+      if ancestorDepth:
+        self.log.info('Job %s has specified ancestor depth' % (job)) 
+        path += self.ancestors+','+self.inputData+','
+      else:
+        path += self.inputData+','
+
+      jobType = classadJob.get_expression('JobType').replace('"','').replace('Unknown','')
+      if jobType=='processing':
+        self.log.info('Job %s is of processing type' % (job)) 
+        path += self.procDB+','
+    else:
+      self.log.info('Job %s has no input data requirement' % (job))      
+
+    path += self.endPath
+    self.log.info('Constructed path for job %s is: %s' %(job,path))
+    result = self.processJob(job,string.split(path,','))
+    return S_OK('Job path constructed')
+
+
+  #############################################################################    
+  def processJob(self,job,chain):
+    """Set job path and send to next optimizer
+    """
+    result = self.setOptimizerChain(job,chain)   
+    if not result['OK']:
+      self.log.error(result['Message'])  
+    result = self.setNextOptimizer(job)
+    if not result['OK']:
+      self.log.error(result['Message']) 
+    result = self.setJobParam(job,'JobPath',string.join(chain,','))  
+    if not result['OK']:
+      self.log.error(result['Message'])
     return result
-
+    
   #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
