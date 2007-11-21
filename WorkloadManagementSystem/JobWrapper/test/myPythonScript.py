f0eaeb2d1fa57c2481894f81a3f300392631fd04
Stuart K. Paterson <stuart.paterson@cern.ch>
2007-11-21 09:48:14 +0000

    Used to test the watchdog, this is a simple script that sleeps a bit and occasionally prints stuff.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5291 4525493e-7705-40b1-a816-d608a930855b

---
+++ WorkloadManagementSystem/JobWrapper/test/myPythonScript.py
@@ -0,0 +1,29 @@
+#!/bin/python
+
+import os,commands,time,sys
+
+print '**************************'
+print 'START myPythonScript.py'
+print '**************************'
+sys.stdout.flush()
+time.sleep(30)
+print 'Hi this is a test'
+print 'hope it works...'
+sys.stdout.flush()
+root = os.getcwd()
+print 'we are here: ',root
+print 'the files in this directory are:'
+status,result = commands.getstatusoutput('ls -al')
+print result
+#time.sleep(80)
+sys.stdout.flush()
+print 'trying to see the local environment:'
+status,result = commands.getstatusoutput('env')
+time.sleep(30)
+print result
+print 'bye.'
+print '**************************'
+print 'END myPythonScript.py'
+print '**************************'
+
+
