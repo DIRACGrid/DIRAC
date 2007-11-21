679569b8558f8f1418e39eca47d8405b841dbbe1
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2007-11-21 18:36:20 +0000

    Updated ClassAd interface
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5321 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Utilities/ClassAd/ClassAdCondor/__init__.py
@@ -0,0 +1,52 @@
+##############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/ClassAd/ClassAdCondor/__init__.py,v 1.1 2007/11/21 18:36:20 atsareg Exp $
+##############################################################
+
+"""This is a Python binding module for the Condor ClassAd
+   library. The ClassAd API is extended for the use in the
+   DIRAC project
+"""
+
+from ClassAdBase import ClassAd, MatchClassAd
+import string
+
+# Provide the API used in the DIRAC project
+
+def insertAttributeStringList(self,name,attrList):
+  """ Insert an attribute of type list of strings
+  """
+  attrString = '{"'+string.join(attrList,'","')+'"}'
+  result = self.insertExpression(name,attrString)
+  return result
+
+ClassAd.insertAttributeStringList = insertAttributeStringList
+
+def lookupAttribute(self,name):
+  return self.get_expression(name) is not None
+
+ClassAd.lookupAttribute = lookupAttribute
+
+def isOK(self):
+  return self.this is not None
+
+ClassAd.isOK = isOK
+
+def matchClassAd(ca1,ca2):
+  """ Match the 2 ClassAds and provide the result as tuple of 3 booleans:
+      'symmetricMatch','leftMatchesRight','rightMatchesLeft'
+  """
+
+  mca = MatchClassAd()
+  mca.initialize(ca1,ca2)
+
+  result_s,error_s = mca.getAttributeBool('symmetricMatch')
+  result_l,error_l = mca.getAttributeBool('leftMatchesRight')
+  result_r,error_r = mca.getAttributeBool('rightMatchesLeft')
+
+  # Have to release the MatchClassAd to avoid Seg Fault while gabarge collection
+  mca.release()
+
+  if error_s and error_l and error_r:
+    return result_s, result_l, result_r
+  else:
+    return S_ERROR('Failed to match the given ClassAds: sym:%d left:%d right"%d' % (error_s,error_l,error_r))
