657ab1865e17c8b419198980b59bd1fbebd08812
Stuart K. Paterson <stuart.paterson@cern.ch>
2007-11-21 17:54:56 +0000

    StagerSystem Package
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5318 4525493e-7705-40b1-a816-d608a930855b

---
+++ StagerSystem/__init__.py
@@ -0,0 +1,9 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/StagerSystem/__init__.py,v 1.1 2007/11/21 17:54:56 paterson Exp $
+############################################################
+
+"""
+    DIRAC.StagerSystem package
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2007/11/21 17:54:56 paterson Exp $"
