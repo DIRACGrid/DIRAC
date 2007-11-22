89bc199b157fa12c0afd7fdb8c3d2e19c4dcdddf
Stuart K. Paterson <stuart.paterson@cern.ch>
2007-11-22 12:16:17 +0000

    DIRAC.StagerSystem.Agent package
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5419 4525493e-7705-40b1-a816-d608a930855b

--- StagerSystem/Agent/__init__.py
+++ StagerSystem/Agent/__init__.py
@@ -1,9 +1,9 @@
 ############################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/StagerSystem/Agent/__init__.py,v 1.2 2007/11/22 12:15:30 paterson Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/StagerSystem/Agent/__init__.py,v 1.3 2007/11/22 12:16:17 paterson Exp $
 ############################################################
 
 """
-    DIRAC.StagerSystem.Client package
+    DIRAC.StagerSystem.Agent package
 """
 
-__RCSID__ = "$Id: __init__.py,v 1.2 2007/11/22 12:15:30 paterson Exp $"
+__RCSID__ = "$Id: __init__.py,v 1.3 2007/11/22 12:16:17 paterson Exp $"
