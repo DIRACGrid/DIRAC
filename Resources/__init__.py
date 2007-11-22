3b4619e2ab60bfcd48f6d99914bda10281634d2a
Stuart K. Paterson <stuart.paterson@cern.ch>
2007-11-22 12:04:05 +0000

    Resources package.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5414 4525493e-7705-40b1-a816-d608a930855b

---
+++ Resources/__init__.py
@@ -0,0 +1,10 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Resources/__init__.py,v 1.1 2007/11/22 12:04:05 paterson Exp $
+############################################################
+
+"""
+DIRAC.Resources package
+
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2007/11/22 12:04:05 paterson Exp $"
