8615931009172a67008a1214bd9236ab78917ad2
Stuart K. Paterson <stuart.paterson@cern.ch>
2007-11-22 12:06:06 +0000

    Resources Storage package
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5416 4525493e-7705-40b1-a816-d608a930855b

---
+++ Resources/Storage/__init__.py
@@ -0,0 +1,10 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Resources/Storage/__init__.py,v 1.1 2007/11/22 12:06:06 paterson Exp $
+############################################################
+
+"""
+DIRAC.Resources.Storage package
+
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2007/11/22 12:06:06 paterson Exp $"
