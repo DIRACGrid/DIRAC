26d5311e41745bc46e2f1c29ac9bc6a2d2368fd8
Stuart K. Paterson <stuart.paterson@cern.ch>
2007-11-26 22:03:13 +0000

    First import of CE Factory.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5458 4525493e-7705-40b1-a816-d608a930855b

---
+++ Resources/Computing/ComputingElementFactory.py
@@ -0,0 +1,52 @@
+########################################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Resources/Computing/ComputingElementFactory.py,v 1.1 2007/11/26 22:03:13 paterson Exp $
+# File :   ComputingElementFactory.py
+# Author : Stuart Paterson
+########################################################################
+
+"""  The Computing Element Factory has one method that instantiates a given Computing Element
+     from the CEUnique ID specified in the JobAgent configuration section.
+"""
+from DIRAC.Resources.Computing.ComputingElement          import ComputingElement
+from DIRAC                                               import S_OK, S_ERROR, gLogger
+
+__RCSID__ = "$Id: ComputingElementFactory.py,v 1.1 2007/11/26 22:03:13 paterson Exp $"
+
+import sys,types
+
+class ComputingElementFactory:
+
+  #############################################################################
+  def __init__(self,ceUniqueID):
+    """ Standard constructor
+    """
+    self.ceUniqueID = ceUniqueID
+    self.log = gLogger
+    #self.log.setLevel('debug')
+
+  #############################################################################
+  def getCE(self):
+    """This method returns the CE instance corresponding to the supplied
+       CEUniqueID.  If no corresponding CE is available, this is indicated.
+    """
+    try:
+      subClassName = "%sComputingElement" % (self.ceUniqueID)
+      ceSubClass = __import__('DIRAC.Resources.Computing.%s' % subClassName,globals(),locals(),[subClassName])
+    except Exception, x:
+      msg = 'ComputingElementFactory could not import DIRAC.Resources.Computing.%s' %(subClassName)
+      self.log.warn(x)
+      self.log.warn(msg)
+      return S_ERROR(msg)
+
+    try:
+      ceStr = 'ceSubClass.%s()' % (subClassName)
+      computingElement = eval(ceStr)
+    except Exception, x:
+      msg = 'ComputingElementFactory could not instantiate %s()' %(subClassName)
+      self.log.warn(x)
+      self.log.warn(msg)
+      return S_ERROR(msg)
+
+    return S_OK(computingElement)
+
+  #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
