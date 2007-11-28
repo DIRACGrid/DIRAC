7893cb65fb346cb638e4b60e78e85067e3b3399c
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2007-11-28 20:23:55 +0000

    Refining the storage interface.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5485 4525493e-7705-40b1-a816-d608a930855b

--- Core/Storage/StorageBase.py
+++ Core/Storage/StorageBase.py
@@ -1,26 +1,34 @@
-""" Base Storage Class has the following methods:
+""" Base Storage Class provides the base interface for all storage plug-ins
 
-    Implemented:
-      changeDir()
-      configure()
-      getCwd()
-      getPath()
-      putDir ()
-
-    Not implemented:
       exists()
-      fsize()
-      get()
-      getDir()
-      getLocalPath()
-      getPFNBase()
-      isdir()
-      isfile()
-      ls()
-      makeDir()
-      put()
-      remove()
-      removeDir()
+
+These are the methods for manipulating files:
+      isFile()
+      getFile()
+      putFile()
+      removeFile()
+      getFileMetadata()
+      getFileSize()
+      prestageFile()
+      getTransportURL()
+
+These are the methods for manipulating directories:
+      isDirectory()
+      getDirectory()
+      putDirectory()
+      createDirectory()
+      removeDirectory()
+      listDirectory()
+      getDirectoryMetadata()
+      getDirectorySize()
+
+These are the methods for manipulating the client:
+      changeDirectory()
+      getCurrentDirectory()
+      getName()
+      getParameters()
+      getProtocolPfn()
+      getCurrentURL()
 """
 
 from DIRAC import gLogger, gConfig, S_OK, S_ERROR
@@ -29,153 +37,142 @@ import re
 class StorageBase:
 
   def __init__(self,name,rootdir):
-    self.configure(name,rootdir)
-
-  def exists(self,path):
-    """Check if the given path exists
-    """
-    print "Storage.exists: implement me!"
-
-  def fsize(self,fname):
-    """Get the physical size of the given file
-    """
-    print "Storage.fsize: implement me!"
-
-  def get(self,path):
-    """Get a local copy of the file specified by its path
-    """
-    print "Storage.get: implement me!"
-
-  def getDir(self,path):
-    """Get locally a directory from the physical storage together with all its
-       files and subdirectories.
-    """
-    print "Storage.getDir: implement me!"
-
-  def isdir(self,dirname):
-    """Check if the given path exists and it is a directory
-    """
-    print "Storage.isdir: implement me!"
-
-  def isfile(self,fname):
-    """Check if the given path exists and it is a file
-    """
-    print "Storage.isfile: implement me!"
-
-  def ls(self,path):
-    """ List the supplied path
-    """
-
-  def makeDir(self,newdir):
-    """ Make a new directory on the physical storage
-    """
-    print "Storage.makeDir: implement me!"
-
-  def put(self,fname):
-    """Put a copy of the local file to the current directory on the
-       physicak storage
-    """
-    print "Storage.put: implement me!"
-
-  def remove(self,path):
-    """Remove physically the file specified by its path
-    """
-    print "Storage.remove: implement me!"
-
-  def removeDir(self,path):
-    """Remove a directory on the physical storage together with all its files and
-       subdirectories.
-    """
-    print "Storage.removeDir: implement me!"
-
-  ############################################################################################
-  #
-  # Below this point the methods are implemented
-  #
-
-  def configure(self,name,rootdir):
-    """ Called when StorageBase is initialised.
-    """
     self.name = name
     self.rootdir = rootdir
     self.cwd = self.rootdir
 
-  def changeDir(self,newdir):
+  def exists(self,path):
+    """Check if the given path exists
+    """
+    return S_ERROR("Storage.exists: implement me!")
+
+  #############################################################
+  #
+  # These are the methods for file manipulation
+  #
+
+  def isFile(self,fname):
+    """Check if the given path exists and it is a file
+    """
+    return S_ERROR("Storage.isFile: implement me!")
+
+  def getFile(self,path):
+    """Get a local copy of the file specified by its path
+    """
+    return S_ERROR("Storage.getFile: implement me!")
+
+  def putFile(self,fname):
+    """Put a copy of the local file to the current directory on the
+       physical storage
+    """
+    return S_ERROR("Storage.putFile: implement me!")
+
+  def removeFile(self,path):
+    """Remove physically the file specified by its path
+    """
+    return S_ERROR("Storage.removeFile: implement me!")
+
+  def getFileMetadata(self,path):
+    """  Get metadata associated to the file
+    """
+    return S_ERROR("Storage.getFileMetadata: implement me!")
+
+  def getFileSize(self,fname):
+    """Get the physical size of the given file
+    """
+    return S_ERROR("Storage.getFileSize: implement me!")
+
+  def prestageFile(self,path):
+    """ Issue prestage request for file
+    """
+    return S_ERROR("Storage.prestageFile: implement me!")
+
+  def getTransportURL(self,path,protocols):
+    """ Obtain the TURLs for the supplied path and protocols
+    """
+    return S_ERROR("Storage.getTransportURL: implement me!")
+
+  #############################################################
+  #
+  # These are the methods for directory manipulation
+  #
+
+  def isDirectory(self,dirname):
+    """Check if the given path exists and it is a directory
+    """
+    return S_ERROR("Storage.isDirectory: implement me!")
+
+  def getDirectory(self,path):
+    """Get locally a directory from the physical storage together with all its
+       files and subdirectories.
+    """
+    return S_ERROR("Storage.getDirectory: implement me!")
+
+  def putDirectory(self,path):
+    """Put a local directory to the physical storage together with all its
+       files and subdirectories.
+    """
+    return S_ERROR("Storage.putDirectory: implement me!")
+
+  def createDirectory(self,newdir):
+    """ Make a new directory on the physical storage
+    """
+    return S_ERROR("Storage.createDirectory: implement me!")
+
+  def removeDirectory(self,path):
+    """Remove a directory on the physical storage together with all its files and
+       subdirectories.
+    """
+    return S_ERROR("Storage.removeDirectory: implement me!")
+
+  def listDirectory(self,path):
+    """ List the supplied path
+    """
+    return S_ERROR("Storage.listDirectory: implement me!")
+
+  def getDirectoryMetadata(self,path):
+    """ Get the metadata for the directory
+    """
+    return S_ERROR("Storage.getDirectoryMetadata: implement me!")
+
+  def getDirectorySize(self,path):
+    """ Get the size of the directory on the storage
+    """
+    return S_ERROR("Storage.getDirectorySize: implement me!")
+
+
+  #############################################################
+  #
+  # These are the methods for manipulting the client
+  #
+
+  def changeDirectory(self,newdir):
     """ Change the current directory
     """
-    newcwd = self.getPath(newdir)
-    self.cwd = newcwd
+    self.cwd = newdir
+    return S_OK()
 
-  def getCwd(self):
+  def getCurrentDirectory(self):
     """ Get the current directory
     """
-    return self.cwd
+    return S_OK(self.cwd)
 
-  def getLocalPath(self,fpath):
-    """ This the full path of a local file.
+  def getName(self):
+    """ The name with which the storage was instantiated
     """
-    res = re.search("^/",fpath)
-    if res is not None:
-      return S_OK(fpath)
-    else:
-      path = '%s/%s' % (os.getcwd(),fpath)
-      return S_OK(path)
+    return S_OK(self.name)
 
-  def getPath(self,fpath):
-    """ Get the full path of the given file name resolving the current working directory.
+  def getParameters(self):
+    """ Get the parameters with which the storage was instantiated
     """
-    if fpath == '/':
-      return self.rootdir
-    res = re.search("^/",fpath)
-    if res is not None:
-      return '%s%s' % (self.rootdir,fpath)
-    else:
-      return '%s/%s' % (self.cwd,fpath)
+    return S_ERROR("Storage.getParameters: implement me!")
 
-  def putDir (self,directory):
-    """Put local directory together with all its files and subdirectories
-       to the current directory on the physical storage.
+  def getProtocolPfn(self,pfnDict,withPort):
+    """ Get the PFN for the protocol with or without the port
     """
+    return S_ERROR("Storage.getProtocolPfn: implement me!")
 
-    remote_cwd = self.cwd
-    if os.path.isdir(directory):
-      dirname = os.path.basename(directory)
-      if not self.isdir(dirname):
-        self.makeDir (dirname)
-        self.changeDir (dirname)
-      else:
-        self.changeDir (dirname)
-    else:
-      print "No such directory: ",  directory
-      return S_ERROR( "No such directory: "+directory )
-
-    cwd = os.getcwd()
-    os.chdir(directory)
-    files = os.listdir(".")
-    all_OK = True
-    some_OK = False
-    if len(files) > 0:
-      for name in files:
-        if not os.path.isdir(name):
-          result = self.put(name);
-          if result['Status'] != "OK":
-            all_OK = False
-          else:
-            some_OK = True
-        else:
-          result = self.putDir (name);
-          if result['Status'] != "OK":
-            all_OK = False
-          else:
-            some_OK = True
-
-    os.chdir(cwd)
-    self.cwd = remote_cwd
-    if not all_OK:
-      if some_OK:
-        return S_ERROR('Not all the files transfered successfully')
-      else:
-        return S_ERROR('putDir() failed completely')
-    else:
-      return S_OK()
-
+  def getCurrentURL(self,fileName):
+    """ Create the full URL for the storage using the configuration, self.cwd and the fileName
+    """
+    return S_ERROR("Storage.getCurrentURL: implement me!")
