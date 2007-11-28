a88e591a16898d6073159400e21c3b6f3f8b483e
Stuart K. Paterson <stuart.paterson@cern.ch>
2007-11-28 15:56:52 +0000

    First commit of watchdog factory.  This will be used by the Job Wrapper.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5476 4525493e-7705-40b1-a816-d608a930855b

---
+++ WorkloadManagementSystem/JobWrapper/WatchdogFactory.py
@@ -0,0 +1,63 @@
+########################################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/JobWrapper/WatchdogFactory.py,v 1.1 2007/11/28 15:56:52 paterson Exp $
+# File :   WatchdogFactory.py
+# Author : Stuart Paterson
+########################################################################
+
+"""  The Watchdog Factory instantiates a given Watchdog based on a quick
+     determination of the local operating system.
+"""
+
+from DIRAC                                               import S_OK, S_ERROR, gLogger
+
+__RCSID__ = "$Id: WatchdogFactory.py,v 1.1 2007/11/28 15:56:52 paterson Exp $"
+
+import re,sys,types,platform
+
+class WatchdogFactory:
+
+  #############################################################################
+  def __init__(self):
+    """ Standard constructor
+    """
+    self.version = platform.uname()
+    self.log = gLogger
+
+  #############################################################################
+  def getWatchdog(self,pid,thread,spObject,jobcputime):
+    """This method returns the CE instance corresponding to the local OS,
+       the Linux watchdog is returned by default.
+    """
+    localOS = None
+
+    if re.search('Darwin',self.version[0]):
+      localOS = 'Mac'
+      self.log.info('WatchdogFactory will create Watchdog%s instance' %(localOS))
+    elif re.search('Windows',self.version[0]):
+      localOS = 'Windows'
+      self.log.info('WatchdogFactory will create Watchdog%s instance' %(localOS))
+    else:
+      localOS = 'Linux'
+      self.log.info('WatchdogFactory will create Watchdog%s instance' %(localOS))
+
+    try:
+      subClassName = "Watchdog%s" % (localOS)
+      ceSubClass = __import__('DIRAC.WorkloadManagementSystem.JobWrapper.%s' % subClassName,globals(),locals(),[subClassName])
+    except Exception, x:
+      msg = 'WatchdogFactory could not import DIRAC.WorkloadManagementSystem.JobWrapper.%s' %(subClassName)
+      self.log.warn(x)
+      self.log.warn(msg)
+      return S_ERROR(msg)
+
+    try:
+      ceStr = 'ceSubClass.%s(pid, thread, spObject, jobcputime)' %(subClassName)
+      watchdogInstance = eval(ceStr)
+    except Exception, x:
+      msg = 'WatchdogFactory could not instantiate %s()' %(subClassName)
+      self.log.warn(x)
+      self.log.warn(msg)
+      return S_ERROR(msg)
+
+    return S_OK(watchdogInstance)
+
+  #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
