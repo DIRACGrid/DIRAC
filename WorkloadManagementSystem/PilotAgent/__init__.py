d66d1685274693190f1ce1729ca304d4b6869e2c
Stuart K. Paterson <stuart.paterson@cern.ch>
2007-12-03 18:34:29 +0000

    DIRAC.WorkloadManagementSystem.PilotAgent package
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5541 4525493e-7705-40b1-a816-d608a930855b

---
+++ WorkloadManagementSystem/PilotAgent/__init__.py
@@ -0,0 +1,9 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/PilotAgent/__init__.py,v 1.1 2007/12/03 18:34:29 paterson Exp $
+############################################################
+
+"""
+   DIRAC.WorkloadManagementSystem.PilotAgent package
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2007/12/03 18:34:29 paterson Exp $"
