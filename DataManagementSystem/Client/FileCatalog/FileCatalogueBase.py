3b29c0b315e84d23a8b4db1df946202ce6ec13d3
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2007-12-11 17:48:30 +0000

    Moved location of the file catalog clients.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5658 4525493e-7705-40b1-a816-d608a930855b

---
+++ DataManagementSystem/Client/FileCatalog/FileCatalogueBase.py
@@ -0,0 +1,245 @@
+""" Class for FileCatalogueBase
+
+    This outlines the interface that must be exposed by every file catalogue plug in
+
+    exists()
+
+    isFile()
+    removeFile()
+    addFile()
+    addReplica()
+    removeReplica()
+    getFileMetadata()
+    getReplicas()
+    getReplicaStatus()
+    getReplicaStatus()
+    setReplicaHost()
+    getFileSize()
+
+    createDirectory()
+    isDirectory()
+    listDirectory()
+    removeDirectory()
+    getDirectoryMetadata()
+    getDirectorySize()
+
+    isLink()
+    createLink()
+    removeLink()
+
+    changeDirectory()
+    getCurrentDirectory()
+    getName()
+
+"""
+from DIRAC import gLogger, gConfig, S_OK, S_ERROR
+import re,types
+
+class FileCatalogueBase:
+
+  def __init__(self,name):
+    self.name = name
+
+  def exists(self, path):
+    if type(path) == types.StringType:
+      paths = [path]
+    else:
+      paths = path
+    return S_ERROR('Implement me')
+
+  ####################################################################
+  #
+  # These are the methods for file manipulation
+  #
+
+  def isFile(self, path):
+    if type(path) == types.StringType:
+      paths = [path]
+    else:
+      paths = path
+    return S_ERROR('Implement me')
+
+  def removeFile(self, path):
+    if type(path) == types.StringType:
+      paths = [path]
+    else:
+      paths = path
+    return S_ERROR('Implement me')
+
+  def addFile(self, fileTuple):
+    """ A tuple should be supplied to this method which contains:
+        (lfn,pfn,size,se,guid)
+        A list of tuples may also be supplied.
+    """
+    if type(fileTuple) == types.TupleType:
+      files = [fileTuple]
+    else:
+      files = fileTuple
+    for fileTuple in files:
+      lfn,pfn,size,se,guid = fileTuple
+    return S_ERROR('Implement me')
+
+  def addReplica(self, replicaTuple):
+    if type(replicaTuple) == types.TupleType:
+      replicas = [replicaTuple]
+    else:
+      replicas = replicaTuple
+    for replicaTuple in replicas:
+      lfn,pfn,se = replicaTuple
+    return S_ERROR('Implement me')
+
+  def getFileMetadata(self, path):
+    if type(path) == types.StringType:
+      paths = [path]
+    else:
+      paths = path
+    return S_ERROR('Implement me')
+
+  def getReplicas(self, path, directory=0):
+    if type(path) == types.StringType:
+      paths = [path]
+    else:
+      paths = path
+    return S_ERROR('Implement me')
+
+  def removeReplica(self, replicaTuple):
+    if type(replicaTuple) == types.TupleType:
+      replicas = [replicaTuple]
+    else:
+      replicas = replicaTuple
+    for replicaTuple in replicas:
+      lfn,se = replicaTuple
+    return S_ERROR('Implement me')
+
+  def getReplicaStatus(self,replicaTuple):
+    if type(replicaTuple) == types.TupleType:
+      replicas = [replicaTuple]
+    else:
+      replicas = replicaTuple
+    for replicaTuple in replicas:
+      lfn,se = replicaTuple
+    return S_ERROR('Implement me')
+
+  def setReplicaStatus(self,replicaTuple):
+    if type(replicaTuple) == types.TupleType:
+      replicas = [replicaTuple]
+    elif type(replicaTuple) == types.ListType:
+      replicas = replicaTuple
+    else:
+      return S_ERROR('LFCClient.setReplicaStatus: Must supply a file tuple or list of file typles')
+    return S_ERROR('Implement me')
+
+  def setReplicaHost(self,replicaTuple):
+    """ This modifies the replica metadata for the SE and space token.
+        The tuple supplied must be of the following form:
+        (lfn,pfn,se,spaceToken)
+    """
+    if type(replicaTuple) == types.TupleType:
+      replicas = [replicaTuple]
+    elif type(replicaTuple) == types.ListType:
+      replicas = replicaTuple
+    else:
+      return S_ERROR('LFCClient.setReplicaHost: Must supply a file tuple or list of file typles')
+    return S_ERROR('Implement me')
+
+  def getFileSize(self, path):
+    if type(path) == types.StringType:
+      paths = [path]
+    else:
+      paths = path
+    return S_ERROR('Implement me')
+
+  ####################################################################
+  #
+  # These are the methods for directory manipulation
+  #
+
+  def createDirectory(self, path):
+    if type(path) == types.StringType:
+      paths = [path]
+    else:
+      paths = path
+    return S_ERROR('Implement me')
+
+  def isDirectory(self, path):
+    if type(path) == types.StringType:
+      paths = [path]
+    else:
+      paths = path
+    return S_ERROR('Implement me')
+
+  def listDirectory(self, path):
+    if type(path) == types.StringType:
+      paths = [path]
+    else:
+      paths = path
+    return S_ERROR('Implement me')
+
+  def removeDirectory(self, path):
+    if type(path) == types.StringType:
+      paths = [path]
+    else:
+      paths = path
+    return S_ERROR('Implement me')
+
+  def getDirectoryReplicas(self, path):
+    if type(path) == types.StringType:
+      paths = [path]
+    else:
+      paths = path
+    return S_ERROR('Implement me')
+
+  def getDirectoryMetadata(self, path):
+    if type(path) == types.StringType:
+      paths = [path]
+    else:
+      paths = path
+    return S_ERROR('Implement me')
+
+  def getDirectorySize(self, path):
+    if type(path) == types.StringType:
+      paths = [path]
+    else:
+      paths = path
+    return S_ERROR('Implement me')
+
+  ####################################################################
+  #
+  # These are the methods for link manipulation
+  #
+
+  def isLink(self, path):
+    if type(path) == types.StringType:
+      paths = [path]
+    else:
+      paths = path
+    return S_ERROR('Implement me')
+
+  def createLink(self, path):
+    if type(path) == types.StringType:
+      paths = [path]
+    else:
+      paths = path
+    return S_ERROR('Implement me')
+
+  def removeLink(self, path):
+    if type(path) == types.StringType:
+      paths = [path]
+    else:
+      paths = path
+    return S_ERROR('Implement me')
+
+  ####################################################################
+  #
+  # These are the get/set methods for use within the client
+  #
+
+  def changeDirectory(self, path):
+    self.cwd = path
+    return S_OK()
+
+  def getCurrentDirectory(self):
+    return S_OK(self.cwd)
+
+  def getName(self):
+    return S_OK(self.name)
---
+++ DataManagementSystem/Client/FileCatalog/LcgFileCatalogCombinedClient.py
@@ -0,0 +1,176 @@
+""" File catalog client for the LFC service combined with
+    multiple read-only mirrors
+"""
+from DIRAC  import gLogger, gConfig, S_OK, S_ERROR
+from DIRAC.DataManagementSystem.Client.LcgFileCatalogClient import LcgFileCatalogClient
+from DIRAC.Core.Utilities.Subprocess import pythonCall
+import random, time,os
+
+class LcgFileCatalogCombinedClient:
+
+  ro_methods = ['exists','isLink','readLink','isFile','getFileMetadata','getReplicas',
+                'getReplicaStatus','getFileSize','isDirectory','getDirectoryReplicas',
+                'listDirectory','getDirectoryMetadata','getDirectorySize']
+
+  write_methods = ['createLink','removeLink','addFile','addReplica','removeReplica',
+                   'removeFile','setReplicaStatus','setReplicaHost','createDirectory',
+                   'removeDirectory']
+
+  def __init__(self, infosys=None, master_host=None, mirrors = []):
+    """ Default constructor
+    """
+    # Obtain the site configuration
+    result = gConfig.getOption('/DIRAC/Site')
+    if not result['OK']:
+      gLogger.error('Failed to get the /DIRAC/Site')
+      self.site = 'Unknown'
+    else:
+      self.site = result['Value']
+
+    if not infosys:
+      configPath = '/DataManagement/FileCatalogs/LFC/LcgGfalInfosys'
+      infosys = gConfig.getValue(configPath)
+
+    if not master_host:
+      configPath = '/DataManagement/FileCatalogs/LFC/LFCMaster'
+      master_host = gConfig.getValue(configPath)
+    # Create the master LFC client first
+    self.lfc = LcgFileCatalogClient(infosys,master_host)
+
+    if not mirrors:
+      configPath = '/DataManagement/FileCatalogs/LFC/LFCReadOnlyMirrors'
+      mirrors = gConfig.getValue(configPath,[])
+    # Create the mirror LFC instances
+    self.mirrors = []
+    for mirror in mirrors:
+      lfc = LcgFileCatalogClient(infosys,mirror)
+      self.mirrors.append(lfc)
+    random.shuffle(self.mirrors)
+    self.nmirrors = len(self.mirrors)
+
+    # Keep the environment for the master instance
+    self.master_host = self.lfc.host
+    os.environ['LFC_HOST'] = self.master_host
+    os.environ['LCG_GFAL_INFOSYS'] = infosys
+    self.name = 'LFC'
+
+    self.timeout = 300
+
+  def getName(self,DN=''):
+    """ Get the file catalog type name
+    """
+    return self.name
+
+  def __getattr__(self, name):
+    self.call = name
+    if name in LcgFileCatalogCombinedClient.write_methods:
+      return self.w_execute
+    elif name in LcgFileCatalogCombinedClient.ro_methods:
+      return self.r_execute
+    else:
+      raise AttributeError
+
+  def w_execute(self, *parms, **kws):
+    """ Write method executor.
+        Dispatches execution of the methods which need Read/Write
+        access to the master LFC instance
+    """
+
+    # If the DN argument is given, this is an operation on behalf
+    # of the user with this DN, prepare setAuthorizationId call
+    userDN = ''
+    if kws.has_key('DN'):
+      userDN = kws['DN']
+      del kws['DN']
+
+    # Try the method 3 times just in case of intermittent errors
+    max_retry = 2
+    count = 0
+    result = S_ERROR()
+
+    while (not result['OK']) and (count <= max_retry):
+      if count > 0:
+        # If retrying, wait a bit
+        time.sleep(1)
+      try:
+        result = S_OK()
+        if userDN:
+          resAuth = pythonCall(self.timeout,self.lfc.setAuthorizationId,userDN)
+          if not resAuth['OK']:
+            result = S_ERROR('Failed to set user authorization')
+        if result['OK']:
+          method = getattr(self.lfc,self.call)
+          resMeth = pythonCall(self.timeout,method,*parms,**kws)
+          if not resMeth['OK']:
+            result = S_ERROR('Timeout calling '+self.call+" method")
+          else:
+            result = resMeth['Value']
+      except Exception,x:
+        result = S_ERROR('Exception while calling LFC Master service '+str(x))
+      count += 1
+
+    return result
+
+  def r_execute(self, *parms, **kws):
+    """ Read-only method executor.
+        Dispatches execution of the methods which need Read-only
+        access to the mirror LFC instances
+    """
+
+    # If the DN argument is given, this is an operation on behalf
+    # of the user with this DN, prepare setAuthorizationId call
+    userDN = ''
+    if kws.has_key('DN'):
+      userDN = kws['DN']
+      del kws['DN']
+
+    result = S_ERROR()
+    # Try the method 3 times just in case of intermittent errors
+    max_retry = 2
+    count = 0
+
+    while (not result['OK']) and (count <= max_retry):
+      i = 0
+      while not result['OK'] and i < self.nmirrors:
+        # Switch environment to the mirror instance
+        os.environ['LFC_HOST'] = self.mirrors[i].host
+        try:
+          result = S_OK()
+          if userDN:
+            resAuth = pythonCall(self.timeout,self.mirrors[i].setAuthorizationId,userDN)
+            if not resAuth['OK']:
+              result = S_ERROR('Failed to set user authorization')
+          if result['OK']:
+            method = getattr(self.mirrors[i],self.call)
+            resMeth = pythonCall(self.timeout,method,*parms,**kws)
+            if not resMeth['OK']:
+              result = S_ERROR('Timout calling '+self.call+" method")
+            else:
+              result = resMeth['Value']
+        except Exception,x:
+          result = S_ERROR('Exception while calling LFC mirror service '+str(x))
+        i += 1
+      count += 1
+
+    # Return environment to the master LFC instance
+    os.environ['LFC_HOST'] = self.master_host
+
+    # Call the master LFC if all the mirrors failed
+    if not result['OK']:
+      try:
+        result = S_OK()
+        if userDN:
+          resAuth = pythonCall(self.timeout,self.lfc.setAuthorizationId,userDN)
+          if not resAuth['OK']:
+            result = S_ERROR('Failed to set user authorization')
+        if result['OK']:
+          method = getattr(self.lfc,self.call)
+          resMeth = pythonCall(self.timeout,method,*parms,**kws)
+          if not resMeth['OK']:
+            result = S_ERROR('Timout calling '+self.call+" method")
+          else:
+            result = resMeth['Value']
+      except Exception,x:
+        result = S_ERROR('Exception while calling LFC Master service '+str(x))
+
+    return result
