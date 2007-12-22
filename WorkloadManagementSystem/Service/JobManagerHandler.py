af11b66dd19bf5aae9962997f10d32c2c074cc9c
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2007-12-22 15:54:06 +0000

    Kill, reschedule and delete with proper per job permissions
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5838 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/Service/JobManagerHandler.py
+++ WorkloadManagementSystem/Service/JobManagerHandler.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Service/JobManagerHandler.py,v 1.1 2007/11/29 15:35:43 atsareg Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Service/JobManagerHandler.py,v 1.2 2007/12/22 15:54:06 atsareg Exp $
 ########################################################################
 
 """ JobManagerHandler is the implementation of the JobManager service
@@ -14,7 +14,7 @@
     
 """
 
-__RCSID__ = "$Id: JobManagerHandler.py,v 1.1 2007/11/29 15:35:43 atsareg Exp $"
+__RCSID__ = "$Id: JobManagerHandler.py,v 1.2 2007/12/22 15:54:06 atsareg Exp $"
 
 from types import *
 from DIRAC.Core.DISET.RequestHandler import RequestHandler
@@ -23,6 +23,7 @@ from DIRAC.ConfigurationSystem.Client.Config import gConfig
 from DIRAC.Core.Utilities.ClassAd.ClassAdLight import ClassAd
 from DIRAC.WorkloadManagementSystem.DB.JobDB import JobDB
 from DIRAC.WorkloadManagementSystem.DB.ProxyRepositoryDB import ProxyRepositoryDB
+from DIRAC.WorkloadManagementSystem.Service.JobPolicy import JobPolicy
 
 # This is a global instance of the JobDB class
 jobDB = False
@@ -82,44 +83,168 @@ class JobManagerHandler( RequestHandler ):
       gLogger.error("Failed to store the user proxy for job %s" % jobID)
       return S_ERROR("Failed to store the user proxy for job %s" % jobID)
 
-    return S_OK(jobID)        
+    return S_OK(jobID)   
+    
+###########################################################################
+  def __get_job_list(self,jobInput):
+    """ Evaluate the jobInput into a list of ints
+    """
+   
+    if type(jobInput) == IntType:
+      return [jobInput]
+    if type(jobInput) == StringType:
+      try:
+        ijob = int(jobInput)
+        return [ijob]
+      except:
+        return []
+    if type(jobInput) == ListType:   
+      try: 
+        ljob = [ int(x) for x in jobInput ] 
+        return ljob
+      except:
+        return []     
+   	
+    return []
+      
+###########################################################################
+  def __get_job_rights(self,jobID,userDN,userGroup):
+    """ Get access rights to jobID for the user userDN/userGroup 
+    """   
+
+    result = jobDB.getJobAttributes(jobID,['OwnerDN','OwnerGroup'])
+    if not result['OK']:
+      return result
+    elif result['Value']:
+      owner = result['Value']['OwnerDN']
+      group = result['Value']['OwnerGroup']
+      result = self.policy.getJobPolicy(userDN,userGroup,owner,group)
+      return result
 
 ###########################################################################
-  types_rescheduleJob = [ IntType, StringType ]
-  def export_rescheduleJob(self, jobID, proxy = None):
+  def __evaluate_rights(self,jobList,userDN,userGroup,right):
+    """ Get access rights to jobID for the user userDN/userGroup 
+    """   
+
+    self.policy = JobPolicy()
+    validJobList = []
+    invalidJobList = []
+    nonauthJobList = []
+    for jobID in jobList:
+      result = self.__get_job_rights(jobID,userDN,userGroup)
+      if result['OK']:
+        if result['Value'][right]:
+          validJobList.append(jobID)
+        else:
+          nonauthJobList.append(jobID)
+      else:
+        invalidJobList.append(jobID)  
+           
+    return validJobList,invalidJobList,nonauthJobList               
+
+###########################################################################
+  types_rescheduleJob = [ ]
+  def export_rescheduleJob(self, jobIDs, proxy = None):
     """  Reschedule a single job. If the optional proxy parameter is given
          it will be used to refresh the proxy in the Proxy Repository
     """  
 
-    #gActivityClient.addMark( "rescheduleJob" )
+    jobList = self.__get_job_list(jobIDs)
+    if not jobList:
+      return S_ERROR('Invalid job specification: '+str(jobIDs))
 
-    result  = jobDB.rescheduleJob( jobID )
-    gLogger.debug( str( result ) )
-    if not result['OK']:
-        return result
+    result = self.getRemoteCredentials()
+    userDN = result['DN']
+    userGroup = result['group']  
+        
+    validJobList,invalidJobList,nonauthJobList = self.__evaluate_rights(jobList,
+                                                                        userDN,
+                                                                        userGroup,
+                                                                        'Run')
+                                                                        
+    if validJobList:
+      if proxy:
+        resProxy = proxyRepository.storeProxy(proxy,userDN,userGroup)
+        if not resProxy['OK']:
+          gLogger.error("Failed to store the user proxy for job %s" % jobID)
 
-    if proxy:
-      resProxy = proxyRepository.storeProxy(proxy,self.sDN,self.sGroup)
-      if not resProxy['OK']:
-        gLogger.error("Failed to store the user proxy for job %s" % jobID)
-        return S_ERROR("Failed to store the user proxy for job %s" % jobID)
+    for jobID in validJobList:  
+      result  = jobDB.rescheduleJob( jobID )
+      gLogger.debug( str( result ) )
+      if not result['OK']:
+          return result
 
-    res = S_OK(result['JobID'])
-    res['RescheduleCounter'] = result['RescheduleCounter']
-    return res
+    result = S_OK(validJobList)
+    if invalidJobList or nonauthJobList:
+      result = S_ERROR('Some jobs failed deletion')
+      if invalidJobList:
+        result['InvalidJobIDs'] = invalidJobList
+      if nonauthJobList:
+        result['NonauthorizedJobIDs'] = nonauthJobList  
+      
+    return result 
+
 
 ###########################################################################
-  types_deleteJob = [ IntType, StringType ]
-  def export_deleteJob(self, jobID):
-    """  Delete a single job
+  types_deleteJob = [  ]
+  def export_deleteJob(self, jobIDs):
+    """  Delete jobs specified in the jobIDs list
     """ 
     
-    return S_OK()
+    jobList = self.__get_job_list(jobIDs)
+    if not jobList:
+      return S_ERROR('Invalid job specification: '+str(jobIDs))
+
+    result = self.getRemoteCredentials()
+    userDN = result['DN']
+    userGroup = result['group']  
+        
+    validJobList,invalidJobList,nonauthJobList = self.__evaluate_rights(jobList,
+                                                                        userDN,
+                                                                        userGroup,
+                                                                        'Delete') 
+            
+    for jobID in validJobList:    
+      result = jobDB.setJobStatus(jobID,'Deleted','Checking accounting')
+      
+    result = S_OK(validJobList)
+    if invalidJobList or nonauthJobList:
+      result = S_ERROR('Some jobs failed deletion')
+      if invalidJobList:
+        result['InvalidJobIDs'] = invalidJobList
+      if nonauthJobList:
+        result['NonauthorizedJobIDs'] = nonauthJobList  
+      
+    return result     
     
 ###########################################################################
   types_killJob = [ IntType, StringType ]
-  def export_killJob(self, jobID):
+  def export_killJob(self, jobIDs):
     """  Kill a single running job
     """ 
     
-    return S_OK()    
+    jobList = self.__get_job_list(jobIDs)
+    if not jobList:
+      return S_ERROR('Invalid job specification: '+str(jobIDs))
+
+    result = self.getRemoteCredentials()
+    userDN = result['DN']
+    userGroup = result['group']  
+    
+    validJobList,invalidJobList,nonauthJobList = self.__evaluate_rights(jobList,
+                                                                        userDN,
+                                                                        userGroup,
+                                                                        'Kill')
+    for jobID in validJobList:    
+      # kill jobID
+      pass
+      
+    result = S_OK(validJobList)
+    if invalidJobList or nonauthJobList:
+      result = S_ERROR('Some jobs failed deletion')
+      if invalidJobList:
+        result['InvalidJobIDs'] = invalidJobList
+      if nonauthJobList:
+        result['NonauthorizedJobIDs'] = nonauthJobList  
+      
+    return result                                                                        
