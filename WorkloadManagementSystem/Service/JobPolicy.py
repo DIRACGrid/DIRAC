a4cec6f4315fbdb5d8850af412fd0920c4bda80b
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2008-01-06 20:59:23 +0000

    Better policy formalization
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5846 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/Service/JobPolicy.py
+++ WorkloadManagementSystem/Service/JobPolicy.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Service/JobPolicy.py,v 1.2 2007/12/22 15:55:00 atsareg Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Service/JobPolicy.py,v 1.3 2008/01/06 20:59:23 atsareg Exp $
 ########################################################################
 
 """ JobPolicy encapsulates authorization rules for different groups
@@ -7,9 +7,23 @@
 
 """
 
-__RCSID__ = "$Id: JobPolicy.py,v 1.2 2007/12/22 15:55:00 atsareg Exp $"
+__RCSID__ = "$Id: JobPolicy.py,v 1.3 2008/01/06 20:59:23 atsareg Exp $"
 
 from DIRAC import gConfig, S_OK, S_ERROR
+
+JOB_RIGHTS = ['GetJob','GetInfo','GetInput','GetOutput','ChangeStatus',
+              'Delete','Kill','Submit','Reschedule','GetStats']
+GROUP_RIGHTS = {}
+GROUP_RIGHTS['Visitor'] = ['GetInfo']
+GROUP_RIGHTS['NormalUser'] = ['GetInfo','Submit']
+GROUP_RIGHTS['JobSharing'] = ['GetInfo','GetInput','GetOutput','ChangeStatus',
+                              'Delete','Kill','Reschedule']
+GROUP_RIGHTS['JobAgent'] = ['GetJob','GetInfo','GetInput','ChangeStatus','Reschedule']                              
+GROUP_RIGHTS['JobAdministrator'] = JOB_RIGHTS
+
+JOB_OWNER_RIGHTS = ['GetInfo','GetInput','GetOutput','ChangeStatus',
+                    'Delete','Kill','Submit','Reschedule']
+
 
 class JobPolicy:
 
@@ -23,29 +37,40 @@ class JobPolicy:
 
     self.jobDB = jobDB
 
+###########################################################################
+  def getUserRightsForJob(self,jobID,userDN,userGroup):
+    """ Get access rights to job with jobID for the user specified by
+        userDN/userGroup 
+    """   
+
+    result = self.jobDB.getJobAttributes(jobID,['OwnerDN','OwnerGroup'])
+    if not result['OK']:
+      return result
+    elif result['Value']:
+      owner = result['Value']['OwnerDN']
+      group = result['Value']['OwnerGroup']
+      result = self.getJobPolicy(userDN,userGroup,owner,group)
+      return result
+      
+###########################################################################      
   def getJobPolicy(self,userDN,userGroup,jobOwnerDN='',jobOwnerGroup=''):
     """ Get the job operations rights for a job owned by jobOwnerDN/jobOwnerGroup
         for a user with userDN/userGroup.
         Returns a dictionary of various operations rights
     """
 
-    rightsList = ['GetInfo','GetInput','GetOutput','ChangeStatus','Delete','Kill']
-
     # Can not do anything by default
     jobDict = {}
-    jobDict['Run'] = False
-    for r in rightsList:
+    for r in JOB_RIGHTS:
       jobDict[r] = False
 
     # Anybody can get info about the jobs
     jobDict['GetInfo'] = True
 
-    # Job Owner can do everything
-    # If the jobOwner is defined, the job is already existing
+    # Job Owner can do everything with his jobs
     if jobOwnerDN == userDN:
-      for r in rightsList:
+      for r in JOB_OWNER_RIGHTS:
         jobDict[r] = True
-      return S_OK(jobDict)
 
     result = gConfig.getOption('/Groups/'+userGroup+'/Properties')
      
@@ -54,25 +79,16 @@ class JobPolicy:
     else:
       return S_ERROR('Failed to get properties for group '+userGroup)
 
-    # Visitors can only get information
-    if 'Visitor' in propertyList:
-      return S_OK(jobDict)
-
-    # Normal users can run jobs
-    if 'NormalUser' in propertyList:
-      jobDict['Run'] = True
-
-    # Administrators can do everything
-    if 'JobAdministrator' in propertyList:
-      for r in rightsList:
-        jobDict[r] = True
-      return S_OK(jobDict)
+    # Visitors, NormalUsers and JobAdministrators
+    for groupProperty in ['Visitor','NormalUser','JobAdministrator','JobAgent']:
+      if groupProperty in propertyList:
+        for right in GROUP_RIGHTS[groupProperty]:
+          jobDict[right] = True
 
     # Members of the same group sharing their jobs can do everything
     if jobOwnerGroup == userGroup:
       if 'JobSharing' in propertyList:
-        for r in rightsList:
-          jobDict[r] = True
-        return S_OK(jobDict)
+        for right in GROUP_RIGHTS['JobSharing']:
+          jobDict[right] = True
 
     return S_OK(jobDict)
