8d9be402f0921fd362c60672a9d99525f2e3eeed
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2008-01-13 01:27:12 +0000

    Several fixes after the VOMS proxy conversion debugging
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5897 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/DB/ProxyRepositoryDB.py
+++ WorkloadManagementSystem/DB/ProxyRepositoryDB.py
@@ -1,16 +1,15 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/DB/Attic/ProxyRepositoryDB.py,v 1.7 2008/01/06 19:08:30 atsareg Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/DB/Attic/ProxyRepositoryDB.py,v 1.8 2008/01/13 01:27:12 atsareg Exp $
 ########################################################################
 """ ProxyRepository class is a front-end to the proxy repository Database
 """
 
-__RCSID__ = "$Id: ProxyRepositoryDB.py,v 1.7 2008/01/06 19:08:30 atsareg Exp $"
+__RCSID__ = "$Id: ProxyRepositoryDB.py,v 1.8 2008/01/13 01:27:12 atsareg Exp $"
 
 import time
 from DIRAC  import gConfig, gLogger, S_OK, S_ERROR
 from DIRAC.Core.Base.DB import DB
-from DIRAC.Core.Utilities.GridCredentials import getProxyTimeLeft
-from DIRAC.Core.Utilities.VOMS import getVOMSAttributes, createVOMSProxy
+from DIRAC.Core.Utilities.GridCredentials import getProxyTimeLeft, getVOMSAttributes, createVOMSProxy
 
 #############################################################################
 class ProxyRepositoryDB(DB):
@@ -40,12 +39,11 @@ class ProxyRepositoryDB(DB):
 #############################################################################
   def storeProxy(self,proxy,dn,group):
     """ Store user proxy into the Proxy repository for a user specified by his
-        DN and group.
-        The grid proxy will be converted into a VOMS proxy if possible
+	DN and group.
+	The grid proxy will be converted into a VOMS proxy if possible
     """
 
     result = getVOMSAttributes(proxy,'db')
-    print result
     if not result['OK']:
       return S_ERROR('Can not analyze proxy')
       
@@ -81,65 +79,61 @@ class ProxyRepositoryDB(DB):
     # Decide if we should store and convert the new proxy
     if not proxy_exists:      
       if proxyType != "VOMS":     
-        # Attempt to convert into a VOMS proxy
-        
-        self.log.verbose('Converting proxy to VOMS for '+dn)
-        
-        if self.vomsGroupMappingDict.has_key(group):
-          proxyAttr = self.VO+":"+self.vomsGroupMappingDict[group]         
-          result = createVOMSProxy(proxy,attributes=proxyAttr)
-        else:  
-          result = createVOMSProxy(proxy,vo=self.VO)  
-          
-        if result['OK']:
-          self.log.verbose('VOMS conversion done for '+dn) 
-          proxy_to_store = result['Value']
-          proxyType = "VOMS"  
+	# Attempt to convert into a VOMS proxy
+	
+	self.log.verbose('Converting proxy to VOMS for '+dn)
+	
+	if self.vomsGroupMappingDict.has_key(group):
+	  proxyAttr = self.VO+":"+self.vomsGroupMappingDict[group]         
+	  result = createVOMSProxy(proxy,attributes=proxyAttr)
+	else:  
+	  result = createVOMSProxy(proxy,vo=self.VO)  
+	  
+	if result['OK']:
+	  self.log.info('VOMS conversion done for '+dn) 
+	  proxy_to_store = result['Value']
+	  proxyType = "VOMS"  
       else:
-        proxy_to_store = proxy
-        
+	proxy_to_store = proxy
+	
       cmd = 'INSERT INTO Proxies ( Proxy, UserDN, UserGroup, ExpirationTime, ' \
-            'ProxyType, ProxyAttributes ) VALUES ' \
-            '(\'%s\', \'%s\', \'%s\', NOW() + INTERVAL %d second, \'%s\', \'%s\')' % (proxy_to_store,dn,group,time_left,proxyType,proxyAttr)
+	    'ProxyType, ProxyAttributes ) VALUES ' \
+	    '(\'%s\', \'%s\', \'%s\', NOW() + INTERVAL %d second, \'%s\', \'%s\')' % (proxy_to_store,dn,group,time_left,proxyType,proxyAttr)
       result = self._update( cmd )
       if result['OK']:
-        self.log.verbose( 'Proxy inserted for DN="%s" and Group="%s"' % (dn,group) )
+	self.log.verbose( 'Proxy inserted for DN="%s" and Group="%s"' % (dn,group) )
       else:
-        self.log.error( 'Proxy insert failed for DN="%s" and Group="%s"' % (dn,group) )
-        return S_ERROR('Failed to store proxy')    
+	self.log.error( 'Proxy insert failed for DN="%s" and Group="%s"' % (dn,group) )
+	return S_ERROR('Failed to store proxy')    
     else:
       # Check if we have to replace the old proxy       
       force_proxy = False
       if old_type and old_type != 'VOMS' and proxyType == 'VOMS':
-        force_proxy = True
+	force_proxy = True
       # Store new proxy if it is significantly longer than the existing one
       # or the new VOMS proxy replaces the old GRID proxy 
       if relative_time_delta > 0.1 or force_proxy:
-        cmd = 'UPDATE Proxies SET Proxy=\'%s\',' % proxy
-        cmd = cmd + ' ExpirationTime = NOW() + INTERVAL %d SECOND, ' % time_left
-        cmd = cmd + ' ProxyType=\'%s\' ' % proxyType
-        if proxyAttr:
-          cmd = cmd + ' ProxyAttributes=\'%s\' ' % proxyAttr
-        cmd = cmd + 'WHERE UserDN=\'%s\' AND UserGroup=\'%s\'' % ( dn, group )
+	cmd = 'UPDATE Proxies SET Proxy=\'%s\',' % proxy
+	cmd = cmd + ' ExpirationTime = NOW() + INTERVAL %d SECOND, ' % time_left
+	cmd = cmd + ' ProxyType=\'%s\' ' % proxyType
+	if proxyAttr:
+	  cmd = cmd + ', ProxyAttributes=\'%s\' ' % proxyAttr
+	cmd = cmd + 'WHERE UserDN=\'%s\' AND UserGroup=\'%s\'' % ( dn, group )
 
-        print "======================================"
-        print cmd
-        print "======================================"
-
-        result = self._update(cmd)
-        if result['OK']:
-          self.log.verbose( 'Proxy Updated for DN=%s and Group=%s' % (dn,group) )
-        else:
-          self.log.error( 'Proxy Update Failed for DN=%s and Group=%s' % (dn,group) )
-          self.log.error(result['Message'])
-          return S_ERROR('Failed to store ticket')
+	result = self._update(cmd)
+	if result['OK']:
+	  self.log.verbose( 'Proxy Updated for DN=%s and Group=%s' % (dn,group) )
+	else:
+	  self.log.error( 'Proxy Update Failed for DN=%s and Group=%s' % (dn,group) )
+	  self.log.error(result['Message'])
+	  return S_ERROR('Failed to store ticket')
 
     return S_OK()
 
 #############################################################################
   def getProxy(self,userDN,userGroup=None):
     """ Get proxy string from the Proxy Repository for use with userDN 
-        in the userGroup
+	in the userGroup
     """
 
     if userGroup:
@@ -160,11 +154,11 @@ class ProxyRepositoryDB(DB):
 #############################################################################
   def getUsers(self,validity=0):
     """ Get all the distinct users from the Proxy Repository. Optionally, only users
-        with valid proxies within the given validity period expressed in seconds
+	with valid proxies within the given validity period expressed in seconds
     """
 
     if validity:
-      cmd = "SELECT UserDN,UserGroup FROM Proxies WHERE (NOW() + INTERVAL %d SECOND) < ExpirationTime" % validity
+      cmd = "SELECT UserDN,UserGroup FROM Proxies WHERE (NOW() + INTERVAL %d SECOND) > ExpirationTime" % validity
     else:
       cmd = "SELECT UserDN,UserGroup FROM Proxies"
     result = self._query( cmd )
