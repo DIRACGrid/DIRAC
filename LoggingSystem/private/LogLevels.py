055eca8f8cdb9cc8ab9c5954fb7343d77538cdf3
Adria Casajus <adria@ecm.ub.es>
2008-01-16 16:23:56 +0000

    'VERBOSE' is now a valid level
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5962 4525493e-7705-40b1-a816-d608a930855b

--- LoggingSystem/private/LogLevels.py
+++ LoggingSystem/private/LogLevels.py
@@ -1,8 +1,8 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/LoggingSystem/private/LogLevels.py,v 1.2 2007/03/16 11:59:46 rgracian Exp $
-__RCSID__ = "$Id: LogLevels.py,v 1.2 2007/03/16 11:59:46 rgracian Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/LoggingSystem/private/LogLevels.py,v 1.3 2008/01/16 16:23:56 acasajus Exp $
+__RCSID__ = "$Id: LogLevels.py,v 1.3 2008/01/16 16:23:56 acasajus Exp $"
 
 class LogLevels:
-  
+
   def __init__(self):
     self.always    = 'ALWAYS'
     self.info      = 'INFO'
@@ -16,18 +16,19 @@ class LogLevels:
        self.always    : 30,
        self.info      : 20,
        self.verbose   : 10,
+       'VERBOSE'      : 10,
        self.debug     : 0,
        self.warn      : -10,
        self.error     : -20,
        self.exception : -20,
        self.fatal     : -30
        }
-    
+
   def getLevelValue(self, sName):
     if self.__levelDict.has_key( sName ):
       return self.__levelDict[ sName ]
     else:
       return None
-    
+
   def getLevels( self ):
     return self.__levelDict.keys()
