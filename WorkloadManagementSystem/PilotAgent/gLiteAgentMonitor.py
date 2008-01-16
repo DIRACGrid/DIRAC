44eb493430941f27eebc7d4932bc869d7ae89af4
Stuart K. Paterson <stuart.paterson@cern.ch>
2008-01-16 14:50:00 +0000

    Updates after changes to base-class etc.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5956 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/PilotAgent/gLiteAgentMonitor.py
+++ WorkloadManagementSystem/PilotAgent/gLiteAgentMonitor.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Id: gLiteAgentMonitor.py,v 1.1 2008/01/15 13:26:19 paterson Exp $
+# $Id: gLiteAgentMonitor.py,v 1.2 2008/01/16 14:50:00 paterson Exp $
 # File :   gLiteAgentMonitor.py
 # Author : Stuart Paterson
 ########################################################################
@@ -7,11 +7,11 @@
 """ The gLite Agent Monitor performs the pilot job status tracking activity for LCG.
 """
 
-__RCSID__ = "$Id: gLiteAgentMonitor.py,v 1.1 2008/01/15 13:26:19 paterson Exp $"
+__RCSID__ = "$Id: gLiteAgentMonitor.py,v 1.2 2008/01/16 14:50:00 paterson Exp $"
 
 from DIRACEnvironment                                        import DIRAC
 from DIRAC.Core.Utilities.Subprocess                         import shellCall
-from DIRAC.ConfigurationSystem.Client.LocalConfiguration     import LocalConfiguration
+from DIRAC.Core.Utilities                                    import List
 from DIRAC.WorkloadManagementSystem.PilotAgent.AgentMonitor  import AgentMonitor
 from DIRAC                                                   import S_OK, S_ERROR, gConfig, gLogger
 
@@ -20,14 +20,15 @@ import os, sys, re, string, time
 class gLiteAgentMonitor(AgentMonitor):
 
   #############################################################################
-  def __init__(self):
+  def __init__(self,configPath,mode):
     """ Standard constructor
     """
-    self.log = gLogger
-    self.log.setLevel('debug')
-    self.cmd = 'glite-wms-job-status'
-    self.cmdTimeout = 60
-    AgentMonitor.__init__(self,'GLITE')
+    self.log = gLogger.getSubLogger('%sAgentMonitor' %(mode))
+    self.sectionPath = configPath
+    self.cmd = gConfig.getValue(self.sectionPath+'/StatusCommand','glite-wms-job-status')
+    self.cmdTimeout = gConfig.getValue(self.sectionPath+'/StatusCommandTimeout',60)
+    self.agentMonConfig = '/%s/%s' % ( '/'.join( List.fromChar(configPath, '/' )[:-1] ), 'AgentMonitor')
+    AgentMonitor.__init__(self,self.agentMonConfig,mode)
 
   #############################################################################
   def getPilotStatus(self,jobID,pilotID):
@@ -37,7 +38,7 @@ class gLiteAgentMonitor(AgentMonitor):
     """
     self.__checkProxy()
     cmd = "%s %s" % (self.cmd,pilotID)
-    self.log.verbose( '--- Executing %s for %s' %(cmd,jobID) )
+    self.log.info( '--- Executing %s for %s' %(cmd,jobID) )
     result = self.__exeCommand(cmd)
 
     if not result['OK']:
@@ -47,8 +48,10 @@ class gLiteAgentMonitor(AgentMonitor):
     status = result['Status']
     stdout = result['StdOut']
     queryTime = result['Time']
-    timing = '>>> GLITE WMS status query time %.2fs' % queryTime
-    self.log.verbose( timing )
+    timing = '>>> gLite status query time %.2fs' % queryTime
+    self.log.info( timing )
+    destination=None
+    jobStatus=None
     if status == 0:
       jobStatus = ''
       lines = stdout.split('\n')
@@ -58,7 +61,7 @@ class gLiteAgentMonitor(AgentMonitor):
         if line.find('Destination:') != -1 :
           destination = line.split()[1].split(":")[0]
 
-      self.log.debug('JobID: %s, PilotStatus: %s, Destination: %s' %(jobID,jobStatus,destination))
+      self.log.info('JobID: %s, PilotStatus: %s, Destination: %s' %(jobID,jobStatus,destination))
       pilot = S_OK()
       pilot['JobID']=jobID
       pilot['PilotStatus']=jobStatus
@@ -86,23 +89,23 @@ class gLiteAgentMonitor(AgentMonitor):
     status = proxyInfo['Value'][0]
     stdout = proxyInfo['Value'][1]
     stderr = proxyInfo['Value'][2]
-    self.log.debug('Status %s' %status)
-    self.log.debug(stdout)
-    self.log.debug(stderr)
+    self.log.verbose('Status %s' %status)
+    self.log.verbose(stdout)
+    self.log.verbose(stderr)
 
   #############################################################################
   def __exeCommand(self,cmd):
     """Runs a submit / list-match command and prints debugging information.
     """
     start = time.time()
-    self.log.debug( cmd )
+    self.log.verbose( cmd )
     result = shellCall(60,cmd)
 
     status = result['Value'][0]
     stdout = result['Value'][1]
     stderr = result['Value'][2]
-    self.log.debug('Status = %s' %status)
-    self.log.debug(stdout)
+    self.log.verbose('Status = %s' %status)
+    self.log.verbose(stdout)
     if stderr:
       self.log.warn(stderr)
     result['Status']=status
@@ -111,16 +114,5 @@ class gLiteAgentMonitor(AgentMonitor):
     subtime = time.time() - start
     result['Time']=subtime
     return result
-
-###############################################################################
-if __name__ == "__main__":
-  """ Main execution method.
-  """
-  localCfg = LocalConfiguration()
-  localCfg.addDefaultEntry( "/DIRAC/Security/UseServerCertificate", "yes" )
-  from DIRAC.Core.Base import Script
-  Script.parseCommandLine()
-  monitor = gLiteAgentMonitor()
-  monitor.run()
 
   #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
