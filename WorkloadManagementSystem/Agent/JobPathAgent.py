c6201a77e9827a19a214c12c2febe51a8fdab084
Stuart K. Paterson <stuart.paterson@cern.ch>
2008-01-20 15:55:29 +0000

    Updated default optimizer path settings.  Disabled addition of Processing DB Agent for processing type jobs until ready.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@5995 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/Agent/JobPathAgent.py
+++ WorkloadManagementSystem/Agent/JobPathAgent.py
@@ -1,18 +1,18 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Agent/JobPathAgent.py,v 1.3 2007/11/19 10:07:40 paterson Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Agent/JobPathAgent.py,v 1.4 2008/01/20 15:55:29 paterson Exp $
 # File :   JobPathAgent.py
 # Author : Stuart Paterson
 ########################################################################
 
-"""   The Job Path Agent determines the chain of Optimizing Agents that must 
+"""   The Job Path Agent determines the chain of Optimizing Agents that must
       work on the job prior to the scheduling decision.
 
       Initially this takes jobs in the received state and starts the jobs on the
-      optimizer chain.  The next development will be to explicitly specify the 
-      path through the optimizers. 
+      optimizer chain.  The next development will be to explicitly specify the
+      path through the optimizers.
 
 """
-__RCSID__ = "$Id: JobPathAgent.py,v 1.3 2007/11/19 10:07:40 paterson Exp $"
+__RCSID__ = "$Id: JobPathAgent.py,v 1.4 2008/01/20 15:55:29 paterson Exp $"
 
 from DIRAC.WorkloadManagementSystem.Agent.Optimizer        import Optimizer
 from DIRAC.ConfigurationSystem.Client.Config               import gConfig
@@ -29,18 +29,18 @@ class JobPathAgent(Optimizer):
     """ Constructor, takes system flag as argument.
     """
     Optimizer.__init__(self,OPTIMIZER_NAME,'Received',enableFlag=True)
-    
-  #############################################################################    
+
+  #############################################################################
   def initialize(self):
     """Initialize specific parameters for JobPathAgent.
-    """ 
-    result = Optimizer.initialize(self)  
-    self.basePath  = gConfig.getValue(self.section+'/BasePath','JobPath,JobSanity,')
+    """
+    result = Optimizer.initialize(self)
+    self.basePath  = gConfig.getValue(self.section+'/BasePath','JobPath,JobSanity,JobPolicy,')
     self.ancestors = gConfig.getValue(self.section+'/Ancestors','AncestorFiles')
     self.inputData = gConfig.getValue(self.section+'/InputData','InputData')
     self.procDB    = gConfig.getValue(self.section+'/ProcDB','ProcessingDB')
-    self.endPath   = gConfig.getValue(self.section+'/EndPath','JobScheduling,TaskQueue')    
-    return result                               
+    self.endPath   = gConfig.getValue(self.section+'/EndPath','JobScheduling,TaskQueue')
+    return result
 
   #############################################################################
   def checkJob(self,job):
@@ -70,12 +70,12 @@ class JobPathAgent(Optimizer):
     jobPath = classadJob.get_expression('JobPath').replace('"','').replace('Unknown','')
     if jobPath:
       self.log.info('Job %s defines its own optimizer chain %s' %(job,jobPath))
-      result = self.processJob(job,string.split(jobPath,',')) 
-      return S_OK('Job defines own path') #in this case next job will be checked before returning 
+      result = self.processJob(job,string.split(jobPath,','))
+      return S_OK('Job defines own path') #in this case next job will be checked before returning
 
-    #If no path, construct based on JDL 
+    #If no path, construct based on JDL
     path = self.basePath
-    
+
     result = self.jobDB.getInputData(job)
     if not result['OK']:
       self.log.error('Failed to get input data from JobdB for %s' % (job) )
@@ -83,20 +83,20 @@ class JobPathAgent(Optimizer):
       return S_OK()
 
     if len(result['Value']):
-      self.log.info('Job %s has an input data requirement' % (job))   
+      self.log.info('Job %s has an input data requirement' % (job))
       ancestorDepth = classadJob.get_expression('AncestorDepth').replace('"','').replace('Unknown','')
       if ancestorDepth:
-        self.log.info('Job %s has specified ancestor depth' % (job)) 
+        self.log.info('Job %s has specified ancestor depth' % (job))
         path += self.ancestors+','+self.inputData+','
       else:
         path += self.inputData+','
 
       jobType = classadJob.get_expression('JobType').replace('"','').replace('Unknown','')
       if jobType=='processing':
-        self.log.info('Job %s is of processing type' % (job)) 
-        path += self.procDB+','
+        self.log.info('Job %s is of processing type' % (job))
+        #path += self.procDB+','
     else:
-      self.log.info('Job %s has no input data requirement' % (job))      
+      self.log.info('Job %s has no input data requirement' % (job))
 
     path += self.endPath
     self.log.info('Constructed path for job %s is: %s' %(job,path))
@@ -104,19 +104,19 @@ class JobPathAgent(Optimizer):
     return S_OK('Job path constructed')
 
 
-  #############################################################################    
+  #############################################################################
   def processJob(self,job,chain):
     """Set job path and send to next optimizer
     """
-    result = self.setOptimizerChain(job,chain)   
+    result = self.setOptimizerChain(job,chain)
     if not result['OK']:
-      self.log.error(result['Message'])  
+      self.log.error(result['Message'])
     result = self.setNextOptimizer(job)
     if not result['OK']:
-      self.log.error(result['Message']) 
-    result = self.setJobParam(job,'JobPath',string.join(chain,','))  
+      self.log.error(result['Message'])
+    result = self.setJobParam(job,'JobPath',string.join(chain,','))
     if not result['OK']:
       self.log.error(result['Message'])
     return result
-    
+
   #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
