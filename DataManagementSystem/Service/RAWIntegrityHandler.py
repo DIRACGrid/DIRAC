27ac4230d28c5590949f6d672db926fe543c5c4d
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-01-20 17:53:26 +0000

    Simple DISET interface to the RAW Integrity DB to allow remote registration of files.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@6003 4525493e-7705-40b1-a816-d608a930855b

---
+++ DataManagementSystem/Service/RAWIntegrityHandler.py
@@ -0,0 +1,28 @@
+""" Simple DISET interface to the RAW Integrity DB to allow remote registration of files.
+"""
+from types import *
+from DIRAC.Core.DISET.RequestHandler import RequestHandler
+from DIRAC import gLogger, gConfig, S_OK, S_ERROR
+from DIRAC.DataManagementSystem.DB.RAWIntegrityDB import RAWIntegrityDB
+
+rawIntegrityDB = False
+
+def initializeRAWIntegrityHandler(serviceInfo):
+  global rawIntegrityDB
+  rawIntegrityDB = RAWIntegrityDB()
+  return S_OK()
+
+class RAWIntegrityHandler(RequestHandler):
+
+  types_addFile = [StringType,StringType,IntType,StringType,StringType,StringType]
+  def export_addFile(self,lfn,pfn,size,se,guid,checksum=''):
+    """ Add a file to the RAW integrity DB
+    """
+    try:
+      gLogger.info("RAWIntegrityHandler.addFile: Attempting to add %s to the database." % lfn)
+      res = rawIntegrityDB.addFile(lfn,pfn,size,se,guid,checksum)
+      return res
+    except Exception,x:
+      errStr = "RAWIntegrityHandler.addFile: Exception while adding file to database."
+      gLogger.exception(errStr,str(x))
+      return S_ERROR(errStr)
