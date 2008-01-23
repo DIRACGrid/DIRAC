5a4513b0b7b0dfce2863f5976878dc3f8d53760c
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-01-23 13:39:03 +0000

    Client for dispatching file catalogue methods to the PlacementDB.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@6037 4525493e-7705-40b1-a816-d608a930855b

---
+++ DataManagementSystem/Client/Catalog/PlacementDBClient.py
@@ -0,0 +1,35 @@
+""" Client for PlacementDB file catalog tables
+"""
+
+from DIRAC  import gLogger, gConfig, S_OK, S_ERROR
+from DIRAC.Core.DISET.RPCClient import RPCClient
+from DIRAC.ConfigurationSystem.Client import PathFinder
+
+class PlacementDBClient:
+  """ File catalog client for placement DB
+  """
+
+  def __init__(self, url=False, useCertificates=False):
+    """ Constructor of the PlacementDB catalogue client
+    """
+    self.name = 'PlaceDB'
+    if not url:
+      self.server = RPCClient("DataManagement/PlacementDB",useCertificates,timeout=120)
+    else:
+      self.server = RPCClient(self.url,useCertificates,timeout = 120)
+
+  def getName(self,DN=''):
+    """ Get the file catalog type name
+    """
+    return self.name
+
+  def __getattr__(self, name):
+    self.call = name
+    return self.execute
+
+  def execute(self, *parms, **kws):
+    """ Magic method dispatcher """
+    try:
+      return self.server.callProxyMethod(self.call,parms,kws)
+    except Exception,x:
+      return S_ERROR("PlacementDBClient.execute: Exception while calling the server.",str(x))
