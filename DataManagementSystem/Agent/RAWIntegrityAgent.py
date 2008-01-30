89d195917b1aa91388772e1b022bca8de0272f9e
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-01-30 11:10:43 +0000

    Added the retrieval of a proxy for the agent loop.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@6152 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Agent/RAWIntegrityAgent.py
+++ DataManagementSystem/Agent/RAWIntegrityAgent.py
@@ -8,6 +8,9 @@ from DIRAC.RequestManagementSystem.Client.Request import RequestClient
 from DIRAC.RequestManagementSystem.Client.DataManagementRequest import DataManagementRequest
 from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
 from DIRAC.DataManagementSystem.DB.RAWIntegrityDB import RAWIntegrityDB
+from DIRAC.Core.DISET.RPCClient import RPCClient
+from DIRAC.Core.Utilities.GridCredentials import setupProxy,restoreProxy,setDIRACGroup, getProxyTimeLeft
+
 
 import time
 from types import *
@@ -27,9 +30,50 @@ class RAWIntegrityAgent(Agent):
     self.RequestDBClient = RequestClient()
     self.ReplicaManager = ReplicaManager()
     self.RAWIntegrityDB = RAWIntegrityDB()
+    self.wmsAdmin = RPCClient('WorkloadManagement/WMSAdministrator')
+    self.proxyDN = gConfig.getValue(self.section+'/ProxyDN')
+    self.proxyGroup = gConfig.getValue(self.section+'/ProxyGroup')
+    self.proxyLength = gConfig.getValue(self.section+'/DefaultProxyLength',12)
+    self.proxyLocation = gConfig.getValue(self.section+'/ProxyLocation')
     return result
 
   def execute(self):
+
+    ############################################################
+    #
+    # Get a valid proxy for the current activity
+    #
+    self.log.info("RAWIntegrityAgent.execute: Determining the length of the %s proxy." %self.proxyDN)
+    obtainProxy = False
+    if not os.path.exists(self.proxyLocation):
+      obtainProxy = True
+    else:
+      currentProxy = open(self.proxyLocation,'r')
+      oldProxyStr = currentProxy.read()
+      res = getProxyTimeLeft(oldProxyStr)
+      if not res["OK"]:
+        gLogger.error("RAWIntegrityAgent.execute: Could not determine the time left for proxy.", res['Message'])
+        return S_OK()
+      proxyValidity = int(res['Value'])
+      self.log.info("RAWIntegrityAgent.execute: %s proxy found to be valid for %s seconds."% (self.proxyDN,proxyValidity))
+      if proxyValidity <= 60:
+        obtainProxy = True
+
+    if obtainProxy:
+      self.log.info("RAWIntegrityAgent.execute: Attempting to renew %s proxy." %self.proxyDN)
+      res = self.wmsAdmin.getProxy(self.proxyDN,self.proxyGroup,self.proxyLength)
+      if not res['OK']:
+        gLogger.error("RAWIntegrityAgent.execute: Could not retrieve proxy from WMS Administrator", res['Message'])
+        return S_OK()
+      proxyStr = res['Value']
+      if not os.path.exists(os.path.dirname(self.proxyLocation)):
+        os.makedirs(os.path.dirname(self.proxyLocation))
+      res = setupProxy(proxyStr,self.proxyLocation)
+      if not res['OK']:
+        gLogger.error("RAWIntegrityAgent.execute: Could not create environment for proxy.", res['Message'])
+        return S_OK()
+      setDIRACGroup(self.proxyGroup)
+      self.log.info("RAWIntegrityAgent.execute: Successfully renewed %s proxy." %self.proxyDN)
 
     ############################################################
     #
@@ -201,5 +245,14 @@ class RAWIntegrityAgent(Agent):
     return S_OK()
 
 
+
+  #############################################################################
+  def __setupProxy(self,job,ownerDN,jobGroup,proxyDir):
+    """Retrieves user proxy with correct role for job and sets up environment to
+       run job locally.
+    """
+
+    self.log.verbose(setupResult)
+    return setupResult
 
 
