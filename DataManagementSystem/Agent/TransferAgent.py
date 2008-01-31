e8eb04fadf01f2f25e69fd261c96fbbc7f2d114f
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-01-31 17:06:55 +0000

    Added the retrieval of proxy.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@6191 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Agent/TransferAgent.py
+++ DataManagementSystem/Agent/TransferAgent.py
@@ -1,15 +1,14 @@
 """  TransferAgent takes transfer requests from the RequestDB and replicates them
 """
 
-
-
-
 from DIRAC  import gLogger, gConfig, gMonitor, S_OK, S_ERROR
 from DIRAC.Core.Base.Agent import Agent
 from DIRAC.Core.Utilities.Pfn import pfnparse, pfnunparse
+from DIRAC.Core.Utilities.GridCredentials import setupProxy,restoreProxy,setDIRACGroup, getProxyTimeLeft
 from DIRAC.RequestManagementSystem.Client.Request import RequestClient
 from DIRAC.RequestManagementSystem.Client.DataManagementRequest import DataManagementRequest
 from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
+
 import time
 from types import *
 
@@ -26,12 +25,54 @@ class TransferAgent(Agent):
     result = Agent.initialize(self)
     self.RequestDBClient = RequestClient()
     self.ReplicaManager = ReplicaManager()
+    self.wmsAdmin = RPCClient('WorkloadManagement/WMSAdministrator')
+    self.proxyDN = gConfig.getValue(self.section+'/ProxyDN')
+    self.proxyGroup = gConfig.getValue(self.section+'/ProxyGroup')
+    self.proxyLength = gConfig.getValue(self.section+'/DefaultProxyLength',12)
+    self.proxyLocation = gConfig.getValue(self.section+'/ProxyLocation')
     gMonitor.registerActivity( "Iteration", "Agent Loops/min",          "TransferAgent", "Atemps", gMonitor.OP_SUM )
     gMonitor.registerActivity( "Execute",   "Request Processed/min",    "TransferAgent", "Atemps", gMonitor.OP_SUM )
     gMonitor.registerActivity( "Done",      "Request Completed/min",    "TransferAgent", "Atemps", gMonitor.OP_SUM )
     return result
 
   def execute(self):
+    ############################################################
+    #
+    # Get a valid proxy for the current activity
+    #
+    self.log.info("TransferAgent.execute: Determining the length of the %s proxy." %self.proxyDN)
+    obtainProxy = False
+    if not os.path.exists(self.proxyLocation):
+      self.log.info("TransferAgent.execute: No proxy found.")
+      obtainProxy = True
+    else:
+      currentProxy = open(self.proxyLocation,'r')
+      oldProxyStr = currentProxy.read()
+      res = getProxyTimeLeft(oldProxyStr)
+      if not res["OK"]:
+        gLogger.error("TransferAgent.execute: Could not determine the time left for proxy.", res['Message'])
+        return S_OK()
+      proxyValidity = int(res['Value'])
+      gLogger.error("TransferAgent.execute: Current proxy found to be valid for %s seconds." % proxyValidity)
+      self.log.info("TransferAgent.execute: %s proxy found to be valid for %s seconds."% (self.proxyDN,proxyValidity))
+      if proxyValidity <= 60:
+        obtainProxy = True
+
+    if obtainProxy:
+      self.log.info("TransferAgent.execute: Attempting to renew %s proxy." %self.proxyDN)
+      res = self.wmsAdmin.getProxy(self.proxyDN,self.proxyGroup,self.proxyLength)
+      if not res['OK']:
+        gLogger.error("TransferAgent.execute: Could not retrieve proxy from WMS Administrator", res['Message'])
+        return S_OK()
+      proxyStr = res['Value']
+      if not os.path.exists(os.path.dirname(self.proxyLocation)):
+        os.makedirs(os.path.dirname(self.proxyLocation))
+      res = setupProxy(proxyStr,self.proxyLocation)
+      if not res['OK']:
+        gLogger.error("TransferAgent.execute: Could not create environment for proxy.", res['Message'])
+        return S_OK()
+      setDIRACGroup(self.proxyGroup)
+      self.log.info("TransferAgent.execute: Successfully renewed %s proxy." %self.proxyDN)
 
     ################################################
     # Get a request from request DB
