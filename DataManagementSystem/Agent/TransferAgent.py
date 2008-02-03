041fb62bcb7dbf198b68f7643d5b9434e3db4c62
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-02-03 13:59:28 +0000

    made proxy handling optional through the CS. not used at the pit.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@6244 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Agent/TransferAgent.py
+++ DataManagementSystem/Agent/TransferAgent.py
@@ -27,11 +27,7 @@ class TransferAgent(Agent):
     result = Agent.initialize(self)
     self.RequestDBClient = RequestClient()
     self.ReplicaManager = ReplicaManager()
-    self.wmsAdmin = RPCClient('WorkloadManagement/WMSAdministrator')
-    self.proxyDN = gConfig.getValue(self.section+'/ProxyDN','')
-    self.proxyGroup = gConfig.getValue(self.section+'/ProxyGroup','')
-    self.proxyLength = gConfig.getValue(self.section+'/DefaultProxyLength',12)
-    self.proxyLocation = gConfig.getValue(self.section+'/ProxyLocation','')
+
     gMonitor.registerActivity( "Iteration", "Agent Loops/min",          "TransferAgent", "Atemps", gMonitor.OP_SUM )
     gMonitor.registerActivity( "Execute",   "Request Processed/min",    "TransferAgent", "Atemps", gMonitor.OP_SUM )
     gMonitor.registerActivity( "Done",      "Request Completed/min",    "TransferAgent", "Atemps", gMonitor.OP_SUM )
@@ -40,49 +36,61 @@ class TransferAgent(Agent):
     gMonitor.registerActivity("Replication failed","Failed replications/min","TransferAgent", "Atemps", gMonitor.OP_SUM )
     gMonitor.registerActivity("Replica registration successful","Successful replica registrations/min","TransferAgent", "Atemps", gMonitor.OP_SUM )
     gMonitor.registerActivity("Replica registration failed","Failed replica registrations/min","TransferAgent", "Atemps", gMonitor.OP_SUM )  
-    self.threadPool = ThreadPool(1,10)
+
+    self.threadPool = ThreadPool(1,1)
+
+    self.useProxies = gConfig.getValue(self.section+'/UseProxies',False)
+    if self.useProxies:
+      self.wmsAdmin = RPCClient('WorkloadManagement/WMSAdministrator')
+      self.proxyDN = gConfig.getValue(self.section+'/ProxyDN','')
+      self.proxyGroup = gConfig.getValue(self.section+'/ProxyGroup','')
+      self.proxyLength = gConfig.getValue(self.section+'/DefaultProxyLength',12)
+      self.proxyLocation = gConfig.getValue(self.section+'/ProxyLocation','')
+
     return result
 
   def execute(self):
-    ############################################################
-    #
-    # Get a valid proxy for the current activity
-    #
-    self.log.info("TransferAgent.execute: Determining the length of the %s proxy." %self.proxyDN)
-    obtainProxy = False
-    if not os.path.exists(self.proxyLocation):
-      self.log.info("TransferAgent.execute: No proxy found.")
-      obtainProxy = True
-    else:
-      currentProxy = open(self.proxyLocation,'r')
-      oldProxyStr = currentProxy.read()
-      res = getProxyTimeLeft(oldProxyStr)
-      if not res["OK"]:
-        gLogger.error("TransferAgent.execute: Could not determine the time left for proxy.", res['Message'])
-        return S_OK()
-      proxyValidity = int(res['Value'])
-      gLogger.debug("TransferAgent.execute: Current proxy found to be valid for %s seconds." % proxyValidity)
-      self.log.info("TransferAgent.execute: %s proxy found to be valid for %s seconds."% (self.proxyDN,proxyValidity))
-      if proxyValidity <= 60:
+
+    if self.useProxies:
+      ############################################################
+      #
+      # Get a valid proxy for the current activity
+      #
+      self.log.info("TransferAgent.execute: Determining the length of the %s proxy." %self.proxyDN)
+      obtainProxy = False
+      if not os.path.exists(self.proxyLocation):
+        self.log.info("TransferAgent.execute: No proxy found.")
         obtainProxy = True
+      else:
+        currentProxy = open(self.proxyLocation,'r')
+        oldProxyStr = currentProxy.read()
+        res = getProxyTimeLeft(oldProxyStr)
+        if not res["OK"]:
+          gLogger.error("TransferAgent.execute: Could not determine the time left for proxy.", res['Message'])
+          return S_OK()
+        proxyValidity = int(res['Value'])
+        gLogger.debug("TransferAgent.execute: Current proxy found to be valid for %s seconds." % proxyValidity)
+        self.log.info("TransferAgent.execute: %s proxy found to be valid for %s seconds."% (self.proxyDN,proxyValidity))
+        if proxyValidity <= 60:
+          obtainProxy = True
 
-    if obtainProxy:
-      self.log.info("TransferAgent.execute: Attempting to renew %s proxy." %self.proxyDN)
-      res = self.wmsAdmin.getProxy(self.proxyDN,self.proxyGroup,self.proxyLength)
-      if not res['OK']:
-        gLogger.error("TransferAgent.execute: Could not retrieve proxy from WMS Administrator", res['Message'])
-        return S_OK()
-      proxyStr = res['Value']
-      if not os.path.exists(os.path.dirname(self.proxyLocation)):
-        os.makedirs(os.path.dirname(self.proxyLocation))
-      res = setupProxy(proxyStr,self.proxyLocation)
-      if not res['OK']:
-        gLogger.error("TransferAgent.execute: Could not create environment for proxy.", res['Message'])
-        return S_OK()
-      setDIRACGroup(self.proxyGroup)
-      self.log.info("TransferAgent.execute: Successfully renewed %s proxy." %self.proxyDN)
+      if obtainProxy:
+        self.log.info("TransferAgent.execute: Attempting to renew %s proxy." %self.proxyDN)
+        res = self.wmsAdmin.getProxy(self.proxyDN,self.proxyGroup,self.proxyLength)
+        if not res['OK']:
+          gLogger.error("TransferAgent.execute: Could not retrieve proxy from WMS Administrator", res['Message'])
+          return S_OK()
+        proxyStr = res['Value']
+        if not os.path.exists(os.path.dirname(self.proxyLocation)):
+          os.makedirs(os.path.dirname(self.proxyLocation))
+        res = setupProxy(proxyStr,self.proxyLocation)
+        if not res['OK']:
+          gLogger.error("TransferAgent.execute: Could not create environment for proxy.", res['Message'])
+          return S_OK()
+        setDIRACGroup(self.proxyGroup)
+        self.log.info("TransferAgent.execute: Successfully renewed %s proxy." %self.proxyDN)
 
-    for i in range(100):
+    for i in range(1):
       requestExecutor = ThreadedJob(self.executeRequest)
       self.threadPool.queueJob(requestExecutor)
     self.threadPool.processResults()
