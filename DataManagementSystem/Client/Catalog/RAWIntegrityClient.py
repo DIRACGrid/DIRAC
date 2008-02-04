7c6d2555bb388800acd01b408403242fd5ecda11
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-02-04 17:20:47 +0000

    added exist() function and modified addFile to take checksum.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@6279 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Client/Catalog/RAWIntegrityClient.py
+++ DataManagementSystem/Client/Catalog/RAWIntegrityClient.py
@@ -21,6 +21,22 @@ class RAWIntegrityClient(FileCatalogueBase):
   def isOK(self):
     return self.valid
 
+  def exists(self,lfn):
+    """ LFN may be a string or list of strings
+    """
+    if type(lfn) in types.StringTypes:
+      lfns = [lfn]
+    elif type(lfn) == types.ListType:
+      lfns = lfn
+    else:
+      return S_ERROR('RAWIntegrityClient.exists: Must supply a path or list of paths.')
+    successful = {}
+    failed = {}
+    for lfn in lfns:
+      successful[lfn] = False
+    resDict = {'Failed':failed,'Successful':successful}
+    return S_OK(resDict)
+
   def addFile(self, fileTuple):
     """ A tuple should be supplied to this method which contains: (lfn,pfn,size,se,guid)
         A list of tuples may also be supplied.
@@ -33,8 +49,8 @@ class RAWIntegrityClient(FileCatalogueBase):
       return S_ERROR('RAWIntegrityClient.addFile: Must supply a file tuple of list of tuples')
     failed = {}
     successful = {}
-    for lfn,pfn,size,se,guid in files:
-      res = self.server.addFile(lfn,pfn,size,se,guid,'')
+    for lfn,pfn,size,se,guid,checksum in files:
+      res = self.server.addFile(str(lfn),str(pfn),size,str(se),str(guid),str(checksum))
       if not res['OK']:
         failed[lfn] = res['Message']
       else:
