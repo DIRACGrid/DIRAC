f8e618cb0e650eafd7982a5b104f904c3c67e5d8
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-02-06 15:18:34 +0000

    Creates the ReplicationPlacementDB database and uses the TransformationDB table definitions. Nothing else.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@6329 4525493e-7705-40b1-a816-d608a930855b

---
+++ DataManagementSystem/DB/ReplicationPlacementDB.sql
@@ -0,0 +1,22 @@
+--  Schema definition for the ReplicationPlacementDB database - containing DataManagement specific Transformations
+--  It simply uses the TransformationDB table definitions.
+---
+--------------------------------------------------------------------------------
+DROP DATABASE IF EXISTS ReplicationPlacementDB;
+CREATE DATABASE ReplicationPlacementDB;
+--------------------------------------------------------------------------------
+
+-- Database owner definition
+USE mysql;
+
+-- Must set passwords for database user by replacing "must_be_set".
+GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON ReplicationPlacementDB.* TO Dirac@localhost IDENTIFIED BY 'must_be_set';
+GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON ReplicationPlacementDB.* TO Dirac@volhcb03.cern.ch IDENTIFIED BY 'must_be_set';
+GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON ReplicationPlacementDB.* TO Dirac@'%' IDENTIFIED BY 'must_be_set';
+FLUSH PRIVILEGES;
+
+-------------------------------------------------------------------------------
+USE ReplicationPlacementDB;
+-------------------------------------------------------------------------------
+
+SOURCE TransformationDB.sql
