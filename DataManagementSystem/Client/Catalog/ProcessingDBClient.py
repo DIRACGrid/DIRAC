21b7bdd316c8b7ce53e380a261a13550ce1440e6
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-02-07 19:52:41 +0000

    present for gennady.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@6393 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Client/Catalog/ProcessingDBClient.py
+++ DataManagementSystem/Client/Catalog/ProcessingDBClient.py
@@ -1,10 +1,10 @@
 """ Client for ProcessingDB file catalog tables
 """
-
 from DIRAC  import gLogger, gConfig, S_OK, S_ERROR
 from DIRAC.Core.DISET.RPCClient import RPCClient
 from DIRAC.ConfigurationSystem.Client import PathFinder
-from types import *
+from DIRAC.Core.Transformation.TransformationDBClient import TransformationDBClient
+import types
 
 class ProcessingDBClient:
   """ File catalog client for processing DB
@@ -13,29 +13,13 @@ class ProcessingDBClient:
   def __init__(self, url=False, useCertificates=False):
     """ Constructor of the ProcessingDB catalogue client
     """
-    self.name = 'ProcDB'
-    if not url:
-      self.server = RPCClient("ProductionManagement/ProcessingDB",useCertificates,timeout=120)
-    else:
-      self.server = RPCClient(self.url,useCertificates,timeout = 120)
-
-  def getName(self,DN=''):
-    """ Get the file catalog type name
-    """
-    return self.name
-
-  def __getattr__(self, name):
-    self.call = name
-    return self.execute
-
-  def execute(self, *parms, **kws):
-    """ Magic method dispatcher """
+    self.name = 'ProcessingDB'
+    self.valid = True
     try:
-      inputParameter = parms[0]
-      if not type(inputParameter) == ListType:
-        inputParameters = [inputParameter]
+      if not url:
+        oServer = RPCClient("ProductionManagement/ProcessingDB")
       else:
-        inputParameters = inputParameter
-      return self.server.callProxyMethod(self.call,inputParameters,kws)
-    except Exception,x:
-      return S_ERROR("ProcessingDBClient.execute: Exception while calling the server.",str(x))
+        oServer = RPCClient(self.url)
+      self.setServer(oServer)
+    except:
+      self.valid = False
