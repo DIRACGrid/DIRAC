6b707b50b30aaa271b0937d42bb6b805dce41788
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-02-07 16:58:50 +0000

    publishTransformation()
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@6377 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Service/ReplicationPlacementHandler.py
+++ DataManagementSystem/Service/ReplicationPlacementHandler.py
@@ -8,16 +8,33 @@ from DIRAC.DataManagementSystem.DB.ReplicationPlacementDB import ReplicationPlac
 from DIRAC.Core.Transformation.TransformationHandler import TransformationHandler
 
 # This is a global instance of the ReplicationPlacementDB class
-ReplicationPlacementDB = False
+placementDB = False
 
 def initializeReplicationPlacementHandler(serviceInfo):
-  global ReplicationPlacementDB
-  ReplicationPlacementDB = ReplicationPlacementDB()
+  global placementDB
+  placementDB = ReplicationPlacementDB()
   return S_OK()
 
 class ReplicationPlacementHandler(TransformationHandler):
 
   def __init__(self,*args,**kargs):
 
-    self.setDatabase(ReplicationPlacementDB)
-    TransformationHandler.__init__(*args,**kargs)
+    self.setDatabase(placementDB)
+    TransformationHandler.__init__(self,*args,**kargs)
+
+  types_publishTransformation = []
+  def export_publishTransformation(self, transName, desciption,longDesription, type, mode, fileMask):
+    """ Publish new transformation in the TransformationDB
+    """
+    authorDN = self.transport.peerCredentials['DN']
+    #authorName = self.transport.peerCredentials['user']
+    authorGroup = self.transport.peerCredentials['group']
+    try:
+      res = placementDB.addTransformation(transName,desciption,longDesription,authorDN,authorGroup,type,mode,'ReplicationPlacementAgent','New',fileMask)
+      return res
+    except Exception,x:
+      errStr = "ReplicationPlacementHandler.publishTransformation: Exception while adding transformation."
+      gLogger.exception(errStr,str(x))
+      return S_ERROR(errStr)
+
+
