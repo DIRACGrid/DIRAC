3f1041acb535c4ad72af971dd28657a9ba3df085
Stuart K. Paterson <stuart.paterson@cern.ch>
2008-02-08 16:38:42 +0000

    DIRAC.Interfaces.scripts package.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@6428 4525493e-7705-40b1-a816-d608a930855b

---
+++ Interfaces/scripts/__init__.py
@@ -0,0 +1,9 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Interfaces/scripts/__init__.py,v 1.1 2008/02/08 16:38:42 paterson Exp $
+############################################################
+
+"""
+   DIRAC.Interfaces.scripts package
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2008/02/08 16:38:42 paterson Exp $"
