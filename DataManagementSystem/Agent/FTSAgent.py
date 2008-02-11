1288a576dc5680ac659b628ab91c3b2efd40fc67
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-02-11 17:43:50 +0000

    correct proxy handling.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@6481 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Agent/FTSAgent.py
+++ DataManagementSystem/Agent/FTSAgent.py
@@ -7,8 +7,9 @@ from DIRAC.DataManagementSystem.DB.TransferDB import TransferDB
 #from DIRAC.Core.FileCatalog.LcgFileCatalogProxyClient import LcgFileCatalogProxyClient
 from DIRAC.Core.Utilities.Pfn import pfnparse, pfnunparse
 from DIRAC.DataManagementSystem.Client.FTSRequest import FTSRequest
-
-import time
+from DIRAC.Core.DISET.RPCClient import RPCClient
+from DIRAC.Core.Utilities.GridCredentials import setupProxy,restoreProxy,setDIRACGroup, getProxyTimeLeft
+import os,time
 from types import *
 
 AGENT_NAME = 'DataManagement/FTSAgent'
@@ -27,9 +28,57 @@ class FTSAgent(Agent):
     self.maxJobsPerChannel = gConfig.getValue(self.section+'/MaxJobsPerChannel',2)
     self.submissionsPerLoop = gConfig.getValue(self.section+'/SubmissionsPerLoop',1)
     self.monitorsPerLoop = gConfig.getValue(self.section+'/MonitorsPerLoop',1)
+
+    self.useProxies = gConfig.getValue(self.section+'/UseProxies','True')
+    if self.useProxies == 'True':
+      self.wmsAdmin = RPCClient('WorkloadManagement/WMSAdministrator')
+      self.proxyDN = gConfig.getValue(self.section+'/ProxyDN','')
+      self.proxyGroup = gConfig.getValue(self.section+'/ProxyGroup','')
+      self.proxyLength = gConfig.getValue(self.section+'/DefaultProxyLength',12)
+      self.proxyLocation = gConfig.getValue(self.section+'/ProxyLocation','')
+
     return result
 
   def execute(self):
+
+    if self.useProxies == 'True':
+      ############################################################
+      #
+      # Get a valid proxy for the current activity
+      #
+      self.log.info("TransferAgent.execute: Determining the length of the %s proxy." %self.proxyDN)
+      obtainProxy = False
+      if not os.path.exists(self.proxyLocation):
+        self.log.info("TransferAgent.execute: No proxy found.")
+        obtainProxy = True
+      else:
+        currentProxy = open(self.proxyLocation,'r')
+        oldProxyStr = currentProxy.read()
+        res = getProxyTimeLeft(oldProxyStr)
+        if not res["OK"]:
+          gLogger.error("TransferAgent.execute: Could not determine the time left for proxy.", res['Message'])
+          return S_OK()
+        proxyValidity = int(res['Value'])
+        gLogger.debug("TransferAgent.execute: Current proxy found to be valid for %s seconds." % proxyValidity)
+        self.log.info("TransferAgent.execute: %s proxy found to be valid for %s seconds."% (self.proxyDN,proxyValidity))
+        if proxyValidity <= 60:
+          obtainProxy = True
+
+      if obtainProxy:
+        self.log.info("TransferAgent.execute: Attempting to renew %s proxy." %self.proxyDN)
+        res = self.wmsAdmin.getProxy(self.proxyDN,self.proxyGroup,self.proxyLength)
+        if not res['OK']:
+          gLogger.error("TransferAgent.execute: Could not retrieve proxy from WMS Administrator", res['Message'])
+          return S_OK()
+        proxyStr = res['Value']
+        if not os.path.exists(os.path.dirname(self.proxyLocation)):
+          os.makedirs(os.path.dirname(self.proxyLocation))
+        res = setupProxy(proxyStr,self.proxyLocation)
+        if not res['OK']:
+          gLogger.error("TransferAgent.execute: Could not create environment for proxy.", res['Message'])
+          return S_OK()
+        setDIRACGroup(self.proxyGroup)
+        self.log.info("TransferAgent.execute: Successfully renewed %s proxy." %self.proxyDN)
 
     for i in range(self.submissionsPerLoop):
       infoStr = "\n\n##################################################################################\n\n"
