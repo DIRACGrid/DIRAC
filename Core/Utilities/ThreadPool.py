07fa289c9ccdb088ea57b1a597fd24e450328710
Adria Casajus <adria@ecm.ub.es>
2008-02-12 14:37:45 +0000

    Moved all private parts under private namespace
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@6499 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/ThreadPool.py
+++ Core/Utilities/ThreadPool.py
@@ -1,8 +1,8 @@
 #################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/ThreadPool.py,v 1.9 2008/01/23 12:09:42 acasajus Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/ThreadPool.py,v 1.10 2008/02/12 14:37:45 acasajus Exp $
 #################################################################
 
-__RCSID__ = "$Id: ThreadPool.py,v 1.9 2008/01/23 12:09:42 acasajus Exp $"
+__RCSID__ = "$Id: ThreadPool.py,v 1.10 2008/02/12 14:37:45 acasajus Exp $"
 
 import time
 import sys
@@ -15,29 +15,29 @@ class WorkingThread( threading.Thread ):
   def __init__( self, oPendingQueue, oResultsQueue, **kwargs ):
     threading.Thread.__init__( self, **kwargs )
     self.setDaemon(1)
-    self.oPendingQueue = oPendingQueue
-    self.oResultsQueue = oResultsQueue
-    self.bAlive = True
-    self.bWorking = False
+    self.__pendingQueue = oPendingQueue
+    self.__resultsQueue = oResultsQueue
+    self.__threadAlive = True
+    self.__working = False
     self.start()
 
   def isWorking(self):
-    return self.bWorking
+    return self.__working
 
   def kill( self ):
-    self.bAlive = False
+    self.__threadAlive = False
 
   def run( self ):
-    while self.bAlive:
-      oJob = self.oPendingQueue.get( block = True )
-      if not self.bAlive:
-        self.oPendingQueue.put( oJob )
+    while self.__threadAlive:
+      oJob = self.__pendingQueue.get( block = True )
+      if not self.__threadAlive:
+        self.__pendingQueue.put( oJob )
         break
-      self.bWorking = True
+      self.__working = True
       oJob.process()
-      self.bWorking = False
-      if oJob.oCallback or oJob.oExceptionCallback:
-        self.oResultsQueue.put( oJob, block = True )
+      self.__working = False
+      if oJob.hasCallback():
+        self.__resultsQueue.put( oJob, block = True )
 
 
 class ThreadedJob:
@@ -49,96 +49,105 @@ class ThreadedJob:
                 sTJId = None,
                 oCallback = None,
                 oExceptionCallback = None ):
-    self.oCallable = oCallable
-    self.args = args or []
-    self.kwargs = kwargs or {}
-    self.sTJId = sTJId
-    self.oCallback = oCallback
-    self.oExceptionCallback = oExceptionCallback
-    self.bProcessed = False
-    self.bExceptionRaised = False
+    self.__jobFunction = oCallable
+    self.__jobArgs = args or []
+    self.__jobKwArgs = kwargs or {}
+    self.__tjID = sTJId
+    self.__resultCallback = oCallback
+    self.__exceptionCallback = oExceptionCallback
+    self.__done = False
+    self.__exceptionRaised = False
 
   def jobId( self ):
-    return self.sTJId
+    return self.__tjID
+
+  def hasCallback( self ):
+    return self.__resultCallback or self.__exceptionCallback
 
   def exceptionRaised( self ):
-    return self.bExceptionRaised
+    return self.__exceptionRaised
 
   def doExceptionCallback( self ):
-    if self.bProcessed and self.bExceptionRaised and self.oExceptionCallback:
-      self.oExceptionCallback( self, self.uException )
+    if self.__done and self.__exceptionRaised and self.__exceptionCallback:
+      self.__exceptionCallback( self, self.__jobException )
 
   def doCallback( self ):
-    if self.bProcessed and not self.bExceptionRaised and self.oCallback:
-      self.oCallback( self, self.uResult )
+    if self.__done and not self.__exceptionRaised and self.__resultCallback:
+      self.__resultCallback( self, self.__jobResult )
 
   def process( self ):
-    self.bProcessed = True
+    self.__done = True
     try:
-      self.uResult = self.oCallable( *self.args, **self.kwargs)
+      self.__jobResult = self.__jobFunction( *self.__jobArgs, **self.__jobKwArgs)
     except:
-      self.bExceptionRaised = True
-      self.uException = sys.exc_info()
+      self.__exceptionRaised = True
+      self.__jobException = sys.exc_info()
 
 class ThreadPool( threading.Thread ):
 
   def __init__( self, iMinThreads, iMaxThreads = 0, iMaxQueuedRequests = 0 ):
     threading.Thread.__init__( self )
     if iMinThreads < 1:
-      self.iMinThreads = 1
+      self.__minThreads = 1
     else:
-      self.iMinThreads = iMinThreads
-    if iMaxThreads < self.iMinThreads:
-      self.iMaxThreads = self.iMinThreads
+      self.__minThreads = iMinThreads
+    if iMaxThreads < self.__minThreads:
+      self.__maxThreads = self.__minThreads
     else:
-      self.iMaxThreads = iMaxThreads
-    self.oPendingQueue = Queue.Queue( iMaxQueuedRequests )
-    self.oResultsQueue = Queue.Queue( iMaxQueuedRequests + iMaxThreads )
-    self.lWorkingThreads = []
+      self.__maxThreads = iMaxThreads
+    self.__pendingQueue = Queue.Queue( iMaxQueuedRequests )
+    self.__resultsQueue = Queue.Queue( iMaxQueuedRequests + iMaxThreads )
+    self.__workingThreadsList = []
     self.__spawnNeededWorkingThreads()
+
+  def getMaxThreads( self ):
+    return self.__maxThreads
+
+  def getMinThreads( self ):
+    return self.__minThreads
 
   def numWorkingThreads( self ):
     return self.__countWorkingThreads()
 
   def __spawnWorkingThread( self ):
-    self.lWorkingThreads.append( WorkingThread( self.oPendingQueue, self.oResultsQueue ) )
+    self.__workingThreadsList.append( WorkingThread( self.__pendingQueue, self.__resultsQueue ) )
 
   def __killWorkingThread( self ):
-    self.lWorkingThreads[0].kill()
-    del( self.lWorkingThreads[0] )
+    self.__workingThreadsList[0].kill()
+    del( self.__workingThreadsList[0] )
 
   def __countWaitingThreads(self ):
     iWaitingThreads = 0
-    for oWT in self.lWorkingThreads:
+    for oWT in self.__workingThreadsList:
       if not oWT.isWorking():
         iWaitingThreads += 1
     return iWaitingThreads
 
   def __countWorkingThreads(self ):
     iWorkingThreads = 0
-    for oWT in self.lWorkingThreads:
+    for oWT in self.__workingThreadsList:
       if oWT.isWorking():
         iWorkingThreads += 1
     return iWorkingThreads
 
   def __spawnNeededWorkingThreads( self ):
-    while len( self.lWorkingThreads ) < self.iMinThreads:
+    while len( self.__workingThreadsList ) < self.__minThreads:
       self.__spawnWorkingThread()
     while self.__countWaitingThreads() == 0 and \
-          len( self.lWorkingThreads ) < self.iMaxThreads:
+          len( self.__workingThreadsList ) < self.__maxThreads:
       self.__spawnWorkingThread()
 
   def __killExceedingWorkingThreads( self ):
-    while len( self.lWorkingThreads ) > self.iMaxThreads:
+    while len( self.__workingThreadsList ) > self.__maxThreads:
       self.__killWorkingThread()
-    while self.__countWaitingThreads() > self.iMinThreads:
+    while self.__countWaitingThreads() > self.__minThreads:
       self.__killWorkingThread()
 
   def queueJob( self, oTJob, blocking = True ):
     if not isinstance( oTJob, ThreadedJob ):
       raise TypeError( "Jobs added to the thread pool must be ThreadedJob instances" )
     try:
-      self.oPendingQueue.put( oTJob, block = blocking )
+      self.__pendingQueue.put( oTJob, block = blocking )
     except Queue.Full:
       return S_ERROR( "Queue is full" )
     return S_OK()
@@ -155,19 +164,19 @@ class ThreadPool( threading.Thread ):
     return self.queueJob( oTJ, blocking )
 
   def pendingJobs( self ):
-    return self.oPendingQueue.qsize()
+    return self.__pendingQueue.qsize()
 
   def isFull( self ):
-    return self.oPendingQueue.full()
+    return self.__pendingQueue.full()
 
   def processResults( self ):
     iProcessed = 0
     while True:
       self.__spawnNeededWorkingThreads()
-      if self.oResultsQueue.empty():
+      if self.__resultsQueue.empty():
         self.__killExceedingWorkingThreads()
         break
-      oJob = self.oResultsQueue.get()
+      oJob = self.__resultsQueue.get()
       oJob.doExceptionCallback()
       oJob.doCallback()
       iProcessed += 1
@@ -175,7 +184,7 @@ class ThreadPool( threading.Thread ):
     return iProcessed
 
   def processAllResults( self ):
-    while not self.oPendingQueue.empty() or self.__countWorkingThreads():
+    while not self.__pendingQueue.empty() or self.__countWorkingThreads():
       self.processResults()
       time.sleep( 0.1 )
     self.processResults()
@@ -218,14 +227,14 @@ if __name__=="__main__":
                        oExceptionCallback = showException )
       oTP.queueJob( oTJ )
 
-  print 'MaxThreads =', oTP.iMaxThreads
-  print 'MinThreads =', oTP.iMinThreads
+  print 'MaxThreads =', oTP.getMaxThreads()
+  print 'MinThreads =', oTP.getMinThreads()
 
   generateWork( 30 )
   while True:
     time.sleep(1)
     iResult = oTP.processResults()
-    iNew = iResult + random.randint(-2,2)
+    iNew = iResult + random.randint(-3,2)
     print "Processed %s, generating %s.." % ( iResult, iNew )
     generateWork( iNew )
     print "Threads %s" % oTP.numWorkingThreads(), oTP.pendingJobs()
