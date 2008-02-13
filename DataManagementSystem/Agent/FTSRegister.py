89ee99b3d6eb44710ebe0d53c9592c47bb6650a1
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-02-13 14:35:29 +0000

    Registers files transferred with FTS.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@6530 4525493e-7705-40b1-a816-d608a930855b

---
+++ DataManagementSystem/Agent/FTSRegister.py
@@ -0,0 +1,93 @@
+"""  FTS Submit Agent takes files from the TransferDB and submits them to the FTS
+"""
+from DIRAC  import gLogger, gConfig, S_OK, S_ERROR
+from DIRAC.Core.Base.Agent import Agent
+from DIRAC.ConfigurationSystem.Client.PathFinder import getDatabaseSection
+from DIRAC.DataManagementSystem.DB.TransferDB import TransferDB
+from DIRAC.Core.DISET.RPCClient import RPCClient
+from DIRAC.Core.Utilities.GridCredentials import setupProxy,setDIRACGroup,getProxyTimeLeft
+from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
+import os,time
+from types import *
+
+AGENT_NAME = 'DataManagement/FTSRegister'
+
+class FTSRegister(Agent):
+
+  def __init__(self):
+    """ Standard constructor
+    """
+    Agent.__init__(self,AGENT_NAME)
+
+  def initialize(self):
+    result = Agent.initialize(self)
+    self.TransferDB = TransferDB()
+    self.ReplicaManager = ReplicaManager()
+
+    self.useProxies = gConfig.getValue(self.section+'/UseProxies','True')
+    if self.useProxies == 'True':
+      self.wmsAdmin = RPCClient('WorkloadManagement/WMSAdministrator')
+      self.proxyDN = gConfig.getValue(self.section+'/ProxyDN','')
+      self.proxyGroup = gConfig.getValue(self.section+'/ProxyGroup','')
+      self.proxyLength = gConfig.getValue(self.section+'/DefaultProxyLength',12)
+      self.proxyLocation = gConfig.getValue(self.section+'/ProxyLocation','')
+    return result
+
+  def execute(self):
+
+    if self.useProxies == 'True':
+      ############################################################
+      #
+      # Get a valid proxy for the current activity
+      #
+      self.log.info("TransferAgent.execute: Determining the length of the %s proxy." %self.proxyDN)
+      obtainProxy = False
+      if not os.path.exists(self.proxyLocation):
+        self.log.info("TransferAgent.execute: No proxy found.")
+        obtainProxy = True
+      else:
+        currentProxy = open(self.proxyLocation,'r')
+        oldProxyStr = currentProxy.read()
+        res = getProxyTimeLeft(oldProxyStr)
+        if not res["OK"]:
+          gLogger.error("TransferAgent.execute: Could not determine the time left for proxy.", res['Message'])
+          return S_OK()
+        proxyValidity = int(res['Value'])
+        gLogger.debug("TransferAgent.execute: Current proxy found to be valid for %s seconds." % proxyValidity)
+        self.log.info("TransferAgent.execute: %s proxy found to be valid for %s seconds."% (self.proxyDN,proxyValidity))
+        if proxyValidity <= 60:
+          obtainProxy = True
+
+      if obtainProxy:
+        self.log.info("TransferAgent.execute: Attempting to renew %s proxy." %self.proxyDN)
+        res = self.wmsAdmin.getProxy(self.proxyDN,self.proxyGroup,self.proxyLength)
+        if not res['OK']:
+          gLogger.error("TransferAgent.execute: Could not retrieve proxy from WMS Administrator", res['Message'])
+          return S_OK()
+        proxyStr = res['Value']
+        if not os.path.exists(os.path.dirname(self.proxyLocation)):
+          os.makedirs(os.path.dirname(self.proxyLocation))
+        res = setupProxy(proxyStr,self.proxyLocation)
+        if not res['OK']:
+          gLogger.error("TransferAgent.execute: Could not create environment for proxy.", res['Message'])
+          return S_OK()
+        setDIRACGroup(self.proxyGroup)
+        self.log.info("TransferAgent.execute: Successfully renewed %s proxy." %self.proxyDN)
+
+    res = self.TransferDB.getWaitingRegistrations()
+    if not res['OK']:
+      gLogger.error("FTSRegister.execute: Failed to get waiting registrations from TransferDB.",res['Message'])
+      return S_OK()
+    lfns = {}
+    replicaTuples = []
+    for fileID,channelID,lfn,pfn,se in res['Value']:
+      lfns[lfn] = (channelID,fileID)
+      replicaTuples.append((lfn,pfn,se))
+    res = self.ReplicaManager.registerReplica(replicaTuples)
+    if not res['OK']:
+      gLogger.error("FTSRegister.execute: Completely failed to regsiter replicas.",res['Message'])
+      return S_OK()
+    for lfn in res['Value']['Successful'].keys():
+      channelID,fileID = lfns[lfn]
+      res = self.TransferDB.setRegistrationDone(channelID,fileID)
+    return S_OK()
