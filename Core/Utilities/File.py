d21df816a11580c245b244df6c0d3d24a2e02ef5
Joel Closier <Joel.Closier@cern.ch>
2008-02-13 14:14:38 +0000

    revert back to remove SPECIFIC LHCb definition.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@6528 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/File.py
+++ Core/Utilities/File.py
@@ -1,12 +1,12 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/File.py,v 1.19 2008/02/13 10:27:56 joel Exp $
-__RCSID__ = "$Id: File.py,v 1.19 2008/02/13 10:27:56 joel Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/File.py,v 1.20 2008/02/13 14:14:38 joel Exp $
+__RCSID__ = "$Id: File.py,v 1.20 2008/02/13 14:14:38 joel Exp $"
 
 """
    Collection of DIRAC useful file related modules
    by default on Error they return None
 """
 
-import os, re
+import os
 import md5
 import random
 
@@ -67,41 +67,3 @@ def getSize( fileName ):
     return -1
 
 
-def makeProductionLfn(self,filetuple,mode,prodstring):
-    """ Constructs the logical file name according to LHCb conventions.
-    Returns the lfn without 'lfn:' prepended
-    """
-
-    try:
-      jobid = int(self.JOB_ID)
-      jobindex = string.zfill(jobid/10000,4)
-    except:
-      jobindex = '0000'
-
-    fname = filetuple[0]
-    if re.search('lfn:',fname):
-      return fname.replace('lfn:','')
-    else:
-      if re.search('LFN:',fname):
-        return fname.replace('LFN:','')
-      else:
-#        path = makeProductionPath(self,mode,prodstring)
-        return self.LFN_ROOT+'/'+filetuple[1]+'/'+prodstring+'/'+jobindex+'/'+filetuple[0]
-
-def makeProductionPath(self,typeName,mode,prodstring,log=False):
-  """ Constructs the path in the logical name space where the output
-  data for the given production will go.
-  """
-#  result = '/lhcb/'+mode+'/'+self.CONFIG_NAME+'/'+self.CONFIG_VERSION+'/'+prodstring+'/'
-#  result = '/lhcb/'+self.DataType+'/'+self.YEAR+'/'+self.appType.upper()+'/'+self.CONFIG_NAME+'/'+prodstring+'/'
-  result = self.LFN_ROOT+'/'+typeName+'/'+self.CONFIG_NAME+'/'+prodstring+'/'
-
-  if log:
-    try:
-      jobid = int(self.JOB_ID)
-      jobindex = string.zfill(jobid/10000,4)
-    except:
-      jobindex = '0000'
-    result += 'LOG/'+jobindex
-
-  return result
