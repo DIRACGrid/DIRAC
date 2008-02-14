c1127a33d50067f04f208b4407328a6f93b63561
Stuart K. Paterson <stuart.paterson@cern.ch>
2008-02-14 16:28:56 +0000

    Minor bug fix.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@6561 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/Client/InputDataByProtocol.py
+++ WorkloadManagementSystem/Client/InputDataByProtocol.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Id: InputDataByProtocol.py,v 1.2 2008/02/13 09:06:21 paterson Exp $
+# $Id: InputDataByProtocol.py,v 1.3 2008/02/14 16:28:56 paterson Exp $
 # File :   InputDataByProtocol.py
 # Author : Stuart Paterson
 ########################################################################
@@ -9,7 +9,7 @@
     defined in the CS for the VO.
 """
 
-__RCSID__ = "$Id: InputDataByProtocol.py,v 1.2 2008/02/13 09:06:21 paterson Exp $"
+__RCSID__ = "$Id: InputDataByProtocol.py,v 1.3 2008/02/14 16:28:56 paterson Exp $"
 
 from DIRAC.Core.DISET.RPCClient                                     import RPCClient
 from DIRAC.DataManagementSystem.Client.ReplicaManager               import ReplicaManager
@@ -170,7 +170,7 @@ class InputDataByProtocol:
                   failedReplicas.append(lfn)
 
       if badTURLCount:
-        self.log.warn('Job Wrapper found %s problematic TURL(s) for job %s' % (badTURLCount,jobID))
+        self.log.warn('Job Wrapper found %s problematic TURL(s) for job %s' % (badTURLCount,self.jobID))
         param = string.join(badTURLs,'\n')
         self.log.info(param)
         result = self.__setJobParam('ProblematicTURLs',param)
