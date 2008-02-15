85599efe570538ba5c2a0abe35af8a43e5f8496e
Stuart K. Paterson <stuart.paterson@cern.ch>
2008-02-15 13:55:50 +0000

    First import of DiracProduction API, only has one method so far (but it works :)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@6589 4525493e-7705-40b1-a816-d608a930855b

---
+++ Interfaces/API/DiracProduction.py
@@ -0,0 +1,162 @@
+########################################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Interfaces/API/DiracProduction.py,v 1.1 2008/02/15 13:55:50 paterson Exp $
+# File :   LHCbJob.py
+# Author : Stuart Paterson
+########################################################################
+
+from DIRAC.Core.Base import Script
+Script.parseCommandLine()
+
+"""DIRAC Production Management Class (Under Development)
+
+   The DIRAC Production class allows to submit jobs using the
+   Production Management System.
+
+   Helper functions are documented with example usage for the DIRAC API.
+"""
+
+__RCSID__ = "$Id: DiracProduction.py,v 1.1 2008/02/15 13:55:50 paterson Exp $"
+
+import string, re, os, time, shutil, types, copy
+
+from DIRAC.Core.Workflow.Parameter                  import *
+from DIRAC.Core.Workflow.Module                     import *
+from DIRAC.Core.Workflow.Step                       import *
+from DIRAC.Core.Workflow.Workflow                   import *
+from DIRAC.Core.Workflow.WorkflowReader             import *
+from DIRAC.Interfaces.API.Job                       import Job
+from DIRAC.Interfaces.API.Dirac                     import Dirac
+from DIRAC.Core.DISET.RPCClient                     import RPCClient
+from DIRAC.Core.Utilities.File                      import makeGuid
+from DIRAC                                          import gConfig, gLogger, S_OK, S_ERROR
+
+COMPONENT_NAME='DiracProduction'
+
+class DiracProduction:
+
+  #############################################################################
+
+  def __init__(self):
+    """Instantiates the Workflow object and some default parameters.
+    """
+    self.log = gLogger.getSubLogger(COMPONENT_NAME)
+    gLogger.setLevel('verbose')
+    self.section    = COMPONENT_NAME
+    self.scratchDir = gConfig.getValue('/LocalSite/ScratchDir','/tmp')
+    self.scratchDir = gConfig.getValue('/LocalSite/ScratchDir','/tmp')
+    self.submittedStatus = gConfig.getValue(self.section+'/ProcDBSubStatus','SUBMITTED')
+    self.prodClient = RPCClient('ProductionManagement/ProductionManager')
+    self.toCleanUp = []
+    self.diracAPI = Dirac()
+
+  #############################################################################
+  def submitProduction(self,productionID,numberOfJobs):
+    """Calls the production manager service to retrieve the necessary information
+       to construct jobs, these are then submitted via the API.
+    """
+    if not type(productionID) == type(" "):
+      return self.__errorReport('Expected string for production ID')
+    if type(numberOfJobs) == type(" "):
+      try:
+        numberOfJobs = int(numberOfJobs)
+      except Exception,x:
+        return self.__errorReport(str(x),'Expected integer or string for number of jobs to submit')
+
+    result = self.prodClient.getJobsToSubmit(long(productionID),numberOfJobs)
+    if not result['OK']:
+      return self.__errorReport(result,'Problem while requesting data from ProductionManager')
+
+    submission = self.__createProductionJobs(productionID,result['Value'],numberOfJobs)
+    return submission
+
+  #############################################################################
+  def __createProductionJobs(self,prodID,prodDict,number):
+    """Wrapper to submit job to WMS.
+    """
+    #{'Body':<workflow_xml>,'JobDictionary':{<job_number(int)>:{paramname:paramvalue}}}
+    if not prodDict.has_key('Body') or not prodDict.has_key('JobDictionary'):
+      return self.__errorReport(prodDict,'Unexpected result from ProductionManager service')
+
+    submitted =  []
+    failed = []
+    xmlString = prodDict['Body']
+    #creating a /tmp/guid/ directory for job submission files
+    jfilename = self.__createJobDescriptionFile(xmlString)
+    prodJob = Job(jfilename)
+    jobDict = prodDict['JobDictionary']
+    for jobNumber,paramsDict in jobDict.items():
+      for paramName,paramValue in paramsDict.items():
+        print jobNumber,paramName,paramValue
+        if paramName=='InputData':
+          self.log.verbose('Setting input data to %s' %paramValue)
+          prodJob.setInputData(paramValue)
+          constructedName = str(prodID).zfill(8)+'_'+str(jobNumber).zfill(8)
+          self.log.verbose('Setting job name to %s' %constructedName)
+          prodJob.setName(constructedName)
+          prodJob._setParamValue('PRODUCTION_ID',str(prodID).zfill(8))
+          prodJob._setParamValue('JOB_ID',str(jobNumber).zfill(8))
+
+      self.log.debug(prodJob.createCode())
+      updatedJob = self.__createJobDescriptionFile(prodJob._toXML())
+      self.log.verbose(prodJob._toJDL(updatedJob))
+      subResult = self.__submitJob(prodJob)
+      if subResult['OK']:
+        jobID = subResult['Value']
+        submitted.append(jobID)
+        #Now update status in the processing DB
+        result = self.prodClient.setJobStatusAndWmsID(long(prodID),long(jobNumber),self.submittedStatus,str(jobID))
+        self.log.debug(result)
+        if not result['OK']:
+          return self.__errorReport(result,'Could not report status to ProcDB, stopping submission of further jobs')
+      else:
+        failed.append(jobNumber)
+
+    self.__cleanUp()
+    result = S_OK()
+    self.log.info('Job Submission Summary: Requested=%s, Submitted=%s, Failed=%s' %(number,len(submitted),len(failed)))
+    result['Value'] = {'Successful':submitted,'Failed':failed}
+    return result
+
+  #############################################################################
+  def __createJobDescriptionFile(self,xmlString):
+    guid = makeGuid()
+    tmpdir = self.scratchDir+'/'+guid
+    self.log.verbose('Created temporary directory for preparing submission %s' % (tmpdir))
+    os.mkdir(tmpdir)
+
+    jfilename = tmpdir+'/jobDescription.xml'
+    jfile=open(jfilename,'w')
+    print >> jfile , xmlString
+    jfile.close()
+    self.toCleanUp.append(tmpdir)
+    return jfilename
+
+  #############################################################################
+  def __cleanUp(self):
+    for i in self.toCleanUp:
+      self.log.debug('Removing temporary directory %s' %i)
+      shutil.rmtree(i)
+    return S_OK()
+
+  #############################################################################
+  def __submitJob(self,prodJob):
+    """Wrapper to submit job to WMS.
+    """
+    self.log.verbose('Attempting to submit job to WMS')
+    submitted = self.diracAPI.submit(prodJob)
+    if not submitted['OK']:
+      self.log.warn('Problem during submission of job')
+      self.log.warn(submitted)
+    return submitted
+
+  #############################################################################
+  def __errorReport(self,error,message=None):
+    """Internal function to return errors and exit with an S_ERROR()
+    """
+    if not message:
+      message = error
+
+    self.log.warn(error)
+    return S_ERROR(message)
+
+  #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
