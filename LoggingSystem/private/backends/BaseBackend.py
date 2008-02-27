cae5119689860d4afa80be03aae9c8f2b3ae0e89
Marcos Antonio Seco <secomig@usc.es>
2008-02-27 09:20:22 +0000

    A really minor update
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@6893 4525493e-7705-40b1-a816-d608a930855b

--- LoggingSystem/private/backends/BaseBackend.py
+++ LoggingSystem/private/backends/BaseBackend.py
@@ -1,5 +1,5 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/LoggingSystem/private/backends/BaseBackend.py,v 1.7 2008/02/26 20:15:59 mseco Exp $
-__RCSID__ = "$Id: BaseBackend.py,v 1.7 2008/02/26 20:15:59 mseco Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/LoggingSystem/private/backends/BaseBackend.py,v 1.8 2008/02/27 09:20:22 mseco Exp $
+__RCSID__ = "$Id: BaseBackend.py,v 1.8 2008/02/27 09:20:22 mseco Exp $"
 """  This constitues the base class for the backends of the logger
 """
 from DIRAC.Core.Utilities import Time
@@ -27,7 +27,7 @@ class BaseBackend:
     timeToShow = Time.toString( messageObject.getTime() ).split('.')[0]
     lines = []
     for lineString in messageObject.getMessage().split( "\n" ):
-      lines.append( "%sUTC %s %s: %s" % ( timeToShow,
+      lines.append( "%s UTC %s %s: %s" % ( timeToShow,
                                            messageName,
                                            messageObject.getLevel().rjust( 5 ),
                                            lineString ) )
