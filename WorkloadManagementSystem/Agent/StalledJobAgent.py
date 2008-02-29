3bcc003a3e5897a37ef7635531baa144c21fbddc
Stuart K. Paterson <stuart.paterson@cern.ch>
2008-02-29 10:22:18 +0000

    Some tidying and set default stalled time to 2hrs since last update.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@6948 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/Agent/StalledJobAgent.py
+++ WorkloadManagementSystem/Agent/StalledJobAgent.py
@@ -1,16 +1,14 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Agent/StalledJobAgent.py,v 1.2 2008/02/26 18:03:10 paterson Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Agent/StalledJobAgent.py,v 1.3 2008/02/29 10:22:18 paterson Exp $
 # File :   StalledJobAgent.py
 ########################################################################
 
 """  The StalledJobAgent hunts for stalled jobs in the Job database. Jobs in "running"
      state not receiving a heartbeat signal for more than stalledTime
-     seconds will be assigned "stalled" state. Jobs in "stalled" state
-     having the last heartbeat no older than revivalTime will
-     be assigned "running" state.
+     seconds will be assigned the "Stalled" state.
 """
 
-__RCSID__ = "$Id: StalledJobAgent.py,v 1.2 2008/02/26 18:03:10 paterson Exp $"
+__RCSID__ = "$Id: StalledJobAgent.py,v 1.3 2008/02/29 10:22:18 paterson Exp $"
 
 from DIRAC.WorkloadManagementSystem.DB.JobDB        import JobDB
 from DIRAC.WorkloadManagementSystem.DB.JobLoggingDB import JobLoggingDB
@@ -47,15 +45,12 @@ class StalledJobAgent(Agent):
     """ The main agent execution method
     """
     self.log.verbose( 'Waking up Stalled Job Agent' )
-    stalledTime   = gConfig.getValue(self.section+'/StalledTimeHours',4)
+    stalledTime   = gConfig.getValue(self.section+'/StalledTimeHours',2)
     self.log.verbose('StalledTime = %s hours' %(stalledTime))
-    revivalTime   = gConfig.getValue(self.section+'/RevivalTimeHours',1)
-    self.log.verbose('RevivalTime = %s hours' %(revivalTime))
     try:
       stalledTime = int(stalledTime)*60*60
-      revivalTime = int(revivalTime)*60*60
     except Exception,x:
-      self.log.warn('Problem while converting stalled and revival times to integers')
+      self.log.warn('Problem while converting stalled time (hours) to seconds')
       self.log.warn(str(x))
       return S_OK('Problem while converting configuration times')
 
@@ -63,14 +58,9 @@ class StalledJobAgent(Agent):
     if not result['OK']:
       self.log.info(result['Message'])
 
-    #Note, jobs will be revived automatically during the heartbeat signal phase
+    #Note, jobs will be revived automatically during the heartbeat signal phase and
     #subsequent status changes will result in jobs not being selected by the
-    #stalled job agent. 
-    
-    #result = self.__reviveJobs(revivalTime)
-    #if not result['OK']:
-    #  self.log.warn(result)
-    #
+    #stalled job agent.
 
     return S_OK('Stalled Job Agent cycle complete')
 
@@ -89,16 +79,16 @@ class StalledJobAgent(Agent):
       self.log.info('%s Running jobs will be checked for being stalled' % (len(jobs)))
       jobs.sort()
 #      jobs = jobs[:10] #for debugging
-      for job in jobs:        
+      for job in jobs:
         result = self.__getStalledJob(job,stalledTime)
         if result['OK']:
           self.log.verbose('Updating status to Stalled for job %s' %(job))
           self.__updateJobStatus(job,'Stalled')
           stalledCounter+=1
         else:
-          self.log.verbose(result['Message']) 
-          runningCounter+=1 
-            
+          self.log.verbose(result['Message'])
+          runningCounter+=1
+
     self.log.info('Total jobs: %s, Stalled job count: %s, Running job count: %s' %(len(jobs),stalledCounter,runningCounter))
     return S_OK()
 
@@ -110,24 +100,24 @@ class StalledJobAgent(Agent):
     result = self.__getLatestUpdateTime(job)
     if not result['OK']:
       return result
-      
+
     currentTime = time.mktime(time.gmtime())
     lastUpdate = result['Value']
 
     elapsedTime = currentTime-lastUpdate
-    self.log.verbose('(CurrentTime-LastUpdate) = %s secs' %(elapsedTime))    
+    self.log.verbose('(CurrentTime-LastUpdate) = %s secs' %(elapsedTime))
     if elapsedTime > stalledTime:
       self.log.info('Job %s is identified as stalled with last update > %s secs ago' %(job,elapsedTime))
       return S_OK('Stalled')
- 
-    return S_ERROR('Job %s is running and will be ignored' %job)        
+
+    return S_ERROR('Job %s is running and will be ignored' %job)
 
  #############################################################################
   def __getLatestUpdateTime(self,job):
     """Returns the most recent of HeartBeatTime and LastUpdateTime
     """
     result = self.jobDB.getJobAttributes(job,['HeartBeatTime','LastUpdateTime'])
-    if not result['OK'] or not result['Value']: 
+    if not result['OK'] or not result['Value']:
       self.log.warn(result)
       return S_ERROR('Could not get attributes for job %s' %job)
 
@@ -140,32 +130,16 @@ class StalledJobAgent(Agent):
 
     if not result['Value']['LastUpdateTime'] or result['Value']['LastUpdateTime']=='None':
       self.log.verbose('LastUpdateTime is null for job %s' %job)
-    else:    
+    else:
       lastUpdate = toEpoch(fromString(result['Value']['LastUpdateTime']))
       if latestUpdate < lastUpdate:
         latestUpdate = lastUpdate
-       
+
     if not latestUpdate:
       return S_ERROR('LastUpdate and HeartBeat times are null for job %s' %job)
     else:
       self.log.verbose('Latest update time from epoch for job %s is %s' %(job,latestUpdate))
-      return S_OK(latestUpdate)         
-
- #############################################################################
-  def __reviveJobs(self,revivalTime):
-    """ Revives jobs inadvertently marked as stalled.
-    """
-    result = self.jobDB.selectJobs({'Status':'Stalled'})
-    if not result['OK']:
-      self.log.warn(result)
-    else:
-      jobs = result['Value']
-      self.log.info('%s jobs will be checked for revival' % (len(jobs)))
-      for job in jobs:
-        self.log.verbose('Updating status to Running for job %s' %(job))
-        self.__updateJobStatus(job,'Running')
-
-    return result
+      return S_OK(latestUpdate)
 
   #############################################################################
   def __updateJobStatus(self,job,status,minorstatus=None):
@@ -190,7 +164,7 @@ class StalledJobAgent(Agent):
       result = self.jobDB.getJobAttributes(job, ['MinorStatus'] )
       if result['OK']:
         minorstatus = result['Value']['MinorStatus']
-    
+
     if self.enable:
       logStatus=status
       result = self.logDB.addLoggingRecord(job,status=logStatus,minor=minorstatus,source='StalledJobAgent')
