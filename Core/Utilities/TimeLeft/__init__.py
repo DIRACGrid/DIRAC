8eb322507223aa5925e392a2422bb6fdc7e181d5
Stuart K. Paterson <stuart.paterson@cern.ch>
2008-03-10 09:31:35 +0000

    DIRAC.Core.Utilities.TimeLeft package.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@7183 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Utilities/TimeLeft/__init__.py
@@ -0,0 +1,9 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/TimeLeft/__init__.py,v 1.1 2008/03/10 09:31:35 paterson Exp $
+############################################################
+
+"""
+   DIRAC.Core.Utilities.TimeLeft package
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2008/03/10 09:31:35 paterson Exp $"
