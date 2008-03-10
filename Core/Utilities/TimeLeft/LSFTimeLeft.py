e11d21dd9d5cee2d684669c47c034865b0a1983e
Stuart K. Paterson <stuart.paterson@cern.ch>
2008-03-10 09:32:02 +0000

    LSF TimeLeft plugin.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@7184 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Utilities/TimeLeft/LSFTimeLeft.py
@@ -0,0 +1,115 @@
+########################################################################
+# $Id: LSFTimeLeft.py,v 1.1 2008/03/10 09:32:02 paterson Exp $
+########################################################################
+
+""" The LSF TimeLeft utility interrogates the LSF batch system for the
+    current CPU and Wallclock consumed, as well as their limits.
+"""
+
+from DIRAC import gLogger, gConfig, S_OK, S_ERROR
+from DIRAC.Core.Utilities.Subprocess import shellCall
+
+__RCSID__ = "$Id: LSFTimeLeft.py,v 1.1 2008/03/10 09:32:02 paterson Exp $"
+
+import os, string, re, time
+
+class LSFTimeLeft:
+
+  #############################################################################
+  def __init__(self):
+    """ Standard constructor
+    """
+    self.log = gLogger.getSubLogger('LSFTimeLeft')
+    self.jobID = None
+    if os.environ.has_key('LSB_JOBID'):
+      self.jobID = os.environ['LSB_JOBID']
+    self.queue = None
+    if os.environ.has_key('LSB_QUEUE'):
+      self.queue = os.environ['LSB_QUEUE']
+    self.bin = None
+    if os.environ.has_key('LSF_BINDIR'):
+      self.bin = os.environ['LSF_BINDIR']
+    self.cpuLimit = None
+    self.wallClockLimit = None
+    self.year = time.strftime('%Y',time.gmtime())
+    self.log.verbose('LSB_JOBID=%s, LSB_QUEUE=%s, LSF_BINDIR=%s' %(self.jobID,self.queue,self.bin))
+
+  #############################################################################
+  def getResourceUsage(self):
+    """Returns a dictionary containing CPUConsumed, CPULimit, WallClockConsumed
+       and WallClockLimit for current slot.  All values returned in seconds.
+    """
+    if not self.bin:
+      return S_ERROR('Could not determine bin directory for LSF')
+
+    cmd = '%s/bjobs -l %s' %(self.bin,self.jobid)
+    result = self.__runCommand(cmd)
+    if not result['OK']:
+      return result
+
+    cpu = None
+    cpuLimit = None
+    wallClock = None
+    wallClockLimit = None
+
+    lines = result['Value'].split('\n')
+    for line in lines:
+      info = line.split()
+      if re.search('.*The CPU time used is.*',line):
+        if len(info)>=5:
+          cpu = float(info[5])
+        else:
+          self.log.warn('Poblem parsing "%s" for CPU consumed' %line)
+      if re.search('.*Started on.*',line):
+        if len(info)>=1:
+          timeStr = '%s %s' %(info[0],self.year)
+          timeTup=time.strptime(timeStr, '%a %b %d %H:%M:%S %Y')
+          self.wallClock=time.mktime(timeTup)
+          if time.daylight==1:
+            self.wallClock += 3600
+        else:
+          self.log.warn('Poblem parsing "%s" for elapsed wall clock time' %line)
+      if re.search('.*CPULIMIT.*',line):
+        if len(info)>=1:
+          cpuLimit = float(info[0])
+        else:
+          self.log.warn('Problem parsing "%s" for CPU limit' %line)
+      if re.search('.*RUNLIMIT.*',line):
+        if len(info)>=1:
+          wallClockLimit = float(info[0])
+        else:
+          self.log.warn('Problem parsing "%s" for wall clock limit' %line)
+
+    consumed = {'CPU':cpu,'CPULimit':cpuLimit,'WallClock':wallClock,'WallClockLimit':wallClockLimit}
+    self.log.debug(consumed)
+    failed = False
+    for k,v in consumed.items():
+      if not v:
+        failed = True
+        self.log.warn('Could not determine %s' %k)
+
+    if not failed:
+      return S_OK(consumed)
+    else:
+      self.log.info('Could not determine some parameters, this is the stdout from the batch system call\n%s' %(result['Value']))
+      return S_ERROR('Could not determine some parameters')
+
+  #############################################################################
+  def __runCommand(self,cmd):
+    """Wrapper around shellCall to return S_OK(stdout) or S_ERROR(message)
+    """
+    result = shellCall(0,cmd)
+    if not result['OK']:
+      return result
+    status = result['Value'][0]
+    stdout = result['Value'][1]
+    stderr = result['Value'][2]
+
+    if status:
+      self.log.warn('Status %s while executing %s' %(status,cmd))
+      self.log.warn(stderr)
+      return S_ERROR(stdout)
+    else:
+      return S_OK(stdout)
+
+  #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
