1b93f09d0e7ef9a769bbec95dee9da3c61cca28b
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2008-03-10 14:57:05 +0000

    proper path to the LcgFileCatalogCombinedClient; few fixes
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@7200 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Service/LcgFileCatalogProxyHandler.py
+++ DataManagementSystem/Service/LcgFileCatalogProxyHandler.py
@@ -4,18 +4,20 @@ This is a service which represents a DISET proxy to the LCG File Catalog
 from types import *
 from DIRAC.Core.DISET.RequestHandler import RequestHandler
 from DIRAC import gLogger, gConfig, S_OK, S_ERROR
-from DIRAC.DataManagementSystem.Client.LcgFileCatalogCombinedClient import LcgFileCatalogCombinedClient
+from DIRAC.DataManagementSystem.Client.Catalog.LcgFileCatalogCombinedClient import LcgFileCatalogCombinedClient
+#from DIRAC.DataManagementSystem.Client.LcgFileCatalogClient import LcgFileCatalogClient
 
 lfcCombinedClient = False
 
 def initializeLcgFileCatalogProxyHandler(serviceInfo):
   global lfcCombinedClient
+  #lfcCombinedClient = LcgFileCatalogClient(host='lfc-lhcb.cern.ch',infosys='lcg-bdii.cern.ch:2170')
   lfcCombinedClient = LcgFileCatalogCombinedClient()
   return S_OK()
 
 class LcgFileCatalogProxyHandler(RequestHandler):
 
-  types_callProxyMethod = [StringType,ListType,DictionaryType]
+  types_callProxyMethod = [StringType,TupleType,DictionaryType]
   def export_callProxyMethod(self, name, args, kargs):
     """ A generic method to call methods of the LCG FileCatalog client
     """
@@ -25,7 +27,7 @@ class LcgFileCatalogProxyHandler(RequestHandler):
       error = "Exception: no method named "+name
       return S_ERROR(error)
 
-    kargs["DN"] = self.sDN
+    #kargs["DN"] = self.sDN
     try:
       result = method(*args,**kargs)
       return result
