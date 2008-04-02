c92af8d0d4cab49c505e9ffdfb538dfbe69a72ab
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2008-04-02 09:28:35 +0000

    First import of the Workflow based request class
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@7365 4525493e-7705-40b1-a816-d608a930855b

--- RequestManagementSystem/Client/Request.py
+++ RequestManagementSystem/Client/Request.py
@@ -1,191 +1,83 @@
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/RequestManagementSystem/Client/Request.py,v 1.8 2008/04/02 09:28:35 atsareg Exp $
 """
-  This is the client implementation for the RequestDB using the DISET framework.
+   Request Base Class
+
+   This class provides generic Request submission functionality suitable for any VO.
+
+   Helper functions are documented with example usage for the DIRAC API.
+
 """
 
-from types import *
-from DIRAC import gLogger, gConfig, S_OK, S_ERROR
-from DIRAC.Core.DISET.RPCClient import RPCClient
-from DIRAC.Core.Utilities.List import randomize, fromChar
-from DIRAC.ConfigurationSystem.Client import PathFinder
+__RCSID__ = "$Id: Request.py,v 1.8 2008/04/02 09:28:35 atsareg Exp $"
 
-class RequestClient:
+from DIRAC.Core.Workflow.Utility                    import *
+from DIRAC.RequestManagementSystem.Client.RequestBase import RequestBase
 
-  def __init__(self,useCertificates = False):
-    """ Constructor of the RequestClient class
+class Request(RequestBase):
+
+  #############################################################################
+
+  def __init__(self,script=None):
+    """Instantiates the Workflow object and some default parameters.
     """
-    voBoxUrls = fromChar( PathFinder.getServiceURL( "RequestManagement/voBoxURLs" ) )
-    self.voBoxUrls = [] 
-    if voBoxUrls:
-      self.voBoxUrls = randomize(voBoxUrls)
-      
-    self.local = False
-    local = PathFinder.getServiceURL("RequestManagement/localURL")
-    if local:
-      self.local = local
-      if local in self.voBoxUrls:   
-        self.voBoxUrls.remove(self.local)
 
-    self.central = False
-    central = PathFinder.getServiceURL("RequestManagement/centralURL")
-    if central:
-      self.central = central
+    RequestBase.__init__(self)
 
-  ########################################################################
-  #
-  # These are the methods operating on existing requests and have fixed URLs
-  #
+    self.moduleType = None
 
-  def updateRequest(self,requestName,requestString,url):
-    """ Update the request at the supplied url
+    if not script:
+      self.workflow = Workflow()
+    else:
+      self.workflow = Workflow(script)
+      for pname in self.genParametersNames:
+        value = self.workflow.findParameter(pname).getValue()
+        if value:
+          self.genParameters[pname] = value
+
+  #############################################################################
+  def define(self,moduleType,parameterDict):
+    """ Specify the module type of the request and setup the workflow
     """
-    try:
-      gLogger.info("RequestDBClient.updateRequest: Attempting to update %s at %s." % (requestName,url))
-      requestRPCClient = RPCClient(url)
-      res = requestRPCClient.updateRequest(requestName,requestString)
-      return res
-    except Exception,x:
-      errStr = "Request.updateRequest: Exception while updating request."
-      gLogger.exception(errStr,"%s %s" % (requestName,str(x)))
-      return S_ERROR(errStr)
 
-  def deleteRequest(self,requestName,url):
-    """ Delete the request at the supplied url
+    for p,value in parameterDict.items():
+      if p in self.genParametersNames:
+        self.genParameters[p] = value
+
+    self.moduleType = moduleType
+    module = ModuleDefinition(moduleType+'Module')
+    for p,value in self.genParameters.items():
+      module.addParameter(Parameter(p,value,'string','','',True,False,'Request parameter description'))
+    for p,value in parameterDict.items():
+      module.addParameter(Parameter(p,value,'string','','',True,False,'Request parameter description'))
+
+    module.setDescription('Request Failover System item')
+    #Below should be changed to a more dynamic approach
+    body = 'from DIRAC.RequestManagementSystem.Client.'+moduleType
+    body += 'Module import '+moduleType+'Module\n'
+    module.setBody(body)
+
+    self.workflow = createSingleModuleWorkflow(module,moduleType)
+
+  #############################################################################
+  def dump(self):
+    """ Dump the request parameters
     """
-    try:
-      gLogger.info("RequestDBClient.deleteRequest: Attempting to delete %s at %s." % (requestName,url))
-      requestRPCClient = RPCClient(url)
-      res = requestRPCClient.deleteRequest(requestName)
-      return res
-    except Exception,x:
-      errStr = "Request.deleteRequest: Exception while deleting request."
-      gLogger.exception(errStr,"%s %s" % (requestName,str(x)))
-      return S_ERROR(errStr)
 
-  def setRequestStatus(self,requestName,requestStatus,url):
-    """ Set the status of a request
+    for p in self.workflow.parameters:
+      print (p.getName()+':').ljust(26),p.getValue()
+
+  #############################################################################
+  def execute(self):
+    """Executes the request locally.
     """
-    try:
-      gLogger.info("RequestDBClient.setRequestStatus: Attempting to set %s to %s." % (requestName,requestStatus))
-      requestRPCClient = RPCClient(url)
-      res = requestRPCClient.setRequestStatus(requestName,requestStatus)
-      return res
-    except Exception,x:
-      errStr = "Request.setRequestStatus: Exception while setting request status."
-      gLogger.exception(errStr,"%s %s" % (requestName,str(x)))
-      return S_ERROR(errStr)
+    self.workflow.execute()
 
-  ##############################################################################
-  #
-  # These are the methods which require URL manipulation
-  #
 
-  def setRequest(self,requestName,requestString,url=''):
-    """ Set request. URL can be supplied if not a all VOBOXes will be tried in random order.
+  #############################################################################
+  def toXML(self):
+    """Internal Function.
+
+       Creates an XML representation of itself as a Job,
+       wraps around workflow toXML().
     """
-    try:
-      if not url:
-        url = self.local
-      urls = [url]
-      for url in urls:
-        requestRPCClient = RPCClient(url)
-        res = requestRPCClient.setRequest(requestName,requestString)
-        if res['OK']:
-          gLogger.info("Succeded setting request  %s at %s" % (requestName,url))
-          res["Server"] = url
-          return res
-        else:
-          errKey = "Failed setting request at %s" % url
-          errExpl = " : for %s because: %s" % (requestName,res['Message'])
-          gLogger.error(errKey,errExpl)
-      errKey = "Completely failed setting request"
-      errExpl = " : %s\n%s" % (requestName,requestString)
-      gLogger.fatal(errKey,errExpl)
-      return S_ERROR(errKey)
-    except Exception,x:
-      errKey = "Completely failed setting request"
-      errExpl = " : for %s with exception %s" % (requestName,str(x))
-      gLogger.exception(errKey,errExpl)
-      return S_ERROR(errKey)
-
-  def getRequest(self,requestType):
-    """ Get request from RequestDB.
-        First try the local repository then if none available or error try random repository
-    """
-    try:
-      url = self.local
-      urls = [url]
-      for url in urls:
-        gLogger.info("RequestDBClient.getRequest: Attempting to get request.", "%s %s" % (url,requestType))
-        requestRPCClient = RPCClient(url)
-        res = requestRPCClient.getRequest(requestType)
-        if res['OK']:
-          if res['Value']:
-            gLogger.info("Got '%s' request from RequestDB (%s)" % (requestType,url))
-            res['Value']['Server'] = url
-            return res
-          else:
-            gLogger.info("Found no '%s' requests on RequestDB (%s)" % (requestType,url))
-        else:
-          errKey = "Failed getting request from %s" % url
-          errExpl = " : %s : %s" % (requestType,res['Message'])
-          gLogger.error(errKey,errExpl)
-      return res
-    except Exception,x:
-      errKey = "Failed to get request"
-      errExpl = " : %s" %str(x)
-      gLogger.exception(errKey,errExpl)
-      return S_ERROR(errKey+errExpl)
-
-
-  def serveRequest(self,requestType):
-    """ Get a request from RequestDB.
-    """
-    try:
-      url = self.local
-      urls = [url]
-      for url in urls:
-        gLogger.info("RequestDBClient.serveRequest: Attempting to obtain request.", "%s %s" % (url,requestType))
-        requestRPCClient = RPCClient(url)
-        res = requestRPCClient.serveRequest(requestType)
-        if res['OK']:
-          if res['Value']:
-            gLogger.info("Got '%s' request from RequestDB (%s)" % (requestType,url))
-            res['Value']['Server'] = url
-            return res
-          else:
-            gLogger.info("Found no '%s' requests on RequestDB (%s)" % (requestType,url))
-        else:
-          errKey = "Failed getting request from %s" % url
-          errExpl = " : %s : %s" % (requestType,res['Message'])
-          gLogger.error(errKey,errExpl)
-      return res
-    except Exception,x:
-      errKey = "Failed to get request"
-      errExpl = " : %s" %str(x)
-      gLogger.exception(errKey,errExpl)
-      return S_ERROR(errKey+errExpl)
-
-  def getDBSummary(self,url=''):
-    """ Get the summary of requests in the RequestDBs. If a URL is not supplied will get status for all.
-    """
-    try:
-      url = self.local
-      urls = [url]
-      urlDict = {}
-      for url in urls:
-        requestRPCClient = RPCClient(url)
-        urlDict[url] = {}
-        result = requestRPCClient.getDBSummary()
-        if result['OK']:
-          gLogger.info("Succeded getting request summary at %s" % url)
-          urlDict[url] = result['Value']
-        else:
-          errKey = "Failed getting request summary"
-          errExpl = " : at %s because %s" % (url,result['Message'])
-          gLogger.error(errKey,errExpl)
-      return S_OK(urlDict)
-    except Exception,x:
-      errKey = "Failed getting request summary"
-      errExpl = " : with exception %s" % str(x)
-      gLogger.exception(errKey,errExpl)
-      return S_ERROR(errKey+errExpl)
+    return self.workflow.toXML()
