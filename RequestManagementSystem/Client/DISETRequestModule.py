63fa8bb85be4c1ed1e4d33efc9c0f9f417c46574
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2008-04-02 19:50:15 +0000

    First import of the DISET request module in the Workflow based request framework
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@7380 4525493e-7705-40b1-a816-d608a930855b

---
+++ RequestManagementSystem/Client/DISETRequestModule.py
@@ -0,0 +1,47 @@
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/RequestManagementSystem/Client/DISETRequestModule.py,v 1.1 2008/04/02 19:50:15 atsareg Exp $
+
+""" DISET Request Module is used together with the Workflow based request
+    to encapsulate generic DISET service calls
+"""
+
+__RCSID__ = "$Id: DISETRequestModule.py,v 1.1 2008/04/02 19:50:15 atsareg Exp $"
+
+from DIRAC.Core.DISET.RPCClient      import RPCClient
+from DIRAC                           import gConfig, gLogger, S_OK, S_ERROR
+
+import os,sys,re
+
+class DISETRequestModule(object):
+
+  def __init__(self):
+    # constructor code
+    self.version = __RCSID__
+    self.log = gLogger.getSubLogger('DISETRequest')
+    self.portalEnv = False
+
+  def execute(self):
+    # main execution function
+    self.log.verbose('---------------------------------------------------------------')
+    self.log.info('Executing request %s' % self.RequestName)
+    self.log.verbose('RequestModule version: %s' %(self.version))
+    self.log.verbose('Request Type: %s' %(self.RequestType))
+    self.log.verbose( '---------------------------------------------------------------')
+
+    if self.portalEnv:
+      client = RPCClient(self.Service,useCertificates=True,delegatedDN=self.OwnerDN,
+                        delegatedGroup=self.OwnerGroup)
+
+    else:
+      client = RPCClient(self.Service)
+
+    timeStamp = self.CreationTime
+    method = self.Method
+    arguments = []
+    if self.Arguments != "None":
+      arguments = self.Arguments.split(':::')
+
+    result = eval('client.'+method+'('+','.join(arguments)+')')
+    print result
+    return result
+
+
