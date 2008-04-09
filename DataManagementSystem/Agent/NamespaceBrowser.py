290e30929855aa3f6724581f93d9953fb97ea883
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-04-09 21:17:51 +0000

    changed not to reset to the beginning once completed.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@7483 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Agent/NamespaceBrowser.py
+++ DataManagementSystem/Agent/NamespaceBrowser.py
@@ -10,17 +10,20 @@ class NameSpaceBrowser:
     self.activeDir = self.activeDirs[0]
     self.baseDir = baseDir
 
+  def isActive(self):
+    if self.activeDirs:
+      return True
+    else:
+      return False
+
   def getBaseDir(self):
     return self.baseDir
 
   def getActiveDir(self):
+    random.shuffle(self.activeDirs)
+    self.activeDir = self.activeDirs[0]
     return self.activeDir
 
   def updateDirs(self,subDirs):
     self.activeDirs.extend(subDirs)
     self.activeDirs.remove(self.activeDir)
-    if len(self.activeDirs) > 0:
-      random.shuffle(self.activeDirs)
-    else:
-      self.activeDirs = [self.baseDir]
-    self.activeDir = self.activeDirs[0]
