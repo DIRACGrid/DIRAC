0056a8dd39c979a2cca7c3af955f7ae0706141e3
Ricardo Graciani <graciani@ecm.ub.es>
2008-04-17 07:04:15 +0000

    add scriptName to be used by scripts
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@7623 4525493e-7705-40b1-a816-d608a930855b

--- Core/Base/Script.py
+++ Core/Base/Script.py
@@ -1,5 +1,5 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Base/Script.py,v 1.7 2008/01/15 18:12:21 acasajus Exp $
-__RCSID__ = "$Id: Script.py,v 1.7 2008/01/15 18:12:21 acasajus Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Base/Script.py,v 1.8 2008/04/17 07:04:15 rgracian Exp $
+__RCSID__ = "$Id: Script.py,v 1.8 2008/04/17 07:04:15 rgracian Exp $"
 
 import sys
 import os.path
@@ -9,15 +9,17 @@ from DIRAC.MonitoringSystem.Client.MonitoringClient import gMonitor
 
 localCfg = LocalConfiguration()
 
-def parseCommandLine( scriptName = False, ignoreErrors = False, initializeMonitor = True ):
-  global localCfg
+scriptName = False
 
+def parseCommandLine( script = False, ignoreErrors = False, initializeMonitor = True ):
+  global localCfg, scriptName
+
+  scriptName = script
   if not scriptName:
     scriptName = os.path.basename( sys.argv[0] )
   scriptSection = localCfg.setConfigurationForScript( scriptName )
   localCfg.addMandatoryEntry( "/DIRAC/Setup" )
   resultDict = localCfg.loadUserData()
-
   if not ignoreErrors and not resultDict[ 'OK' ]:
     gLogger.error( "There were errors when loading configuration", resultDict[ 'Message' ] )
     sys.exit(1)
