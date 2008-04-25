9906b332c7b6639effcc0f2c529df09428af8f82
Ricardo Graciani <graciani@ecm.ub.es>
2008-04-25 09:03:27 +0000

    Utility to handle LD_LIBRARY_PATH and PATH
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@7789 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Utilities/ldLibraryPath.py
@@ -0,0 +1,53 @@
+########################################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/ldLibraryPath.py,v 1.1 2008/04/25 09:03:27 rgracian Exp $
+# File :   ldLibraryPath
+# Author : Ricardo Graciani
+########################################################################
+__RCSID__   = "$Id: ldLibraryPath.py,v 1.1 2008/04/25 09:03:27 rgracian Exp $"
+__VERSION__ = "$Revision: 1.1 $"
+"""
+  Utilities to handle issues with LD_LIBRARY_PATH
+"""
+from DIRAC import platformTuple
+import os
+
+def unify( path, newpath ):
+  # for Linux and MacOS link or files in pathList in a single directory path.
+  # For that we go a long pathList in reverse order and link all files
+  # from a path, the latest appearence of a file will take precedence
+  if not platformTuple[0] == 'Windows':
+    if os.path.exists( newpath ) and not os.path.isdir(newpath):
+      try:
+        os.remove( newpath )
+      except:
+        return path
+    else:
+      try:
+        os.makedirs( newpath )
+      except:
+        return path
+    pathList = path.split(':')
+    for dummy in pathList[:]:
+      ldDir = pathList.pop()
+      if not os.path.isdir( ldDir ):
+        continue
+      ldLibs = os.listdir( ldDir )
+      for f in ldLibs:
+        newF = os.path.join( newpath,f )
+        ldF  = os.path.join( ldDir, f)
+        # 1. Check if the file exist (broken links will return False)
+        if os.path.isfile( ldF ):
+          ldF = os.path.realpath( ldF )
+          # 2. Check if the link is present already
+          if os.path.exists( newF ):
+            # 3. Check is the point to the same file
+            if os.path.samefile( newF, ldF ):
+              continue
+            else:
+              os.remove( newF )
+          # 4. Create the link
+          os.symlink( ldF, newldF )
+    return newpath
+  else:
+    # Windows does nothing for the moment
+    return path
