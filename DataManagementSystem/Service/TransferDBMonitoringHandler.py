944e98aeeee8a98c0b9cfff8151ad19eb7184197
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-05-07 21:54:17 +0000

    added methods for monitoring the requests,subrequest and files tables.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@8893 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Service/TransferDBMonitoringHandler.py
+++ DataManagementSystem/Service/TransferDBMonitoringHandler.py
@@ -13,9 +13,10 @@ SUMMARY = ['Status','NumberOfFiles','PercentageComplete','TotalSize','SubmitTime
 
 
 RequestsColumns = ['RequestID','RequestName','JobID','OwnerDN','DIRACInstance','Status','CreationTime','SubmissionTime']
-SubRequestsColumns = ['RequestID','SubRequestID','RequestType','Status','Operation','SourceSE','TargetSE','Catalogue','SubmissionTime']
-FilesColums = ['SubRequestID','FileID','LFN','Size','PFN','GUID','Md5','Addler','Attempt','Status']
-
+SubRequestsColumns = ['RequestID','SubRequestID','RequestType','Status','Operation','SourceSE','TargetSE','Catalogue','SubmissionTime','LastUpdate']
+FilesColumns = ['SubRequestID','FileID','LFN','Size','PFN','GUID','Md5','Addler','Attempt','Status']
+DatasetColumns = ['SubRequestID','Dataset','Status']
+ 
 def initializeTransferDBMonitoringHandler(serviceInfo):
 
   global transferDB
@@ -82,6 +83,8 @@ class TransferDBMonitoringHandler(RequestHandler):
 ######################################################################################
 ######################################################################################
 
+
+
   types_getRequestPageSummaryWeb = [DictType, ListType, IntType, IntType]
   def export_getRequestPageSummaryWeb(self, selectDict, sortList, startItem, maxItems):
     """ Get the summary of the request information for a given page in the
@@ -110,7 +113,7 @@ class TransferDBMonitoringHandler(RequestHandler):
 
     iniRequest = startItem
     lastRequest = iniRequest + maxItems
-    if iniRequet >= nRequests:
+    if iniRequest >= nRequests:
       return S_ERROR('Item number out of range')
 
     if lastRequest > nRequests:
@@ -137,3 +140,118 @@ class TransferDBMonitoringHandler(RequestHandler):
     resultDict['ParameterNames'] = paramNames
     resultDict['Records'] = records
     return S_OK(resultDict)
+
+
+    
+  types_getSubRequestPageSummaryWeb = [DictType, ListType, IntType, IntType]
+  def export_getSubRequestPageSummaryWeb(self, selectDict, sortList, startItem, maxItems):
+    """ Get the summary of the request information for a given page in the
+        request monitor in a generic format
+    """
+    resultDict = {}
+    last_update = None
+    if selectDict.has_key('LastUpdate'):
+      last_update = selectDict['LastUpdate']
+      del selectDict['LastUpdate']
+    # Sorting instructions. Only one for the moment.
+    if sortList:
+      orderAttribute = sortList[0][0]+":"+sortList[0][1]
+    else:
+      orderAttribute = None
+    
+    result = transferDB.selectSubRequests(selectDict, orderAttribute=orderAttribute, newer=last_update)
+    if not result['OK']:
+      return S_ERROR('Failed to select jobs: '+result['Message'])
+
+    requestList = result['Value']
+    nRequests = len(requestList)
+    resultDict['TotalRecords'] = nRequests
+    if nRequests == 0:
+      return S_OK(resultDict) 
+        
+    iniRequest = startItem
+    lastRequest = iniRequest + maxItems
+    if iniRequest >= nRequests:
+      return S_ERROR('Item number out of range')
+
+    if lastRequest > nRequests:
+      lastRequests = nRequests
+
+    summaryRequestList = requestList[iniRequest:lastRequest]
+    result = transferDB.getAttributesForSubRequestList(summaryRequestList,SubRequestsColumns)
+    if not result['OK']:
+      return S_ERROR('Failed to get request summary: '+result['Message'])
+    
+    summaryDict = result['Value']
+
+    # prepare the standard structure now
+    key = summaryDict.keys()[0]
+    paramNames = summaryDict[key].keys()
+
+    records = []
+    for requestID, requestDict in summaryDict.items():  
+      rParList = []
+      for pname in paramNames:
+        rParList.append(requestDict[pname])
+      records.append(rParList)
+
+    resultDict['ParameterNames'] = paramNames
+    resultDict['Records'] = records
+    return S_OK(resultDict)
+
+
+  types_getFilesPageSummaryWeb = [DictType, ListType, IntType, IntType]
+  def export_getFilesPageSummaryWeb(self, selectDict, sortList, startItem, maxItems):
+    """ Get the summary of the request information for a given page in the
+        request monitor in a generic format
+    """
+    resultDict = {}
+    last_update = None
+    if selectDict.has_key('LastUpdate'):
+      last_update = selectDict['LastUpdate']
+      del selectDict['LastUpdate']
+    # Sorting instructions. Only one for the moment.
+    if sortList:
+      orderAttribute = sortList[0][0]+":"+sortList[0][1]
+    else:
+      orderAttribute = None
+    
+    result = transferDB.selectFiles(selectDict, orderAttribute=orderAttribute, newer=last_update)
+    if not result['OK']:
+      return S_ERROR('Failed to select jobs: '+result['Message'])
+
+    requestList = result['Value']
+    nRequests = len(requestList)
+    resultDict['TotalRecords'] = nRequests
+    if nRequests == 0:
+      return S_OK(resultDict) 
+        
+    iniRequest = startItem
+    lastRequest = iniRequest + maxItems
+    if iniRequest >= nRequests:
+      return S_ERROR('Item number out of range')
+
+    if lastRequest > nRequests:
+      lastRequests = nRequests
+    summaryRequestList = requestList[iniRequest:lastRequest]
+    result = transferDB.getAttributesForFilesList(summaryRequestList,FilesColumns)
+    if not result['OK']:
+      return S_ERROR('Failed to get request summary: '+result['Message'])
+    
+    summaryDict = result['Value']
+    
+    # prepare the standard structure now
+    key = summaryDict.keys()[0]   
+    paramNames = summaryDict[key].keys()
+    
+    records = []
+    for requestID, requestDict in summaryDict.items():
+      rParList = []
+      for pname in paramNames:
+        rParList.append(requestDict[pname])
+      records.append(rParList)
+      
+    resultDict['ParameterNames'] = paramNames
+    resultDict['Records'] = records
+    return S_OK(resultDict)
+
