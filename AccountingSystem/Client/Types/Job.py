d2806c06d97deb6eee75fe3d8862507859c27ccc
Adria Casajus <adria@ecm.ub.es>
2008-05-07 09:34:15 +0000

    Change of the accounting type
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@8854 4525493e-7705-40b1-a816-d608a930855b

--- AccountingSystem/Client/Types/Job.py
+++ AccountingSystem/Client/Types/Job.py
@@ -1,5 +1,5 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/AccountingSystem/Client/Types/Attic/WMSJob.py,v 1.8 2008/05/06 20:52:28 acasajus Exp $
-__RCSID__ = "$Id: WMSJob.py,v 1.8 2008/05/06 20:52:28 acasajus Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/AccountingSystem/Client/Types/Job.py,v 1.1 2008/05/07 09:34:15 acasajus Exp $
+__RCSID__ = "$Id: Job.py,v 1.1 2008/05/07 09:34:15 acasajus Exp $"
 
 from DIRAC.AccountingSystem.Client.Types.BaseAccountingType import BaseAccountingType
 
@@ -9,22 +9,27 @@ class WMSJob( BaseAccountingType ):
 
   def __init__( self ):
     BaseAccountingType.__init__( self )
-    self.definitionKeyFields = [ ( 'JobGroup', "VARCHAR(32)" ),
+    self.definitionKeyFields = [ ( 'User', 'VARCHAR(32)' ),
+                                 ( 'UserGroup', 'VARCHAR(32)' ),
+                                 ( 'JobGroup', "VARCHAR(64)" ),
                                  ( 'JobType', 'VARCHAR(32)' ),
                                  ( 'JobClass', 'VARCHAR(32)' ),
                                  ( 'Site', 'VARCHAR(32)' ),
-                                 ( 'ProcType', 'VARCHAR(32)' ),
+                                 ( 'ProccessingType', 'VARCHAR(32)' ),
                                  ( 'FinalMajorStatus', 'VARCHAR(32)' ),
                                  ( 'FinalMinorStatus', 'VARCHAR(64)' )
                                ]
     self.definitionAccountingFields = [ ( 'CPUTime', "INT" ),
                                         ( 'NormCPUTime', "INT" ),
                                         ( 'ExecTime', "INT" ),
-                                        ( 'InputData', 'INT' ),
-                                        ( 'OutputData', 'INT' ),
+                                        ( 'InputDataVolume', 'INT' ),
+                                        ( 'OutputDataVolume', 'INT' ),
+                                        ( 'InputDataFiles', 'INT' ),
+                                        ( 'OutputDataFiles', 'INT' ),
                                         ( 'DiskSpace', 'INT' ),
-                                        ( 'InputSandBox', 'INT' ),
-                                        ( 'OutputSandBox', 'INT' )
+                                        ( 'InputSandBoxVolume', 'INT' ),
+                                        ( 'OutputSandBoxVolume', 'INT' ),
+                                        ( 'ProcessedEvents', 'INT' )
                                       ]
     self.checkType()
     #Fill the site
