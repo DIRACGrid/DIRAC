f9f14c4c2281bf9f113a9d448b7466092ea7612c
Adria Casajus <adria@ecm.ub.es>
2008-05-08 19:09:06 +0000

    bugfix: Fixed conditions and moved to the base class
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@8917 4525493e-7705-40b1-a816-d608a930855b

--- AccountingSystem/private/Plotters/BasePlotter.py
+++ AccountingSystem/private/Plotters/BasePlotter.py
@@ -6,6 +6,9 @@ from DIRAC.AccountingSystem.private.PlotsCache import gPlotsCache
 class BasePlotter(DBUtils):
 
   requiredParams = ()
+  _PARAM_CHECK_FOR_NONE = 'checkNone'
+  _PARAM_CONVERT_TO_GRANULARITY = 'convertToGranularity'
+  _VALID_PARAM_CONVERT_TO_GRANULARITY = ( 'sum', 'average' )
 
   def __init__( self, db, setup ):
     DBUtils.__init__( self, db, setup )
@@ -39,3 +42,47 @@ class BasePlotter(DBUtils):
         viewList.append( attr.replace( "_plot", "" ) )
     viewList.sort()
     return viewList
+
+
+  def _getTypeData( self, startTime, endTime, selectFields, preCondDict, groupingFields, metadataDict ):
+    condDict = {}
+    #Check params
+    if not self._PARAM_CHECK_FOR_NONE in metadataDict:
+      metadataDict[ self._PARAM_CHECK_FOR_NONE ] = False
+    if not self._PARAM_CONVERT_TO_GRANULARITY in metadataDict:
+      metadataDict[ self._PARAM_CONVERT_TO_GRANULARITY ] = "sum"
+    elif metadataDict[ self._PARAM_CONVERT_TO_GRANULARITY ] not in self._VALID_PARAM_CONVERT_TO_GRANULARITY:
+      return S_ERROR( "%s field metadata is invalid" % self._PARAM_CONVERT_TO_GRANULARITY )
+    #Make safe selections
+    for keyword in self._typeKeyFields:
+      if keyword in preCondDict:
+        condDict[ keyword ] = preCondDict[ keyword ]
+    #Query!
+    retVal = self._retrieveBucketedData( self._typeName,
+                                          startTime,
+                                          endTime,
+                                          selectFields,
+                                          condDict,
+                                          [ 'startTime' ] + groupingFields,
+                                          [ 'startTime' ]
+                                          )
+    if not retVal[ 'OK' ]:
+      return retVal
+    dataDict = self._groupByField( 0, retVal[ 'Value' ] )
+    coarsestGranularity = self._getBucketLengthForTime( self._typeName, startTime )
+    #Transform!
+    for keyField in dataDict:
+      if metadataDict[ self._PARAM_CHECK_FOR_NONE ]:
+        dataDict[ keyField ] = self._convertNoneToZero( dataDict[ keyField ] )
+      if metadataDict[ self._PARAM_CONVERT_TO_GRANULARITY ] == "average":
+        dataDict[ keyField ] = self._averageToGranularity( coarsestGranularity, dataDict[ keyField ] )
+      if metadataDict[ self._PARAM_CONVERT_TO_GRANULARITY ] == "sum":
+        dataDict[ keyField ] = self._sumToGranularity( coarsestGranularity, dataDict[ keyField ] )
+    return S_OK( ( dataDict, coarsestGranularity ) )
+
+
+  def _getSQLStringForGrouping( self, groupingFields ):
+    if len( groupingFields ) == 1:
+      return "%s"
+    else:
+      return "CONCAT( %s, ' -> ', %s )"
--- AccountingSystem/private/Plotters/JobPlotter.py
+++ AccountingSystem/private/Plotters/JobPlotter.py
@@ -1,66 +1,52 @@
 
 from DIRAC import S_OK, S_ERROR, gLogger
-from DIRAC.AccountingSystem.Client.Types.DataOperation import DataOperation
+from DIRAC.AccountingSystem.Client.Types.Job import Job
 from DIRAC.AccountingSystem.private.Plotters.BasePlotter import BasePlotter
 from DIRAC.AccountingSystem.private.Plots import *
 from DIRAC.Core.Utilities import Time
 
 class JobPlotter(BasePlotter):
 
-  __typeName = "Job"
+  _typeName = "Job"
+  _typeKeyFields = [ dF[0] for dF in Job().definitionKeyFields ]
 
-  def _plotCPUEfficiency( self, startTime, endTime, argsDict, grouping, filename ):
-    return self.__generateCPUEfficiencyJobPlot(startTime, endTime, grouping, argsDict, filename)
+  def _plotCPUEfficiency( self, startTime, endTime, condDict, groupingFields, filename ):
+    selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, SUM(%s)/SUM(%s)",
+                     groupingFields + [ 'startTime', 'bucketLength',
+                                    'CPUTime', 'ExecTime'
+                                   ]
+                   )
 
-  def __generateCPUEfficiencyJobPlot( self, startTime, endTime, keyNameList, argsDict, filename ):
-    retVal = self.__getCPUEfficiencyJob( startTime, endTime, keyNameList, argsDict )
+    retVal = self._getTypeData( startTime,
+                                endTime,
+                                selectFields,
+                                condDict,
+                                groupingFields,
+                                { 'checkNone' : True, 'convertToGranularity' : 'average' } )
     if not retVal[ 'OK' ]:
       return retVal
     dataDict, granularity = retVal[ 'Value' ]
+    startTime = startTime - startTime % granularity
     self.stripDataField( dataDict, 0 )
     gLogger.info( "Generating plot", "%s with granularity of %s" % ( filename, granularity ) )
-    metadata = { 'title' : 'Job CPU efficiency by %s' % " -> ".join( keyNameList ) ,
+    metadata = { 'title' : 'Job CPU efficiency by %s' % " -> ".join( groupingFields ) ,
                  'starttime' : startTime,
                  'endtime' : endTime,
                  'span' : granularity }
     return generateQualityPlot( filename, dataDict, metadata )
 
-  def __getCPUEfficiencyJob( self, startTime, endTime, keyNameList, argsDict ):
-    condDict = {}
-    for keyword in keyNameList:
-      if keyword in argsDict:
-        condDict[ keyword ] = argsDict[ keyword ]
-    if len( keyNameList ) == 1:
-      keySQLString = "%s"
-    else:
-      keySQLString = "CONCAT( %s, ' -> ', %s )"
-    selectFields = ( keySQLString + ", %s, %s, SUM(%s)/SUM(%s)",
-                     keyNameList + [ 'startTime', 'bucketLength',
-                                    'CPUTime', 'ExecTime'
+  def _plotCPUUsed( self, startTime, endTime, condDict, groupingFields, filename ):
+    selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, %s",
+                     groupingFields + [ 'startTime', 'bucketLength',
+                                    'CPUTime'
                                    ]
                    )
-    retVal = self._retrieveBucketedData( self.__typeName,
-                                          startTime,
-                                          endTime,
-                                          selectFields,
-                                          condDict,
-                                          [ 'startTime' ] + keyNameList,
-                                          [ 'startTime' ]
-                                          )
-    if not retVal[ 'OK' ]:
-      return retVal
-    dataDict = self._groupByField( 0, retVal[ 'Value' ] )
-    coarsestGranularity = self._getBucketLengthForTime( self.__typeName, startTime )
-    for keyField in dataDict:
-      dataDict[ keyField ] = self._convertNoneToZero( dataDict[ keyField ] )
-      dataDict[ keyField ] = self._averageToGranularity( coarsestGranularity, dataDict[ keyField ] )
-    return S_OK( ( dataDict, coarsestGranularity ) )
-
-  def _plotCPUUsed( self, startTime, endTime, argsDict, grouping, filename ):
-    return self.__generateJobCPUUsedPlot(startTime, endTime, grouping, argsDict, filename)
-
-  def __generateJobCPUUsedPlot( self, startTime, endTime, keyNameList, argsDict, filename ):
-    retVal = self.__getJobCPUUsedData( startTime, endTime, keyNameList, argsDict )
+    retVal = self._getTypeData( startTime,
+                                endTime,
+                                selectFields,
+                                condDict,
+                                groupingFields,
+                                {} )
     if not retVal[ 'OK' ]:
       return retVal
     dataDict, granularity = retVal[ 'Value' ]
@@ -68,7 +54,7 @@ class JobPlotter(BasePlotter):
     self.stripDataField( dataDict, 0 )
     dataDict = self._fillWithZero( granularity, startTime, endTime, dataDict )
     gLogger.info( "Generating plot", "%s with granularity of %s" % ( filename, granularity ) )
-    metadata = { 'title' : 'CPU used by %s' % " -> ".join( keyNameList ) ,
+    metadata = { 'title' : 'CPU used by %s' % " -> ".join( groupingFields ) ,
                  'starttime' : startTime,
                  'endtime' : endTime,
                  'span' : granularity,
@@ -76,60 +62,18 @@ class JobPlotter(BasePlotter):
                  'is_cumulative' : False }
     return generateCumulativePlot( filename, dataDict, metadata )
 
-  def __getJobCPUUsedData( self, startTime, endTime, keyNameList, argsDict ):
-    condDict = {}
-    for keyword in keyNameList:
-      if keyword in argsDict:
-        condDict[ keyword ] = argsDict[ keyword ]
-    if len( keyNameList ) == 1:
-      keySQLString = "%s"
-    else:
-      keySQLString = "CONCAT( %s, ' -> ', %s )"
-    selectFields = ( keySQLString + ", %s, %s, %s",
-                     keyNameList + [ 'startTime', 'bucketLength',
+  def _plotCPUUsage( self, startTime, endTime, condDict, groupingFields, filename ):
+    selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, %s",
+                     groupingFields + [ 'startTime', 'bucketLength',
                                     'CPUTime'
                                    ]
                    )
-    retVal = self._retrieveBucketedData( self.__typeName,
-                                          startTime,
-                                          endTime,
-                                          selectFields,
-                                          condDict,
-                                          [ 'startTime' ] + keyNameList,
-                                          [ 'startTime' ]
-                                          )
-    if not retVal[ 'OK' ]:
-      return retVal
-    dataDict = self._groupByField( 0, retVal[ 'Value' ] )
-    coarsestGranularity = self._getBucketLengthForTime( self.__typeName, startTime )
-    for keyField in dataDict:
-      dataDict[ keyField ] = self._sumToGranularity( coarsestGranularity, dataDict[ keyField ] )
-    return S_OK( ( dataDict, coarsestGranularity ) )
-
-  def _plotCPUUsage( self, startTime, endTime, argsDict, grouping, filename ):
-    return self.__generateCPUUsageJobPlot(startTime, endTime, grouping, argsDict, filename)
-
-  def __generateCPUUsageJobPlot( self, startTime, endTime, keyNameList, argsDict, filename ):
-    retVal = self.__getJobCPUUsedData( startTime, endTime, keyNameList, argsDict )
-    if not retVal[ 'OK' ]:
-      return retVal
-    dataDict, granularity = retVal[ 'Value' ]
-    self.stripDataField( dataDict, 0 )
-    dataDict = self._fillWithZero( granularity, startTime, endTime, dataDict )
-    gLogger.info( "Generating plot", "%s with granularity of %s" % ( filename, granularity ) )
-    metadata = { 'title' : 'CPU usage by %s' % " -> ".join( keyNameList ) ,
-                 'ylabel' : 'secs',
-                 'starttime' : startTime,
-                 'endtime' : endTime,
-                 'span' : granularity }
-    return generateTimedStackedBarPlot( filename, dataDict, metadata )
-
-
-  def _plotNumberOfJobs( self, startTime, endTime, argsDict, grouping, filename ):
-    return self.__generateJobNumberJobsPlot(startTime, endTime, grouping, argsDict, filename)
-
-  def __generateJobNumberJobsPlot( self, startTime, endTime, keyNameList, argsDict, filename ):
-    retVal = self.__getJobNumJobsData( startTime, endTime, keyNameList, argsDict )
+    retVal = self._getTypeData( startTime,
+                                endTime,
+                                selectFields,
+                                condDict,
+                                groupingFields,
+                                {} )
     if not retVal[ 'OK' ]:
       return retVal
     dataDict, granularity = retVal[ 'Value' ]
@@ -137,40 +81,36 @@ class JobPlotter(BasePlotter):
     self.stripDataField( dataDict, 0 )
     dataDict = self._fillWithZero( granularity, startTime, endTime, dataDict )
     gLogger.info( "Generating plot", "%s with granularity of %s" % ( filename, granularity ) )
-    metadata = { 'title' : 'Jobs by %s' % " -> ".join( keyNameList ) ,
+    metadata = { 'title' : 'CPU usage by %s' % " -> ".join( groupingFields ) ,
+                 'starttime' : startTime,
+                 'endtime' : endTime,
+                 'span' : granularity,
+                 'ylabel' : "secs" }
+    return generateTimedStackedBarPlot( filename, dataDict, metadata )
+
+  def _plotNumberOfJobs( self, startTime, endTime, condDict, groupingFields, filename ):
+    selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, %s",
+                     groupingFields + [ 'startTime', 'bucketLength',
+                                    'entriesInBucket'
+                                   ]
+                   )
+    retVal = self._getTypeData( startTime,
+                                endTime,
+                                selectFields,
+                                condDict,
+                                groupingFields,
+                                {} )
+    if not retVal[ 'OK' ]:
+      return retVal
+    dataDict, granularity = retVal[ 'Value' ]
+    startTime = startTime - startTime % granularity
+    self.stripDataField( dataDict, 0 )
+    dataDict = self._fillWithZero( granularity, startTime, endTime, dataDict )
+    gLogger.info( "Generating plot", "%s with granularity of %s" % ( filename, granularity ) )
+    metadata = { 'title' : 'Jobs by %s' % " -> ".join( groupingFields ) ,
                  'starttime' : startTime,
                  'endtime' : endTime,
                  'span' : granularity,
                  'ylabel' : "jobs",
                  'is_cumulative' : False }
     return generateCumulativePlot( filename, dataDict, metadata )
-
-  def __getJobNumJobsData( self, startTime, endTime, keyNameList, argsDict ):
-    condDict = {}
-    for keyword in keyNameList:
-      if keyword in argsDict:
-        condDict[ keyword ] = argsDict[ keyword ]
-    if len( keyNameList ) == 1:
-      keySQLString = "%s"
-    else:
-      keySQLString = "CONCAT( %s, ' -> ', %s )"
-    selectFields = ( keySQLString + ", %s, %s, %s",
-                     keyNameList + [ 'startTime', 'bucketLength',
-                                    'entriesInBucket'
-                                   ]
-                   )
-    retVal = self._retrieveBucketedData( self.__typeName,
-                                          startTime,
-                                          endTime,
-                                          selectFields,
-                                          condDict,
-                                          [ 'startTime' ] + keyNameList,
-                                          [ 'startTime' ]
-                                          )
-    if not retVal[ 'OK' ]:
-      return retVal
-    dataDict = self._groupByField( 0, retVal[ 'Value' ] )
-    coarsestGranularity = self._getBucketLengthForTime( self.__typeName, startTime )
-    for keyField in dataDict:
-      dataDict[ keyField ] = self._sumToGranularity( coarsestGranularity, dataDict[ keyField ] )
-    return S_OK( ( dataDict, coarsestGranularity ) )
