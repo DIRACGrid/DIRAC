6d4ac166d465b3387d5c87807b2c0d5d6f01bae0
Adria Casajus <adria@ecm.ub.es>
2008-05-08 19:23:52 +0000

    bugfix: Fixed conditions and moved to the base class
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@8918 4525493e-7705-40b1-a816-d608a930855b

--- AccountingSystem/private/Plotters/DataOperationPlotter.py
+++ AccountingSystem/private/Plotters/DataOperationPlotter.py
@@ -7,7 +7,8 @@ from DIRAC.Core.Utilities import Time
 
 class DataOperationPlotter(BasePlotter):
 
-  __typeName = "DataOperation"
+  _typeName = "DataOperation"
+  _typeKeyFields = [ dF[0] for dF in DataOperation().definitionKeyFields ]
 
   def _translateGrouping( self, grouping ):
     if grouping == "Channel":
@@ -15,138 +16,84 @@ class DataOperationPlotter(BasePlotter):
     else:
       return [ grouping ]
 
-  def _plotSuceededTransfers( self, startTime, endTime, argsDict, grouping, filename ):
-    return self.__generateDataOperationSuceededTransfersPlot(startTime, endTime, grouping, argsDict, filename)
+  def _plotSuceededTransfers( self, startTime, endTime, condDict, groupingFields, filename ):
+    return self._realPlotSuceededFailedTransfers( startTime, endTime, condDict, groupingFields, filename, 'Suceeded', ( 'Failed', 0 ) )
 
-  def __generateDataOperationSuceededTransfersPlot( self, startTime, endTime, keyNameList, argsDict, filename ):
-    retVal = self.__getDataOperationTransfers( startTime, endTime, keyNameList, argsDict )
-    if not retVal[ 'OK' ]:
-      return retVal
-    dataDict, granularity = retVal[ 'Value' ]
-    strippedData = self.stripDataField( dataDict, 0 )
-    if strippedData:
-      dataDict[ 'Failed' ] = strippedData[0]
-    dataDict = self._fillWithZero( granularity, startTime, endTime, dataDict )
-    gLogger.info( "Generating plot", "%s with granularity of %s" % ( filename, granularity ) )
-    metadata = { 'title' : 'Suceeded Transfers by %s' % " -> ".join( keyNameList ) ,
-                 'ylabel' : 'files',
-                 'starttime' : startTime,
-                 'endtime' : endTime,
-                 'span' : granularity }
-    return generateTimedStackedBarPlot( filename, dataDict, metadata )
+  def _plotFailedTransfers( self, startTime, endTime, condDict, groupingFields, filename ):
+    return self._realPlotSuceededFailedTransfers( startTime, endTime, condDict, groupingFields, filename, 'Failed', ( 'Suceeded', 1 ) )
 
-  def _plotFailedTransfers( self, startTime, endTime, argsDict, grouping, filename ):
-    return self.__generateDataOperationFailedTransfersPlot(startTime, endTime, grouping, argsDict, filename)
-
-  def __generateDataOperationFailedTransfersPlot( self, startTime, endTime, keyNameList, argsDict, filename ):
-    retVal = self.__getDataOperationTransfers( startTime, endTime, keyNameList, argsDict )
-    if not retVal[ 'OK' ]:
-      return retVal
-    dataDict, granularity = retVal[ 'Value' ]
-    strippedData = self.stripDataField( dataDict, 1 )
-    if strippedData:
-      dataDict[ 'Suceeded' ] = strippedData[0]
-    dataDict = self._fillWithZero( granularity, startTime, endTime, dataDict )
-    gLogger.info( "Generating plot", "%s with granularity of %s" % ( filename, granularity ) )
-    metadata = { 'title' : 'Failed Transfers by %s' % " -> ".join( keyNameList ) ,
-                 'ylabel' : 'files',
-                 'starttime' : startTime,
-                 'endtime' : endTime,
-                 'span' : granularity }
-    return generateTimedStackedBarPlot( filename, dataDict, metadata )
-
-  def __getDataOperationTransfers( self, startTime, endTime, keyNameList, argsDict ):
-    typeName = "DataOperation"
-    condDict = {}
-    for keyword in keyNameList:
-      if keyword in argsDict:
-        condDict[ keyword ] = argsDict[ keyword ]
-    if len( keyNameList ) == 1:
-      keySQLString = "%s"
-    else:
-      keySQLString = "CONCAT( %s, ' -> ', %s )"
-    selectFields = ( keySQLString + ", %s, %s, SUM(%s), SUM(%s)-SUM(%s)",
-                     keyNameList + [ 'startTime', 'bucketLength',
+  def _realPlotSuceededFailedTransfers( self, startTime, endTime, condDict, groupingFields, filename, titleType, togetherFieldsToPlot ):
+    selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, SUM(%s), SUM(%s)-SUM(%s)",
+                     groupingFields + [ 'startTime', 'bucketLength',
                        'TransferOK', 'TransferTotal', 'TransferOK',
                       ]
                    )
-    retVal = self._retrieveBucketedData( "DataOperation",
-                                          startTime,
-                                          endTime,
-                                          selectFields,
-                                          condDict,
-                                          [ 'startTime' ] + keyNameList,
-                                          [ 'startTime' ]
-                                          )
-    if not retVal[ 'OK' ]:
-      return retVal
-    dataDict = self._groupByField( 0, retVal[ 'Value' ] )
-    coarsestGranularity = self._getBucketLengthForTime( typeName, startTime )
-    for keyField in dataDict:
-      dataDict[ keyField ] = self._sumToGranularity( coarsestGranularity, dataDict[ keyField ] )
-    return S_OK( ( dataDict, coarsestGranularity ) )
-
-  def _plotQuality( self, startTime, endTime, argsDict, grouping, filename ):
-    return self.__generateDataOperationQualityPlot(startTime, endTime, grouping, argsDict, filename)
-
-  def __generateDataOperationQualityPlot( self, startTime, endTime, keyNameList, argsDict, filename ):
-    retVal = self.__getDataOperationQuality( startTime, endTime, keyNameList, argsDict )
+    retVal = self._getTypeData( startTime,
+                                endTime,
+                                selectFields,
+                                condDict,
+                                groupingFields,
+                                {} )
     if not retVal[ 'OK' ]:
       return retVal
     dataDict, granularity = retVal[ 'Value' ]
-    self.stripDataField( dataDict, 0 )
+    startTime = startTime - startTime % granularity
+    strippedData = self.stripDataField( dataDict, togetherFieldsToPlot[1] )
+    if strippedData:
+      dataDict[ togetherFieldsToPlot[0] ] = strippedData[0]
+    dataDict = self._fillWithZero( granularity, startTime, endTime, dataDict )
     gLogger.info( "Generating plot", "%s with granularity of %s" % ( filename, granularity ) )
-    metadata = { 'title' : 'Transfer quality by %s' % " -> ".join( keyNameList ) ,
+    metadata = { 'title' : '%s Transfers by %s' % ( titleType, " -> ".join( groupingFields ) ),
+                 'ylabel' : 'files',
                  'starttime' : startTime,
                  'endtime' : endTime,
                  'span' : granularity }
-    return generateQualityPlot( filename, dataDict, metadata )
+    return generateTimedStackedBarPlot( filename, dataDict, metadata )
 
-  def __getDataOperationQuality( self, startTime, endTime, keyNameList, argsDict ):
-    typeName = "DataOperation"
-    condDict = {}
-    for keyword in keyNameList:
-      if keyword in argsDict:
-        condDict[ keyword ] = argsDict[ keyword ]
-    if len( keyNameList ) == 1:
-      keySQLString = "%s"
-    else:
-      keySQLString = "CONCAT( %s, ' -> ', %s )"
-    selectFields = ( keySQLString + ", %s, %s, SUM(%s)/SUM(%s)",
-                     keyNameList + [ 'startTime', 'bucketLength',
+  def _plotQuality( self, startTime, endTime, condDict, groupingFields, filename ):
+    selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, SUM(%s)/SUM(%s)",
+                     groupingFields + [ 'startTime', 'bucketLength',
                                     'TransferOK', 'TransferTotal'
                                    ]
                    )
-    retVal = self._retrieveBucketedData( "DataOperation",
-                                          startTime,
-                                          endTime,
-                                          selectFields,
-                                          condDict,
-                                          [ 'startTime' ] + keyNameList,
-                                          [ 'startTime' ]
-                                          )
-    if not retVal[ 'OK' ]:
-      return retVal
-    dataDict = self._groupByField( 0, retVal[ 'Value' ] )
-    coarsestGranularity = self._getBucketLengthForTime( typeName, startTime )
-    for keyField in dataDict:
-      dataDict[ keyField ] = self._convertNoneToZero( dataDict[ keyField ] )
-      dataDict[ keyField ] = self._averageToGranularity( coarsestGranularity, dataDict[ keyField ] )
-    return S_OK( ( dataDict, coarsestGranularity ) )
-
-
-  def _plotTransferedData( self, startTime, endTime, argsDict, grouping, filename ):
-    return self.__generateDataOperationTransferedDataPlot(startTime, endTime, grouping, argsDict, filename)
-
-  def __generateDataOperationTransferedDataPlot( self, startTime, endTime, keyNameList, argsDict, filename ):
-    retVal = self.__getDataOperationTransferedData( startTime, endTime, keyNameList, argsDict )
+    retVal = self._getTypeData( startTime,
+                                endTime,
+                                selectFields,
+                                condDict,
+                                groupingFields,
+                                { 'checkNone' : True, 'convertToGranularity' : 'average' } )
     if not retVal[ 'OK' ]:
       return retVal
     dataDict, granularity = retVal[ 'Value' ]
     startTime = startTime - startTime % granularity
     self.stripDataField( dataDict, 0 )
     gLogger.info( "Generating plot", "%s with granularity of %s" % ( filename, granularity ) )
-    metadata = { 'title' : 'Transfered data by %s' % " -> ".join( keyNameList ) ,
+    metadata = { 'title' : 'Transfer quality by %s' % " -> ".join( groupingFields ) ,
+                 'starttime' : startTime,
+                 'endtime' : endTime,
+                 'span' : granularity }
+    return generateQualityPlot( filename, dataDict, metadata )
+
+  def _plotTransferedData( self, startTime, endTime, condDict, groupingFields, filename ):
+    selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, %s/1000000000",
+                     groupingFields + [ 'startTime', 'bucketLength',
+                                    'TransferSize'
+                                   ]
+                   )
+    retVal = self._getTypeData( startTime,
+                                endTime,
+                                selectFields,
+                                condDict,
+                                groupingFields,
+                                {} )
+    if not retVal[ 'OK' ]:
+      return retVal
+    dataDict, granularity = retVal[ 'Value' ]
+    startTime = startTime - startTime % granularity
+    self.stripDataField( dataDict, 0 )
+    dataDict = self._fillWithZero( granularity, startTime, endTime, dataDict )
+    gLogger.info( "Generating plot", "%s with granularity of %s" % ( filename, granularity ) )
+    metadata = { 'title' : 'Transfered data by %s' % " -> ".join( groupingFields ) ,
                  'starttime' : startTime,
                  'endtime' : endTime,
                  'span' : granularity,
@@ -154,80 +101,28 @@ class DataOperationPlotter(BasePlotter):
                  'is_cumulative' : False }
     return generateCumulativePlot( filename, dataDict, metadata )
 
-  def __getDataOperationTransferedData( self, startTime, endTime, keyNameList, argsDict ):
-    typeName = "DataOperation"
-    condDict = {}
-    for keyword in keyNameList:
-      if keyword in argsDict:
-        condDict[ keyword ] = argsDict[ keyword ]
-    if len( keyNameList ) == 1:
-      keySQLString = "%s"
-    else:
-      keySQLString = "CONCAT( %s, ' -> ', %s )"
-    selectFields = ( keySQLString + ", %s, %s, %s/1000000000",
-                     keyNameList + [ 'startTime', 'bucketLength',
-                                    'TransferSize'
-                                   ]
+  def _plotThroughput( self, startTime, endTime, condDict, groupingFields, filename ):
+    selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, (SUM(%s)/SUM(%s))/1000000",
+                     groupingFields + [ 'startTime', 'bucketLength',
+                       'TransferSize', 'bucketLength',
+                      ]
                    )
-    retVal = self._retrieveBucketedData( "DataOperation",
-                                          startTime,
-                                          endTime,
-                                          selectFields,
-                                          condDict,
-                                          [ 'startTime' ] + keyNameList,
-                                          [ 'startTime' ]
-                                          )
-    if not retVal[ 'OK' ]:
-      return retVal
-    dataDict = self._groupByField( 0, retVal[ 'Value' ] )
-    coarsestGranularity = self._getBucketLengthForTime( typeName, startTime )
-    for keyField in dataDict:
-      dataDict[ keyField ] = self._sumToGranularity( coarsestGranularity, dataDict[ keyField ] )
-    return S_OK( ( dataDict, coarsestGranularity ) )
-
-  def _plotThroughput( self, startTime, endTime, argsDict, grouping, filename ):
-    return self.__generateDataOperationThroughputPlot(startTime, endTime, grouping, argsDict, filename)
-
-  def __generateDataOperationThroughputPlot( self, startTime, endTime, keyNameList, argsDict, filename ):
-    retVal = self.__getDataOperationThroughput( startTime, endTime, keyNameList, argsDict )
+    retVal = self._getTypeData( startTime,
+                                endTime,
+                                selectFields,
+                                condDict,
+                                groupingFields,
+                                {} )
     if not retVal[ 'OK' ]:
       return retVal
     dataDict, granularity = retVal[ 'Value' ]
+    startTime = startTime - startTime % granularity
     self.stripDataField( dataDict, 0 )
+    dataDict = self._fillWithZero( granularity, startTime, endTime, dataDict )
     gLogger.info( "Generating plot", "%s with granularity of %s" % ( filename, granularity ) )
-    metadata = { 'title' : 'Throughput by %s' % " -> ".join( keyNameList ) ,
+    metadata = { 'title' : 'Throughput by %s' % " -> ".join( groupingFields ) ,
                  'ylabel' : 'Mbyte/s',
                  'starttime' : startTime,
                  'endtime' : endTime,
                  'span' : granularity }
     return generateTimedStackedBarPlot( filename, dataDict, metadata )
-
-  def __getDataOperationThroughput( self, startTime, endTime, keyNameList, argsDict ):
-    condDict = {}
-    for keyword in keyNameList:
-      if keyword in argsDict:
-        condDict[ keyword ] = argsDict[ keyword ]
-    if len( keyNameList ) == 1:
-      keySQLString = "%s"
-    else:
-      keySQLString = "CONCAT( %s, ' -> ', %s )"
-    selectFields = ( keySQLString + ", %s, %s, (SUM(%s)/SUM(%s))/1000000",
-                     keyNameList + [ 'startTime', 'bucketLength',
-                       'TransferSize', 'bucketLength',
-                      ]
-                   )
-    retVal = self._retrieveBucketedData( self.__typeName,
-                                          startTime,
-                                          endTime,
-                                          selectFields,
-                                          condDict,
-                                          [ 'startTime' ] + keyNameList,
-                                          [ 'startTime' ]
-                                          )
-    if not retVal[ 'OK' ]:
-      return retVal
-    dataDict = self._groupByField( 0, retVal[ 'Value' ] )
-    coarsestGranularity = self._getBucketLengthForTime( self.__typeName, startTime )
-    for keyField in dataDict:
-      dataDict[ keyField ] = self._averageToGranularity( coarsestGranularity, dataDict[ keyField ] )
-    return S_OK( ( dataDict, coarsestGranularity ) )
