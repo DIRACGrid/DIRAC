d91cc0e3c3490dc01ebfb334d77719399473038c
Adria Casajus <adria@ecm.ub.es>
2008-05-08 17:27:09 +0000

    Convert UTC to local so graphtool plots data in real utc, not utc(utc(localtime))
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@8915 4525493e-7705-40b1-a816-d608a930855b

--- AccountingSystem/private/Plots.py
+++ AccountingSystem/private/Plots.py
@@ -1,10 +1,28 @@
-
+import time
 try:
   from graphtool.graphs.common_graphs import *
 except:
   raise Exception( "Missing GraphTool" )
 
 from DIRAC import S_OK, S_ERROR
+
+def convertUTCToLocal( metadata, data ):
+  """
+  Convert epoch times from utc to local
+  bucketsData must be a list of lists where each list contains
+    - field 0: datetime
+    - field 1: bucketLength
+    - fields 2-n: numericalFields
+  """
+  for mF in ( 'startime', 'endtime' ):
+    if mF in metadata:
+      metadata[ mF ] = metadata[ mF ] - time.altzone
+  for kF in data:
+    convertedData = {}
+    for iP in data[ kF ]:
+      convertedData[ iP - time.altzone ] = data[kF][iP]
+    data[kF] = convertedData
+  return metadata, data
 
 class TimeBarGraph( TimeGraph, BarGraph ):
   pass
@@ -19,6 +37,7 @@ def generateTimedStackedBarPlot( fileName, data, metadata ):
     return S_ERROR( "Can't open %s" % filename )
   if not data:
     return S_ERROR( "No data for that selection" )
+  metadata, data = convertUTCToLocal( metadata, data )
   plotter = TimeStackedBarGraph()
   plotter( data, fn, metadata )
   fn.close()
@@ -32,6 +51,7 @@ def generateQualityPlot( fileName, data, metadata ):
   if not data:
     return S_ERROR( "No data for that selection" )
   plotter = QualityMap()
+  metadata, data = convertUTCToLocal( metadata, data )
   plotter( data, fn, metadata )
   fn.close()
   return S_OK()
@@ -46,6 +66,7 @@ def generateCumulativePlot( fileName, data, metadata ):
   if not data:
     return S_ERROR( "No data for that selection" )
   plotter = CumulativeGraph()
+  metadata, data = convertUTCToLocal( metadata, data )
   plotter( data, fn, metadata )
   fn.close()
   return S_OK()
