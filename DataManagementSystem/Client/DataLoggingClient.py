3b0203707c73c46fcb25fd702b571058a71eb174
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-05-16 11:10:12 +0000

    Commit to make it easier to add a data logging record safely with multithreaded agents.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@9010 4525493e-7705-40b1-a816-d608a930855b

---
+++ DataManagementSystem/Client/DataLoggingClient.py
@@ -0,0 +1,30 @@
+""" Client for DataLoggingDB
+"""
+from DIRAC  import gLogger, gConfig, S_OK, S_ERROR
+from DIRAC.Core.DISET.RPCClient import RPCClient
+from DIRAC.ConfigurationSystem.Client import PathFinder
+import types
+
+class DataLoggingClient:
+  """ Client for DataLoggingDB
+  """
+  def __init__(self,url=False,useCertificates=False):
+    """ Constructor of the DataLogging client
+    """
+    try:
+      if not url:
+        self.url = PathFinder.getServiceURL('DataManagement/DataLogging')
+      else:
+        self.url = url
+    except Exception, x:
+      errStr = "DataLoggingClient.__init__: Exception while obtaining service URL."
+      gLogger.exception(errStr,str(x))
+
+  def addFileRecord(self,lfn,status,minor,date,source):
+    try:
+      client = RPCClient(self.url)
+      return client.addFileRecord(lfn,status,minor,date,source)
+    except Exception, x:
+      errStr = "DataLoggingClient.__init__: Exception while adding file record."
+      gLogger.exception(errStr,str(x))
+      return S_ERROR(errStr)
