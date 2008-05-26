3568b7b7830d60beda1211d76abb3e6f4506aaef
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-05-26 09:38:38 +0000

    First commit....
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@9152 4525493e-7705-40b1-a816-d608a930855b

---
+++ DataManagementSystem/DB/DatasetDB.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS DatasetDB;
+CREATE DATABASE DatasetDB;
+
+DROP TABLE IF EXISTS Datasets;
+CREATE TABLE Datasets(
+    DatasetID INTEGER NOT NULL AUTO_INCREMENT,
+    DatasetHandle VARCHAR(255) NOT NULL,
+    Description VARCHAR(255),
+    LongDescription  BLOB,
+    CreationDate DATETIME,
+    AuthorDN VARCHAR(255) NOT NULL,
+    AuthorGroup VARCHAR(255) NOT NULL,
+    Type CHAR(16) DEFAULT 'Simulation',
+    Status  CHAR(16) DEFAULT 'New',
+    PRIMARY KEY(DatasetID)
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS DatasetParameters;
+CREATE TABLE DatasetParameters (
+    DatasetID INTEGER NOT NULL,
+    ParameterName VARCHAR(32) NOT NULL,
+    ParameterValue VARCHAR(255) NOT NULL,
+    PRIMARY KEY(DatasetID,ParameterName)
+);
+
+DROP TABLE IF EXISTS DatasetLog;
+CREATE TABLE DatasetLog (
+    DatasetID INTEGER NOT NULL,
+    Message VARCHAR(255) NOT NULL,
+    Author VARCHAR(255) NOT NULL DEFAULT "Unknown",
+    MessageDate DATETIME NOT NULL,
+    INDEX (DatasetID)
+);
