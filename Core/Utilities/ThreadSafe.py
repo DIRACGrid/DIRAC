b5c3fd0f607064c8a404b2ba4faa126e81bd07c2
Adria Casajus <adria@ecm.ub.es>
2008-05-26 18:37:50 +0000

    Added Write Once Read Many object
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@9270 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/ThreadSafe.py
+++ Core/Utilities/ThreadSafe.py
@@ -1,5 +1,5 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/ThreadSafe.py,v 1.1 2007/12/19 18:01:49 acasajus Exp $
-__RCSID__ = "$Id: ThreadSafe.py,v 1.1 2007/12/19 18:01:49 acasajus Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Utilities/ThreadSafe.py,v 1.2 2008/05/26 18:37:50 acasajus Exp $
+__RCSID__ = "$Id: ThreadSafe.py,v 1.2 2008/05/26 18:37:50 acasajus Exp $"
 
 import threading
 
@@ -27,3 +27,65 @@ class Synchronizer:
           print "UNLOCKING", self.lockName
         self.lock.release()
     return lockedFunc
+
+
+class WORM:
+  """
+  Write One - Read Many
+  """
+  def __init__( self, maxReads = 10 ):
+    self.__lock = threading.Lock()
+    self.__maxReads = maxReads
+    self.__semaphore = threading.Semaphore( maxReads )
+
+  def write( self, funcToCall ):
+    def __doWriteLock( *args, **kwargs ):
+      try:
+          self.__startWriteZone()
+          return funcToCall(*args, **kwargs)
+      finally:
+        self.__endWriteZone()
+    return __doWriteLock
+
+  def read( self, funcToCall ):
+    def __doReadLock( *args, **kwargs ):
+      try:
+        self.__startReadZone()
+        return funcToCall(*args, **kwargs)
+      finally:
+        self.__endReadZone()
+    return __doReadLock
+
+  def __startWriteZone(self):
+    """
+    Locks Event to prevent further threads from reading.
+    Stops current thread until no other thread is accessing.
+    PRIVATE USE
+    """
+    self.__lock.acquire()
+    for i in range( self.__maxReads ):
+      self.__semaphore.acquire()
+    self.__lock.release()
+
+  def __endWriteZone(self):
+    """
+    Unlocks Event.
+    PRIVATE USE
+    """
+    for i in range( self.__maxReads ):
+      self.__semaphore.release()
+
+  def __startReadZone(self):
+    """
+    Start of danger zone. This danger zone may be or may not be a mutual exclusion zone.
+    Counter is maintained to know how many threads are inside and be able to enable and disable mutual exclusion.
+    PRIVATE USE
+    """
+    self.__semaphore.acquire()
+
+  def __endReadZone( self ):
+    """
+    End of danger zone.
+    PRIVATE USE
+    """
+    self.__semaphore.release()
