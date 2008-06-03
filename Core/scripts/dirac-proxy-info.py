ca8671984a74d2c0309f376777979a011d4cd653
Adria Casajus <adria@ecm.ub.es>
2008-06-03 12:56:30 +0000

    dirac-proxy-info
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@9469 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/scripts/dirac-proxy-info.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+########################################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/scripts/Attic/dirac-proxy-info.py,v 1.1 2008/06/03 12:56:30 acasajus Exp $
+# File :   dirac-proxy-init.py
+# Author : Adrian Casajus
+########################################################################
+__RCSID__   = "$Id: dirac-proxy-info.py,v 1.1 2008/06/03 12:56:30 acasajus Exp $"
+__VERSION__ = "$Revision: 1.1 $"
+
+import sys
+import os.path
+from DIRACEnvironment import DIRAC
+from DIRAC.Core.Base import Script
+
+
+class Params:
+
+  debug = False
+  proxyLoc = False
+  checkExists = False
+
+  def setDebug( self, arg ):
+    print "Enabling debug output"
+    self.debug = True
+    return DIRAC.S_OK()
+
+  def showVersion( self, arg ):
+    print "Version:"
+    print " ", __RCSID__
+    print " ", __VERSION__
+    sys.exit(0)
+    return DIRAC.S_OK()
+
+  def setProxyLocation( self, arg ):
+    self.proxyLoc = arg
+    return DIRAC.S_OK()
+
+  def checkExists( self, arg ):
+    self.checkExists = True
+    return DIRAC.S_OK()
+
+params = Params()
+
+Script.registerSwitch( "d", "debug", "Enable debug output", params.setDebug )
+Script.registerSwitch( "f:", "file=", "File to use as user key", params.setProxyLocation )
+Script.registerSwitch( "i", "version", "Print version", params.showVersion )
+
+
+Script.disableCS()
+Script.parseCommandLine()
+
+from DIRAC.Core import Security
+
+proxyLoc = params.proxyLoc
+if not proxyLoc:
+  proxyLoc = Security.getProxyLocation()
+
+if params.debug:
+  print "Proxy file: %s" % proxyLoc
+if not proxyLoc:
+  print "Can't find any valid proxy"
+  sys.exit(1)
+
+chain = Security.X509Chain()
+retVal = chain.loadChainFromFile( proxyLoc )
+if not retVal[ 'OK' ]:
+  print "Can't load %s: %s" % ( proxyLoc, retVal[ 'Message' ] )
+  sys.exit(1)
+retVal = chain.loadKeyFromFile( proxyLoc )
+print retVal
+if not retVal[ 'OK' ]:
+  print "Can't load %s: %s" % ( proxyLoc, retVal[ 'Message' ] )
+  sys.exit(1)
+
+print "subject     : %s" % chain.getCertInChain().getSubjectDN()[ 'Value' ]
+print "issuer      : %s" % chain.getCertInChain().getIssuerDN()[ 'Value' ]
+print "identity    : %s" % chain.getCertInChain(-1).getSubjectDN()[ 'Value' ]
+group = chain.getDIRACGroup()[ 'Value' ]
+if not group:
+  group = '<not set>'
+print "DIRAC Group : %s" % group
+secs = chain.getRemainingSecs()[ 'Value' ]
+hours = int( secs /  3600 )
+secs -= hours * 3600
+mins = int( secs / 60 )
+secs -= mins * 60
+print "time left   : %02d:%02d:%02d" % ( hours, mins, secs )
+sys.exit(0)
