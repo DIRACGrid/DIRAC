39da8b90409b796d668dd8bbbbf8f74a0021f71d
Adria Casajus <adria@ecm.ub.es>
2008-06-10 12:38:09 +0000

    Possiblity to group check with CS
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@9618 4525493e-7705-40b1-a816-d608a930855b

--- Core/scripts/dirac-proxy-init.py
+++ Core/scripts/dirac-proxy-init.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/scripts/Attic/dirac-proxy-init.py,v 1.5 2008/06/06 12:32:03 acasajus Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/scripts/Attic/dirac-proxy-init.py,v 1.6 2008/06/10 12:38:09 acasajus Exp $
 # File :   dirac-proxy-init.py
 # Author : Adrian Casajus
 ########################################################################
-__RCSID__   = "$Id: dirac-proxy-init.py,v 1.5 2008/06/06 12:32:03 acasajus Exp $"
-__VERSION__ = "$Revision: 1.5 $"
+__RCSID__   = "$Id: dirac-proxy-init.py,v 1.6 2008/06/10 12:38:09 acasajus Exp $"
+__VERSION__ = "$Revision: 1.6 $"
 
 import sys
+import getpass
 from DIRACEnvironment import DIRAC
 from DIRAC.Core.Base import Script
 
@@ -21,6 +22,7 @@ class Params:
   certLoc = False
   keyLoc = False
   proxyLoc = False
+  checkWithCS = True
 
   def setProxyLifeTime( self, arg ):
     try:
@@ -64,12 +66,20 @@ class Params:
     self.proxyLoc = arg
     return DIRAC.S_OK()
 
+  def setDisableCSCheck( self, arg ):
+    self.checkWithCS = False
+    return DIRAC.S_OK()
+
   def showVersion( self, arg ):
     print "Version:"
     print " ", __RCSID__
     print " ", __VERSION__
     sys.exit(0)
     return DIRAC.S_OK()
+
+  def debugMsg( self, msg ):
+    if self.debug:
+      print msg
 
 params = Params()
 
@@ -81,10 +91,14 @@ Script.registerSwitch( "d", "debug", "Enable debug output", params.setDebug )
 Script.registerSwitch( "c:", "cert=", "File to use as user certificate", params.setCertLocation )
 Script.registerSwitch( "k:", "key=", "File to use as user key", params.setKeyLocation )
 Script.registerSwitch( "u:", "out=", "File to write as proxy", params.setProxyLocation )
+Script.registerSwitch( "x", "nocs", "Disable CS check", params.setDisableCSCheck )
 Script.registerSwitch( "i", "version", "Print version", params.showVersion )
+Script.addDefaultOptionValue( "LogLevel", "always" )
 
 Script.disableCS()
 Script.parseCommandLine()
+
+userPasswd = getpass.getpass( "Enter Certificate password:" )
 
 from DIRAC.Core import Security
 
@@ -125,16 +139,49 @@ retVal = chain.loadChainFromFile( certLoc )
 if not retVal[ 'OK' ]:
   print "Can't load %s" % certLoc
   sys.exit(1)
-retVal = chain.loadKeyFromFile( keyLoc )
+retVal = chain.loadKeyFromFile( keyLoc, password = userPasswd )
 if not retVal[ 'OK' ]:
   print "Can't load %s" % keyLoc
   sys.exit(1)
 
+if params.checkWithCS:
+  retVal = chain.generateProxyToFile( proxyLoc,
+                                      params.proxyLifeTime,
+                                      strength =params.proxyStrength,
+                                      limited = params.limitedProxy )
+
+  params.debugMsg( "Contacting CS..." )
+
+  retVal = Script.enableCS()
+  if not retVal[ 'OK' ]:
+    print "Can't contact DIRAC CS: %s" % retVal[ 'Message' ]
+    print "Aborting..."
+    sys.exit(1)
+  if not params.diracGroup:
+    params.diracGroup = Security.CS.getDefaultUserGroup()
+  userDN = chain.getCertInChain( -1 )['Value'].getSubjectDN()['Value']
+  params.debugMsg( "Checking DN %s" % userDN )
+  retVal = Security.CS.getUsernameForDN( userDN )
+  if not retVal[ 'OK' ]:
+    print "DN %s is not registered" % userDN
+    sys.exit(1)
+  username = retVal[ 'Value' ]
+  params.debugMsg( "Username is %s" % username )
+  retVal = Security.CS.getGroupsForUser( username )
+  if not retVal[ 'OK' ]:
+    print "User %s has no groups defined" % username
+    sys.exit(1)
+  groups = retVal[ 'Value' ]
+  if params.diracGroup not in groups:
+    print "Requested group %s is not valid for user %s" % ( params.diracGroup, username )
+    sys.exit(1)
+  params.debugMsg( "Creating proxy for %s@%s (%s)" % ( username, params.diracGroup, userDN ) )
+
 retVal = chain.generateProxyToFile( proxyLoc,
                                     params.proxyLifeTime,
                                     params.diracGroup,
-                                    params.proxyStrength,
-                                    params.limitedProxy )
+                                    strength =params.proxyStrength,
+                                    limited = params.limitedProxy )
 
 if not retVal[ 'OK' ]:
   print "Couldn't generate proxy: %s" % retVal[ 'Message' ]
