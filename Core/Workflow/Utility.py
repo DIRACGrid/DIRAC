930713b95ec19475837ce0cf63de9a3bb960c234
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2008-06-15 11:31:06 +0000

    More utilities
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@9736 4525493e-7705-40b1-a816-d608a930855b

--- Core/Workflow/Utility.py
+++ Core/Workflow/Utility.py
@@ -1,40 +1,103 @@
-# $Id: Utility.py,v 1.1 2008/04/28 14:35:24 atsareg Exp $
+# $Id: Utility.py,v 1.2 2008/06/15 11:31:06 atsareg Exp $
 
 """
-    Workflow Utility module contains a number of functions use ful for various
+    Workflow Utility module contains a number of functions useful for various
     workflow operations
 """
 
-__RCSID__ = "$Revision: 1.1 $"
+__RCSID__ = "$Revision: 1.2 $"
 
-from DIRAC.Core.Workflow.Workflow import Workflow
-from DIRAC.Core.Workflow.Step import *
+import types, re
 
-def createSingleModuleWorkflow(module,name):
-  """ Creates a workflow based on a single module definition
+def getSubstitute(param):
+  """ Get the variable name to which the given parameter is referring
+  """
+  result = ''
+  sres = re.search("@{([][\w,.:$()]+)}",str(param))
+  if sres:
+    result = sres.group(1)
+
+  return result
+
+def substitute(param,variable,value):
+  """ Substitute the variable reference with the value
   """
 
-  moduleType = module.getType()
-  moduleName = name
+  tmp_string = str(param).replace('@{'+variable+'}',value)
+  if type(param) in types.StringTypes:
+    return tmp_string
+  else:
+    return eval(tmp_string)
 
-  workflow = Workflow()
-  step = StepDefinition(moduleType+'_step')
-  step.addModule(module)
-  moduleInstance = step.createModuleInstance(moduleType,moduleName)
+def resolveVariables(varDict):
+  """ Resolve variables defined in terms of others within the same dictionary
+  """
+  max_tries = 10
+  variables = varDict.keys()
+  ntry = 0
+  while ntry < max_tries:
+    substFlag = False
+    for var,value in varDict.items():
+      if type(value) in types.StringTypes:
+        substitute_var = getSubstitute(value)
+        if substitute_var in variables:
+          varDict[var] = substitute(varDict[var],substitute_var,varDict[substitute_var])
+          substFlag = True
+    if not substFlag:
+      break
+    ntry += 1
+  else:
+    print "Failed to resolve referencies in %d attempts" % max_tries
 
-  step.addParameter(moduleInstance.parameters.getInput())
-  workflow.addParameter(moduleInstance.parameters.getInput())
+def dataFromOption(parameter):
 
-  workflow.addStep(step)
-  stepInstance = workflow.createStepInstance(moduleType+'_step',moduleName+'_step')
+  result = []
 
-  # Propagate the module input parameters to the workflow level
-  moduleInstance.linkParameterUp(moduleInstance.parameters.getInput())
-  stepInstance.linkParameterUp(moduleInstance.parameters.getInput())
+  if parameter.type.lower() == 'option':
 
-  workflow.setName(name)
-  workflow.setDescription('Single module workflow from '+moduleType+' type module')
-  workflow.setDescrShort(moduleType+' workflow')
-  return workflow
+    fields = parameter.value.split(',')
 
+    for f in fields:
+      if ( re.search('FILE\s*=',f ) ) :
+        #print f
+        fname = re.search("FILE\s*=\s*'([][;\/\w.:\s@{}-]+)'",f).group(1)
+        res = re.search("TYP\w*\s*=\s*'(\w+)'",f)
+        if res:
+          ftype = res.group(1)
+        else:
+          ftype = "Unknown"
 
+        result.append((fname,ftype))
+
+  return result
+
+def expandDatafileOption(option):
+
+  result = ''
+
+  if not re.search(';;',option.value):
+    return result
+
+  files = dataFromOption(option)
+  if len(files) == 1:
+    fname,ftype = files[0]
+    fnames = fname.split(';;')
+    if len(fnames) > 1:
+
+      template = option.value.strip().replace('=','',1)
+      template = template.replace('{','')
+      template = template.replace('}','')
+      opt = []
+      for f in fnames:
+        opt.append(template.replace(fname,f))
+
+      result = '={'+string.join(opt,',')+'}'
+
+  return result
+
+def dataFromOptions(parameters):
+
+  for n,v in parameters.items():
+    files = dataFromOption(v)
+    if fname:
+      gLog.info( str( files ) )
