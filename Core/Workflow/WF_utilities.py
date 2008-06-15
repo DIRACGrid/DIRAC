d2e77eb13c8fc22838924d2a800d042b13873948
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2008-06-15 11:30:27 +0000

    Utilities that can simplify creation of workflows
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@9735 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Workflow/WF_utilities.py
@@ -0,0 +1,38 @@
+# $Id: WF_utilities.py,v 1.1 2008/06/15 11:30:27 atsareg Exp $
+
+"""
+    Workflow Utility module contains a number of functions useful for the
+    client operations
+"""
+
+__RCSID__ = "$Revision: 1.1 $"
+
+from DIRAC.Core.Workflow.Workflow import Workflow
+from DIRAC.Core.Workflow.Step import *
+
+def createSingleModuleWorkflow(module,name):
+  """ Creates a workflow based on a single module definition
+  """
+
+  moduleType = module.getType()
+  moduleName = name
+
+  workflow = Workflow()
+  step = StepDefinition(moduleType+'_step')
+  step.addModule(module)
+  moduleInstance = step.createModuleInstance(moduleType,moduleName)
+
+  step.addParameter(moduleInstance.parameters.getInput())
+  workflow.addParameter(moduleInstance.parameters.getInput())
+
+  workflow.addStep(step)
+  stepInstance = workflow.createStepInstance(moduleType+'_step',moduleName+'_step')
+
+  # Propagate the module input parameters to the workflow level
+  moduleInstance.linkParameterUp(moduleInstance.parameters.getInput())
+  stepInstance.linkParameterUp(moduleInstance.parameters.getInput())
+
+  workflow.setName(name)
+  workflow.setDescription('Single module workflow from '+moduleType+' type module')
+  workflow.setDescrShort(moduleType+' workflow')
+  return workflow
