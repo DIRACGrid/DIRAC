65dd48a1a296b3f47d90210c14a912b8f63b61db
Stuart K. Paterson <stuart.paterson@cern.ch>
2008-06-23 13:20:42 +0000

    DIRAC.Core.Workflow.Modules package.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@10068 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Workflow/Modules/__init__.py
@@ -0,0 +1,11 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Workflow/Modules/__init__.py,v 1.1 2008/06/23 13:20:42 paterson Exp $
+############################################################
+
+"""
+    DIRAC.Core.Workflow.Modules package.
+
+    Contains all generic workflow modules suitable for any VO.
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2008/06/23 13:20:42 paterson Exp $"
