905cb6f20da92cf3fe62d27a4d0bf27a33b88d82
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2008-06-25 11:03:34 +0000

    New policies added
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@10108 4525493e-7705-40b1-a816-d608a930855b

--- Core/Security/Properties.py
+++ Core/Security/Properties.py
@@ -1,4 +1,20 @@
-
+#
 TRUSTED_HOST = "TrustedHost"
-#Allow getting full delegated proxies
+# Allow getting full delegated proxies
 FULL_DELEGATION = "FullDelegation"
+# Normal user operations
+NORMAL_USER = "NormalUser"
+# CS Administrator - possibility to edit the Configuration Service
+CS_ADMINISTRATOR = "CSAdministrator"
+# Job sharing among members of a group
+JOB_SHARING = "JobSharing"
+# DIRAC Service Administrator
+SERVICE_ADMINISTRATOR = "ServiceAdministrator"
+# Job Administrator
+JOB_ADMINISTRATOR = "JobAdministrator"
+# Job Pilot
+JOB_PILOT = "JobPilot"
+# Site Manager
+SITE_MANAGER = "SiteManager"
+# Visitor of the DIRAC Web portal
+VISITOR = "Visitor"
