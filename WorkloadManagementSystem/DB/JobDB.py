d420d6cdbae81afd9df4ad065797d2c551b8f2cb
Adria Casajus <adria@ecm.ub.es>
2008-07-10 14:49:26 +0000

    upper once
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@10516 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/DB/JobDB.py
+++ WorkloadManagementSystem/DB/JobDB.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/DB/JobDB.py,v 1.60 2008/07/04 14:48:12 paterson Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/DB/JobDB.py,v 1.61 2008/07/10 14:49:26 acasajus Exp $
 ########################################################################
 
 """ DIRAC JobDB class is a front-end to the main WMS database containing
@@ -52,7 +52,7 @@
     getCounters()
 """
 
-__RCSID__ = "$Id: JobDB.py,v 1.60 2008/07/04 14:48:12 paterson Exp $"
+__RCSID__ = "$Id: JobDB.py,v 1.61 2008/07/10 14:49:26 acasajus Exp $"
 
 import re, os, sys, string, types
 import time
@@ -1177,8 +1177,8 @@ class JobDB(DB):
     res = self._getFields('TaskQueues',['Requirements','TaskQueueId'],[],[])
     if not res['OK']:
       return res
-    classAdJob = ClassAd('[ Requirements = '+requirements+' ]')
-    jobRequirement = classAdJob.get_expression("Requirements")
+    classAdJob = ClassAd( '[ Requirements = %s ]' % requirements )
+    jobRequirement = classAdJob.get_expression("Requirements").upper()
     for row in res['Value']:
       classAdQueue = ClassAd(row[0])
       queueID = row[1]
@@ -1187,7 +1187,7 @@ class JobDB(DB):
         self._update( cmd )
       else:
         queueRequirement = classAdQueue.get_expression("Requirements")
-        if queueRequirement.upper() == jobRequirement.upper():
+        if queueRequirement.upper() == jobRequirement:
           return S_OK( queueID )
 
     self.log.info( 'JobDB.selectQueue: creating a new Queue' )
