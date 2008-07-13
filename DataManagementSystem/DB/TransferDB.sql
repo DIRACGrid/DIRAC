722945383c5269b47063d1706afba2fb036ad4f1
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-07-13 19:22:02 +0000

    TransferDB must always exist with a RequestDB on top, therefore we source the RequestManagement/DB/RequestDB.sql before creating the TransferDB tables.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@10598 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/DB/TransferDB.sql
+++ DataManagementSystem/DB/TransferDB.sql
@@ -1,69 +1,4 @@
-DROP DATABASE IF EXISTS TransferDB;
-CREATE DATABASE TransferDB;
-
---
--- Must set passwords for database user by replacing "must_be_set".
---
-use mysql;
-GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON TransferDB.* TO 'Dirac'@'localhost' IDENTIFIED BY 'must_be_set';
-GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON TransferDB.* TO 'Dirac'@'%' IDENTIFIED BY 'must_be_set';
-
-use TransferDB;
-
--- THESE ARE THE TABLES FOR THE REQUEST DB
--- Requests, SubRequests,Files,Datasets
-
-DROP TABLE IF EXISTS Requests;
-CREATE TABLE Requests (
-   RequestID INTEGER NOT NULL AUTO_INCREMENT,
-   Status varchar(32) DEFAULT 'Receiving',
-   RequestName varchar(128) NOT NULL,
-   JobID int(11) DEFAULT NULL,
-   OwnerDN varchar(255) DEFAULT NULL,
-   DIRACInstance varchar(32) DEFAULT NULL,
-   CreationTime DATETIME,
-   SubmissionTime DATETIME,
-   LastUpdate DATETIME,
-   PRIMARY KEY (RequestID,RequestName)
-);
-
-DROP TABLE IF EXISTS SubRequests;
-CREATE TABLE SubRequests (
-   RequestID INTEGER NOT NULL,
-   SubRequestID INTEGER NOT NULL AUTO_INCREMENT,
-   Status varchar(32) DEFAULT 'Receiving',
-   RequestType  varchar(32) NOT NULL,
-   Operation varchar(32),
-   SourceSE varchar(32),
-   TargetSE varchar(255),
-   Catalogue varchar(32),
-   SubmissionTime datetime,
-   LastUpdate datetime,
-   PRIMARY KEY (RequestID,SubRequestID)
-);
-
-DROP TABLE IF EXISTS Files;
-CREATE TABLE Files (
-   SubRequestID INTEGER NOT NULL,
-   FileID INTEGER NOT NULL AUTO_INCREMENT,
-   Status varchar(32) DEFAULT 'New',
-   LFN  varchar(255),
-   Size INTEGER,
-   PFN varchar(255),
-   GUID varchar(64),
-   Md5 varchar(32),
-   Addler varchar(32),
-   Attempt varchar(32),
-   PRIMARY KEY (SubRequestID, FileID)
-);
-
-DROP TABLE IF EXISTS Datasets;
-CREATE TABLE Datasets(
-   SubRequestID INTEGER NOT NULL,
-   Dataset varchar(255) NOT NULL,
-   Status varchar(32) DEFAULT 'NEW',
-   PRIMARY KEY (SubRequestID,Dataset)
-);
+SOURCE DIRAC/RequestManagementSystem/DB/RequestDB.sql
 
 -- THESE ARE THE TABLES FOR THE TRANSFER DB
 -- Channels,Channel,FTSReq,FileToFTS,FTSReqLogging,FileToCat,ReplicationTree
@@ -103,7 +38,7 @@ CREATE TABLE FTSReq (
   TotalSize bigint(20) DEFAULT 0,
   SubmitTime datetime NOT NULL,
   LastMonitor datetime,
-  PercentageComplete float default 0.0
+  PercentageComplete float default 0.0,
   PRIMARY KEY (FTSReqID,ChannelID)
 );
 
@@ -112,7 +47,7 @@ CREATE TABLE FileToFTS (
   FileID INTEGER NOT NULL,
   FTSReqID varchar(64) NOT NULL,
   ChannelID INTEGER NOT NULL,
-  Status varchar(32) DEFAULT 'Submitted'
+  Status varchar(32) DEFAULT 'Submitted',
   Duration int(8) DEFAULT 0,
   Reason varchar(511),
   Retries int(8) DEFAULT 0,
