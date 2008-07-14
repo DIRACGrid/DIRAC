26f5feeedf8e7594a270c9b9327c4d674b67913e
Adria Casajus <adria@ecm.ub.es>
2008-07-14 16:14:59 +0000

    Can get multiple shifter types
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@10621 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/Shifter.py
+++ Core/Utilities/Shifter.py
@@ -3,18 +3,20 @@ import os
 from DIRAC import S_OK, S_ERROR, gLogger, gConfig
 from DIRAC.FrameworkSystem.Client.ProxyManagerClient import gProxyManager
 
-def getShifterProxy():
+def getShifterProxy( type ):
   """
   This method returns a shifter's proxy
+    - type : ProductionManager / DataManager...
   """
-  userName = gConfig.getValue('/Operations/Production/ShifterUsername','')
+  shifterSection = "/Operations/Shifter/%s" % type
+  userName = gConfig.getValue( '%s/User' % shifterSection, '' )
   if not userName:
-    return S_ERROR( "No shifter defined in /Operations/Production/ShifterUsername" )
+    return S_ERROR( "No shifter defined in %s/User" % shifterSection )
   result = CS.getDNForUsername( userName )
   if not result[ 'OK' ]:
     return result
   userDN = result[ 'Value' ][0]
-  userGroup = gConfig.getValue( '/Operations/Production/ShifterGroup', 'lhcb_prod' )
+  userGroup = gConfig.getValue( '%s/Group' % shifterSection, 'lhcb_prod' )
   gLogger.info( "Getting proxy for shifter %s@%s (%s)" % ( userName, userGroup, userDN ) )
   result = gProxyManager.downloadVOMSProxy( userDN, userGroup )
   if not result[ 'OK' ]:
@@ -30,10 +32,12 @@ def getShifterProxy():
                  'chain' : result[ 'Value' ],
                  'proxyFile' : fileName } )
 
-def setupShifterProxyInEnv():
+def setupShifterProxyInEnv( type ):
   """
   Return the shifter's proxy and set it up as the default
   proxy via changing the environment
+  This method returns a shifter's proxy
+    - type : ProductionManager / DataManager...
   """
   result = getShifterProxy()
   if not result[ 'OK' ]:
