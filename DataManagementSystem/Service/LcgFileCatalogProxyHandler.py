1406b1f015757ef1ebb76fa3b93f518c3efd317d
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-07-15 10:07:55 +0000

    < from DIRAC.DataManagementSystem.Client.Catalog.LcgFileCatalogCombinedClient import LcgFileCatalogCombinedClient < lfcCombinedClient = LcgFileCatalogCombinedClient() < method = getattr(lfcCombinedClient,name) ---
    > from DIRAC.DataManagementSystem.Client.FileCatalog import FileCatalog
    > fileCatalog = FileCatalog(['LcgFileCatalogCombined'])
    > method = getattr(fileCatalog,name)
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@10644 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Service/LcgFileCatalogProxyHandler.py
+++ DataManagementSystem/Service/LcgFileCatalogProxyHandler.py
@@ -4,15 +4,13 @@ This is a service which represents a DISET proxy to the LCG File Catalog
 from types import *
 from DIRAC.Core.DISET.RequestHandler import RequestHandler
 from DIRAC import gLogger, gConfig, S_OK, S_ERROR
-from DIRAC.DataManagementSystem.Client.Catalog.LcgFileCatalogCombinedClient import LcgFileCatalogCombinedClient
-#from DIRAC.DataManagementSystem.Client.LcgFileCatalogClient import LcgFileCatalogClient
+from DIRAC.DataManagementSystem.Client.FileCatalog import FileCatalog
 
 lfcCombinedClient = False
 
 def initializeLcgFileCatalogProxyHandler(serviceInfo):
   global lfcCombinedClient
-  #lfcCombinedClient = LcgFileCatalogClient(host='lfc-lhcb.cern.ch',infosys='lcg-bdii.cern.ch:2170')
-  lfcCombinedClient = LcgFileCatalogCombinedClient()
+  fileCatalog = FileCatalog(['LcgFileCatalogCombined'])
   return S_OK()
 
 class LcgFileCatalogProxyHandler(RequestHandler):
@@ -22,12 +20,11 @@ class LcgFileCatalogProxyHandler(RequestHandler):
     """ A generic method to call methods of the LCG FileCatalog client
     """
     try:
-      method = getattr(lfcCombinedClient,name)
+      method = getattr(fileCatalog,name)
     except AttributeError, x:
       error = "Exception: no method named "+name
       return S_ERROR(error)
 
-    #kargs["DN"] = self.sDN
     try:
       result = method(*args,**kargs)
       return result
