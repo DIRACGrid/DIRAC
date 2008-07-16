3237de7a4ea9718152e410a6e29d07adf39596da
Adria Casajus <adria@ecm.ub.es>
2008-07-16 22:38:30 +0000

    Properly calculate values in each record before adding. This has taken a while to realize :P
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@10713 4525493e-7705-40b1-a816-d608a930855b

--- AccountingSystem/private/Plotters/WMSHistoryPlotter.py
+++ AccountingSystem/private/Plotters/WMSHistoryPlotter.py
@@ -11,9 +11,9 @@ class WMSHistoryPlotter(BasePlotter):
   _typeKeyFields = [ dF[0] for dF in Job().definitionKeyFields ]
 
   def _plotNumberOfJobs( self, startTime, endTime, condDict, groupingFields, filename ):
-    selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, SUM(%s)/(SUM(%s)/COUNT(%s))",
+    selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, SUM(%s/%s)",
                      groupingFields + [ 'startTime', 'bucketLength',
-                                    'Jobs', 'entriesInBucket', 'entriesInBucket'
+                                    'Jobs', 'entriesInBucket'
                                    ]
                    )
     retVal = self._getTypeData( startTime,
@@ -33,4 +33,29 @@ class WMSHistoryPlotter(BasePlotter):
                  'endtime' : endTime,
                  'span' : granularity,
                  'ylabel' : "jobs"  }
+    return generateTimedStackedBarPlot( filename, dataDict, metadata )
+
+  def _plotNumberOfReschedules( self, startTime, endTime, condDict, groupingFields, filename ):
+    selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, SUM(%s/%s)",
+                     groupingFields + [ 'startTime', 'bucketLength',
+                                    'Reschedules', 'entriesInBucket'
+                                   ]
+                   )
+    retVal = self._getTypeData( startTime,
+                                endTime,
+                                selectFields,
+                                condDict,
+                                groupingFields,
+                                {} )
+    if not retVal[ 'OK' ]:
+      return retVal
+    dataDict, granularity = retVal[ 'Value' ]
+    self.stripDataField( dataDict, 0 )
+    dataDict = self._fillWithZero( granularity, startTime, endTime, dataDict )
+    gLogger.info( "Generating plot", "%s with granularity of %s" % ( filename, granularity ) )
+    metadata = { 'title' : 'Reschedules by %s' % " -> ".join( groupingFields ) ,
+                 'starttime' : startTime,
+                 'endtime' : endTime,
+                 'span' : granularity,
+                 'ylabel' : "reschedules"  }
     return generateTimedStackedBarPlot( filename, dataDict, metadata )
