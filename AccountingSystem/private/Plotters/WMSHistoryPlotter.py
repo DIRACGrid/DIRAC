8ef9b42256eb936e92442dc239c0b18c7e9fdd2e
Adria Casajus <adria@ecm.ub.es>
2008-07-16 17:54:50 +0000

    buffix
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@10701 4525493e-7705-40b1-a816-d608a930855b

--- AccountingSystem/private/Plotters/WMSHistoryPlotter.py
+++ AccountingSystem/private/Plotters/WMSHistoryPlotter.py
@@ -10,36 +10,10 @@ class WMSHistoryPlotter(BasePlotter):
   _typeName = "WMSHistory"
   _typeKeyFields = [ dF[0] for dF in Job().definitionKeyFields ]
 
-  def _plotTotalNumberOfJobs( self, startTime, endTime, condDict, groupingFields, filename ):
-    selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, SUM(%s)/SUM(%s)",
-                     groupingFields + [ 'startTime', 'bucketLength',
-                                    'Jobs', 'entriesInBucket'
-                                   ]
-                   )
-    retVal = self._getTypeData( startTime,
-                                endTime,
-                                selectFields,
-                                condDict,
-                                groupingFields,
-                                {} )
-    if not retVal[ 'OK' ]:
-      return retVal
-    dataDict, granularity = retVal[ 'Value' ]
-    self.stripDataField( dataDict, 0 )
-    dataDict = self._acumulate( granularity, startTime, endTime, dataDict )
-    gLogger.info( "Generating plot", "%s with granularity of %s" % ( filename, granularity ) )
-    metadata = { 'title' : 'Total Jobs by %s' % " -> ".join( groupingFields ) ,
-                 'starttime' : startTime,
-                 'endtime' : endTime,
-                 'span' : granularity,
-                 'ylabel' : "jobs",
-                 'is_cumulative' : True }
-    return generateCumulativePlot( filename, dataDict, metadata )
-
   def _plotNumberOfJobs( self, startTime, endTime, condDict, groupingFields, filename ):
-    selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, SUM(%s)/SUM(%s)",
+    selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, SUM(%s)/(SUM(%s)/COUNT(%s))",
                      groupingFields + [ 'startTime', 'bucketLength',
-                                    'Jobs', 'entriesInBucket'
+                                    'Jobs', 'entriesInBucket', 'entriesInBucket'
                                    ]
                    )
     retVal = self._getTypeData( startTime,
