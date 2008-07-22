49f519e80e99bda35cf53b3d857ead7a89a80274
Adria Casajus <adria@ecm.ub.es>
2008-07-22 13:55:50 +0000

    Added functions to get info from proxies
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@10856 4525493e-7705-40b1-a816-d608a930855b

--- Core/Security/Misc.py
+++ Core/Security/Misc.py
@@ -1,11 +1,31 @@
 import os
+import types
 from DIRAC import S_OK, S_ERROR
 from DIRAC.Core.Security.X509Chain import X509Chain
 from DIRAC.Core.Security.VOMS import VOMS
 from DIRAC.Core.Security import Locations, CS
 
-def getProxyInfo( proxyLoc = False, showUsername = False ):
 
+def getProxyInfo( proxyLoc = False, disableVOMS = False ):
+  """
+  Returns a dict with all the proxy info
+  * values that will be there always
+   'path' : path to the file,
+   'chain' : chain object containing the proxy
+   'subject' : subject of the proxy
+   'issuer' : issuer of the proxy
+   'isProxy' : bool
+   'isLimitedProxy' : bool
+   'validDN' : Valid DN in DIRAC
+   'validGroup' : Valid Group in DIRAC
+   'secondsLeft' : Seconds left
+  * values that can be there
+   'group' : DIRAC group
+   'username' : DIRAC username
+   'identity' : DN that generated the proxy
+   'hostname' : DIRAC host nickname
+   'VOMS'
+  """
   if not proxyLoc:
     proxyLoc = Locations.getProxyLocation()
 
@@ -17,78 +37,45 @@ def getProxyInfo( proxyLoc = False, showUsername = False ):
   if not retVal[ 'OK' ]:
     return S_ERROR( "Can't load %s: %s" % ( proxyLoc, retVal[ 'Message' ] ) )
 
-  info = chain.getInfoAsString()['Value']
-  info += "\npath        : %s\n" % proxyLoc
-  if showUsername:
-    retVal = chain.getIssuerCert()
-    if not retVal[ 'OK' ]:
-      dn = ""
-    else:
-      dn = retVal[ 'Value' ].getSubjectDN()[ 'Value' ]
-    retVal = CS.getUsernameForDN( dn )
-    if not retVal[ 'OK' ]:
-      info += "username    : <unknown>\n"
-    else:
-      info += "username    : %s\n" % retVal[ 'Value' ]
-  if chain.isVOMS()['Value']:
-    info += "extra       : Contains voms extensions\n"
-    voms = VOMS()
-    retVal = voms.getVOMSAttributes( proxyLoc )
-    if retVal[ 'OK' ]:
-      for entry in retVal[ 'Value' ]:
-        info += " voms data  : %s\n" % entry
-    else:
-      info += " Can't decode voms data (%s)\n" % retVal[ 'Message' ]
-  return S_OK( info )
-
-def Proxy( proxyLoc = False ):
-
-
-  if not proxyLoc:
-    proxyLoc = Locations.getProxyLocation()
-  result = S_OK(proxyLoc)
-
-  if not proxyLoc:
-    return S_ERROR( "Can't find valid proxy" )
-
-  chain = X509Chain()
-  retVal = chain.loadProxyFromFile( proxyLoc )
-  if not retVal[ 'OK' ]:
-    return S_ERROR( "Can't load %s: %s" % ( proxyLoc, retVal[ 'Message' ] ) )
-
-  retVal = chain.isValidProxy()
+  retVal = chain.getCredentials()
   if not retVal[ 'OK' ]:
     return retVal
 
-  result['Chain'] = chain
+  infoDict = retVal[ 'Value' ]
+  infoDict[ 'path' ] = proxyLoc
+  infoDict[ 'chain' ] = chain
 
-  retVal = chain.getIssuerCert()
-  if not retVal[ 'OK' ]:
-    return S_ERROR( "Can't get DN: %s" % retVal[ 'Message' ] )
-  else:
-    result['OwnerDN'] = retVal[ 'Value' ].getSubjectDN()[ 'Value' ]
-
-  retVal = chain.getDIRACGroup()
-  if not retVal[ 'OK' ]:
-    return S_ERROR( "Can't get DN: %s" % retVal[ 'Message' ] )
-  else:
-    result['Group'] = retVal[ 'Value' ]
-  
-  result['isVOMS'] = chain.isVOMS()['Value']
-
-  if result['isVOMS']:
-    voms = VOMS()
+  if not disableVOMS and chain.isVOMS()['Value']:
     retVal = voms.getVOMSAttributes( proxyLoc )
     if retVal[ 'OK' ]:
-      result['VOMSAttributes'] = retVal[ 'Value' ]
-    else:
-      return S_ERROR( "Can't decode voms data: %s" % retVal[ 'Message' ] )
+      infoDict[ 'VOMS' ] = retVal[ 'Value' ]
 
-  retVal = CS.getUsernameForDN( result['OwnerDN'] )
+  return S_OK( infoDict )
+
+def getProxyInfoAsString( proxyLoc = False, disableVOMS = False ):
+  retVal = getProxyInfo( proxyLoc, disableVOMS )
   if not retVal[ 'OK' ]:
-    result['Owner'] = 'unknown'
-  else:
-    result['Owner'] = retVal[ 'Value' ]
-
-  return result
-  
+    return retVal
+  infoDict = retVal[ 'Value' ]
+  leftAlign = 13
+  contentList = []
+  for field in ( 'subject', 'issuer', 'identity', ( 'secondsLeft', 'time left' ),
+                 ( 'group', 'DIRAC group' ), 'path', 'username', ( 'VOMS', 'VOMS fqan' ) ):
+    if type( field ) == types.StringType:
+      dispField = field
+    else:
+      dispField = field[1]
+      field = field[0]
+    if not field in infoDict:
+      continue
+    if field == 'secondsLeft':
+      secs = infoDict[ field ]
+      hours = int( secs /  3600 )
+      secs -= hours * 3600
+      mins = int( secs / 60 )
+      secs -= mins * 60
+      value = "%02d:%02d:%02d" % ( hours, mins, secs )
+    else:
+      value = infoDict[ field ]
+    contentList.append( "%s: %s" % ( dispField.ljust( leftAlign ), value ) )
+  return S_OK( "\n".join( contentList ) )
--- Core/Security/X509Chain.py
+++ Core/Security/X509Chain.py
@@ -1,9 +1,9 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Security/X509Chain.py,v 1.21 2008/07/01 16:20:51 acasajus Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/Security/X509Chain.py,v 1.22 2008/07/22 13:55:50 acasajus Exp $
 ########################################################################
 """ X509Chain is a class for managing X509 chains with their Pkeys
 """
-__RCSID__ = "$Id: X509Chain.py,v 1.21 2008/07/01 16:20:51 acasajus Exp $"
+__RCSID__ = "$Id: X509Chain.py,v 1.22 2008/07/22 13:55:50 acasajus Exp $"
 
 import types
 import os
@@ -546,31 +546,12 @@ class X509Chain:
   def __repr__(self):
     return self.__str__()
 
-  def getInfoAsString(self):
-    if not self.__loadedChain:
-      return S_ERROR( "No chain loaded" )
-    info = ""
-    lastCert = self.getCertInChain()['Value']
-    mainCert = self.getIssuerCert()['Value']
-    info += "subject     : %s\n" % lastCert.getSubjectDN()[ 'Value' ]
-    info += "issuer      : %s\n" % lastCert.getIssuerDN()[ 'Value' ]
-    info += "identity    : %s\n" % mainCert.getSubjectDN()[ 'Value' ]
-    group = self.getDIRACGroup()[ 'Value' ]
-    if not group:
-      group = '<not set>'
-    info +=  "DIRAC Group : %s\n" % group
-    secs = self.getRemainingSecs()[ 'Value' ]
-    hours = int( secs /  3600 )
-    secs -= hours * 3600
-    mins = int( secs / 60 )
-    secs -= mins * 60
-    info += "time left   : %02d:%02d:%02d" % ( hours, mins, secs )
-    return S_OK( info )
-
-  def getCredentials(self):
+  def getCredentials( self ):
     if not self.__loadedChain:
       return S_ERROR( "No chain loaded" )
     credDict = { 'subject' : self.__certList[0].get_subject().one_line(),
+                 'issuer' : self.__certList[0].get_issuer().one_line(),
+                 'secondsLeft' : self.getRemainingSecs()[ 'Value' ],
                  'isProxy' : self.__isProxy,
                  'isLimitedProxy' : self.__isProxy and self.__isLimitedProxy,
                  'validDN' : False,
