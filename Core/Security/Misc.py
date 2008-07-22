cd6297cf68b5002bb0295d570c3510cd8dc26525
Ricardo Graciani <graciani@ecm.ub.es>
2008-07-22 07:24:11 +0000

    New Proxy Function, with ProxyLocation, chain, ownerDN, Group, Nickname,... to replace GridCredentials
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@10834 4525493e-7705-40b1-a816-d608a930855b

--- Core/Security/Misc.py
+++ Core/Security/Misc.py
@@ -40,3 +40,55 @@ def getProxyInfo( proxyLoc = False, showUsername = False ):
     else:
       info += " Can't decode voms data (%s)\n" % retVal[ 'Message' ]
   return S_OK( info )
+
+def Proxy( proxyLoc = False ):
+
+
+  if not proxyLoc:
+    proxyLoc = Locations.getProxyLocation()
+  result = S_OK(proxyLoc)
+
+  if not proxyLoc:
+    return S_ERROR( "Can't find valid proxy" )
+
+  chain = X509Chain()
+  retVal = chain.loadProxyFromFile( proxyLoc )
+  if not retVal[ 'OK' ]:
+    return S_ERROR( "Can't load %s: %s" % ( proxyLoc, retVal[ 'Message' ] ) )
+
+  retVal = chain.isValidProxy()
+  if not retVal[ 'OK' ]:
+    return retVal
+
+  result['Chain'] = chain
+
+  retVal = chain.getIssuerCert()
+  if not retVal[ 'OK' ]:
+    return S_ERROR( "Can't get DN: %s" % retVal[ 'Message' ] )
+  else:
+    result['OwnerDN'] = retVal[ 'Value' ].getSubjectDN()[ 'Value' ]
+
+  retVal = chain.getDIRACGroup()
+  if not retVal[ 'OK' ]:
+    return S_ERROR( "Can't get DN: %s" % retVal[ 'Message' ] )
+  else:
+    result['Group'] = retVal[ 'Value' ]
+  
+  result['isVOMS'] = chain.isVOMS()['Value']
+
+  if result['isVOMS']:
+    voms = VOMS()
+    retVal = voms.getVOMSAttributes( proxyLoc )
+    if retVal[ 'OK' ]:
+      result['VOMSAttributes'] = retVal[ 'Value' ]
+    else:
+      return S_ERROR( "Can't decode voms data: %s" % retVal[ 'Message' ] )
+
+  retVal = CS.getUsernameForDN( result['OwnerDN'] )
+  if not retVal[ 'OK' ]:
+    result['Owner'] = 'unknown'
+  else:
+    result['Owner'] = retVal[ 'Value' ]
+
+  return result
+  
