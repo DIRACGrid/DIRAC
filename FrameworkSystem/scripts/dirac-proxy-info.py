963a9b9a7e30199be384bf6ae89950907b25eafb
Adria Casajus <adria@ecm.ub.es>
2008-07-22 13:56:46 +0000

    update to new functions
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@10857 4525493e-7705-40b1-a816-d608a930855b

--- FrameworkSystem/scripts/dirac-proxy-info.py
+++ FrameworkSystem/scripts/dirac-proxy-info.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/FrameworkSystem/scripts/dirac-proxy-info.py,v 1.2 2008/07/09 10:18:50 rgracian Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/FrameworkSystem/scripts/dirac-proxy-info.py,v 1.3 2008/07/22 13:56:20 acasajus Exp $
 # File :   dirac-proxy-init.py
 # Author : Adrian Casajus
 ########################################################################
-__RCSID__   = "$Id: dirac-proxy-info.py,v 1.2 2008/07/09 10:18:50 rgracian Exp $"
-__VERSION__ = "$Revision: 1.2 $"
+__RCSID__   = "$Id: dirac-proxy-info.py,v 1.3 2008/07/22 13:56:20 acasajus Exp $"
+__VERSION__ = "$Revision: 1.3 $"
 
 import sys
 import os.path
@@ -17,7 +17,8 @@ class Params:
 
   proxyLoc = False
   checkExists = False
-  showUsername = False
+  vomsEnabled = True
+  csEnabled = True
 
   def showVersion( self, arg ):
     print "Version:"
@@ -33,29 +34,34 @@ class Params:
   def checkExists( self, arg ):
     self.checkExists = True
     return DIRAC.S_OK()
-  
-  def enableShowUsername( self, arg ):
-    self.showUsername = True
+
+  def disableVOMS( self, arg ):
+    self.vomsEnabled = False
+    return DIRAC.S_OK()
+
+  def disableCS( self, arg ):
+    self.csEnabled = False
     return DIRAC.S_OK()
 
 params = Params()
 
 Script.registerSwitch( "f:", "file=", "File to use as user key", params.setProxyLocation )
 Script.registerSwitch( "i", "version", "Print version", params.showVersion )
-Script.registerSwitch( "u", "username", "Show username if possible", params.enableShowUsername )
+Script.registerSwitch( "n", "novoms", "Disable VOMS", params.disableVOMS )
+Script.registerSwitch( "x", "nocs", "Disable CS", params.disableCS )
 
 
 Script.disableCS()
 Script.parseCommandLine()
 
-if params.showUsername:
+if params.csEnabled:
   retVal = Script.enableCS()
   if not retVal[ 'OK' ]:
     print "Cannot contact CS to get user list"
 
-from DIRAC.Core.Security.Misc import getProxyInfo
+from DIRAC.Core.Security.Misc import getProxyInfoAsString
 
-result = getProxyInfo( params.proxyLoc, params.showUsername )
+result = getProxyInfoAsString( params.proxyLoc, not params.vomsEnabled )
 if not result[ 'OK' ]:
   print "Error: %s" % result[ 'Message' ]
 print result[ 'Value' ],
