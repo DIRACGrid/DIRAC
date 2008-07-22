80bde0b34ab632c4d3f78a417921fa52b84b907c
Adria Casajus <adria@ecm.ub.es>
2008-07-22 18:36:43 +0000

    Code in a function so it can be imported
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@10876 4525493e-7705-40b1-a816-d608a930855b

--- FrameworkSystem/scripts/dirac-proxy-init.py
+++ FrameworkSystem/scripts/dirac-proxy-init.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/FrameworkSystem/scripts/dirac-proxy-init.py,v 1.2 2008/07/09 12:43:12 rgracian Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/FrameworkSystem/scripts/dirac-proxy-init.py,v 1.3 2008/07/22 18:36:43 acasajus Exp $
 # File :   dirac-proxy-init.py
 # Author : Adrian Casajus
 ########################################################################
-__RCSID__   = "$Id: dirac-proxy-init.py,v 1.2 2008/07/09 12:43:12 rgracian Exp $"
-__VERSION__ = "$Revision: 1.2 $"
+__RCSID__   = "$Id: dirac-proxy-init.py,v 1.3 2008/07/22 18:36:43 acasajus Exp $"
+__VERSION__ = "$Revision: 1.3 $"
 
 import sys
 import getpass
 from DIRACEnvironment import DIRAC
 from DIRAC.Core.Base import Script
 
-class Params:
+class CLIParams:
 
   proxyLifeTime = 86400
   diracGroup = False
@@ -24,6 +24,7 @@ class Params:
   proxyLoc = False
   checkWithCS = True
   stdinPasswd = False
+  userPasswd = ""
 
   def setProxyLifeTime( self, arg ):
     try:
@@ -34,9 +35,24 @@ class Params:
       return DIRAC.S_ERROR( "Can't parse time argument" )
     return DIRAC.S_OK()
 
+  def setProxyRemainingSecs( self, arg ):
+    self.proxyLifeTime = int( arg )
+    return DIRAC.S_OK()
+
+  def getProxyLifeTime( self ):
+    hours = self.proxyLifeTime / 3600
+    mins = self.proxyLifeTime / 60 - hours * 60
+    return "%s:%s" % ( hours, mins )
+
+  def getProxyRemainingSecs( self ):
+    return self.proxyLifeTime
+
   def setDIRACGroup( self, arg ):
     self.diracGroup = arg
     return DIRAC.S_OK()
+
+  def getDIRACGroup( self ):
+    return self.diracGroup
 
   def setProxyStrength( self, arg ):
     try:
@@ -86,120 +102,122 @@ class Params:
     if self.debug:
       print msg
 
-params = Params()
+  def registerCLISwitches( self ):
+    Script.registerSwitch( "v:", "valid=", "Valid HH:MM for the proxy. By default is 24 hours", self.setProxyLifeTime )
+    Script.registerSwitch( "g:", "group=", "DIRAC Group to embed in the proxy", self.setDIRACGroup )
+    Script.registerSwitch( "b:", "strength=", "Set the proxy strength in bytes", self.setProxyStrength )
+    Script.registerSwitch( "l", "limited", "Generate a limited proxy", self.setProxyLimited )
+    Script.registerSwitch( "d", "debug", "Enable debug output", self.setDebug )
+    Script.registerSwitch( "c:", "cert=", "File to use as user certificate", self.setCertLocation )
+    Script.registerSwitch( "k:", "key=", "File to use as user key", self.setKeyLocation )
+    Script.registerSwitch( "u:", "out=", "File to write as proxy", self.setProxyLocation )
+    Script.registerSwitch( "x", "nocs", "Disable CS check", self.setDisableCSCheck )
+    Script.registerSwitch( "p", "pwstdin", "Get passwd from stdin", self.setStdinPasswd )
+    Script.registerSwitch( "i", "version", "Print version", self.showVersion )
+    Script.addDefaultOptionValue( "LogLevel", "always" )
 
-Script.registerSwitch( "v:", "valid=", "Valid HH:MM for the proxy. By default is 24 hours", params.setProxyLifeTime )
-Script.registerSwitch( "g:", "group=", "DIRAC Group to embed in the proxy", params.setDIRACGroup )
-Script.registerSwitch( "b:", "strength=", "Set the proxy strength in bytes", params.setProxyStrength )
-Script.registerSwitch( "l", "limited", "Generate a limited proxy", params.setProxyLimited )
-Script.registerSwitch( "d", "debug", "Enable debug output", params.setDebug )
-Script.registerSwitch( "c:", "cert=", "File to use as user certificate", params.setCertLocation )
-Script.registerSwitch( "k:", "key=", "File to use as user key", params.setKeyLocation )
-Script.registerSwitch( "u:", "out=", "File to write as proxy", params.setProxyLocation )
-Script.registerSwitch( "x", "nocs", "Disable CS check", params.setDisableCSCheck )
-Script.registerSwitch( "p", "pwstdin", "Get passwd from stdin", params.setStdinPasswd )
-Script.registerSwitch( "i", "version", "Print version", params.showVersion )
-Script.addDefaultOptionValue( "LogLevel", "always" )
-
-Script.disableCS()
-Script.parseCommandLine()
-
+from DIRAC import S_OK, S_ERROR
 from DIRAC.Core.Security.X509Chain import X509Chain
 from DIRAC.Core.Security import Locations, CS
 
-certLoc = params.certLoc
-keyLoc = params.keyLoc
-if not certLoc or not keyLoc:
-  cakLoc = Locations.getCertificateAndKeyLocation()
-  if not cakLoc:
-    print "Can't find user certificate and key"
-    sys.exit(1)
-  if not certLoc:
-    certLoc = cakLoc[0]
-  if not keyLoc:
-    keyLoc = cakLoc[1]
+def generateProxy( params ):
+  certLoc = params.certLoc
+  keyLoc = params.keyLoc
+  if not certLoc or not keyLoc:
+    cakLoc = Locations.getCertificateAndKeyLocation()
+    if not cakLoc:
+      return S_ERROR( "Can't find user certificate and key" )
+    if not certLoc:
+      certLoc = cakLoc[0]
+    if not keyLoc:
+      keyLoc = cakLoc[1]
 
-userPasswd = ""
-testChain = X509Chain()
-retVal = testChain.loadKeyFromFile(keyLoc, password = userPasswd )
-if not retVal[ 'OK' ]:
-  passwdPrompt = "Enter Certificate password:"
-  if params.stdinPasswd:
-    userPasswd = sys.stdin.readline().strip("\n")
-  else:
-    userPasswd = getpass.getpass( passwdPrompt )
+  testChain = X509Chain()
+  retVal = testChain.loadKeyFromFile(keyLoc, password = params.userPasswd )
+  if not retVal[ 'OK' ]:
+    passwdPrompt = "Enter Certificate password:"
+    if params.stdinPasswd:
+      userPasswd = sys.stdin.readline().strip("\n")
+    else:
+      userPasswd = getpass.getpass( passwdPrompt )
+    params.userPasswd = userPasswd
 
-proxyLoc = params.proxyLoc
-if not proxyLoc:
-  proxyLoc = Locations.getDefaultProxyLocation()
+  proxyLoc = params.proxyLoc
+  if not proxyLoc:
+    proxyLoc = Locations.getDefaultProxyLocation()
 
-if params.debug:
-  h = int( params.proxyLifeTime / 3600 )
-  m = int( params.proxyLifeTime / 60 )- h * 60
-  print "Proxy lifetime will be %02d:%02d" % ( h, m )
-  print "User cert is %s" % certLoc
-  print "User key  is %s" % keyLoc
-  print "Proxy will be written to %s" % proxyLoc
-  if params.diracGroup:
-    print "DIRAC Group will be set to %s" % params.diracGroup
-  else:
-    print "No DIRAC Group will be set"
-  print "Proxy strength will be %s" % params.proxyStrength
-  if params.limitedProxy:
-    print "Proxy will be limited"
+  if params.debug:
+    h = int( params.proxyLifeTime / 3600 )
+    m = int( params.proxyLifeTime / 60 )- h * 60
+    print "Proxy lifetime will be %02d:%02d" % ( h, m )
+    print "User cert is %s" % certLoc
+    print "User key  is %s" % keyLoc
+    print "Proxy will be written to %s" % proxyLoc
+    if params.diracGroup:
+      print "DIRAC Group will be set to %s" % params.diracGroup
+    else:
+      print "No DIRAC Group will be set"
+    print "Proxy strength will be %s" % params.proxyStrength
+    if params.limitedProxy:
+      print "Proxy will be limited"
 
-chain = X509Chain()
-#Load user cert and key
-retVal = chain.loadChainFromFile( certLoc )
-if not retVal[ 'OK' ]:
-  print "Can't load %s" % certLoc
-  sys.exit(1)
-retVal = chain.loadKeyFromFile( keyLoc, password = userPasswd )
-if not retVal[ 'OK' ]:
-  print "Can't load %s" % keyLoc
-  sys.exit(1)
+  chain = X509Chain()
+  #Load user cert and key
+  retVal = chain.loadChainFromFile( certLoc )
+  if not retVal[ 'OK' ]:
+    return S_ERROR( "Can't load %s" % certLoc )
+  retVal = chain.loadKeyFromFile( keyLoc, password = params.userPasswd )
+  if not retVal[ 'OK' ]:
+    return S_ERROR( "Can't load %s" % keyLoc )
 
-if params.checkWithCS and params.diracGroup:
+  if params.checkWithCS and params.diracGroup:
+    retVal = chain.generateProxyToFile( proxyLoc,
+                                        params.proxyLifeTime,
+                                        strength =params.proxyStrength,
+                                        limited = params.limitedProxy )
+
+    params.debugMsg( "Contacting CS..." )
+
+    retVal = Script.enableCS()
+    if not retVal[ 'OK' ]:
+      return S_ERROR( "Can't contact DIRAC CS: %s" % retVal[ 'Message' ] )
+    if not params.diracGroup:
+      params.diracGroup = CS.getDefaultUserGroup()
+    userDN = chain.getCertInChain( -1 )['Value'].getSubjectDN()['Value']
+    params.debugMsg( "Checking DN %s" % userDN )
+    retVal = CS.getUsernameForDN( userDN )
+    if not retVal[ 'OK' ]:
+      return S_ERROR( "DN %s is not registered" % userDN )
+    username = retVal[ 'Value' ]
+    params.debugMsg( "Username is %s" % username )
+    retVal = CS.getGroupsForUser( username )
+    if not retVal[ 'OK' ]:
+      return S_ERROR( "User %s has no groups defined" % username )
+    groups = retVal[ 'Value' ]
+    if params.diracGroup not in groups:
+      return S_ERROR( "Requested group %s is not valid for user %s" % ( params.diracGroup, username ) )
+    params.debugMsg( "Creating proxy for %s@%s (%s)" % ( username, params.diracGroup, userDN ) )
+
   retVal = chain.generateProxyToFile( proxyLoc,
                                       params.proxyLifeTime,
+                                      params.diracGroup,
                                       strength =params.proxyStrength,
                                       limited = params.limitedProxy )
 
-  params.debugMsg( "Contacting CS..." )
-
-  retVal = Script.enableCS()
   if not retVal[ 'OK' ]:
-    print "Can't contact DIRAC CS: %s" % retVal[ 'Message' ]
-    print "Aborting..."
-    sys.exit(1)
-  if not params.diracGroup:
-    params.diracGroup = CS.getDefaultUserGroup()
-  userDN = chain.getCertInChain( -1 )['Value'].getSubjectDN()['Value']
-  params.debugMsg( "Checking DN %s" % userDN )
-  retVal = CS.getUsernameForDN( userDN )
+    return S_ERROR( "Couldn't generate proxy: %s" % retVal[ 'Message' ] )
+  return S_OK( proxyLoc )
+
+
+if __name__ == "__main__":
+  cliParams = CLIParams()
+  cliParams.registerCLISwitches()
+
+  Script.disableCS()
+  Script.parseCommandLine()
+
+  retVal = generateProxy( cliParams )
   if not retVal[ 'OK' ]:
-    print "DN %s is not registered" % userDN
+    print retVal[ 'Message' ]
     sys.exit(1)
-  username = retVal[ 'Value' ]
-  params.debugMsg( "Username is %s" % username )
-  retVal = CS.getGroupsForUser( username )
-  if not retVal[ 'OK' ]:
-    print "User %s has no groups defined" % username
-    sys.exit(1)
-  groups = retVal[ 'Value' ]
-  if params.diracGroup not in groups:
-    print "Requested group %s is not valid for user %s" % ( params.diracGroup, username )
-    sys.exit(1)
-  params.debugMsg( "Creating proxy for %s@%s (%s)" % ( username, params.diracGroup, userDN ) )
-
-retVal = chain.generateProxyToFile( proxyLoc,
-                                    params.proxyLifeTime,
-                                    params.diracGroup,
-                                    strength =params.proxyStrength,
-                                    limited = params.limitedProxy )
-
-if not retVal[ 'OK' ]:
-  print "Couldn't generate proxy: %s" % retVal[ 'Message' ]
-  sys.exit(1)
-sys.exit(0)
-
+  sys.exit(0)
