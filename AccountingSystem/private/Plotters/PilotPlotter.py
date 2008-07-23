569cc9f5dafc588c934e7613e738695af7d5cd80
Adria Casajus <adria@ecm.ub.es>
2008-07-23 14:27:58 +0000

    fix
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@10920 4525493e-7705-40b1-a816-d608a930855b

--- AccountingSystem/private/Plotters/PilotPlotter.py
+++ AccountingSystem/private/Plotters/PilotPlotter.py
@@ -10,6 +10,32 @@ class PilotPlotter(BasePlotter):
   _typeName = "Pilot"
   _typeKeyFields = [ dF[0] for dF in Pilot().definitionKeyFields ]
 
+  def _plotTotalNumberOfJobs( self, startTime, endTime, condDict, groupingFields, filename ):
+    selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, SUM(%s)",
+                     groupingFields + [ 'startTime', 'bucketLength',
+                                    'Jobs'
+                                   ]
+                   )
+    retVal = self._getTypeData( startTime,
+                                endTime,
+                                selectFields,
+                                condDict,
+                                groupingFields,
+                                {} )
+    if not retVal[ 'OK' ]:
+      return retVal
+    dataDict, granularity = retVal[ 'Value' ]
+    self.stripDataField( dataDict, 0 )
+    dataDict = self._acumulate( granularity, startTime, endTime, dataDict )
+    gLogger.info( "Generating plot", "%s with granularity of %s" % ( filename, granularity ) )
+    metadata = { 'title' : 'Total Jobs by %s' % " -> ".join( groupingFields ) ,
+                 'starttime' : startTime,
+                 'endtime' : endTime,
+                 'span' : granularity,
+                 'ylabel' : "jobs",
+                 'is_cumulative' : True }
+    return generateCumulativePlot( filename, dataDict, metadata )
+
   def _plotNumberOfJobs( self, startTime, endTime, condDict, groupingFields, filename ):
     selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, SUM(%s)",
                      groupingFields + [ 'startTime', 'bucketLength',
@@ -34,6 +60,32 @@ class PilotPlotter(BasePlotter):
                  'span' : granularity,
                  'ylabel' : "jobs"  }
     return generateTimedStackedBarPlot( filename, dataDict, metadata )
+
+  def _plotTotalNumberOfPilots( self, startTime, endTime, condDict, groupingFields, filename ):
+    selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, SUM(%s)",
+                     groupingFields + [ 'startTime', 'bucketLength',
+                                    'entriesInBucket'
+                                   ]
+                   )
+    retVal = self._getTypeData( startTime,
+                                endTime,
+                                selectFields,
+                                condDict,
+                                groupingFields,
+                                {} )
+    if not retVal[ 'OK' ]:
+      return retVal
+    dataDict, granularity = retVal[ 'Value' ]
+    self.stripDataField( dataDict, 0 )
+    dataDict = self._acumulate( granularity, startTime, endTime, dataDict )
+    gLogger.info( "Generating plot", "%s with granularity of %s" % ( filename, granularity ) )
+    metadata = { 'title' : 'Total Pilots by %s' % " -> ".join( groupingFields ) ,
+                 'starttime' : startTime,
+                 'endtime' : endTime,
+                 'span' : granularity,
+                 'ylabel' : "pilots",
+                 'is_cumulative' : True }
+    return generateCumulativePlot( filename, dataDict, metadata )
 
   def _plotNumberOfPilots( self, startTime, endTime, condDict, groupingFields, filename ):
     selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, SUM(%s)",
