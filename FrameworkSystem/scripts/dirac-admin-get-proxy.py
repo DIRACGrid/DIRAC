2df6ddfa9a03e85875ea062c335a4a3507ed4d99
Adria Casajus <adria@ecm.ub.es>
2008-07-24 10:56:48 +0000

    all proxy downloading in one script
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@10952 4525493e-7705-40b1-a816-d608a930855b

--- FrameworkSystem/scripts/dirac-admin-get-proxy.py
+++ FrameworkSystem/scripts/dirac-admin-get-proxy.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/FrameworkSystem/scripts/dirac-admin-get-proxy.py,v 1.1 2008/07/23 13:37:10 acasajus Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/FrameworkSystem/scripts/dirac-admin-get-proxy.py,v 1.2 2008/07/24 10:56:48 acasajus Exp $
 # File :   dirac-admin-get-proxy
 # Author : Stuart Paterson
 ########################################################################
-__RCSID__   = "$Id: dirac-admin-get-proxy.py,v 1.1 2008/07/23 13:37:10 acasajus Exp $"
-__VERSION__ = "$Revision: 1.1 $"
+__RCSID__   = "$Id: dirac-admin-get-proxy.py,v 1.2 2008/07/24 10:56:48 acasajus Exp $"
+__VERSION__ = "$Revision: 1.2 $"
 import os
 from DIRACEnvironment import DIRAC
 from DIRAC.Core.Base import Script
@@ -17,6 +17,8 @@ class Params:
   limited = False
   proxyPath = False
   proxyLifeTime = 86400
+  enableVOMS = False
+  vomsAttr = False
 
   def setLimited( self, args ):
     self.limited = True
@@ -35,10 +37,21 @@ class Params:
       return DIRAC.S_ERROR( "Can't parse time argument" )
     return DIRAC.S_OK()
 
+  def automaticVOMS( self, arg ):
+    self.enableVOMS = True
+    return DIRAC.S_OK()
+
+  def setVOMSAttr( self, arg ):
+    self.enableVOMS = True
+    self.vomsAttr = arg
+    return DIRAC.S_OK()
+
   def registerCLISwitches( self ):
     Script.registerSwitch( "v:", "valid=", "Valid HH:MM for the proxy. By default is 24 hours", self.setProxyLifeTime )
     Script.registerSwitch( "l", "limited", "Get a limited proxy", self.setLimited )
     Script.registerSwitch( "u:", "out=", "File to write as proxy", self.setProxyLocation )
+    Script.registerSwitch( "a", "voms", "Get proxy with VOMS extension mapped to the DIRAC group", self.automaticVOMS )
+    Script.registerSwitch( "m:", "vomsAttr=", "VOMS attribute to require", self.setVOMSAttr )
 
 params = Params()
 params.registerCLISwitches()
@@ -47,29 +60,47 @@ Script.parseCommandLine( ignoreErrors = True )
 args = Script.getPositionalArgs()
 
 def usage():
-  print 'Usage: %s <DN> <DIRAC group>' %(Script.scriptName)
+  print 'Usage: %s <DN>/<username> <DIRAC group>' %( Script.scriptName )
+  print " <username> will fail if there's more than one DN registered for that username"
   DIRAC.exit(2)
 
 if len(args) != 2:
   usage()
 
-try:
-  userDN = str(args[0])
-  userGroup = str(args[1])
-except Exception,x:
-  print 'Expected strings for DN and proxy group', args[0], args[1]
-  DIRAC.exit(2)
+userGroup = str(args[1])
+userDN = str(args[0])
+userName = False
+if userDN.find( "/" ) != 0:
+  userName = userDN
+  retVal = CS.getDNForUsername( userName )
+  if not retVal[ 'OK' ]:
+    print "Cannot discover DN for username %s" % ( userName, retVal[ 'Value' ] )
+    DIRAC.exit(2)
+  DNList = retVal[ 'Value' ]
+  if len( DNList ) > 1:
+    print "Username %s has more than one DN registered" % userName
+    for dn in DNList:
+      print " %s" % dn
+    print "Which dn do you want to download?"
+    DIRAC.exit(2)
+  userDN = DNList[0]
 
 if not params.proxyPath:
-  result = CS.getUsernameForDN( userDN )
-  if not result[ 'OK' ]:
-    print "DN '%s' is not registered in DIRAC" % userDN
-    DIRAC.exit(2)
-  userName = result[ 'Value' ]
+  if not userName:
+    result = CS.getUsernameForDN( userDN )
+    if not result[ 'OK' ]:
+      print "DN '%s' is not registered in DIRAC" % userDN
+      DIRAC.exit(2)
+    userName = result[ 'Value' ]
   params.proxyPath = "%s/proxy.%s.%s" % ( os.getcwd(), userName, userGroup )
 
-
-result = gProxyManager.downloadProxy( userDN, userGroup, limited = params.limited, requiredTimeLeft = params.proxyLifeTime )
+if params.enableVOMS:
+  result = gProxyManager.downloadVOMSProxy( userDN, userGroup, limited = params.limited,
+                                            requiredTimeLeft = params.proxyLifeTime,
+                                            requiredVOMSAttribute = params.vomsAttr )
+else:
+  result = gProxyManager.downloadProxy( userDN, userGroup, limited = params.limited,
+                                        requiredTimeLeft = params.proxyLifeTime )
 if not result['OK']:
   print 'Proxy file cannot be retrieved: %s' % result['Message']
   DIRAC.exit(2)
