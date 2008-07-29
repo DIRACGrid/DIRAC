9bb3980b954644df22d5c24ea574ab8c5e287614
Adria Casajus <adria@ecm.ub.es>
2008-07-29 10:55:05 +0000

    Added jobs per pilot
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@11113 4525493e-7705-40b1-a816-d608a930855b

--- AccountingSystem/private/Plotters/PilotPlotter.py
+++ AccountingSystem/private/Plotters/PilotPlotter.py
@@ -9,7 +9,7 @@ class PilotPlotter(BasePlotter):
   _typeName = "Pilot"
   _typeKeyFields = [ dF[0] for dF in Pilot().definitionKeyFields ]
 
-  def _plotTotalNumberOfJobs( self, startTime, endTime, condDict, groupingFields, filename ):
+  def _plotCumulativeNumberOfJobs( self, startTime, endTime, condDict, groupingFields, filename ):
     selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, SUM(%s)",
                      groupingFields + [ 'startTime', 'bucketLength',
                                     'Jobs'
@@ -60,7 +60,7 @@ class PilotPlotter(BasePlotter):
                  'ylabel' : "jobs"  }
     return self._generateTimedStackedBarPlot( filename, dataDict, metadata )
 
-  def _plotTotalNumberOfPilots( self, startTime, endTime, condDict, groupingFields, filename ):
+  def _plotCumulativeNumberOfPilots( self, startTime, endTime, condDict, groupingFields, filename ):
     selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, SUM(%s)",
                      groupingFields + [ 'startTime', 'bucketLength',
                                     'entriesInBucket'
@@ -109,4 +109,55 @@ class PilotPlotter(BasePlotter):
                  'endtime' : endTime,
                  'span' : granularity,
                  'ylabel' : "pilots"  }
+    return self._generateTimedStackedBarPlot( filename, dataDict, metadata )
+
+  def _plotCumulativeJobsPerPilot( self, startTime, endTime, condDict, groupingFields, filename ):
+    selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, SUM(%s)/SUM(%s)",
+                     groupingFields + [ 'startTime', 'bucketLength',
+                                    'Jobs', 'entriesInBucket'
+                                   ]
+                   )
+    retVal = self._getTypeData( startTime,
+                                endTime,
+                                selectFields,
+                                condDict,
+                                groupingFields,
+                                { 'convertToGranularity' : 'average' }  )
+    if not retVal[ 'OK' ]:
+      return retVal
+    dataDict, granularity = retVal[ 'Value' ]
+    self.stripDataField( dataDict, 0 )
+    dataDict = self._acumulate( granularity, startTime, endTime, dataDict )
+    gLogger.info( "Generating plot", "%s with granularity of %s" % ( filename, granularity ) )
+    metadata = { 'title' : 'Total Jobs per pilot by %s' % " -> ".join( groupingFields ) ,
+                 'starttime' : startTime,
+                 'endtime' : endTime,
+                 'span' : granularity,
+                 'ylabel' : "jobs/pilot",
+                 'is_cumulative' : True }
+    return self._generateCumulativePlot( filename, dataDict, metadata )
+
+  def _plotJobsPerPilot( self, startTime, endTime, condDict, groupingFields, filename ):
+    selectFields = ( self._getSQLStringForGrouping( groupingFields) + ", %s, %s, SUM(%s)/SUM(%s)",
+                     groupingFields + [ 'startTime', 'bucketLength',
+                                    'Jobs', 'entriesInBucket'
+                                   ]
+                   )
+    retVal = self._getTypeData( startTime,
+                                endTime,
+                                selectFields,
+                                condDict,
+                                groupingFields,
+                                { 'convertToGranularity' : 'average' }  )
+    if not retVal[ 'OK' ]:
+      return retVal
+    dataDict, granularity = retVal[ 'Value' ]
+    self.stripDataField( dataDict, 0 )
+    dataDict = self._fillWithZero( granularity, startTime, endTime, dataDict )
+    gLogger.info( "Generating plot", "%s with granularity of %s" % ( filename, granularity ) )
+    metadata = { 'title' : 'Jobs per pilot by %s' % " -> ".join( groupingFields ) ,
+                 'starttime' : startTime,
+                 'endtime' : endTime,
+                 'span' : granularity,
+                 'ylabel' : "jobs/pilot" }
     return self._generateTimedStackedBarPlot( filename, dataDict, metadata )
