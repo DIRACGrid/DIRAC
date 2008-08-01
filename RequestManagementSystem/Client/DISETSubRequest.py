7fa71bb64d9afc25a3835b12b4f2b67b78bb30c0
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-08-01 17:20:55 +0000

    thanks to Adria the rpc stub contains all the information required to execute the RPC call. this means that we only need to provide the rpc stub which is serialised in the 'Arguments' subrequest attribute.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@11300 4525493e-7705-40b1-a816-d608a930855b

--- RequestManagementSystem/Client/DISETSubRequest.py
+++ RequestManagementSystem/Client/DISETSubRequest.py
@@ -1,11 +1,11 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/RequestManagementSystem/Client/DISETSubRequest.py,v 1.3 2008/06/12 15:09:51 atsareg Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/RequestManagementSystem/Client/DISETSubRequest.py,v 1.4 2008/08/01 17:20:55 acsmith Exp $
 """
    DISETSubRequest Class encapsulates a request definition to accomplish a DISET
    RPC call
 
 """
 
-__RCSID__ = "$Id: DISETSubRequest.py,v 1.3 2008/06/12 15:09:51 atsareg Exp $"
+__RCSID__ = "$Id: DISETSubRequest.py,v 1.4 2008/08/01 17:20:55 acsmith Exp $"
 
 import commands, datetime
 from DIRAC.Core.Utilities import DEncode
@@ -17,32 +17,25 @@ class DISETSubRequest:
   def __init__(self,rpcStub= None):
     """Instantiates the Workflow object and some default parameters.
     """
-
-    self.subAttributeNames = ['Status','SubRequestID','Method','Type','CreationTime','ExecutionTime',
-                              'TargetComponent','Call','Arguments']
+    self.subAttributeNames = ['Status','SubRequestID','Operation','CreationTime','LastUpdate','Arguments']
     self.subAttributes = {}
 
     for attr in self.subAttributeNames:
       self.subAttributes[attr] = "Unknown"
 
     # Some initial values
-    self.subAttributes['Status'] = "New"
+    self.subAttributes['Status'] = "Waiting"
     status,self.subAttributes['SubRequestID'] = commands.getstatusoutput('uuidgen')
-    self.subAttributes['Method'] = "DISET"
     self.subAttributes['CreationTime'] = str(datetime.datetime.utcnow())
-    self.subAttributes['Type'] = 'Unknown'
 
     if rpcStub:
-      self.subAttributes['TargetComponent'] = rpcStub[0]
-      self.subAttributes['Call'] = rpcStub[2]
-      self.subAttributes['Arguments'] = DEncode.encode(rpcStub[3])
+      self.subAttributes['Arguments'] = DEncode.encode(rpcStub)
 
-  def setRPCCall(self,rpcStub):
+  def setRPCStub(self,rpcStub):
     """ Define the  RPC call details
     """
-    self.subAttributes['TargetComponent'] = rpcStub[0]
-    self.subAttributes['Call'] = rpcStub[2]
-    self.subAttributes['Arguments'] = DEncode.encode(rpcStub[3])
+    self.subAttributes['Operation'] = rpcStub[0]
+    self.subAttributes['Arguments'] = DEncode.encode(rpcStub)
 
   def getDictionary(self):
     """ Get the request representation as a dictionary
@@ -50,20 +43,3 @@ class DISETSubRequest:
     resultDict = {}
     resultDict['Attributes'] = self.subAttributes
     return resultDict
-
-  def setTargetComponent(self,target):
-    """ Set the RPC call target component
-    """
-    self.subAttributes['TargetComponent'] = target
-
-  def setRPCCall(self,call):
-    """ Set the RPC call method name
-    """
-    self.subAttributes['Call'] = call
-
-  def setArguments(self,arguments):
-    """ Set the RPC call arguments
-    """
-    self.subAttributes['Arguments'] = DEncode.encode(arguments)
-
-
