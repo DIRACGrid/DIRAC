4c5c02a8b2ca8cb3d91342f7d81a79180903583f
Ricardo Graciani <graciani@ecm.ub.es>
2008-08-04 15:29:21 +0000

    updates
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@11316 4525493e-7705-40b1-a816-d608a930855b

--- RequestManagementSystem/Client/DISETSubRequest.py
+++ RequestManagementSystem/Client/DISETSubRequest.py
@@ -1,14 +1,15 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/RequestManagementSystem/Client/DISETSubRequest.py,v 1.4 2008/08/01 17:20:55 acsmith Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/RequestManagementSystem/Client/DISETSubRequest.py,v 1.5 2008/08/04 15:29:21 rgracian Exp $
 """
    DISETSubRequest Class encapsulates a request definition to accomplish a DISET
    RPC call
 
 """
 
-__RCSID__ = "$Id: DISETSubRequest.py,v 1.4 2008/08/01 17:20:55 acsmith Exp $"
+__RCSID__ = "$Id: DISETSubRequest.py,v 1.5 2008/08/04 15:29:21 rgracian Exp $"
 
-import commands, datetime
+import commands
 from DIRAC.Core.Utilities import DEncode
+from DIRAC import Time
 
 class DISETSubRequest:
 
@@ -26,7 +27,7 @@ class DISETSubRequest:
     # Some initial values
     self.subAttributes['Status'] = "Waiting"
     status,self.subAttributes['SubRequestID'] = commands.getstatusoutput('uuidgen')
-    self.subAttributes['CreationTime'] = str(datetime.datetime.utcnow())
+    self.subAttributes['CreationTime'] = Time.toString()
 
     if rpcStub:
       self.subAttributes['Arguments'] = DEncode.encode(rpcStub)
