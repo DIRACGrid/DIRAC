9efd691468964e6d19af814b98fa3772a3b72943
Ricardo Graciani <graciani@ecm.ub.es>
2008-08-12 17:32:43 +0000

    most of VOPolicy goes in the JobDB.insertNewJobToDB
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@11459 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/Agent/JobPolicyAgent.py
+++ WorkloadManagementSystem/Agent/JobPolicyAgent.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Agent/JobPolicyAgent.py,v 1.3 2008/01/29 15:33:09 paterson Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Agent/JobPolicyAgent.py,v 1.4 2008/08/12 17:32:43 rgracian Exp $
 # File :   JobPolicyAgent.py
 # Author : Stuart Paterson
 ########################################################################
@@ -9,7 +9,7 @@
 
 """
 
-__RCSID__ = "$Id: JobPolicyAgent.py,v 1.3 2008/01/29 15:33:09 paterson Exp $"
+__RCSID__ = "$Id: JobPolicyAgent.py,v 1.4 2008/08/12 17:32:43 rgracian Exp $"
 
 from DIRAC.WorkloadManagementSystem.Agent.Optimizer        import Optimizer
 from DIRAC.Core.Utilities.ClassAd.ClassAdLight             import ClassAd
@@ -35,59 +35,19 @@ class JobPolicyAgent(Optimizer):
     return result
 
   #############################################################################
-  def checkJob(self,job):
+  def checkJob( self, job, jdl = None, classad = None ):
     """This method controls the checking of the job.
     """
 
-    voPolicyDict = gConfig.getOptionsDict('/DIRAC/VOPolicy')
-    if not voPolicyDict['OK']:
-      self.log.warn('Could not obtain /DIRAC/VOPolicy from CS')
-      self.log.verbose(voPolicyDict)
-      return voPolicyDict
-
     self.log.info("Checking JDL for job: %s" %(job))
-    retVal = self.jobDB.getJobJDL(job)
-    if not retVal['OK']:
-      result = S_ERROR()
-      result['Message'] = "Job JDL not found in JobDB"
-      return result
 
-    jdl = retVal['Value']
-    if not jdl:
-      result = S_ERROR('Null JDL returned from JobDB')
-      return result
-
-    classadJob = ClassAd(jdl)
-    if not classadJob.isOK():
-      self.log.debug("Warning: illegal JDL for job %s, will be marked problematic" % (job))
-      result = S_ERROR()
-      result['Value'] = "Illegal Job JDL"
-      return result
-
-    voPolicy = voPolicyDict['Value']
-    jdlModified = False
-    for param,val in voPolicy.items():
-      if classadJob.lookupAttribute(param):
-        self.log.verbose('%s already exists in job %s JDL' %(param,job))
-      else:
-        jdlModified = True
-        self.log.verbose('Insterting default %s = %s in JDL for job %s' %(param,val,job))
-        classadJob.insertAttributeString(param,val)
-
-    if jdlModified:
-      self.log.verbose('Storing modified JDL for %s' %(job))
-      newJDL = classadJob.asJDL()
-      result = self.jobDB.setJobJDL(int(job),newJDL)
-      if not result['OK']:
-        self.log.warn('Problem storing modified job JDL')
-        self.log.warn(result)
-        return result
-
-    self.log.info('Processed job %s, JDL modified flag %s' %(job,jdlModified))
-
-    result = self.setNextOptimizer(job)
+    result = self.getJDLandClassad( job, jdl, classad )
     if not result['OK']:
-      self.log.warn(result['Message'])
-    return result
+      return result
+
+    JDL = result['JDL']
+    classadJob = result['Classad']
+    
+    return self.setNextOptimizer(job)
 
   #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
