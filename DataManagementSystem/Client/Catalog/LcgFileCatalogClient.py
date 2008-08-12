7ebe6d79565c11ac239c8ef49a468d4fe9822a75
Ricardo Graciani <graciani@ecm.ub.es>
2008-08-12 11:27:01 +0000

    fix
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@11440 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Client/Catalog/LcgFileCatalogClient.py
+++ DataManagementSystem/Client/Catalog/LcgFileCatalogClient.py
@@ -13,7 +13,7 @@ try:
   importCorrectly = True
   gLogger.debug("LcgFileCatalogClient.__init__: Successfully imported lfc module.")
 except ImportError, x:
-  gLogger.exception("LcgFileCatalogClient.__init__: Failed to import lfc module.",lException=x)
+  gLogger.exception("LcgFileCatalogClient.__init__: Failed to import lfc module.")
   importCorrectly = False
 
 class LcgFileCatalogClient(FileCatalogueBase):
