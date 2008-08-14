9b6f4459d691129e1011f42ae844cd6cdcc3e204
Ricardo Graciani <graciani@ecm.ub.es>
2008-08-14 13:49:46 +0000

    fix
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@11522 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/Agent/JobSchedulingAgent.py
+++ WorkloadManagementSystem/Agent/JobSchedulingAgent.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Agent/JobSchedulingAgent.py,v 1.37 2008/08/14 13:30:43 rgracian Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Agent/JobSchedulingAgent.py,v 1.38 2008/08/14 13:49:46 rgracian Exp $
 # File :   JobSchedulingAgent.py
 # Author : Stuart Paterson
 ########################################################################
@@ -14,7 +14,7 @@
       meaningfully.
 
 """
-__RCSID__ = "$Id: JobSchedulingAgent.py,v 1.37 2008/08/14 13:30:43 rgracian Exp $"
+__RCSID__ = "$Id: JobSchedulingAgent.py,v 1.38 2008/08/14 13:49:46 rgracian Exp $"
 
 from DIRAC.WorkloadManagementSystem.Agent.Optimizer        import Optimizer
 from DIRAC.Core.Utilities.ClassAd.ClassAdLight             import ClassAd
@@ -145,7 +145,7 @@ class JobSchedulingAgent(Optimizer):
       #Single site candidate chosen and staging required
       self.log.verbose('Job %s requires staging of input data' %(job))
 
-      siteCandidates = {'SiteCandidates':[destinationSites[0]]}
+      siteCandidates = {'SiteCandidates':{destinationSites[0]:{}}}
       result = self.setOptimizerJobInfo(job,self.dataAgentName,siteCandidates)
       if not result['OK']:
         return result
