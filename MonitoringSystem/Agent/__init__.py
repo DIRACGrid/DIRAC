e5fe3df0227fe552db2f80a0803921acc114fde2
Stuart K. Paterson <stuart.paterson@cern.ch>
2008-09-12 09:29:54 +0000

    Add Agents package.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@11971 4525493e-7705-40b1-a816-d608a930855b

---
+++ MonitoringSystem/Agent/__init__.py
@@ -0,0 +1,2 @@
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/MonitoringSystem/Agent/__init__.py,v 1.1 2008/09/12 09:29:54 paterson Exp $
+__RCSID__ = "$Id: __init__.py,v 1.1 2008/09/12 09:29:54 paterson Exp $"
