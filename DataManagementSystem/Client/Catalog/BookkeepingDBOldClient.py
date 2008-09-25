3c5a7ec3d553aa328f207824389583468b87f53b
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-09-25 16:09:59 +0000

    updated to removal methods.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@12283 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Client/Catalog/BookkeepingDBOldClient.py
+++ DataManagementSystem/Client/Catalog/BookkeepingDBOldClient.py
@@ -1,9 +1,9 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/DataManagementSystem/Client/Catalog/Attic/BookkeepingDBOldClient.py,v 1.3 2008/09/23 22:18:17 acsmith Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/DataManagementSystem/Client/Catalog/Attic/BookkeepingDBOldClient.py,v 1.4 2008/09/25 16:09:59 acsmith Exp $
 
 """ Client for the BookkeeppingDB file catalog old XML based service
 """
 
-__RCSID__ = "$Id: BookkeepingDBOldClient.py,v 1.3 2008/09/23 22:18:17 acsmith Exp $"
+__RCSID__ = "$Id: BookkeepingDBOldClient.py,v 1.4 2008/09/25 16:09:59 acsmith Exp $"
 
 from DIRAC  import gLogger, gConfig, S_OK, S_ERROR
 from DIRAC.Core.DISET.RPCClient import RPCClient
@@ -31,10 +31,51 @@ class BookkeepingDBOldClient(FileCatalogueBase):
   def isOK(self):
     return self.valid
 
+
+  def addFile(self,fileTuple):
+    """ A tuple should be supplied to this method which contains:
+        (lfn,pfn,size,se,guid,checksum)
+        A list of tuples may also be supplied.
+    """
+    successful = {}
+    failed = {}
+    if type(fileTuple) == types.TupleType:
+      files = [fileTuple]
+    elif type(fileTuple) == types.ListType:
+      files = fileTuple
+    else:
+      return S_ERROR('BookkeepingDBClient.addFile: Must supply a file tuple of list of tuples')
+    replicaTupleList = []
+    for lfn,pfn,size,se,guid,checksum in files:
+      replicaTupleList.append((lfn,pfn,se,'IGNORE'))
+    return self.addReplica(replicaTupleList)
+
+  def addReplica(self,replicaTuple):
+    """ This adds a replica to the catalogue
+        The tuple to be supplied is of the following form:
+          (lfn,pfn,se,master)
+        where master = True or False
+    """
+    if type(replicaTuple) == types.TupleType:
+      replicas = [replicaTuple]
+    elif type(replicaTuple) == types.ListType:
+      replicas = replicaTuple
+    else:
+      return S_ERROR('BookkeepingDBOldClient.addReplica: Must supply a replica tuple of list of tuples')
+    successful = {}
+    failed = {}
+    for lfn,pfn,se,master in replicas:
+      res = self.__addReplica(lfn,pfn,se)
+      if not res['OK']:
+        failed[lfn] = res['Message']
+      else:
+        successful[lfn] = True
+    resDict = {'Successful':successful,'Failed':failed}
+    return S_OK(resDict)
+
   def __addReplica(self,lfn,pfn,se):
     """ Add replica info to the Bookkeeping database"
     """
-
     repscript = """<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE Replicas SYSTEM "book.dtd">
 <Replicas>
@@ -45,69 +86,21 @@ class BookkeepingDBOldClient(FileCatalogueBase):
 </Replicas>
 """ % (lfn,pfn,se,se)
     fname = os.path.basename(lfn)+".A.xml"
-    #print "sending",fname
     server = RPCClient(self.url)
     result = server.sendBookkeeping(fname,repscript)
-
     return result
 
-  def addFile(self,fileTuple):
-    """ A tuple should be supplied to this method which contains:
-        (lfn,pfn,size,se,guid)
-        A list of tuples may also be supplied.
-    """
-
-    if type(fileTuple) == types.TupleType:
-      files = [fileTuple]
+  def removeFile(self,path):
+    if type(path) == types.StringType:
+      lfns = [path]
+    elif type(path) == types.ListType:
+       lfns = path
     else:
-      files = fileTuple
-    replicaTupleList = []
-    for fileTuple in files:
-      lfn,pfn,size,se,guid,checksum = fileTuple
-      replicaTupleList.append((lfn,pfn,se,'IGNORE'))
-
-    return self.addReplica(replicaTupleList)
-
-  def addReplica(self, replicaTuple):
-    """ Add replica info
-    """
-
-    result = S_OK()
-
-    if type(replicaTuple) == types.TupleType:
-      replicas = [replicaTuple]
-    else:
-      replicas = replicaTuple
-
-    failed_lfns = {}
-    done_lfns = {}
-
-    for replicaTuple in replicas:
-      lfn,pfn,se,ignore = replicaTuple
-      resRep = self.__addReplica(lfn,pfn,se)
-      if not resRep['OK']:
-        failed_lfns[lfn] = resRem['Message']
-      else:
-        done_lfns[lfn] = True
-
-    resDict = {'Successful':done_lfns,'Failed':failed_lfns}
-    return S_OK(resDict)
-
-  def removeFile(self,lfns):
-    """Remove the LFN record from the BK Catalog
-    """
-
-    if type(lfns) == types.StringType:
-      lfnList = [lfns]
-    else:
-      lfnList = lfns
-
-    result = S_OK()
-    failed_lfns = {}
-    done_lfns = {}
-
-    for lfn in lfnList:
-
+      return S_ERROR('BookkeepingDBOldClient.removeFile: Must supply a path or list of paths')
+    successful = {}
+    failed = {}
+    server = RPCClient(self.url)
+    for lfn in lfns:
       repscript = """<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE Replicas SYSTEM "book.dtd">
 <Replicas>
@@ -117,53 +110,26 @@ class BookkeepingDBOldClient(FileCatalogueBase):
            Action="Delete"  />
 </Replicas>
 """ % (lfn)
-      #print repscript
       fname = os.path.basename(lfn)+".R.xml"
-      #print "sending",fname
-      server = RPCClient(self.url)
-      resRem = server.sendBookkeeping(fname,repscript)
-      if not resRem['OK']:
-        failed_lfns[lfn] = resRem['Message']
+      res = server.sendBookkeeping(fname,repscript)
+      if not res['OK']:
+        failed[lfn] = res['Message']
       else:
-        done_lfns[lfn] = True
-
-    resDict = {'Successful':done_lfns,'Failed':failed_lfns}
+        successful[lfn] = True
+    resDict = {'Successful':successful,'Failed':failed}
     return S_OK(resDict)
 
-
-  def removeReplica(self,replicas):
-    """ Remove replica info from bookkeeping
+  def removeReplica(self,replicaTuple):
+    """ Remove replica info from bookkeeping (this is a dummy method as the replica flag should only be unset when there are no replicas)
     """
-
-    if type(replicas) == types.StringType:
-      replicaList = [replicas]
+    if type(replicaTuple) == types.TupleType:
+      replicas = [replicaTuple]
+    elif type(replicaTuple) == types.ListType:
+      replicas = replicaTuple
     else:
-      replicaList = replicas
-
-    result = S_OK()
-    failed_lfns = {}
-    done_lfns = {}
-
-    for lfn,se in replicaList:
-      repscript = """<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE Replicas SYSTEM "book.dtd">
-<Replicas>
-  <Replica File="%s"
-           Name="%s"
-           Location="%s"
-           SE="%s"
-           Action="Delete"  />
-</Replicas>
-""" % (lfn,'ANY',se,se)
-      #print repscript
-      fname = os.path.basename(lfn)+".R.xml"
-      #print "sending",fname
-      server = RPCClient(self.url)
-      resRem = server.sendBookkeeping(fname,repscript)
-      if not resRem['OK']:
-        failed_lfns[lfn] = resRem['Message']
-      else:
-        done_lfns[lfn] = True
-
-    resDict = {'Successful':done_lfns,'Failed':failed_lfns}
+      return S_ERROR('BookkeepingDBOldClient.removeReplica: Must supply a file tuple or list of file typles')
+    successful = {}
+    for lfn,pfn,se in replicas:
+      successful[lfn] = True
+    resDict = {'Failed':{},'Successful':successful}
     return S_OK(resDict)
