e47bca0a0ad63739cba7a8b8ac6bd0830049e79e
Marcos Antonio Seco <secomig@usc.es>
2008-10-09 10:28:14 +0000

    This file test the Logger API functions
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@12513 4525493e-7705-40b1-a816-d608a930855b

---
+++ LoggingSystem/test/test_LoggerClient.py
@@ -0,0 +1,52 @@
+from DIRACEnvironment import DIRAC
+from DIRAC import gLogger
+from DIRAC.Core.DISET.RPCClient import RPCClient
+from DIRAC.LoggingSystem.Client.LoggerClient import LoggerClient
+
+
+LClient = LoggerClient()
+
+retval = LClient.getSites()
+if not retval['OK']: print retval['Message']
+else: print retval['Value'][0:2]
+
+conditions = { 'SystemName': ['Framework/SecurityLog', 'DataManagement/TransferDBMonitoring']}
+retval = LClient.getMessages( conds = conditions, beginDate = '2008-10-06' )
+if not retval['OK']: print retval['Message']
+else: print retval['Value'][0:2]
+
+retval = LClient.getSystems()
+if not retval['OK']: print retval['Message']
+else: print retval['Value'][0:2]
+
+retval = LClient.getSubSystems()
+if not retval['OK']: print retval['Message']
+else: print retval['Value'][0:2]
+
+retval = LClient.getGroups()
+if not retval['OK']: print retval['Message']
+else: print retval['Value'][0:2]
+
+retval = LClient.getFixedTextStrings()
+if not retval['OK']: print retval['Message']
+else: print retval['Value'][0:2]
+
+retval = LClient.getMessagesByFixedText( 'File not found!' )
+if not retval['OK']: print retval['Message']
+else: print retval['Value'][0:4]
+
+showFields= [ 'SystemName', 'SubSystemName', 'OwnerDN' ]
+conditions={ 'SystemName': [ 'WorkloadManagement/Matcher', 'Framework/ProxyManager' ],  
+             'LogLevel': 'ERROR' }
+orderFields = [ [ 'OwnerDN', 'ASC' ], ['SystemName', 'ASC' ] ]
+retval = LClient.getGroupedMessages( fieldList = showFields, conds = conditions, 
+                                     beginDate = '2008-09-18',endDate = '2008-09-20',
+                                     groupField = 'SystemName', orderList = orderFields )
+if not retval['OK']: print retval['Message']
+else: print retval['Value']
+
+orderFields = [ [ 'recordCount', 'DESC' ] ]
+retval =  LClient.getGroupedMessages( groupField = 'FixedTextString', orderList = orderFields,
+                                      maxRecords = 10 )
+if not retval['OK']: print retval['Message']
+else: print retval['Value']
