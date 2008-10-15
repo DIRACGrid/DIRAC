16d1f1146629c5f99baef331e9341f32b504e8af
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-10-15 15:22:11 +0000

    very dangerous hack to be removed as soon as possible.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@12667 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Service/LcgFileCatalogProxyHandler.py
+++ DataManagementSystem/Service/LcgFileCatalogProxyHandler.py
@@ -5,12 +5,18 @@ from types import *
 from DIRAC.Core.DISET.RequestHandler import RequestHandler
 from DIRAC import gLogger, gConfig, S_OK, S_ERROR
 from DIRAC.DataManagementSystem.Client.FileCatalog import FileCatalog
+from DIRAC.Core.Utilities.Shifter import setupShifterProxyInEnv
 
 fileCatalog = False
 
 def initializeLcgFileCatalogProxyHandler(serviceInfo):
   global fileCatalog
   fileCatalog = FileCatalog(['LcgFileCatalogCombined'])
+  result = setupShifterProxyInEnv("DataManager")
+  if not result[ 'OK' ]:
+    self.log.error( "Can't get shifter's proxy: %s" % result[ 'Message' ] )
+    return result
+
   return S_OK()
 
 class LcgFileCatalogProxyHandler(RequestHandler):
