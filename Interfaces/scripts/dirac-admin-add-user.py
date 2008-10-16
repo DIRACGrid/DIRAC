02073a0e7b94ac2b1be2fefc1385f6b03723ec15
Stuart K. Paterson <stuart.paterson@cern.ch>
2008-10-16 09:06:54 +0000

    Move to .py extension.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@12677 4525493e-7705-40b1-a816-d608a930855b

---
+++ Interfaces/scripts/dirac-admin-add-user.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+########################################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Interfaces/scripts/dirac-admin-add-user.py,v 1.1 2008/10/16 09:06:54 paterson Exp $
+# File :   dirac-admin-add-user
+# Author : Adrian Casajus
+########################################################################
+__RCSID__   = "$Id: dirac-admin-add-user.py,v 1.1 2008/10/16 09:06:54 paterson Exp $"
+__VERSION__ = "$Revision: 1.1 $"
+from DIRACEnvironment import DIRAC
+from DIRAC.Core.Base import Script
+
+Script.registerSwitch( "p:", "property=", "Add property to the user <name>=<value>" )
+
+from DIRAC.Interfaces.API.DiracAdmin                         import DiracAdmin
+
+args = Script.getPositionalArgs()
+
+def usage():
+  print 'Usage: %s [<options>] <username> <DN> <groups>+' %(Script.scriptName)
+  DIRAC.exit(2)
+
+
+diracAdmin = DiracAdmin()
+exitCode = 0
+errorList = []
+
+if len(args) < 3:
+  usage()
+
+userProps = {}
+for unprocSw in Script.getUnprocessedSwitches():
+  if unprocSw[0] in ( "p", "property" ):
+    prop = unprocSw[1]
+    pl = prop.split( "=" )
+    if len( pl ) < 2:
+      errorList.append( ( "in arguments", "Property %s has to include a '=' to separate name from value" % prop ) )
+      exitCode = 255
+    else:
+      pName = pl[0]
+      pValue = "=".join( pl[1:] )
+      print "Setting property %s to %s" % ( pName, pValue )
+      userProps[ pName ] = pValue
+
+userProps[ 'DN' ] = args[1]
+userProps[ 'groups' ] = args[2:]
+
+if not diracAdmin.csRegisterUser( args[0], userProps ):
+  errorList.append( ( "add user", "Cannot register user %s" % args[0] ) )
+  exitCode = 255
+else:
+  result = diracAdmin.csCommitChanges()
+  if not result[ 'OK' ]:
+    errorList.append( ( "commit", result[ 'Message' ] ) )
+    exitCode = 255
+
+for error in errorList:
+  print "ERROR %s: %s" % error
+
+DIRAC.exit(exitCode)
