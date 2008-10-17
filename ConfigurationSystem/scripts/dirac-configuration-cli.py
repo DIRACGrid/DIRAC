783f14c1625e905b718e7c44a1b49e81423499d8
Ricardo Graciani <graciani@ecm.ub.es>
2008-10-17 13:04:02 +0000

    rename script with .py
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@12714 4525493e-7705-40b1-a816-d608a930855b

--- ConfigurationSystem/scripts/dirac-configuration-cli.py
+++ ConfigurationSystem/scripts/dirac-configuration-cli.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ConfigurationSystem/scripts/Attic/dirac-configuration-cli,v 1.1 2008/04/16 19:09:02 rgracian Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ConfigurationSystem/scripts/dirac-configuration-cli.py,v 1.4 2008/10/17 13:04:01 rgracian Exp $
 # File :   dirac-configuration-cli
 # Author : Adria Casajus
 ########################################################################
-__RCSID__   = "$Id: dirac-configuration-cli,v 1.1 2008/04/16 19:09:02 rgracian Exp $"
-__VERSION__ = "$Revision: 1.1 $"
+__RCSID__   = "$Id: dirac-configuration-cli.py,v 1.4 2008/10/17 13:04:01 rgracian Exp $"
+__VERSION__ = "$Revision: 1.4 $"
 
 from DIRACEnvironment import DIRAC
 from DIRAC.Core.Base import Script
