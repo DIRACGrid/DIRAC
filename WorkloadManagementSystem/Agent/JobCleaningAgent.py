81fa2ac4456676ad4045027e88eb2b67ee6af3ac
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2008-10-20 13:16:48 +0000

    Treat all the final statuses
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@12734 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/Agent/JobCleaningAgent.py
+++ WorkloadManagementSystem/Agent/JobCleaningAgent.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Agent/JobCleaningAgent.py,v 1.1 2008/10/20 07:08:29 atsareg Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Agent/JobCleaningAgent.py,v 1.2 2008/10/20 13:16:48 atsareg Exp $
 # File :   JobCleaningAgent.py
 # Author : A.T.
 ########################################################################
@@ -7,13 +7,18 @@
 """  The Job Cleaning Agent controls removing jobs from the WMS in the end of their life cycle.
 """
 
-__RCSID__ = "$Id: JobCleaningAgent.py,v 1.1 2008/10/20 07:08:29 atsareg Exp $"
+__RCSID__ = "$Id: JobCleaningAgent.py,v 1.2 2008/10/20 13:16:48 atsareg Exp $"
 
 from DIRAC.Core.Base.Agent                   import Agent
 from DIRAC.WorkloadManagementSystem.DB.JobDB import JobDB
 from DIRAC                                   import S_OK, S_ERROR, gConfig, gLogger
+import DIRAC.Core.Utilities.Time as Time
 
 AGENT_NAME = 'WorkloadManagement/JobCleaningAgent'
+REMOVE_STATUS_DELAY = {'Deleted':0,
+                       'Done':14,
+                       'Killed':7,
+                       'Failed':14 }
 
 class JobCleaningAgent(Agent):
 
@@ -38,22 +43,31 @@ class JobCleaningAgent(Agent):
     """The PilotAgent execution method.
     """
 
-    result = self.removeDeletedJobs()
+    # Remove jobs with final status
+    for status,delay in REMOVE_STATUS_DELAY.items():
+      delTime = Time.dateTime() - delay*Time.day
+      result = self.removeJobsByStatus(status,delTime)
+      if not result['OK']:
+        gLogger.warn('Failed to remove jobs in status %s' % status)
     return S_OK()
 
-  def removeDeletedJobs(self):
+  def removeJobsByStatus(self,status,delay):
     """ Remove deleted jobs
     """
 
-    result = self.jobDB.selectJobsWithStatus('Deleted')
+    result = self.jobDB.selectJobs({'Status':status},older=delay)
     if not result['OK']:
       return result
 
     jobList = result['Value']
+    count = 0
     for jobID in jobList:
       result = self.jobDB.removeJobFromDB(jobID)
       if not result['OK']:
         gLogger.warn('Failed to remove job %d from JobDB' % jobID, result['Message'])
+      else:
+        count += 1
 
+    if count > 0:
+      gLogger.info('Deleted %d jobs from JobDB' % count)
     return S_OK()
-  #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
