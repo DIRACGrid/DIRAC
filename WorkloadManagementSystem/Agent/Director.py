f73859b0e965a19113310f278dc949986b74010b
Ricardo Graciani <graciani@ecm.ub.es>
2008-10-22 08:25:33 +0000

    test commit
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@12762 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/Agent/Director.py
+++ WorkloadManagementSystem/Agent/Director.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Agent/Attic/Director.py,v 1.66 2008/10/22 08:25:22 rgracian Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Agent/Attic/Director.py,v 1.67 2008/10/22 08:25:33 rgracian Exp $
 # File :   Director.py
 # Author : Stuart Paterson, Ricardo Graciani
 ########################################################################
@@ -37,6 +37,7 @@
      necessary paramenters:
        gLite:
 
+
        LCG:
 
        The following paramenters will be taken from the Director section if not
@@ -47,7 +48,7 @@
 
 """
 
-__RCSID__ = "$Id: Director.py,v 1.66 2008/10/22 08:25:22 rgracian Exp $"
+__RCSID__ = "$Id: Director.py,v 1.67 2008/10/22 08:25:33 rgracian Exp $"
 
 import types, time
 
