29dd14a3fc957f9da2e8874ae16c9a28ddf43d6b
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2008-12-11 16:05:29 +0000

    new schema to support retaining SRM requestIDs for stage and pin requests.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@13444 4525493e-7705-40b1-a816-d608a930855b

--- StagerSystem/DB/StagerDB.sql
+++ StagerSystem/DB/StagerDB.sql
@@ -1,54 +1,60 @@
 -------------------------------------------------------------
--- Stager Service DB definition
 -- author: A. Smith
--- date: 17.03.2007
+-- StagerDB definition
 -------------------------------------------------------------
 
 DROP DATABASE IF EXISTS StagerDB;
 
 CREATE DATABASE StagerDB;
 --
--- Must set passwords for database user by replacing "must_be_set".
 --
 -- Create user DIRAC
 use mysql;
 GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON StagerDB.* TO 'Dirac'@'localhost' IDENTIFIED BY 'must_be_set';
-GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON StagerDB.* TO 'Dirac'@'%' IDENTIFIED BY 'must_be_set';
+#GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON StagerDB.* TO 'Dirac'@'%' IDENTIFIED BY 'must_be_set';
 
 FLUSH PRIVILEGES;
 
--------------------------------------------------------------
-
 use StagerDB;
 
-DROP TABLE IF EXISTS SiteFiles;
-CREATE TABLE SiteFiles(
-   LFN VARCHAR(255) NOT NULL,
-   Site varchar(32) NOT NULL,
-   SURL varchar(255) NOT NULL,
-   SE varchar(32) NOT NULL,
-   StageSubmit DATETIME NOT NULL,
-   StageComplete DATETIME NOT NULL,
-   Status varchar(32) DEFAULT 'New',
-   JobID varchar(32) NOT NULL,
-   Retry integer DEFAULT 0,
-   Source varchar(32) NOT NULL,
-   PRIMARY KEY (LFN, Site,JobID),
-   INDEX (Status,Source)
+DROP TABLE IF EXISTS Files;
+CREATE TABLE Files(
+  FileID INTEGER AUTO_INCREMENT,
+  Status VARCHAR(32) DEFAULT 'New',
+  StorageElement VARCHAR(32) NOT NULL,
+  LFN VARCHAR(255) NOT NULL,
+  FileSize INTEGER(32) DEFAULT 0,
+  PFN VARCHAR(255),
+  PRIMARY KEY (FileID,LFN,StorageElement),
+  INDEX(StorageElement,Status)
 );
 
-DROP TABLE IF EXISTS SiteTiming;
-CREATE TABLE SiteTiming(
-  Site varchar(32) NOT NULL,
-  Command varchar(32) NOT NULL,
-  CommTime FLOAT DEFAULT 0.0,
-  Files integer NOT NULL,
-  Time DATETIME NOT NULL
+DROP TABLE IF EXISTS Tasks;
+CREATE TABLE Tasks(
+  FileID INTEGER NOT NULL,
+  Status VARCHAR(32) DEFAULT 'New',
+  TaskID INTEGER(8) NOT NULL,
+  Source VARCHAR(32) NOT NULL,
+  SubmitTime DATETIME NOT NULL,
+  CallBackMethod VARCHAR(255),
+  PRIMARY KEY(FileID,Source,TaskID)
 );
 
-DROP TABLE IF EXISTS StageTimeRepository;
-CREATE TABLE StageTimeRepository(
-  Site varchar(32) NOT NULL,
-  Time DATETIME NOT NULL,
-  StageTime FLOAT NOT NULL
+DROP TABLE IF EXISTS StageRequests;
+CREATE TABLE StageRequests(
+  FileID INTEGER NOT NULL,
+  StageStatus VARCHAR(32) DEFAULT 'Submitted',
+  SRMRequestID INTEGER(32),
+  StageRequestSubmitTime DATETIME NOT NULL,
+  INDEX (StageStatus)
+);
+
+DROP TABLE IF EXISTS Pins;
+CREATE TABLE Pins(
+  FileID INTEGER NOT NULL,
+  PinStatus VARCHAR(32) DEFAULT 'Created',
+  SRMRequestID INTEGER(32),
+  PinCreationTime DATETIME NOT NULL,
+  PinExpiryTime DATETIME NOT NULL,
+  INDEX(PinStatus)
 );
