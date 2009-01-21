f462fede45bfc8381a3b6ce204928128497052e1
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2009-01-21 08:09:56 +0000

    Status reason is given in finalize()
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@13787 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/Agent/JobAgent.py
+++ WorkloadManagementSystem/Agent/JobAgent.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Agent/JobAgent.py,v 1.50 2009/01/20 18:03:11 atsareg Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/Agent/JobAgent.py,v 1.51 2009/01/21 08:09:56 atsareg Exp $
 # File :   JobAgent.py
 # Author : Stuart Paterson
 ########################################################################
@@ -10,7 +10,7 @@
      status that is used for matching.
 """
 
-__RCSID__ = "$Id: JobAgent.py,v 1.50 2009/01/20 18:03:11 atsareg Exp $"
+__RCSID__ = "$Id: JobAgent.py,v 1.51 2009/01/21 08:09:56 atsareg Exp $"
 
 from DIRAC.Core.Utilities.ModuleFactory                  import ModuleFactory
 from DIRAC.Core.Utilities.ClassAd.ClassAdLight           import ClassAd
@@ -594,7 +594,7 @@ class JobAgent(Agent):
     gridCE = gConfig.getValue('LocalSite/GridCE','Unknown')
     
     wmsAdmin = RPCClient('WorkloadManagement/WMSAdministrator')
-    result = wmsAdmin.setPilotStatus(str(self.pilotReference),'Done')
+    result = wmsAdmin.setPilotStatus(str(self.pilotReference),'Done','Report from JobAgent')
     if not result['OK']:
       self.log.warn(result['Message'])
     
