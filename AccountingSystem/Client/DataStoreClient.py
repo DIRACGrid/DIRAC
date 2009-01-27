cca89ee8894195075ee2949cf488d78544cfe9a8
Adria Casajus <adria@ecm.ub.es>
2009-01-27 18:43:37 +0000

    Added sending to failover the accounting requests that have waited more than "retryGraceTime"
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@13879 4525493e-7705-40b1-a816-d608a930855b

--- AccountingSystem/Client/DataStoreClient.py
+++ AccountingSystem/Client/DataStoreClient.py
@@ -1,19 +1,24 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/AccountingSystem/Client/DataStoreClient.py,v 1.6 2008/11/10 15:23:13 acasajus Exp $
-__RCSID__ = "$Id: DataStoreClient.py,v 1.6 2008/11/10 15:23:13 acasajus Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/AccountingSystem/Client/DataStoreClient.py,v 1.7 2009/01/27 18:43:37 acasajus Exp $
+__RCSID__ = "$Id: DataStoreClient.py,v 1.7 2009/01/27 18:43:37 acasajus Exp $"
 
-from DIRAC import S_OK, S_ERROR
+import time
+from DIRAC import S_OK, S_ERROR, gLogger
 from DIRAC.Core.DISET.RPCClient import RPCClient
 from DIRAC.Core.Utilities.ThreadSafe import Synchronizer
+from DIRAC.RequestManagementSystem.Client.RequestContainer import RequestContainer
+from DIRAC.RequestManagementSystem.Client.RequestClient import RequestClient
 
 gAccountingSynchro = Synchronizer()
 
 class DataStoreClient:
 
 
-  def __init__( self, setup = False ):
+  def __init__( self, setup = False, retryGraceTime = 0 ):
     self.__setup = setup
     self.__maxRecordsInABundle = 100
     self.__registersList = []
+    self.__maxTimeRetrying = retryGraceTime
+    self.__lastSuccessfulCommit = time.time()
 
   def __checkBaseType( self, obj ):
     """
@@ -53,9 +58,36 @@ class DataStoreClient:
       registersToSend = self.__registersList[ :self.__maxRecordsInABundle ]
       retVal = rpcClient.commitRegisters( registersToSend )
       if not retVal[ 'OK' ]:
-        return retVal
+        if time.time() - self.__lastSuccessfulCommit > self.__maxTimeRetrying:
+          gLogger.verbose( "Sending accounting records to failover" )
+          result = self.__sendToFailover( self.__setup )
+          if not result[ 'OK' ]:
+            return result
+        return S_ERROR( "Cannot commit data to DataStore service" )
       sent += len( registersToSend )
       del( self.__registersList[ :self.__maxRecordsInABundle ] )
+      self.__lastSuccessfulCommit = time.time()
     return S_OK( sent )
+
+  def __sendToFailover( self, setup = False ):
+    if self.__setup:
+      rpcClient = RPCClient( "Accounting/DataStore", setup = self.__setup )
+    else:
+      rpcClient = RPCClient( "Accounting/DataStore" )
+    requestClient = RequestClient()
+    while len( self.__registersList ) > 0:
+      registersToSend = self.__registersList[ :self.__maxRecordsInABundle ]
+      retVal = rpcClient.commitRegisters( registersToSend )
+      if not retVal[ 'OK' ]:
+        request = RequestContainer()
+        request.setDISETRequest( retVal[ 'rpcStub' ] )
+
+        requestStub = request.toXML()['Value']
+        result = requestClient.setRequest( "Accounting.DataStore.%s" % time.time(),
+                                           requestStub )
+        if not result[ 'OK' ]:
+          return result
+        del( self.__registersList[ :self.__maxRecordsInABundle ] )
+    return S_OK()
 
 gDataStoreClient = DataStoreClient()
