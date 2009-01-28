b81cc0422c4a3e956179b668d95357f4fb064238
Adria Casajus <adria@ecm.ub.es>
2009-01-28 13:42:58 +0000

    script to rerun the optimizers
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@13902 4525493e-7705-40b1-a816-d608a930855b

---
+++ WorkloadManagementSystem/scripts/dirac-admin-reoptimize-jobs.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+########################################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/scripts/dirac-admin-reoptimize-jobs.py,v 1.1 2009/01/28 13:42:58 acasajus Exp $
+# File :   dirac-admin-submit-pilot-for-job
+# Author : Ricardo Graciani
+########################################################################
+__RCSID__   = "$Id: dirac-admin-reoptimize-jobs.py,v 1.1 2009/01/28 13:42:58 acasajus Exp $"
+__VERSION__ = "$Revision: 1.1 $"
+import sys
+from DIRACEnvironment import DIRAC
+from DIRAC.Core.Base import Script
+
+Script.parseCommandLine( ignoreErrors = True )
+args = Script.getPositionalArgs()
+
+from DIRAC.WorkloadManagementSystem.DB.TaskQueueDB import TaskQueueDB
+from DIRAC.WorkloadManagementSystem.DB.JobDB import JobDB
+
+jobdb = JobDB()
+tqdb = TaskQueueDB()
+
+result = jobdb.selectJobs( { 'Status' : [ 'Received', 'Checking', 'Waiting' ] } )
+if not result[ 'OK' ]:
+  print result[ 'Message' ]
+  sys.exit(1)
+jobList = result[ 'Value' ]
+print tqdb.forceRecreationOfTables()
+for job in jobList:
+  result = jobdb.getJobAttribute( job, 'RescheduleCounter' )
+  if not result[ 'OK' ]:
+    print "Cannot get reschedule counter for job %s" % job
+    rC = 0
+  rC = result[ 'Value' ]
+  if rC >= jobdb.maxRescheduling:
+    jobdb.setJobAttribute( job, "RescheduleCounter", "0" )
+  jobdb.rescheduleJob( job )
+  jobdb.setJobAttribute( job, "RescheduleCounter", rC )
+sys.exit(0)
