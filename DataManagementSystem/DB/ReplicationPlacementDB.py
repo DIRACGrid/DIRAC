cb164cad5a55f760cea2ef3c25608fb3ad9ae6ef
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2009-01-29 18:25:14 +0000

    hack.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@13936 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/DB/ReplicationPlacementDB.py
+++ DataManagementSystem/DB/ReplicationPlacementDB.py
@@ -6,6 +6,7 @@ from DIRAC.Core.Base.DB import DB
 from DIRAC.ConfigurationSystem.Client.Config import gConfig
 from DIRAC  import gLogger, gMonitor, S_OK, S_ERROR
 from DIRAC.Core.Transformation.TransformationDB import TransformationDB
+from DIRAC.Core.Utilities.List import stringListToString,intListToString
 
 class ReplicationPlacementDB(TransformationDB):
 
@@ -13,3 +14,27 @@ class ReplicationPlacementDB(TransformationDB):
     """ Constructor
     """
     TransformationDB.__init__(self,'ReplicationPlacementDB', 'DataManagement/ReplicationPlacementDB', maxQueueSize)
+
+  def setFileStatusForTransformation(self,transName,status,lfns):
+    """ Set file status for the given transformation identified by transID
+        for the given stream for files in the list of lfns
+    """
+    transID = self.getTransformationID(transName)
+    fileIDs = self.__getFileIDsForLfns(lfns).keys()
+    if not fileIDs:
+      return S_ERROR('TransformationDB.setFileStatusForTransformation: No files found.')
+    else:
+      req = "UPDATE T_%s SET Status='%s' WHERE FileID IN (%s);" % (transID,status,intListToString(fileIDs))
+      return self._update(req)
+
+  def __getFileIDsForLfns(self,lfns):
+    """ Get file IDs for the given list of lfns
+    """
+    fids = {}
+    req = "SELECT LFN,FileID FROM DataFiles WHERE LFN in (%s);" % stringListToString(lfns)
+    res = self._query(req)
+    if not res['OK']:
+      return res
+    for lfn,fileID in res['Value']:
+      fids[fileID] = lfn
+    return fids
