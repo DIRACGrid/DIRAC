a3b419c49f1a912bea5bf71cc7a65ffe0289ce79
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2009-02-03 21:40:27 +0000

    added the methods for managing file lifetime.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@14028 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Client/Storage/StorageBase.py
+++ DataManagementSystem/Client/Storage/StorageBase.py
@@ -41,7 +41,7 @@ class StorageBase:
     self.rootdir = rootdir
     self.cwd = self.rootdir
 
-  def exists(self,path):
+  def exists(self,*parms, **kws):
     """Check if the given path exists
     """
     return S_ERROR("Storage.exists: implement me!")
@@ -51,91 +51,106 @@ class StorageBase:
   # These are the methods for file manipulation
   #
 
-  def isFile(self,fname):
+  def isFile(self,*parms, **kws):
     """Check if the given path exists and it is a file
     """
     return S_ERROR("Storage.isFile: implement me!")
 
-  def getFile(self,path):
+  def getFile(self,*parms, **kws):
     """Get a local copy of the file specified by its path
     """
     return S_ERROR("Storage.getFile: implement me!")
 
-  def putFile(self,fname):
+  def putFile(self,*parms, **kws):
     """Put a copy of the local file to the current directory on the
        physical storage
     """
     return S_ERROR("Storage.putFile: implement me!")
 
-  def removeFile(self,path):
+  def removeFile(self,*parms, **kws):
     """Remove physically the file specified by its path
     """
     return S_ERROR("Storage.removeFile: implement me!")
 
-  def getFileMetadata(self,path):
+  def getFileMetadata(self,*parms, **kws):
     """  Get metadata associated to the file
     """
     return S_ERROR("Storage.getFileMetadata: implement me!")
 
-  def getFileSize(self,fname):
+  def getFileSize(self,*parms, **kws):
     """Get the physical size of the given file
     """
     return S_ERROR("Storage.getFileSize: implement me!")
 
-  def prestageFile(self,path):
+  def getTransportURL(self,*parms, **kws):
+    """ Obtain the TURLs for the supplied path and protocols
+    """
+    return S_ERROR("Storage.getTransportURL: implement me!")
+
+  def prestageFile(self,*parms, **kws):
     """ Issue prestage request for file
     """
     return S_ERROR("Storage.prestageFile: implement me!")
 
-  def getTransportURL(self,path,protocols=False):
-    """ Obtain the TURLs for the supplied path and protocols
+  def prestageFileStatus(self,*parms, **kws):
+    """ Obtain the status of the prestage request
     """
-    return S_ERROR("Storage.getTransportURL: implement me!")
+    return S_ERROR("Storage.prestageFileStatus: implement me!")
+
+  def pinFile(self,*parms, **kws):
+    """ Pin the file on the destination storage element
+    """
+    return S_ERROR("Storage.pinFile: implement me!")    
+
+  def releaseFile(self,*parms, **kws):
+    """ Release the file on the destination storage element
+    """
+    return S_ERROR("Storage.releaseFile: implement me!")
 
   #############################################################
   #
   # These are the methods for directory manipulation
   #
 
-  def isDirectory(self,dirname):
+  def isDirectory(self,*parms, **kws):
     """Check if the given path exists and it is a directory
     """
     return S_ERROR("Storage.isDirectory: implement me!")
 
-  def getDirectory(self,path):
+  def getDirectory(self,*parms, **kws):
     """Get locally a directory from the physical storage together with all its
        files and subdirectories.
     """
     return S_ERROR("Storage.getDirectory: implement me!")
 
-  def putDirectory(self,path):
+  def putDirectory(self,*parms, **kws):
     """Put a local directory to the physical storage together with all its
        files and subdirectories.
     """
     return S_ERROR("Storage.putDirectory: implement me!")
 
-  def createDirectory(self,newdir):
+  def createDirectory(self,*parms, **kws):
     """ Make a new directory on the physical storage
     """
     return S_ERROR("Storage.createDirectory: implement me!")
 
-  def removeDirectory(self,path):
+  def removeDirectory(self,*parms, **kws):
     """Remove a directory on the physical storage together with all its files and
        subdirectories.
     """
     return S_ERROR("Storage.removeDirectory: implement me!")
 
-  def listDirectory(self,path):
+  def listDirectory(self,*parms, **kws):
     """ List the supplied path
     """
     return S_ERROR("Storage.listDirectory: implement me!")
 
-  def getDirectoryMetadata(self,path):
+  def getDirectoryMetadata(self,*parms, **kws):
     """ Get the metadata for the directory
     """
     return S_ERROR("Storage.getDirectoryMetadata: implement me!")
 
-  def getDirectorySize(self,path):
+  def getDirectorySize(self,*parms, **kws):
     """ Get the size of the directory on the storage
     """
     return S_ERROR("Storage.getDirectorySize: implement me!")
@@ -165,17 +180,17 @@ class StorageBase:
     """
     return S_OK(self.name)
 
-  def getParameters(self):
+  def getParameters(self,*parms, **kws):
     """ Get the parameters with which the storage was instantiated
     """
     return S_ERROR("Storage.getParameters: implement me!")
 
-  def getProtocolPfn(self,pfnDict,withPort):
+  def getProtocolPfn(self,*parms, **kws):
     """ Get the PFN for the protocol with or without the port
     """
     return S_ERROR("Storage.getProtocolPfn: implement me!")
 
-  def getCurrentURL(self,fileName):
+  def getCurrentURL(self,*parms, **kws):
     """ Create the full URL for the storage using the configuration, self.cwd and the fileName
     """
     return S_ERROR("Storage.getCurrentURL: implement me!")
