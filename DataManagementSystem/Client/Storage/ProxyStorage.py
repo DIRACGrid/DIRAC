a70ae6d1b74928135818796cfc922be1d8d57b3f
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2009-02-10 19:23:52 +0000

    first import of the proxy storage plugin. This needs to be tested against the TestStorageElement unit-test.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@14133 4525493e-7705-40b1-a816-d608a930855b

---
+++ DataManagementSystem/Client/Storage/ProxyStorage.py
@@ -0,0 +1,221 @@
+""" This is the LHCb Online storage """
+
+from DIRAC import gLogger, gConfig, S_OK, S_ERROR
+from DIRAC.DataManagementSystem.Client.Storage.StorageBase import StorageBase
+from DIRAC.ConfigurationSystem.Client import PathFinder
+from DIRAC.Core.DISET.RPCClient import RPCClient
+import types
+ 
+ISOK = True
+
+class ProxyStorage(StorageBase):
+
+  def __init__(self,storageName,protocol,path,host,port,spaceToken,wspath):
+    self.isok = ISOK
+
+    self.protocolName = 'Proxy'
+    self.name = storageName
+    self.protocol = protocol
+    self.path = path
+    self.host = host
+    self.port = port
+    self.wspath = wspath
+    self.spaceToken = spaceToken
+    self.cwd = self.path
+    apply(StorageBase.__init__,(self,self.name,self.path))
+    self.url = PathFinder.getServiceURL("DataManagement/StorageElementProxy")
+    if not self.url:
+      self.isok = False
+
+  ######################################
+  # URL manipulation functionalities
+  ######################################   
+
+
+  ######################################
+  # File transfer functionalities
+  ######################################   
+
+  def getFile(self,path,localPath=False):
+    res = self.__checkArgumentFormatDict(path)
+    if not res['OK']:
+      return res
+    urls = res['Value']
+    failed = {}
+    successful = {}
+    client = RPCClient(self.url)
+    transferClient = TransferClient(self.url)
+    for src_url in urls.keys():
+      res = client.prepareFile(self.name,src_url)
+      if not res['OK']:
+        gLogger.error("ProxyStorage.getFile: Failed to prepare file on remote server.", res['Message'])
+        failed[src_url] = res['Message']
+      else:
+        fileName = os.path.basename(src_url)
+        if localPath:  
+          dest_file = "%s/%s" % (localPath,fileName)
+        else:
+          dest_file = "%s/%s" % (os.getcwd(),fileName)
+        res = transferClient.receiveFile(dest_file,'getFile/%s' % fileName)
+        if not res['OK']:
+          gLogger.error("ProxyStorage.getFile: Failed to recieve file from proxy server.", res['Message'])
+          failed[src_url] = res['Message']
+        elif not os.path.exists(dest_file):
+          errStr = "ProxyStorage.getFile: The destination local file does not exist."
+          gLogger.error(errStr,dest_file)
+          failed[src_url] = errStr
+        else:
+          destSize = getSize(dest_file)
+          if destSize == -1:
+            errStr = "ProxyStorage.getFile: Failed to get the local file size."
+            gLogger.error(errStr,dest_file)
+            failed[src_url] = errStr
+          else:
+            successful[src_url] = destSize
+    resDict = {'Failed':failed,'Successful':successful}
+    return S_OK(resDict)
+      
+  def putFile(self,path,sourceSize=0):
+    client = RPCClient(self.url)
+    if sourceSize:
+      gLogger.debug("ProxyStorage.putFile: The client has provided the source file size implying a replication is requested.")
+      return client.callProxyMethod(self.name,'putFile',path,{'sourceSize':sourceSize})
+    gLogger.debug("ProxyStorage.putFile: No source size was provided therefore a simple put will be performed.")
+    res = self.__checkArgumentFormatDict(path)
+    if not res['OK']:
+      return res
+    urls = res['Value']
+    failed = {}
+    successful = {}
+    client = RPCClient(self.url)
+    transferClient = TransferClient(self.url)
+    for dest_url,src_file in urls.items():
+      fileName = os.path.basename(dest_url)
+      res = transferClient.sendFile(src_file,'putFile/%s' % fileName)
+      if not res['OK']:
+        gLogger.error("ProxyStorage.putFile: Failed to send file to proxy server.", res['Message'])
+        failed[dest_url] = res['Message']
+      else:
+        res = client.uploadFile(storageElementName,pfn)
+        print res,'!!!!!!!!!!!'
+        if not res['OK']:
+          gLogger.error("ProxyStorage.putFile: Failed to upload file to storage element from proxy server.", res['Message'])
+          failed[dest_url] = res['Message']
+        else:
+          res = self.__executeOperation(dest_url,'getFileSize')
+          if not res['OK']:
+            gLogger.error("ProxyStorage.putFile: Failed to determine destination file size.",res['Message'])
+            failed[dest_url] = res['Message']
+          else:
+            successful[dest_url] = res['Value']
+    resDict = {'Failed':failed,'Successful':successful}
+    return S_OK(resDict)           
+
+  ######################################
+  # File manipulation functionalities
+  ######################################
+
+  def exists(self,path):
+    client = RPCClient(self.url)
+    return client.callProxyMethod(self.name,'exists',path)
+
+  def isFile(self,path): 
+    client = RPCClient(self.url)
+    return client.callProxyMethod(self.name,'isFile',path)
+
+  def getFileSize(self,path):
+    client = RPCClient(self.url)
+    return client.callProxyMethod(self.name,'getFileSize',path)
+
+  def getFileMetadata(self,path):
+    client = RPCClient(self.url)
+    return client.callProxyMethod(self.name,'getFileMetadata',path)
+
+  def getTransportURL(self,path,protocols=False):
+    client = RPCClient(self.url)
+    return client.callProxyMethod(self.name,'getTransportURL',path,{'protocols':protocols})
+
+  def removeFile(self,path):
+    client = RPCClient(self.url)
+    return client.callProxyMethod(self.name,'removeFile',path)
+
+  def prestageFile(self,path):
+    client = RPCClient(self.url)
+    return client.callProxyMethod(self.name,'prestageFile',path)
+ 
+  def prestageFileStatus(self,path):
+    client = RPCClient(self.url)
+    return client.callProxyMethod(self.name,'prestageFileStatus',path)
+
+  def pinFile(self,path,lifetime=60*60*24): 
+    client = RPCClient(self.url)
+    return client.callProxyMethod(self.name,'pinFile',path,{'lifetime':lifetime})
+
+  def releaseFile(self,path):
+    client = RPCClient(self.url)
+    return client.callProxyMethod(self.name,'releaseFile',path)
+
+  ######################################
+  # Directory manipulation functionalities
+  ######################################
+
+  def isDirectory(self,path):
+    client = RPCClient(self.url)
+    return client.callProxyMethod(self.name,'isDirectory',path)
+
+  def getDirectoryMetadata(self,path):
+    client = RPCClient(self.url)
+    return client.callProxyMethod(self.name,'getDirectoryMetadata',path)
+
+  def getDirectorySize(self,path):
+    client = RPCClient(self.url)
+    return client.callProxyMethod(self.name,'getDirectorySize',path)     
+
+  def listDirectory(self,path):
+    client = RPCClient(self.url)
+    return client.callProxyMethod(self.name,'listDirectory',path)
+
+  def createDirectory(self,path):
+    client = RPCClient(self.url)
+    return client.callProxyMethod(self.name,'createDirectory',path)
+   
+  def removeDirectory(self,path,recursive=False):
+    client = RPCClient(self.url)
+    return client.callProxyMethod(self.name,'removeDirectory',path,{'recursive':recursive})
+
+  def getDirectory(self,path):
+    return S_ERROR("Not supported")
+
+  def putDirectory(self,path):
+    return S_ERROR("Not supported")
+
+
+  def __checkArgumentFormatDict(self,path):  
+    if type(path) in types.StringTypes:
+      urls = {path:False}
+    elif type(path) == types.ListType:
+      urls = {}
+      for url in path:
+        urls[url] = False
+    elif type(path) == types.DictType:
+     urls = path
+    else:
+      return S_ERROR("ProxyStorage.__checkArgumentFormatDict: Supplied path is not of the correct format.")
+    return S_OK(urls)
+
+  def __executeOperation(self,url,method):
+    """ Executes the requested functionality with the supplied url
+    """
+    execString = "res = self.%s(url)" % method
+    try:
+      exec(execString)
+      if not res['OK']:
+        return S_ERROR(res['Message'])
+      elif not res['Value']['Successful'].has_key(url):
+        return S_ERROR(res['Value']['Failed'][url])
+      else:
+        return S_OK(res['Value']['Successful'][url])
+    except AttributeError,errMessage:
+      exceptStr = "ProxyStorage.__executeOperation: Exception while perfoming %s." % method
+      gLogger.exception(exceptStr,'',errMessage)
+      return S_ERROR("%s%s" % (exceptStr,errMessage))
