ee052ddc7a7ac87908f0d28ed1a197cb0c6f4dd1
Stuart K. Paterson <stuart.paterson@cern.ch>
2009-03-08 22:09:18 +0000

    Updated to setup the proxy and perform renewal as necessary.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@14620 4525493e-7705-40b1-a816-d608a930855b

--- Resources/Computing/InProcessComputingElement.py
+++ Resources/Computing/InProcessComputingElement.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Id: InProcessComputingElement.py,v 1.4 2008/01/08 22:47:08 paterson Exp $
+# $Id: InProcessComputingElement.py,v 1.5 2009/03/08 22:09:18 paterson Exp $
 # File :   InProcessComputingElement.py
 # Author : Stuart Paterson
 ########################################################################
@@ -7,11 +7,14 @@
 """ The simplest Computing Element instance that submits jobs locally.
 """
 
-__RCSID__ = "$Id: InProcessComputingElement.py,v 1.4 2008/01/08 22:47:08 paterson Exp $"
+__RCSID__ = "$Id: InProcessComputingElement.py,v 1.5 2009/03/08 22:09:18 paterson Exp $"
 
 from DIRAC.Resources.Computing.ComputingElement          import ComputingElement
+from DIRAC.FrameworkSystem.Client.ProxyManagerClient     import gProxyManager
+from DIRAC.Core.Utilities.ThreadScheduler                import gThreadScheduler
 from DIRAC.Core.Utilities.Subprocess                     import shellCall
-from DIRAC                                               import S_OK, S_ERROR
+from DIRAC.Core.Security.Misc                            import getProxyInfoAsString
+from DIRAC                                               import gConfig,S_OK,S_ERROR
 
 import os,sys
 
@@ -24,12 +27,26 @@ class InProcessComputingElement(ComputingElement):
     """ Standard constructor.
     """
     ComputingElement.__init__(self,CE_NAME)
+    self.minProxyTime = gConfig.getValue( '/Security/MinProxyLifeTime', 10800 ) #secs
+    self.defaultProxyTime = gConfig.getValue( '/Security/DefaultProxyLifeTime', 86400 ) #secs
+    self.proxyCheckPeriod = gConfig.getValue('/Security/ProxyCheckingPeriod',60) #mins
     self.submittedJobs = 0
 
   #############################################################################
-  def submitJob(self,executableFile,jdl,localID):
+  def submitJob(self,executableFile,jdl,proxy,localID):
     """ Method to submit job, should be overridden in sub-class.
     """
+    self.log.verbose('Setting up proxy for payload')
+    result = self.writeProxyToFile(proxy)
+    if not result['OK']:
+      return result
+
+    payloadProxy = result['Value']
+    pilotProxy = os.environ['X509_USER_PROXY']
+    os.environ[ 'X509_USER_PROXY' ] = payloadProxy
+    self.log.verbose('Starting process for monitoring payload proxy')
+    gThreadScheduler.addPeriodicTask(self.proxyCheckPeriod,self.monitorProxy,taskArgs=(pilotProxy,payloadProxy),executions=0,elapsedTime=0)
+
     if not os.access(executableFile, 5):
       os.chmod(executableFile,0755)
     cmd = os.path.abspath(executableFile)
@@ -53,5 +70,26 @@ class InProcessComputingElement(ComputingElement):
     result['RunningJobs'] = 0
     result['WaitingJobs'] = 0
     return S_OK(result)
+
+  #############################################################################
+  def monitorProxy(self,pilotProxy,payloadProxy):
+    """ Monitor the payload proxy and renew as necessary.
+    """
+    if not os.path.exists(pilotProxy):
+      return S_ERROR('Pilot proxy not found at %s' %pilotProxy)
+    if not os.path.exists(payloadProxy):
+      return S_ERROR('Payload proxy not found at %s' %payloadProxy)
+
+    result = getProxyInfoAsString(payloadProxy)
+    if not result['OK']:
+      self.log.error('Could not get payload proxy info',result)
+      return result
+
+    self.log.verbose('Payload proxy information:\n%s' %result['Value'])
+    gProxyManager.renewProxy(minLifeTime=self.minProxyTime,
+                             newProxyLifeTime=self.defaultProxyTime,
+                             proxyToConnect=pilotProxy)
+
+    return S_OK('Proxy checked')
 
   #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
