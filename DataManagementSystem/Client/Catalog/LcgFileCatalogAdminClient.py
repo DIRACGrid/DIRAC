b01d726aa7fe33d4b0e87dd27c72db529902cbe4
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2009-03-24 13:34:18 +0000

    first import, just a place holder for the moment. still have to add the new methods in the LcgFileCatalogClient.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@14853 4525493e-7705-40b1-a816-d608a930855b

---
+++ DataManagementSystem/Client/Catalog/LcgFileCatalogAdminClient.py
@@ -0,0 +1,12 @@
+""" This is the admin interface to the LFC. It exposes the functionality not available through the standard LcgFileCatalogCombinedClient """
+
+from DIRAC  import gLogger, gConfig, S_OK, S_ERROR
+from DIRAC.DataManagementSystem.Client.Catalog.LcgFileCatalogCombinedClient import LcgFileCatalogCombinedClient
+from DIRAC.Core.Utilities.Subprocess import pythonCall
+import random, time,os
+import lfc
+
+class LcgFileCatalogAdminClient(LcgFileCatalogCombinedClient):
+
+  LcgFileCatalogCombinedClient.ro_methods.extend([])
+  LcgFileCatalogCombinedClient.write_methods.extend([])
