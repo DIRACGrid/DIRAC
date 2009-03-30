d5ccd30cf74980c7006d81a607acded4d1dccc95
Adria Casajus <adria@ecm.ub.es>
2009-03-30 14:55:53 +0000

    Added proper timeouts for dip sockets
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@14867 4525493e-7705-40b1-a816-d608a930855b

--- Core/DISET/private/Transports/PlainTransport.py
+++ Core/DISET/private/Transports/PlainTransport.py
@@ -1,7 +1,9 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/DISET/private/Transports/PlainTransport.py,v 1.13 2008/10/14 13:54:22 acasajus Exp $
-__RCSID__ = "$Id: PlainTransport.py,v 1.13 2008/10/14 13:54:22 acasajus Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Core/DISET/private/Transports/PlainTransport.py,v 1.14 2009/03/30 14:55:53 acasajus Exp $
+__RCSID__ = "$Id: PlainTransport.py,v 1.14 2009/03/30 14:55:53 acasajus Exp $"
 
 import socket
+import select
+import time
 from DIRAC.Core.DISET.private.Transports.BaseTransport import BaseTransport
 from DIRAC.LoggingSystem.Client.Logger import gLogger
 from DIRAC.Core.Utilities.ReturnValues import S_ERROR, S_OK
@@ -10,7 +12,23 @@ class PlainTransport( BaseTransport ):
 
   def initAsClient( self ):
     self.oSocket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-    self.oSocket.connect( self.stServerAddress )
+    if 'timeout' in self.extraArgsDict:
+      self.oSocket.settimeout( self.extraArgsDict[ 'timeout' ] )
+    try:
+      self.oSocket.connect( self.stServerAddress )
+    except socket.error ,e:
+      if e.args[0] != 115:
+        return S_ERROR( "Can't connect: %s" % str( e ) )
+      #Connect in progress
+      oL = select.select( [], [ self.oSocket ], [], self.extraArgsDict[ 'timeout' ] )[1]
+      if len( oL ) == 0:
+        self.oSocket.close()
+        return S_ERROR( "Connection timeout" )
+      errno = self.oSocket.getsockopt( socket.SOL_SOCKET, socket.SO_ERROR )
+      if errno != 0:
+        return S_ERROR( "Can't connect: %s" % str( ( errno, os.strerror( errno ) ) ) )
+    else:
+      self.oSocket.connect( self.stServerAddress )
     self.remoteAddress = self.oSocket.getpeername()
     return S_OK( self.oSocket )
 
@@ -36,6 +54,8 @@ class PlainTransport( BaseTransport ):
     if self.serverMode():
       raise RuntimeError( "Mustbe initialized as client mode" )
     self.oSocket = oSocket
+    if 'timeout' in self.extraArgsDict:
+      self.oSocket.settimeout( self.extraArgsDict[ 'timeout' ] )
     self.remoteAddress = self.oSocket.getpeername()
 
   def acceptConnection( self ):
@@ -44,6 +64,52 @@ class PlainTransport( BaseTransport ):
     oClientSocket, stClientAddress = self.oSocket.accept()
     oClientTransport.setClientSocket( oClientSocket )
     return S_OK( oClientTransport )
+
+  def _read( self, bufSize = 4096, skipReadyCheck = False ):
+    start = time.time()
+    timeout = False
+    if 'timeout' in self.extraArgsDict:
+      timeout = self.extraArgsDict[ 'timeout' ]
+    while True:
+      if timeout:
+        if time.time() - start > timeout:
+          return S_ERROR( "Socket read timeout exceeded" )
+      try:
+        data = self.oSocket.recv( bufSize )
+        return S_OK( data )
+      except socket.error, e:
+        if e[0] == 11:
+          time.sleep(0.1)
+        else:
+          return S_ERROR( "Exception while reading from peer: %s" % str( e ) )
+      except Exception, e:
+        return S_ERROR( "Exception while reading from peer: %s" % str( e ) )
+
+  def _write( self, buffer ):
+    sentBytes = 0
+    timeout = False
+    if 'timeout' in self.extraArgsDict:
+      timeout = self.extraArgsDict[ 'timeout' ]
+    if timeout:
+      start = time.time()
+    while sentBytes < len( buffer ):
+      try:
+        if timeout:
+          if time.time() - start > timeout:
+            return S_ERROR( "Socket write timeout exceeded" )
+        sent = self.oSocket.send( buffer[ sentBytes: ] )
+        if sent == 0:
+          return S_ERROR( "Connection closed by peer" )
+        if sent > 0:
+          sentBytes += sent
+      except socket.error, e:
+        if e[0] == 11:
+          time.sleep(0.1)
+        else:
+          return S_ERROR( "Exception while sending to peer: %s" % str( e ) )
+      except Exception, e:
+        return S_ERROR( "Error while sending: %s" % str( e ) )
+    return S_OK( sentBytes )
 
 def checkSanity( *args, **kwargs ):
   return S_OK()
