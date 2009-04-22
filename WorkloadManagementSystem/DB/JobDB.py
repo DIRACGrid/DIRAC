bc87292494978422ebdfe72f4accde6d8df88cc6
Ricardo Graciani <graciani@ecm.ub.es>
2009-04-22 13:20:47 +0000

    and one more
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@15216 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/DB/JobDB.py
+++ WorkloadManagementSystem/DB/JobDB.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/DB/JobDB.py,v 1.154 2009/04/22 12:55:54 rgracian Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/DB/JobDB.py,v 1.155 2009/04/22 13:20:47 rgracian Exp $
 ########################################################################
 
 """ DIRAC JobDB class is a front-end to the main WMS database containing
@@ -47,7 +47,7 @@
     getCounters()
 """
 
-__RCSID__ = "$Id: JobDB.py,v 1.154 2009/04/22 12:55:54 rgracian Exp $"
+__RCSID__ = "$Id: JobDB.py,v 1.155 2009/04/22 13:20:47 rgracian Exp $"
 
 import re, os, sys, string, types
 import time, datetime, operator
@@ -1392,7 +1392,7 @@ class JobDB(DB):
     ret = self._escapeString(jobID)
     if not ret['OK']:
       return ret
-    jobID = ret['Value']
+    e_jobID = ret['Value']
 
 
     # If this is a master job delete the children first
