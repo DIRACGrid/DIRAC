2fa37d898a2cd20fd6fec2d2d99857ae649b8352
Ricardo Graciani <graciani@ecm.ub.es>
2009-05-05 21:37:32 +0000

    return error string
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@15473 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/DB/JobDB.py
+++ WorkloadManagementSystem/DB/JobDB.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/DB/JobDB.py,v 1.157 2009/05/05 16:15:51 rgracian Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/WorkloadManagementSystem/DB/JobDB.py,v 1.158 2009/05/05 21:37:32 rgracian Exp $
 ########################################################################
 
 """ DIRAC JobDB class is a front-end to the main WMS database containing
@@ -47,7 +47,7 @@
     getCounters()
 """
 
-__RCSID__ = "$Id: JobDB.py,v 1.157 2009/05/05 16:15:51 rgracian Exp $"
+__RCSID__ = "$Id: JobDB.py,v 1.158 2009/05/05 21:37:32 rgracian Exp $"
 
 import re, os, sys, string, types
 import time, datetime, operator
@@ -1367,7 +1367,7 @@ class JobDB(DB):
 
     if error:
 
-      retVal = S_ERROR()
+      retVal = S_ERROR(error)
       retVal['JobId'] = jobID
       retVal['Status'] = 'Failed'
       retVal['MinorStatus'] = error
