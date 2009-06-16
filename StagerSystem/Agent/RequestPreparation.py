df366441038b4323f9f256e6a770d64c607b2f14
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2009-06-16 22:01:56 +0000

    pass the reason for terminal failure back to main loop to allow this to be logged in the task files.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@16068 4525493e-7705-40b1-a816-d608a930855b

--- StagerSystem/Agent/RequestPreparation.py
+++ StagerSystem/Agent/RequestPreparation.py
@@ -1,9 +1,9 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/StagerSystem/Agent/RequestPreparation.py,v 1.1 2009/06/15 18:39:51 acsmith Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/StagerSystem/Agent/RequestPreparation.py,v 1.2 2009/06/16 22:01:56 acsmith Exp $
 
 """  TransferAgent takes transfer requests from the RequestDB and replicates them
 """
 
-__RCSID__ = "$Id: RequestPreparation.py,v 1.1 2009/06/15 18:39:51 acsmith Exp $"
+__RCSID__ = "$Id: RequestPreparation.py,v 1.2 2009/06/16 22:01:56 acsmith Exp $"
 
 
 from DIRAC.Core.DISET.RPCClient import RPCClient
@@ -60,7 +60,15 @@ class RequestPreparation(Agent):
     for lfn,errMessage in failed.items():
       gLogger.error("RequestPreparation.prepareNewFiles: Failed to get file metadata.", "%s %s" % (lfn,errMessage))
 
+    # For all the terminal files get all the tasks associated to these files and fail them.
+    if terminal:
+      #First set the failure reason of the terminal files 
+      for lfn,reason in terminal.items():
+        pass
+      res = self.__getAssociatedTaskFiles(terminal)
+
     #res = self.__clearFailedFileTasks()
+    
 
     notAtRequestedSE = {}
     selectedFiles = []
@@ -69,7 +77,7 @@ class RequestPreparation(Agent):
       replicas = metadata['Replicas']
       for storageElement in lfnToSE[lfn].keys():
         if not replicas.has_key(storageElement):
-          gLogger.error(": File does not have replica at requested storage element","%s %s" % (lfn,storageElement))
+          gLogger.error("RequestPreparation.: File does not have replica at requested storage element","%s %s" % (lfn,storageElement))
           notAtRequestedSE[lfn][storageElement]
         else:
           pfn = replicas[storageElement]
@@ -85,7 +93,30 @@ class RequestPreparation(Agent):
     """
     return S_OK()
 
-
+  def __getAssociatedTaskFiles(self,fileIDs):
+    """ This obtains all the FileIDs for the tasks associated with the supplied fileIDs """
+    # First get the list of task IDs associated to the files that failed
+    res = self.stagerClient.getTasksForFileIDs(fileIDs)
+    if not res['OK']:
+      gLogger.error("RequestPreparation.__getAssociatedTaskFiles: Failed to get task IDs associated to the fileIDs.",res['Message'])
+      return res
+    taskIDs = res['Value']
+    failedTasks = []
+    for fileID,tasks in taskIDs.items():
+      for task in tasks:
+        if not task in failedTasks:
+          failedTasks.append(task)
+    # Then get all the files associated to those tasks
+    res = self.stagerClient.getFileIDsForTasks(failedTasks)
+    if not res['OK']:
+      gLogger.error("RequestPreparation.__getAssociatedTaskFiles: Failed to get file IDs associated to the tasks.",res['Message'])
+      return res
+    fileIDs = res['Value']
+    for taskID,files in fileIDs.items():
+      for file in files:
+        if not file in allFileIDs:
+          allFileIDs.append(file)
+    
   def __clearFailedFileTasks(self,failedFileIDs):
     """ This obtains the TaskIDs associated to the fileIDs at the supplied storage element """
     # First get the list of task IDs associated to the files that failed
@@ -99,7 +130,7 @@ class RequestPreparation(Agent):
       for task in tasks:
         if not task in failedTasks:
           failedTasks.append(task)
-    # Then get all the files associated to that task
+    # Then get all the files associated to those tasks
     res = self.stagerClient.getFileIDsForTasks(failedTasks)
     if not res['OK']:
       gLogger.error("RequestPreparation.__clearFailedFileTasks: Failed to get file IDs associated to the tasks.",res['Message'])
@@ -110,14 +141,14 @@ class RequestPreparation(Agent):
       for file in files:
         if not file in allFileIDs:
           allFileIDs.append(file)
-
     goodFileIDs = []
     for fileID in allFileIDs: 
       if not fileID in failedFileIDs:
         goodFileIDs.append(fileID)
 
   def __reportProblematicFiles(self,lfns,reason):
-    res = self.dataIntegrityClient.setFileProblematic(self,missing,reason,self.name)
+    return S_OK()
+    res = self.dataIntegrityClient.setFileProblematic(lfns,reason,self.name)
     if not res['OK']:
       gLogger.error("RequestPreparation.__reportProblematicFiles: Failed to report missing files.",res['Message'])
       return res
@@ -173,7 +204,7 @@ class RequestPreparation(Agent):
     if not res['OK']:
       return res
     failed.update(res['Value']['Failed'])
-    terminal.extend(res['Value']['ZeroSize'])
+    terminal.update(res['Value']['ZeroSize'])
     fileSizes = res['Value']['FileSizes']
     if not fileSizes:
       gLogger.error('RequestPreparation.__getFileMetadata: Failed determine sizes of any files')
@@ -184,7 +215,7 @@ class RequestPreparation(Agent):
     if not res['OK']:
       return res
     failed.update(res['Value']['Failed'])
-    terminal.extend(res['Value']['ZeroReplicas'])
+    terminal.update(res['Value']['ZeroReplicas'])
     replicas = res['Value']['Replicas']
     if not replicas:
       gLogger.error('RequestPreparation.__getFileMetadata: Failed determine replicas for any files')
@@ -200,7 +231,7 @@ class RequestPreparation(Agent):
   def __getExistingFiles(self,lfns):
     """ This checks that the files exist in the FileCatalog. """  
     filesExist = []
-    missing = []
+    missing = {}
     res = self.fileCatalog.exists(lfns)
     if not res['OK']:
       gLogger.error("RequestPreparation.__getExistingFiles: Failed to determine whether files exist.",res['Message'])
@@ -210,18 +241,18 @@ class RequestPreparation(Agent):
       if exists:
         filesExist.append(lfn)
       else:
-        missing.append(lfn)
+        missing[lfn] = 'LFN not registered in the FileCatalog'
     if missing:
-      for lfn in missing:
-        gLogger.warn("RequestPreparation.__getExistingFiles: Requested file did not exist",lfn)
-      self.__reportProblematicFiles(missing,'LFN-LFC-DoesntExist')
+      for lfn,reason in missing.items():
+        gLogger.warn("RequestPreparation.__getExistingFiles: %s" % reason,lfn)
+      self.__reportProblematicFiles(missing.keys(),'LFN-LFC-DoesntExist')
     return S_OK({'Exist':filesExist,'Missing':missing,'Failed':failed})
 
   def __getFileSize(self,lfns):
     """ This obtains the file size from the FileCatalog. """
     failed = []
     fileSizes = {}
-    zeroSize = []
+    zeroSize = {}
     res = self.fileCatalog.getFileSize(lfns)
     if not res['OK']:
       gLogger.error("RequestPreparation.__getFileSize: Failed to get sizes for files.", res['Message'])
@@ -229,19 +260,19 @@ class RequestPreparation(Agent):
     failed = res['Value']['Failed']
     for lfn,size in res['Value']['Successful'].items():
       if size == 0:
-        zeroSize.append(lfn)
+        zeroSize[lfn] = "LFN registered with zero size in the FileCatalog"
       else:
         fileSizes[lfn] = size
     if zeroSize:
-      for lfn in zeroSize:
-        gLogger.warn("RequestPreparation.__getFileSize: Requested file has zero size",lfn)
-      self.__reportProblematicFiles(noReplicas,'LFN-LFC-ZeroSize')
+      for lfn,reason in zeroSize.items():
+        gLogger.warn("RequestPreparation.__getFileSize: %s" % reason,lfn)
+      self.__reportProblematicFiles(zeroSize.keys(),'LFN-LFC-ZeroSize')
     return S_OK({'FileSizes':fileSizes,'ZeroSize':zeroSize,'Failed':failed})  
 
   def __getFileReplicas(self,lfns):
     """ This obtains the replicas from the FileCatalog. """
     replicas = {}
-    noReplicas = []
+    noReplicas = {}
     res = self.fileCatalog.getReplicas(lfns)
     if not res['OK']:
       gLogger.error("RequestPreparation.__getFileReplicas: Failed to obtain file replicas.",res['Message'])
@@ -249,16 +280,11 @@ class RequestPreparation(Agent):
     failed = res['Value']['Failed']
     for lfn,lfnReplicas in res['Value']['Successful'].items():
       if len(lfnReplicas.keys()) == 0:
-        noReplicas.append(lfn)         
+        noReplicas[lfn] = "LFN registered with zero replicas in the FileCatalog"         
       else:
         replicas[lfn] = lfnReplicas
     if noReplicas:
-      for lfn in noReplicas:
-        gLogger.warn("RequestPreparation.__getFileReplicas: Requested file has no replicas",lfn)
-      self.__reportProblematicFiles(noReplicas,'LFN-LFC-NoReplicas')
+      for lfn,reason in noReplicas.items():
+        gLogger.warn("RequestPreparation.__getFileReplicas: %s" % reason,lfn)
+      self.__reportProblematicFiles(noReplicas.keys(),'LFN-LFC-NoReplicas')
     return S_OK({'Replicas':replicas,'ZeroReplicas':noReplicas,'Failed':failed})
-
-
-
-
-
