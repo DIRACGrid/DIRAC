d807ebde670e61a55d9699e569366ea5252a7d2a
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2009-06-24 09:35:46 +0000

    easy script to completely remove files.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@16147 4525493e-7705-40b1-a816-d608a930855b

---
+++ DataManagementSystem/scripts/dirac-dms-remove-files.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from DIRAC.Core.Base.Script import parseCommandLine
+parseCommandLine()
+########################################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/DataManagementSystem/scripts/dirac-dms-remove-files.py,v 1.1 2009/06/24 09:35:46 acsmith Exp $
+########################################################################
+__RCSID__   = "$Id: dirac-dms-remove-files.py,v 1.1 2009/06/24 09:35:46 acsmith Exp $"
+__VERSION__ = "$ $"
+
+from DIRAC.Core.Utilities.List import sortList
+from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
+rm = ReplicaManager()
+import os,sys
+
+if len(sys.argv) < 2:
+  print 'Usage: ./dirac-dms-remove-replicas.py <LFN | fileContainingLFNs>'
+  sys.exit()
+else:
+  inputFileName = sys.argv[1]
+
+if os.path.exists(inputFileName):
+  inputFile = open(inputFileName,'r')
+  string = inputFile.read()
+  lfns = string.splitlines()
+  inputFile.close()
+else:
+  lfns = [inputFileName]
+
+res = rm.removeFile(lfns)
+if not res['OK']:
+  print res['Message']
+  sys.exit()
+for lfn in sortList(res['Value']['Successful'].keys()):
+  print 'Successfully removed %s' % (lfn)
+for lfn in sortList(res['Value']['Failed'].keys()):
+  message = res['Value']['Failed'][lfn]
+  print 'Failed to remove %s: %s' % (lfn,message)
