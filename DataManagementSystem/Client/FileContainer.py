2e27073bbe4454253b676bda288c8f1cefe9e063
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2009-08-03 14:24:10 +0000

    allow the file object to interact with the CatalogFile class to populate members. Also added the ability to store CatalogReplica information.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@16807 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Client/FileContainer.py
+++ DataManagementSystem/Client/FileContainer.py
@@ -1,94 +1,151 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/DataManagementSystem/Client/FileContainer.py,v 1.2 2009/07/28 14:02:38 acsmith Exp $
-__RCSID__ = "$Id: FileContainer.py,v 1.2 2009/07/28 14:02:38 acsmith Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/DataManagementSystem/Client/FileContainer.py,v 1.3 2009/08/03 14:24:10 acsmith Exp $
+__RCSID__ = "$Id: FileContainer.py,v 1.3 2009/08/03 14:24:10 acsmith Exp $"
 
-""" The file container is to store all information associated to a file
+""" This module contains classes associated to Files.
+
+    The File class contains the member elements: LFN, Status, Size, GUID and Checksum and provides access methods for each (inluding type checking).
 """
 
-import os
-from DIRAC.Core.Utilities.File import makeGuid
+import types
 from DIRAC import S_OK, S_ERROR
+from DIRAC.DataManagementSystem.Client.ReplicaManager import CatalogFile
+from DIRAC.DataManagementSystem.Client.ReplicaContainers import CatalogReplica
 
-class FileContainer:
+class File:
 
-  def __init__(self,attributeDict={}):
-    # These are the possible attributes for the file
-    self.fileAttributes = { 'FileID'     :  0,
-                            'Status'     :  'Waiting',
-                            'LFN'        :  '',
-                            'PFN'        :  '',
-                            'Size'       :  0,
-                            'GUID'       :  makeGuid(),
-                            'Md5'        :  '',
-                            'Adler'      :  '',
-                            'Attempt'    :  0,
-                            'Error'      :  ''}
-    for key,value in attributeDict.items():
-      self.fileAttributes[key] = value
+  def __init__(self, lfn='', status='', size=0, guid='', checksum=''):
+    # These are the possible attributes for a file
+    if not type(lfn) in types.StringTypes:
+      raise AttributeError, "lfn should be string type"
+    self.lfn = str(lfn)
+    if not type(status) in types.StringTypes:
+      raise AttributeError, "status should be string type"
+    self.status = str(status)
+    try:
+      self.size = int(size)  
+    except:
+      raise AttributeError, "size should be integer type"
+    if not type(guid) in types.StringTypes:
+      raise AttributeError, "guid should be string type"
+    self.guid = str(guid)
+    if not type(checksum) in types.StringTypes:
+      raise AttributeError, "checksum should be string type"
+    self.checksum = str(checksum)
+    self.catalogReplicas = []
 
-  #####################################################################
-  #
-  #  Attribute access methods
-  #
-
-  def __getattr__(self,name):
-    """ Generic method to access request attributes or parameters
-    """
-    # If the client has attempted to get a file attribute
-    if name.find('get') ==0:
-      item = name[3:]
-      self.item_called = item
-      if item in self.fileAttributes.keys():
-        return self.__get_attribute
-      else:
-        return S_ERROR("Item %s not known" % item)
-    # If the client has attempted to set a file attribute
-    elif name.find('set') == 0:
-      item = name[3:]
-      self.item_called = item
-      if item in self.fileAttributes.keys():
-        return self.__set_attribute
-      else:
-        return S_ERROR("Item %s not known" % item)
-    # Otherwise they are trying to do something we dont like
-    else:
-      return S_ERROR("Action %s not known" % name)
-
-  def __get_attribute(self):
-    """ Generic method to get attributes
-    """
-    return S_OK(self.fileAttributes[self.item_called])
-
-  def __set_attribute(self,value):
-    """ Generic method to set attribute value
-    """
-    if type(value) != type(self.fileAttributes[self.item_called]):
-      return S_ERROR("%s should be type %s and not %s" % (self.item_called,type(self.fileAttributes[self.item_called]),type(value)))
-    self.fileAttributes[self.item_called] = value
+  def setLFN(self,lfn):
+    if not type(lfn) in types.StringTypes: 
+      return S_ERROR("LFN should be %s and not %s" % (types.StringType,type(se)))
+    self.lfn = str(lfn)  
+    return S_OK()
+      
+  def setStatus(self,status):
+    if not type(status) in types.StringTypes:
+      return S_ERROR("Status should be %s and not %s" % (types.StringType,type(status)))
+    self.status = str(status)
     return S_OK()
 
-  def getAttributes(self):
-    """ Get the dictionary of the file attributes
-    """
-    return S_OK(self.fileAttributes)
+  def setSize(self,size):
+    try:
+      self.size = int(size)
+      return S_OK()
+    except:
+      return S_ERROR("Size should be %s and not %s" % (types.IntType,type(size)))
+      
+  def setGUID(self,guid):
+    if not type(guid) in types.StringTypes:
+      return S_ERROR("GUID should be %s and not %s" % (types.StringType,type(guid)))
+    self.guid = str(guid)
+    return S_OK()
 
-  def isEmpty(self):
-    if self.fileAttributes['Status'] in ['Done','Failed']:
-      return S_OK(1)
-    return S_OK(0)
+  def setChecksum(self,checksum):
+    if not type(checksum) in types.StringTypes:
+      return S_ERROR("Checksum should be %s and not %s" % (types.StringType,type(checksum)))
+    self.checksum = str(checksum)
+    return S_OK()
 
-  def getDigest(self):
-    """ Get short description string of file and status 
+  def addCatalogReplica(self,se,pfn,status='U'):
+    for replica in self.catalogReplicas:
+      if (replica.pfn == pfn) and (replica.se == se):
+        return S_OK()
+    oCatalogReplica = CatalogReplica(pfn=pfn,storageElement=se,status=status)
+    self.catalogReplicas.append(oCatalogReplica)
+    return S_OK()
+  
+  def getLFN(self):
+    return S_OK(self.lfn)
+
+  def getStatus(self):
+    if self.status:
+      return S_OK(self.status)
+    if not self.lfn:
+      return S_ERROR('No LFN is known')
+    res = self.__populateMetadata()
+    if not res['OK']:
+      return res
+    return S_OK(self.status)
+
+  def getSize(self):
+    if self.size:
+      return S_OK(self.size)
+    if not self.lfn:
+      return S_ERROR('No LFN is known')
+    res = self.__populateMetadata()
+    if not res['OK']:
+      return res
+    return S_OK(self.size)
+
+  def getGUID(self):
+    if self.guid:
+      return S_OK(self.guid)
+    if not self.lfn:
+      return S_ERROR('No LFN is known')
+    res = self.__populateMetadata()
+    if not res['OK']:
+      return res
+    return S_OK(self.guid)
+
+  def getChecksum(self):
+    if self.checksum:
+      return S_OK(self.checksum)
+    if not self.lfn:
+      return S_ERROR('No LFN is known')
+    res = self.__populateMetadata()
+    if not res['OK']:
+      return res
+    return S_OK(self.checksum)
+
+  def __populateMetadata(self):
+    oCatalog = CatalogFile()
+    res = oCatalog.getCatalogFileMetadata(self.lfn,singleFile=True)
+    if not res['OK']:
+      return res
+    metadata = res['Value']
+    self.setChecksum(metadata['CheckSumValue'])
+    self.setGUID(metadata['GUID'])
+    self.setSize(metadata['Size']) 
+    self.setStatus(metadata['Status'])
+    return S_OK()
+
+  def getReplicas(self):
+    if not self.lfn:
+      return S_ERROR('No LFN is known')
+    if self.catalogReplicas:
+      replicas = {}
+      for replica in self.catalogReplicas:
+        replicas[replica.se] = replica.pfn
+      return S_OK(replicas)
+    oCatalog = CatalogFile()
+    res = oCatalog.getCatalogReplicas(self.lfn,singleFile=True)
+    if not res['OK']:
+      return res
+    replicas = res['Value']
+    for se,pfn in replicas.items():
+      oCatalogReplica = CatalogReplica(pfn=pfn,storageElement=se,status='U')
+      self.catalogReplicas.append(oCatalogReplica)
+    return S_OK(replicas)
+  
+  def digest(self):
+    """ Get short description string of file attributes
     """
-    digestList = []
-    fname = ''
-    if self.fileAttributes['LFN']:
-      fname = os.path.basename(self.fileAttributes['LFN'])
-    elif self.fileAttributes['PFN']:
-      fname = os.path.basename(self.fileAttributes['PFN'])
-    digestList.append(fname)
-    digestList.append(self.fileAttributes['Status'])
-    if self.fileAttributes['Attempt']:
-      digestList.append(self.fileAttributes['Attempt'])
-      digestList.append(self.fileAttributes['Error'])
-    digestString = ":".join(digestList)
-    return S_OK(digestString)
+    return S_OK("%s:%s:%d:%s:%s" % (self.lfn,self.status,self.size,self.guid,self.checksum))
