07d03a8827ba0e0780a62d4fca411b6cbf9a5aa5
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2009-08-11 18:17:14 +0000

    use NotificationClient, update to AgentModule framework.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@16970 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Agent/UserStorageQuotaAgent.py
+++ DataManagementSystem/Agent/UserStorageQuotaAgent.py
@@ -1,157 +1,93 @@
-"""  UserStorageQuotaAgent determines the current storage usage for each user.\
+"""  UserStorageQuotaAgent obtains the usage by each user from the StorageUsageDB and compares with a quota present in the CS.
 """
-from DIRAC  import gLogger, gConfig, gMonitor, S_OK, S_ERROR
-from DIRAC.Core.Base.Agent import Agent
-from DIRAC.Core.Utilities.Pfn import pfnparse, pfnunparse
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/DataManagementSystem/Agent/UserStorageQuotaAgent.py,v 1.6 2009/08/11 18:17:14 acsmith Exp $
+__RCSID__ = "$Id: UserStorageQuotaAgent.py,v 1.6 2009/08/11 18:17:14 acsmith Exp $"
+
+from DIRAC  import gLogger, gConfig, gMonitor, S_OK, S_ERROR, rootPath
+from DIRAC.Core.Base.AgentModule import AgentModule
+
 from DIRAC.Core.DISET.RPCClient import RPCClient
-from DIRAC.Core.Utilities.Shifter import setupShifterProxyInEnv
-from DIRAC.RequestManagementSystem.Client.DataManagementRequest import DataManagementRequest
-from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
-from DIRAC.DataManagementSystem.Agent.NamespaceBrowser import NamespaceBrowser
-from DIRAC.DataManagementSystem.Client.FileCatalog import FileCatalog
 from DIRAC.Core.Utilities.List import sortList
-from DIRAC.DataManagementSystem.DB.StorageUsageDB import StorageUsageDB
-from DIRAC.Core.Utilities.Subprocess import shellCall
+from DIRAC.FrameworkSystem.Client.NotificationClient import NotificationClient
 
 import time,os
 from types import *
-import lfc
 
 AGENT_NAME = 'DataManagement/UserStorageQuotaAgent'
 
-class UserStorageQuotaAgent(Agent):
-
-  def __init__(self):
-    """ Standard constructor
-    """
-    Agent.__init__(self,AGENT_NAME)
+class UserStorageQuotaAgent(AgentModule):
 
   def initialize(self):
-    result = Agent.initialize(self)
-    self.lfc = FileCatalog(['LcgFileCatalogCombined'])
-    self.StorageUsageDB = StorageUsageDB()
-    self.defaultQuota = float(gConfig.getValue('/Groups/lhcb/Quota'))
-
-    self.useProxies = gConfig.getValue(self.section+'/UseProxies','True').lower() in ( "y", "yes", "true" )
-    self.proxyLocation = gConfig.getValue( self.section+'/ProxyLocation', '' )
-    if not self.proxyLocation:
-      self.proxyLocation = False
-
-    return result
+    self.StorageUsageDB = RPCClient('DataManagement/StorageUsage')
+    self.am_setModuleParam("shifterProxy", "DataManager")
+    self.am_setModuleParam("shifterProxyLocation","%s/runit/%s/proxy" % (rootPath,AGENT_NAME))
+    self.defaultQuota = self.am_getOption('DefaultQuota',1000) # Default is 1TB
+    self.defaultQuota = 76
+    gLogger.info("initialize: Default quota found to be %d GB" % self.defaultQuota)
+    return S_OK()
 
   def execute(self):
-
-    if self.useProxies:
-      result = setupShifterProxyInEnv( "DataManager", self.proxyLocation )
-      if not result[ 'OK' ]:
-        self.log.error( "Can't get shifter's proxy: %s" % result[ 'Message' ] )
-      return result
-
     res = self.StorageUsageDB.getUserStorageUsage()
     usageDict = res['Value']
     usageToUserDict = {}
     userNameDNDict = {}
 
-    byteToMB = 1000*1000.0
+    byteToGB = 1000*1000*1000.0
 
+    gLogger.info("Determining quota usage for %s users." % len(usageDict.keys()))
     for userName in sortList(usageDict.keys()):
-      usageMB = usageDict[userName]/byteToMB
+      usageGB = usageDict[userName]/byteToGB
       res = gConfig.getOptionsDict('/Security/Users/%s' % userName)
       if not res['OK']:
-        gLogger.error("UserStorageQuotaAgent: Username not found in the CS.",userName)
-        userQuota = 0
+        gLogger.error("Username not found in the CS.","%s using %.2f GB" % (userName,usageGB))
+        continue
+      elif not res['Value'].has_key('email'):
+        gLogger.error("CS does not contain email information for user.",userName)
+        continue
+      elif not res['Value'].has_key('Quota'):
+        userQuota = float(self.defaultQuota)
       else:
-        if not res['Value'].has_key('Quota'):
-          userQuota = self.defaultQuota
-        else:
-          userQuota = float(res['Value']['Quota'])
-
-
-        #we probably want diffirent behaviour for 90% exceeded, 110% exceeded and 150% exceeded
-        if (1.5*userQuota) < usageMB:
-          gLogger.info("UserStorageQuotaAgent: %s is 50%s over quota %s: %s." % (userName,'%',userQuota,usageMB))
-          userMail = res['Value']['mail']
-          res = self.sendBlockedMail(userMail,userQuota,usageMB)
-          if not res['OK']:
-            gLogger.error("UserStorageQuotaAgent: Failed to send blocked email to user.",res['Message'])
-          else:
-            gLogger.info("UserStorageQuotaAgent: Sent blocked email to user.")
-            gLogger.info("!!!!!!!!!!!!!!!!!!!!!!!!REMEMBER TO MODIFY THE ACLs and STATUS HERE!!!!!!!!!!!!!!!!!")
-        elif (1.1*userQuota) < usageMB:
-          gLogger.info("UserStorageQuotaAgent: %s is 10%s over quota %s: %s." % (userName,'%',userQuota,usageMB))
-          userMail = res['Value']['mail']
-          res = self.sendWarningMail(userMail,userQuota,usageMB)
-          if not res['OK']:
-            gLogger.error("UserStorageQuotaAgent: Failed to send warning email to user.",res['Message'])
-          else:
-            gLogger.info("UserStorageQuotaAgent: Sent warning email to user.")
-        else:
-          gLogger.info("UserStorageQuotaAgent: %s within quota %s: %s." % (userName,userQuota,usageMB))
+        userQuota = float(res['Value']['Quota'])
+      userMail = res['Value']['email']
+      # Diffirent behaviour for 90% exceeded, 110% exceeded and 150% exceeded
+      if (1.5*userQuota) < usageGB:      
+        gLogger.info("%s is at %d%s of quota %d GB (%.1f GB)." % (userName,(usageGB*100)/userQuota,'%',userQuota,usageGB))
+        self.sendBlockedMail(userName,userMail,userQuota,usageGB)
+        gLogger.info("!!!!!!!!!!!!!!!!!!!!!!!!REMEMBER TO MODIFY THE ACLs and STATUS HERE!!!!!!!!!!!!!!!!!")
+      elif (1.1*userQuota) < usageGB:
+        gLogger.info("%s is at %d%s of quota %d GB (%.1f GB)." % (userName,(usageGB*100)/userQuota,'%',userQuota,usageGB))
+        self.sendSecondWarningMail(userName,userMail,userQuota,usageGB)
+      elif (0.9*userQuota) < usageGB:
+        gLogger.info("%s is at %d%s of quota %d GB (%.1f GB)." % (userName,(usageGB*100)/userQuota,'%',userQuota,usageGB))
+        self.sendFirstWarningMail(userName,userMail,userQuota,usageGB)
     return S_OK()
 
-  def sendWarningMail(self,userMail,quota,usage):
-    try:
-      import smtplib
-      from email.MIMEText import MIMEText
-      msgbody = "This mail has been generated automatically.\n\n"
-      msgbody += "You have received this mail because your Grid storage usage has exceeded your quota of %sMB.\n\n" % int(quota)
-      msgbody += "You are currently using %sMB.\n\n" % int(usage)
-      msgbody += "Please reduce you usage by removing some files. If you have reduced your usage in the last 24 hours please ignore this message."
-      msg = MIMEText(msgbody)
-      msg['From'] = 'Andrew C. Smith <a.smith@cern.ch>'
-      msg['To'] = userMail
-      msg['Subject'] = 'Grid Storage Quota'
-      s = smtplib.SMTP()
-      s.connect()
-      s.sendmail('a.smith@cern.ch', [userMail], msg.as_string())
-      return S_OK()
-    except Exception,x:
-      return S_ERROR(str(x))
+  def sendFirstWarningMail(self,userName,userMail,quota,usage):
+    msgbody = "This mail has been generated automatically.\n\n"
+    msgbody += "You have received this mail because you are approaching your Grid storage usage quota of %sGB.\n\n" % int(quota)
+    msgbody += "You are currently using %.1f GB.\n\n" % usage
+    msgbody += "Please reduce you usage by removing some files. If you have reduced your usage in the last 24 hours please ignore this message."
+    fromAddress = 'LHCb Data Manager <lhcb-datamanagement@cern.ch>'
+    subject = 'Grid storage use near quota (%s)' % userName 
+    toAddress = 'a.smith@cern.ch' # userMail
+    NotificationClient().sendMail(toAddress, subject, msgbody, fromAddress)
 
-  def sendBlockedMail(self,userMail,quota,usage):
-    try:
-      import smtplib
-      from email.MIMEText import MIMEText
-      msgbody = "This mail has been generated automatically.\n\n"
-      msgbody += "You have received this mail because your Grid storage usage has exceeded your quota of %sMB.\n\n" % int(quota)
-      msgbody += "You are currently using %sMB.\n\n" % int(usage)
-      msgbody += "Your account has now been blocked and you will not be able to save more output without creating space. If you have reduced your usage in the last 24 hours please ignore this message."
-      msg = MIMEText(msgbody)
-      msg['From'] = 'Andrew C. Smith <a.smith@cern.ch>'
-      msg['To'] = userMail
-      msg['Subject'] = 'Grid Storage Quota'
-      s = smtplib.SMTP()
-      s.connect()
-      s.sendmail('a.smith@cern.ch', [userMail], msg.as_string())
-      return S_OK()
-    except Exception,x:
-      return S_ERROR(str(x))
+  def sendSecondWarningMail(self,userName,userMail,quota,usage):
+    msgbody = "This mail has been generated automatically.\n\n"
+    msgbody += "You have received this mail because your Grid storage usage has exceeded your quota of %sGB.\n\n" % int(quota)
+    msgbody += "You are currently using %.1f GB.\n\n" % usage
+    msgbody += "Please reduce you usage by removing some files. If you have reduced your usage in the last 24 hours please ignore this message."
+    fromAddress = 'LHCb Data Manager <lhcb-datamanagement@cern.ch>'
+    subject = 'Grid storage use over quota (%s)' % userName
+    toAddress = 'a.smith@cern.ch' # userMail
+    NotificationClient().sendMail(toAddress, subject, msgbody, fromAddress)
 
-
-
-    """
-    gLogger.info("%s %s" % ('User'.ljust(30),'Total usage'.ljust(30)))
-    lfnBase = '/lhcb/user'
-    for userName in sortList(usageDict.keys()):
-      lfn = "%s/%s/%s" % (lfnBase,userName[0],userName)
-      res = self.lfc.getDirectoryMetadata(lfn)
-      if not res['OK']:
-        gLogger.error("Failed to get the directory metadata:", res['Message'])
-        userNameDNDict[userName] = userName
-      elif not res['Value']['Successful'].has_key(lfn):
-        gLogger.error("Failed to get metadata:", res['Value']['Failed'][lfn])
-        userNameDNDict[userName] = userName
-      else:
-        lfnMetadata = res['Value']['Successful'][lfn]
-        creatorDN = lfnMetadata['CreatorDN']
-        userNameDNDict[userName] = creatorDN
-
-      #gLogger.info("%s %s" % (userName.ljust(30),str(usageDict[userName]/byteToGB).ljust(30)))
-      gLogger.info("%s %s %s" % (creatorDN.ljust(90),userName.ljust(30),str(usageDict[userName]/byteToGB).ljust(30)))
-      usageToUserDict[usageDict[userName]] = userName
-
-    gLogger.info("Top ten users.")
-    gLogger.info("%s %s" % ('User'.ljust(30),'Total usage'.ljust(30)))
-    for usage in sortList(usageToUserDict.keys(),True):
-      gLogger.info("%s %s %s" % (userNameDNDict[usageToUserDict[usage]].ljust(90),usageToUserDict[usage].ljust(30),str(usage/byteToGB).ljust(30)))
-    """
+  def sendBlockedMail(self,userName,userMail,quota,usage):
+    msgbody = "This mail has been generated automatically.\n\n"
+    msgbody += "You have received this mail because your Grid storage usage has exceeded your quota of %sGB.\n\n" % int(quota)
+    msgbody += "You are currently using %.1f GB.\n\n" % usage
+    msgbody += "Your account has now been blocked and you will not be able to save more output without creating space. If you have reduced your usage in the last 24 hours please ignore this message."
+    fromAddress = 'LHCb Data Manager <lhcb-datamanagement@cern.ch>'
+    subject = 'Grid storage use blocked (%s)' % userName
+    toAddress = 'a.smith@cern.ch' # userMail
+    NotificationClient().sendMail(toAddress, subject, msgbody, fromAddress) 
