3e3cf8465217875fc7dde84e74b42d8854a91c08
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2009-08-12 14:37:09 +0000

    first version.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@16981 4525493e-7705-40b1-a816-d608a930855b

---
+++ DataManagementSystem/scripts/dirac-dms-storage-usage-summary.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+########################################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/DataManagementSystem/scripts/dirac-dms-storage-usage-summary.py,v 1.1 2009/08/12 14:37:09 acsmith Exp $
+########################################################################
+__RCSID__   = "$Id: dirac-dms-storage-usage-summary.py,v 1.1 2009/08/12 14:37:09 acsmith Exp $"
+__VERSION__ = "$Revision: 1.1 $"
+import DIRAC
+from DIRAC.Core.Base import Script
+unit = 'GB'
+dir = ''
+Script.registerSwitch( "u:", "unit=","   Unit to use [%s] (MB,GB,TB,PB)" % unit)
+Script.registerSwitch( "d:", "dir=", "   Dir to search [ALL]")
+Script.parseCommandLine( ignoreErrors = False )
+
+from DIRAC.Core.DISET.RPCClient import RPCClient
+from DIRAC.Core.Utilities.List import sortList
+
+def usage():
+  print 'Usage: %s [<options>] <Directory>' % (Script.scriptName)
+  print ' Get a summary of the storage usage <for an optionally supplied directory>.'
+  print ' The usage can be given in any of the following units: (MB,GB,TB,PB)' 
+  print ' Type "%s --help" for the available options and syntax' % Script.scriptName
+  DIRAC.exit(2)
+
+args = Script.getPositionalArgs()
+for switch in Script.getUnprocessedSwitches():
+  if switch[0].lower() == "u" or switch[0].lower() == "unit":
+    unit = switch[1]
+  if switch[0].lower() == "d" or switch[0].lower() == "dir":
+    dir = switch[1]
+
+scaleDict = { 'MB' : 1000*1000.0,
+              'GB' : 1000*1000*1000.0,
+              'TB' : 1000*1000*1000*1000.0,
+              'PB' : 1000*1000*1000*1000*1000.0}
+if not unit in scaleDict.keys():
+  usage()
+scaleFactor = scaleDict[unit]  
+             
+rpc = RPCClient('dips://volhcb08.cern.ch:9151/DataManagement/StorageUsage')
+res = rpc.getStorageSummary(dir)
+if not res['Value']:
+  print 'No usage found'
+  DIRAC.exit(2)
+print '%s %s %s' % ('SE'.ljust(20),('Size (%s)' % unit).ljust(20),'Files'.ljust(20))
+for se in sortList(res['Value'].keys()):
+  dict = res['Value'][se]
+  files = dict['Files']
+  size = dict['Size']
+  print "%s %s %s" % (se.ljust(20),('%.1f' % (size/scaleFactor)).ljust(20),str(files).ljust(20))
+DIRAC.exit(0)
