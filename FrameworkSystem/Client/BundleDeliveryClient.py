71bcdb3bae58cce791edffb346088b3b41216a21
Adria Casajus <adria@ecm.ub.es>
2009-08-12 14:44:32 +0000

    Initial commit of BundleDelivery service and client to ship CAs to sites
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@16983 4525493e-7705-40b1-a816-d608a930855b

---
+++ FrameworkSystem/Client/BundleDeliveryClient.py
@@ -0,0 +1,78 @@
+
+import os
+import tarfile
+import cStringIO
+from DIRAC import S_OK, S_ERROR, gLogger
+from DIRAC.Core.DISET.RPCClient import RPCClient
+from DIRAC.Core.DISET.TransferClient import TransferClient
+from DIRAC.Core.Utilities import File
+from DIRAC.Core.Security import Locations
+
+class BundleDeliveryClient:
+
+  def __init__( self, rpcClient = False, transferClient = False ):
+    self.rpcClient = rpcClient
+    self.transferClient = transferClient
+    self.log = gLogger.getSubLogger( "BundleDelivery" )
+
+  def __getRPCClient(self):
+    if self.rpcClient:
+      return self.rpcClient
+    return RPCClient( "Framework/BundleDelivery", skipCACheck = True )
+  
+  def __getTransferClient(self):
+    if self.transferClient:
+      return self.transferClient
+    return TransferClient( "Framework/BundleDelivery", skipCACheck = True )
+
+  def __getHash( self, bundleID, dirToSyncTo ):
+    if not os.path.isdir( dirToSyncTo ):
+      self.log.info( "Creating dir %s" % dirToSyncTo )
+      os.makedirs( dirToSyncTo )
+    try:
+      fd = open( os.path.join( dirToSyncTo, ".dab.%s" % bundleID ), "rb" )
+      hash = fd.read().strip()
+      fd.close()
+      return hash
+    except:
+      return ""
+    
+  def __setHash( self, bundleID, dirToSyncTo, hash ):
+    try:
+      fileName =  os.path.join( dirToSyncTo, ".dab.%s" % bundleID )
+      fd = open( fileName, "wb" )
+      fd.write( hash )
+      fd.close()
+    except Exception, e:
+      self.log.error( "Could not save hash after synchronization", "%s: %s" % ( fileName, str(e) ) )
+
+  def syncDir( self, bundleID, dirToSyncTo ):
+    currentHash = self.__getHash( bundleID, dirToSyncTo )
+    self.log.info( "Current hash for bundle %s in dir %s is '%s'" % ( bundleID, dirToSyncTo, currentHash ) )
+    buff = cStringIO.StringIO()
+    transferClient = self.__getTransferClient()
+    result = transferClient.receiveFile( buff, ( bundleID, currentHash ) )
+    if not result[ 'OK' ]:
+      self.log.error( "Could not sync dir", result[ 'Message' ] )
+      buff.close()
+      return result
+    newHash = result[ 'Value' ]
+    if newHash == currentHash:
+      self.log.info( "Dir %s was already in sync" % dirToSyncTo )
+      return S_OK( False )
+    buff.seek( 0 )
+    self.log.info( "Synchronizing dir with remote bundle" )
+    tF = tarfile.open( mode = "r:gz", fileobj = buff )
+    for tarinfo in tF:
+      tF.extract( tarinfo, dirToSyncTo )
+    tF.close()
+    buff.close()
+    self.__setHash( bundleID, dirToSyncTo, newHash )
+    self.log.info( "Dir has been synchronized" )
+    return S_OK( True )
+  
+  def syncCAs( self ):
+    return self.syncDir( "CAs", Locations.getCAsLocation() )
+  
+  def syncCRLs( self ):
+    return self.syncDir( "CRLs", Locations.getCAsLocation() )
---
+++ FrameworkSystem/Service/BundleDeliveryHandler.py
@@ -0,0 +1,132 @@
+########################################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/FrameworkSystem/Service/BundleDeliveryHandler.py,v 1.1 2009/08/12 14:44:31 acasajus Exp $
+########################################################################
+
+""" ProxyManager is the implementation of the ProxyManagement service
+    in the DISET framework
+"""
+
+__RCSID__ = "$Id: BundleDeliveryHandler.py,v 1.1 2009/08/12 14:44:31 acasajus Exp $"
+
+import types
+import os
+import cStringIO
+import tarfile
+import md5
+from DIRAC.Core.DISET.RequestHandler import RequestHandler
+from DIRAC import gLogger, S_OK, S_ERROR, gConfig
+from DIRAC.Core.Utilities.ThreadScheduler import gThreadScheduler
+from DIRAC.Core.Utilities import File, List
+from DIRAC.Core.Security import Locations
+
+class BundleManager:
+  
+  def __init__( self, baseCSPath ):
+    self.__csPath = baseCSPath
+    self.__bundles = {}
+    self.updateBundles()
+    
+  def __getDirsToBundle( self ):
+    dirsToBundle = {}
+    result = gConfig.getOptionsDict( "%s/DirsToBundle" % self.__csPath )
+    if result[ 'OK' ]:
+      dB = result[ 'Value' ]
+      for bId in dB:
+        dirsToBundle[ bId ] =  List.fromChar( dB[ bId ] )
+    if gConfig.getValue( "%s/BundleCAs" % self.__csPath, True ):
+      dirsToBundle[ 'CAs' ] = [ "%s/*.0" % Locations.getCAsLocation() ]
+    if gConfig.getValue( "%s/BundleCRLs" % self.__csPath, True ):
+      dirsToBundle[ 'CRLs' ] = [ "%s/*.r0" % Locations.getCAsLocation() ]
+    return dirsToBundle
+  
+  def getBundles( self ):
+    return dict( [ ( bId, self.__bundles[ bId ] )  for bId in self.__bundles ] )
+  
+  def bundleExists( self, bId ):
+    return bId in self.__bundles
+  
+  def getBundleVersion( self, bId ):
+    try:
+      return self.__bundles[ bId ][0]
+    except:
+      return ""
+    
+  def getBundleData( self, bId ):
+    try:
+      return self.__bundles[ bId ][1]
+    except:
+      return ""
+    
+  def updateBundles( self ):
+    dirsToBundle = self.__getDirsToBundle()
+    #Delete bundles that don't have to be updated
+    for bId in self.__bundles:
+      if bId not in dirsToBundle:
+        gLogger.info( "Deleting old bundle %s" % bId )
+        del( self.__bundles[ bId ] )
+    for bId in dirsToBundle:
+      bundlePaths = dirsToBundle[ bId ]
+      gLogger.info( "Updating %s bundle %s" % ( bId, bundlePaths ) )
+      buffer = cStringIO.StringIO()
+      filesToBundle = sorted( File.getGlobbedFiles( bundlePaths ) )
+      commonPath = File.getCommonPath( filesToBundle )
+      commonEnd = len( commonPath )
+      gLogger.info( "Bundle will have %s files with common path %s" % ( len( filesToBundle ), commonPath ) )
+      tarBuffer = tarfile.open( None, "w:gz", buffer )
+      for filePath in filesToBundle:
+        tarBuffer.add( filePath, filePath[ commonEnd: ] )
+      tarBuffer.close()
+      zippedData = buffer.getvalue()
+      buffer.close()
+      hash = File.getMD5ForFiles( filesToBundle )
+      gLogger.info( "Bundled %s : %s bytes (%s)" % ( bId, len( zippedData ), hash ) )
+      self.__bundles[ bId ] = ( hash, zippedData )
+  
+gBundleManager = False
+
+def initializeBundleDeliveryHandler( serviceInfoDict ):
+  global gBundleManager
+  csPath = serviceInfoDict[ 'serviceSectionPath' ]
+  gBundleManager = BundleManager( csPath )
+  updateBundleTime = gConfig.getValue( "%s/BundlesLifeTime" % csPath, 3600*6 )
+  gLogger.info( "Bundles will be updated each %s secs" % updateBundleTime )
+  gThreadScheduler.addPeriodicTask( updateBundleTime, gBundleManager.updateBundles )
+  return S_OK()
+  
+
+class BundleDeliveryHandler( RequestHandler ):
+  
+  types_getListOfBundles = []
+  def export_getListOfBundles( self ):
+    global gBundleManager
+    return S_OK( gBundleManager.getBundles() )
+
+  def transfer_toClient( self, fileId, token, fileHelper ):
+    global gBundleManager
+    version = ""
+    if type( fileId ) in ( types.StringType, types.UnicodeType ):
+      bId = fileId 
+    elif type( fileId ) in ( types.ListType, types.TupleType ):
+      if len( fileId ) == 0:
+        fileHelper.markAsTransferred()
+        return S_ERROR( "No bundle specified!"  )
+      elif len( fileId ) == 1:
+        bId = fileId[0]
+      else:
+        bId = fileId[0]
+        version = fileId[1] 
+    if not gBundleManager.bundleExists( bId ):
+      fileHelper.markAsTransferred()
+      return S_ERROR( "Unknown bundle %s" % bId )
+    
+    bundleVersion = gBundleManager.getBundleVersion( bId )
+    if version == bundleVersion:
+      fileHelper.markAsTransferred()
+      return S_OK( bundleVersion )
+      
+    buffer = cStringIO.StringIO( gBundleManager.getBundleData( bId ) )
+    result = fileHelper.DataSourceToNetwork( buffer )
+    buffer.close()
+    if not result[ 'OK' ]:
+      return result
+    return S_OK( bundleVersion )
