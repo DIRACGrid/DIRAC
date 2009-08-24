b136912cc3787f639ed1c7657c994238de57221e
Florian Feldhaus <florian.feldhaus@udo.edu>
2009-08-24 14:33:15 +0000

    update for proxy handling
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@17091 4525493e-7705-40b1-a816-d608a930855b

--- Resources/Computing/TorqueComputingElement.py
+++ Resources/Computing/TorqueComputingElement.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Id: TorqueComputingElement.py,v 1.18 2009/08/15 23:26:47 ffeldhau Exp $
+# $Id: TorqueComputingElement.py,v 1.19 2009/08/24 14:33:15 ffeldhau Exp $
 # File :   TorqueComputingElement.py
 # Author : Stuart Paterson, Paul Szczypka
 ########################################################################
@@ -7,7 +7,7 @@
 """ The simplest Computing Element instance that submits jobs locally.
 """
 
-__RCSID__ = "$Id: TorqueComputingElement.py,v 1.18 2009/08/15 23:26:47 ffeldhau Exp $"
+__RCSID__ = "$Id: TorqueComputingElement.py,v 1.19 2009/08/24 14:33:15 ffeldhau Exp $"
 
 from DIRAC.Resources.Computing.ComputingElement          import ComputingElement
 from DIRAC.Core.Utilities.Subprocess                     import shellCall
@@ -17,7 +17,7 @@ from DIRAC                                               import gConfig
 from DIRAC.Core.Security.Misc                            import getProxyInfo
 
 import os,sys, time, re, socket
-import string
+import string, shutil, bz2, base64, tempfile
 
 DIRAC_PILOT   = os.path.join( rootPath, 'DIRAC', 'WorkloadManagementSystem', 'PilotAgent', 'dirac-pilot' )
 DIRAC_INSTALL = os.path.join( rootPath, 'scripts', 'dirac-install' )
@@ -50,16 +50,11 @@ class TorqueComputingElement(ComputingElement):
   def submitJob(self,executableFile,jdl,proxy,localID):
     """ Method to submit job, should be overridden in sub-class.
     """
-    self.log.verbose('Setting up proxy for payload')
-    result = self.writeProxyToFile(proxy)
-    if not result['OK']:
-      return result
-    proxyLocation = result['Value']
     
     self.log.info("Executable file path: %s" %executableFile)
     if not os.access(executableFile, 5):
       os.chmod(executableFile,0755)
-
+      
     #Perform any other actions from the site admin
     if self.ceParameters.has_key('AdminCommands'):
       commands = self.ceParameters['AdminCommands'].split(';')
@@ -69,15 +64,58 @@ class TorqueComputingElement(ComputingElement):
         if not result['OK'] or result['Value'][0]:
           self.log.error('Error during "%s":' %command,result)
           return S_ERROR('Error executing %s CE AdminCommands' %CE_NAME)
+    
+    # if no proxy is supplied, the executable can be submitted directly
+    # otherwise a wrapper script is needed to get the proxy to the execution node
+    # The wrapper script makes debugging more complicated and thus it is 
+    # recommended to transfer a proxy inside the executable if possible.
+    if proxy:
+      self.log.verbose('Setting up proxy for payload')
+      
+      compressedAndEncodedProxy = base64.b64encode( bz2.compress( proxy ) )
+      compressedAndEncodedExecutable = base64.b64encode( bz2.compress( open( executableFile, "rb" ).read(), 9 ) )
+      
+      wrapperContent = """#!/usr/bin/env python
+# Wrapper script for executable and proxy
+import os, tempfile, sys, base64, bz2
+try:
+  workingDirectory = tempfile.mkdtemp( suffix = '_wrapper', prefix= 'TORQUE_' )
+  os.chdir( workingDirectory )
+  open( 'proxy', "w" ).write(bz2.decompress( base64.b64decode( "%(compressedAndEncodedProxy)s" ) ) )
+  open( '%(executable)s', "w" ).write(bz2.decompress( base64.b64decode( "%(compressedAndEncodedExecutable)s" ) ) )
+  os.chmod('proxy',0600)
+  os.chmod('%(executable)s',0700)
+  os.environ["X509_USER_PROXY"]=os.path.join(workingDirectory, 'proxy')
+except Exception, x:
+  print >> sys.stderr, x
+  sys.exit(-1)
+cmd = "%(executable)s"
+print 'Executing: ', cmd
+sys.stdout.flush()
+os.system( cmd )
 
-    # submit run###.py to the torque batch system keeping the local env
-    cmd = "qsub -W stagein=%(proxy)s -v %(variable_list)s -o %(output)s -e %(error)s -q %(queue)s %(executable)s" % \
-      {'proxy': proxyLocation + '@' + self.hostname + ':' + '/tmp/' + os.path.basename(proxyLocation), \
-       'variable_list': 'X509_USER_PROXY=/tmp/' + os.path.basename(proxyLocation) ,
-       'output': self.batchOutput, \
+shutil.rmtree( workingDirectory )
+
+""" % { 'compressedAndEncodedProxy': compressedAndEncodedProxy, \
+        'compressedAndEncodedExecutable': compressedAndEncodedExecutable, \
+        'executable': os.path.basename(executableFile) }
+      
+      fd, name = tempfile.mkstemp( suffix = '_wrapper.py', prefix = 'TORQUE_', dir=os.getcwd())
+      wrapper = os.fdopen(fd, 'w')
+      wrapper.write( wrapperContent )
+      wrapper.close()
+      
+      submitFile = name
+        
+    else: # no proxy
+      submitFile = executableFile
+
+    # submit submitFile to the batch system
+    cmd = "qsub -o %(output)s -e %(error)s -q %(queue)s %(executable)s" % \
+      {'output': self.batchOutput, \
        'error': self.batchError, \
        'queue': self.queue, \
-       'executable': os.path.abspath( executableFile ) }
+       'executable': os.path.abspath( submitFile ) }
     
     self.log.verbose('CE submission command: %s' %(cmd))
 
