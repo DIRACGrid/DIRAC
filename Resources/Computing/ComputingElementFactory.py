cdb89e4e9fb3cbcd3a0347939f8fca62743879db
Ricardo Graciani <graciani@ecm.ub.es>
2009-08-26 16:33:29 +0000

    bug fixes
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@17145 4525493e-7705-40b1-a816-d608a930855b

--- Resources/Computing/ComputingElementFactory.py
+++ Resources/Computing/ComputingElementFactory.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Resources/Computing/ComputingElementFactory.py,v 1.2 2009/04/18 18:26:57 rgracian Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Resources/Computing/ComputingElementFactory.py,v 1.3 2009/08/26 16:33:29 rgracian Exp $
 # File :   ComputingElementFactory.py
 # Author : Stuart Paterson
 ########################################################################
@@ -8,9 +8,9 @@
      from the CEUnique ID specified in the JobAgent configuration section.
 """
 from DIRAC.Resources.Computing.ComputingElement          import ComputingElement
-from DIRAC                                               import S_OK, S_ERROR, gLogger
+from DIRAC                                               import S_OK, S_ERROR, gLogger, gConfig
 
-__RCSID__ = "$Id: ComputingElementFactory.py,v 1.2 2009/04/18 18:26:57 rgracian Exp $"
+__RCSID__ = "$Id: ComputingElementFactory.py,v 1.3 2009/08/26 16:33:29 rgracian Exp $"
 
 import sys,types
 
@@ -20,8 +20,18 @@ class ComputingElementFactory:
   def __init__(self,ceUniqueID):
     """ Standard constructor
     """
-    self.ceUniqueID = ceUniqueID
     self.log = gLogger
+    self.ceUniqueID = ceUniqueID
+    # Check if the given UniqueID is defined as CE in the /LocalSite section
+    result = gConfig.getSections( '/LocalSite' )
+    if result['OK'] and self.ceUniqueID in result['Value']:
+      # If defined it should contain an Option CEType defining the Type of CE
+      self.ceType = gConfig.getValue( '/LocalSite/%s/CEType' % self.ceUniqueID, 'None' )
+      if self.ceType == 'None':
+        self.log.error( 'CE %s does not define a CEType, will fail to instantiate' )
+    else:
+      # The UniqueID is assume to be the Type
+      self.ceType = self.ceUniqueID
     #self.log.setLevel('debug')
 
   #############################################################################
@@ -30,23 +40,23 @@ class ComputingElementFactory:
        CEUniqueID.  If no corresponding CE is available, this is indicated.
     """
     try:
-      subClassName = "%sComputingElement" % (self.ceUniqueID)
+      subClassName = "%sComputingElement" % (self.ceType)
       ceSubClass = __import__('DIRAC.Resources.Computing.%s' % subClassName,globals(),locals(),[subClassName])
     except Exception, x:
-      msg = 'ComputingElementFactory could not import DIRAC.Resources.Computing.%s' %(subClassName)
-      self.log.warn(x)
-      self.log.warn(msg)
-      return S_ERROR(msg)
+      msg = 'ComputingElementFactory could not import DIRAC.Resources.Computing.%s' % ( subClassName )
+      self.log.exception()
+      self.log.warn( msg )
+      return S_ERROR( msg )
 
     try:
-      ceStr = 'ceSubClass.%s()' % (subClassName)
-      computingElement = eval(ceStr)
+      ceStr = 'ceSubClass.%s("%s")' % ( subClassName, self.ceUniqueID )
+      computingElement = eval( ceStr )
     except Exception, x:
       msg = 'ComputingElementFactory could not instantiate %s()' %(subClassName)
-      self.log.warn(x)
-      self.log.warn(msg)
-      return S_ERROR(msg)
+      self.log.exception()
+      self.log.warn( msg )
+      return S_ERROR( msg )
 
-    return S_OK(computingElement)
+    return S_OK( computingElement )
 
 #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
