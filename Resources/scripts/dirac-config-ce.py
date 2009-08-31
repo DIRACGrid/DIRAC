cb90f9e615b0f6c56455db5d2c853710aa7ed2db
Ricardo Graciani <graciani@ecm.ub.es>
2009-08-31 16:46:31 +0000

    First version of configuration script for CEs, a dirac.cfg.new file is created and it should be copy onto the default dirac.cfg
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@17170 4525493e-7705-40b1-a816-d608a930855b

---
+++ Resources/scripts/dirac-config-ce.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+########################################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/Resources/scripts/dirac-config-ce.py,v 1.1 2009/08/31 16:46:31 rgracian Exp $
+# File :   dirac-config-ce
+# Author : Ricardo Graciani
+########################################################################
+__RCSID__   = "$Id: dirac-config-ce.py,v 1.1 2009/08/31 16:46:31 rgracian Exp $"
+__VERSION__ = "$Revision: 1.1 $"
+from DIRACEnvironment import DIRAC
+from DIRAC.Core.Base import Script
+from DIRAC import S_OK, gConfig, gLogger
+
+configFile = gConfig.diracConfigFilePath
+
+ceName = ''
+ceType = ''
+
+def setCEName( args ):
+  global ceName
+  ceName = args
+  return S_OK()
+
+def setCEType( args ):
+  global ceType
+  ceType = args
+  return S_OK()
+
+Script.registerSwitch( "N:", "Name=", "Computing Element Name", setCEName )
+Script.registerSwitch( "T:", "Type=", "Computing Element Type", setCEType )
+
+Script.parseCommandLine( ignoreErrors = True )
+
+if not ceName or not ceType:
+  gLogger.error( 'Missing Mandatory Argument "-N CEName" or "-T CEType"' )
+  Script.localCfg._LocalConfiguration__showHelp()
+
+gLogger.info( 'Configuring CE:', ceName )
+
+from DIRAC.Core.Utilities.CFG import CFG
+cfg = CFG()
+cfg.loadFromFile(configFile)
+
+localsiteCfg = cfg['LocalSite']
+if localsiteCfg.existsKey( ceName ):
+  gLogger.info(' Removing existing configuration', localsiteCfg.deleteKey( ceName ) )
+localsiteCfg.createNewSection( ceName )
+
+ceCfg = localsiteCfg[ceName]
+
+cfgSection = '/Scripts/%s' % Script.localCfg.componentName
+gConfig.setOptionValue( cfgSection+'/CEType', ceType )
+
+configDict = gConfig.getOptionsDict(cfgSection)['Value']
+configOptions = configDict.keys()
+configOptions.sort()
+
+# Add script command line options to CE Configuration
+for option in configOptions:
+  value = configDict[option]
+  gLogger.info(' Adding CE option', '%s = %s' % ( option, value ) )
+  ceCfg.setOption( option, value )
+
+# Now ResourceDict (if it exists)
+resSection = cfgSection + '/ResourceDict'
+resDict = gConfig.getOptionsDict( resSection )
+if resDict['OK']:
+  ceCfg.createNewSection('ResourceDict')
+  resCfg = ceCfg['ResourceDict']
+  resDict = resDict['Value']
+  resOptions = resDict.keys()
+  resOptions.sort()
+  for option in resOptions:
+    value = resDict[option]
+    gLogger.info(' Adding ResourceDict option ', '%s = %s' % ( option, value ) )
+    resCfg.setOption( option, value )
+
+# Now load this cfg into gConfig and try to instantiate the CE
+gConfig.loadCFG( cfg )
+from DIRAC.Resources.Computing.ComputingElementFactory    import ComputingElementFactory
+ceFactory = ComputingElementFactory( ceName )
+try:
+  ceInstance = ceFactory.getCE()
+except Exception, x:
+  DIRAC.abort( -1, 'Fail to instantiate CE', x )
+if not ceInstance['OK']:
+  DIRAC.abort( -1, 'Fail to instantiate CE', ceInstance['Message'] )
+
+# Everything is OK, we can save the new cfg (for the moment let's copy by hand
+newConfigFile = configFile+'.new'
+cfg.writeToFile( newConfigFile )
+gLogger.always( 'Copy %s to %s for final installation' % ( newConfigFile, configFile ) )
+
+
