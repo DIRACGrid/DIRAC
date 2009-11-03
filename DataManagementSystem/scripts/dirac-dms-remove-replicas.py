e62a78479fa2c54a24ab07c9bf783bf78f7c6355
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2009-11-03 10:36:28 +0000

    remove replicas in chunks of 500.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@17987 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/scripts/dirac-dms-remove-replicas.py
+++ DataManagementSystem/scripts/dirac-dms-remove-replicas.py
@@ -2,12 +2,12 @@
 from DIRAC.Core.Base.Script import parseCommandLine
 parseCommandLine()
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/DataManagementSystem/scripts/dirac-dms-remove-replicas.py,v 1.2 2009/03/05 09:30:02 acsmith Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/DataManagementSystem/scripts/dirac-dms-remove-replicas.py,v 1.3 2009/11/03 10:36:28 acsmith Exp $
 ########################################################################
-__RCSID__   = "$Id: dirac-dms-remove-replicas.py,v 1.2 2009/03/05 09:30:02 acsmith Exp $"
+__RCSID__   = "$Id: dirac-dms-remove-replicas.py,v 1.3 2009/11/03 10:36:28 acsmith Exp $"
 __VERSION__ = "$ $"
 
-from DIRAC.Core.Utilities.List import sortList
+from DIRAC.Core.Utilities.List                        import sortList,breakListIntoChunks
 from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
 rm = ReplicaManager()
 import os,sys
@@ -27,12 +27,12 @@ if os.path.exists(inputFileName):
 else:
   lfns = [inputFileName]
 
-res = rm.removeReplica(storageElementName,lfns)
-if not res['OK']:
-  print res['Message']
-  sys.exit()
-for lfn in sortList(res['Value']['Successful'].keys()):
-  print 'Successfully removed %s replica of %s' % (storageElementName,lfn)
-for lfn in sortList(res['Value']['Failed'].keys()):
-  message = res['Value']['Failed'][lfn]
-  print 'Failed to remove %s replica of %s: %s' % (storageElementName,lfn,message)
+for lfnList in breakListIntoChunks(sortList(lfns,True),500):
+  res = rm.removeReplica(storageElementName,lfnList)
+  if not res['OK']:
+    print res['Message']
+  for lfn in sortList(res['Value']['Successful'].keys()):
+    print 'Successfully removed %s replica of %s' % (storageElementName,lfn)
+  for lfn in sortList(res['Value']['Failed'].keys()):
+    message = res['Value']['Failed'][lfn]
+    print 'Failed to remove %s replica of %s: %s' % (storageElementName,lfn,message)
