e93e7aa9ee3ef7a3b6999737cfe27ab4953b980d
Adria Casajus <adria@ecm.ub.es>
2009-11-05 20:52:16 +0000

    Deploy scripts in root/scripts directory
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@18066 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/scripts/dirac-deploy.py
@@ -0,0 +1,101 @@
+# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC/WorkloadManagementSystem/Agent/JobCleaningAgent.py $
+"""
+Deploy all scripts and extensions
+"""
+__RCSID__ = "$Id: JobCleaningAgent.py 18064 2009-11-05 19:40:01Z acasajus $"
+
+import os
+import shutil
+import stat
+
+moduleSuffix = "DIRAC"
+defaultPerms = stat.S_IWUSR | stat.S_IRUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH  
+
+wrapperTemplate = """#!/usr/bin/env python
+#
+import os,sys,popen2
+#
+if 'DIRACROOT' in os.environ:
+  DiracRoot = os.environ['DIRACROOT']
+else:
+  DiracRoot = os.path.dirname(os.path.dirname( os.path.realpath( sys.argv[0] ) ))
+if 'DIRACPLAT' in os.environ:
+  DiracPlatform = os.environ['DIRACPLAT']
+else:
+  dirac_platform = os.path.join(DiracRoot,"scripts","platform.py")
+  if not os.path.exists( dirac_platform ):
+    print >> sys.stderr, "Missing file %s" % dirac_platform
+    sys.exit(-1)
+  p3 = popen2.Popen3( dirac_platform )
+  DiracPlatform = p3.fromchild.read().strip()
+  p3.wait()
+  if not DiracPlatform or DiracPlatform == "ERROR":
+    print >> sys.stderr, "Can not determine local platform"
+    sys.exit(-1)
+DiracPath        = '%s' % ( os.path.join(DiracRoot,DiracPlatform,'bin'), )
+DiracLibraryPath = '%s' % ( os.path.join(DiracRoot,DiracPlatform,'lib'), )
+DiracPythonPath  = '%s' % ( os.path.join(DiracRoot,'scripts'), )
+
+os.environ['PATH'] = '%s:%s' % ( DiracPath, os.environ['PATH'] )
+
+if 'LD_LIBRARY_PATH' not in os.environ:
+  os.environ['LD_LIBRARY_PATH'] = DiracLibraryPath
+else:
+  os.environ['LD_LIBRARY_PATH'] = '%s:%s' % ( DiracLibraryPath, os.environ['LD_LIBRARY_PATH'] )
+
+if 'PYTHONPATH' not in os.environ:
+  os.environ['PYTHONPATH'] = DiracPythonPath
+else:
+  os.environ['PYTHONPATH'] = '%s:%s' % ( DiracPythonPath, os.environ['PYTHONPATH'] )
+
+DiracScript = os.path.join( DiracRoot, '$SCRIPTLOCATION$' )
+
+if sys.argv[1:]:
+  args = ' "%s"' % '" "'.join( sys.argv[1:] )
+else:
+  args = ''
+sys.exit( os.system('python "%s"%s' % ( DiracScript, args )  ) / 256 )
+"""
+
+
+def lookForScriptsInPath( basePath, rootModule ):
+  isScriptsDir = os.path.split( rootModule )[1] == "scripts"
+  scriptFiles = []
+  for entry in os.listdir( basePath ):
+    absEntry = os.path.join( basePath, entry )
+    if os.path.isdir( absEntry ):
+      scriptFiles.extend( lookForScriptsInPath( absEntry, os.path.join( rootModule, entry ) ) )
+    elif isScriptsDir and os.path.isfile( absEntry ):
+      scriptFiles.append( ( os.path.join( rootModule, entry ), entry ) )
+  return scriptFiles
+
+rootPath = os.path.realpath( __file__ )
+for i in range( 4 ):
+  rootPath = os.path.dirname( rootPath )
+
+scriptsPath = os.path.join( rootPath, "scripts" )
+if not os.path.isdir( scriptsPath ):
+  os.mkdir( scriptsPath )
+
+for rootModule in os.listdir( rootPath ):
+  modulePath = os.path.join( rootPath, rootModule )
+  if not os.path.isdir( modulePath ):
+    continue
+  extSuffixPos = rootModule.find( moduleSuffix )
+  if extSuffixPos == -1 or extSuffixPos != len( rootModule ) - len( moduleSuffix ):
+    continue
+  print "Inspecting %s module" % rootModule
+  scripts = lookForScriptsInPath( modulePath, rootModule )
+  for script in scripts:
+    scriptPath = script[0]
+    scriptName = script[1]
+    scriptLen = len( scriptName )
+    if scriptName.find( ".py" ) == scriptLen - 3 and scriptName.find( "dirac-" ) == 0:
+      fakeScriptPath = os.path.join( scriptsPath, scriptName[:-3] )
+      fd = open( fakeScriptPath, "w" )
+      fd.write( wrapperTemplate.replace( '$SCRIPTLOCATION$', scriptPath ) )
+      fd.close()
+      os.chmod( fakeScriptPath, defaultPerms )
+    else:
+      shutil.copy( os.path.join( rootPath, scriptPath ), scriptsPath )
+      os.chmod( os.path.join( scriptsPath, scriptName ), defaultPerms )
