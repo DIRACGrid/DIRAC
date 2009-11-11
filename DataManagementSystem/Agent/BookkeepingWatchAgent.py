1ed60756306a4b7fbe679e7db46c1750e828f5ec
Stuart K. Paterson <stuart.paterson@cern.ch>
2009-11-11 16:51:47 +0000

    +LHCb to import statement
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@18192 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Agent/BookkeepingWatchAgent.py
+++ DataManagementSystem/Agent/BookkeepingWatchAgent.py
@@ -5,7 +5,7 @@
 __RCSID__ = "$Id$"
 
 from DIRAC                                            import S_OK, S_ERROR, gConfig, gLogger, gMonitor
-from DIRAC.LHCbSystem.Agent.BookkeepingWatchAgent     import BookkeepingWatchAgent
+from LHCbDIRAC.LHCbSystem.Agent.BookkeepingWatchAgent     import BookkeepingWatchAgent
 
 
 AGENT_NAME = 'DataManagement/BookkeepingWatchAgent'
