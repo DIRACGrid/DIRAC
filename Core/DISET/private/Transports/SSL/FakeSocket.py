087b1986219c9df4439eb695783ea671fc64e00f
Adria Casajus <adria@ecm.ub.es>
2009-11-20 17:27:24 +0000

    Allow to pass copies by default
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@18441 4525493e-7705-40b1-a816-d608a930855b

--- Core/DISET/private/Transports/SSL/FakeSocket.py
+++ Core/DISET/private/Transports/SSL/FakeSocket.py
@@ -14,11 +14,12 @@ class FakeSocket:
   def __getattr__(self, name):
     return getattr( self.sock, name )
 
-  def __init__(self, sock):
-    self.iCopies = 0
+  def __init__(self, sock, copies = 0):
+    self.iCopies = copies
     self.sock = sock
 
   def close(self):
+    print "CLosE of thE SoCkeEt"
     if self.iCopies == 0:
       self.sock.shutdown()
       self.sock.close()
@@ -26,5 +27,6 @@ class FakeSocket:
       self.iCopies -= 1
 
   def makefile(self, mode, bufsize=None):
+    print "MAKEFILE of sOcKEt"
     self.iCopies += 1
     return socket._fileobject( self.sock, mode, bufsize)
