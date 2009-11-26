f221995967fc5d936db38ecd4fff29f6ae092511
Adria Casajus <adria@ecm.ub.es>
2009-11-26 11:32:07 +0000

    Initial commit of VOMSAdmin
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@18649 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Security/VOMSAdmin.py
@@ -0,0 +1,52 @@
+# $HeadURL$
+__RCSID__ = "$Id$"
+
+from DIRAC import gConfig, S_OK, S_ERROR
+from DIRAC.Core.Utilities.SOAPFactory import getSOAPClient
+
+class VOMSAdmin:
+  
+  def __init__( self, url = False ):
+    if not url:
+      url = gConfig.getValue( "/Security/VOMSAdminURL", "" )
+    if not url:
+      raise Exception( "No URL defined for VOMSAdmin" )
+    self.__soapClient = getSOAPClient( "%s?wsdl" % url )
+    
+  def __processListReturn( self, soapReturn ):
+    data = []
+    for entry in soapReturn:
+      data.append( str( entry ) )
+    return data
+  
+  def __processListDictReturn( self, soapReturn ):
+    data = []
+    for entry in soapReturn:
+      entryData = {}
+      for info in entry:
+        entryData[ info[0] ] = str( info[1] )
+      data.append( entryData )
+    return S_OK( data )
+  
+  def listMembers(self):
+    try:
+      result = self.__soapClient.service.listMembers()
+    except Exception, e:
+      return S_ERROR( "Error in function listMembers: %s" %  str(e) )
+    return S_OK( self.__processListDictReturn( result.listMembersReturn ) )
+  
+  def listRoles( self ):
+    try:
+      result = self.__soapClient.service.listRoles()
+    except Exception, e:
+      return S_ERROR( "Error in function listRoles: %s" %  str(e) )
+    return S_OK( self.__processListReturn( result.listRolesReturn ) )
+    
+  
+  def listUsersWithRole( self, group, role ):
+    try:
+      result = self.__soapClient.service.listUsersWithRole( group, role )
+    except Exception, e:
+      return S_ERROR( "Error in function listUsersWithRole: %s" %  str(e) )
+    return S_OK( self.__processListDictReturn( result.listUsersWithRoleReturn ) )
+  
