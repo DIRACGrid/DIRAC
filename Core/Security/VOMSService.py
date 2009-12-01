4cfb6e1db9a5089335d60a87e37d432fcc1f6a91
Adria Casajus <adria@ecm.ub.es>
2009-12-01 19:54:46 +0000

    Moved VOMSAdmin to VOMSService because it also interfaces with VOMSAttributes
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@18847 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/Security/VOMSService.py
@@ -0,0 +1,60 @@
+# $HeadURL$
+__RCSID__ = "$Id$"
+
+from DIRAC import gConfig, S_OK, S_ERROR
+from DIRAC.Core.Utilities.SOAPFactory import getSOAPClient
+
+class VOMSService:
+
+  def __init__( self, adminUrl = False, attributesUrl = False ):
+    self.__soapClients = {}
+    for key, url in ( ( 'Admin', adminUrl ), ( 'Attributes', attributesUrl ) ):
+      if not url:
+        url = gConfig.getValue( "/Registry/URLs/VOMS%s" % key, "" )
+      if not url:
+        raise Exception( "No URL defined for VOMS%s" % key )
+      self.__soapClients[ key ] = getSOAPClient( "%s?wsdl" % url )
+
+  def __processListReturn( self, soapReturn ):
+    data = []
+    for entry in soapReturn:
+      data.append( str( entry ) )
+    return data
+
+  def __processListDictReturn( self, soapReturn ):
+    data = []
+    for entry in soapReturn:
+      entryData = {}
+      for info in entry:
+        entryData[ info[0] ] = str( info[1] )
+      data.append( entryData )
+    return data
+
+  def admListMembers( self ):
+    try:
+      result = self.__soapClients[ 'Admin' ].service.listMembers()
+    except Exception, e:
+      return S_ERROR( "Error in function listMembers: %s" % str( e ) )
+    return S_OK( self.__processListDictReturn( result.listMembersReturn ) )
+
+  def admListRoles( self ):
+    try:
+      result = self.__soapClients[ 'Admin' ].service.listRoles()
+    except Exception, e:
+      return S_ERROR( "Error in function listRoles: %s" % str( e ) )
+    return S_OK( self.__processListReturn( result.listRolesReturn ) )
+
+
+  def admListUsersWithRole( self, group, role ):
+    try:
+      result = self.__soapClients[ 'Admin' ].service.listUsersWithRole( group, role )
+    except Exception, e:
+      return S_ERROR( "Error in function listUsersWithRole: %s" % str( e ) )
+    return S_OK( self.__processListDictReturn( result.listUsersWithRoleReturn ) )
+
+  def admGetVOName( self ):
+    try:
+      result = self.__soapClients[ 'Admin' ].service.getVOName()
+    except Exception, e:
+      return S_ERROR( "Error in function listUsersWithRole: %s" % str( e ) )
+    return S_OK( result )
