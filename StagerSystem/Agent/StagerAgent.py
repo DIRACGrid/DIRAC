dac17e0d84342c90f14e550c0809a639d04529ef
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2009-12-02 17:31:28 +0000

    Updated to the new agent framework
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@18895 4525493e-7705-40b1-a816-d608a930855b

--- StagerSystem/Agent/StagerAgent.py
+++ StagerSystem/Agent/StagerAgent.py
@@ -13,31 +13,26 @@
 
 __RCSID__ = "$Id$"
 
-from DIRAC.Core.Base.Agent                                 import Agent
+from DIRAC.Core.Base.AgentModule                           import AgentModule
 from DIRAC.Core.DISET.RPCClient                            import RPCClient
 from DIRAC.Core.Utilities.Shifter                          import setupShifterProxyInEnv
 from DIRAC                                                 import S_OK, S_ERROR, gConfig, gLogger
-
+from DIRAC.ConfigurationSystem.Client                      import PathFinder
 import os, sys, re, string, time
 
 AGENT_NAME = 'Stager/StagerAgent'
 
-class StagerAgent(Agent):
-
-  #############################################################################
-  def __init__(self):
-    """ Standard constructor for Agent
-    """
-    Agent.__init__(self,AGENT_NAME)
+class StagerAgent(AgentModule):
 
   #############################################################################
   def initialize(self):
     """Sets defaults
     """
-    result = Agent.initialize(self)
-    self.pollingTime = gConfig.getValue(self.section+'/PollingTime',120)
-    self.threadStartDelay = gConfig.getValue(self.section+'/ThreadStartDelay',5)
-    self.siteStager = gConfig.getValue(self.section+'/ModulePath','DIRAC.StagerSystem.Agent.SiteStager')
+
+    self.section = PathFinder.getAgentSection( AGENT_NAME )
+    self.pollingTime = self.am_getOption('PollingTime',120)
+    self.threadStartDelay = self.am_getOption('ThreadStartDelay',5)
+    self.siteStager = self.am_getOption('ModulePath','DIRAC.StagerSystem.Agent.SiteStager')
     self.started = False
     try:
       self.importModule = __import__(self.siteStager,globals(),locals(),['SiteStager'])
@@ -47,14 +42,20 @@ class StagerAgent(Agent):
       self.log.warn(msg)
       return S_ERROR(msg)
     return result
+  
+    self.proxyLocation = self.am_getOption('ProxyLocation', '' )
+    if not self.proxyLocation:
+      self.proxyLocation = False
+
+    self.am_setModuleParam('shifter','ProductionManager')
+    self.am_setModuleParam('shifterProxyLocation',self.proxyLocation)
+    
+    return S_OK()
 
   #############################################################################
   def execute(self):
     """The StagerAgent execution method.
     """
-    result = setupShifterProxyInEnv( "ProductionManager" )
-    if not result[ 'OK' ]:
-      return S_ERROR( "Can't get shifter's proxy: %s" % result[ 'Message' ] )
 
     agent = {}
     if not self.started:
