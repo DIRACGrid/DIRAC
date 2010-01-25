08e1b0bd42cb75a1838d02627f8d768e8733a92e
Stuart K. Paterson <stuart.paterson@cern.ch>
2010-01-25 16:02:16 +0000

    Added global error dictionary and method to report new errors that can be retrieved later.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@20498 4525493e-7705-40b1-a816-d608a930855b

--- Core/Base/API.py
+++ Core/Base/API.py
@@ -8,11 +8,13 @@ __RCSID__ = "$Id: API.py 19233 2009-12-04 22:40:35Z acsmith $"
 
 from DIRAC.Core.Base                import Script
 Script.parseCommandLine()
+
 from DIRAC                          import gConfig, gLogger, S_OK, S_ERROR
 from DIRAC.Core.Utilities.List      import sortList
 from DIRAC.Core.Security.Misc       import getProxyInfo,formatProxyInfoAsString
 from DIRAC.Core.Security.CS         import getDNForUsername
-import string,pprint 
+
+import string,pprint,sys
 
 COMPONENT_NAME='API'
 
@@ -23,6 +25,8 @@ class API:
     self.log = gLogger.getSubLogger(COMPONENT_NAME)
     self.section = COMPONENT_NAME
     self.pPrint = pprint.PrettyPrinter()
+    #Global error dictionary
+    self.errorDict = {}      
 
   #############################################################################
   def _errorReport(self,error,message=None):
@@ -100,3 +104,34 @@ class API:
     if not res['OK']:
       return self._errorReport('Failed to get proxies for user',res['Message'])
     return S_OK(proxyInfo['username'])
+  
+  #############################################################################
+  def _reportError(self,message,name='',**kwargs):
+    """Internal Function. Gets caller method name and arguments, formats the 
+       information and adds an error to the global error dictionary to be 
+       returned to the user. 
+    """
+    className = name
+    if not name:
+      className = __name__
+    methodName = sys._getframe(1).f_code.co_name
+    arguments = []
+    for key in kwargs:
+      if kwargs[key]:
+        arguments.append('%s = %s ( %s )' %(key,kwargs[key],type(kwargs[key])))
+    finalReport = 'Problem with %s.%s() call:\nArguments: %s\nMessage: %s\n' %(className,methodName,string.join(arguments,', '),message)
+    if self.errorDict.has_key(methodName):
+      tmp = self.errorDict[methodName]
+      tmp.append(finalReport)
+      self.errorDict[methodName]=tmp
+    else:  
+      self.errorDict[methodName]=[finalReport]
+    self.log.verbose(finalReport)
+    return S_ERROR(finalReport) 
+
+  #############################################################################
+  def _getErrors(self):
+    """Returns the dictionary of stored errors.
+    """
+    return self.errorDict
+   
