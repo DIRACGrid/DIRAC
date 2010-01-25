38553d5aeee8a3b2024ea3c1b49eb82639e39d1c
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2010-01-25 21:55:36 +0000

    added auto resolution of get and set.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@20550 4525493e-7705-40b1-a816-d608a930855b

--- Interfaces/API/Transformation.py
+++ Interfaces/API/Transformation.py
@@ -15,11 +15,7 @@ COMPONENT_NAME='Transformation'
 class Transformation(API):
 
   #############################################################################
-  def __init__(self):
-    self.transClient = TransformationDBClient()
-    #TODO REMOVE THIS
-    self.transClient.setServer("ProductionManagement/ProductionManager")
-
+  def __init__(self,transID=0):
     self.paramTypes =   { 'TransformationID'      : [types.IntType,types.LongType],
                           'TransformationName'    : types.StringTypes,
                           'Status'                : types.StringTypes,
@@ -51,51 +47,97 @@ class Transformation(API):
                           'MaxNumberOfJobs'       : 0,
                           'EventsPerJob'          : 0}
     self.extraParams  = {}
+
+    self.transClient = TransformationDBClient()
+    #TODO REMOVE THIS
+    self.transClient.setServer("ProductionManagement/ProductionManager")
     self.exists = False
     if transID:
-      res = self.getTransformation(transID)
+      self.paramValues['TransformationID'] = transID
+      res = self.getTransformation()
       if res['OK']:
         self.exists = True
       else:
+        self.paramValues['TransformationID'] = 0
         gLogger.fatal("The supplied transformation does not exist in transformation database", "%s @ %s" % (transID,self.transClient.serverURL))
-        return S_ERROR()
 
+  def __getattr__(self,name):
+    if name.find('get') ==0:
+      item = name[3:]
+      self.item_called = item
+      return self.__getParam
+    if name.find('set') == 0:
+      item = name[3:]
+      self.item_called = item
+      return self.__setParam
+    raise AttributeError, name
+
+  def __getParam(self):
+    if self.item_called == 'Parameters':
+      return S_OK(self.paramValues.keys()+self.extraParams.keys())
+    if self.item_called in self.paramValues.keys():
+      return S_OK(self.paramValues[self.item_called])
+    if self.item_called in self.extraParams.keys():
+      return S_OK(self.extraParams[self.item_called])
+    raise AttributeError, "Unknown parameter for transformation: %s" % self.item_called
+
+  def __setParam(self,value):
+    if not self.item_called in self.paramTypes.keys():
+       self.extraParams[self.item_called] = value
+    else:
+      if type(value) in self.paramTypes[self.item_called]:
+        self.paramValues[self.item_called] = value
+      else:
+        raise TypeError, "%s %s %s expected one of %s" % (self.item_called,value,type(value),self.paramTypes[self.item_called])
+    return S_OK()
       
-  def getTransformation(self,transID,printOutput=False):
+  def getTransformation(self,printOutput=False):
+    transID = self.paramValues['TransformationID']
+    if not transID:
+      gLogger.fatal("No TransformationID known")
+      return S_ERROR()
     res = self.transClient.getTransformation(transID,extraParams=True)
     if not res['OK']:
       return res
-    for paramName,parmValue in res['Value'].items():
-      if paramName in self.paramValues.keys():
-        self.paramValues[paramName] = paramValue
-      else:
-        self.extraParams[paramName] = paramValue
-    
+    transParams = res['Value']
+    for paramName,paramValue in transParams.items():
+      self.item_called = paramName
+      self.__setParam(paramValue)
+    if printOutput:
+      gLogger.info("No printing available yet")
+    return S_OK(transParams)
 
-  def getTransformationLogging(self,transID,printOutput=False):
-    """The logging information for the given transformation is returned. """
+  def getTransformationLogging(self,printOutput=False):
+    transID = self.paramValues['TransformationID']
+    if not transID:
+      gLogger.fatal("No TransformationID known")
+      return S_ERROR()
+    res = self.transClient.getTransformationLogging(transID)
+    if not res['OK']:
+      return res
+    loggingList = res['Value']
+    if printOutput:
+      self._printFormattedDictList(loggingList,['Message','MessageDate','AuthorDN'],'MessageDate','MessageDate')
+    return S_OK(loggingList)
+
+  def getTransformationProgress(self,transID=None,printOutput=False):
     pass
 
-    
-  def __getattr__(self,name):
-    if name.startswith('get'):
-      paramName = name.replace('get','')
-      if paramName in self.paramValues.keys():
-        return S_OK(self.paramValues[paramName])
-    if name.startswith('set'):
-      paramName = name.replace('set','')
-      paramValue = *parms[0]
-      if paramName in self.paramValues.keys():
-        if not (type(paramValue) in self.paramTypes[paramName]):
-          return self._reportError() # TODO FILL ARGUMENTS
-        self.paramValues[paramName] = paramValue
-        if paramName = 'TransformationID':
-          self.exists = True
-      else:
-        paramValues[paramName] = paramValue
-      return S_OK()
-    raise AttributeError
-  
+  def extendTransformation(self,transID,nTasks,printOutput=False):
+    pass
+
+  def deleteTransformation(self,transName,printOutput=False):
+    pass
+          
+  def cleanTransformation(self,transName,printOutput=False):
+    pass
+          
+  def setTransformationParameter(self,transName,paramName,paramValue,printOutput=False):
+    pass
+
+  def setTransformationStatus(self,transName,status,printOutput=False):
+    pass
+
   def addTransformation(self,transName,description,longDescription,type,plugin,agentType,fileMask,
                             transformationGroup = 'General',
                             groupSize           = 1,
@@ -106,7 +148,9 @@ class Transformation(API):
                             addFiles            = True,
                             printOutput = False):
     pass
-    
+
+
+
   #############################################################################
   def getTransformations(self,transID=[], transStatus=[], outputFields=['TransformationID','Status','AgentType','TransformationName','CreationDate'],orderBy='TransformationID',printOutput=False):
     condDict = {}
@@ -125,18 +169,6 @@ class Transformation(API):
     return res
 
   #############################################################################
-  def getTransformationLogging(self,transID,printOutput=False):
-    """The logging information for the given transformation is returned.
-    """
-    pass
-
-  #############################################################################
-  def getTransformationProgress(self,transID=None,printOutput=False):
-    """Returns the status of transformation
-    """
-    pass
-  
-  #############################################################################
   def checkFilesStatus(self,lfns,transID='',printOutput=False):
     """ Checks the given LFN(s) status in the transformation database. """
     pass
@@ -147,39 +179,6 @@ class Transformation(API):
     pass
 
   #############################################################################
-  def getTransformationTaskInfo(self,transID,taskID,printOutput=False):
-    """Retrieve transformation task information from transformation database """
-    res = self.transClient.getTransformationTasks(condDict={'TransformationID':transID,'JobID':taskID},inputVector=True)
-    pass
-  
-  #############################################################################
-  def extendTransformation(self,transID,nTasks,printOutput=False):
-    """ Extend Simulation type transformation by number of tasks. """
-    pass
-
-  #############################################################################
-  def addTransformation(self,transName,description,longDescription,type,plugin,agentType,fileMask,
-                            transformationGroup = 'General',
-                            groupSize           = 1,
-                            inheritedFrom       = 0,
-                            body                = '', 
-                            maxJobs             = 0,
-                            eventsPerJob        = 0,
-                            addFiles            = True,
-                            printOutput = False):
-    pass
-              
-  def deleteTransformation(self,transName,printOutput=False):
-    pass
-          
-  def cleanTransformation(self,transName,printOutput=False):
-    pass
-          
-  def setTransformationParameter(self,transName,paramName,paramValue,printOutput=False):
-    pass
-
-  def setTransformationStatus(self,transName,status,printOutput=False):
-    pass
 
   def getTransformationParameters(self,transName,paramNames,printOutput=False):
     pass
@@ -215,9 +214,6 @@ class Transformation(API):
     pass
 
   def getTransformationStats(self,transName, printOutput=False):
-    pass
-
-  def getTransformation(self,transName,extraParams=False,printOutput=False):
     pass
 
   def getTransformationFiles(self,condDict={},older=None, newer=None, timeStamp='LastUpdate', orderAttribute=None, limit=None, printOutput=False):
