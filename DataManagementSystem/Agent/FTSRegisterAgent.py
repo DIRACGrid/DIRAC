43afef279fae81589b271e6499ad03095eaa0702
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2010-02-04 22:39:26 +0000

    removed some old code that was unused.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@20813 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Agent/FTSRegisterAgent.py
+++ DataManagementSystem/Agent/FTSRegisterAgent.py
@@ -43,43 +43,6 @@ class FTSRegisterAgent(AgentModule):
     if not res['OK']:
       gLogger.error("FTSRegisterAgent.execute: Failed to get the completed replications from TransferDB.",res['Message'])
       return S_OK()
-    filesToRemove = {}
-    for operation,sourceSE,lfn in res['Value']:
-      # This should get us the files that were supposed to be moved
-      if re.search('ralmigration',operation.lower()):
-        if not filesToRemove.has_key(sourceSE):
-          filesToRemove[sourceSE] = []
-        filesToRemove[sourceSE].append(lfn)
-
-    for sourceSE,lfns in filesToRemove.items():
-      #tmp hack until the ral people sort it out!!
-      from DIRAC.RequestManagementSystem.Client.RequestClient import RequestClient
-      from DIRAC.RequestManagementSystem.Client.DataManagementRequest import DataManagementRequest
-      import time
-      client = RequestClient()
-      oRequest = DataManagementRequest()
-      subRequestIndex = oRequest.initiateSubRequest('removal')['Value']
-      attributeDict = {'Operation':'replicaRemoval','TargetSE':sourceSE}
-      oRequest.setSubRequestAttributes(subRequestIndex,'removal',attributeDict)
-      filesList = []
-      for lfn in lfns:
-        filesList.append({'LFN':lfn})
-      oRequest.setSubRequestFiles(subRequestIndex,'removal',filesList)
-      requestString = oRequest.toXML()['Value']
-      requestName = 'RAL-removal-FTSRegister.%s' % time.time()
-      res = client.setRequest(requestName,requestString,'dips://volhcb03.cern.ch:9143/RequestManagement/RequestManager')
-      print res['OK']
-      """
-      gLogger.info("FTSRegisterAgent.execute:  Attemping to remove %s file(s) from %s." % (len(lfns),sourceSE))
-      print lfns[0]
-      res = self.ReplicaManager.removeReplica(sourceSE,lfns)
-      if not res['OK']:
-        gLogger.error("FTSRegisterAgent.execute: Completely failed to remove replicas.",res['Message'])
-        return S_OK()
-      for lfn in res['Value']['Successful'].keys():
-        print 'successful'
-        self.DataLog.addFileRecord(lfn,'RemoveReplica',sourceSE,'','FTSRegisterAgent')
-      """
 
     res = self.TransferDB.getWaitingRegistrations()
     if not res['OK']:
@@ -88,6 +51,8 @@ class FTSRegisterAgent(AgentModule):
     lfns = {}
     replicaTuples = []
     for fileID,channelID,lfn,pfn,se in res['Value']:
+      if lfns.has_key(lfn):
+        continue
       lfns[lfn] = (channelID,fileID,se)
       replicaTuples.append((lfn,pfn,se))
 
