1161b5167df7ebf3c59b12f11d067ab45b4b89dc
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2010-02-06 23:51:59 +0000

    Added installMySQL() and installDatabase() methods
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@20853 4525493e-7705-40b1-a816-d608a930855b

--- FrameworkSystem/Client/SystemAdministratorClient.py
+++ FrameworkSystem/Client/SystemAdministratorClient.py
@@ -8,8 +8,8 @@
 
 __RCSID__ = "$Id:  $"
 
-import re, time, random, os, types
-
+import re, time, random, os, types, getpass
+from DIRAC.Core.DISET.RPCClient  import RPCClient
 from DIRAC import S_OK, S_ERROR, gLogger,gConfig
 from DIRAC.Core.Base.Client import Client
 
@@ -24,3 +24,43 @@ class SystemAdministratorClient(Client):
       self.serverURL = url
     else:  
       self.serverURL = 'Framework/SystemAdministrator'
+      
+  def getDatabases(self,password=None):
+    """ Get the installed databases
+    """
+    if not password:
+      pword = getpass.getpass('MySQL root password: ')
+    else:
+      pword = password  
+    server = RPCClient(self.serverURL)
+    return server.getDatabases(pword)    
+  
+  def installMySQL(self,rootpwd=None,diracpwd=None):
+    """ Install the MySQL database on the server side
+    """
+    if not rootpwd:
+      rpword = getpass.getpass('MySQL root password: ')
+    else:
+      rpword = rootpwd
+    if not diracpwd:
+      dpword = getpass.getpass('MySQL Dirac password: ')
+    else:
+      dpword = diracpwd
+      
+    server = RPCClient(self.serverURL)
+    return server.installMySQL(rpword,dpword)
+  
+  def installDatabase(self,database,rootpwd=None):
+    """ Install the MySQL database on the server side
+    """
+    if not rootpwd:
+      rpword = getpass.getpass('MySQL root password: ')
+    else:
+      rpword = rootpwd
+      
+    server = RPCClient(self.serverURL)
+    return server.installDatabase(rpword,database)      
+    
+    
+    
+    
