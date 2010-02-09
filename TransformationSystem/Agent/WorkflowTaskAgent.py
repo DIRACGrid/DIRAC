568b34b82e40dd16ee4f910c8e54b3c24ec653c4
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2010-02-09 20:17:06 +0000

    agent for submitting productions. to be moved to production management system.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@21089 4525493e-7705-40b1-a816-d608a930855b

---
+++ TransformationSystem/Agent/WorkflowTaskAgent.py
@@ -0,0 +1,22 @@
+########################################################################
+# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC/TransformationSystem/Agent/WorkflowTaskAgent.py $
+########################################################################
+"""  The Request Task Agent takes workflow tasks created in the transformation database and submits to the workload management system. """
+__RCSID__ = "$Id: WorkflowTaskAgent.py 20001 2010-01-20 12:47:38Z acsmith $"
+
+from DIRAC                                                          import S_OK, S_ERROR, gConfig, gMonitor, gLogger, rootPath
+from DIRAC.TransformationSystem.Agent.TaskManagerAgentBase          import TaskManagerAgentBase
+from DIRAC.TransformationSystem.Client.TaskManager                  import WorkflowTasks
+
+AGENT_NAME = 'TransformationSystem/WorkflowTaskAgent'
+
+class WorkflowTaskAgent(TaskManagerAgentBase,WorkflowTasks):
+
+  #############################################################################
+  def initialize(self):
+    """ Sets defaults """
+    TaskManagerAgentBase.initialize(self)
+    WorkflowTasks.__init__(self)
+    self.transType = ['MCSimulation','DataReconstruction','DataStripping','MCStripping','Merge']
+    self.transClient.setServer('ProductionManagement/ProductionManager')
+    return S_OK()
