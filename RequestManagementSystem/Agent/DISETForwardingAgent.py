04bb60a6f05b18f2882b11aa5e082ff9969cc76e
Ricardo Graciani <graciani@ecm.ub.es>
2010-02-16 13:32:18 +0000

    FIX: result -> S_OK() (it is undefined)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@21469 4525493e-7705-40b1-a816-d608a930855b

--- RequestManagementSystem/Agent/DISETForwardingAgent.py
+++ RequestManagementSystem/Agent/DISETForwardingAgent.py
@@ -36,7 +36,7 @@ class DISETForwardingAgent(AgentModule,RequestAgentMixIn):
       errStr = 'The RequestManagement/localURL option must be defined.'
       gLogger.fatal(errStr)
       return S_ERROR(errStr)
-    return result
+    return S_OK()
 
   def execute(self):
     """ Takes the DISET requests and forwards to destination service
