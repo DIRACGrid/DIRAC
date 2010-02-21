5b8796330ba0c50420a6cc2b1ee702b55ae4ad42
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2010-02-21 19:08:00 +0000

    First import of the System Administrator CLI
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@21944 4525493e-7705-40b1-a816-d608a930855b

---
+++ FrameworkSystem/scripts/dirac-admin-sysadmin-cli.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+########################################################################
+# $HeadURL:  $
+########################################################################
+__RCSID__   = "$Id:  $"
+import sys,os
+import DIRAC
+from DIRAC import gLogger
+from DIRAC.Core.Base import Script
+
+host = None
+Script.registerSwitch( "H:", "host=","   Target host")
+Script.parseCommandLine( ignoreErrors = False )
+for switch in Script.getUnprocessedSwitches():
+  if switch[0].lower() == "h" or switch[0].lower() == "host":
+    host = switch[1]
+
+from DIRAC.FrameworkSystem.Client.SystemAdministratorClientCLI import SystemAdministratorClientCLI
+
+cli = SystemAdministratorClientCLI(host)
+cli.cmdloop()  
