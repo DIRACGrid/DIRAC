735208cbda36c4bd9bf3a6f13abc052157ebb3f1
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2010-03-07 14:09:41 +0000

    Added getVersion()
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@22420 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/Version.py
+++ Core/Utilities/Version.py
@@ -23,4 +23,26 @@ def getCurrentVersion():
       pass
     
   return S_OK(version)  
+
+def getVersion():
+  """ Get a dictionary corresponding to the current version of the DIRAC package and all the installed
+      extension packages
+  """
+  
+  import DIRAC
+  vDict = {'Extensions':{}}
+  vDict['DIRAC'] = DIRAC.version
+  
+  extensions = gConfig.getOption('/DIRAC/Extensions',[])
+  for e in extensions['Value']:
+    try: 
+      exec "import %sDIRAC" % e
+      version = "%sDIRAC " % e + eval('%sDIRAC.version' % e) +'; '+ version
+      vDict['Extensions'][e] = version
+    except ImportError:
+      pass
+    except AttributeError:
+      pass
+    
+  return S_OK(vDict)  
   
