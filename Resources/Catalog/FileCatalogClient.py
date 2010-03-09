14ceb94274ddc5de7ed61186403624b546220652
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2010-03-09 19:52:32 +0000

    make better use of the base client. added isOK method that to determine whether the service is connected when initialised (instead of everytime). updated getReplicas to pass the flag for allStatus correctly.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@22622 4525493e-7705-40b1-a816-d608a930855b

--- Resources/Catalog/FileCatalogClient.py
+++ Resources/Catalog/FileCatalogClient.py
@@ -1,29 +1,40 @@
 ########################################################################
 # $HeadURL$
 ########################################################################
-
-""" The FileCatalogClient is a class representing the client of the DIRAC
-    File Catalog 
-""" 
+""" The FileCatalogClient is a class representing the client of the DIRAC File Catalog  """ 
 
 __RCSID__ = "$Id$"
 
 import re, time, random, os, types
 
-from DIRAC import S_OK, S_ERROR, gLogger,gConfig
-from DIRAC.Core.DISET.RPCClient import RPCClient
-from DIRAC.Core.Base.Client import Client
-
+from DIRAC                              import S_OK, S_ERROR, gLogger,gConfig
+from DIRAC.Core.Base.Client             import Client
 
 class FileCatalogClient(Client):
 
   def __init__(self):
     """ Constructor function.
     """
-    self.serverURL = 'DataManagement/FileCatalog'
+    self.setServer('DataManagement/FileCatalog')
+    self.available = False
+    res = self.isOK()
+    if res['OK']:
+      self.available = res['Value']
 
-  def getReplicas( self, lfns, allStatus='allStatus' ):
-    return RPCClient(self.serverURL).getReplicas(lfns)
+  def isOK(self,rpc='',url='',timeout=120):
+    if not self.available:
+      rpcClient = self._getRPC(rpc=rpc,url=url,timeout=timeout)
+      res = rpcClient.isOK()
+      if not res['OK']:
+        self.available = False
+      else:
+        self.available = True
+    return S_OK(self.available)
+    
+  def getReplicas(self, lfns, allStatus=False,rpc='',url='',timeout=120):
+    rpcClient = self._getRPC(rpc=rpc,url=url,timeout=timeout)
+    return rpcClient.getReplicas(lfns,allStatus)
 
-  def listDirectory(self, lfn, verbose=False ):
-    return RPCClient(self.serverURL).listDirectory(lfn,verbose)
+  def listDirectory(self, lfn, verbose=False, rpc='',url='',timeout=120):
+    rpcClient = self._getRPC(rpc=rpc,url=url,timeout=timeout)
+    return rpcClient.listDirectory(lfn,verbose)
