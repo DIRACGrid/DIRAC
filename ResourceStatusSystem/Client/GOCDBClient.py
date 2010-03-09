f318218cfc2f7b60a66314247a38543a14690506
Elisa Lanciotti <Elisa.Lanciotti@cern.ch>
2010-03-09 21:09:27 +0000

    GOCDBClient update in order to return the URL of the downtime
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@22626 4525493e-7705-40b1-a816-d608a930855b

--- ResourceStatusSystem/Client/GOCDBClient.py
+++ ResourceStatusSystem/Client/GOCDBClient.py
@@ -11,7 +11,6 @@ from DIRAC import gLogger
 from DIRAC.ResourceStatusSystem.Utilities.Exceptions import *
 from DIRAC.ResourceStatusSystem.Utilities.Utils import *
 
-
 class GOCDBClient:
   
 #############################################################################
@@ -70,10 +69,12 @@ class GOCDBClient:
       return [{'DT':'None'}]
     
     res = self._xmlParsing(resCDL, granularity, startDate, startDateMax)
-    
+   
+ 
     if res is None or res == []:
       return [{'DT':'None'}]
-      
+    
+    self.buildURL(res)  
     return res
   
 #############################################################################
@@ -82,6 +83,15 @@ class GOCDBClient:
     
     pass
 
+#############################################################################
+  def buildURL(self, DTList):
+    '''build the URL  for the downtime on the basis of the downtime ID '''
+    baseURL = 'https://goc.gridops.org/downtime/list?id='
+    for dt in DTList:
+      id = dt['id']
+      url = baseURL + str(id)
+      dt['url'] = url
+    
 #############################################################################
   
   def _curlDownload(self, entity, startDate=None):
@@ -100,13 +110,16 @@ class GOCDBClient:
     if startDate is None: 
       when = "&ongoing_only=yes" 
       gocdbpi_startDate = ""
+      gocdb_ep = gocdbpi_url + "method=" + gocdbpi_method + "&topentity=" + gocdbpi_topEntity + when + gocdbpi_startDate
+      gocdb_ongoing=gocdb_ep
     else:
       when = "&startdate="
       gocdbpi_startDate = startDate
-     
-    # GOCDB-PI to query
-    gocdb_ep = gocdbpi_url + "method=" + gocdbpi_method + "&topentity=" + gocdbpi_topEntity + when + gocdbpi_startDate
+      gocdb_ep = gocdbpi_url + "method=" + gocdbpi_method + "&topentity=" + gocdbpi_topEntity + when + gocdbpi_startDate
+      gocdb_ongoing = gocdbpi_url + "method=" + gocdbpi_method + "&topentity=" + gocdbpi_topEntity + "&ongoing_only=yes"
+    
 
+    # GOCDB-PI to query
     try:
       opener = urllib2.build_opener()
       dtPage = opener.open(gocdb_ep)
@@ -118,20 +131,56 @@ class GOCDBClient:
       exceptStr = where(self, self._curlDownload) + " while opening %s." % gocdb_ep
       gLogger.exception(exceptStr,'',errorMsg)
       return None
-      
-    dt = dtPage.read()
-    
-    opener.close()
 
-    return dt
-    
+    dt = dtPage.read()
+
+
+    try:
+      dtPage_ongoing = opener.open(gocdb_ongoing)
+    except IOError, errorMsg:
+      exceptStr = where(self, self._curlDownload) + " while opening %s." % gocdb_ep
+      gLogger.exception(exceptStr,'',errorMsg)
+      return dt
+    except Exception, errorMsg:
+      exceptStr = where(self, self._curlDownload) + " while opening %s." % gocdb_ep
+      gLogger.exception(exceptStr,'',errorMsg)
+      return dt
+
+    dt_on= dtPage_ongoing.read()
+
+    #now mangling and merging  the 2 XML documents (ongoing ones - that have always to be reported - and future ones if appliable)
+
+    if dt_on.rfind("ROOT/") != -1:
+      my_dt_on=dt_on.replace("</ROOT>","")
+    else:
+      my_dt_on=dt_on
+      
+    if dt.rfind("ROOT/") != -1:
+      my_dt= dt.replace("<?xml version=\"1.0\"?>","")
+      my_dt=my_dt.replace("<ROOT>","")
+    else:
+      my_dt=dt
+
+    if (dt.rfind("ROOT/") == -1) and (dt_on.rfind("ROOT/") == -1):
+      final=my_dt_on+my_dt
+      final=final.replace("\n","")
+      final=final.replace("</ROOT>","</ROOT>\n")
+      final=final.replace("<?xml version=\"1.0\"?>","<?xml version=\"1.0\"?>\n")
+    elif dt.rfind("ROOT/") == -1 and dt_on.rfind("ROOT/") != -1:
+      final=dt
+    elif dt.rfind("ROOT/") != -1 and dt_on.rfind("ROOT/") == -1:
+      final=dt_on
+    elif dt.rfind("ROOT/") != -1 and dt_on.rfind("ROOT/") != -1:
+      final=dt_on
+    opener.close()
+    return final
+    #return dt
 
 #############################################################################
 
   def _xmlParsing(self, dt, siteOrRes, startDate = None, startDateMax = None):
     """ Performs xml parsing from the dt string (returns a dictionary)
     """
-
     try:
       doc = minidom.parseString(dt)
 
@@ -141,6 +190,7 @@ class GOCDBClient:
       DTList = []  
       
       for dt in downtimes:
+        typeOfDT = 'unknown'
         handler = {}  
         if dt.getAttributeNode("CLASSIFICATION"):
           attrs_class = dt.attributes["CLASSIFICATION"]
@@ -148,9 +198,10 @@ class GOCDBClient:
           dom_elements = dt.childNodes
           
           for elements in dom_elements:
-            if siteOrRes == 'Site':
-              if elements.nodeName == "HOSTNAME":
-                break
+            if siteOrRes == 'Site' and elements.nodeName == "HOSTNAME": # user asked for DT relative to the site
+              typeOfDT = 'service'
+              break
+
             if elements.nodeName == "SEVERITY":
               for element in elements.childNodes:
                 if element.nodeType == element.TEXT_NODE: 
@@ -170,7 +221,16 @@ class GOCDBClient:
                 end_date = datetime.utcfromtimestamp(edate)
                 end_date_STR = end_date.isoformat(' ')
                 handler['EndDate'] = end_date_STR
-        
+
+          if siteOrRes == 'Site' and typeOfDT == 'service':
+              print 'dont store the dt'
+          else:
+            print 'store the dt'
+            if dt.getAttributeNode("ID"):
+              attrs_id = dt.attributes["ID"]
+              id=attrs_id.nodeValue.replace("u","")
+              handler['id'] = id
+            
           try:
             if startDate is not None:
               if end_date < startDate:
@@ -185,9 +245,9 @@ class GOCDBClient:
           except NameError:
             pass
         
+
         if handler != {}:
           DTList.append(handler)
-          
       return DTList
     
     except Exception, errorMsg:
