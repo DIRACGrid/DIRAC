2b80501f941f1ab063a7d096114a1aa582075996
Elisa Lanciotti <Elisa.Lanciotti@cern.ch>
2010-03-10 20:23:42 +0000

    GGUSTicketClient returns the statistics of ticket grouped only in two categories: open states and terminal states
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@22677 4525493e-7705-40b1-a816-d608a930855b

--- ResourceStatusSystem/Client/GGUSTicketsClient.py
+++ ResourceStatusSystem/Client/GGUSTicketsClient.py
@@ -50,7 +50,7 @@ class GGUSTicketsClient:
     self.globalStatistics()
     # create the URL to get tickets relative to the site:
     GGUSURL = "https://gus.fzk.de/ws/ticket_search.php?show_columns_check[]=REQUEST_ID&show_columns_check[]=TICKET_TYPE&show_columns_check[]=AFFECTED_VO&show_columns_check[]=AFFECTED_SITE&show_columns_check[]=RESPONSIBLE_UNIT&show_columns_check[]=STATUS&show_columns_check[]=DATE_OF_CREATION&show_columns_check[]=LAST_UPDATE&show_columns_check[]=SHORT_DESCRIPTION&ticket=&supportunit=all&vo=all&user=&keyword=&involvedsupporter=&assignto=&affectedsite=" + self.siteName + "&specattrib=0&status=open&priority=all&typeofproblem=all&mouarea=&radiotf=1&timeframe=lastweek&tf_date_day_s=&tf_date_month_s=&tf_date_year_s=&tf_date_day_e=&tf_date_month_e=&tf_date_year_e=&lm_date_day=12&lm_date_month=2&lm_date_year=2010&orderticketsby=GHD_INT_REQUEST_ID&orderhow=descending"
-    return self.count, GGUSURL
+    return self.statusCount, GGUSURL
 
 #############################################################################
   def globalStatistics(self):
@@ -66,13 +66,28 @@ class GGUSTicketsClient:
         self.selectedTickets[id]['site'] = str(ticket[4])
     print 'total number of tickets: ', len(self.selectedTickets.keys()) 
     self.count = {}
+    # group tickets in only 2 categories: open and terminal states   
+    openStates = ['assigned', 'in progress', 'new', 'on hold', 'reopened', 'waiting for reply']
+    terminalStates = ['solved', 'unsolved', 'verified']
+    self.statusCount = {}
+    self.statusCount['open'] = 0
+    self.statusCount['terminal'] = 0
     for id in self.selectedTickets.keys():
       status = self.selectedTickets[id]['status']
       if status not in self.count.keys():
         self.count[status] = 0
       self.count[status] += 1
+      if status in terminalStates:
+        self.statusCount['terminal'] +=1
+      elif status in openStates:
+        self.statusCount['open'] +=1
+      else:
+        print 'ERROR! status unknown: ', status
+      
     for status in self.count.keys():
-      print 'in status ', status, '->', self.count[status]   
+      print 'in status ', status, '->', self.count[status]
+    
+    
     return
 
 #############################################################################
--- ResourceStatusSystem/Client/GOCDBClient.py
+++ ResourceStatusSystem/Client/GOCDBClient.py
@@ -11,6 +11,7 @@ from DIRAC import gLogger
 from DIRAC.ResourceStatusSystem.Utilities.Exceptions import *
 from DIRAC.ResourceStatusSystem.Utilities.Utils import *
 
+
 class GOCDBClient:
   
 #############################################################################
@@ -51,30 +52,30 @@ class GOCDBClient:
     startDate_STR = None
     startDateMax = None
     startDateMax_STR = None
-    
+
+    # set the max. starting date for the DT    
     if startingInHours is not None:
       startDate = datetime.utcnow()
       startDateMax = startDate + timedelta(hours = startingInHours)
       startDateMax_STR = startDateMax.isoformat(' ')[0:10]
         
+    # 
     if startDate is not None:
       if isinstance(startDate, basestring):
         startDate_STR = startDate
         startDate = datetime(*time.strptime(startDate, "%Y-%m-%d")[0:3])
       elif isinstance(startDate, datetime):
         startDate_STR = startDate.isoformat(' ')[0:10]
-    
+    print 'startDate_STR is ', startDate_STR
     resCDL = self._curlDownload(self._entity, startDate_STR)
     if resCDL is None:
       return [{'DT':'None'}]
     
     res = self._xmlParsing(resCDL, granularity, startDate, startDateMax)
-   
- 
+    
     if res is None or res == []:
       return [{'DT':'None'}]
-    
-    self.buildURL(res)  
+      
     return res
   
 #############################################################################
@@ -83,15 +84,6 @@ class GOCDBClient:
     
     pass
 
-#############################################################################
-  def buildURL(self, DTList):
-    '''build the URL  for the downtime on the basis of the downtime ID '''
-    baseURL = 'https://goc.gridops.org/downtime/list?id='
-    for dt in DTList:
-      id = dt['id']
-      url = baseURL + str(id)
-      dt['url'] = url
-    
 #############################################################################
   
   def _curlDownload(self, entity, startDate=None):
@@ -110,16 +102,13 @@ class GOCDBClient:
     if startDate is None: 
       when = "&ongoing_only=yes" 
       gocdbpi_startDate = ""
-      gocdb_ep = gocdbpi_url + "method=" + gocdbpi_method + "&topentity=" + gocdbpi_topEntity + when + gocdbpi_startDate
-      gocdb_ongoing=gocdb_ep
     else:
       when = "&startdate="
       gocdbpi_startDate = startDate
-      gocdb_ep = gocdbpi_url + "method=" + gocdbpi_method + "&topentity=" + gocdbpi_topEntity + when + gocdbpi_startDate
-      gocdb_ongoing = gocdbpi_url + "method=" + gocdbpi_method + "&topentity=" + gocdbpi_topEntity + "&ongoing_only=yes"
-    
-
+     
     # GOCDB-PI to query
+    gocdb_ep = gocdbpi_url + "method=" + gocdbpi_method + "&topentity=" + gocdbpi_topEntity + when + gocdbpi_startDate
+
     try:
       opener = urllib2.build_opener()
       dtPage = opener.open(gocdb_ep)
@@ -131,56 +120,20 @@ class GOCDBClient:
       exceptStr = where(self, self._curlDownload) + " while opening %s." % gocdb_ep
       gLogger.exception(exceptStr,'',errorMsg)
       return None
-
-    dt = dtPage.read()
-
-
-    try:
-      dtPage_ongoing = opener.open(gocdb_ongoing)
-    except IOError, errorMsg:
-      exceptStr = where(self, self._curlDownload) + " while opening %s." % gocdb_ep
-      gLogger.exception(exceptStr,'',errorMsg)
-      return dt
-    except Exception, errorMsg:
-      exceptStr = where(self, self._curlDownload) + " while opening %s." % gocdb_ep
-      gLogger.exception(exceptStr,'',errorMsg)
-      return dt
-
-    dt_on= dtPage_ongoing.read()
-
-    #now mangling and merging  the 2 XML documents (ongoing ones - that have always to be reported - and future ones if appliable)
-
-    if dt_on.rfind("ROOT/") != -1:
-      my_dt_on=dt_on.replace("</ROOT>","")
-    else:
-      my_dt_on=dt_on
       
-    if dt.rfind("ROOT/") != -1:
-      my_dt= dt.replace("<?xml version=\"1.0\"?>","")
-      my_dt=my_dt.replace("<ROOT>","")
-    else:
-      my_dt=dt
-
-    if (dt.rfind("ROOT/") == -1) and (dt_on.rfind("ROOT/") == -1):
-      final=my_dt_on+my_dt
-      final=final.replace("\n","")
-      final=final.replace("</ROOT>","</ROOT>\n")
-      final=final.replace("<?xml version=\"1.0\"?>","<?xml version=\"1.0\"?>\n")
-    elif dt.rfind("ROOT/") == -1 and dt_on.rfind("ROOT/") != -1:
-      final=dt
-    elif dt.rfind("ROOT/") != -1 and dt_on.rfind("ROOT/") == -1:
-      final=dt_on
-    elif dt.rfind("ROOT/") != -1 and dt_on.rfind("ROOT/") != -1:
-      final=dt_on
+    dt = dtPage.read()
+    
     opener.close()
-    return final
-    #return dt
+
+    return dt
+    
 
 #############################################################################
 
   def _xmlParsing(self, dt, siteOrRes, startDate = None, startDateMax = None):
     """ Performs xml parsing from the dt string (returns a dictionary)
     """
+
     try:
       doc = minidom.parseString(dt)
 
@@ -190,7 +143,6 @@ class GOCDBClient:
       DTList = []  
       
       for dt in downtimes:
-        typeOfDT = 'unknown'
         handler = {}  
         if dt.getAttributeNode("CLASSIFICATION"):
           attrs_class = dt.attributes["CLASSIFICATION"]
@@ -198,10 +150,9 @@ class GOCDBClient:
           dom_elements = dt.childNodes
           
           for elements in dom_elements:
-            if siteOrRes == 'Site' and elements.nodeName == "HOSTNAME": # user asked for DT relative to the site
-              typeOfDT = 'service'
-              break
-
+            if siteOrRes == 'Site':
+              if elements.nodeName == "HOSTNAME":
+                break
             if elements.nodeName == "SEVERITY":
               for element in elements.childNodes:
                 if element.nodeType == element.TEXT_NODE: 
@@ -221,16 +172,7 @@ class GOCDBClient:
                 end_date = datetime.utcfromtimestamp(edate)
                 end_date_STR = end_date.isoformat(' ')
                 handler['EndDate'] = end_date_STR
-
-          if siteOrRes == 'Site' and typeOfDT == 'service':
-              print 'dont store the dt'
-          else:
-            print 'store the dt'
-            if dt.getAttributeNode("ID"):
-              attrs_id = dt.attributes["ID"]
-              id=attrs_id.nodeValue.replace("u","")
-              handler['id'] = id
-            
+        
           try:
             if startDate is not None:
               if end_date < startDate:
@@ -245,9 +187,9 @@ class GOCDBClient:
           except NameError:
             pass
         
-
         if handler != {}:
           DTList.append(handler)
+          
       return DTList
     
     except Exception, errorMsg:
