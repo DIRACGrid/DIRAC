a7961e57b40b1b566645bdd364250fcd72d250f3
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2010-03-17 13:50:41 +0000

    Make the client interface the same as in the Graphs module
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@23223 4525493e-7705-40b1-a816-d608a930855b

--- FrameworkSystem/Client/PlottingClient.py
+++ FrameworkSystem/Client/PlottingClient.py
@@ -30,6 +30,8 @@ class PlottingClient:
     return TransferClient(self.serviceName)
   
   def getPlotToMemory( self, plotName ):
+    """ Get the prefabricated plot from the service and return it as a string
+    """
     transferClient = self.__getTransferClient()
     tmpFile = tempfile.TemporaryFile()
     retVal = transferClient.receiveFile( tmpFile, plotName )
@@ -41,6 +43,8 @@ class PlottingClient:
     return S_OK( data )
 
   def getPlotToFile( self, plotName, fileName ):
+    """ Get the prefabricated plot from the service and store it in a file
+    """
     transferClient = self.__getTransferClient()
     try:
       destFile = file( fileName, "wb" )
@@ -50,26 +54,61 @@ class PlottingClient:
     if not retVal[ 'OK' ]:
       return retVal
     destFile.close()
-    return S_OK()  
+    return S_OK(fileName)  
   
-  def getPlot(self,data,plotMetadata={},fname=False,**kw):
+  def graph(self,data,fname=False,*args,**kw):
+    """ Generic method to obtain graphs from the Plotting service. The requested
+        graphs are completely described by their data and metadata
+    """
     
     client = self.__getRPCClient()
-    plotMetadata.update(kw)
-    subplotMetadata = plotMetadata.get('metadata',[])
-    result = client.generatePlot(data,plotMetadata,subplotMetadata)
-    
-    print result
-    
+    plotMetadata = {}
+    for arg in args:
+      if type(arg) == types.DictType:
+        plotMetadata.update(arg)
+      else:
+        return S_ERROR('Non-dictionary non-keyed argument')
+    plotMetadata.update(kw)    
+    result = client.generatePlot(data,plotMetadata)   
     if not result['OK']:
       return result
     
     plotName = result['Value']
-    if fname:
+    if fname or fname != 'Memory':
       result = self.getPlotToFile(plotName,fname)
     else:
       result = self.getPlotToMemory(plotName)
       
     return result    
      
-     
+  def barGraph( self, data, file, *args, **kw ):
+    return self.graph( data, file, plot_type = 'BarGraph', statistics_line=True, *args, **kw )
+  
+  def lineGraph( data, file, *args, **kw ):
+    return self.graph( data, file, plot_type = 'LineGraph', statistics_line=True, *args, **kw )
+  
+  def cumulativeGraph( self, data, file, *args, **kw ):
+    return self.graph( data, file, plot_type = 'LineGraph', cumulate_data = True, *args, **kw )
+  
+  def pieGraph( self, data, file, *args, **kw ):
+    prefs = {'xticks':False, 'yticks':False, 'legend_position':'right'}
+    return self.graph( data, file, prefs, plot_type = 'PieGraph', *args, **kw )
+  
+  def qualityGraph( self, data, file, *args, **kw ):
+    prefs = {'plot_axis_grid':False}
+    return self.graph( data, file, prefs, plot_type = 'QualityMapGraph', *args, **kw )
+  
+  def textGraph( self, text, file, *args, **kw ):
+    prefs = {'text_image':text}
+    return self.graph( {}, file, prefs, *args, **kw )
+  
+  def histogram( self, data, file, bins, *args, **kw ):
+    try:
+      from pylab import hist
+    except:
+      return S_ERROR("No pylab module available")  
+    values,vbins,patches = hist(data,bins)
+    histo = dict(zip(vbins,values))
+    span = (max(data)-min(data))/float(bins)*0.98 
+    kw = __checkKW( kw )
+    return self.graph( histo, file, plot_type = 'BarGraph', span=span, statistics_line=True, *args, **kw ) 
