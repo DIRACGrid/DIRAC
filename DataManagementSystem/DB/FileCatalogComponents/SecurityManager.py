d0842a3f8a96cecc69cb2b58d7677f3aeb267c6e
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2010-03-22 10:16:52 +0000

    first security mechanism.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@23595 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/DB/FileCatalogComponents/SecurityManager.py
+++ DataManagementSystem/DB/FileCatalogComponents/SecurityManager.py
@@ -6,7 +6,7 @@
 
 __RCSID__ = "$Id:  $"
 
-import time
+import time,os
 from DIRAC import S_OK, S_ERROR, gConfig
 
 class SecurityManagerBase:
@@ -44,7 +44,48 @@ class NoSecurityManager(SecurityManagerBase):
     return S_OK(True)
 
 class DirectorySecurityManager(SecurityManagerBase):
-  pass
+  
+  def hasAccess(self,opType,paths,credDict):
+    successful = {}
+    if not opType in ['Read','Write']:
+      return S_ERROR("Operation type not known")
+    if self.db.globalReadAccess and (opType.lower() == 'read'):
+      for path in paths:
+        successful[path] = True
+      resDict = {'Successful':successful,'Failed':{}}
+      return S_OK(resDict)
+    toGet = dict(zip(paths,[ [path] for path in paths ]))
+    permissions = {}
+    failed = {}
+    while toGet:
+      paths = toGet.keys()
+      res = self.db.dtree.getPathPermissions(paths,credDict)
+      if not res['OK']:
+        return res
+      for path,mode in res['Value']['Successful'].items():
+        for resolvedPath in toGet[path]:
+          permissions[resolvedPath] = mode
+        toGet.pop(path)  
+      for path,error in res['Value']['Failed'].items():
+        if error != 'No such file or directory':
+          for resolvedPath in toGet[path]:
+            failed[resolvedPath] = error
+          toGet.pop(path)
+      for path,resolvedPaths in toGet.items():
+        if path == '/':
+          for resolvedPath in resolvedPaths:
+            permissions[path] = {'Read':True,'Write':True,'Execute':True}
+        if not toGet.has_key(os.path.dirname(path)):
+          toGet[os.path.dirname(path)] = []
+        toGet[os.path.dirname(path)] += resolvedPaths
+        toGet.pop(path)
+    for path,permDict in permissions.items():
+      if permDict[opType]:
+        successful[path] = True
+      else:
+        successful[path] = False
+    resDict = {'Successful':successful,'Failed':failed}
+    return S_OK(resDict)
 
 class FullSecurityManager(SecurityManagerBase):
   pass
