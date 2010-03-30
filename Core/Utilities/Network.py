7633d72347976525d405a420e780185eeef63729
Adria Casajus <adria@ecm.ub.es>
2010-03-30 14:20:02 +0000

    Added functions to get info from all interfaces
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@23766 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/Network.py
+++ Core/Utilities/Network.py
@@ -12,36 +12,64 @@ import array
 import os
 from DIRAC.Core.Utilities.ReturnValues import S_OK, S_ERROR
 
+def discoverInterfaces():
+  max_possible = 128
+  bytes = max_possible * 32
+  s = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+  names = array.array( 'B', '\0' * bytes )
+  #0x8912 SICGIFCONF
+  fcntlOut = fcntl.ioctl( s.fileno(), 0x8912, struct.pack( 'iL', bytes, names.buffer_info()[0] ) )
+  outbytes = struct.unpack( 'iL', fcntlOut )[0]
+  namestr = names.tostring()
+  ifaces = {}
+  arch = platform.architecture()[0]
+  if arch.find( '32' ) == 0:
+    for i in range( 0, outbytes, 32 ):
+      name = namestr[i:i + 32].split( '\0', 1 )[0]
+      ip = namestr[i + 20:i + 24]
+      ifaces[ name ] = { 'ip' : socket.inet_ntoa( ip ), 'mac' : getMACFromInterface( name ) }
+  else:
+    for i in range( 0, outbytes, 40 ):
+      name = namestr[i:i + 16].split( '\0', 1 )[0]
+      ip = namestr[i + 20:i + 24]
+      ifaces[ name ] = { 'ip' : socket.inet_ntoa( ip ), 'mac' : getMACFromInterface( name ) }
+  return ifaces
+
 def getAllInterfaces():
   import fcntl
   max_possible = 128  # arbitrary. raise if needed.
   bytes = max_possible * 32
-  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  names = array.array('B', '\0' * bytes)
-  outbytes = struct.unpack(
+  s = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+  names = array.array( 'B', '\0' * bytes )
+  outbytes = struct.unpack( 
                             'iL',
-                            fcntl.ioctl(
+                            fcntl.ioctl( 
                                          s.fileno(),
-                                         0x8912,  # SIOCGIFCONF
+                                         0x8912, # SIOCGIFCONF
                                          struct.pack( 'iL',
                                                       bytes,
                                                       names.buffer_info()[0] )
                                        )
                           )[0]
   namestr = names.tostring()
-  return [namestr[i:i+32].split('\0', 1)[0] for i in range(0, outbytes, 32)]
+  return [namestr[i:i + 32].split( '\0', 1 )[0] for i in range( 0, outbytes, 32 )]
 
 def getAddressFromInterface( ifName ):
   import fcntl
   try:
     s = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
-    return socket.inet_ntoa( fcntl.ioctl(
+    return socket.inet_ntoa( fcntl.ioctl( 
                                           s.fileno(),
-                                          0x8915,  # SIOCGIFADDR
-                                          struct.pack('256s', ifName[:15] )
+                                          0x8915, # SIOCGIFADDR
+                                          struct.pack( '256s', ifName[:15] )
                                         )[20:24] )
   except:
     return False
+
+def getMACFromInterface( self, ifname ):
+  s = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+  info = fcntl.ioctl( s.fileno(), 0x8927, struct.pack( '256s', ifname[:15] ) )
+  return ''.join( ['%02x:' % ord( char ) for char in info[18:24]] )[:-1]
 
 def getFQDN():
   sFQDN = socket.getfqdn()
@@ -67,7 +95,7 @@ def splitURL( URL ):
     path = path[:-1]
   portStart = host.find( ":" )
   if portStart > -1:
-    port = int( host[ portStart+1: ] )
+    port = int( host[ portStart + 1: ] )
     host = host[ :portStart ]
   else:
     port = 0
@@ -94,7 +122,7 @@ def checkHostsMatch( host1, host2 ):
   #Check
   for i in range( len( ipLists ) - 1 ):
     for ip in ipLists[i]:
-      for ipl in ipLists[i+1]:
+      for ipl in ipLists[i + 1]:
         if ip in ipl:
           return S_OK( True )
   return S_OK( False )
