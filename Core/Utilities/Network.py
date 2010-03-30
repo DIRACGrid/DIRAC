d63ea07ceb4fd59cb48bf8ab12b5d931a8365f3b
Adria Casajus <adria@ecm.ub.es>
2010-03-30 14:32:19 +0000

    s
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@23769 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/Network.py
+++ Core/Utilities/Network.py
@@ -68,7 +68,7 @@ def getAddressFromInterface( ifName ):
   except:
     return False
 
-def getMACFromInterface( self, ifname ):
+def getMACFromInterface( ifname ):
   s = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
   info = fcntl.ioctl( s.fileno(), 0x8927, struct.pack( '256s', ifname[:15] ) )
   return ''.join( ['%02x:' % ord( char ) for char in info[18:24]] )[:-1]
