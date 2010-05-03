d06947950071901d35cdfcb63313be6a598281ec
Adria Casajus <adria@ecm.ub.es>
2010-05-03 15:53:15 +0000

    CHANGE: Do not specify a palette
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@24643 4525493e-7705-40b1-a816-d608a930855b

--- AccountingSystem/private/Plots.py
+++ AccountingSystem/private/Plots.py
@@ -7,31 +7,6 @@ except Exception, e:
 
 from DIRAC import S_OK, S_ERROR, rootPath
 
-gPredefinedPalettes = { 'Status' : { 'Failed'    : '#E66266',
-                                     'Done'      : '#7BEA81',
-                                     'Completed' : '#109000',
-                                     'Waiting'   : '#FFE533',
-                                     'Deleted'   : '#6E6E6E',
-                                     'Running'   : '#7FBAFF',
-                                     'Received'  : '#BACEAC',
-                                     'Stalled'   : '#7D69A3',
-                                     'Killed'    : '#CC99FF',
-                                     'Checking'  : '#FFF4AA',
-                                     'Staging'   : '#3D4E7E',
-                                     'Matched'   : '#0076FF',
-                                    },
-                         'Site'  : { 'LCG.CERN.ch'   : '#7777FF',
-                                     'LCG.RAL.uk'    : '#77FF77',
-                                     'LCG.NIKHEF.nl' : '#FF7777',
-                                     'LCG.GRIDKA.de' : '#FFFF77',
-                                     'LCG.CNAF.it'   : '#FF77FF',
-                                     'LCG.PIC.es'    : '#77FFFF',
-                                     'LCG.IN2P3.fr'  : '#777777',
-                                   }
-                       }
-
-gCompiledPalettes = dict( [ ( t, gPredefinedPalettes[k][t] ) for k in gPredefinedPalettes for t in gPredefinedPalettes[k] ] )
-
 def checkMetadata( metadata ):
   if 'span' in metadata:
     granularity = metadata[ 'span' ]
@@ -39,8 +14,6 @@ def checkMetadata( metadata ):
       metadata[ 'starttime' ] = metadata[ 'starttime' ] - metadata[ 'starttime' ] % granularity
     if 'endtime' in metadata:
       metadata[ 'endtime' ] = metadata[ 'endtime' ] - metadata[ 'endtime' ] % granularity
-  if not 'colors' in metadata:
-    metadata[ 'colors' ] = gCompiledPalettes
   if 'limit_labels' not in metadata:
     metadata[ 'limit_labels' ] = 30
 
