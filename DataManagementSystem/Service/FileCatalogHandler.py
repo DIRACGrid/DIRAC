29621ac86acc2817c23715e909cc8763d41dba2a
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2010-05-08 22:46:57 +0000

    0775 as a default umask value
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@24979 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Service/FileCatalogHandler.py
+++ DataManagementSystem/Service/FileCatalogHandler.py
@@ -40,7 +40,7 @@ def initializeFileCatalogHandler(serviceInfo):
   # If true this option not store PFNs in the replica table but rather resolve it at read time
   databaseConfig['ResolvePFN'] = gConfig.getValue('%s/%s' % (serviceCS,'ResolvePFN'),True)
   # Default umask
-  databaseConfig['DefaultUmask'] = gConfig.getValue('%s/%s' % (serviceCS,'DefaultUmask'),775)
+  databaseConfig['DefaultUmask'] = gConfig.getValue('%s/%s' % (serviceCS,'DefaultUmask'),0775)
   
   res = fcDB.setConfig(databaseConfig)
   return res
