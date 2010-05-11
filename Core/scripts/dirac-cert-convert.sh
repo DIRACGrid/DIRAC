7a0c0cc0a5914e2c962290443a828435091929d4
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2010-05-11 14:29:29 +0000

    Fix to the case when a fake file is given to the script
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@25163 4525493e-7705-40b1-a816-d608a930855b

--- Core/scripts/dirac-cert-convert.sh
+++ Core/scripts/dirac-cert-convert.sh
@@ -12,6 +12,17 @@
 #
 ##################################################################################
 
+function usage {
+  echo Usage:
+  echo "    " dirac-cert-convert.sh CERT_FILE_NAME.p12 
+  exit 1
+}
+
+if [ $# = 0 ]; then
+  echo User Certificate P12 file is not given. 
+  usage
+fi  
+
 GLOBUS=$HOME/.globus
 USERCERT_P12_ORIG=$1
 USERCERT_P12=$GLOBUS/`basename $USERCERT_P12_ORIG`
@@ -20,11 +31,9 @@ USERKEY_PEM=$GLOBUS/userkey.pem
 OPENSSL=`which openssl`
 DATE=`/bin/date +%F-%k:%M`
 
-if [ -z "$USERCERT_P12_ORIG" ]; then
-  echo User Certificate P12 is not given. 
-  echo Usage:
-  echo "    " dirac-cert-convert.sh CERT_FILE_NAME.p12 
-  exit 1
+if [ ! -f "$USERCERT_P12_ORIG" ]; then
+  echo file $USERCERT_P12_ORIG does not exist
+  usage
 fi  
 
 if [ ! -d $GLOBUS ]; then 
