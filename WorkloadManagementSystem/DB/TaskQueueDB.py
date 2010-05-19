15e7a5127f4d6757e5c92b0d496bff7aa7b0e417
Ricardo Graciani <graciani@ecm.ub.es>
2010-05-19 10:11:45 +0000

    FIX: longer queues have been added 6*_DAY, 8*_DAY, 10*_DAY, 12.5*_DAY
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@25297 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/DB/TaskQueueDB.py
+++ WorkloadManagementSystem/DB/TaskQueueDB.py
@@ -18,7 +18,7 @@ from DIRAC.Core.Security import Properties, CS
 _MIN = 60
 _HOUR = 3600
 _DAY = 86400
-maxCPUSegments = [ 6*_MIN, 30*_MIN, 1*_HOUR, 6*_HOUR, 12*_HOUR, 1*_DAY, 2*_DAY, 3*_DAY, 4*_DAY ]
+maxCPUSegments = [ 6*_MIN, 30*_MIN, 1*_HOUR, 6*_HOUR, 12*_HOUR, 1*_DAY, 2*_DAY, 3*_DAY, 4*_DAY, 6*_DAY, 8*_DAY, 10*_DAY, 12.5*_DAY ]
 DEFAULT_GROUP_SHARE = 1000
 TQ_MIN_SHARE = 0.001
 
