de4fcec39ee5b5155a159e9f072c36e6410b4d34
Stephane Poss <Stephane.Poss@cern.ch>
2010-05-25 13:50:33 +0000

    Look into CS to find default Catalog in /Resources/FileCatalogs/. Use first one found as default. Also implement interface to LcgFileCatalogCombinedClient
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@25358 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/scripts/dirac-dms-filecatalog-cli.py
+++ DataManagementSystem/scripts/dirac-dms-filecatalog-cli.py
@@ -6,19 +6,24 @@ __RCSID__   = "$Id$"
 __VERSION__ = "$Revision: 1.1 $"
 import sys,os
 import DIRAC
-from DIRAC import gLogger
+from DIRAC import gLogger, gConfig
 from DIRAC.Core.Base import Script
 
-fcType = 'LFC'
+fcType = ''
+
 Script.registerSwitch( "f:", "file-catalog=","   Catalog client type to use")
 Script.parseCommandLine( ignoreErrors = False )
+res = gConfig.getSections("/Resources/FileCatalogs/")
+if res['OK']:
+  fcType = res['Value'][0]
+
 for switch in Script.getUnprocessedSwitches():
   if switch[0].lower() == "f" or switch[0].lower() == "file-catalog":
     fcType = switch[1]
 
 from DIRAC.DataManagementSystem.Client.FileCatalogClientCLI import FileCatalogClientCLI
 
-if fcType == "LFC":
+if fcType == "LcgFileCatalog":
   from DIRAC.Resources.Catalog.LcgFileCatalogClient import LcgFileCatalogClient
   cli = FileCatalogClientCLI(LcgFileCatalogClient())
   try:
@@ -28,12 +33,17 @@ if fcType == "LFC":
     sys.exit(1)
   print "Starting LFC FileCatalog client"
   cli.cmdloop()
-elif fcType == "LFCProxy":
+elif fcType == "LcgFileCatalogProxy":
   from DIRAC.Resources.Catalog.LcgFileCatalogProxyClient import LcgFileCatalogProxyClient
   cli = FileCatalogClientCLI(LcgFileCatalogProxyClient())
   print "Starting LFC Proxy FileCatalog client"
   cli.cmdloop() 
-elif fcType == "DiracFC":
+elif fcType == "LcgFileCatalogCombined":
+  from DIRAC.Resources.Catalog.LcgFileCatalogCombinedClient import LcgFileCatalogCombinedClient
+  cli = FileCatalogClientCLI(LcgFileCatalogCombinedClient())
+  print "Starting LFC FileCatalog Combined client"
+  cli.cmdloop()  
+elif fcType == "FileCatalog":
   from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
   cli = FileCatalogClientCLI(FileCatalogClient())
   print "Starting DIRAC FileCatalog client"
