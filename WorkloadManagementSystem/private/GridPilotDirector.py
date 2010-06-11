46a34e953383840cfe75ef6d85ea3a02d5513dc2
Ricardo Graciani <graciani@ecm.ub.es>
2010-06-11 06:27:32 +0000

    NEW: update in the requirements expression to make use of the recently introduced CPUScalingReferenceSI00 if available to determine the CPUWork provided by a queue. If it is not defined the old GlueHostBenchmarkSI00 is used of for this purpose. A new Option has been introduced CPUPowerRef (in SI00) that defines the Reference Power (in SI00) to which the CPUTime (in seconds) requested refers to.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@26066 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/private/GridPilotDirector.py
+++ WorkloadManagementSystem/private/GridPilotDirector.py
@@ -15,17 +15,17 @@
 __RCSID__ = "$Id$"
 
 
-GRIDENV                = ''
-TIME_POLICY            = 'TimeRef * 250 / other.GlueHostBenchmarkSI00 / 60'
-REQUIREMENTS           = ['Rank > -2']
-RANK                   = '( other.GlueCEStateWaitingJobs == 0 ? other.GlueCEStateFreeCPUs * 10 / other.GlueCEInfoTotalCPUs : -other.GlueCEStateWaitingJobs * 4 / (other.GlueCEStateRunningJobs + 1 ) - 1 )'
-FUZZY_RANK             = 'true'
+GRIDENV = ''
+CPU_POWER_REF = '250' # 250 SpecInt 2000 or 1 HepSpec 2006
+REQUIREMENTS = ['Rank > -2']
+RANK = '( other.GlueCEStateWaitingJobs == 0 ? other.GlueCEStateFreeCPUs * 10 / other.GlueCEInfoTotalCPUs : -other.GlueCEStateWaitingJobs * 4 / (other.GlueCEStateRunningJobs + 1 ) - 1 )'
+FUZZY_RANK = 'true'
 
-ERROR_VOMS       = 'Proxy without VOMS Extensions'
-ERROR_CE         = 'No queue available for pilot'
-ERROR_JDL        = 'Could not create Grid JDL'
-ERROR_RB         = 'No Broker available'
-ERROR_TOKEN      = 'Invalid proxy token request'
+ERROR_VOMS = 'Proxy without VOMS Extensions'
+ERROR_CE = 'No queue available for pilot'
+ERROR_JDL = 'Could not create Grid JDL'
+ERROR_RB = 'No Broker available'
+ERROR_TOKEN = 'Invalid proxy token request'
 
 import os, tempfile, time, re, shutil, sys
 
@@ -36,9 +36,9 @@ from DIRAC.FrameworkSystem.Client.NotificationClient       import NotificationCl
 from DIRAC.Core.Security.Misc                              import getProxyInfoAsString
 from DIRAC.Core.Utilities.Grid                             import executeGridCommand
 from DIRAC.WorkloadManagementSystem.Client.ServerUtils     import pilotAgentsDB
-from DIRAC import S_OK, S_ERROR, DictCache, List, Time, systemCall, gConfig
+from DIRAC import S_OK, S_ERROR, DictCache, List, Time, gConfig
 
-class GridPilotDirector(PilotDirector):
+class GridPilotDirector( PilotDirector ):
   """
     Base Grid PilotDirector class
     Derived classes must declare:
@@ -51,20 +51,20 @@ class GridPilotDirector(PilotDirector):
     """
      Define some defaults and call parent __init__
     """
-    self.gridEnv              = GRIDENV
+    self.gridEnv = GRIDENV
 
-    self.timePolicy           = TIME_POLICY
-    self.requirements         = REQUIREMENTS
-    self.rank                 = RANK
-    self.fuzzyRank            = FUZZY_RANK
+    self.cpuPowerRef = CPU_POWER_REF
+    self.requirements = REQUIREMENTS
+    self.rank = RANK
+    self.fuzzyRank = FUZZY_RANK
 
-    self.__failingWMSCache   = DictCache()
-    self.__ticketsWMSCache   = DictCache()
+    self.__failingWMSCache = DictCache()
+    self.__ticketsWMSCache = DictCache()
     self.__listMatchWMSCache = DictCache()
 
     PilotDirector.__init__( self, submitPool )
 
-  def configure(self, csSection, submitPool ):
+  def configure( self, csSection, submitPool ):
     """
      Here goes common configuration for all Grid PilotDirectors
     """
@@ -85,7 +85,7 @@ class GridPilotDirector(PilotDirector):
     if self.gridEnv:
       self.log.info( ' GridEnv:        ', self.gridEnv )
     if self.resourceBrokers:
-      self.log.info( ' ResourceBrokers:', ', '.join(self.resourceBrokers) )
+      self.log.info( ' ResourceBrokers:', ', '.join( self.resourceBrokers ) )
 
   def configureFromSection( self, mySection ):
     """
@@ -93,21 +93,21 @@ class GridPilotDirector(PilotDirector):
     """
     PilotDirector.configureFromSection( self, mySection )
 
-    self.gridEnv = gConfig.getValue( mySection+'/GridEnv', self.gridEnv )
+    self.gridEnv = gConfig.getValue( mySection + '/GridEnv', self.gridEnv )
     if not self.gridEnv:
       # No specific option found, try a general one
-      setup = gConfig.getValue('/DIRAC/Setup','')
+      setup = gConfig.getValue( '/DIRAC/Setup', '' )
       if setup:
-        instance = gConfig.getValue('/DIRAC/Setups/%s/WorkloadManagement' % setup,'')
+        instance = gConfig.getValue( '/DIRAC/Setups/%s/WorkloadManagement' % setup, '' )
         if instance:
-          self.gridEnv = gConfig.getValue('/Systems/WorkloadManagement/%s/GridEnv' % instance,'')
-          
-    self.resourceBrokers      = gConfig.getValue( mySection+'/ResourceBrokers'      , self.resourceBrokers )
+          self.gridEnv = gConfig.getValue( '/Systems/WorkloadManagement/%s/GridEnv' % instance, '' )
 
-    self.timePolicy           = gConfig.getValue( mySection+'/TimePolicy'           , self.timePolicy )
-    self.requirements         = gConfig.getValue( mySection+'/Requirements'         , self.requirements )
-    self.rank                 = gConfig.getValue( mySection+'/Rank'                 , self.rank )
-    self.fuzzyRank            = gConfig.getValue( mySection+'/FuzzyRank'            , self.fuzzyRank )
+    self.resourceBrokers = gConfig.getValue( mySection + '/ResourceBrokers'      , self.resourceBrokers )
+
+    self.cpuPowerRef = gConfig.getValue( mySection + '/CPUPowerRef'           , self.cpuPowerRef )
+    self.requirements = gConfig.getValue( mySection + '/Requirements'         , self.requirements )
+    self.rank = gConfig.getValue( mySection + '/Rank'                 , self.rank )
+    self.fuzzyRank = gConfig.getValue( mySection + '/FuzzyRank'            , self.fuzzyRank )
 
   def _submitPilots( self, workDir, taskQueueDict, pilotOptions, pilotsToSubmit,
                      ceMask, submitPrivatePilot, privateTQ, proxy, pilotsPerJob ):
@@ -124,14 +124,14 @@ class GridPilotDirector(PilotDirector):
     taskQueueID = taskQueueDict['TaskQueueID']
     # ownerDN = taskQueueDict['OwnerDN']
     ownerDN = proxy.getCredentials()['Value']['identity']
-    
+
 
     if not self.resourceBrokers:
       # Since we can exclude RBs from the list, it may become empty
       return S_ERROR( ERROR_RB )
 
     # Need to get VOMS extension for the later interactions with WMS
-    ret = gProxyManager.getVOMSAttributes(proxy)
+    ret = gProxyManager.getVOMSAttributes( proxy )
     if not ret['OK']:
       self.log.error( ERROR_VOMS, ret['Message'] )
       return S_ERROR( ERROR_VOMS )
@@ -139,7 +139,7 @@ class GridPilotDirector(PilotDirector):
       return S_ERROR( ERROR_VOMS )
     vomsGroup = ret['Value'][0]
 
-    workingDirectory = tempfile.mkdtemp( prefix= 'TQ_%s_' % taskQueueID, dir = workDir )
+    workingDirectory = tempfile.mkdtemp( prefix = 'TQ_%s_' % taskQueueID, dir = workDir )
     self.log.verbose( 'Using working Directory:', workingDirectory )
 
     # Write JDL
@@ -147,7 +147,7 @@ class GridPilotDirector(PilotDirector):
                                 ceMask, submitPrivatePilot, privateTQ )
     jdl = retDict['JDL']
     pilotRequirements = retDict['Requirements']
-    rb  = retDict['RB']
+    rb = retDict['RB']
     if not jdl:
       try:
         shutil.rmtree( workingDirectory )
@@ -176,7 +176,7 @@ class GridPilotDirector(PilotDirector):
 
     # Now we are ready for the actual submission, so
 
-    self.log.verbose('Submitting Pilots for TaskQueue', taskQueueID )
+    self.log.verbose( 'Submitting Pilots for TaskQueue', taskQueueID )
     submitRet = self._submitPilot( proxy, pilotsPerJob, jdl, taskQueueID, rb )
     try:
       shutil.rmtree( workingDirectory )
@@ -192,34 +192,34 @@ class GridPilotDirector(PilotDirector):
       # Parametric jobs are used
       for pilotReference, resourceBroker in submitRet:
         pilotReference = self._getChildrenReferences( proxy, pilotReference, taskQueueID )
-        submittedPilots += len(pilotReference)
-        pilotAgentsDB.addPilotTQReference(pilotReference, taskQueueID, ownerDN,
+        submittedPilots += len( pilotReference )
+        pilotAgentsDB.addPilotTQReference( pilotReference, taskQueueID, ownerDN,
                       vomsGroup, resourceBroker, self.gridMiddleware,
                       pilotRequirements )
     else:
       for pilotReference, resourceBroker in submitRet:
         pilotReference = [pilotReference]
-        submittedPilots += len(pilotReference)
-        pilotAgentsDB.addPilotTQReference(pilotReference, taskQueueID, ownerDN,
-                      vomsGroup, broker=resourceBroker, gridType=self.gridMiddleware,
-                      requirements=pilotRequirements )
+        submittedPilots += len( pilotReference )
+        pilotAgentsDB.addPilotTQReference( pilotReference, taskQueueID, ownerDN,
+                      vomsGroup, broker = resourceBroker, gridType = self.gridMiddleware,
+                      requirements = pilotRequirements )
 
     # add some sleep here
-    time.sleep(0.1*submittedPilots)
+    time.sleep( 0.1 * submittedPilots )
 
     if pilotsToSubmit > pilotsPerJob:
       # Additional submissions are necessary, need to get a new token and iterate.
       pilotsToSubmit -= pilotsPerJob
-      ownerDN    = self.genericPilotDN
+      ownerDN = self.genericPilotDN
       ownerGroup = self.genericPilotGroup
       result = gProxyManager.requestToken( ownerDN, ownerGroup, pilotsToSubmit )
       if not result[ 'OK' ]:
         self.log.error( ERROR_TOKEN, result['Message'] )
         return S_ERROR( ERROR_TOKEN )
-      (token, numberOfUses) = result[ 'Value' ]
+      ( token, numberOfUses ) = result[ 'Value' ]
       for option in pilotOptions:
-        if option.find('-o /Security/ProxyToken=') == 0:
-          pilotOptions.remove(option)
+        if option.find( '-o /Security/ProxyToken=' ) == 0:
+          pilotOptions.remove( option )
       pilotOptions.append( '-o /Security/ProxyToken=%s' % token )
       result = self._submitPilots( workDir, taskQueueDict, pilotOptions,
                                    pilotsToSubmit, ceMask,
@@ -232,14 +232,14 @@ class GridPilotDirector(PilotDirector):
 
     return S_OK( submittedPilots )
 
-  def _prepareJDL(self, taskQueueDict, workingDirectory, pilotOptions, pilotsToSubmit, ceMask, submitPrivatePilot, privateTQ ):
+  def _prepareJDL( self, taskQueueDict, workingDirectory, pilotOptions, pilotsToSubmit, ceMask, submitPrivatePilot, privateTQ ):
     """
       This method should be overridden in a subclass
     """
     self.log.error( '_prepareJDL() method should be implemented in a subclass' )
     sys.exit()
 
-  def _JobJDL(self, taskQueueDict, pilotOptions, ceMask ):
+  def _JobJDL( self, taskQueueDict, pilotOptions, ceMask ):
     """
      The Job JDL is the same for LCG and GLite
     """
@@ -248,21 +248,39 @@ class GridPilotDirector(PilotDirector):
 
     pilotJDL += 'Arguments     = "%s";\n' % ' '.join( pilotOptions )
 
-    pilotJDL += 'TimeRef       = %s;\n' % taskQueueDict['CPUTime']
+    pilotJDL += 'CPUTimeRef    = %s;\n' % taskQueueDict['CPUTime']
 
-    pilotJDL += 'TimePolicy    = ( %s );\n' % self.timePolicy
+    pilotJDL += 'CPUPowerRef   = %s;\n' % self.cpuPowerRef
 
-    requirements = list(self.requirements)
+    pilotJDL += """CPUWorkRef    = real( CPUTimeRef * CPUPowerRef );
+
+Lookup        = "CPUScalingReferenceSI00=*";
+cap = isList( other.GlueCECapability ) ? other.GlueCECapability : { "dummy" };
+i0 = regexp( Lookup, cap[0] ) ? 0 : undefined;
+i1 = isString( cap[1] ) && regexp( Lookup, cap[1] ) ? 1 : i0;
+i2 = isString( cap[2] ) && regexp( Lookup, cap[2] ) ? 2 : i1;
+i3 = isString( cap[3] ) && regexp( Lookup, cap[3] ) ? 3 : i2;
+i4 = isString( cap[4] ) && regexp( Lookup, cap[4] ) ? 4 : i3;
+i5 = isString( cap[5] ) && regexp( Lookup, cap[5] ) ? 5 : i4;
+index = isString( cap[6] ) && regexp( Lookup, cap[6] ) ? 6 : i5;
+i = isUndefined( index ) ? 0 : index;
+
+QueuePowerRef = real( ! isUndefined( index ) ? int( substr( cap[i], size( Lookup ) - 1 ) ) : other.GlueHostBenchmarkSI00 );
+QueueTimeRef  = real( other.GlueCEPolicyMaxCPUTime * 60 );
+QueueWorkRef  = QueuePowerRef * QueueTimeRef;
+"""
+
+    requirements = list( self.requirements )
     if 'GridCEs' in taskQueueDict and taskQueueDict['GridCEs']:
       # if there an explicit Grig CE requested by the TQ, remove the Ranking requirement
       for req in self.requirements:
         if req.strip().lower()[:6] == 'rank >':
-          requirements.remove(req)
+          requirements.remove( req )
 
-    requirements.append( 'other.GlueCEPolicyMaxCPUTime > TimePolicy' )
+    requirements.append( 'QueueWorkRef > WorkRef' )
 
     siteRequirements = '\n || '.join( [ 'other.GlueCEInfoHostName == "%s"' % s for s in ceMask ] )
-    requirements.append( "( %s\n )" %  siteRequirements )
+    requirements.append( "( %s\n )" % siteRequirements )
 
     pilotRequirements = '\n && '.join( requirements )
 
@@ -279,10 +297,10 @@ class GridPilotDirector(PilotDirector):
 
     self.log.verbose( pilotJDL )
 
-    return (pilotJDL,pilotRequirements)
+    return ( pilotJDL, pilotRequirements )
 
 
-  def parseListMatchStdout(self, proxy, cmd, taskQueueID, rb ):
+  def parseListMatchStdout( self, proxy, cmd, taskQueueID, rb ):
     """
       Parse List Match stdout to return list of matched CE's
     """
@@ -296,33 +314,33 @@ class GridPilotDirector(PilotDirector):
       self.__sendErrorMail( rb, 'List Match', cmd, ret, proxy )
       return False
     if ret['Value'][0] != 0:
-      self.log.error( 'Error executing List Match:', str(ret['Value'][0]) + '\n'.join( ret['Value'][1:3] ) )
+      self.log.error( 'Error executing List Match:', str( ret['Value'][0] ) + '\n'.join( ret['Value'][1:3] ) )
       self.__sendErrorMail( rb, 'List Match', cmd, ret, proxy )
       return False
-    self.log.info( 'List Match Execution Time: %.2f for TaskQueue %d' % ((time.time()-start),taskQueueID) )
+    self.log.info( 'List Match Execution Time: %.2f for TaskQueue %d' % ( ( time.time() - start ), taskQueueID ) )
 
     stdout = ret['Value'][1]
     stderr = ret['Value'][2]
     availableCEs = []
     # Parse std.out
-    for line in List.fromChar(stdout,'\n'):
-      if re.search('/jobmanager-',line) or re.search('/cream-',line):
+    for line in List.fromChar( stdout, '\n' ):
+      if re.search( '/jobmanager-', line ) or re.search( '/cream-', line ):
         # TODO: the line has to be stripped from extra info
-        availableCEs.append(line)
+        availableCEs.append( line )
 
     if not availableCEs:
       self.log.info( 'List-Match failed to find CEs for TaskQueue', taskQueueID )
       self.log.info( stdout )
       self.log.info( stderr )
     else:
-      self.log.debug( 'List-Match returns:', str(ret['Value'][0]) + '\n'.join( ret['Value'][1:3] ) )
-      self.log.info( 'List-Match found %s CEs for TaskQueue' % len(availableCEs), taskQueueID )
-      self.log.verbose( ', '.join(availableCEs) )
+      self.log.debug( 'List-Match returns:', str( ret['Value'][0] ) + '\n'.join( ret['Value'][1:3] ) )
+      self.log.info( 'List-Match found %s CEs for TaskQueue' % len( availableCEs ), taskQueueID )
+      self.log.verbose( ', '.join( availableCEs ) )
 
 
     return availableCEs
 
-  def parseJobSubmitStdout(self, proxy, cmd, taskQueueID, rb):
+  def parseJobSubmitStdout( self, proxy, cmd, taskQueueID, rb ):
     """
       Parse Job Submit stdout to return pilot reference
     """
@@ -336,10 +354,10 @@ class GridPilotDirector(PilotDirector):
       self.__sendErrorMail( rb, 'Job Submit', cmd, ret, proxy )
       return False
     if ret['Value'][0] != 0:
-      self.log.error( 'Error executing Job Submit:', str(ret['Value'][0]) + '\n'.join( ret['Value'][1:3] ) )
+      self.log.error( 'Error executing Job Submit:', str( ret['Value'][0] ) + '\n'.join( ret['Value'][1:3] ) )
       self.__sendErrorMail( rb, 'Job Submit', cmd, ret, proxy )
       return False
-    self.log.info( 'Job Submit Execution Time: %.2f for TaskQueue %d' % ((time.time()-start),taskQueueID) )
+    self.log.info( 'Job Submit Execution Time: %.2f for TaskQueue %d' % ( ( time.time() - start ), taskQueueID ) )
 
     stdout = ret['Value'][1]
     stderr = ret['Value'][2]
@@ -348,30 +366,30 @@ class GridPilotDirector(PilotDirector):
 
     failed = 1
     rb = ''
-    for line in List.fromChar(stdout,'\n'):
-      m = re.search("(https:\S+)",line)
-      if (m):
-        glite_id = m.group(1)
+    for line in List.fromChar( stdout, '\n' ):
+      m = re.search( "(https:\S+)", line )
+      if ( m ):
+        glite_id = m.group( 1 )
         submittedPilot = glite_id
         if not rb:
-          m = re.search("https://(.+):.+",glite_id)
-          rb = m.group(1)
+          m = re.search( "https://(.+):.+", glite_id )
+          rb = m.group( 1 )
         failed = 0
     if failed:
-      self.log.error( 'Job Submit returns no Reference:', str(ret['Value'][0]) + '\n'.join( ret['Value'][1:3] ) )
+      self.log.error( 'Job Submit returns no Reference:', str( ret['Value'][0] ) + '\n'.join( ret['Value'][1:3] ) )
       return False
 
     self.log.info( 'Reference %s for TaskQueue %s' % ( glite_id, taskQueueID ) )
 
-    return glite_id,rb
+    return glite_id, rb
 
-  def _writeJDL(self, filename, jdlList):
+  def _writeJDL( self, filename, jdlList ):
     try:
-      f = open(filename,'w')
+      f = open( filename, 'w' )
       f.write( '\n'.join( jdlList ) )
       f.close()
     except Exception, x:
-      self.log.exception( )
+      self.log.exception()
       return ''
 
     return filename
@@ -397,18 +415,18 @@ class GridPilotDirector(PilotDirector):
         mailAddress = self.errorMailAddress
         msg = ''
         if not result['OK']:
-          subject    = "%s: timeout executing %s" % ( rb, name )
-          msg       += '\n%s' % result['Message']
+          subject = "%s: timeout executing %s" % ( rb, name )
+          msg += '\n%s' % result['Message']
         elif result['Value'][0] != 0:
-          if re.search('the server is temporarily drained',' '.join(result['Value'][1:3])):
+          if re.search( 'the server is temporarily drained', ' '.join( result['Value'][1:3] ) ):
             return
-          if re.search('System load is too high:',' '.join(result['Value'][1:3])):
+          if re.search( 'System load is too high:', ' '.join( result['Value'][1:3] ) ):
             return
-          subject    = "%s: error executing %s"  % ( rb, name )
+          subject = "%s: error executing %s" % ( rb, name )
         else:
           return
         msg += ' '.join( command )
-        msg += '\nreturns: %s\n' % str(result['Value'][0]) +  '\n'.join( result['Value'][1:3] )
+        msg += '\nreturns: %s\n' % str( result['Value'][0] ) + '\n'.join( result['Value'][1:3] )
         msg += '\nUsing Proxy:\n' + getProxyInfoAsString( proxy )['Value']
 
         #msg += '\nUsing Proxy:\n' + gProxyManager.
@@ -418,13 +436,13 @@ class GridPilotDirector(PilotDirector):
         if self.__ticketsWMSCache.exists( rb ):
           mailAddress = self.alarmMailAddress
           # the RB was already detected failing a short time ago
-          msg        = 'Submit GGUS Ticket for this error if not already opened\n' + \
-                       'It has been failing at least for %s hours\n' % ( ticketTime/60/60 ) + msg
+          msg = 'Submit GGUS Ticket for this error if not already opened\n' + \
+                       'It has been failing at least for %s hours\n' % ( ticketTime / 60 / 60 ) + msg
         else:
           self.__ticketsWMSCache.add( rb, ticketTime )
 
         if mailAddress:
-          result = NotificationClient().sendMail(mailAddress,subject,msg,fromAddress=self.mailFromAddress)
+          result = NotificationClient().sendMail( mailAddress, subject, msg, fromAddress = self.mailFromAddress )
           if not result[ 'OK' ]:
             self.log.error( "Mail could not be sent" )
 
