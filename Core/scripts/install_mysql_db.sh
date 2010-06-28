baac71d165a39d16e2142a33e29f89f0545fd21a
Adria Casajus <adria@ecm.ub.es>
2010-06-28 14:48:35 +0000

    FIX: Remove added spacing between all of the operators in the file
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@26560 4525493e-7705-40b1-a816-d608a930855b

--- Core/scripts/install_mysql_db.sh
+++ Core/scripts/install_mysql_db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ -z "$DIRACINSTANCE" ] && DIRACINSTANCE = Development
+[ -z "$DIRACINSTANCE" ] && DIRACINSTANCE=Development
 
 if [ ! $1 ]; then
   echo
@@ -9,47 +9,47 @@ if [ ! $1 ]; then
   exit 1
 fi
 
-for dir in / opt / dirac / mysql / db / opt / dirac / mysql / log ; do
+for dir in /opt/dirac/mysql/db /opt/dirac/mysql/log ; do
   [ ! -e $dir ] && echo "MySQL is not installed" && echo "Skip DB installation" && exit
 done
 
 
-cd / opt / dirac / pro
+cd /opt/dirac/pro
 
 while [ $1 ]; do
 
-  DB = $1
+  DB=$1
   shift
-  file = `ls */*/ DB / ${DB}.sql`
+  file=`ls */*/DB/${DB}.sql`
   [ -z "$file" ] && echo "DB $DB not found" && continue
   echo $DB
-  grep - qi "use $DB;" $file || echo ERROR $file
-  grep - qi "use $DB;" $file || continue
+  grep -qi "use $DB;" $file || echo ERROR $file
+  grep -qi "use $DB;" $file || continue
   if [ -z "$MYSQL_ROOT_PWD" ] ; then
-    echo - n 'Enter MySQL root password:'
-    read - t 30 - s passwd || exit 1
+    echo -n 'Enter MySQL root password:'
+    read -t 30 -s passwd || exit 1
     echo
-    export MYSQL_ROOT_PWD = "$passwd"
+    export MYSQL_ROOT_PWD="$passwd"
   fi
 
-  mysqladmin - u root - p$MYSQL_ROOT_PWD create $DB || continue
+  mysqladmin -u root -p$MYSQL_ROOT_PWD create $DB || continue
   if [ -z "$MYSQL_DIRAC_PWD" ] ; then
-    echo - n 'Enter Dirac password:'
-    read - t 30 - s diracpwd || exit 1
+    echo -n 'Enter Dirac password:'
+    read -t 30 -s diracpwd || exit 1
     echo
-    export MYSQL_DIRAC_PWD = "$diracpwd"
+    export MYSQL_DIRAC_PWD="$diracpwd"
   fi
-
-  echo "GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON $DB.* TO Dirac@localhost IDENTIFIED BY '"$MYSQL_DIRAC_PWD"'" | mysql - u root - p$MYSQL_ROOT_PWD
+  
+  echo "GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON $DB.* TO Dirac@localhost IDENTIFIED BY '"$MYSQL_DIRAC_PWD"'" | mysql -u root -p$MYSQL_ROOT_PWD
   echo "GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON $DB.* TO Dirac@'"$HOSTNAME"' IDENTIFIED BY '"$MYSQL_DIRAC_PWD"'" | mysql - u root - p$MYSQL_ROOT_PWD
-  echo "GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON $DB.* TO Dirac@'%' IDENTIFIED BY '"$MYSQL_DIRAC_PWD"'" | mysql - u root - p$MYSQL_ROOT_PWD
+  echo "GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON $DB.* TO Dirac@'%' IDENTIFIED BY '"$MYSQL_DIRAC_PWD"'" | mysql -u root -p$MYSQL_ROOT_PWD
   export DB
   awk 'BEGIN { N = 0 }
          { if ( tolower($0) ~ tolower("use "ENVIRON["DB"]";") ) N=1;
-           if ( N == 1 ) print }' $file | mysql - u root - p$MYSQL_ROOT_PWD $DB
+           if ( N == 1 ) print }' $file | mysql -u root -p$MYSQL_ROOT_PWD $DB
 
-SYSTEM = `dirname $file` ; SYSTEM = `dirname $SYSTEM` ;SYSTEM = `basename $SYSTEM System`
-cat << EOF > / tmp / $DB.cfg
+SYSTEM=`dirname $file` ; SYSTEM=`dirname $SYSTEM` ;SYSTEM=`basename $SYSTEM System`
+cat << EOF > /tmp/$DB.cfg
 Systems
 {
   $SYSTEM
@@ -77,19 +77,19 @@ mainCFG = CFG()
 mainCFG.loadFromFile( "etc/dirac.cfg" )
 db = CFG()
 db.loadFromFile( "/tmp/$DB.cfg" )
-open( "etc/dirac.cfg", "w" ).write( str( mainCFG.mergeWith( db ) ) )
+open( "etc/dirac.cfg", "w" ).write( str(mainCFG.mergeWith( db )) )
 EOF
 
-touch etc / DBs.cfg
+touch etc/DBs.cfg
 python << EOF
 from DIRAC.Core.Utilities.CFG import CFG
 mainCFG = CFG()
 mainCFG.loadFromFile( "etc/DBs.cfg" )
 db = CFG()
 db.loadFromFile( "/tmp/$DB.cfg" )
-open( "etc/DBs.cfg", "w" ).write( str( mainCFG.mergeWith( db ) ) )
+open( "etc/DBs.cfg", "w" ).write( str(mainCFG.mergeWith( db )) )
 EOF
 
 done
 
-[ -z "$MYSQL_ROOT_PWD" ] || mysqladmin - u root - p$MYSQL_ROOT_PWD flush - privileges
+[ -z "$MYSQL_ROOT_PWD" ] || mysqladmin -u root -p$MYSQL_ROOT_PWD flush-privileges
