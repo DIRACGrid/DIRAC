41eaae9197bb4e4672bf8c41ce46bb8545a582ee
Ricardo Graciani <graciani@ecm.ub.es>
2010-07-15 08:38:34 +0000

    FIX: avoid md5 deprecation warning, by moving to hashlib when available
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@26835 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/DB/FileCatalogComponents/Utilities.py
+++ DataManagementSystem/DB/FileCatalogComponents/Utilities.py
@@ -7,63 +7,68 @@
 
 __RCSID__ = "$Id$"
 
-import md5, random, os
+try:
+  import hashlib as md5
+except:
+  import md5
+
+import random, os
 from types import *
 from DIRAC import S_OK, S_ERROR
 
-def checkArgumentFormat(path):
+def checkArgumentFormat( path ):
   """ Bring the various possible form of arguments to FileCatalog methods to
       the standard dictionary form
   """
-  result = checkArgumentDict(path)
+  result = checkArgumentDict( path )
   if not result['OK']:
     return result
-  
+
   # Bring the lfn path to the normalized form
   urls = {}
   for url in path:
-    normpath = os.path.normpath(url)
+    normpath = os.path.normpath( url )
     urls[normpath] = path[url]
-  return S_OK(urls)  
+  return S_OK( urls )
 
-def checkArgumentList(path):
+def checkArgumentList( path ):
   """ Check and process format of the arguments to FileCatalog methods """
-  if type(path) in StringTypes:
+  if type( path ) in StringTypes:
     urls = [path]
-  elif type(path) == ListType:
+  elif type( path ) == ListType:
     urls = path
-  elif type(path) == DictType:
+  elif type( path ) == DictType:
     urls = path.keys()
   else:
-    return S_ERROR("checkArgumentList: Supplied path is not of the correct format")
-  return S_OK(urls)  
+    return S_ERROR( "checkArgumentList: Supplied path is not of the correct format" )
+  return S_OK( urls )
 
-def checkArgumentDict(path):
+def checkArgumentDict( path ):
   """ Check and process format of the arguments to FileCatalog methods """
-  if type(path) in StringTypes:
+  if type( path ) in StringTypes:
     urls = {path:False}
-  elif type(path) == ListType:
+  elif type( path ) == ListType:
     urls = {}
     for url in path:
       urls[url] = False
-  elif type(path) == DictType:
+  elif type( path ) == DictType:
     urls = path
   else:
-    return S_ERROR("checkArgumentDict: Supplied path is not of the correct format")
-  return S_OK(urls)  
+    return S_ERROR( "checkArgumentDict: Supplied path is not of the correct format" )
+  return S_OK( urls )
 
-def generateGuid(checksum,checksumtype):
+def generateGuid( checksum, checksumtype ):
     """ Generate a GUID based on the file checksum
     """
-    
+
     if checksum:
       if checksumtype == "MD5":
         checksumString = checksum
       elif checksumtype == "Adler32":
-        checksumString = str(checksum).zfill(32)  
+        checksumString = str( checksum ).zfill( 32 )
       else:
         checksumString = ''
-      if checksumString:    
+      if checksumString:
         guid = "%s-%s-%s-%s-%s" % ( checksumString[0:8],
                                     checksumString[8:12],
                                     checksumString[12:16],
@@ -71,11 +76,11 @@ def generateGuid(checksum,checksumtype):
                                     checksumString[20:32] )
         guid = guid.upper()
         return guid
-        
+
     # Failed to use the check sum, generate a new guid    
-    myMd5 = md5.new()
+    myMd5 = md5.md5()
     myMd5.update( str( random.getrandbits( 128 ) ) )
-    md5HexString = myMd5.hexdigest()    
+    md5HexString = myMd5.hexdigest()
     guid = "%s-%s-%s-%s-%s" % ( md5HexString[0:8],
                                 md5HexString[8:12],
                                 md5HexString[12:16],
