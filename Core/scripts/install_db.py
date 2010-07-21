a570e867e15eb52e184dd9bfc61132f2593ef546
Ricardo Graciani <graciani@ecm.ub.es>
2010-07-21 12:47:46 +0000

    FIX: make use of new InstallTools. The same tools are use by Service interface than by new installation scripts
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@26888 4525493e-7705-40b1-a816-d608a930855b

---
+++ Core/scripts/install_db.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC/Core/scripts/dirac-install.py $
+"""
+Do the initial installation and configuration of the DIRAC MySQL server
+"""
+__RCSID__ = "$Id: dirac-install.py 26844 2010-07-16 08:44:22Z rgracian $"
+#
+from DIRAC.Core.Utilities import InstallTools
+#
+from DIRAC import gConfig
+InstallTools.exitOnError = True
+#
+from DIRAC.Core.Base import Script
+Script.parseCommandLine()
+args = Script.getPositionalArgs()
+#
+def usage():
+  Script.showHelp( '\n'.join( ['Arguments:',
+                               '  <DB> [<DB>] Name of the DB (mandatory)'] ) )
+  exit( -1 )
+
+if len( args ) < 1:
+  usage()
+  exit( -1 )
+
+InstallTools.getMySQLPasswords()
+for db in args:
+  extension, system = InstallTools.installDatabase( db )['Value']
+  InstallTools.addDatabaseOptionsToCS( gConfig, system, db, True )
