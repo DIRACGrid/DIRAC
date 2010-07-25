ee83668b2f6e1bbb37e504fcdd26bdf3e730b233
Ricardo Graciani <graciani@ecm.ub.es>
2010-07-25 14:28:15 +0000

    FIX: consider also /LocalSite/InstancePath as alternative to rootPath
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@27012 4525493e-7705-40b1-a816-d608a930855b

--- StorageManagementSystem/Agent/SENamespaceCatalogCheckAgent.py
+++ StorageManagementSystem/Agent/SENamespaceCatalogCheckAgent.py
@@ -1,40 +1,40 @@
 ########################################################################
 # $HeadURL: /tmp/libdirac/tmp.FKduyw2449/dirac/DIRAC3/DIRAC/StorageManagementSystem/Agent/SENamespaceCatalogCheck.py,v 1.1 2009/11/03 10:24:20 acsmith Exp $
 ########################################################################
-__RCSID__   = "$Id: SENamespaceCatalogCheck.py,v 1.1 2009/11/03 10:24:20 acsmith Exp $"
+__RCSID__ = "$Id: SENamespaceCatalogCheck.py,v 1.1 2009/11/03 10:24:20 acsmith Exp $"
 __VERSION__ = "$Revision: 1.1 $"
 
 from DIRAC                                                     import S_OK, S_ERROR, gConfig, gMonitor, gLogger, rootPath
 from DIRAC.Core.Base.AgentModule                               import AgentModule
 from DIRAC.DataManagementSystem.Client.ReplicaManager          import ReplicaManager
-from DIRAC.Core.Utilities.List                                 import sortList,breakListIntoChunks
+from DIRAC.Core.Utilities.List                                 import sortList, breakListIntoChunks
 from DIRAC.Core.Utilities.Shifter                              import setupShifterProxyInEnv
-from datetime                                                  import datetime,timedelta
+from datetime                                                  import datetime, timedelta
 
 import re, os, urllib2
 
 
 AGENT_NAME = 'DataManagement/SENamespaceCatalogCheckAgent'
 
-class SENamespaceCatalogCheckAgent(AgentModule):
+class SENamespaceCatalogCheckAgent( AgentModule ):
 
   #############################################################################
-  def initialize(self):
+  def initialize( self ):
     """Sets defaults
     """
     self.replicaManager = ReplicaManager()
-    self.am_setModuleParam("shifterProxy", "DataManager")
-    self.am_setModuleParam("shifterProxyLocation","%s/runit/%s/proxy" % (rootPath,AGENT_NAME))
+    self.am_setModuleParam( "shifterProxy", "DataManager" )
+    self.am_setModuleParam( "shifterProxyLocation", "%s/runit/%s/proxy" % ( gConfig.getValue( '/LocalSite/InstancePath', rootPath ), AGENT_NAME ) )
     return S_OK()
 
   #############################################################################
-  def execute(self):
+  def execute( self ):
     """ The SENamespaceCatalogCheck execution method.
     """
-    self.enableFlag = self.am_getOption('EnableFlag','True')
+    self.enableFlag = self.am_getOption( 'EnableFlag', 'True' )
     if not self.enableFlag == 'True':
-      self.log.info('SENamespaceCatalogCheck is disabled by configuration option %s/EnableFlag' %(self.section))
-      return S_OK('Disabled via CS flag')
+      self.log.info( 'SENamespaceCatalogCheck is disabled by configuration option %s/EnableFlag' % ( self.section ) )
+      return S_OK( 'Disabled via CS flag' )
 
     """
     castorLocations = {  'http://castor.web.cern.ch/castor/DiskPoolDump/lhcb.lhcbmdst.last'      : ['CERN_M-DST'],
@@ -44,26 +44,26 @@ class SENamespaceCatalogCheckAgent(AgentModule):
                          'http://castor.web.cern.ch/castor/DiskPoolDump/lhcb.lhcbuser.last'      : ['CERN-USER']}
                          'http://castor.web.cern.ch/castor/DiskPoolDump/lhcb.lhcbdata.last'      : ['CERN-disk','CERN_MC_M-DST'],
     """
-    castorLocations = {  'http://castor.web.cern.ch/castor/DiskPoolDump/lhcb.lhcbdata.last'      : ['CERN-disk','CERN_MC_M-DST']}
-    for dumpLocation,storageElements in castorLocations.items():
-      gLogger.info('Attempting to get dump at %s for SEs %s' % (dumpLocation,storageElements))
-      res = self.__getDump(dumpLocation)
+    castorLocations = {  'http://castor.web.cern.ch/castor/DiskPoolDump/lhcb.lhcbdata.last'      : ['CERN-disk', 'CERN_MC_M-DST']}
+    for dumpLocation, storageElements in castorLocations.items():
+      gLogger.info( 'Attempting to get dump at %s for SEs %s' % ( dumpLocation, storageElements ) )
+      res = self.__getDump( dumpLocation )
       if not res['OK']:
         return res
-      res = self.verifyCastorDump(res['Value'],storageElements)
+      res = self.verifyCastorDump( res['Value'], storageElements )
       if not res['OK']:
-        gLogger.error("Failed to verify Castor dump",res['Message'])
+        gLogger.error( "Failed to verify Castor dump", res['Message'] )
       else:
-        gLogger.info("Successfully completed Castor dump for %s" % storageElements)
+        gLogger.info( "Successfully completed Castor dump for %s" % storageElements )
     return S_OK()
 
-  def verifyCastorDump(self,dump,storageElements):
-    pfns = self.__getCastorPfns(dump)
+  def verifyCastorDump( self, dump, storageElements ):
+    pfns = self.__getCastorPfns( dump )
     pfnsDict = {}
     for pfn in pfns:
-      if not re.search('dirac_directory',pfn[0]):
+      if not re.search( 'dirac_directory', pfn[0] ):
         pfnsDict[pfn[0]] = pfn[1]
-    res = self.__verifyPfns(pfnsDict,storageElements)
+    res = self.__verifyPfns( pfnsDict, storageElements )
     if not res['OK']:
       return res
     pfnsToRemove = res['Value']['Remove']
@@ -85,74 +85,74 @@ class SENamespaceCatalogCheckAgent(AgentModule):
     """
     return S_OK()
 
-  def __verifyPfns(self,pfnSizes,storageElements):
-    gLogger.info('Checking %s storage files exist in the catalog' % len(pfnSizes))
+  def __verifyPfns( self, pfnSizes, storageElements ):
+    gLogger.info( 'Checking %s storage files exist in the catalog' % len( pfnSizes ) )
     pfnsToRemove = []
     incorrectlyRegistered = []
     allDone = True
     # First get all the PFNs as they should be registered in the catalog
-    for pfns in breakListIntoChunks(sortList(pfnSizes.keys()),100):
-      res = self.replicaManager.getPfnForProtocol(pfns,storageElements[0],withPort=False)
+    for pfns in breakListIntoChunks( sortList( pfnSizes.keys() ), 100 ):
+      res = self.replicaManager.getPfnForProtocol( pfns, storageElements[0], withPort = False )
       if not res['OK']:
         allDone = False
         continue
       for pfn, error in res['Value']['Failed'].items():
-        gLogger.error('Failed to obtain registered PFN for physical file','%s %s' % (pfn,error))
+        gLogger.error( 'Failed to obtain registered PFN for physical file', '%s %s' % ( pfn, error ) )
       if res['Value']['Failed']:
         allDone = False
       catalogStoragePfns = res['Value']['Successful']
       # Determine whether these PFNs are registered and if so obtain the LFN
-      res = self.replicaManager.getCatalogLFNForPFN(catalogStoragePfns.values())
+      res = self.replicaManager.getCatalogLFNForPFN( catalogStoragePfns.values() )
       if not res['OK']:
         allDone = False
         continue
-      for surl in sortList(res['Value']['Failed'].keys()):
+      for surl in sortList( res['Value']['Failed'].keys() ):
         if res['Value']['Failed'][surl] == 'No such file or directory':
           #pfnsToRemove.append(surl)
           print surl
         else:
-          gLogger.error('Failed to get LFN for PFN','%s %s' % (surl,res['Value']['Failed'][surl]))
+          gLogger.error( 'Failed to get LFN for PFN', '%s %s' % ( surl, res['Value']['Failed'][surl] ) )
       existingLFNs = res['Value']['Successful'].values()
       if existingLFNs:
-        res = self.replicaManager.getCatalogReplicas(existingLFNs)
+        res = self.replicaManager.getCatalogReplicas( existingLFNs )
         if not res['OK']:
           allDone = False
           continue
         for lfn, error in res['Value']['Failed'].items():
-          gLogger.error('Failed to obtain registered replicas for LFN','%s %s' % (lfn,error))
+          gLogger.error( 'Failed to obtain registered replicas for LFN', '%s %s' % ( lfn, error ) )
         if res['Value']['Failed']:
           allDone = False
-        for lfn,replicas in res['Value']['Successful'].items():
+        for lfn, replicas in res['Value']['Successful'].items():
           match = False
           for storageElement in storageElements:
-            if storageElement in replicas.keys():  
+            if storageElement in replicas.keys():
               match = True
           if not match:
             pass#incorrectlyRegistered.append(lfn)
             #print lfn
-    gLogger.info("Verification of PFNs complete")
+    gLogger.info( "Verification of PFNs complete" )
     if incorrectlyRegistered:
-      gLogger.info("Found %d files incorrectly registered" % len(incorrectlyRegistered))
+      gLogger.info( "Found %d files incorrectly registered" % len( incorrectlyRegistered ) )
     if pfnsToRemove:
-      gLogger.info("Found %d files to be removed" % len(pfnsToRemove))
-    resDict = {'Remove':pfnsToRemove,'ReRegister':incorrectlyRegistered,'AllDone':allDone}
-    return S_OK(resDict)
+      gLogger.info( "Found %d files to be removed" % len( pfnsToRemove ) )
+    resDict = {'Remove':pfnsToRemove, 'ReRegister':incorrectlyRegistered, 'AllDone':allDone}
+    return S_OK( resDict )
 
-  def __getDCachePfns(self,dumpStr):
+  def __getDCachePfns( self, dumpStr ):
     #exp = re.compile(r'<entry name="(\S+)"><size>(\d+)</size><checksum name="adler32">(\S+)</checksum></entry>')
-    exp = re.compile(r'<entry name="(\S+)"><size>(\d+)</size>')#</entry>')
-    return re.findall(exp,dumpStr)
+    exp = re.compile( r'<entry name="(\S+)"><size>(\d+)</size>' )#</entry>')
+    return re.findall( exp, dumpStr )
 
-  def __getCastorPfns(self,dumpStr):
-    exp = re.compile(r'(\S+) \(\d+\) size (\d+)')#</entry>')
-    return re.findall(exp,dumpStr)
+  def __getCastorPfns( self, dumpStr ):
+    exp = re.compile( r'(\S+) \(\d+\) size (\d+)' )#</entry>')
+    return re.findall( exp, dumpStr )
 
-  def __getDump(self,location):
+  def __getDump( self, location ):
     try:
-      response = urllib2.urlopen(location)
+      response = urllib2.urlopen( location )
       dumpStr = response.read()
-      gLogger.info("Found %s lines in the dump" % len(dumpStr.splitlines()))
-      return S_OK(dumpStr)
-    except Exception,x:
-      gLogger.exception("Exception getting dump",location,x)
-      return S_ERROR("Exception getting dump")
+      gLogger.info( "Found %s lines in the dump" % len( dumpStr.splitlines() ) )
+      return S_OK( dumpStr )
+    except Exception, x:
+      gLogger.exception( "Exception getting dump", location, x )
+      return S_ERROR( "Exception getting dump" )
