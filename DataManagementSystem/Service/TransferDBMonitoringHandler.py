fbaa0cc38e2ebaeb3cf9a01c58d193b76f63bf22
Ricardo Graciani <graciani@ecm.ub.es>
2010-07-25 11:12:43 +0000

    FIX: consider also /LocalSite/InstancePath as alternative to rootPath
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@27004 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/Service/TransferDBMonitoringHandler.py
+++ DataManagementSystem/Service/TransferDBMonitoringHandler.py
@@ -3,7 +3,7 @@
 
 from types import *
 from DIRAC.Core.DISET.RequestHandler import RequestHandler
-from DIRAC import gLogger, gConfig, S_OK, S_ERROR
+from DIRAC import gLogger, gConfig, S_OK, S_ERROR, rootPath
 from DIRAC.DataManagementSystem.DB.TransferDB import TransferDB
 from DIRAC.ConfigurationSystem.Client import PathFinder
 import DIRAC
@@ -12,27 +12,27 @@ import os
 # These are global instances of the DB classes
 transferDB = False
 #this should also select the SourceSite,DestinationSite
-SUMMARY = ['Status','NumberOfFiles','PercentageComplete','TotalSize','SubmitTime','LastMonitor']
+SUMMARY = ['Status', 'NumberOfFiles', 'PercentageComplete', 'TotalSize', 'SubmitTime', 'LastMonitor']
 
 
-RequestsColumns = ['RequestID','RequestName','JobID','OwnerDN','DIRACInstance','Status','CreationTime','SubmissionTime']
-SubRequestsColumns = ['RequestID','SubRequestID','RequestType','Status','Operation','SourceSE','TargetSE','Catalogue','SubmissionTime','LastUpdate']
-FilesColumns = ['SubRequestID','FileID','LFN','Size','PFN','GUID','Md5','Addler','Attempt','Status']
-DatasetColumns = ['SubRequestID','Dataset','Status']
+RequestsColumns = ['RequestID', 'RequestName', 'JobID', 'OwnerDN', 'DIRACInstance', 'Status', 'CreationTime', 'SubmissionTime']
+SubRequestsColumns = ['RequestID', 'SubRequestID', 'RequestType', 'Status', 'Operation', 'SourceSE', 'TargetSE', 'Catalogue', 'SubmissionTime', 'LastUpdate']
+FilesColumns = ['SubRequestID', 'FileID', 'LFN', 'Size', 'PFN', 'GUID', 'Md5', 'Addler', 'Attempt', 'Status']
+DatasetColumns = ['SubRequestID', 'Dataset', 'Status']
 
-def initializeTransferDBMonitoringHandler(serviceInfo):
+def initializeTransferDBMonitoringHandler( serviceInfo ):
 
   global transferDB
   transferDB = TransferDB()
 
-  monitoringSection = PathFinder.getServiceSection("DataManagement/TransferDBMonitoring")
+  monitoringSection = PathFinder.getServiceSection( "DataManagement/TransferDBMonitoring", 'FileCache' )
   #Get data location
   retDict = gConfig.getOption( "%s/DataLocation" % monitoringSection )
   if not retDict[ 'OK' ]:
     return retDict
   dataPath = retDict[ 'Value' ].strip()
   if "/" != dataPath[0]:
-    dataPath = os.path.realpath( "%s/%s" % ( DIRAC.rootPath, dataPath ) )
+    dataPath = os.path.realpath( "%s/%s" % ( gConfig.getValue( '/LocalSite/InstancePath', rootPath ), dataPath ) )
   gLogger.info( "Data will be written into %s" % dataPath )
   try:
     os.makedirs( dataPath )
@@ -48,85 +48,85 @@ def initializeTransferDBMonitoringHandler(serviceInfo):
     return S_ERROR( "Data location is not writable" )
   return S_OK()
 
-class TransferDBMonitoringHandler(RequestHandler):
+class TransferDBMonitoringHandler( RequestHandler ):
 
   types_getChannels = []
-  def export_getChannels(self):
+  def export_getChannels( self ):
     """ Get the details of the channels
     """
     return transferDB.getChannels()
-    
+
   types_increaseChannelFiles = [IntType]
-  def export_increaseChannelFiles(self,channelID):
+  def export_increaseChannelFiles( self, channelID ):
     """ Increase the numner of files on a channel
-    """  
-    return transferDB.increaseChannelFiles(channelID)
-  
+    """
+    return transferDB.increaseChannelFiles( channelID )
+
   types_decreaseChannelFiles = [IntType]
-  def export_decreaseChannelFiles(self,channelID):
+  def export_decreaseChannelFiles( self, channelID ):
     """ Decrease the numner of files on a channel
     """
-    return transferDB.decreaseChannelFiles(channelID)
-    
+    return transferDB.decreaseChannelFiles( channelID )
+
   types_getSites = []
-  def export_getSites(self):
+  def export_getSites( self ):
     """ Get the details of the sites
     """
     return transferDB.getSites()
 
   types_getFTSInfo = [IntType]
-  def export_getFTSInfo(self,ftsReqID):
+  def export_getFTSInfo( self, ftsReqID ):
     """ Get the details of a particular FTS job
     """
-    return transferDB.getFTSJobDetail(ftsReqID)
+    return transferDB.getFTSJobDetail( ftsReqID )
 
   types_getFTSJobs = []
-  def export_getFTSJobs(self):
+  def export_getFTSJobs( self ):
     """ Get all the FTS jobs from the DB
     """
     return transferDB.getFTSJobs()
 
-  types_getChannelObservedThroughput= [IntType]
-  def export_getChannelObservedThroughput(self,interval):
+  types_getChannelObservedThroughput = [IntType]
+  def export_getChannelObservedThroughput( self, interval ):
     """ Get the observed throughput on the channels defined
     """
-    return transferDB.getChannelObservedThroughput(interval)
+    return transferDB.getChannelObservedThroughput( interval )
 
 ##############################################################################
   types_getReqPageSummary = [DictType, StringType, IntType, IntType]
-  def export_getReqPageSummary(self, attrDict, orderAttribute, pageNumber, numberPerPage):
+  def export_getReqPageSummary( self, attrDict, orderAttribute, pageNumber, numberPerPage ):
     """ Get the summary of the fts req information for a given page in the fts monitor
     """
     last_update = None
-    if attrDict.has_key('LastUpdate'):
+    if attrDict.has_key( 'LastUpdate' ):
       last_update = attrDict['LastUpdate']
       del attrDict['LastUpdate']
-    res = transferDB.selectFTSReqs(attrDict, orderAttribute=orderAttribute, newer=last_update)
+    res = transferDB.selectFTSReqs( attrDict, orderAttribute = orderAttribute, newer = last_update )
     if not res['OK']:
-      return S_ERROR('Failed to select FTS requests: '+res['Message'])
+      return S_ERROR( 'Failed to select FTS requests: ' + res['Message'] )
 
     ftsReqList = res['Value']
-    nFTSReqs = len(ftsReqList)
+    nFTSReqs = len( ftsReqList )
     if nFTSReqs == 0:
       resDict = {'TotalFTSReq':nFTSReqs}
-      return S_OK(resDict)
-    iniReq = pageNumber*numberPerPage
-    lastReq = iniReq+numberPerPage
+      return S_OK( resDict )
+    iniReq = pageNumber * numberPerPage
+    lastReq = iniReq + numberPerPage
     if iniReq >= nFTSReqs:
-      return S_ERROR('Page number out of range')
+      return S_ERROR( 'Page number out of range' )
     if lastReq > nFTSReqs:
       lastReq = nFTSReqs
 
     summaryReqList = ftsReqList[iniReq:lastReq]
-    res = transferDB.getAttributesForReqList(summaryReqList,SUMMARY)
+    res = transferDB.getAttributesForReqList( summaryReqList, SUMMARY )
     if not res['OK']:
-      return S_ERROR('Failed to get request summary: '+res['Message'])
+      return S_ERROR( 'Failed to get request summary: ' + res['Message'] )
     summaryDict = res['Value']
 
     resDict = {}
     resDict['TotalFTSReq'] = nFTSReqs
     resDict['SummaryDict'] = summaryDict
-    return S_OK(resDict)
+    return S_OK( resDict )
 
 
 ######################################################################################
@@ -135,43 +135,43 @@ class TransferDBMonitoringHandler(RequestHandler):
 
 
   types_getRequestPageSummaryWeb = [DictType, ListType, IntType, IntType]
-  def export_getRequestPageSummaryWeb(self, selectDict, sortList, startItem, maxItems):
+  def export_getRequestPageSummaryWeb( self, selectDict, sortList, startItem, maxItems ):
     """ Get the summary of the request information for a given page in the
         request monitor in a generic format
     """
     resultDict = {}
     last_update = None
-    if selectDict.has_key('LastUpdate'):
+    if selectDict.has_key( 'LastUpdate' ):
       last_update = selectDict['LastUpdate']
       del selectDict['LastUpdate']
     # Sorting instructions. Only one for the moment.
     if sortList:
-      orderAttribute = sortList[0][0]+":"+sortList[0][1]
+      orderAttribute = sortList[0][0] + ":" + sortList[0][1]
     else:
       orderAttribute = None
 
-    result = transferDB.selectRequests(selectDict, orderAttribute=orderAttribute, newer=last_update)
+    result = transferDB.selectRequests( selectDict, orderAttribute = orderAttribute, newer = last_update )
     if not result['OK']:
-      return S_ERROR('Failed to select jobs: '+result['Message'])
+      return S_ERROR( 'Failed to select jobs: ' + result['Message'] )
 
     requestList = result['Value']
-    nRequests = len(requestList)
+    nRequests = len( requestList )
     resultDict['TotalRecords'] = nRequests
     if nRequests == 0:
-      return S_OK(resultDict)
+      return S_OK( resultDict )
 
     iniRequest = startItem
     lastRequest = iniRequest + maxItems
     if iniRequest >= nRequests:
-      return S_ERROR('Item number out of range')
+      return S_ERROR( 'Item number out of range' )
 
     if lastRequest > nRequests:
       lastRequest = nRequests
 
     summaryRequestList = requestList[iniRequest:lastRequest]
-    result = transferDB.getAttributesForRequestList(summaryRequestList,RequestsColumns)
+    result = transferDB.getAttributesForRequestList( summaryRequestList, RequestsColumns )
     if not result['OK']:
-      return S_ERROR('Failed to get request summary: '+result['Message'])
+      return S_ERROR( 'Failed to get request summary: ' + result['Message'] )
 
     summaryDict = result['Value']
 
@@ -183,53 +183,53 @@ class TransferDBMonitoringHandler(RequestHandler):
     for requestID, requestDict in summaryDict.items():
       rParList = []
       for pname in paramNames:
-        rParList.append(requestDict[pname])
-      records.append(rParList)
+        rParList.append( requestDict[pname] )
+      records.append( rParList )
 
     resultDict['ParameterNames'] = paramNames
     resultDict['Records'] = records
-    return S_OK(resultDict)
+    return S_OK( resultDict )
 
 
 
   types_getSubRequestPageSummaryWeb = [DictType, ListType, IntType, IntType]
-  def export_getSubRequestPageSummaryWeb(self, selectDict, sortList, startItem, maxItems):
+  def export_getSubRequestPageSummaryWeb( self, selectDict, sortList, startItem, maxItems ):
     """ Get the summary of the request information for a given page in the
         request monitor in a generic format
     """
     resultDict = {}
     last_update = None
-    if selectDict.has_key('LastUpdate'):
+    if selectDict.has_key( 'LastUpdate' ):
       last_update = selectDict['LastUpdate']
       del selectDict['LastUpdate']
     # Sorting instructions. Only one for the moment.
     if sortList:
-      orderAttribute = sortList[0][0]+":"+sortList[0][1]
+      orderAttribute = sortList[0][0] + ":" + sortList[0][1]
     else:
       orderAttribute = None
 
-    result = transferDB.selectSubRequests(selectDict, orderAttribute=orderAttribute, newer=last_update)
+    result = transferDB.selectSubRequests( selectDict, orderAttribute = orderAttribute, newer = last_update )
     if not result['OK']:
-      return S_ERROR('Failed to select jobs: '+result['Message'])
+      return S_ERROR( 'Failed to select jobs: ' + result['Message'] )
 
     requestList = result['Value']
-    nRequests = len(requestList)
+    nRequests = len( requestList )
     resultDict['TotalRecords'] = nRequests
     if nRequests == 0:
-      return S_OK(resultDict)
+      return S_OK( resultDict )
 
     iniRequest = startItem
     lastRequest = iniRequest + maxItems
     if iniRequest >= nRequests:
-      return S_ERROR('Item number out of range')
+      return S_ERROR( 'Item number out of range' )
 
     if lastRequest > nRequests:
       lastRequest = nRequests
 
     summaryRequestList = requestList[iniRequest:lastRequest]
-    result = transferDB.getAttributesForSubRequestList(summaryRequestList,SubRequestsColumns)
+    result = transferDB.getAttributesForSubRequestList( summaryRequestList, SubRequestsColumns )
     if not result['OK']:
-      return S_ERROR('Failed to get request summary: '+result['Message'])
+      return S_ERROR( 'Failed to get request summary: ' + result['Message'] )
 
     summaryDict = result['Value']
 
@@ -241,51 +241,51 @@ class TransferDBMonitoringHandler(RequestHandler):
     for requestID, requestDict in summaryDict.items():
       rParList = []
       for pname in paramNames:
-        rParList.append(requestDict[pname])
-      records.append(rParList)
+        rParList.append( requestDict[pname] )
+      records.append( rParList )
 
     resultDict['ParameterNames'] = paramNames
     resultDict['Records'] = records
-    return S_OK(resultDict)
+    return S_OK( resultDict )
 
 
   types_getFilesPageSummaryWeb = [DictType, ListType, IntType, IntType]
-  def export_getFilesPageSummaryWeb(self, selectDict, sortList, startItem, maxItems):
+  def export_getFilesPageSummaryWeb( self, selectDict, sortList, startItem, maxItems ):
     """ Get the summary of the request information for a given page in the
         request monitor in a generic format
     """
     resultDict = {}
     last_update = None
-    if selectDict.has_key('LastUpdate'):
+    if selectDict.has_key( 'LastUpdate' ):
       last_update = selectDict['LastUpdate']
       del selectDict['LastUpdate']
     # Sorting instructions. Only one for the moment.
     if sortList:
-      orderAttribute = sortList[0][0]+":"+sortList[0][1]
+      orderAttribute = sortList[0][0] + ":" + sortList[0][1]
     else:
       orderAttribute = None
 
-    result = transferDB.selectFiles(selectDict, orderAttribute=orderAttribute, newer=last_update)
+    result = transferDB.selectFiles( selectDict, orderAttribute = orderAttribute, newer = last_update )
     if not result['OK']:
-      return S_ERROR('Failed to select jobs: '+result['Message'])
+      return S_ERROR( 'Failed to select jobs: ' + result['Message'] )
 
     requestList = result['Value']
-    nRequests = len(requestList)
+    nRequests = len( requestList )
     resultDict['TotalRecords'] = nRequests
     if nRequests == 0:
-      return S_OK(resultDict)
+      return S_OK( resultDict )
 
     iniRequest = startItem
     lastRequest = iniRequest + maxItems
     if iniRequest >= nRequests:
-      return S_ERROR('Item number out of range')
+      return S_ERROR( 'Item number out of range' )
 
     if lastRequest > nRequests:
       lastRequest = nRequests
     summaryRequestList = requestList[iniRequest:lastRequest]
-    result = transferDB.getAttributesForFilesList(summaryRequestList,FilesColumns)
+    result = transferDB.getAttributesForFilesList( summaryRequestList, FilesColumns )
     if not result['OK']:
-      return S_ERROR('Failed to get request summary: '+result['Message'])
+      return S_ERROR( 'Failed to get request summary: ' + result['Message'] )
 
     summaryDict = result['Value']
 
@@ -297,65 +297,65 @@ class TransferDBMonitoringHandler(RequestHandler):
     for requestID, requestDict in summaryDict.items():
       rParList = []
       for pname in paramNames:
-        rParList.append(requestDict[pname])
-      records.append(rParList)
+        rParList.append( requestDict[pname] )
+      records.append( rParList )
 
     resultDict['ParameterNames'] = paramNames
     resultDict['Records'] = records
-    return S_OK(resultDict)
+    return S_OK( resultDict )
 
   ########################################################################
   #
   # Sub Request monitor methods
   #
   types_getSubRequestStatuses = []
-  def export_getSubRequestStatuses(self):
-    return transferDB.getDistinctSubRequestAttributes('Status')
+  def export_getSubRequestStatuses( self ):
+    return transferDB.getDistinctSubRequestAttributes( 'Status' )
 
   types_getSubRequestTypes = []
-  def export_getSubRequestTypes(self):
-    return transferDB.getDistinctSubRequestAttributes('RequestType')
+  def export_getSubRequestTypes( self ):
+    return transferDB.getDistinctSubRequestAttributes( 'RequestType' )
 
   types_getSubRequestOperations = []
-  def export_getSubRequestOperations(self):
-    return transferDB.getDistinctSubRequestAttributes('Operation')
+  def export_getSubRequestOperations( self ):
+    return transferDB.getDistinctSubRequestAttributes( 'Operation' )
 
   types_getSubRequestSourceSEs = []
-  def export_getSubRequestSourceSEs(self):
-    return transferDB.getDistinctSubRequestAttributes('SourceSE')
+  def export_getSubRequestSourceSEs( self ):
+    return transferDB.getDistinctSubRequestAttributes( 'SourceSE' )
 
   types_getSubRequestTargetSEs = []
-  def export_getSubRequestTargetSEs(self):
-    return transferDB.getDistinctSubRequestAttributes('TargetSE')
+  def export_getSubRequestTargetSEs( self ):
+    return transferDB.getDistinctSubRequestAttributes( 'TargetSE' )
 
   ########################################################################
   #
   # Request monitor methods
   #
   types_getRequestStatuses = []
-  def export_getRequestStatuses(self):
-    return transferDB.getDistinctRequestAttributes('Status')
+  def export_getRequestStatuses( self ):
+    return transferDB.getDistinctRequestAttributes( 'Status' )
 
   ########################################################################
   #
   # File monitor methods
   #
   types_getFilesStatuses = []
-  def export_getFilesStatuses(self):
-    return transferDB.getDistinctFilesAttributes('Status')
+  def export_getFilesStatuses( self ):
+    return transferDB.getDistinctFilesAttributes( 'Status' )
 
   ########################################################################
   #
   # Channels monitor methods
   #
   types_getChannelSources = []
-  def export_getChanelSources(self):
-    return transferDB.getDistinctChannelsAttributes('SourceSite')
+  def export_getChanelSources( self ):
+    return transferDB.getDistinctChannelsAttributes( 'SourceSite' )
 
   types_getChannelDestinations = []
-  def export_getChanelDestinations(self):
-    return transferDB.getDistinctChannelsAttributes('DestinationSite')
+  def export_getChanelDestinations( self ):
+    return transferDB.getDistinctChannelsAttributes( 'DestinationSite' )
 
   types_getChannelStatus = []
-  def export_getChanelStatus(self):
-    return transferDB.getDistinctChannelsAttributes('Status')
+  def export_getChanelStatus( self ):
+    return transferDB.getDistinctChannelsAttributes( 'Status' )
