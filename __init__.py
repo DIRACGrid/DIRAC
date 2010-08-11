a8c7d8b50c4e111ee7b10bfd0e95616bcf29d9c1
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2010-08-11 10:11:36 +0000

    v5r8p2
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@27913 4525493e-7705-40b1-a816-d608a930855b

--- __init__.py
+++ __init__.py
@@ -39,7 +39,7 @@ import sys, os, platform
 majorVersion = 5
 minorVersion = 8
 patchLevel = 0
-preVersion = 1
+preVersion = 2
 
 version = "v%sr%s" % ( majorVersion, minorVersion )
 buildVersion = "v%dr%d" % ( majorVersion, minorVersion )
