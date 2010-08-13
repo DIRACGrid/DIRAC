9c1f2c5298b3708b05c5487315666df3fa49203a
Ricardo Graciani <graciani@ecm.ub.es>
2010-08-13 09:15:57 +0000

    FIX: set keywords
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@27992 4525493e-7705-40b1-a816-d608a930855b

--- Core/scripts/install_portal.py
+++ Core/scripts/install_portal.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC/Core/scripts/dirac-install.py $
+# $HeadURL$
 """
 Do the initial installation of a DIRAC Web portal
 """
-__RCSID__ = "$Id: dirac-install.py 26844 2010-07-16 08:44:22Z rgracian $"
+__RCSID__ = "$Id$"
 #
 from DIRAC.Core.Utilities import InstallTools
 #
