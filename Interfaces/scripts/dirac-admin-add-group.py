996402cf86c7a9c3851e29d8a5f1fe7ac9d5548f
Ricardo Graciani <graciani@ecm.ub.es>
2010-08-27 15:35:12 +0000

    NEW:$ dirac-admin-add-group --help 2010-08-27 15:25:07 UTC Framework  INFO: Add or Modify a Group info in DIRAC 2010-08-27 15:25:07 UTC Framework  INFO: Usage: 2010-08-27 15:25:07 UTC Framework  INFO: dirac-admin-add-group.py [option|cfgfile] ... Property=<Value> ... 2010-08-27 15:25:07 UTC Framework  INFO: Arguments: 2010-08-27 15:25:07 UTC Framework  INFO:  Property=<Value>: Other properties to be added to the User like (VOMSRole=XXXX) 2010-08-27 15:25:07 UTC Framework  INFO: Options: 2010-08-27 15:25:07 UTC Framework  INFO: -o:  --option=  :  Option=value to add 2010-08-27 15:25:07 UTC Framework  INFO: -s:  --section=  :  Set base section for relative parsed options 2010-08-27 15:25:07 UTC Framework  INFO: -c:  --cert=  :  Use server certificate to connect to Core Services 2010-08-27 15:25:07 UTC Framework  INFO: -h  --help  :  Shows this help 2010-08-27 15:25:07 UTC Framework  INFO: -G:  --GroupName:  :  Name of the Group (Mandatory) 2010-08-27 15:25:07 UTC Framework  INFO: -U:  --UserName:  :  Short Name of user to be added to the Group (Allow Multiple instances or None) 2010-08-27 15:25:07 UTC Framework  INFO: -P:  --Property:  :  Property to be added to the Group (Allow Multiple instances or None)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@28173 4525493e-7705-40b1-a816-d608a930855b

---
+++ Interfaces/scripts/dirac-admin-add-group.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+########################################################################
+# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC/Core/scripts/dirac-admin-add-site.py $
+########################################################################
+__RCSID__ = "$Id: dirac-admin-add-site.py 28168 2010-08-27 13:35:11Z rgracian $"
+import DIRAC
+from DIRAC.Core.Base                                   import Script
+
+groupName = None
+groupProperties = []
+userNames = []
+
+def setGroupName( arg ):
+  global groupName
+  if groupName or not arg:
+    Script.showHelp()
+    DIRAC.exit( -1 )
+  groupName = arg
+
+def addUserName( arg ):
+  global userNames
+  if not arg:
+    Script.showHelp()
+    DIRAC.exit( -1 )
+  if not arg in userNames:
+    userNames.append( arg )
+
+def addProperty( arg ):
+  global groupProperties
+  if not arg:
+    Script.showHelp()
+    DIRAC.exit( -1 )
+  if not arg in groupProperties:
+    groupProperties.append( arg )
+
+Script.setUsageMessage( '\n'.join( ['Add or Modify a Group info in DIRAC',
+                                    'Usage:',
+                                    '%s [option|cfgfile] ... Property=<Value> ...' % Script.scriptName,
+                                    'Arguments:',
+                                    ' Property=<Value>: Other properties to be added to the User like (VOMSRole=XXXX)', ] ) )
+
+Script.registerSwitch( 'G:', 'GroupName:', 'Name of the Group (Mandatory)', setGroupName )
+Script.registerSwitch( 'U:', 'UserName:', 'Short Name of user to be added to the Group (Allow Multiple instances or None)', addUserName )
+Script.registerSwitch( 'P:', 'Property:', 'Property to be added to the Group (Allow Multiple instances or None)', addProperty )
+
+Script.parseCommandLine( ignoreErrors = True )
+
+if groupName == None:
+  Script.showHelp()
+  DIRAC.exit( -1 )
+
+args = Script.getPositionalArgs()
+
+from DIRAC.Interfaces.API.DiracAdmin                         import DiracAdmin
+diracAdmin = DiracAdmin()
+exitCode = 0
+errorList = []
+
+groupProps = {}
+if userNames:
+  groupProps['Users'] = ', '.join( userNames )
+if groupProperties:
+  groupProps['Properties'] = ', '.join( groupProperties )
+
+for prop in args:
+  pl = prop.split( "=" )
+  if len( pl ) < 2:
+    errorList.append( ( "in arguments", "Property %s has to include a '=' to separate name from value" % prop ) )
+    exitCode = 255
+  else:
+    pName = pl[0]
+    pValue = "=".join( pl[1:] )
+    Script.gLogger.info( "Setting property %s to %s" % ( pName, pValue ) )
+    groupProps[ pName ] = pValue
+
+print groupProps
+if not diracAdmin.csModifyGroup( groupName, groupProps, createIfNonExistant = True )['Value']:
+  errorList.append( ( "add group", "Cannot register group %s" % groupName ) )
+  exitCode = 255
+else:
+  result = diracAdmin.csCommitChanges()
+  if not result[ 'OK' ]:
+    errorList.append( ( "commit", result[ 'Message' ] ) )
+    exitCode = 255
+
+for error in errorList:
+  Script.gLogger.error( "%s: %s" % error )
+
+DIRAC.exit( exitCode )
