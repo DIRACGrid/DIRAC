507c77536bdef451913cda6d6d1cac89703ab394
Ricardo Graciani <graciani@ecm.ub.es>
2010-08-27 16:44:29 +0000

    NEW: 2010-08-27 16:43:14 UTC Framework  INFO: Add or Modify a Host info in DIRAC 2010-08-27 16:43:14 UTC Framework  INFO: Usage: 2010-08-27 16:43:14 UTC Framework  INFO: dirac-admin-add-host.py [option|cfgfile] ... Property=<Value> ... 2010-08-27 16:43:14 UTC Framework  INFO: Arguments: 2010-08-27 16:43:14 UTC Framework  INFO:  Property=<Value>: Other properties to be added to the User like (Responsible=XXXX) 2010-08-27 16:43:14 UTC Framework  INFO: Options: 2010-08-27 16:43:14 UTC Framework  INFO: -o:  --option=  :  Option=value to add 2010-08-27 16:43:14 UTC Framework  INFO: -s:  --section=  :  Set base section for relative parsed options 2010-08-27 16:43:14 UTC Framework  INFO: -c:  --cert=  :  Use server certificate to connect to Core Services 2010-08-27 16:43:14 UTC Framework  INFO: -h  --help  :  Shows this help 2010-08-27 16:43:14 UTC Framework  INFO: -H:  --HostName:  :  Name of the Host (Mandatory) 2010-08-27 16:43:14 UTC Framework  INFO: -D:  --HostDN:  :  DN of the Host Certificate (Mandatory) 2010-08-27 16:43:14 UTC Framework  INFO: -P:  --Property:  :  Property to be added to the Host (Allow Multiple instances or None)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@28178 4525493e-7705-40b1-a816-d608a930855b

---
+++ Interfaces/scripts/dirac-admin-add-host.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+########################################################################
+# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC/Core/scripts/dirac-admin-add-site.py $
+########################################################################
+__RCSID__ = "$Id: dirac-admin-add-site.py 28168 2010-08-27 13:35:11Z rgracian $"
+import DIRAC
+from DIRAC.Core.Base                                   import Script
+
+hostName = None
+hostDN = None
+hostProperties = []
+
+def setHostName( arg ):
+  global hostName
+  if hostName or not arg:
+    Script.showHelp()
+    DIRAC.exit( -1 )
+  hostName = arg
+
+def setHostDN( arg ):
+  global hostDN
+  if hostDN or not arg:
+    Script.showHelp()
+    DIRAC.exit( -1 )
+  hostDN = arg
+
+def addProperty( arg ):
+  global hostProperties
+  if not arg:
+    Script.showHelp()
+    DIRAC.exit( -1 )
+  if not arg in hostProperties:
+    hostProperties.append( arg )
+
+Script.setUsageMessage( '\n'.join( ['Add or Modify a Host info in DIRAC',
+                                    'Usage:',
+                                    '%s [option|cfgfile] ... Property=<Value> ...' % Script.scriptName,
+                                    'Arguments:',
+                                    ' Property=<Value>: Other properties to be added to the User like (Responsible=XXXX)', ] ) )
+
+Script.registerSwitch( 'H:', 'HostName:', 'Name of the Host (Mandatory)', setHostName )
+Script.registerSwitch( 'D:', 'HostDN:', 'DN of the Host Certificate (Mandatory)', setHostDN )
+Script.registerSwitch( 'P:', 'Property:', 'Property to be added to the Host (Allow Multiple instances or None)', addProperty )
+
+Script.parseCommandLine( ignoreErrors = True )
+
+if hostName == None or hostDN == None:
+  Script.showHelp()
+  DIRAC.exit( -1 )
+
+args = Script.getPositionalArgs()
+
+from DIRAC.Interfaces.API.DiracAdmin                         import DiracAdmin
+diracAdmin = DiracAdmin()
+exitCode = 0
+errorList = []
+
+hostProps = {'DN': hostDN}
+if hostProperties:
+  hostProps['Properties'] = ', '.join( hostProperties )
+
+for prop in args:
+  pl = prop.split( "=" )
+  if len( pl ) < 2:
+    errorList.append( ( "in arguments", "Property %s has to include a '=' to separate name from value" % prop ) )
+    exitCode = 255
+  else:
+    pName = pl[0]
+    pValue = "=".join( pl[1:] )
+    Script.gLogger.info( "Setting property %s to %s" % ( pName, pValue ) )
+    hostProps[ pName ] = pValue
+
+if not diracAdmin.csModifyHost( hostName, hostProps, createIfNonExistant = True )['Value']:
+  errorList.append( ( "add host", "Cannot register host %s" % hostName ) )
+  exitCode = 255
+else:
+  result = diracAdmin.csCommitChanges()
+  if not result[ 'OK' ]:
+    errorList.append( ( "commit", result[ 'Message' ] ) )
+    exitCode = 255
+
+for error in errorList:
+  Script.gLogger.error( "%s: %s" % error )
+
+DIRAC.exit( exitCode )
