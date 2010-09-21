9b28650d0790604d2fdc1e4a6e43ad20c0a57daf
Ricardo Graciani <graciani@ecm.ub.es>
2010-09-21 11:44:09 +0000

    NEW: move maxCPUSegments from TaskQueueDB (requires MySQLdb module)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@28465 4525493e-7705-40b1-a816-d608a930855b

---
+++ WorkloadManagementSystem/private/Queues.py
@@ -0,0 +1,4 @@
+_MIN = 60
+_HOUR = 3600
+_DAY = 86400
+maxCPUSegments = [ 6 * _MIN, 30 * _MIN, 1 * _HOUR, 6 * _HOUR, 12 * _HOUR, 1 * _DAY, 2 * _DAY, 3 * _DAY, 4 * _DAY, 6 * _DAY, 8 * _DAY, 10 * _DAY, int( 12.5 * _DAY ) ]
