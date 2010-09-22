fae7a1f914de950ed800af52fd1a9a180238400c
Ricardo Graciani <graciani@ecm.ub.es>
2010-09-22 09:13:10 +0000

    FIX: set Keywords
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@28478 4525493e-7705-40b1-a816-d608a930855b

--- ConfigurationSystem/Agent/__init__.py
+++ ConfigurationSystem/Agent/__init__.py
@@ -1,5 +1,5 @@
 ############################################################
-# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk/LHCbDIRAC/Interfaces/API/__init__.py $
+# $HeadURL$
 ############################################################
 
 """
@@ -7,4 +7,4 @@
    
 """
 
-__RCSID__ = "$Id: __init__.py 18700 2009-11-30 13:48:50Z paterson $"
+__RCSID__ = "$Id$"
