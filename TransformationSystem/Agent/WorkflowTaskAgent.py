bd5a98e1e4f2d8061d5c07cd40df41f41052907e
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2010-09-23 14:19:23 +0000

    don't take hospital prods in the agent configuration.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@28559 4525493e-7705-40b1-a816-d608a930855b

--- TransformationSystem/Agent/WorkflowTaskAgent.py
+++ TransformationSystem/Agent/WorkflowTaskAgent.py
@@ -20,5 +20,4 @@ class WorkflowTaskAgent( TaskManagerAgentBase, WorkflowTasks ):
     self.transType = self.am_getOption("TransType",['MCSimulation', 'DataReconstruction', 'DataStripping', 'MCStripping', 'Merge'])
     self.am_setModuleParam( 'shifterProxy', 'ProductionManager' )
     self.am_setModuleParam( "shifterProxyLocation", "%s/runit/%s/proxy" % ( gConfig.getValue( '/LocalSite/InstancePath', rootPath ), AGENT_NAME ) )
-    self.hospitalProds = self.am_getOption('HospitalProds',[])
     return S_OK()
