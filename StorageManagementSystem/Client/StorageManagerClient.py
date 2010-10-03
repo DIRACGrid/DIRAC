aa560437ba612a2cdac007786bb67fe1768b936c
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2010-10-03 10:13:07 +0000

    added keywords
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@28874 4525493e-7705-40b1-a816-d608a930855b

--- StorageManagementSystem/Client/StorageManagerClient.py
+++ StorageManagementSystem/Client/StorageManagerClient.py
@@ -1,9 +1,9 @@
 """ Class that contains client access to the StorageManagerDB handler. """
 ########################################################################
-# $Id: StorageManagerClient.py .py 22438 2010-03-08 08:33:14Z acsmith $
-# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC/TransformationSystem/Client/TransformationDBClient.py $
+# $Id$
+# $HeadURL$
 ########################################################################
-__RCSID__ = "$Id: StorageManagerClient.py 22438 2010-03-08 08:33:14Z acsmith $"
+__RCSID__ = "$Id$"
 
 from DIRAC                                          import gLogger, gConfig, S_OK, S_ERROR
 from DIRAC.Core.Base.Client                         import Client
