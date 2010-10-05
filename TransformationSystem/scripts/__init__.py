8bc24960b3dc88b6b8cea69fbb81e4cf1b2ba3ba
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2010-10-05 14:54:43 +0000

    Script to verify the output of a transformation from the command line.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@29040 4525493e-7705-40b1-a816-d608a930855b

---
+++ TransformationSystem/scripts/__init__.py
@@ -0,0 +1,2 @@
+# $Id: __init__.py 18161 2009-11-11 12:07:09Z acasajus $
+__RCSID__ = "$Revision: 1.2 $"
---
+++ TransformationSystem/scripts/dirac-transformation-verify-outputdata.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+from DIRAC.Core.Base.Script import parseCommandLine
+parseCommandLine()
+########################################################################
+# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk/LHCbDIRAC/TransformationSystem/scripts/dirac-transformation-verify-outputdata.py $
+########################################################################
+__RCSID__   = "$Id: dirac-transformation-verify-outputdata.py 29039 2010-10-05 14:49:58Z acsmith $"
+__VERSION__ = "$Revision: 1.10 $"
+
+import sys
+if len(sys.argv) < 2:
+  print 'Usage: dirac-transformation-verify-outputdata transID [transID] [transID]'
+  sys.exit()
+else:
+  transIDs = [int(arg) for arg in sys.argv[1:]]
+
+from LHCbDIRAC.TransformationSystem.Agent.ValidateOutputDataAgent       import ValidateOutputDataAgent
+from LHCbDIRAC.TransformationSystem.Client.TransformationDBClient       import TransformationDBClient
+from DIRAC import gLogger
+import DIRAC
+
+agent = ValidateOutputDataAgent('Transformation/ValidateOutputDataAgent','dirac-transformation-verify-outputdata')
+agent.initialize()
+
+client = TransformationDBClient()
+for transID in transIDs:
+  res = client.getTransformationParameters(transID,['Status'])
+  if not res['OK']:
+    gLogger.error("Failed to determine transformation status")
+    gLogger.error(res['Message'])
+    continue
+  status = res['Value']
+  if not status in ['ValidatingOutput','WaitingIntegrity','Active','Completed']:
+    gLogger.error("The transformation is in %s status and can not be validated" % status)
+    continue
+  agent.checkTransformationIntegrity(transID)
