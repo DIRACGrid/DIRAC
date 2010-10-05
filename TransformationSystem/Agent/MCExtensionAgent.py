f57401e021ee98b1489814b0b8b75281dc4523a8
Andrew C. Smith <Andrew.Cameron.Smith@cern.ch>
2010-10-05 17:44:44 +0000

    First commit of agent to automatically extend MC production based on a max number supplied in the transformation definition.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@29053 4525493e-7705-40b1-a816-d608a930855b

---
+++ TransformationSystem/Agent/MCExtensionAgent.py
@@ -0,0 +1,92 @@
+########################################################################
+# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk/LHCbDIRAC/TransformationSystem/Agent/TransformationCleaningAgent.py $
+########################################################################
+__RCSID__ = "$Id: TransformationCleaningAgent.py 28908 2010-10-05 07:57:08Z acsmith $"
+__VERSION__ = "$Revision: 1.5 $"
+
+from DIRAC                                                          import S_OK, S_ERROR, gConfig, gMonitor, gLogger, rootPath
+from DIRAC.Core.Base.AgentModule                                    import AgentModule
+from DIRAC.Core.Utilities.List                                      import sortList
+from DIRAC.Core.Utilities.Shifter                                   import setupShifterProxyInEnv
+from DIRAC.TransformationSystem.Client.TransformationDBClient       import TransformationDBClient
+
+AGENT_NAME = 'Transformation/MCExtensionAgent'
+
+class MCExtensionAgent(AgentModule):
+
+  #############################################################################
+  def initialize( self ):
+    """Sets defaults """
+    self.transClient = TransformationDBClient()
+    self.am_setModuleParam( "shifterProxy", "DataManager" )
+    self.am_setModuleParam( "shifterProxyLocation", "%s/runit/%s/proxy" % ( gConfig.getValue( '/LocalSite/InstancePath', rootPath ), AGENT_NAME ) )
+    self.transformationTypes = sortList(self.am_getOption( 'TransformationTypes', ['MCSimulation','Simulation']))
+    gLogger.info("Will consider the following transformation types: %s" % str(self.transformationTypes))
+    self.maxIterationTasks = self.am_getOption('TasksPerIteration', 50)
+    gLogger.info("Will create a maximum of %s tasks per iteration" % self.maxIterationTasks)
+    self.maxFailRate = self.am_getOption('MaxFailureRate', 30)
+    gLogger.info("Will not submit tasks for transformations with failure rate greater than %s%s" % (self.maxFailRate,'%'))
+    self.maxWaitingJobs = self.am_getOption('MaxWaitingJobs', 1000)
+    gLogger.info("Will not submit tasks for transformations with more than %d waiting jobs" % self.maxWaitingJobs)
+    return S_OK()
+
+  #############################################################################
+  def execute(self):
+    """ The MCExtensionAgent execution method."""
+
+    self.enableFlag = self.am_getOption('EnableFlag','True')
+    if not self.enableFlag == 'True':
+      self.log.info('TransformationCleaningAgent is disabled by configuration option %s/EnableFlag'%(self.section))
+      return S_OK('Disabled via CS flag')
+
+    # Obtain the transformations in Cleaning status and remove any mention of the jobs/files
+    res = self.transClient.getTransformations({'Status':'Active','Type':self.transformationTypes})
+    if res['OK']:
+      for transDict in res['Value']:
+        transID = transDict['TransformationID']
+        maxTasks = transDict['MaxNumberOfTasks']
+        self.extendTransformation(transID,maxTasks)
+    return S_OK()
+  
+  def extendTransformation(self,transID,maxTasks):
+    gLogger.info("Considering extension of transformation %d" % transID)
+    # Get the current count of tasks submitted for this transformation
+    res = self.transClient.getTransformationTaskStats(transID)
+    if not res['OK']:
+      gLogger.error("Failed to get task statistics","%s %s"  % (transID,res['Message']))
+      return res
+    statusDict = res['Value']
+    gLogger.verbose("Current task count for transformation %d" % transID)
+    for status in sortList(statusDict.keys()):
+      statusCount = statusDict[status]
+      gLogger.verbose("%s : %s" % (status.ljust(20),str(statusCount).rjust(8)))
+    # Determine the number of tasks to be created
+    numberOfTasks = self.calculateTaskNumber(maxTasks,statusDict)
+    if not numberOfTasks:
+      gLogger.info("No tasks required for transformation %d" % taskID)
+      return S_OK()
+    # Extend the transformation by the determined number of tasks
+    res = self.transClient.extendTransformation(transID,numberOfTasks)
+    if not res['OK']:
+      gLoger.error("Failed to extend transformation", "%s %s" % (transID,res['Message']))
+      return res
+    gLogger.info("Successfully extended transformation %d by %d tasks" % (transID,numberOfTasks))
+    return S_OK()
+  
+  def calculateTaskNumber(self,maxTasks,statusDict):
+    done = statusDict.get('Done',0)
+    failed = statusDict.get('Failed',0)
+    running = statusDict.get('Running',0)
+    total = statusDict.get('Total',0)
+    # If the failure rate is higher than acceptable
+    if (total != 0) and ((100.0*float(failed)/float(total)) > self.maxFailRate):
+      return 0
+    # If we already have enough completed jobs
+    if done >= maxTasks:
+      return 0
+    if waiting > self.maxWaitingJobs:
+      return 0
+    numberOfTasks = maxTasks-(total-failed)
+    if numberOfTasks > self.maxIterationTasks:
+      numberOfTasks = self.maxIterationTasks
+    return numberOfTasks
