a76409742fd16e5be553cba77f89f3d222e8581e
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2010-10-13 15:55:51 +0000

    Reverting back the previous "fix"
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@29445 4525493e-7705-40b1-a816-d608a930855b

--- Resources/Computing/ComputingElement.py
+++ Resources/Computing/ComputingElement.py
@@ -451,9 +451,8 @@ def getResourceDict( ceName = None ):
 
   # now add some defaults
   resourceDict['Setup'] = gConfig.getValue('/DIRAC/Setup','None')
-  # A.T. causing problems if not defined. We should get rid of this method completely
-  #if not 'CPUTime' in resourceDict:
-  #  resourceDict['CPUTime'] = maxCPUSegments[-1]
+  if not 'CPUTime' in resourceDict:
+    resourceDict['CPUTime'] = maxCPUSegments[-1]
   if not 'PilotType' in resourceDict:
     # FIXME: this is a test, we need the list of available types
     resourceDict['PilotType'] = 'private'
