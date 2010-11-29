109c68e3da731312f1f5734f46065f3d4155b371
Ricardo Graciani <graciani@ecm.ub.es>
2010-11-29 07:11:55 +0000

    FIX: Proper Help message
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@30882 4525493e-7705-40b1-a816-d608a930855b

--- ConfigurationSystem/scripts/dirac-configuration-cli.py
+++ ConfigurationSystem/scripts/dirac-configuration-cli.py
@@ -4,14 +4,19 @@
 # File :   dirac-configuration-cli
 # Author : Adria Casajus
 ########################################################################
+"""
+  Command line interface to DIRAC Configuration Server
+"""
 __RCSID__   = "$Id$"
-__VERSION__ = "$Revision: 1.4 $"
 
 import DIRAC
 from DIRAC.Core.Base import Script
 from DIRAC.ConfigurationSystem.Client.CSCLI import CSCLI
 
 Script.localCfg.addDefaultEntry( "LogLevel", "fatal" )
+Script.setUsageMessage('\n'.join( [ __doc__.split( '\n' )[1],
+                                    'Usage:',
+                                    '  %s [option|cfgfile] ...' % Script.scriptName, ] )   )
 Script.parseCommandLine()
 
 CSCLI().start()
