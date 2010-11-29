eb3c0e6a72be98fea6eba8c1375857870e4e4762
Ricardo Graciani <graciani@ecm.ub.es>
2010-11-29 08:23:34 +0000

    FIX: extra code Documentation + indentation
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@30896 4525493e-7705-40b1-a816-d608a930855b

--- ConfigurationSystem/Client/Config.py
+++ ConfigurationSystem/Client/Config.py
@@ -1,7 +1,11 @@
 # $HeadURL$
+"""
+  Instantiate the global Configuration Object
+  gConfig is used everywhere within DIRAC to access Configuration data
+"""
 __RCSID__ = "$Id$"
 from DIRAC.ConfigurationSystem.private.ConfigurationClient import ConfigurationClient
 
 gConfig = ConfigurationClient()
 def getConfig():
-    return gConfig
+  return gConfig
