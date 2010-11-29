536dc629a55e627bfbec14c124c8e449a1969d20
Ricardo Graciani <graciani@ecm.ub.es>
2010-11-29 10:45:01 +0000

    FIX: indentation
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@30921 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/Tests.py
+++ Core/Utilities/Tests.py
@@ -11,15 +11,15 @@ def run( testDict, name=None ):
   for module in testDict:
     testFailed = False
     for test in testDict[module]:
-        retVal = apply( test['method'], test['arguments'] )
-        if retVal <> test['output']:
-          DIRAC.gLogger.error( "Failed test:", "%s\n" % module +
-                               "%s%s should return '%s'\n" % ( test['method'].__name__, 
-                                                               test['arguments'], 
-                                                               test['output'] ) +
-                               "It returns '%s' %s " % ( retVal, type( retVal ) ) )
-          testFailed = True
-          break
+      retVal = apply( test['method'], test['arguments'] )
+      if retVal != test['output']:
+        DIRAC.gLogger.error( "Failed test:", "%s\n" % module +
+                             "%s%s should return '%s'\n" % ( test['method'].__name__, 
+                                                             test['arguments'], 
+                                                             test['output'] ) +
+                             "It returns '%s' %s " % ( retVal, type( retVal ) ) )
+        testFailed = True
+        break
     if testFailed:
       DIRAC.gLogger.exception('')
       DIRAC.sys.exit(-1)
