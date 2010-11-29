1b10c048d0012f33ef5136597a6659a946b1120d
Ricardo Graciani <graciani@ecm.ub.es>
2010-11-29 11:46:08 +0000

    FIX: indentation
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@30943 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/DB/FileCatalogComponents/Utilities.py
+++ DataManagementSystem/DB/FileCatalogComponents/Utilities.py
@@ -58,33 +58,33 @@ def checkArgumentDict( path ):
   return S_OK( urls )
 
 def generateGuid( checksum, checksumtype ):
-    """ Generate a GUID based on the file checksum
-    """
+  """ Generate a GUID based on the file checksum
+  """
 
-    if checksum:
-      if checksumtype == "MD5":
-        checksumString = checksum
-      elif checksumtype == "Adler32":
-        checksumString = str( checksum ).zfill( 32 )
-      else:
-        checksumString = ''
-      if checksumString:
-        guid = "%s-%s-%s-%s-%s" % ( checksumString[0:8],
-                                    checksumString[8:12],
-                                    checksumString[12:16],
-                                    checksumString[16:20],
-                                    checksumString[20:32] )
-        guid = guid.upper()
-        return guid
+  if checksum:
+    if checksumtype == "MD5":
+      checksumString = checksum
+    elif checksumtype == "Adler32":
+      checksumString = str( checksum ).zfill( 32 )
+    else:
+      checksumString = ''
+    if checksumString:
+      guid = "%s-%s-%s-%s-%s" % ( checksumString[0:8],
+                                  checksumString[8:12],
+                                  checksumString[12:16],
+                                  checksumString[16:20],
+                                  checksumString[20:32] )
+      guid = guid.upper()
+      return guid
 
-    # Failed to use the check sum, generate a new guid    
-    myMd5 = md5.md5()
-    myMd5.update( str( random.getrandbits( 128 ) ) )
-    md5HexString = myMd5.hexdigest()
-    guid = "%s-%s-%s-%s-%s" % ( md5HexString[0:8],
-                                md5HexString[8:12],
-                                md5HexString[12:16],
-                                md5HexString[16:20],
-                                md5HexString[20:32] )
-    guid = guid.upper()
-    return guid
+  # Failed to use the check sum, generate a new guid    
+  myMd5 = md5.md5()
+  myMd5.update( str( random.getrandbits( 128 ) ) )
+  md5HexString = myMd5.hexdigest()
+  guid = "%s-%s-%s-%s-%s" % ( md5HexString[0:8],
+                              md5HexString[8:12],
+                              md5HexString[12:16],
+                              md5HexString[16:20],
+                              md5HexString[20:32] )
+  guid = guid.upper()
+  return guid
