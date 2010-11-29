bac45ee261ab766b030f5750f1ddf2cb656dcb9a
Ricardo Graciani <graciani@ecm.ub.es>
2010-11-29 09:56:49 +0000

    FIX: typo
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@30912 4525493e-7705-40b1-a816-d608a930855b

--- Core/scripts/dirac-version.py
+++ Core/scripts/dirac-version.py
@@ -4,10 +4,9 @@
 # File :   dirac-version
 # Author : Ricardo Graciani
 ########################################################################
+"""
+    print version of current DIRAC installation
+"""
 __RCSID__   = "$Id: dirac-version,v 1.3 2008/03/22 10:39:02 rgracian Exp $"
-__VERSION__ = "$Revision: 1.3 $"
-"""
-    print version of current DIRAC instalation
-"""
 import DIRAC
 print DIRAC.version
