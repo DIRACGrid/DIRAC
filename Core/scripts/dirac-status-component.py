840abaed059a947865c1257ee63397b4fdd24db4
Ricardo Graciani <graciani@ecm.ub.es>
2010-11-29 14:54:22 +0000

    FIX: use incode doc for Usage Message + header
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@30959 4525493e-7705-40b1-a816-d608a930855b

--- Core/scripts/dirac-status-component.py
+++ Core/scripts/dirac-status-component.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
-# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC/Core/scripts/dirac-install.py $
+########################################################################
+# $HeadURL$
+# File :   dirac-status-compoment
+# Author : Ricardo Graciani
+########################################################################
 """
-Do the initial installation and configuration of the DIRAC MySQL server
+Status of DIRAC components using runsvstat utility
 """
-__RCSID__ = "$Id: dirac-install.py 26844 2010-07-16 08:44:22Z rgracian $"
+__RCSID__ = "$Id$"
 #
 from DIRAC.Core.Base import Script
 Script.disableCS()
-Script.setUsageMessage( '\n'.join( ['Status of DIRAC components using runsvstat utility',
-                                    'Usage:',
-                                    '  %s [option|cfgfile] ... [system [service|agent]]' % Script.scriptName,
-                                    'Arguments:',
-                                    '  system:        Name of the system for the component (default *: all)',
-                                    '  service|agent: Name of the particular component (default *: all)' ] ) )
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                     'Usage:',
+                                     '  %s [option|cfgfile] ... [system [service|agent]]' % Script.scriptName,
+                                     'Arguments:',
+                                     '  system:        Name of the system for the component (default *: all)',
+                                     '  service|agent: Name of the particular component (default *: all)' ] ) )
 Script.parseCommandLine()
 args = Script.getPositionalArgs()
 if len( args ) > 2:
