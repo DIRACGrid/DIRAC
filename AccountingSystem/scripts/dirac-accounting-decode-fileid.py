6a5a3715c16fc649541bbfd3aed2f806631807e2
Adria Casajus <adria@ecm.ub.es>
2010-11-30 15:06:46 +0000

    NEW: Allow full URLs to be passed as params
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31037 4525493e-7705-40b1-a816-d608a930855b

--- AccountingSystem/scripts/dirac-accounting-decode-fileid.py
+++ AccountingSystem/scripts/dirac-accounting-decode-fileid.py
@@ -2,10 +2,12 @@
 ########################################################################
 # $HeadURL$
 ########################################################################
-__RCSID__   = "$Id$"
+__RCSID__ = "$Id$"
 
 import pprint
 import sys
+import urlparse
+import cgi
 from DIRAC import gLogger
 from DIRAC.Core.Base import Script
 from DIRAC.AccountingSystem.private.FileCoding import extractRequestFromFileId
@@ -15,10 +17,17 @@ Script.parseCommandLine()
 fileIds = Script.getPositionalArgs()
 
 for fileId in fileIds:
+  #Try to find if it's a url
+  parseRes = urlparse.urlparse( fileId )
+  if parseRes.query:
+    queryRes = cgi.parse_qs( parseRes.query )
+    if 'file' in queryRes:
+      fileId = queryRes[ 'file' ][0]
+  #Decode
   result = extractRequestFromFileId( fileId )
   if not result[ 'OK' ]:
-    gLogger.error( "Could not decode fileId","'%s', error was %s" % ( fileId, result[ 'Messaage' ] ) )
-    sys.exit(1)
+    gLogger.error( "Could not decode fileId", "'%s', error was %s" % ( fileId, result[ 'Message' ] ) )
+    sys.exit( 1 )
   gLogger.notice( "Decode for '%s' is:\n%s" % ( fileId, pprint.pformat( result[ 'Value' ] ) ) )
 
-sys.exit(0)
+sys.exit( 0 )
