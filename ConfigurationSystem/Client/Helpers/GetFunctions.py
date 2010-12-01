885ec5d37b29da61471f0950d082313410b3b562
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-01 18:25:59 +0000

    NEW: Configuration Helpers to get list of extensions from CS and list of locally installed extensions
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31113 4525493e-7705-40b1-a816-d608a930855b

---
+++ ConfigurationSystem/Client/Helpers/GetFunctions.py
@@ -0,0 +1,32 @@
+########################################################################
+# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC/WorkloadManagementSystem/scripts/dirac-admin-submit-pilot-for-job.py $
+# File :   GetFunctions.py
+# Author : Ricardo Graciani
+########################################################################
+"""
+Some Helper functions to retrieve common location from the CS
+"""
+__RCSID__ = "$Id: dirac-admin-submit-pilot-for-job.py 18161 2009-11-11 12:07:09Z acasajus $"
+
+from DIRAC import gConfig
+
+def getCSExtensions():
+  """
+    Return list of extensions registered in the CS
+    They do not include DIRAC
+  """
+  return gConfig.getValue( '/DIRAC/Extensions', [] )
+
+def getInstalledExtensions():
+  """
+    Return list of extensions registered in the CS and available in local installation
+  """
+  extensions = []
+  for extension in getCSExtensions():
+    try:
+      exec 'import %sDIRAC' % extension
+      extensions.append( '%sDIRAC' % extension )
+    except:
+      pass
+  extensions.append['DIRAC']
+  return exceptions
