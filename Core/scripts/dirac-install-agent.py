03955f426cf9d753c27fcf2534d2ac61f51ec7f9
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-01 18:34:36 +0000

    FIX: Use new getCSExtensions method
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31114 4525493e-7705-40b1-a816-d608a930855b

--- Core/scripts/dirac-install-agent.py
+++ Core/scripts/dirac-install-agent.py
@@ -10,12 +10,13 @@ Do the initial installation and configuration of a DIRAC agent
 __RCSID__ = "$Id$"
 #
 from DIRAC.Core.Utilities import InstallTools
+from DIRAC.ConfigurationSystem.Client.Helpers import getCSExtensions
 #
 from DIRAC import gConfig
 InstallTools.exitOnError = True
 #
 from DIRAC.Core.Base import Script
-Script.setUsageMessage('\n'.join( [ __doc__.split( '\n' )[1],
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                     'Usage:',
                                     '  %s [option|cfgfile] ... System Agent|System/Agent' % Script.scriptName,
                                     'Arguments:',
@@ -24,16 +25,15 @@ Script.setUsageMessage('\n'.join( [ __doc__.split( '\n' )[1],
 Script.parseCommandLine()
 args = Script.getPositionalArgs()
 if len( args ) == 1:
-  args = args[0].split('/')
+  args = args[0].split( '/' )
 
 if len( args ) != 2:
-  Script.showHelp( )
+  Script.showHelp()
   exit( -1 )
 #
 system = args[0]
 agent = args[1]
-extensions = gConfig.getValue( '/DIRAC/Extensions', [] )
 
-InstallTools.addDefaultOptionsToCS( gConfig, 'agent', system, agent, extensions, True )
+InstallTools.addDefaultOptionsToCS( gConfig, 'agent', system, agent, getCSExtensions(), True )
 
-InstallTools.installComponent( 'agent', system, agent, extensions )
+InstallTools.installComponent( 'agent', system, agent, getCSExtensions() )
