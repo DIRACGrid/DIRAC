c6ede79fb040cacb57bf55f97b259927e3788236
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-01 18:35:51 +0000

    FIX: Use new getCSExtensions method
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31116 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/Version.py
+++ Core/Utilities/Version.py
@@ -2,47 +2,45 @@
 
 __RCSID__ = "$Id: $"
 
-from DIRAC import gConfig, S_OK, S_ERROR
+from DIRAC import S_OK, S_ERROR
+from DIRAC.ConfigurationSystem.Client.Helpers import getCSExtensions
 
 def getCurrentVersion():
   """ Get a string corresponding to the current version of the DIRAC package and all the installed
       extension packages
   """
-  
+
   import DIRAC
-  version = 'DIRAC '+DIRAC.version
-  
-  extensions = gConfig.getOption('/DIRAC/Extensions',[])
-  for e in extensions['Value']:
-    try: 
+  version = 'DIRAC ' + DIRAC.version
+
+  for e in getCSExtensions():
+    try:
       exec "import %sDIRAC" % e
-      version = "%sDIRAC " % e + eval('%sDIRAC.version' % e) +'; '+ version
+      version = "%sDIRAC " % e + eval( '%sDIRAC.version' % e ) + '; ' + version
     except ImportError:
       pass
     except AttributeError:
       pass
-    
-  return S_OK(version)  
+
+  return S_OK( version )
 
 def getVersion():
   """ Get a dictionary corresponding to the current version of the DIRAC package and all the installed
       extension packages
   """
-  
+
   import DIRAC
   vDict = {'Extensions':{}}
   vDict['DIRAC'] = DIRAC.version
-  
-  extensions = gConfig.getOption('/DIRAC/Extensions',[])
-  for e in extensions['Value']:
-    try: 
+
+  for e in getCSExtensions():
+    try:
       exec "import %sDIRAC" % e
-      version = eval('%sDIRAC.version' % e)
+      version = eval( '%sDIRAC.version' % e )
       vDict['Extensions'][e] = version
     except ImportError:
       pass
     except AttributeError:
       pass
-    
-  return S_OK(vDict)  
-  
+
+  return S_OK( vDict )
