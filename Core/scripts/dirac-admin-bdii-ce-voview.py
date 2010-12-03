d6673321345257b4c3d2a4efd099bfcbc1376550
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-03 18:18:06 +0000

    NEW: use getVO
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31169 4525493e-7705-40b1-a816-d608a930855b

--- Core/scripts/dirac-admin-bdii-ce-voview.py
+++ Core/scripts/dirac-admin-bdii-ce-voview.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 ########################################################################
 # $HeadURL$
-# File :   dirac-admin-bdii-ce-voview
-# Author : Adria Casajus
+# File :    dirac-admin-bdii-ce-voview
+# Author :  Adria Casajus
 ########################################################################
 """
   Check info on BDII for VO view of CE
@@ -13,28 +13,29 @@ from DIRAC.Core.Base                                         import Script
 
 Script.registerSwitch( "H:", "host=", "BDII host" )
 Script.registerSwitch( "V:", "vo=", "vo" )
-Script.setUsageMessage('\n'.join( [ __doc__.split( '\n' )[1],
-                                    'Usage:',
-                                    '  %s [option|cfgfile] ... CE' % Script.scriptName,
-                                    'Arguments:',
-                                    '  CE: Name of the CE(ie: ce111.cern.ch)'] ) )
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                     'Usage:',
+                                     '  %s [option|cfgfile] ... CE' % Script.scriptName,
+                                     'Arguments:',
+                                     '  CE: Name of the CE(ie: ce111.cern.ch)'] ) )
 
 Script.parseCommandLine( ignoreErrors = True )
 args = Script.getPositionalArgs()
 
 from DIRAC.Interfaces.API.DiracAdmin                         import DiracAdmin
+from DIRAC.ConfigurationSystem.Client.Helpers                import getVO
 
 def usage():
   Script.showHelp()
-  DIRAC.exit(2)
+  DIRAC.exit( 2 )
 
-if not len(args)==1:
+if not len( args ) == 1:
   usage()
 
 ce = args[0]
 
 host = None
-vo = DIRAC.gConfig.getValue('/DIRAC/VirtualOrganization', 'lhcb')
+vo = getVO( 'lhcb' )
 for unprocSw in Script.getUnprocessedSwitches():
   if unprocSw[0] in ( "H", "host" ):
         host = unprocSw[1]
@@ -43,17 +44,17 @@ for unprocSw in Script.getUnprocessedSwitches():
 
 diracAdmin = DiracAdmin()
 
-result = diracAdmin.getBDIICEVOView(ce, useVO=vo, host=host)
+result = diracAdmin.getBDIICEVOView( ce, useVO = vo, host = host )
 if not ['OK']:
   print test['Message']
-  DIRAC.exit(2)
-  
+  DIRAC.exit( 2 )
+
 
 ces = result['Value']
 for ce in ces:
-  print "CEVOView: %s {"%ce.get('GlueChunkKey','Unknown')
+  print "CEVOView: %s {" % ce.get( 'GlueChunkKey', 'Unknown' )
   for item in ce.iteritems():
-    print "%s: %s"%item
+    print "%s: %s" % item
   print "}"
 
 
