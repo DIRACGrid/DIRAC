d50d40e37a2023f3f9b7f8c21122155c07d8834f
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-03 18:21:48 +0000

    NEW: use getVO
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31172 4525493e-7705-40b1-a816-d608a930855b

--- Core/scripts/dirac-info.py
+++ Core/scripts/dirac-info.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 ########################################################################
 # $HeadURL$
-# File :   dirac-info
-# Author : Andrei Tsaregorodtsev
+# File :    dirac-info
+# Author :  Andrei Tsaregorodtsev
 ########################################################################
 """
   Report info about local DIRAC installation
@@ -13,19 +13,21 @@ from pprint import pprint
 import DIRAC
 from DIRAC import gConfig
 from DIRAC.Core.Base import Script
-Script.setUsageMessage('\n'.join( [ __doc__.split( '\n' )[1],
-                                    'Usage:',
-                                    '  %s [option|cfgfile] ... Site' % Script.scriptName,] ) )
+from DIRAC.ConfigurationSystem.Client.Helpers                import getVO
+
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                     'Usage:',
+                                     '  %s [option|cfgfile] ... Site' % Script.scriptName, ] ) )
 Script.parseCommandLine( ignoreErrors = True )
 args = Script.getPositionalArgs()
 
 infoDict = {}
 
-infoDict['Setup'] = gConfig.getValue('/DIRAC/Setup','Unknown')
-infoDict['ConfigurationServer'] = gConfig.getValue('/DIRAC/Configuration/Servers',[])
-infoDict['VirtualOrganization'] = gConfig.getValue('/DIRAC/VirtualOrganization','Unknown')
+infoDict['Setup'] = gConfig.getValue( '/DIRAC/Setup', 'Unknown' )
+infoDict['ConfigurationServer'] = gConfig.getValue( '/DIRAC/Configuration/Servers', [] )
+infoDict['VirtualOrganization'] = getVO( 'Unknown' )
 
-print 'DIRAC version'.rjust(20),':',DIRAC.version
+print 'DIRAC version'.rjust( 20 ), ':', DIRAC.version
 
-for k,v in infoDict.items():
-  print k.rjust(20),':',str(v)
+for k, v in infoDict.items():
+  print k.rjust( 20 ), ':', str( v )
