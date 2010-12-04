4002f8e2535a56bd2ebcc5239b1eaec624bae4ba
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-04 11:27:15 +0000

    FIX: remove obsolete __VERSION__ = "$Revision:$"
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31292 4525493e-7705-40b1-a816-d608a930855b

--- Interfaces/scripts/dirac-admin-externals-versions.py
+++ Interfaces/scripts/dirac-admin-externals-versions.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 ########################################################################
 # $HeadURL$
-# File :   dirac-admin-externals-versions
-# Author : Stuart Paterson
+# File :    dirac-admin-externals-versions
+# Author :  Stuart Paterson
 ########################################################################
-__RCSID__   = "$Id$"
-__VERSION__ = "$Revision: 1.1 $"
+__RCSID__ = "$Id$"
 import DIRAC
 from DIRAC.Core.Base import Script
 from DIRAC.Interfaces.API.DiracAdmin                         import DiracAdmin
 
 Script.parseCommandLine( ignoreErrors = True )
-args = Script.getPositionalArgs()
 
 diracAdmin = DiracAdmin()
 diracAdmin.getExternalPackageVersions()
-DIRAC.exit(0)
+DIRAC.exit( 0 )
 
