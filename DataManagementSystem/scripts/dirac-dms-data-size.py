e28bf1c86b6e2cc57888c4bb8578094de50ff664
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-04 10:44:47 +0000

    FIX: remove obsolete __VERSION__ = "$Revision:$"
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31261 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/scripts/dirac-dms-data-size.py
+++ DataManagementSystem/scripts/dirac-dms-data-size.py
@@ -2,56 +2,55 @@
 ########################################################################
 # $HeadURL$
 ########################################################################
-__RCSID__   = "$Id$"
-__VERSION__ = "$Revision: 1.1 $"
-import sys,os
+__RCSID__ = "$Id$"
+import sys, os
 import DIRAC
 from DIRAC import gLogger
 from DIRAC.Core.Base import Script
 
 unit = 'GB'
-Script.registerSwitch( "u:", "Unit=","   Unit to use [%s] (MB,GB,TB,PB)" % unit)
+Script.registerSwitch( "u:", "Unit=", "   Unit to use [%s] (MB,GB,TB,PB)" % unit )
 Script.parseCommandLine( ignoreErrors = False )
 for switch in Script.getUnprocessedSwitches():
   if switch[0].lower() == "u" or switch[0].lower() == "unit":
     unit = switch[1]
-scaleDict = { 'MB' : 1000*1000.0,
-              'GB' : 1000*1000*1000.0,
-              'TB' : 1000*1000*1000*1000.0,
-              'PB' : 1000*1000*1000*1000*1000.0}
+scaleDict = { 'MB' : 1000 * 1000.0,
+              'GB' : 1000 * 1000 * 1000.0,
+              'TB' : 1000 * 1000 * 1000 * 1000.0,
+              'PB' : 1000 * 1000 * 1000 * 1000 * 1000.0}
 if not unit in scaleDict.keys():
-  gLogger.error("Unit must be one of MB,GB,TB,PB")
-  DIRAC.exit(2)
+  gLogger.error( "Unit must be one of MB,GB,TB,PB" )
+  DIRAC.exit( 2 )
 scaleFactor = scaleDict[unit]
 
 args = Script.getPositionalArgs()
 lfns = []
 for inputFileName in args:
-  if os.path.exists(inputFileName):
-    inputFile = open(inputFileName,'r')
+  if os.path.exists( inputFileName ):
+    inputFile = open( inputFileName, 'r' )
     string = inputFile.read()
     inputFile.close()
-    lfns.extend(string.splitlines())
+    lfns.extend( string.splitlines() )
   else:
-    lfns.append(inputFileName)
+    lfns.append( inputFileName )
 
 from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
 rm = ReplicaManager()
 
-res = rm.getCatalogFileSize(lfns)
+res = rm.getCatalogFileSize( lfns )
 if not res['OK']:
-  gLogger.error("Failed to get size of data",res['Message'])
-  DIRAC.exit(-2)
-for lfn,reason in res['Value']['Failed'].items():
-  gLogger.error("Failed to get size for %s" % lfn, reason)
+  gLogger.error( "Failed to get size of data", res['Message'] )
+  DIRAC.exit( -2 )
+for lfn, reason in res['Value']['Failed'].items():
+  gLogger.error( "Failed to get size for %s" % lfn, reason )
 totalSize = 0
 totalFiles = 0
-for lfn,size in res['Value']['Successful'].items():
-  totalFiles+=1
-  totalSize+=size
-gLogger.info('-' * 30)
-gLogger.info('%s|%s' % ('Files'.ljust(15),('Size (%s)' % unit).rjust(15))) 
-gLogger.info('-' * 30)
-gLogger.info('%s|%s' % (str(totalFiles).ljust(15),str('%.1f' % (totalSize/scaleFactor)).rjust(15)))
-gLogger.info('-' * 30)
-DIRAC.exit(0)
+for lfn, size in res['Value']['Successful'].items():
+  totalFiles += 1
+  totalSize += size
+gLogger.info( '-' * 30 )
+gLogger.info( '%s|%s' % ( 'Files'.ljust( 15 ), ( 'Size (%s)' % unit ).rjust( 15 ) ) )
+gLogger.info( '-' * 30 )
+gLogger.info( '%s|%s' % ( str( totalFiles ).ljust( 15 ), str( '%.1f' % ( totalSize / scaleFactor ) ).rjust( 15 ) ) )
+gLogger.info( '-' * 30 )
+DIRAC.exit( 0 )
