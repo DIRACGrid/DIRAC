69a6b5ecd088640f43cc6fa624b165619f0b9e30
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-04 10:45:16 +0000

    FIX: remove obsolete __VERSION__ = "$Revision:$"
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31262 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/scripts/dirac-dms-filecatalog-cli.py
+++ DataManagementSystem/scripts/dirac-dms-filecatalog-cli.py
@@ -2,18 +2,17 @@
 ########################################################################
 # $HeadURL$
 ########################################################################
-__RCSID__   = "$Id$"
-__VERSION__ = "$Revision: 1.1 $"
-import sys,os
+__RCSID__ = "$Id$"
+import sys, os
 import DIRAC
 from DIRAC import gLogger, gConfig
 from DIRAC.Core.Base import Script
 
 fcType = ''
 
-Script.registerSwitch( "f:", "file-catalog=","   Catalog client type to use")
+Script.registerSwitch( "f:", "file-catalog=", "   Catalog client type to use" )
 Script.parseCommandLine( ignoreErrors = False )
-res = gConfig.getSections("/Resources/FileCatalogs/")
+res = gConfig.getSections( "/Resources/FileCatalogs/" )
 if res['OK']:
   ##Take catalog -1 as default as it seems that the list returned is reversed compared to CS web interface
   fcType = res['Value'][-1]
@@ -24,30 +23,30 @@ for switch in Script.getUnprocessedSwitches():
 
 from DIRAC.DataManagementSystem.Client.FileCatalogClientCLI import FileCatalogClientCLI
 
-if fcType == "LcgFileCatalog" or fcType =="LFC" :
+if fcType == "LcgFileCatalog" or fcType == "LFC" :
   from DIRAC.Resources.Catalog.LcgFileCatalogClient import LcgFileCatalogClient
-  cli = FileCatalogClientCLI(LcgFileCatalogClient())
+  cli = FileCatalogClientCLI( LcgFileCatalogClient() )
   try:
     host = os.environ['LFC_HOST']
-  except Exception,x:
+  except Exception, x:
     print "LFC_HOST environment variable not defined"
-    sys.exit(1)
+    sys.exit( 1 )
   print "Starting LFC FileCatalog client"
   cli.cmdloop()
 elif fcType == "LcgFileCatalogProxy" or fcType == "LFCproxy":
   from DIRAC.Resources.Catalog.LcgFileCatalogProxyClient import LcgFileCatalogProxyClient
-  cli = FileCatalogClientCLI(LcgFileCatalogProxyClient())
+  cli = FileCatalogClientCLI( LcgFileCatalogProxyClient() )
   print "Starting LFC Proxy FileCatalog client"
-  cli.cmdloop() 
+  cli.cmdloop()
 elif fcType == "LcgFileCatalogCombined" or fcType == "LFCCombined":
   from DIRAC.Resources.Catalog.LcgFileCatalogCombinedClient import LcgFileCatalogCombinedClient
-  cli = FileCatalogClientCLI(LcgFileCatalogCombinedClient())
+  cli = FileCatalogClientCLI( LcgFileCatalogCombinedClient() )
   print "Starting LFC FileCatalog Combined client"
-  cli.cmdloop()  
+  cli.cmdloop()
 elif fcType == "FileCatalog" or fcType == "DiracFC" :
   from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
-  cli = FileCatalogClientCLI(FileCatalogClient())
+  cli = FileCatalogClientCLI( FileCatalogClient() )
   print "Starting DIRAC FileCatalog client"
-  cli.cmdloop()  
+  cli.cmdloop()
 else:
   print "Unknown catalog type", fcType
