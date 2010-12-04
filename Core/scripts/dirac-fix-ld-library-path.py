9b8eea7ba28a2770fdfbf806c3e35df73cf645a1
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-04 10:36:56 +0000

    FIX: remove obsolete __VERSION__ = "$Revision:$"
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31253 4525493e-7705-40b1-a816-d608a930855b

--- Core/scripts/dirac-fix-ld-library-path.py
+++ Core/scripts/dirac-fix-ld-library-path.py
@@ -4,12 +4,10 @@
 # File :   dirac-fix-ld-lib
 # Author : Joel Closier
 ########################################################################
-__RCSID__   = "$Id$"
-__VERSION__ = "$Revision: 1.3 $"
-
+__RCSID__ = "$Id$"
 """  This is a script to fix oversized LD_LIBRARY_PATH variables.
 """
-import sys,os,shutil,string,re
+import sys, os, shutil, string, re
 import DIRAC
 from DIRAC.Core.Base import Script
 from DIRAC.Core.Utilities.Os import uniquePath
@@ -17,7 +15,7 @@ from DIRAC.Core.Utilities.Subprocess import shellCall
 
 DEBUG = 0
 
-def fixLDPath( root, ldpath, directory):
+def fixLDPath( root, ldpath, directory ):
   """
       This is a utility to fix the LD_LIBRARY_PATH on Grid WNs. The
       shared libraries from the original LD_LIBRARY_PATH are linked to
@@ -25,20 +23,20 @@ def fixLDPath( root, ldpath, directory):
       this needs some refurbishment.
   """
 
-  if os.path.exists(directory):
-    shutil.rmtree(directory)
+  if os.path.exists( directory ):
+    shutil.rmtree( directory )
 
   start = os.getcwd()
-  os.mkdir(directory)
-  os.chdir(directory)
-  uniqueLD = uniquePath(ldpath)
+  os.mkdir( directory )
+  os.chdir( directory )
+  uniqueLD = uniquePath( ldpath )
 
   if DEBUG:
     print 'Unique LD LIBRARY PATH is:'
     print uniqueLD
     sys.stdout.flush()
 
-  ldlist = string.split(uniqueLD,':')
+  ldlist = string.split( uniqueLD, ':' )
   if DEBUG:
     print ''
     print 'LD List is:'
@@ -47,20 +45,20 @@ def fixLDPath( root, ldpath, directory):
     sys.stdout.flush()
 
   for path in ldlist:
-    if os.path.exists(path):
+    if os.path.exists( path ):
 
       if DEBUG:
         print 'Searching for shared libraries in:'
         print path
         print '-----------------------------------------------'
-        res = shellCall(0,'ls '+path+'/*.so*')
+        res = shellCall( 0, 'ls ' + path + '/*.so*' )
         if res['OK']:
           print res['Value']
         else:
           print res
         print '-----------------------------------------------'
 
-      output = shellCall(0,'ls '+path+'/*.so*')
+      output = shellCall( 0, 'ls ' + path + '/*.so*' )
       #must be tidied for Windows (same below)
 
       if DEBUG:
@@ -71,18 +69,18 @@ def fixLDPath( root, ldpath, directory):
           print '**************************'
 
       if not output['Value'][0]:
-        ldlibs = output['Value'][1].split('\n')
+        ldlibs = output['Value'][1].split( '\n' )
         for lib in ldlibs:
-          if os.path.exists(lib):
-            filename = os.path.basename(lib)
-            output = shellCall(0,'ln -s '+str(lib)+' '+str(filename))
+          if os.path.exists( lib ):
+            filename = os.path.basename( lib )
+            output = shellCall( 0, 'ln -s ' + str( lib ) + ' ' + str( filename ) )
             #N.B. for Windows this should be a copy...
             if DEBUG:
               if not output['OK']:
                 print '********************************'
                 print 'Warning, problem creating link:'
-                print 'File: ',filename
-                print 'Path: ',lib
+                print 'File: ', filename
+                print 'Path: ', lib
                 print output
                 print '********************************'
 
@@ -90,14 +88,14 @@ def fixLDPath( root, ldpath, directory):
         print 'Searching for rootmap file in:'
         print path
         print '-----------------------------------------------'
-        res = shellCall(0,'ls '+path+'/*rootmap*')
+        res = shellCall( 0, 'ls ' + path + '/*rootmap*' )
         if res['OK']:
           print res['Value']
         else:
           print res
         print '-----------------------------------------------'
 
-      output = shellCall(0,'ls '+path+'/*rootmap*')
+      output = shellCall( 0, 'ls ' + path + '/*rootmap*' )
 
       if DEBUG:
         if not output['OK']:
@@ -107,22 +105,22 @@ def fixLDPath( root, ldpath, directory):
           print '**************************'
 
       if not output['Value'][0]:
-        ldlibs = output['Value'][1].split('\n')
+        ldlibs = output['Value'][1].split( '\n' )
         for lib in ldlibs:
-          if os.path.exists(lib):
-            if re.search('RELAX',lib) is not None:
-              filename = os.path.basename(lib)
-              output = shellCall(0,'ln -s '+str(lib)+' '+str(filename))
+          if os.path.exists( lib ):
+            if re.search( 'RELAX', lib ) is not None:
+              filename = os.path.basename( lib )
+              output = shellCall( 0, 'ln -s ' + str( lib ) + ' ' + str( filename ) )
               if DEBUG:
                 if not output['OK']:
                   print '********************************'
                   print 'Warning, problem creating link:'
-                  print 'File: ',filename
-                  print 'Path: ',lib
+                  print 'File: ', filename
+                  print 'Path: ', lib
                   print output
                   print '********************************'
 
-  os.chdir(start)
+  os.chdir( start )
   sys.stdout.flush()
 
 
@@ -132,4 +130,4 @@ positionalArgs = Script.getPositionalArgs()
 if len( positionalArgs ) != 3:
   DIRAC.abort( 1, "Must specify which is the role you want" )
 
-fixLDPath(*positionalArgs)
+fixLDPath( *positionalArgs )
