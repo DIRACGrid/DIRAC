63efffe2dab1746adac373f8700bcc1cca25aedd
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-04 11:11:30 +0000

    FIX: remove obsolete __VERSION__ = "$Revision:$"
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31271 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/scripts/dirac-dms-user-quota.py
+++ DataManagementSystem/scripts/dirac-dms-user-quota.py
@@ -2,26 +2,25 @@
 ########################################################################
 # $HeadURL$
 ########################################################################
-__RCSID__   = "$Id$"
-__VERSION__ = "$Revision: 1.2 $"
+__RCSID__ = "$Id$"
 import DIRAC
 from DIRAC.Core.Base import Script
 Script.parseCommandLine( ignoreErrors = False )
 from DIRAC import gLogger, gConfig
 from DIRAC.Core.Security.Misc import getProxyInfo
 
-res = getProxyInfo(False,False)
+res = getProxyInfo( False, False )
 if not res['OK']:
-  gLogger.error("Failed to get client proxy information.",res['Message'])
-  DIRAC.exit(2)
+  gLogger.error( "Failed to get client proxy information.", res['Message'] )
+  DIRAC.exit( 2 )
 proxyInfo = res['Value']
 username = proxyInfo['username']
 
 try:
-  quota = gConfig.getValue('/Registry/DefaultStorageQuota', 0. )
-  quota = gConfig.getValue('/Registry/Users/%s/Quota' % username, quota )
-  gLogger.info('Current quota found to be %.1f GB' % quota)
-  DIRAC.exit(0)
-except Exception,x:
-  gLogger.exception("Failed to convert retrieved quota",'',x)
-  DIRAC.exit(-1)
+  quota = gConfig.getValue( '/Registry/DefaultStorageQuota', 0. )
+  quota = gConfig.getValue( '/Registry/Users/%s/Quota' % username, quota )
+  gLogger.info( 'Current quota found to be %.1f GB' % quota )
+  DIRAC.exit( 0 )
+except Exception, x:
+  gLogger.exception( "Failed to convert retrieved quota", '', x )
+  DIRAC.exit( -1 )
