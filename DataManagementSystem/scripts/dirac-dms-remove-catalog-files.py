5587144e8a6f6c94f023c21bde1d34ce01fd3a25
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-04 10:45:50 +0000

    FIX: remove obsolete __VERSION__ = "$Revision:$"
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31263 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/scripts/dirac-dms-remove-catalog-files.py
+++ DataManagementSystem/scripts/dirac-dms-remove-catalog-files.py
@@ -4,33 +4,32 @@ parseCommandLine()
 ########################################################################
 # $Header: /local/reps/dirac/DIRAC3/DIRAC/DataManagementSystem/scripts/dirac-dms-remove-catalog-replicas.py,v 1.2 2009/03/19 17:45:06 acsmith Exp $
 ########################################################################
-__RCSID__   = "$Id: dirac-dms-remove-catalog-replicas.py,v 1.2 2009/03/19 17:45:06 acsmith Exp $"
-__VERSION__ = "$ $"
+__RCSID__ = "$Id: dirac-dms-remove-catalog-replicas.py,v 1.2 2009/03/19 17:45:06 acsmith Exp $"
 
 from DIRAC.Core.Utilities.List import sortList
 from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
 rm = ReplicaManager()
-import os,sys
+import os, sys
 
-if len(sys.argv) < 2:
+if len( sys.argv ) < 2:
   print 'Usage: ./dirac-dms-remove-files.py <LFN | fileContainingLFNs>'
   sys.exit()
 else:
   inputFileName = sys.argv[1]
 
-if os.path.exists(inputFileName):
-  inputFile = open(inputFileName,'r')
+if os.path.exists( inputFileName ):
+  inputFile = open( inputFileName, 'r' )
   string = inputFile.read()
   lfns = string.splitlines()
   inputFile.close()
 else:
   lfns = [inputFileName]
 
-res = rm.removeCatalogFile(lfns)
+res = rm.removeCatalogFile( lfns )
 if not res['OK']:
   print res['Message']
   sys.exit()
-for lfn in sortList(res['Value']['Failed'].keys()):
+for lfn in sortList( res['Value']['Failed'].keys() ):
   message = res['Value']['Failed'][lfn]
-  print 'Failed to remove %s: %s' % (lfn,message)
-print 'Successfully removed %d catalog files.' % (len(res['Value']['Successful']))
+  print 'Failed to remove %s: %s' % ( lfn, message )
+print 'Successfully removed %d catalog files.' % ( len( res['Value']['Successful'] ) )
