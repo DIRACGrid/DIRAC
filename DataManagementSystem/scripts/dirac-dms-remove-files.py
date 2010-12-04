a2e8aa1b286bd4b29001499a066017753943653e
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-04 11:06:22 +0000

    FIX: remove obsolete __VERSION__ = "$Revision:$"
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31265 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/scripts/dirac-dms-remove-files.py
+++ DataManagementSystem/scripts/dirac-dms-remove-files.py
@@ -2,9 +2,8 @@
 ########################################################################
 # $HeadURL$
 ########################################################################
-__RCSID__   = "$Id$"
-__VERSION__ = "$Revision: 1.4 $"
-import sys,os
+__RCSID__ = "$Id$"
+import sys, os
 import DIRAC
 from DIRAC import gLogger
 from DIRAC.Core.Base import Script
@@ -12,32 +11,32 @@ Script.parseCommandLine()
 args = Script.getPositionalArgs()
 lfns = []
 for inputFileName in args:
-  if os.path.exists(inputFileName):
-    inputFile = open(inputFileName,'r')
+  if os.path.exists( inputFileName ):
+    inputFile = open( inputFileName, 'r' )
     string = inputFile.read()
     inputFile.close()
-    lfns.extend(string.splitlines())
+    lfns.extend( string.splitlines() )
   else:
-    lfns.append(inputFileName)
+    lfns.append( inputFileName )
 
-from DIRAC.Core.Utilities.List import sortList,breakListIntoChunks
+from DIRAC.Core.Utilities.List import sortList, breakListIntoChunks
 from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
 rm = ReplicaManager()
 
 errorReasons = {}
 successfullyRemoved = 0
-for lfnList in breakListIntoChunks(lfns,100):
-  res = rm.removeFile(lfnList)
+for lfnList in breakListIntoChunks( lfns, 100 ):
+  res = rm.removeFile( lfnList )
   if not res['OK']:
-    gLogger.error("Failed to remove data",res['Message'])
-    DIRAC.exit(-2)
-  for lfn,reason in res['Value']['Failed'].items():
+    gLogger.error( "Failed to remove data", res['Message'] )
+    DIRAC.exit( -2 )
+  for lfn, reason in res['Value']['Failed'].items():
     if not reason in errorReasons.keys():
       errorReasons[reason] = []
-    errorReasons[reason].append(lfn)
-  successfullyRemoved+=len(res['Value']['Successful'].keys())
+    errorReasons[reason].append( lfn )
+  successfullyRemoved += len( res['Value']['Successful'].keys() )
 
-for reason,lfns in errorReasons.items():
-  gLogger.info("Failed to remove %d files with error: %s" % (len(lfns),reason))
-gLogger.info("Successfully removed %d files" % successfullyRemoved)
-DIRAC.exit(0)
+for reason, lfns in errorReasons.items():
+  gLogger.info( "Failed to remove %d files with error: %s" % ( len( lfns ), reason ) )
+gLogger.info( "Successfully removed %d files" % successfullyRemoved )
+DIRAC.exit( 0 )
