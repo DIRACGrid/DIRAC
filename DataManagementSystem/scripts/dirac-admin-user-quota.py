a406ee750c436e4bbdef839153d5f133ad6f70d7
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-04 10:42:34 +0000

    FIX: remove obsolete __VERSION__ = "$Revision:$"
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31259 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/scripts/dirac-admin-user-quota.py
+++ DataManagementSystem/scripts/dirac-admin-user-quota.py
@@ -2,17 +2,16 @@
 ########################################################################
 # $HeadURL$
 ########################################################################
-__RCSID__   = "$Id$"
-__VERSION__ = "$Revision: 1.2 $"
+__RCSID__ = "$Id$"
 import DIRAC
 from DIRAC.Core.Base import Script
 
-Script.setUsageMessage("""
+Script.setUsageMessage( """
 Show storage quotas for specified users or for all registered users if nobody is specified
 
 Usage:
    %s [user1 ...]
-""" % Script.scriptName)
+""" % Script.scriptName )
 
 Script.parseCommandLine()
 users = Script.getPositionalArgs()
@@ -22,19 +21,19 @@ from DIRAC.Core.Utilities.List import sortList
 from DIRAC.Core.Security.Misc import getProxyInfo
 
 if not users:
-  res = gConfig.getSections('/Registry/Users')
+  res = gConfig.getSections( '/Registry/Users' )
   if not res['OK']:
-    gLogger.error("Failed to retrieve user list from CS",res['Message'])
-    DIRAC.exit(2)
+    gLogger.error( "Failed to retrieve user list from CS", res['Message'] )
+    DIRAC.exit( 2 )
   users = res['Value']
 
-gLogger.notice("-"*30)
-gLogger.notice("%s|%s" % ('Username'.ljust(15),'Quota (GB)'.rjust(15)))
-gLogger.notice("-"*30)
-for user in sortList(users):
-  quota = gConfig.getValue('/Registry/Users/%s/Quota' % user,0)
+gLogger.notice( "-"*30 )
+gLogger.notice( "%s|%s" % ( 'Username'.ljust( 15 ), 'Quota (GB)'.rjust( 15 ) ) )
+gLogger.notice( "-"*30 )
+for user in sortList( users ):
+  quota = gConfig.getValue( '/Registry/Users/%s/Quota' % user, 0 )
   if not quota:
-    quota = gConfig.getValue('/Registry/DefaultStorageQuota')
-  gLogger.notice("%s|%s" % (user.ljust(15),str(quota).rjust(15)))
-gLogger.notice("-"*30)
-DIRAC.exit(0)
+    quota = gConfig.getValue( '/Registry/DefaultStorageQuota' )
+  gLogger.notice( "%s|%s" % ( user.ljust( 15 ), str( quota ).rjust( 15 ) ) )
+gLogger.notice( "-"*30 )
+DIRAC.exit( 0 )
