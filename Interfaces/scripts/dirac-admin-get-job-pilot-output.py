b88356c8571ab409f104405a01e9407cfe8f8ed6
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-04 11:27:49 +0000

    FIX: remove obsolete __VERSION__ = "$Revision:$"
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31293 4525493e-7705-40b1-a816-d608a930855b

--- Interfaces/scripts/dirac-admin-get-job-pilot-output.py
+++ Interfaces/scripts/dirac-admin-get-job-pilot-output.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 ########################################################################
 # $HeadURL$
-# File :   dirac-admin-get-job-pilot-output
-# Author : Stuart Paterson
+# File :    dirac-admin-get-job-pilot-output
+# Author :  Stuart Paterson
 ########################################################################
-__RCSID__   = "$Id$"
-__VERSION__ = "$Revision: 1.1 $"
+__RCSID__ = "$Id$"
 import DIRAC
 from DIRAC.Core.Base import Script
 from DIRAC.Interfaces.API.DiracAdmin                         import DiracAdmin
@@ -14,12 +13,12 @@ Script.parseCommandLine( ignoreErrors = True )
 args = Script.getPositionalArgs()
 
 def usage():
-  print 'Usage: %s <JobID> [<JobID>]' %(Script.scriptName)
-  DIRAC.exit(2)
+  print 'Usage: %s <JobID> [<JobID>]' % ( Script.scriptName )
+  DIRAC.exit( 2 )
 
-if len(args) < 1:
+if len( args ) < 1:
   usage()
-  
+
 diracAdmin = DiracAdmin()
 exitCode = 0
 errorList = []
@@ -27,18 +26,18 @@ errorList = []
 for job in args:
 
   try:
-    job = int(job)
-  except Exception,x:
-    errorList.append( ('Expected integer for JobID', job ) )
+    job = int( job )
+  except Exception, x:
+    errorList.append( ( 'Expected integer for JobID', job ) )
     exitCode = 2
     continue
 
-  result = diracAdmin.getJobPilotOutput(job)
+  result = diracAdmin.getJobPilotOutput( job )
   if not result['OK']:
-    errorList.append( (job, result['Message']) )
+    errorList.append( ( job, result['Message'] ) )
     exitCode = 2
 
 for error in errorList:
   print "ERROR %s: %s" % error
 
-DIRAC.exit(exitCode)
+DIRAC.exit( exitCode )
