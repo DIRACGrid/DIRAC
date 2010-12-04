25636063da6c13d2381e183efd59ea9a1a04e30e
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-04 10:41:08 +0000

    FIX: remove obsolete __VERSION__ = "$Revision:$"
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31256 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/scripts/dirac-admin-ban-catalog.py
+++ DataManagementSystem/scripts/dirac-admin-ban-catalog.py
@@ -2,32 +2,31 @@
 ########################################################################
 # $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC/DataManagementSystem/scripts/dirac-admin-ban-catalog.py $
 ########################################################################
-__RCSID__   = "$Id: dirac-admin-ban-catalog.py 18161 2009-11-11 12:07:09Z acsmith $"
-__VERSION__ = "$Revision: 1.3 $"
+__RCSID__ = "$Id: dirac-admin-ban-catalog.py 18161 2009-11-11 12:07:09Z acsmith $"
 import DIRAC
 from DIRAC.Core.Base                                   import Script
 
-Script.setUsageMessage("""
+Script.setUsageMessage( """
 Ban the File Catalog mirrors at one or more sites
 
 Usage:
    %s site1 [site2 ...]
-""" % Script.scriptName)
+""" % Script.scriptName )
 
-Script.parseCommandLine(ignoreErrors = True)
+Script.parseCommandLine( ignoreErrors = True )
 
 sites = Script.getPositionalArgs()
 
 from DIRAC.ConfigurationSystem.Client.CSAPI           import CSAPI
 from DIRAC.FrameworkSystem.Client.NotificationClient  import NotificationClient
 from DIRAC.Core.Security.Misc                         import getProxyInfo
-from DIRAC                                            import gConfig,gLogger
+from DIRAC                                            import gConfig, gLogger
 csAPI = CSAPI()
 
 res = getProxyInfo()
 if not res['OK']:
-  gLogger.error("Failed to get proxy information",res['Message'])
-  DIRAC.exit(2)
+  gLogger.error( "Failed to get proxy information", res['Message'] )
+  DIRAC.exit( 2 )
 userName = res['Value']['username']
 group = res['Value']['group']
 
@@ -38,31 +37,31 @@ if not sites:
 catalogCFGBase = "/Resources/FileCatalogs/LcgFileCatalogCombined"
 banned = []
 for site in sites:
-  res = gConfig.getOptionsDict('%s/%s' % (catalogCFGBase,site))
+  res = gConfig.getOptionsDict( '%s/%s' % ( catalogCFGBase, site ) )
   if not res['OK']:
-    gLogger.error("The provided site (%s) does not have an associated catalog." % site)
+    gLogger.error( "The provided site (%s) does not have an associated catalog." % site )
     continue
-  
-  res = csAPI.setOption("%s/%s/Status" % (storageCFGBase,site),"InActive")
+
+  res = csAPI.setOption( "%s/%s/Status" % ( storageCFGBase, site ), "InActive" )
   if not res['OK']:
-    gLogger.error("Failed to update %s catalog status to InActive" % site)
+    gLogger.error( "Failed to update %s catalog status to InActive" % site )
   else:
-    gLogger.debug("Successfully updated %s catalog status to InActive" % site)
-    banned.append(site)
+    gLogger.debug( "Successfully updated %s catalog status to InActive" % site )
+    banned.append( site )
 
 if not banned:
-  gLogger.error("Failed to ban any catalog mirrors")
-  DIRAC.exit(-1)
+  gLogger.error( "Failed to ban any catalog mirrors" )
+  DIRAC.exit( -1 )
 
 res = csAPI.commitChanges()
 if not res['OK']:
-  gLogger.error("Failed to commit changes to CS",res['Message'])
-  DIRAC.exit(-1)
+  gLogger.error( "Failed to commit changes to CS", res['Message'] )
+  DIRAC.exit( -1 )
 
-subject = '%d catalog instance(s) banned for use' % len(banned)
-address = gConfig.getValue('/Operations/EMail/Production','lhcb-grid@cern.ch')
+subject = '%d catalog instance(s) banned for use' % len( banned )
+address = gConfig.getValue( '/Operations/EMail/Production', 'lhcb-grid@cern.ch' )
 body = 'The catalog mirrors at the following sites were banned'
 for site in banned:
-  body = "%s\n%s" % (body,site)
-NotificationClient().sendMail(address,subject,body,'%s@cern.ch' % userName)
-DIRAC.exit(0)
+  body = "%s\n%s" % ( body, site )
+NotificationClient().sendMail( address, subject, body, '%s@cern.ch' % userName )
+DIRAC.exit( 0 )
