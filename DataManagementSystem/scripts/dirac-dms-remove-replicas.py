1293a67d5453c19a9598684ad9dc259c38363491
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-04 11:07:45 +0000

    FIX: remove obsolete __VERSION__ = "$Revision:$"
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31266 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/scripts/dirac-dms-remove-replicas.py
+++ DataManagementSystem/scripts/dirac-dms-remove-replicas.py
@@ -4,36 +4,35 @@ parseCommandLine()
 ########################################################################
 # $HeadURL$
 ########################################################################
-__RCSID__   = "$Id$"
-__VERSION__ = "$ $"
+__RCSID__ = "$Id$"
 
-from DIRAC.Core.Utilities.List                        import sortList,breakListIntoChunks
+from DIRAC.Core.Utilities.List                        import sortList, breakListIntoChunks
 from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
 rm = ReplicaManager()
-import os,sys
+import os, sys
 
-if len(sys.argv) < 3:
+if len( sys.argv ) < 3:
   print 'Usage: ./dirac-dms-remove-replicas.py <LFN | fileContainingLFNs> SE [SE] ...'
   sys.exit()
 else:
   inputFileName = sys.argv[1]
   storageElementNames = sys.argv[2:]
 
-if os.path.exists(inputFileName):
-  inputFile = open(inputFileName,'r')
+if os.path.exists( inputFileName ):
+  inputFile = open( inputFileName, 'r' )
   string = inputFile.read()
   lfns = string.splitlines()
   inputFile.close()
 else:
   lfns = [inputFileName]
 
-for lfnList in breakListIntoChunks(sortList(lfns,True),500):
+for lfnList in breakListIntoChunks( sortList( lfns, True ), 500 ):
   for storageElementName in storageElementNames:
-    res = rm.removeReplica(storageElementName,lfnList)
+    res = rm.removeReplica( storageElementName, lfnList )
     if not res['OK']:
       print res['Message']
-    for lfn in sortList(res['Value']['Successful'].keys()):
-      print 'Successfully removed %s replica of %s' % (storageElementName,lfn)
-    for lfn in sortList(res['Value']['Failed'].keys()):
+    for lfn in sortList( res['Value']['Successful'].keys() ):
+      print 'Successfully removed %s replica of %s' % ( storageElementName, lfn )
+    for lfn in sortList( res['Value']['Failed'].keys() ):
       message = res['Value']['Failed'][lfn]
-      print 'Failed to remove %s replica of %s: %s' % (storageElementName,lfn,message)
+      print 'Failed to remove %s replica of %s: %s' % ( storageElementName, lfn, message )
