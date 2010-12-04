a4a37263da228fb19a10a4e6728853b1de5e0f11
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-04 10:44:14 +0000

    FIX: remove obsolete __VERSION__ = "$Revision:$"
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31260 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/scripts/dirac-dms-check-file-integrity.py
+++ DataManagementSystem/scripts/dirac-dms-check-file-integrity.py
@@ -4,21 +4,20 @@ parseCommandLine()
 ########################################################################
 # $HeadURL$
 ########################################################################
-__RCSID__   = "$Id$"
-__VERSION__ = "$Revision: 1.7 $"
+__RCSID__ = "$Id$"
 
 from DIRAC import gLogger
 from DIRAC.DataManagementSystem.Client.DataIntegrityClient import DataIntegrityClient
-import sys,os
+import sys, os
 
-if len(sys.argv) < 2:
+if len( sys.argv ) < 2:
   print 'Usage: dirac-dms-check-file-integrity <lfn | fileContainingLfns>'
   sys.exit()
 else:
   inputFileName = sys.argv[1]
 
-if os.path.exists(inputFileName):
-  inputFile = open(inputFileName,'r')
+if os.path.exists( inputFileName ):
+  inputFile = open( inputFileName, 'r' )
   string = inputFile.read()
   lfns = string.splitlines()
   inputFile.close()
@@ -26,13 +25,13 @@ else:
   lfns = [inputFileName]
 
 integrityClient = DataIntegrityClient()
-res = integrityClient.catalogFileToBK(lfns)
+res = integrityClient.catalogFileToBK( lfns )
 if not res['OK']:
-  gLogger.error(res['Message'])
+  gLogger.error( res['Message'] )
   sys.exit()
 replicas = res['Value']['CatalogReplicas']
 metadata = res['Value']['CatalogMetadata']
-res = integrityClient.checkPhysicalFiles(replicas,metadata)
+res = integrityClient.checkPhysicalFiles( replicas, metadata )
 if not res['OK']:
-  gLogger.error(res['Message'])
+  gLogger.error( res['Message'] )
   sys.exit()
