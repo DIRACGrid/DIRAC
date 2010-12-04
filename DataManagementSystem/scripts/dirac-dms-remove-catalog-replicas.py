9ef4d239cd812bc8e8411a9455193592edfd71ab
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-04 11:05:51 +0000

    FIX: remove obsolete __VERSION__ = "$Revision:$"
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31264 4525493e-7705-40b1-a816-d608a930855b

--- DataManagementSystem/scripts/dirac-dms-remove-catalog-replicas.py
+++ DataManagementSystem/scripts/dirac-dms-remove-catalog-replicas.py
@@ -4,34 +4,33 @@ parseCommandLine()
 ########################################################################
 # $Header: /local/reps/dirac/DIRAC3/DIRAC/DataManagementSystem/scripts/dirac-dms-remove-catalog-replicas.py,v 1.2 2009/03/19 17:45:06 acsmith Exp $
 ########################################################################
-__RCSID__   = "$Id$"
-__VERSION__ = "$ $"
+__RCSID__ = "$Id$"
 
 from DIRAC.Core.Utilities.List import sortList
 from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
 rm = ReplicaManager()
-import os,sys
+import os, sys
 
-if len(sys.argv) < 3:
+if len( sys.argv ) < 3:
   print 'Usage: ./dirac-dms-remove-replicas.py <LFN | fileContainingLFNs> SE'
   sys.exit()
 else:
   inputFileName = sys.argv[1]
   storageElementName = sys.argv[2]
 
-if os.path.exists(inputFileName):
-  inputFile = open(inputFileName,'r')
+if os.path.exists( inputFileName ):
+  inputFile = open( inputFileName, 'r' )
   string = inputFile.read()
   lfns = string.splitlines()
   inputFile.close()
 else:
   lfns = [inputFileName]
 
-res = rm.removeReplicaFromCatalog(storageElementName,lfns)
+res = rm.removeReplicaFromCatalog( storageElementName, lfns )
 if not res['OK']:
   print res['Message']
   sys.exit()
-for lfn in sortList(res['Value']['Failed'].keys()):
+for lfn in sortList( res['Value']['Failed'].keys() ):
   message = res['Value']['Failed'][lfn]
-  print 'Failed to remove %s replica of %s: %s' % (storageElementName,lfn,message)
-print 'Successfully remove %d catalog replicas at %s' % (len(res['Value']['Successful']),storageElementName)
+  print 'Failed to remove %s replica of %s: %s' % ( storageElementName, lfn, message )
+print 'Successfully remove %d catalog replicas at %s' % ( len( res['Value']['Successful'] ), storageElementName )
