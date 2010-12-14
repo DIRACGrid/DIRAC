95587f32b6c9079e079eb178fba5aa5c68f7e14e
Adria Casajus <adria@ecm.ub.es>
2010-12-14 16:11:03 +0000

    GetFunctions -> CSGlobals
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@32030 4525493e-7705-40b1-a816-d608a930855b

--- ConfigurationSystem/Client/Helpers/__init__.py
+++ ConfigurationSystem/Client/Helpers/__init__.py
@@ -4,4 +4,4 @@ __RCSID__ = "$Id: __init__.py 18161 2009-11-11 12:07:09Z acasajus $"
    DIRAC.ConfigurationSystem.Client.Helpers package  
 """
 from DIRAC.ConfigurationSystem.Client.Helpers.Path import *
-from DIRAC.ConfigurationSystem.Client.Helpers.GetFunctions import getCSExtensions, getInstalledExtensions, getVO
+from DIRAC.ConfigurationSystem.Client.Helpers.CSGlobals import getCSExtensions, getInstalledExtensions, getVO
