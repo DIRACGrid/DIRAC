55b825524fcf1ed7b8ea1146ee4b7bbc03850fcf
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-14 18:54:06 +0000

    FIX: remove obsolete usage function
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@32040 4525493e-7705-40b1-a816-d608a930855b

--- Interfaces/scripts/dirac-framework-ping-service.py
+++ Interfaces/scripts/dirac-framework-ping-service.py
@@ -4,21 +4,28 @@
 # File :    dirac-framework-ping-service
 # Author :  Stuart Paterson
 ########################################################################
+"""
+  Ping the given DIRAC Service
+"""
 __RCSID__ = "$Id$"
 import DIRAC
 from DIRAC.Core.Base import Script
-from DIRAC.Interfaces.API.Dirac                        import Dirac
 
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                    'Usage:',
+                                    '  %s [option|cfgfile] ... System Service|System/Agent' % Script.scriptName,
+                                    'Arguments:',
+                                    '  System:   Name of the DIRAC system (ie: WorkloadManagement)',
+                                    '  Service:  Name of the DIRAC service (ie: Matcher)'] ) )
 Script.parseCommandLine( ignoreErrors = True )
 args = Script.getPositionalArgs()
-
-def usage():
-  print 'Usage: %s <DIRAC System Name> <DIRAC Service Name>' % ( Script.scriptName )
-  DIRAC.exit( 2 )
+if len( args ) == 1:
+  args = args[0].split( '/' )
 
 if len( args ) < 2:
-  usage()
+  Script.showHelp()
 
+from DIRAC.Interfaces.API.Dirac                        import Dirac
 dirac = Dirac()
 exitCode = 0
 
@@ -27,11 +34,10 @@ service = args[1]
 result = dirac.ping( system, service, printOutput = True )
 
 if not result:
-  exitCode = 2
   print 'ERROR: Null result from ping()'
-  DIRAC.exit( exitCode )
-
-if not result['OK']:
+  exitCode = 2
+elif not result['OK']:
+  print 'ERROR: ', result['Message']
   exitCode = 2
 
 DIRAC.exit( exitCode )
