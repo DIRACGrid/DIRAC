e694d02ce84bc3ed9aa7dfa73c9d6d44c90133a4
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-14 13:32:44 +0000

    FIX: remove obsolete usage function
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@32023 4525493e-7705-40b1-a816-d608a930855b

--- Interfaces/scripts/dirac-dms-add-file.py
+++ Interfaces/scripts/dirac-dms-add-file.py
@@ -4,31 +4,34 @@
 # File :    dirac-dms-add-file
 # Author :  Stuart Paterson
 ########################################################################
+"""
+  Upload a file to the grid storage and register it in the File Catalog
+"""
 __RCSID__ = "$Id$"
 
 from DIRAC.Core.Base import Script
 
-Script.setUsageMessage( """
-Upload a file to the grid storage and register it in the File Catalog
-
-Usage:
-   %s <LFN> <FILE PATH> <DIRAC SE> [<GUID>]
-""" % Script.scriptName )
-
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                     'Usage:',
+                                     '  %s [option|cfgfile] ... LFN Path SE [GUID]' % Script.scriptName,
+                                     'Arguments:',
+                                     '  LFN:      Logical File Name',
+                                     '  Path:     Local path to the file',
+                                     '  SE:       DIRAC Storage Element',
+                                     '  GUID:     GUID to use in the registration (optional)' ] ) )
 Script.parseCommandLine( ignoreErrors = True )
 args = Script.getPositionalArgs()
 
 import DIRAC
-from DIRAC.Interfaces.API.Dirac import Dirac    
 
 if len( args ) < 3 or len( args ) > 4:
   Script.showHelp()
-  DIRAC.exit( 2 )
 
 guid = None
 if len( args ) > 3:
   guid = args[3]
 
+from DIRAC.Interfaces.API.Dirac import Dirac
 dirac = Dirac()
 exitCode = 0
 result = dirac.addFile( args[0], args[1], args[2], guid, printOutput = True )
