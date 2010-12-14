1fbae0a79aed5309f2c5d494eddd83121d844a8b
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-14 13:19:05 +0000

    FIX: DiracAdmin does a parsing of the CommandLine, thus has to imported afterwards
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@32016 4525493e-7705-40b1-a816-d608a930855b

--- Interfaces/scripts/dirac-admin-reset-job.py
+++ Interfaces/scripts/dirac-admin-reset-job.py
@@ -10,7 +10,6 @@
 __RCSID__ = "$Id$"
 import DIRAC
 from DIRAC.Core.Base import Script
-from DIRAC.Interfaces.API.DiracAdmin                         import DiracAdmin
 
 Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                      'Usage:',
@@ -24,6 +23,7 @@ args = Script.getPositionalArgs()
 if len( args ) < 1:
   Script.showHelp()
 
+from DIRAC.Interfaces.API.DiracAdmin                         import DiracAdmin
 diracAdmin = DiracAdmin()
 exitCode = 0
 errorList = []
