2d11ad2b1aaf343c6b87f63510145a2f4ee431c3
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-14 12:51:19 +0000

    FIX: remove obsolete usage function
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@31991 4525493e-7705-40b1-a816-d608a930855b

--- Interfaces/scripts/dirac-admin-get-job-pilot-output.py
+++ Interfaces/scripts/dirac-admin-get-job-pilot-output.py
@@ -4,20 +4,24 @@
 # File :    dirac-admin-get-job-pilot-output
 # Author :  Stuart Paterson
 ########################################################################
+"""
+  Retrieve the output of the pilot that executed a given job
+"""
 __RCSID__ = "$Id$"
 import DIRAC
 from DIRAC.Core.Base import Script
 from DIRAC.Interfaces.API.DiracAdmin                         import DiracAdmin
 
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                     'Usage:',
+                                     '  %s [option|cfgfile] ... JobID ...' % Script.scriptName,
+                                     'Arguments:',
+                                     '  JobID:    DIRAC ID of the Job' ] ) )
 Script.parseCommandLine( ignoreErrors = True )
 args = Script.getPositionalArgs()
 
-def usage():
-  print 'Usage: %s <JobID> [<JobID>]' % ( Script.scriptName )
-  DIRAC.exit( 2 )
-
 if len( args ) < 1:
-  usage()
+  Script.showHelp()
 
 diracAdmin = DiracAdmin()
 exitCode = 0
