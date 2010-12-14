8cece5cd8d8d645b044e7984241b0627787b8677
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-14 13:27:44 +0000

    FIX: remove obsolete usage function
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@32022 4525493e-7705-40b1-a816-d608a930855b

--- Interfaces/scripts/dirac-admin-upload-proxy.py
+++ Interfaces/scripts/dirac-admin-upload-proxy.py
@@ -4,29 +4,29 @@
 # File :    dirac-admin-upload-proxy
 # Author :  Stuart Paterson
 ########################################################################
+"""
+  Upload a proxy to the Proxy Manager using delegation
+"""
 __RCSID__ = "$Id$"
 import DIRAC
 from DIRAC.Core.Base import Script
-from DIRAC.Interfaces.API.DiracAdmin                         import DiracAdmin
+
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                     'Usage:',
+                                     '  %s [option|cfgfile] ... Group' % Script.scriptName,
+                                     'Arguments:',
+                                     '  Group:    Group name in the uploaded proxy' ] ) )
 
 Script.parseCommandLine( ignoreErrors = True )
 args = Script.getPositionalArgs()
 
-def usage():
-  print 'Usage: %s <DIRAC group>' % ( Script.scriptName )
-  DIRAC.exit( 2 )
-
 if len( args ) != 1:
-  usage()
+  Script.showHelp()
 
+from DIRAC.Interfaces.API.DiracAdmin                         import DiracAdmin
 diracAdmin = DiracAdmin()
 
-try:
-  group = str( args[0] )
-except Exception, x:
-  print 'Expected string for DIRAC proxy group', args
-  DIRAC.exit( 2 )
-
+group = args[0]
 result = diracAdmin.uploadProxy( group )
 if result['OK']:
   print 'Proxy uploaded for group %s' % ( group )
