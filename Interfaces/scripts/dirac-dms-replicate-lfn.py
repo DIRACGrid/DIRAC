b9f96435258060e88c9ef35602a74c9e16f7165b
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-14 18:49:06 +0000

    FIX: remove obsolete usage function
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@32039 4525493e-7705-40b1-a816-d608a930855b

--- Interfaces/scripts/dirac-dms-replicate-lfn.py
+++ Interfaces/scripts/dirac-dms-replicate-lfn.py
@@ -4,32 +4,42 @@
 # File :    dirac-dms-replicate-lfn
 # Author  : Stuart Paterson
 ########################################################################
+"""
+  Replicate an existing LFN to another Storage Element
+"""
 __RCSID__ = "$Id$"
 import DIRAC
 from DIRAC.Core.Base import Script
-from DIRAC.Interfaces.API.Dirac                       import Dirac
 
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                     'Usage:',
+                                     '  %s [option|cfgfile] ... LFN Source [Dest [Cache]]' % Script.scriptName,
+                                     'Arguments:',
+                                     '  LFN:      Physical File Name',
+                                     '  Source:   Valid DIRAC SE',
+                                     '  Dest:     Valid DIRAC SE',
+                                     '  Cache:    Local directory to be used as cache' ] ) )
 Script.parseCommandLine( ignoreErrors = True )
 args = Script.getPositionalArgs()
 
-def usage():
-  print 'Usage: %s <LFN> <DESTINATION SE> [<SOURCE SE> <LOCAL CACHE>]' % ( Script.scriptName )
-  DIRAC.exit( 2 )
+if len( args ) < 2 or len( args ) > 4:
+  Script.showHelp()
 
-if len( args ) < 2 or len( args ) > 4 or len( args ) == 3:
-  usage()
-
+lfn = args[0]
+seName = args[1]
 sourceSE = ''
 localCache = ''
 if len( args ) > 2:
   sourceSE = args[2]
+if len( args ) == 4:
   localCache = args[3]
 
+from DIRAC.Interfaces.API.Dirac                       import Dirac
 dirac = Dirac()
 exitCode = 0
-result = dirac.replicateFile( args[0], args[1], sourceSE, localCache, printOutput = True )
+result = dirac.replicateFile( lfn, seName, sourceSE, localCache, printOutput = True )
 if not result['OK']:
-  print 'ERROR %s' % ( result['Message'] )
+  print 'ERROR: ', result['Message']
   exitCode = 2
 
 DIRAC.exit( exitCode )
