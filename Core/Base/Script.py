fc626a042268cdfe20e7173ae3c292e2e47637a8
Adria Casajus <adria@ecm.ub.es>
2010-12-14 15:46:43 +0000

    FIX: Only disable the logger headers if the command line has been parsed. I suppose that ONLY scripts parse the command-line "by hand"
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@32026 4525493e-7705-40b1-a816-d608a930855b

--- Core/Base/Script.py
+++ Core/Base/Script.py
@@ -12,12 +12,11 @@ localCfg = LocalConfiguration()
 scriptName = os.path.basename( sys.argv[0] ).replace( '.py', '' )
 
 def parseCommandLine( script = False, ignoreErrors = False, initializeMonitor = False ):
+  gLogger.showHeaders( False )
   initialize( script, ignoreErrors, initializeMonitor, True )
 
 def initialize( script = False, ignoreErrors = False, initializeMonitor = False, enableCommandLine = False ):
   global localCfg, scriptName
-
-  gLogger.showHeaders( False )
 
   userDisabled = not localCfg.isCSEnabled()
   if not userDisabled:
