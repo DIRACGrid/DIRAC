0747497f2bb3fbd889e701bdff10a3727ad6cb3f
Ricardo Graciani <graciani@ecm.ub.es>
2010-12-17 12:48:29 +0000

    FIX: remove obsolete usage function + review
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@32105 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/scripts/dirac-admin-submit-pilot-for-job.py
+++ WorkloadManagementSystem/scripts/dirac-admin-submit-pilot-for-job.py
@@ -4,20 +4,23 @@
 # File :    dirac-admin-submit-pilot-for-job
 # Author :  Ricardo Graciani
 ########################################################################
+"""
+  Submit a DIRAC pilot for the given DIRAC job. Requires access to taskQueueDB and PilotAgentsDB
+"""
 __RCSID__ = "$Id$"
 import DIRAC
 from DIRAC.Core.Base import Script
 
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                     'Usage:',
+                                     '  %s [option|cfgfile] ... JobID ...' % Script.scriptName,
+                                     'Arguments:',
+                                     '  JobID:    DIRAC Job ID' ] ) )
 Script.parseCommandLine( ignoreErrors = True )
 args = Script.getPositionalArgs()
 
-def usage():
-  print 'Usage: %s [<options>] <JobID> [<JobID>]' % ( Script.scriptName )
-  print ' Submit 1 dirac pilot for each <JobID>'
-  DIRAC.exit( 2 )
-
 if len( args ) < 1:
-  usage()
+  Script.showHelp()
 
 from DIRAC.WorkloadManagementSystem.Agent.TaskQueueDirector import taskQueueDB, gLitePilotDirector
 
