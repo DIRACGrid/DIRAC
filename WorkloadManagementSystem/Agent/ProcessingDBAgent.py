01047226dcbe595f7e891998f2eda94a5490314c
Ricardo Graciani <graciani@ecm.ub.es>
2011-01-18 13:22:28 +0000

    FIX: reformat
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@32733 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/Agent/ProcessingDBAgent.py
+++ WorkloadManagementSystem/Agent/ProcessingDBAgent.py
@@ -18,90 +18,90 @@ from DIRAC                                                 import S_OK, S_ERROR
 
 import os, re, time, string
 
-class ProcessingDBAgent(OptimizerModule):
+class ProcessingDBAgent( OptimizerModule ):
 
   #############################################################################
-  def initializeOptimizer(self):
+  def initializeOptimizer( self ):
     """ Initialization of the Agent.
     """
-    self.jobTypeToCheck       = 'processing'
+    self.jobTypeToCheck = 'processing'
 
     #disabled until available
-    self.disableProcDBCheck   = self.am_getOption( 'ProcDBFlag', True )
-    self.failedMinorStatus    = self.am_getOption( 'FailedJobStatus', 'ProcessingDB Error' )
+    self.disableProcDBCheck = self.am_getOption( 'ProcDBFlag', True )
+    self.failedMinorStatus = self.am_getOption( 'FailedJobStatus', 'ProcessingDB Error' )
 
     self.PDBFileCatalog = None
     if not self.disableProcDBCheck:
       dbURL = self.am_getOption( 'ProcDBURL', 'processingdb.cern.ch' )
       try:
         from DIRAC.DataManagementSystem.Client.ProcDBCatalogClient import ProcDBCatalogClient
-        self.PDBFileCatalog = ProcDBCatalogClient(dbURL)
-        self.log.debug("Instantiating ProcessingDB Catalog at %s" % dbURL )
-      except Exception,x:
+        self.PDBFileCatalog = ProcDBCatalogClient( dbURL )
+        self.log.debug( "Instantiating ProcessingDB Catalog at %s" % dbURL )
+      except Exception, x:
         msg = "Failed to create ProcDBFileCatalogClient"
-        self.log.fatal( msg, str(x) )
+        self.log.fatal( msg, str( x ) )
         return S_ERROR( msg )
 
     return S_OK()
 
   #############################################################################
-  def checkJob( self, job,classAdJob ):
+  def checkJob( self, job, classAdJob ):
     """This method controls the checking of the job.
     """
 
-    result = self.jobDB.getInputData(job)
+    result = self.jobDB.getInputData( job )
     if not result['OK']:
-      self.log.error('Failed to get input data from JobdB ', "For job %s: %s" % (job, result['Message'] ) )
+      self.log.error( 'Failed to get input data from JobdB ', "For job %s: %s" % ( job, result['Message'] ) )
       return S_ERROR( "Failed to get input data" )
     if not result['Value']:
-      self.log.info('Job %s has no input data requirement' % (job) )
-      return self.setNextOptimizer(job)
+      self.log.info( 'Job %s has no input data requirement' % ( job ) )
+      return self.setNextOptimizer( job )
 
-    self.log.debug('Job %s has an input data requirement ' % (job))
+    self.log.debug( 'Job %s has an input data requirement ' % ( job ) )
     inputData = result['Value']
     #check job is of correct type
-    result = self.getJobType(job)
+    result = self.getJobType( job )
     if not result['OK']:
-      self.log.error(result['Message'])
+      self.log.error( result['Message'] )
       return result
 
     jobType = result['Value']
     if jobType != self.jobTypeToCheck:
-      self.log.info('Job %s is of type %s and will be ignored' % (job,jobType))
+      self.log.info( 'Job %s is of type %s and will be ignored' % ( job, jobType ) )
       return self.setNextOptimizer( job )
 
-    self.log.info('Job %s is of type %s and will be processed' % (job,jobType))
+    self.log.info( 'Job %s is of type %s and will be processed' % ( job, jobType ) )
     if not self.disableProcDBCheck:
-      procDBResult = self.checkProcDB(job,inputData)
+      procDBResult = self.checkProcDB( job, inputData )
       if not procDBResult['OK']:
-        self.log.error(procDBResult['Message'])
+        self.log.error( procDBResult['Message'] )
         return procDBResult
-      result = self.setOptimizerJobInfo(job,self.am_getModuleParam( 'agentName' ),procDBResult)
+      result = self.setOptimizerJobInfo( job, self.am_getModuleParam( 'agentName' ), procDBResult )
       if not result['OK']:
-        self.log.error(result['Message'])
+        self.log.error( result['Message'] )
         return result
-    return self.setNextOptimizer(job)
+    return self.setNextOptimizer( job )
 
   #############################################################################
-  def getJobType(self,job):
+  def getJobType( self, job ):
     """This method checks the JobDB for the type of the job being checked.
     """
-    self.log.debug("self.jobDB.getJobAttribute("+str(job)+",JobType)")
-    result = self.jobDB.getJobAttribute(job,'JobType')
+    self.log.debug( "self.jobDB.getJobAttribute(" + str( job ) + ",JobType)" )
+    result = self.jobDB.getJobAttribute( job, 'JobType' )
     return result
 
   #############################################################################
-  def checkProcDB(self,job,inputData):
+  def checkProcDB( self, job, inputData ):
     """This method checks the file catalogue for replica information.  This should
        also add the single site candidate to the optimizer information to be used
        in the scheduling decision.
     """
 
-    lfns = [string.replace(fname,'LFN:','') for fname in inputData]
+    lfns = [string.replace( fname, 'LFN:', '' ) for fname in inputData]
     start = time.time()
-    result = self.PDBFileCatalog.getPfnsByLfnList(lfns)
+    result = self.PDBFileCatalog.getPfnsByLfnList( lfns )
     timing = time.time() - start
-    self.log.info(self.am_getModuleParam( 'agentName' ) + ' Lookup Time: %s seconds ' % (timing) )
+    self.log.info( self.am_getModuleParam( 'agentName' ) + ' Lookup Time: %s seconds ' % ( timing ) )
     return result
 
 #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
