22a267b18dc4016e9d5d95dda5200497a771f13f
Ricardo Graciani <graciani@ecm.ub.es>
2011-01-18 13:17:36 +0000

    FIX: added some extra doc strings and clean up of none used code.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@32731 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/Agent/PilotMonitorAgent.py
+++ WorkloadManagementSystem/Agent/PilotMonitorAgent.py
@@ -3,7 +3,6 @@
 # File :   PilotMonitor.py
 # Author : Stuart Paterson
 ########################################################################
-
 """  The Pilot Monitor Agent controls the tracking of pilots via the AgentMonitor and Grid
      specific sub-classes. This is a simple wrapper that performs the instantiation and monitoring
      of the AgentMonitor instance for all Grids.
@@ -12,93 +11,38 @@
 __RCSID__ = "$Id$"
 
 from DIRAC.Core.Base.AgentModule    import AgentModule
-from DIRAC                    import S_OK, S_ERROR, gConfig, gLogger
+from DIRAC                    import S_OK, gLogger
 from DIRAC.WorkloadManagementSystem.DB.PilotAgentsDB import PilotAgentsDB
-from DIRAC.WorkloadManagementSystem.DB.JobDB import JobDB
-import DIRAC.Core.Utilities.Time as Time
-
-import os, time
 
 class PilotMonitorAgent( AgentModule ):
+  """
+      The specific agents must provide the following methods:
+      - initialize() for initial settings
+      - beginExecution()
+      - execute() - the main method called in the agent cycle
+      - endExecution()
+      - finalize() - the graceful exit of the method, this one is usually used
+                 for the agent restart
+  """
 
   #############################################################################
   def initialize( self ):
     """Sets defaults
     """
     self.am_setOption( 'PollingTime', 120 )
-    self.selectJobLimit = self.am_getOption( 'JobSelectLimit', 100 )
-    self.maxWaitingTime = self.am_getOption( 'MaxJobWaitingTime', 5 * 60 )
-    self.maxPilotAgents = self.am_getOption( 'MaxPilotsPerJob', 4 )
     self.clearPilotsDelay = self.am_getOption( 'ClearPilotsDelay', 30 )
     self.clearAbortedDelay = self.am_getOption( 'ClearAbortedPilotsDelay', 7 )
 
     self.pilotDB = PilotAgentsDB()
-    self.jobDB = JobDB()
     return S_OK()
 
   #############################################################################
   def execute( self ):
-    """The PilotMonitor execution method.
     """
-
-    for minor in ['Pilot Agent Response', 'Director Submitting']:
-
-      selection = {'Status':'Waiting', 'MinorStatus':minor}
-      #delay  = time.localtime( time.time() - self.maxWaitingTime )
-      #delay = time.strftime( "%Y-%m-%d %H:%M:%S", delay )
-      delay = Time.toString( Time.dateTime() - self.maxWaitingTime * Time.second )
-      result = self.jobDB.selectJobs( selection, older = delay, limit = self.selectJobLimit, orderAttribute = 'LastUpdateTime' )
-      if not result['OK']:
-        return result
-
-      jobList = result['Value']
-      for jobID in jobList:
-
-        self.log.info( "Processing job", jobID )
-
-        result = self.jobDB.lookUpJobInQueue( jobID )
-        if not result:
-          self.log.warn( 'Job Not in TaskQueue', jobID )
-          continue
-
-
-        result = self.pilotDB.getPilotsForJob( int( jobID ) )
-        if not result['OK']:
-          self.log.warn( 'Failed to get pilots for job %d' % int( jobID ) )
-          # Assume no pilots were sent yet
-          result = self.jobDB.setJobAttribute( jobID, "MinorStatus",
-                                              "Pilot Agent Submission",
-                                              update = True )
-          continue
-
-        pilotList = result['Value']
-        result = self.pilotDB.getPilotInfo( pilotList )
-        if not result['OK']:
-          self.log.warn( 'Failed to get pilots info for job %d' % int( jobID ) )
-          continue
-
-        resultDict = result['Value']
-
-        self.log.debug( "Pilot info", resultDict )
-
-        aborted_pilots = []
-        submitted_pilots = []
-        for pRef, pilotDict in resultDict.items():
-          if pilotDict['Status'] == "Aborted":
-            aborted_pilots.append( pRef )
-          if pilotDict['Status'] == "Submitted" or \
-             pilotDict['Status'] == "Scheduled":
-            submitted_pilots.append( pRef )
-
-        if len( submitted_pilots ) < self.maxPilotAgents:
-          result = self.jobDB.setJobAttribute( jobID, "MinorStatus",
-                                              "Pilot Agent Submission",
-                                              update = True )
-        else:
-          result = self.jobDB.setJobAttribute( jobID, "MinorStatus",
-                                               minor,
-                                               update = True )
-
+      Remove from PilotDB pilots that:
+      - are older than self.clearPilotsDelay
+      - are Aborted and older than self.clearAbortedDelay
+    """
     result = self.pilotDB.clearPilots( self.clearPilotsDelay, self.clearAbortedDelay )
     if not result['OK']:
       self.log.warn( 'Failed to clear old pilots in the PilotAgentsDB' )
