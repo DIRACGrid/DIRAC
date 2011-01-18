93b218cf9fa32498b4def3d831be4e451f000315
Ricardo Graciani <graciani@ecm.ub.es>
2011-01-18 13:11:58 +0000

    FIX: added some extra doc strings and clean up of none used code.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@32728 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/Agent/JobPolicyAgent.py
+++ WorkloadManagementSystem/Agent/JobPolicyAgent.py
@@ -3,28 +3,30 @@
 # File :   JobPolicyAgent.py
 # Author : Stuart Paterson
 ########################################################################
-
-"""   The Job Policy Agent populates all job JDL with default VO parameters
-      specified in the DIRAC/VOPolicy CS section.
+"""
+  The Job Policy Agent populates all job JDL with default VO parameters
+  specified in the DIRAC/VOPolicy CS section.
 
 """
 
 __RCSID__ = "$Id$"
 
 from DIRAC.WorkloadManagementSystem.Agent.OptimizerModule  import OptimizerModule
-from DIRAC.Core.Utilities.ClassAd.ClassAdLight             import ClassAd
-from DIRAC                                                 import gConfig, S_OK, S_ERROR
 
-import os, re, time, string
-
-class JobPolicyAgent(OptimizerModule):
+class JobPolicyAgent( OptimizerModule ):
+  """
+      The specific Optimizer must provide the following methods:
+      - checkJob() - the main method called for each job
+      and it can provide:
+      - initializeOptimizer() before each execution cycle      
+  """
 
   #############################################################################
   def checkJob( self, job, classAdJob ):
     """This method controls the checking of the job.
     """
 
-    self.log.info("Checking JDL for job: %s" %(job))
-    return self.setNextOptimizer(job)
+    self.log.info( "Checking JDL for job: %s" % ( job ) )
+    return self.setNextOptimizer( job )
 
 #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
