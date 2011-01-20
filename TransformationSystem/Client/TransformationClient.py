936a728a20f7343fffc0e73d4faf85ce415d568b
Federico Stagni <fstagni@cern.ch>
2011-01-20 13:45:21 +0000

    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@32782 4525493e-7705-40b1-a816-d608a930855b

--- TransformationSystem/Client/TransformationClient.py
+++ TransformationSystem/Client/TransformationClient.py
@@ -5,9 +5,8 @@
 ########################################################################
 __RCSID__ = "$Id$"
 
-from DIRAC                                          import gLogger, gConfig, S_OK, S_ERROR
+from DIRAC                                          import S_OK, S_ERROR
 from DIRAC.Core.Base.Client                         import Client
-from DIRAC.Core.DISET.RPCClient                     import RPCClient
 from DIRAC.Core.Utilities.List                      import breakListIntoChunks
 from DIRAC.Resources.Catalog.FileCatalogueBase      import FileCatalogueBase
 import types
