e7c27622723a6d6a50f81fe6c69b8a5c5859f9f7
Ricardo Graciani <graciani@ecm.ub.es>
2011-02-07 23:37:17 +0000

    some svn properties updated
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@33529 4525493e-7705-40b1-a816-d608a930855b

--- StorageManagementSystem/Agent/StageRequestAgent.py
+++ StorageManagementSystem/Agent/StageRequestAgent.py
@@ -1,5 +1,5 @@
-# $HeadURL: /tmp/libdirac/tmp.FKduyw2449/dirac/DIRAC3/DIRAC/StorageManagementSystem/Agent/StageRequest.py,v 1.2 2009/10/30 22:03:03 acsmith Exp $
-__RCSID__ = "$Id: StageRequest.py,v 1.2 2009/10/30 22:03:03 acsmith Exp $"
+# $HeadURL$
+__RCSID__ = "$Id$"
 
 from DIRAC import gLogger, gConfig, gMonitor, S_OK, S_ERROR, rootPath
 
