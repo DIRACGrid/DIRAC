cd4401c374dc848a2ce966f396439257657deba4
Federico Stagni <fstagni@cern.ch>
2011-02-08 13:01:47 +0000

    Updated test
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@33545 4525493e-7705-40b1-a816-d608a930855b

--- ResourceStatusSystem/Agent/ClientsCacheFeeder.py
+++ ResourceStatusSystem/Agent/ClientsCacheFeeder.py
@@ -4,18 +4,16 @@
 """ This agents feeds the ClientsCache table.
 """
 
-import copy
-import datetime
+import copy, datetime
 
 from DIRAC import S_OK, S_ERROR
 from DIRAC import gLogger
 
-from DIRAC.ResourceStatusSystem.Utilities.CS import *
+from DIRAC.ResourceStatusSystem.Utilities.CS import getExt
 
 from DIRAC.Core.Base.AgentModule import AgentModule
 from DIRAC.Core.DISET.RPCClient import RPCClient
-from DIRAC.ResourceStatusSystem.DB.ResourceStatusDB import *
-from DIRAC.ResourceStatusSystem.Utilities.Exceptions import *
+from DIRAC.ResourceStatusSystem.DB.ResourceStatusDB import ResourceStatusDB
 from DIRAC.ResourceStatusSystem.Command.CommandCaller import CommandCaller
 from DIRAC.ResourceStatusSystem.Command.ClientsInvoker import ClientsInvoker
 
@@ -23,27 +21,27 @@ __RCSID__ = "$Id:  $"
 
 AGENT_NAME = 'ResourceStatus/ClientsCacheFeeder'
 
-class ClientsCacheFeeder(AgentModule):
+class ClientsCacheFeeder( AgentModule ):
 
 #############################################################################
 
-  def initialize(self):
+  def initialize( self ):
     """ ClientsCacheFeeder initialization
     """
-    
+
     try:
 
       self.rsDB = ResourceStatusDB()
-      
+
       self.clientsInvoker = ClientsInvoker()
 
       VOExtension = getExt()
-      
-      configModule = __import__(VOExtension+"DIRAC.ResourceStatusSystem.Policy.Configurations", 
-                                globals(), locals(), ['*'])
-      commandsList_ClientsCache = copy.deepcopy(configModule.Commands_ClientsCache)
 
-      commandsList_AccountingCache = copy.deepcopy(configModule.Commands_AccountingCache)
+      configModule = __import__( VOExtension + "DIRAC.ResourceStatusSystem.Policy.Configurations",
+                                globals(), locals(), ['*'] )
+      commandsList_ClientsCache = copy.deepcopy( configModule.Commands_ClientsCache )
+
+      commandsList_AccountingCache = copy.deepcopy( configModule.Commands_AccountingCache )
 
       self.commandObjectsList_ClientsCache = []
 
@@ -51,59 +49,59 @@ class ClientsCacheFeeder(AgentModule):
 
       cc = CommandCaller()
 
-      RPCWMSAdmin = RPCClient("WorkloadManagement/WMSAdministrator")
-      RPCAccounting = RPCClient("Accounting/ReportGenerator")
+      RPCWMSAdmin = RPCClient( "WorkloadManagement/WMSAdministrator" )
+      RPCAccounting = RPCClient( "Accounting/ReportGenerator" )
 
       for command in commandsList_ClientsCache:
-        cObj = cc.setCommandObject(command)
-        cc.setCommandClient(command, cObj, RPCWMSAdmin = RPCWMSAdmin, 
-                            RPCAccounting = RPCAccounting)
-        self.commandObjectsList_ClientsCache.append((command, cObj))
-        
+        cObj = cc.setCommandObject( command )
+        cc.setCommandClient( command, cObj, RPCWMSAdmin = RPCWMSAdmin,
+                            RPCAccounting = RPCAccounting )
+        self.commandObjectsList_ClientsCache.append( ( command, cObj ) )
+
       for command in commandsList_AccountingCache:
-        cObj = cc.setCommandObject(command)
-        cc.setCommandClient(command, cObj, RPCAccounting = RPCAccounting)
+        cObj = cc.setCommandObject( command )
+        cc.setCommandClient( command, cObj, RPCAccounting = RPCAccounting )
         try:
           cArgs = command[2]
         except IndexError:
           cArgs = ()
-        self.commandObjectsList_AccountingCache.append((command, cObj, cArgs))
-        
+        self.commandObjectsList_AccountingCache.append( ( command, cObj, cArgs ) )
+
       return S_OK()
 
     except Exception:
       errorStr = "ClientsCacheFeeder initialization"
-      gLogger.exception(errorStr)
-      return S_ERROR(errorStr)
+      gLogger.exception( errorStr )
+      return S_ERROR( errorStr )
 
 #############################################################################
 
-  def execute(self):
+  def execute( self ):
     """ The main ClientsCacheFeeder execution method
     """
-    
+
     try:
-      
+
       now = datetime.datetime.utcnow()
-      
+
       for co in self.commandObjectsList_ClientsCache:
         try:
-          self.clientsInvoker.setCommand(co[1])
+          self.clientsInvoker.setCommand( co[1] )
           res = self.clientsInvoker.doCommand()
           for key in res.keys():
             if 'ID' in res[key].keys():
               for value in res[key].keys():
                 if value != 'ID':
-                  self.rsDB.addOrModifyClientsCacheRes(key.split()[1], co[0][1].split('_')[0], 
-                                                       value, res[key][value], res[key]['ID'])
+                  self.rsDB.addOrModifyClientsCacheRes( key.split()[1], co[0][1].split( '_' )[0],
+                                                       value, res[key][value], res[key]['ID'] )
             else:
               for value in res[key].keys():
-                self.rsDB.addOrModifyClientsCacheRes(key, co[0][1].split('_')[0], 
-                                                     value, res[key][value])
+                self.rsDB.addOrModifyClientsCacheRes( key, co[0][1].split( '_' )[0],
+                                                     value, res[key][value] )
         except:
-          gLogger.exception("Exception when executing " + co[0][1])
+          gLogger.exception( "Exception when executing " + co[0][1] )
           continue
-      
+
       for co in self.commandObjectsList_AccountingCache:
         if co[0][3] == 'Hourly':
           if now.minute >= 10:
@@ -111,25 +109,25 @@ class ClientsCacheFeeder(AgentModule):
         elif co[0][3] == 'Daily':
           if now.hour >= 1:
             continue
-        
+
         try:
-          co[1].setArgs(co[2])
-          self.clientsInvoker.setCommand(co[1])
+          co[1].setArgs( co[2] )
+          self.clientsInvoker.setCommand( co[1] )
           res = self.clientsInvoker.doCommand()
           plotType = res.keys()[0]
           for name in res[plotType].keys():
-            plotName = co[0][1].split('_')[0] + '_' + str(co[2][0])
-            self.rsDB.addOrModifyAccountingCacheRes(name, plotType, plotName,
-                                                    res[plotType][name])
+            plotName = co[0][1].split( '_' )[0] + '_' + str( co[2][0] )
+            self.rsDB.addOrModifyAccountingCacheRes( name, plotType, plotName,
+                                                    res[plotType][name] )
         except:
-          gLogger.exception("Exception when executing " + co[0][1])
+          gLogger.exception( "Exception when executing " + co[0][1] )
           continue
-      
+
       return S_OK()
-    
+
     except Exception:
       errorStr = "ClientsCacheFeeder execution"
-      gLogger.exception(errorStr)
-      return S_ERROR(errorStr)
+      gLogger.exception( errorStr )
+      return S_ERROR( errorStr )
 
 #############################################################################
--- ResourceStatusSystem/Agent/test/Test_Agents.py
+++ ResourceStatusSystem/Agent/test/Test_Agents.py
@@ -1,14 +1,13 @@
 import unittest
 import sys
-from DIRAC.ResourceStatusSystem.Utilities.mock import Mock
 import DIRAC.ResourceStatusSystem.test.fake_AgentModule
 import DIRAC.ResourceStatusSystem.test.fake_rsDB
 import DIRAC.ResourceStatusSystem.test.fake_Logger
 
-class AgentsTestCase(unittest.TestCase):
+class AgentsTestCase( unittest.TestCase ):
   """ Base class for the Agents test cases
   """
-  def setUp(self):
+  def setUp( self ):
 
     sys.modules["DIRAC.LoggingSystem.Client.Logger"] = DIRAC.ResourceStatusSystem.test.fake_Logger
     sys.modules["DIRAC.Core.Base.AgentModule"] = DIRAC.ResourceStatusSystem.test.fake_AgentModule
@@ -18,115 +17,115 @@ class AgentsTestCase(unittest.TestCase):
     sys.modules["DIRAC.ConfigurationSystem.Client.CSAPI"] = DIRAC.ResourceStatusSystem.test.fake_Logger
 
     from DIRAC.ResourceStatusSystem.Agent.ClientsCacheFeeder import ClientsCacheFeeder
-    self.ccFeeder = ClientsCacheFeeder("", "")
+    self.ccFeeder = ClientsCacheFeeder( "", "" )
 
     from DIRAC.ResourceStatusSystem.Agent.CleanerAgent import CleanerAgent
-    self.clAgent = CleanerAgent("", "")
+    self.clAgent = CleanerAgent( "", "" )
 
     from DIRAC.ResourceStatusSystem.Agent.TokenAgent import TokenAgent
-    self.tokenAgent = TokenAgent("", "")
+    self.tokenAgent = TokenAgent( "", "" )
 
     from DIRAC.ResourceStatusSystem.Agent.RSInspectorAgent import RSInspectorAgent
-    self.rsIAgent = RSInspectorAgent("", "")
+    self.rsIAgent = RSInspectorAgent( "", "" )
 
     from DIRAC.ResourceStatusSystem.Agent.SSInspectorAgent import SSInspectorAgent
-    self.ssIAgent = SSInspectorAgent("", "")
+    self.ssIAgent = SSInspectorAgent( "", "" )
 
     from DIRAC.ResourceStatusSystem.Agent.SeSInspectorAgent import SeSInspectorAgent
-    self.sesIAgent = SeSInspectorAgent("", "")
+    self.sesIAgent = SeSInspectorAgent( "", "" )
 
     from DIRAC.ResourceStatusSystem.Agent.StElInspectorAgent import StElInspectorAgent
-    self.stelIAgent = StElInspectorAgent("", "")
+    self.stelIAgent = StElInspectorAgent( "", "" )
 
-        
-class ClientsCacheFeederSuccess(AgentsTestCase):
 
-  def test_initialize(self):
+class ClientsCacheFeederSuccess( AgentsTestCase ):
+
+  def test_initialize( self ):
     res = self.ccFeeder.initialize()
-    self.assert_(res['OK'])
+    self.assert_( res['OK'] )
 
-  def test_execute(self):
+  def test_execute( self ):
     self.ccFeeder.initialize()
     res = self.ccFeeder.execute()
-    self.assert_(res['OK'])
+    self.assert_( res['OK'] )
 
-class CleanerSuccess(AgentsTestCase):
+class CleanerSuccess( AgentsTestCase ):
 
-  def test_initialize(self):
+  def test_initialize( self ):
     res = self.clAgent.initialize()
-    self.assert_(res['OK'])
+    self.assert_( res['OK'] )
 
-  def test_execute(self):
+  def test_execute( self ):
     self.clAgent.initialize()
     res = self.clAgent.execute()
-    self.assert_(res['OK'])
+    self.assert_( res['OK'] )
 
-class TokenSuccess(AgentsTestCase):
+class TokenSuccess( AgentsTestCase ):
 
-  def test_initialize(self):
+  def test_initialize( self ):
     res = self.tokenAgent.initialize()
-    self.assert_(res['OK'])
+    self.assert_( res['OK'] )
 
-  def test_execute(self):
+  def test_execute( self ):
     self.tokenAgent.initialize()
     res = self.tokenAgent.execute()
-    self.assert_(res['OK'])
+    self.assert_( res['OK'] )
 
-class RSInspectorSuccess(AgentsTestCase):
+class RSInspectorSuccess( AgentsTestCase ):
 
-  def test_initialize(self):
+  def test_initialize( self ):
     res = self.rsIAgent.initialize()
-    self.assert_(res['OK'])
+    self.assert_( res['OK'] )
 
-  def test_execute(self):
+  def test_execute( self ):
     self.rsIAgent.initialize()
     res = self.rsIAgent.execute()
-    self.assert_(res['OK'])
+    self.assert_( res['OK'] )
 
-class SSInspectorSuccess(AgentsTestCase):
+class SSInspectorSuccess( AgentsTestCase ):
 
-  def test_initialize(self):
+  def test_initialize( self ):
     res = self.ssIAgent.initialize()
-    self.assert_(res['OK'])
+    self.assert_( res['OK'] )
 
-  def test_execute(self):
+  def test_execute( self ):
     self.ssIAgent.initialize()
     res = self.ssIAgent.execute()
-    self.assert_(res['OK'])
+    self.assert_( res['OK'] )
 
-class SeSInspectorSuccess(AgentsTestCase):
+class SeSInspectorSuccess( AgentsTestCase ):
 
-  def test_initialize(self):
+  def test_initialize( self ):
     res = self.sesIAgent.initialize()
-    self.assert_(res['OK'])
+    self.assert_( res['OK'] )
 
-  def test_execute(self):
+  def test_execute( self ):
     self.sesIAgent.initialize()
     res = self.sesIAgent.execute()
-    self.assert_(res['OK'])
+    self.assert_( res['OK'] )
 
 
-class StElInspectorSuccess(AgentsTestCase):
+class StElInspectorSuccess( AgentsTestCase ):
 
-  def test_initialize(self):
+  def test_initialize( self ):
     res = self.stelIAgent.initialize()
-    self.assert_(res['OK'])
+    self.assert_( res['OK'] )
 
-  def test_execute(self):
+  def test_execute( self ):
     self.stelIAgent.initialize()
     res = self.stelIAgent.execute()
-    self.assert_(res['OK'])
+    self.assert_( res['OK'] )
 
 
 
 
 if __name__ == '__main__':
-  suite = unittest.defaultTestLoader.loadTestsFromTestCase(AgentsTestCase)
-  suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(ClientsCacheFeederSuccess))
-  suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(CleanerSuccess))
-  suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TokenSuccess))
-  suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(RSInspectorSuccess))
-  suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(SSInspectorSuccess))
-  suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(SeSInspectorSuccess))
-  suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(StElInspectorSuccess))
-  testResult = unittest.TextTestRunner(verbosity=2).run(suite)
+  suite = unittest.defaultTestLoader.loadTestsFromTestCase( AgentsTestCase )
+  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( ClientsCacheFeederSuccess ) )
+  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( CleanerSuccess ) )
+  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( TokenSuccess ) )
+  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( RSInspectorSuccess ) )
+  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( SSInspectorSuccess ) )
+  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( SeSInspectorSuccess ) )
+  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( StElInspectorSuccess ) )
+  testResult = unittest.TextTestRunner( verbosity = 2 ).run( suite )
