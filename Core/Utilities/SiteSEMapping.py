2b06dacd4521287137cfafc8532e80bf3ce1ddc7
Philippe Charpentier <Philippe.Charpentier@cern.ch>
2011-02-16 12:18:41 +0000

    isSameSiteSE: treat case when an SE is not associated to a site
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@34115 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/SiteSEMapping.py
+++ Core/Utilities/SiteSEMapping.py
@@ -12,90 +12,90 @@
 
 __RCSID__ = "$Id$"
 
-import string,re
+import string, re
 
 from DIRAC import gConfig, gLogger, S_OK, S_ERROR
 
 #############################################################################
-def getSiteSEMapping(gridName=''):
+def getSiteSEMapping( gridName = '' ):
   """ Returns a dictionary of all sites and their localSEs as a list, e.g.
       {'LCG.CERN.ch':['CERN-RAW','CERN-RDST',...]}
       If gridName is specified, result is restricted to that Grid type.
   """
   siteSEMapping = {}
-  gridTypes = gConfig.getSections('Resources/Sites/',[])
+  gridTypes = gConfig.getSections( 'Resources/Sites/', [] )
   if not gridTypes['OK']:
-    gLogger.warn('Problem retrieving sections in /Resources/Sites')
+    gLogger.warn( 'Problem retrieving sections in /Resources/Sites' )
     return gridTypes
 
   gridTypes = gridTypes['Value']
   if gridName:
     if not gridName in gridTypes:
-      return S_ERROR('Could not get sections for /Resources/Sites/%s' %gridName)
+      return S_ERROR( 'Could not get sections for /Resources/Sites/%s' % gridName )
     gridTypes = [gridName]
 
-  gLogger.debug('Grid Types are: %s' %(string.join(gridTypes,', ')))
+  gLogger.debug( 'Grid Types are: %s' % ( string.join( gridTypes, ', ' ) ) )
   for grid in gridTypes:
-    sites = gConfig.getSections('/Resources/Sites/%s' %grid,[])
+    sites = gConfig.getSections( '/Resources/Sites/%s' % grid, [] )
     if not sites['OK']:
-      gLogger.warn('Problem retrieving /Resources/Sites/%s section' %grid)
+      gLogger.warn( 'Problem retrieving /Resources/Sites/%s section' % grid )
       return sites
     for candidate in sites['Value']:
-      candidateSEs = gConfig.getValue('/Resources/Sites/%s/%s/SE' %(grid,candidate),[])
+      candidateSEs = gConfig.getValue( '/Resources/Sites/%s/%s/SE' % ( grid, candidate ), [] )
       if candidateSEs:
-        siteSEMapping[candidate]=candidateSEs
+        siteSEMapping[candidate] = candidateSEs
       else:
-        gLogger.debug('No SEs defined for site %s' %candidate)
+        gLogger.debug( 'No SEs defined for site %s' % candidate )
 
-  return S_OK(siteSEMapping)
+  return S_OK( siteSEMapping )
 
 #############################################################################
-def getSESiteMapping(gridName=''):
+def getSESiteMapping( gridName = '' ):
   """ Returns a dictionary of all SEs and their associated site(s), e.g.
       {'CERN-RAW':'LCG.CERN.ch','CERN-RDST':'LCG.CERN.ch',...]}
       Although normally one site exists for a given SE, it is possible over all
-      Grid types to have multiple entries. 
+      Grid types to have multiple entries.
       If gridName is specified, result is restricted to that Grid type.
       Assumes CS structure of: /Resources/Sites/<GRIDNAME>/<SITENAME>
   """
   seSiteMapping = {}
-  gridTypes = gConfig.getSections('/Resources/Sites/',[])
+  gridTypes = gConfig.getSections( '/Resources/Sites/', [] )
   if not gridTypes['OK']:
-    gLogger.warn('Problem retrieving sections in /Resources/Sites')
+    gLogger.warn( 'Problem retrieving sections in /Resources/Sites' )
     return gridTypes
 
   gridTypes = gridTypes['Value']
   if gridName:
     if not gridName in gridTypes:
-      return S_ERROR('Could not get sections for /Resources/Sites/%s' %gridName)
+      return S_ERROR( 'Could not get sections for /Resources/Sites/%s' % gridName )
     gridTypes = [gridName]
 
-  gLogger.debug('Grid Types are: %s' %(string.join(gridTypes,', ')))
+  gLogger.debug( 'Grid Types are: %s' % ( string.join( gridTypes, ', ' ) ) )
   for grid in gridTypes:
-    sites = gConfig.getSections('/Resources/Sites/%s' %grid,[])
+    sites = gConfig.getSections( '/Resources/Sites/%s' % grid, [] )
     if not sites['OK']: #gConfig returns S_ERROR for empty sections until version
-      gLogger.warn('Problem retrieving /Resources/Sites/%s section' %grid)
+      gLogger.warn( 'Problem retrieving /Resources/Sites/%s section' % grid )
       return sites
     if sites:
       for candidate in sites['Value']:
-        siteSEs = gConfig.getValue('/Resources/Sites/%s/%s/SE' %(grid,candidate),[])
+        siteSEs = gConfig.getValue( '/Resources/Sites/%s/%s/SE' % ( grid, candidate ), [] )
         for se in siteSEs:
           if se not in seSiteMapping:
             seSiteMapping[se] = []
-          seSiteMapping[se].append(candidate)
+          seSiteMapping[se].append( candidate )
 
-  return S_OK(seSiteMapping)
+  return S_OK( seSiteMapping )
 
 #############################################################################
-def getSitesForSE(storageElement,gridName=''):
+def getSitesForSE( storageElement, gridName = '' ):
   """ Given a DIRAC SE name this method returns a list of corresponding sites.
       Optionally restrict to Grid specified by name.
   """
-  
+
   finalSites = []
-  gridTypes = gConfig.getSections('/Resources/Sites/',[])
+  gridTypes = gConfig.getSections( '/Resources/Sites/', [] )
   if not gridTypes['OK']:
-    gLogger.warn('Problem retrieving sections in /Resources/Sites')
+    gLogger.warn( 'Problem retrieving sections in /Resources/Sites' )
     return gridTypes
 
   gridTypes = gridTypes['Value']
@@ -103,72 +103,75 @@ def getSitesForSE(storageElement,gridName=''):
     if gridName in gridTypes:
       gridTypes = [gridName]
     else:
-      return S_ERROR('Grid type %s not in list: %s' %(gridName,string.join(gridTypes,', ')))
-  
+      return S_ERROR( 'Grid type %s not in list: %s' % ( gridName, string.join( gridTypes, ', ' ) ) )
+
   for grid in gridTypes:
-    sites = gConfig.getSections('/Resources/Sites/%s' %grid,[])
+    sites = gConfig.getSections( '/Resources/Sites/%s' % grid, [] )
     if not sites['OK']: #gConfig returns S_ERROR for empty sections until version
-      gLogger.warn('Problem retrieving /Resources/Sites/%s section' %grid)
+      gLogger.warn( 'Problem retrieving /Resources/Sites/%s section' % grid )
       return sites
     if sites:
       siteList = sites['Value']
       for candidate in siteList:
-        siteSEs = gConfig.getValue('/Resources/Sites/%s/%s/SE' %(grid,candidate),[])
+        siteSEs = gConfig.getValue( '/Resources/Sites/%s/%s/SE' % ( grid, candidate ), [] )
         if storageElement in siteSEs:
-          finalSites.append(candidate)
+          finalSites.append( candidate )
 
-  return S_OK(finalSites)
+  return S_OK( finalSites )
 
 #############################################################################
-def getSEsForSite(siteName):
+def getSEsForSite( siteName ):
   """ Given a DIRAC site name this method returns a list of corresponding SEs.
   """
-  if not re.search('.',siteName):
-    return S_ERROR('%s is not a valid site name' %siteName)
-  gridName = string.split(siteName,'.')[0]
-  siteSection = '/Resources/Sites/%s/%s/SE' %(gridName,siteName)
-  ses = gConfig.getValue(siteSection,[])
-  return S_OK(ses)
+  if not re.search( '.', siteName ):
+    return S_ERROR( '%s is not a valid site name' % siteName )
+  gridName = string.split( siteName, '.' )[0]
+  siteSection = '/Resources/Sites/%s/%s/SE' % ( gridName, siteName )
+  ses = gConfig.getValue( siteSection, [] )
+  return S_OK( ses )
 
 #############################################################################
-def isSameSiteSE(se1,se2):
+def isSameSiteSE( se1, se2 ):
   """ Check if the 2 SEs are from the same site
   """
   if se1 == se2:
-    return S_OK(True)
+    return S_OK( True )
 
-  result = getSitesForSE(se1)
+  result = getSitesForSE( se1 )
   if not result['OK']:
     return result
   sites1 = result['Value']
-  result = getSitesForSE(se2)
+  result = getSitesForSE( se2 )
   if not result['OK']:
     return result
   sites2 = result['Value']
-  if sites1[0] == sites2[0]:
-    return S_OK(True)
+  try:
+    if sites1[0] == sites2[0]:
+      return S_OK( True )
+  except:
+    return S_OK( False )
 
   for site in sites1:
     if site in sites2:
-      return S_OK(True)
+      return S_OK( True )
 
-  return S_OK(False)
+  return S_OK( False )
 
 #############################################################################
-def getSEsForCountry(country):
+def getSEsForCountry( country ):
   """ Determines the associated SEs from the country code
   """
   mappedCountries = [country]
   while True:
-    mappedCountry = gConfig.getValue('/Resources/Countries/%s/AssignedTo' % country, country)
+    mappedCountry = gConfig.getValue( '/Resources/Countries/%s/AssignedTo' % country, country )
     if mappedCountry == country:
       break
     elif mappedCountry in mappedCountries:
-      return S_ERROR('Circular mapping detected for %s' % country)
+      return S_ERROR( 'Circular mapping detected for %s' % country )
     else:
       country = mappedCountry
-      mappedCountries.append(mappedCountry)
-  res = gConfig.getOptionsDict('/Resources/Countries/%s/AssociatedSEs' % country)
+      mappedCountries.append( mappedCountry )
+  res = gConfig.getOptionsDict( '/Resources/Countries/%s/AssociatedSEs' % country )
   if not res['OK']:
-    return S_ERROR('Failed to obtain AssociatedSEs for %s' % country)
-  return S_OK(res['Value'].values())
+    return S_ERROR( 'Failed to obtain AssociatedSEs for %s' % country )
+  return S_OK( res['Value'].values() )
