dc1384bbb8b3d123273650ba751854fb44ee533b
Joel Closier <Joel.Closier@cern.ch>
2011-03-05 16:38:39 +0000

    getProtocolPfn was giving back a fullpath without / while we were expecting a filename
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@35421 4525493e-7705-40b1-a816-d608a930855b

--- Resources/Storage/LHCbOnlineStorage.py
+++ Resources/Storage/LHCbOnlineStorage.py
@@ -1,16 +1,13 @@
 """ This is the LHCb Online storage """
 
-from DIRAC                                      import gLogger, gConfig, S_OK, S_ERROR
+from DIRAC                                      import gLogger, S_OK, S_ERROR
 from DIRAC.Resources.Storage.StorageBase        import StorageBase
-from DIRAC.Core.Utilities.Subprocess            import pythonCall
-from DIRAC.Core.Utilities.Pfn                   import pfnparse,pfnunparse
-from DIRAC.Core.Utilities.File                  import getSize
 from stat                                       import *
-import types,re,os,xmlrpclib
+import types, xmlrpclib
 
-class LHCbOnlineStorage(StorageBase):
+class LHCbOnlineStorage( StorageBase ):
 
-  def __init__(self,storageName,protocol,path,host,port,spaceToken,wspath):
+  def __init__( self, storageName, protocol, path, host, port, spaceToken, wspath ):
     self.isok = True
 
     self.protocolName = 'LHCbOnline'
@@ -22,14 +19,14 @@ class LHCbOnlineStorage(StorageBase):
     self.wspath = wspath
     self.spaceToken = spaceToken
     self.cwd = self.path
-    apply(StorageBase.__init__,(self,self.name,self.path))
+    apply( StorageBase.__init__, ( self, self.name, self.path ) )
 
     self.timeout = 100
 
-    serverString = "%s://%s:%s" % (protocol,host,port)
-    self.server = xmlrpclib.Server(serverString)
+    serverString = "%s://%s:%s" % ( protocol, host, port )
+    self.server = xmlrpclib.Server( serverString )
 
-  def getParameters(self):
+  def getParameters( self ):
     """ This gets all the storage specific parameters pass when instantiating the storage
     """
     parameterDict = {}
@@ -41,95 +38,95 @@ class LHCbOnlineStorage(StorageBase):
     parameterDict['Port'] = self.port
     parameterDict['SpaceToken'] = self.spaceToken
     parameterDict['WSUrl'] = self.wspath
-    return S_OK(parameterDict)
+    return S_OK( parameterDict )
 
-  def getProtocolPfn(self,pfnDict,withPort):
+  def getProtocolPfn( self, pfnDict, withPort ):
     """ From the pfn dict construct the SURL to be used
     """
-    pfnDict['Path'] = ''
-    res = pfnunparse(pfnDict)
-    pfn = res['Value'].replace('/','')
-    return S_OK(pfn)
+#    pfnDict['Path'] = ''
+#    res = pfnunparse(pfnDict)
+#    pfn = res['Value'].replace('/','')
+    return S_OK( pfnDict['FileName'] )
 
-  def getFileSize(self,path):
+  def getFileSize( self, path ):
     """ Get a fake file size
     """
-    res = self.__checkArgumentFormat(path)
+    res = self.__checkArgumentFormat( path )
     if not res['OK']:
       return res
     urls = res['Value']
-    if not len(path) > 0:
-      return S_ERROR("LHCbOnline.getFileSize: No surls supplied.")
+    if not len( path ) > 0:
+      return S_ERROR( "LHCbOnline.getFileSize: No surls supplied." )
     successful = {}
     failed = {}
     for pfn in urls:
       successful[pfn] = 0
-    resDict = {'Failed':failed,'Successful':successful}
-    return S_OK(resDict)
+    resDict = {'Failed':failed, 'Successful':successful}
+    return S_OK( resDict )
 
-  def retransferOnlineFile(self,path):
+  def retransferOnlineFile( self, path ):
     """ Tell the Online system that the migration failed and we want to get the request again
     """
-    res = self.__checkArgumentFormat(path)
+    res = self.__checkArgumentFormat( path )
     if not res['OK']:
       return res
     urls = res['Value']
-    if not len(urls) > 0:
-      return S_ERROR("LHCbOnline.requestRetransfer: No surls supplied.")
+    if not len( urls ) > 0:
+      return S_ERROR( "LHCbOnline.requestRetransfer: No surls supplied." )
     successful = {}
     failed = {}
     for pfn in urls:
       try:
-        success,error = self.server.errorMigratingFile(pfn)
+        success, error = self.server.errorMigratingFile( pfn )
         if success:
           successful[pfn] = True
-          gLogger.info("LHCbOnline.requestRetransfer: Successfully requested file from RunDB.")
+          gLogger.info( "LHCbOnline.requestRetransfer: Successfully requested file from RunDB." )
         else:
           errStr = "LHCbOnline.requestRetransfer: Failed to request file from RunDB: %s" % error
           failed[pfn] = errStr
-          gLogger.error(errStr,pfn)
-      except Exception,x:
+          gLogger.error( errStr, pfn )
+      except Exception, x:
         errStr = "LHCbOnline.requestRetransfer: Exception while requesting file from RunDB."
-        gLogger.exception(errStr,lException=x)
+        gLogger.exception( errStr, lException = x )
         failed[pfn] = errStr
-    resDict = {'Failed':failed,'Successful':successful}
-    return S_OK(resDict)
+    resDict = {'Failed':failed, 'Successful':successful}
+    return S_OK( resDict )
 
-  def removeFile(self,path):
+  def removeFile( self, path ):
     """Remove physically the file specified by its path
     """
-    res = self.__checkArgumentFormat(path)
+    res = self.__checkArgumentFormat( path )
     if not res['OK']:
       return res
     urls = res['Value']
-    if not len(urls) > 0:
-      return S_ERROR("LHCbOnline.removeFile: No surls supplied.")
+    if not len( urls ) > 0:
+      return S_ERROR( "LHCbOnline.removeFile: No surls supplied." )
     successful = {}
     failed = {}
     for pfn in urls:
       try:
-        success,error = self.server.endMigratingFile(pfn)
+        success, error = self.server.endMigratingFile( pfn )
         if success:
           successful[pfn] = True
-          gLogger.info("LHCbOnline.getFile: Successfully issued removal to RunDB.")
+          gLogger.info( "LHCbOnline.getFile: Successfully issued removal to RunDB." )
         else:
           errStr = "LHCbOnline.getFile: Failed to issue removal to RunDB: %s" % error
           failed[pfn] = errStr
-          gLogger.error(errStr,pfn)
-      except Exception,x:
+          gLogger.error( errStr, pfn )
+      except Exception, x:
         errStr = "LHCbOnline.getFile: Exception while issuing removal to RunDB."
-        gLogger.exception(errStr,lException=x)
+        gLogger.exception( errStr, lException = x )
         failed[pfn] = errStr
-    resDict = {'Failed':failed,'Successful':successful}
-    return S_OK(resDict)
+    resDict = {'Failed':failed, 'Successful':successful}
+    return S_OK( resDict )
 
-  def __checkArgumentFormat(self,path):
-    if type(path) in types.StringTypes:
+  def __checkArgumentFormat( self, path ):
+    if type( path ) in types.StringTypes:
       urls = [path]
-    elif type(path) == types.ListType:
+    elif type( path ) == types.ListType:
       urls = path
-    elif type(path) == types.DictType:
+    elif type( path ) == types.DictType:
       urls = path.keys()
     else:
-      return S_ERROR("LHCbOnline.__checkArgumentFormat: Supplied path is not of the correct format.")
-    return S_OK(urls)   
+      return S_ERROR( "LHCbOnline.__checkArgumentFormat: Supplied path is not of the correct format." )
+    return S_OK( urls )
