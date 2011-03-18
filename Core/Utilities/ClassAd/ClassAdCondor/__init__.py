3efa332ec6e164f9bf25a68bc8e81cc4f0d39fe8
Ricardo Graciani <graciani@ecm.ub.es>
2011-03-18 12:32:20 +0000

    reformat
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@36059 4525493e-7705-40b1-a816-d608a930855b

--- Core/Utilities/ClassAd/ClassAdCondor/__init__.py
+++ Core/Utilities/ClassAd/ClassAdCondor/__init__.py
@@ -13,94 +13,94 @@ import string
 
 # Provide the API used in the DIRAC project
 
-def insertAttributeStringList(self,name,attrList):
+def insertAttributeStringList( self, name, attrList ):
   """ Insert an attribute of type list of strings
   """
-  attrString = '{"'+string.join(attrList,'","')+'"}'
-  result = self.insertExpression(name,attrString)
+  attrString = '{"' + string.join( attrList, '","' ) + '"}'
+  result = self.insertExpression( name, attrString )
   return result
 
 ClassAd.insertAttributeStringList = insertAttributeStringList
 
-def getAttributeStringList(self,name):
+def getAttributeStringList( self, name ):
   """ Get a list of values from a given expression
   """
 
-  tempString = self.get_expression(name)
+  tempString = self.get_expression( name )
   if tempString != '<error:null expr>':
-    tempString = tempString.replace("{","").replace("}","").replace("\"","").replace(" ","")
-    return tempString.split(',')
+    tempString = tempString.replace( "{", "" ).replace( "}", "" ).replace( "\"", "" ).replace( " ", "" )
+    return tempString.split( ',' )
   else:
     return []
 
 ClassAd.getAttributeStringList = getAttributeStringList
 
-def lookupAttribute(self,name):
-  expr = self.get_expression(name) 
+def lookupAttribute( self, name ):
+  expr = self.get_expression( name )
   if expr is not None and expr != '<error:null expr>':
     return True
   else:
-    return False  
+    return False
 
 ClassAd.lookupAttribute = lookupAttribute
 
-def isOK(self):
+def isOK( self ):
   return self.this is not None
 
 ClassAd.isOK = isOK
 
-def getAttributeString(self,name):
-  result,success = self.get_attribute_string(name)
+def getAttributeString( self, name ):
+  result, success = self.get_attribute_string( name )
   if success:
     return result
   else:
     return None
-    
-def getAttributeInt(self,name):
-  result,success = self.get_attribute_int(name)
+
+def getAttributeInt( self, name ):
+  result, success = self.get_attribute_int( name )
   if success:
     return result
   else:
-    return None      
-    
-def getAttributeFloat(self,name):
-  result,success = self.get_attribute_float(name)
+    return None
+
+def getAttributeFloat( self, name ):
+  result, success = self.get_attribute_float( name )
   if success:
     return result
   else:
-    return None    
-    
-def getAttributeBool(self,name):
-  result,success = self.get_attribute_bool(name)
+    return None
+
+def getAttributeBool( self, name ):
+  result, success = self.get_attribute_bool( name )
   if success:
     return result
   else:
-    return None    
-    
+    return None
+
 ClassAd.getAttributeString = getAttributeString
 ClassAd.getAttributeInt = getAttributeInt
 ClassAd.getAttributeFloat = getAttributeFloat
 ClassAd.getAttributeBool = getAttributeBool
-  
 
-def matchClassAd(ca1,ca2):
+
+def matchClassAd( ca1, ca2 ):
   """ Match the 2 ClassAds and provide the result as tuple of 3 booleans:
       'symmetricMatch','leftMatchesRight','rightMatchesLeft'
   """
 
   mca = MatchClassAd()
-  mca.initialize(ca1,ca2)
+  mca.initialize( ca1, ca2 )
 
-  result_s = mca.getAttributeBool('symmetricMatch')
-  result_l = mca.getAttributeBool('leftMatchesRight')
-  result_r = mca.getAttributeBool('rightMatchesLeft')
+  result_s = mca.getAttributeBool( 'symmetricMatch' )
+  result_l = mca.getAttributeBool( 'leftMatchesRight' )
+  result_r = mca.getAttributeBool( 'rightMatchesLeft' )
 
   # Have to release the MatchClassAd to avoid Seg Fault while gabarge collection
   mca.release()
 
   if result_s is not None and result_l is not None and result_r is not None:
-    return S_OK((result_s, result_l, result_r))
+    return S_OK( ( result_s, result_l, result_r ) )
   else:
-    result = S_ERROR('Failed to match the given ClassAds: sym:%s left:%s right"%s' % (result_s,result_l,result_r))
-    result['Value'] = (result_s, result_l, result_r)
+    result = S_ERROR( 'Failed to match the given ClassAds: sym:%s left:%s right"%s' % ( result_s, result_l, result_r ) )
+    result['Value'] = ( result_s, result_l, result_r )
     return result
