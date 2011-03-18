94b64a9712f7507400b6aaa3d89a699c60e473b2
Ricardo Graciani <graciani@ecm.ub.es>
2011-03-18 12:13:43 +0000

    reformat
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@36053 4525493e-7705-40b1-a816-d608a930855b

--- Core/LCG/SAMResultsClient.py
+++ Core/LCG/SAMResultsClient.py
@@ -11,10 +11,10 @@ import socket
 from DIRAC import S_OK, S_ERROR
 
 class SAMResultsClient:
-  
+
 #############################################################################
 
-  def getStatus(self, granularity, name, siteName, tests = None, timeout = None):
+  def getStatus( self, granularity, name, siteName, tests = None, timeout = None ):
     """  
     Return stats of entity in args
     
@@ -37,26 +37,26 @@ class SAMResultsClient:
       }
     """
 
-    if granularity in ('Site', 'Sites'):
+    if granularity in ( 'Site', 'Sites' ):
       siteName = name
-    elif granularity in ('Resource', 'Resources'):
+    elif granularity in ( 'Resource', 'Resources' ):
       siteName = siteName
-    
+
     if timeout is not None:
-      socket.setdefaulttimeout(timeout)
-    
-    sam = self._curlDownload(granularity, site = siteName, tests = tests)
-    
+      socket.setdefaulttimeout( timeout )
+
+    sam = self._curlDownload( granularity, site = siteName, tests = tests )
+
     if sam is None:
-      return S_OK(None)
-    
-    samStatus = self._xmlParsing(granularity, sam, name, tests)
-    
+      return S_OK( None )
+
+    samStatus = self._xmlParsing( granularity, sam, name, tests )
+
     if samStatus is None or samStatus == {}:
-      return S_OK(None)
-    
-    return S_OK(samStatus)
-    
+      return S_OK( None )
+
+    return S_OK( samStatus )
+
 #############################################################################
 
 #  def getLink(self, name, tests):
@@ -65,16 +65,16 @@ class SAMResultsClient:
 
 #############################################################################
 
-  
-  def _curlDownload(self, granularity, site, tests):
+
+  def _curlDownload( self, granularity, site, tests ):
     """ Download SAM status for entity using the SAM DB programmatic interface
     """
 
     samdbpi_url = "http://lcg-sam.cern.ch:8080/same-pi/"
     # Set your method
-    if granularity in ('Site', 'Sites'):
+    if granularity in ( 'Site', 'Sites' ):
       samdbpi_method = "site_status.jsp?"
-    elif granularity in ('Resource', 'Resources'):
+    elif granularity in ( 'Resource', 'Resources' ):
       samdbpi_method = "service_endpoint_status.jsp?"
     # Set your site
     samdbpi_site = site
@@ -82,76 +82,76 @@ class SAMResultsClient:
     samdbpi_test = ""
     if tests is None:
       samdbpi_test = "&only_ss"
-      
+
     # SAMDB-PI to query
     samdb_ep = samdbpi_url + samdbpi_method + "VO_name=LHCb" + "&Site_name=" + samdbpi_site + samdbpi_test
-    
-    req = urllib2.Request(samdb_ep)
-    samPage = urllib2.urlopen(req)
+
+    req = urllib2.Request( samdb_ep )
+    samPage = urllib2.urlopen( req )
 
     sam = samPage.read()
 
     return sam
-    
+
 #############################################################################
 
-  def _xmlParsing(self, granularity, sam, entity, tests):
+  def _xmlParsing( self, granularity, sam, entity, tests ):
     """ Performs xml parsing from the sam string 
         Returns a dictionary containing status of entity
     """
 
     status = {}
 
-    doc = minidom.parseString(sam)
-  
-    if granularity in ('Site', 'Sites'):
+    doc = minidom.parseString( sam )
+
+    if granularity in ( 'Site', 'Sites' ):
         try:
-          s = doc.getElementsByTagName("status")[0].childNodes
-          status['SiteStatus'] = str(s[0].nodeValue)
+          s = doc.getElementsByTagName( "status" )[0].childNodes
+          status['SiteStatus'] = str( s[0].nodeValue )
         except IndexError:
           return None
-      
-    elif granularity in ('Resource', 'Resources'):
-      
-      services = doc.getElementsByTagName("Service") 
-      
+
+    elif granularity in ( 'Resource', 'Resources' ):
+
+      services = doc.getElementsByTagName( "Service" )
+
       serviceToCheck = None
       for service in services:
-        if service.getAttributeNode("endpoint"):
+        if service.getAttributeNode( "endpoint" ):
           endpoint = service.attributes["endpoint"]
-          res = str(endpoint.value)
+          res = str( endpoint.value )
           if res == entity:
             serviceToCheck = service
             break
-      
+
       if serviceToCheck is None:
-        return S_ERROR("There are no SAM tests for this service")
+        return S_ERROR( "There are no SAM tests for this service" )
 #        raise NoSAMTests
-      
+
       if tests is None or tests == []:
         tests = ['SS']
-      
+
       for test in tests:
-        
-        metrics = serviceToCheck.getElementsByTagName("ServiceMetric")
+
+        metrics = serviceToCheck.getElementsByTagName( "ServiceMetric" )
         metricToCheck = None
-        
+
         for metric in metrics:
-          if metric.getAttributeNode("abbreviation"):
+          if metric.getAttributeNode( "abbreviation" ):
             metricName = metric.attributes["abbreviation"]
-            res = str(metricName.value)
+            res = str( metricName.value )
             if res == test:
               metricToCheck = metric
               break
-        
+
         if metricToCheck is None:
           continue
 
-        s = metricToCheck.getElementsByTagName("status")[0].childNodes
-        status[test] = str(s[0].nodeValue)
-    
+        s = metricToCheck.getElementsByTagName( "status" )[0].childNodes
+        status[test] = str( s[0].nodeValue )
+
     return status
-    
+
 #############################################################################
 #
 #class NoSAMTests(Exception):
@@ -162,5 +162,5 @@ class SAMResultsClient:
 #  
 #  def __str__(self):
 #    return "There are no SAM tests for this service \n" + repr(self.message)
-  
+
 #############################################################################
