de0a25ffe0d1b3a6a893a74407ca68be526f91ea
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2011-03-31 15:55:46 +0000

    Beautifying
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC@37094 4525493e-7705-40b1-a816-d608a930855b

--- WorkloadManagementSystem/Service/WMSUtilities.py
+++ WorkloadManagementSystem/Service/WMSUtilities.py
@@ -9,7 +9,6 @@ __RCSID__ = "$Id$"
 
 from tempfile import mkdtemp
 import shutil, os
-from DIRAC.FrameworkSystem.Client.ProxyManagerClient       import gProxyManager
 from DIRAC.Core.Utilities.Grid import executeGridCommand
 
 from DIRAC import S_OK, S_ERROR, gConfig
@@ -30,55 +29,55 @@ def getPilotOutput( proxy, grid, pilotRef ):
   elif grid == 'gLite':
     cmd = [ 'glite-wms-job-output' ]
   else:
-    return S_ERROR( 'Unknown GRID %s' % grid  )
+    return S_ERROR( 'Unknown GRID %s' % grid )
 
-  cmd.extend( ['--noint','--dir', tmp_dir, pilotRef] )
-  
+  cmd.extend( ['--noint', '--dir', tmp_dir, pilotRef] )
+
   gridEnv = ''
-  setup = gConfig.getValue('/DIRAC/Setup','')
+  setup = gConfig.getValue( '/DIRAC/Setup', '' )
   if setup:
-    instance = gConfig.getValue('/DIRAC/Setups/%s/WorkloadManagement' % setup,'')
+    instance = gConfig.getValue( '/DIRAC/Setups/%s/WorkloadManagement' % setup, '' )
     if instance:
-      gridEnv = gConfig.getValue('/Systems/WorkloadManagement/%s/GridEnv' % instance,'')
-      
-  ret = executeGridCommand( proxy, cmd, gridEnv )  
+      gridEnv = gConfig.getValue( '/Systems/WorkloadManagement/%s/GridEnv' % instance, '' )
+
+  ret = executeGridCommand( proxy, cmd, gridEnv )
   if not ret['OK']:
-    shutil.rmtree(tmp_dir)
+    shutil.rmtree( tmp_dir )
     return ret
 
-  status,output,error = ret['Value']
-  
+  status, output, error = ret['Value']
+
   for errorString in [ 'already retrieved',
                        'Output not yet Ready',
                        'not yet ready',
                        'the status is ABORTED' ]:
     if error.find( errorString ) != -1:
-      shutil.rmtree(tmp_dir)
-      return S_ERROR(error)
+      shutil.rmtree( tmp_dir )
+      return S_ERROR( error )
 
   if status:
-    shutil.rmtree(tmp_dir)
-    return S_ERROR(error)
+    shutil.rmtree( tmp_dir )
+    return S_ERROR( error )
 
   # Get the list of files
 
   # LCG always creates an unique sub-directory
   # gLite does it too now
-  result = executeGridCommand( proxy, ['glite-version'], gridEnv )  
+  result = executeGridCommand( proxy, ['glite-version'], gridEnv )
   if not result['OK']:
-    shutil.rmtree(tmp_dir)
+    shutil.rmtree( tmp_dir )
     return result
-  status,output,error = result['Value']
-  if output.find('3.2') != -1:
-    tmp_dir = os.path.join(tmp_dir,os.listdir(tmp_dir)[0])
+  status, output, error = result['Value']
+  if output.find( '3.2' ) != -1:
+    tmp_dir = os.path.join( tmp_dir, os.listdir( tmp_dir )[0] )
 
   result = S_OK()
   result['FileList'] = outputSandboxFiles
 
   for filename in outputSandboxFiles:
     tmpname = os.path.join( tmp_dir, filename )
-    if os.path.exists(tmpname):
-      myfile = file(tmpname,'r')
+    if os.path.exists( tmpname ):
+      myfile = file( tmpname, 'r' )
       f = myfile.read()
       myfile.close()
     else:
@@ -86,11 +85,11 @@ def getPilotOutput( proxy, grid, pilotRef ):
     # HACK: removed after the current scheme has been in production for at least 1 week
     if filename == 'std.out' and f:
       filename = 'StdOut'
-    if filename == 'std.err' and f: 
+    if filename == 'std.err' and f:
       filename = 'StdErr'
     result[filename] = f
 
-  shutil.rmtree(tmp_dir)
+  shutil.rmtree( tmp_dir )
   return result
 
 ###########################################################################
@@ -103,24 +102,24 @@ def getPilotLoggingInfo( proxy, grid, pilotRef ):
   elif grid == 'gLite':
     cmd = [ 'glite-wms-job-logging-info', '-v', '3' ]
   else:
-    return S_ERROR( 'Unknnown GRID %s' % grid  )
+    return S_ERROR( 'Unknnown GRID %s' % grid )
 
   cmd.extend( ['--noint', pilotRef] )
 
   gridEnv = ''
-  setup = gConfig.getValue('/DIRAC/Setup','')
+  setup = gConfig.getValue( '/DIRAC/Setup', '' )
   if setup:
-    instance = gConfig.getValue('/DIRAC/Setups/%s/WorkloadManagement' % setup,'')
+    instance = gConfig.getValue( '/DIRAC/Setups/%s/WorkloadManagement' % setup, '' )
     if instance:
-      gridEnv = gConfig.getValue('/Systems/WorkloadManagement/%s/GridEnv' % instance,'')
-      
+      gridEnv = gConfig.getValue( '/Systems/WorkloadManagement/%s/GridEnv' % instance, '' )
+
   ret = executeGridCommand( proxy, cmd, gridEnv )
   if not ret['OK']:
     return ret
 
-  status,output,error = ret['Value']
+  status, output, error = ret['Value']
   if status:
-    return S_ERROR(error)
+    return S_ERROR( error )
 
   return S_OK( output )
 
