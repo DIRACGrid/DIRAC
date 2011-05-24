1216287fc59c78521f4080fa5e2aeb9bb225577a
Vincent Bernardoff <vincent.bernardoff@cern.ch>
2011-05-24 12:02:26 +0200

    [RSS] PDP refactoring: first step, putting functions into separate modules.

---
+++ ResourceStatusSystem/PolicySystem/Actions/Alarm_PolType.py
@@ -0,0 +1,66 @@
+"""
+AlarmPolType Actions
+"""
+
+import copy
+
+from DIRAC.ResourceStatusSystem.Utilities.CS import getMailForUser
+
+configModule = __import__("LHCbDIRAC.ResourceStatusSystem.Policy.Configurations",
+                          globals(), locals(), ['*'])
+AssigneeGroups = copy.deepcopy(configModule.AssigneeGroups)
+
+def getUsersToNotify(granularity, setup, siteType = None, serviceType = None,
+                     resourceType = None):
+
+  NOTIF = []
+
+  for ag in AssigneeGroups.keys():
+
+    if setup in AssigneeGroups[ag]['Setup'] \
+          and granularity in AssigneeGroups[ag]['Granularity']:
+      if siteType is not None and siteType not in AssigneeGroups[ag]['SiteType']:
+        continue
+      if serviceType is not None and serviceType not in AssigneeGroups[ag]['ServiceType']:
+        continue
+      if resourceType is not None and resourceType not in AssigneeGroups[ag]['ResourceType']:
+        continue
+      NOTIF.append( {'Users':AssigneeGroups[ag]['Users'],
+                     'Notifications':AssigneeGroups[ag]['Notifications']} )
+
+  return NOTIF
+
+def AlarmPolTypeActions(granularity, name, siteType, serviceType, resourceType, res, nc, setup, rsDB):
+  # raise alarms, right now makes a simple notification
+
+  if res['Action']:
+
+    notif = "%s %s is perceived as" %(granularity, name)
+    notif = notif + " %s. Reason: %s." %(res['Status'], res['Reason'])
+
+    NOTIF_D = getUsersToNotify(granularity,
+                                      setup, siteType,
+                                      serviceType,
+                                      resourceType)
+
+    for notification in NOTIF_D:
+      for user in notification['Users']:
+        if 'Web' in notification['Notifications']:
+          nc.addNotificationForUser(user, notif)
+        if 'Mail' in notification['Notifications']:
+          mailMessage = "Granularity = %s \n" % granularity
+          mailMessage = mailMessage + "Name = %s\n" % name
+          mailMessage = mailMessage + "New perceived status = %s\n" % res['Status']
+          mailMessage = mailMessage + "Reason for status change = %s\n" % res['Reason']
+
+          was = rsDB.getMonitoredsHistory(granularity,
+                                          ['Status', 'Reason', 'DateEffective'],
+                                          name, False, 'DESC', 1)[0]
+
+          mailMessage = mailMessage + "Was in status \"%s\", " % (was[0])
+          mailMessage = mailMessage + "with reason \"%s\", since %s\n" % (was[1], was[2])
+
+          mailMessage = mailMessage + "Setup = %s\n" % setup
+
+          nc.sendMail(getMailForUser(user)['Value'][0],
+                      '%s: %s' % (name, res['Status']), mailMessage)
--- ResourceStatusSystem/PolicySystem/PEP.py
+++ ResourceStatusSystem/PolicySystem/PEP.py
@@ -12,18 +12,18 @@
        c. other....
 """
 
-from DIRAC.ResourceStatusSystem.Utilities.CS import getSetup, \
-    getStorageElementStatus, getOperationMails, getMailForUser
+from DIRAC.ResourceStatusSystem.Utilities.CS import getSetup
 
-from DIRAC.ResourceStatusSystem.Utilities.Utils import where, assignOrRaise
+from DIRAC.ResourceStatusSystem.Utilities.Utils import assignOrRaise
 from DIRAC.ResourceStatusSystem.PolicySystem.Configurations import ValidRes, \
     ValidStatus, ValidSiteType, ValidServiceType, ValidResourceType
 
-from DIRAC.ResourceStatusSystem.Utilities.Exceptions import RSSException, \
+from DIRAC.ResourceStatusSystem.Utilities.Exceptions import \
     InvalidRes, InvalidStatus, InvalidResourceType, InvalidServiceType, InvalidSiteType
 
-import copy
-import time
+from DIRAC.ResourceStatusSystem.PolicySystem.Actions.Resource_PolType import ResourcePolTypeActions
+from DIRAC.ResourceStatusSystem.PolicySystem.Actions.Alarm_PolType    import AlarmPolTypeActions
+from DIRAC.ResourceStatusSystem.PolicySystem.Actions.RealBan_PolType  import RealBanPolTypeActions
 
 class PEP:
 #############################################################################
@@ -96,11 +96,6 @@ class PEP:
 #        pass
 
     self.useNewRes = useNewRes
-
-    configModule = __import__(self.VOExtension+"DIRAC.ResourceStatusSystem.Policy.Configurations",
-                              globals(), locals(), ['*'])
-
-    self.AssigneeGroups = copy.deepcopy(configModule.AssigneeGroups)
 
 #############################################################################
 
@@ -225,18 +220,19 @@ class PEP:
         if self.__realBan == False:
           rsDB.setLastMonitoredCheckTime(self.__granularity, self.__name)
         else:
-          self._ResourcePolTypeActions(resDecisions, res, rsDB, rmDB)
+          ResourcePolTypeActions(self.__granularity, self.__name, resDecisions, res, rsDB, rmDB)
 
       if 'Alarm_PolType' in self.__policyType:
-        self._AlarmPolTypeActions(res, nc, setup, rsDB)
+        AlarmPolTypeActions(self.__granularity, self.__name,
+                            self.__siteType, self.__serviceType, self.__resourceType,
+                            res, nc, setup, rsDB)
 
       if 'RealBan_PolType' in self.__policyType and self.__realBan == True:
-        self._RealBanPolTypeActions(res, da, csAPI, setup)
+        RealBanPolTypeActions(self.__granularity, self.__name, res, da, csAPI, setup)
 
       if 'Collective_PolType' in self.__policyType:
         # do something
         pass
-
 
 #
 #      if res['Action']:
@@ -250,223 +246,3 @@ class PEP:
 #          newPEP.enforce(pdpIn = pdp, rsDBIn = rsDB)
 #        except AttributeError:
 #          pass
-
-
-
-
-#############################################################################
-
-  def _ResourcePolTypeActions(self, resDecisions, res, rsDB, rmDB):
-    # Update the DB
-
-    if res['Action']:
-      if self.__granularity == 'Site':
-        rsDB.setSiteStatus(self.__name, res['Status'], res['Reason'], 'RS_SVC')
-        rsDB.setMonitoredToBeChecked(['Service', 'Resource', 'StorageElement'], 'Site', self.__name)
-
-      elif self.__granularity == 'Service':
-        rsDB.setServiceStatus(self.__name, res['Status'], res['Reason'], 'RS_SVC')
-        rsDB.setMonitoredToBeChecked(['Site', 'Resource', 'StorageElement'], 'Service', self.__name)
-
-      elif self.__granularity == 'Resource':
-        rsDB.setResourceStatus(self.__name, res['Status'], res['Reason'], 'RS_SVC')
-        rsDB.setMonitoredToBeChecked(['Site', 'Service', 'StorageElement'], 'Resource', self.__name)
-
-      elif self.__granularity == 'StorageElement':
-
-#        print "********* CHANGE", self.__name, res['Status'], res['Reason']
-
-        rsDB.setStorageElementStatus(self.__name, res['Status'], res['Reason'], 'RS_SVC')
-        rsDB.setMonitoredToBeChecked(['Site', 'Service', 'Resource'], 'StorageElement', self.__name)
-
-    else:
-      rsDB.setMonitoredReason(self.__granularity, self.__name, res['Reason'], 'RS_SVC')
-
-    rsDB.setLastMonitoredCheckTime(self.__granularity, self.__name)
-
-    for resP in resDecisions['SinglePolicyResults']:
-      if not resP.has_key('OLD'):
-        rmDB.addOrModifyPolicyRes(self.__granularity, self.__name,
-                                  resP['PolicyName'], resP['Status'], resP['Reason'])
-
-    if res.has_key('EndDate'):
-      rsDB.setDateEnd(self.__granularity, self.__name, res['EndDate'])
-
-
-#############################################################################
-
-  def _AlarmPolTypeActions(self, res, nc, setup, rsDB):
-        # raise alarms, right now makes a simple notification
-
-    if res['Action']:
-
-      notif = "%s %s is perceived as" %(self.__granularity, self.__name)
-      notif = notif + " %s. Reason: %s." %(res['Status'], res['Reason'])
-
-      NOTIF_D = self.__getUsersToNotify(self.__granularity,
-                                        setup, self.__siteType,
-                                        self.__serviceType,
-                                        self.__resourceType)
-
-      for notification in NOTIF_D:
-        for user in notification['Users']:
-          if 'Web' in notification['Notifications']:
-            nc.addNotificationForUser(user, notif)
-          if 'Mail' in notification['Notifications']:
-            mailMessage = "Granularity = %s \n" %self.__granularity
-            mailMessage = mailMessage + "Name = %s\n" %self.__name
-            mailMessage = mailMessage + "New perceived status = %s\n" %res['Status']
-            mailMessage = mailMessage + "Reason for status change = %s\n" %res['Reason']
-
-            was = rsDB.getMonitoredsHistory(self.__granularity,
-                                            ['Status', 'Reason', 'DateEffective'],
-                                            self.__name, False, 'DESC', 1)[0]
-
-            mailMessage = mailMessage + "Was in status \"%s\", " %(was[0])
-            mailMessage = mailMessage + "with reason \"%s\", since %s\n" %(was[1], was[2])
-
-            mailMessage = mailMessage + "Setup = %s\n" %setup
-
-            nc.sendMail(getMailForUser(user)['Value'][0],
-                        '%s: %s' %(self.__name, res['Status']), mailMessage)
-
-#          for alarm in Configurations.alarms_list:
-#            nc.updateAlarm(alarmKey = alarm, comment = notif)
-
-
-#############################################################################
-
-  def _RealBanPolTypeActions(self, res, da, csAPI, setup):
-    # implement real ban
-
-    if res['Action']:
-
-      if self.__granularity == 'Site':
-
-        banList = da.getBannedSites()
-        if not banList['OK']:
-          raise RSSException, where(self, self.enforce) + banList['Message']
-        else:
-          banList = banList['Value']
-
-
-        if res['Status'] == 'Banned':
-
-          if self.__name not in banList:
-            banSite = da.banSiteFromMask(self.__name, res['Reason'])
-            if not banSite['OK']:
-              raise RSSException, where(self, self.enforce) + banSite['Message']
-            if 'Production' in setup:
-              address = getOperationMails('Production')['Value']
-            else:
-              address = 'fstagni@cern.ch'
-
-            subject = '%s is banned for %s setup' %(self.__name, setup)
-            body = 'Site %s is removed from site mask for %s ' %(self.__name, setup)
-            body += 'setup by the DIRAC RSS on %s.\n\n' %(time.asctime())
-            body += 'Comment:\n%s' %res['Reason']
-            sendMail = da.sendMail(address,subject,body)
-            if not sendMail['OK']:
-              raise RSSException, where(self, self.enforce) + sendMail['Message']
-
-        else:
-          if self.__name in banList:
-            addSite = da.addSiteInMask(self.__name, res['Reason'])
-            if not addSite['OK']:
-              raise RSSException, where(self, self.enforce) + addSite['Message']
-            if setup == 'LHCb-Production':
-              address = getOperationMails('Production')['Value']
-            else:
-              address = 'fstagni@cern.ch'
-
-            subject = '%s is added in site mask for %s setup' %(self.__name, setup)
-            body = 'Site %s is added to the site mask for %s ' %(self.__name, setup)
-            body += 'setup by the DIRAC RSS on %s.\n\n' %(time.asctime())
-            body += 'Comment:\n%s' %res['Reason']
-            sendMail = da.sendMail(address,subject,body)
-            if not sendMail['OK']:
-              raise RSSException, where(self, self.enforce) + sendMail['Message']
-
-
-      elif self.__granularity == 'StorageElement':
-
-        presentReadStatus = getStorageElementStatus( self.__name, 'ReadAccess')['Value']
-#        presentWriteStatus = getStorageElementStatus( self.__name, 'WriteAccess')['Value']
-
-        if res['Status'] == 'Banned':
-
-          if presentReadStatus != 'InActive':
-            banSE = csAPI.setOption("/Resources/StorageElements/%s/ReadAccess" %(self.__name), "InActive")
-            if not banSE['OK']:
-              raise RSSException, where(self, self.enforce) + banSE['Message']
-            banSE = csAPI.setOption("/Resources/StorageElements/%s/WriteAccess" %(self.__name), "InActive")
-            if not banSE['OK']:
-              raise RSSException, where(self, self.enforce) + banSE['Message']
-            commit = csAPI.commit()
-            if not commit['OK']:
-              raise RSSException, where(self, self.enforce) + commit['Message']
-            if 'Production' in setup:
-              address = getSetup()['Value']
-            else:
-              address = 'fstagni@cern.ch'
-
-            subject = '%s is banned for %s setup' %(self.__name, setup)
-            body = 'SE %s is removed from mask for %s ' %(self.__name, setup)
-            body += 'setup by the DIRAC RSS on %s.\n\n' %(time.asctime())
-            body += 'Comment:\n%s' %res['Reason']
-            sendMail = da.sendMail(address,subject,body)
-            if not sendMail['OK']:
-              raise RSSException, where(self, self.enforce) + sendMail['Message']
-
-        else:
-
-          if presentReadStatus == 'InActive':
-
-            allowSE = csAPI.setOption("/Resources/StorageElements/%s/ReadAccess" %(self.__name), "Active")
-            if not allowSE['OK']:
-              raise RSSException, where(self, self.enforce) + allowSE['Message']
-            allowSE = csAPI.setOption("/Resources/StorageElements/%s/WriteAccess" %(self.__name), "Active")
-            if not allowSE['OK']:
-              raise RSSException, where(self, self.enforce) + allowSE['Message']
-            commit = csAPI.commit()
-            if not commit['OK']:
-              raise RSSException, where(self, self.enforce) + commit['Message']
-            if setup == 'LHCb-Production':
-              address = getSetup()['Value']
-            else:
-              address = 'fstagni@cern.ch'
-
-            subject = '%s is allowed for %s setup' %(self.__name, setup)
-            body = 'SE %s is added to the mask for %s ' %(self.__name, setup)
-            body += 'setup by the DIRAC RSS on %s.\n\n' %(time.asctime())
-            body += 'Comment:\n%s' %res['Reason']
-            sendMail = da.sendMail(address,subject,body)
-            if not sendMail['OK']:
-              raise RSSException, where(self, self.enforce) + sendMail['Message']
-
-
-
-#############################################################################
-
-
-  def __getUsersToNotify(self, granularity, setup, siteType = None, serviceType = None,
-                         resourceType = None):
-
-    NOTIF = []
-
-    for ag in self.AssigneeGroups.keys():
-
-      if setup in self.AssigneeGroups[ag]['Setup'] \
-      and granularity in self.AssigneeGroups[ag]['Granularity']:
-        if siteType is not None and siteType not in self.AssigneeGroups[ag]['SiteType']:
-          continue
-        if serviceType is not None and serviceType not in self.AssigneeGroups[ag]['ServiceType']:
-          continue
-        if resourceType is not None and resourceType not in self.AssigneeGroups[ag]['ResourceType']:
-          continue
-        NOTIF.append( {'Users':self.AssigneeGroups[ag]['Users'],
-                       'Notifications':self.AssigneeGroups[ag]['Notifications']} )
-
-    return NOTIF
-
-#############################################################################
