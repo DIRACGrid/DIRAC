63bf99e6dba79234dfaed78f1c823aab44e860d4
Vincent Roger Yvan Bernardoff <vibernar@lxplus315.cern.ch>
2011-05-30 17:31:27 +0200

    [RSS][rss-config] Updated script to update RSS config. Should be working.

--- ResourceStatusSystem/Utilities/BrowseConfig.py
+++ ResourceStatusSystem/Utilities/BrowseConfig.py
@@ -1,90 +1,43 @@
 """
 Access RSSConfiguration from CS easily.
 """
-from DIRAC import gConfig
-from DIRAC.Core.Utilities.CFG import CFG
-from DIRAC.ConfigurationSystem.Client.CSAPI import CSAPI
-from DIRAC.ResourceStatusSystem.Utilities.Exceptions import RSSException
 
-rssConfigRootPath = "/Operations/RSSConfiguration"
+from DIRAC.ConfigurationSystem.private.Modificator      import Modificator
+import itertools
 
-def getOptions(path):
-  """Gives the options of a CS section in a Python dict with values as
-  lists and converted into integers if needed"""
+class BrowseConfig(Modificator):
 
-  pathdict = gConfig.getOptionsDict(path)
-  try:
-    pathdict = pathdict['Value']
-  except KeyError:
-    raise RSSException, pathdict['Message']
+  rssConfigRootPath = "/Operations/RSSConfiguration"
 
-  for k in pathdict:
-    pathdict[k] = pathdict[k].split(',')
-    try:
-      pathdict[k] = [int(i) for i in pathdict[k]]
+  def getOptionsDict(self, path):
+    """Gives the options of a CS section in a Python dict with values as
+    lists and converted into integers if needed"""
+
+    opts = self.getOptions(path)
+    vals = [self.getValue(path + "/" + o) for o in opts]
+    pathdict = dict(itertools.imap(None, opts, vals))
+
+    for k in pathdict:
+      pathdict[k] = pathdict[k].split(',')
+      try:               pathdict[k] = [int(i) for i in pathdict[k]]
+      except ValueError: pass
       if len(pathdict[k]) == 1:
         pathdict[k] = pathdict[k][0]
-    except ValueError:
-      pass
 
-  return pathdict
+    return pathdict
 
-def getSections(path):
-  """Gives the sections in a CS section as a Python list"""
-  seclist = gConfig.getSections(path)
-  try:
-    return seclist['Value']
-  except KeyError:
-    raise RSSException, seclist['Message']
+  def getDictRootedAt(self, relpath = "", root = rssConfigRootPath):
+    """Gives the configuration rooted at path in a Python dict. The
+    result is a Python dictionnary that reflects the structure of the
+    config file."""
+    def getDictRootedAt(path):
+      retval = {}
+      opts = self.getOptions(path)
+      secs = self.getSections(path)
+      for k in opts:
+        retval[k] = opts[k]
+      for i in secs:
+        retval[i] = getDictRootedAt(path + "/" + i)
+      return retval
 
-def getDictRootedAt(relpath = "", root = rssConfigRootPath):
-  """Gives the configuration rooted at path in a Python dict. The
-  result is a Python dictionnary that reflects the structure of the
-  config file."""
-  def getDictRootedAt(path):
-    retval = {}
-    opts = getOptions(path)
-    secs = getSections(path)
-    for k in opts:
-      retval[k] = opts[k]
-    for i in secs:
-      retval[i] = getDictRootedAt(path + "/" + i)
-    return retval
-
-  return getDictRootedAt(root + "/" + relpath)
-
-def createSection(path):
-  """Create a new section in the CS"""
-  def createNewSection(acc, new):
-    if len(new) == 0: return
-    else:
-      if acc == "":
-        cfg.createNewSection(new[0])
-        return createNewSection(new[0], new[1:])
-      else:
-        cfg.createNewSection(acc + "/" + new[0])
-        return createNewSection(acc + "/" + new[0], new[1:])
-
-  pl = [pe for pe in path.split("/") if pe != ""]
-  cfg = CFG()
-  api = CSAPI()
-
-  createNewSection("", pl)
-  res = api.mergeFromCFG(cfg)
-  if res['OK'] == True:
-    api.commit()
-  else:
-    raise RSSException, "Unable to create new section in config" + res['Message']
-
-def setOption(path, value):
-  return gConfig.setOptionValue(path, value)
-
-if __name__ == "__main__":
-  import sys
-  from DIRAC.Core.Base import Script
-  Script.parseCommandLine(ignoreErrors = True)
-
-  if len(sys.argv) > 1:
-    print getDictRootedAt(sys.argv[1])
-  else:
-    print getDictRootedAt()
+    return getDictRootedAt(root + "/" + relpath)
--- ResourceStatusSystem/scripts/dirac-rss-config.py
+++ ResourceStatusSystem/scripts/dirac-rss-config.py
@@ -7,12 +7,12 @@ import sys
 import os.path
 import cmd
 
-from DIRAC.Core.Base import Script
-Script.parseCommandLine(ignoreErrors = True)
+from DIRAC.Core.Base                                    import Script
+from DIRAC.ConfigurationSystem.Client.ConfigurationData import gConfigurationData
+from DIRAC.Core.DISET.RPCClient                         import RPCClient
+from DIRAC.ResourceStatusSystem.Utilities.BrowseConfig  import BrowseConfig
 
-from DIRAC.ResourceStatusSystem.Utilities.Exceptions import RSSException
-from DIRAC.ResourceStatusSystem.Utilities.BrowseConfig import getSections, getOptions, createSection, \
-    rssConfigRootPath, setOption
+Script.parseCommandLine()
 
 # Invariants:
 # * root does not end with "/" or root is "/"
@@ -20,18 +20,45 @@ from DIRAC.ResourceStatusSystem.Utilities.BrowseConfig import getSections, getOp
 
 class RSSConfigCmd(cmd.Cmd):
 
-  root = rssConfigRootPath
-  prompt = "[cfgedit " + root + " ]% "
-
   def __init__(self):
     cmd.Cmd.__init__(self)
+    self.bc     = BrowseConfig(RPCClient(gConfigurationData.getMasterServer()))
+    self.bc.loadFromRemote()
+    self.bc.loadCredentials()
+    self.connected = True
+    self.root   = BrowseConfig.rssConfigRootPath
+    self.prompt = "[cfgedit " + self.root + " ]% "
+    self.dirty = False
 
   def update_prompt(self):
     self.prompt = "[cfgedit " + self.root + " ]% "
 
+  def do_connect(self, line):
+    """connect
+    Connect to the CS
+    Usage: connect <URL> (Connect to the CS at the specified URL)
+           connect       (Connect to the default CS URL of your config)
+    """
+    if line == "":
+      line = gConfigurationData.getMasterServer()
+    try:
+      self.bc = BrowseConfig(RPCClient(line))
+      self.bc.loadFromRemote()
+      self.bc.loadCredentials()
+      self.connected = True
+    except:
+      self.do_disconnect("")
+
+  def do_disconnect(self, _line):
+    """Disconnect from CS"""
+    self.bc = None
+    self.connected = False
+
   def do_ls(self, line):
-    secs = getSections(self.root)
-    opts = getOptions(self.root)
+    """ls
+    List the sections and options of CS of the current root"""
+    secs = self.bc.getSections(self.root)
+    opts = self.bc.getOptions(self.root)
     if line.startswith("-") and "l" in line:
       for i in secs:
         print '\033[94m' + i + '\033[0m' + "  "
@@ -45,57 +72,99 @@ class RSSConfigCmd(cmd.Cmd):
       print ""
 
   def do_cd(self, line):
+    """cd
+    Go one directory deeper in the CS"""
     # Check if invariant holds
     assert(self.root == "/" or not self.root.endswith("/"))
     assert(self.root.startswith("/"))
-    secs = getSections(self.root)
+    secs = self.bc.getSections(self.root)
     if line == "..":
       self.root = os.path.dirname(self.root)
       self.update_prompt()
     else:
-      if os.path.dirname(line) in secs:
+      if os.path.normpath(line) in secs:
         if self.root == "/":
-          self.root = os.path.normpath(self.root + os.path.dirname(line))
+          self.root = self.root + os.path.normpath(line)
         else:
-          self.root = os.path.normpath(self.root + "/" + os.path.dirname(line))
+          self.root = self.root + "/" + os.path.normpath(line)
         self.update_prompt()
       else:
         print "cd: no such section: " + line
 
   def complete_cd(self, text, _line, _begidx, _endidx):
-    secs = getSections(self.root)
+    secs = self.bc.getSections(self.root)
     return [(s + "/") for s in secs if s.startswith(text)]
 
   def do_cat(self, line):
-    opts = getOptions(self.root)
-    if line in opts:
+    """cat
+    Read the content of an option in the CS"""
+    opts = self.bc.getOptionsDict(self.root)
+
+    if line in opts.keys():
       print opts[line]
     else:
       print "cat: No such option"
 
   def complete_cat(self, text, _line, _begidx, _endidx):
-    opts = getOptions(self.root)
+    opts = self.bc.getOptions(self.root)
     return [o for o in opts if o.startswith(text)]
 
-  def do_less(self, line):
-    return self.do_cat(line)
-
-  def complete_less(self, text, _line, _begidx, _endidx):
-    return self.complete_cat(text, _line, _begidx, _endidx)
+  do_less = do_cat
+  complete_less = complete_cat
 
   def do_mkdir(self, line):
-    """Create a new section in the CS"""
-    try:
-      createSection(self.root + "/" + line)
-    except RSSException:
-      print "Unable to create section \"" + line + "\": Not allowed"
+    """mkdir
+    Create a new section in the CS"""
+    self.bc.createSection(self.root + "/" + line)
+    self.dirty = True
+
+  complete_mkdir = complete_cd
+
+  def do_rmdir(self, line):
+    """rmdir
+    Delete a section in the CS"""
+    self.bc.removeSection(self.root + "/" + line)
+    self.dirty = True
+
+  complete_rmdir = complete_cd
+
+  def do_rm(self, line):
+    """rm
+    Delete an option in the CS"""
+    self.bc.removeOption(self.root + "/" + line)
+    self.dirty = True
+
+  complete_rm = complete_cat
 
   def do_set(self, line):
-    line = line.split(" ")
+    """set
+    Set an option in the CS (or create it if it does not exists)
+    Usage: set <str> to set a string option (will be stored as a string in CS)
+           set <str>,<str>,... to set a list option (will be stored as a list in CS)
+    """
+    line = line.split(" ", 2)
     if len(line) != 2:
       print "Usage: set <key> <value>"
     else:
-      return setOption(self.root + "/" + line[0], line[1])
+      self.bc.setOptionValue(self.root + "/" + line[0], line[1])
+      self.dirty = True
+
+  complete_set = complete_cat
+
+  def do_unset(self, line):
+    """unset
+    Unset an option in the CS: Making the option equal to the
+    empty string."""
+    self.bc.setOptionValue(self.root + "/" + line, "")
+    self.dirty = True
+
+  complete_unset = complete_cat
+
+  def do_commit(self, _line):
+    """commit
+    Commit the modifications to the CS"""
+    if self.dirty:
+      self.bc.commit()
 
   def default(self, line):
     """Override [Cmd.default(line)] function."""
@@ -109,12 +178,14 @@ class RSSConfigCmd(cmd.Cmd):
   def do_quit(self, _line):
     """quit
     Quit"""
+    if self.dirty:
+      res = raw_input("Do you want to commit your changes ? [Y/n] ")
+      if res.lower() in ["", "y", "yes"]:
+        self.do_commit()
+
     return True
 
-  def do_exit(self, line):
-    """exit
-    Quit"""
-    return self.do_quit(line)
+  do_exit = do_quit
 
 def main():
   rsscmd = RSSConfigCmd()
