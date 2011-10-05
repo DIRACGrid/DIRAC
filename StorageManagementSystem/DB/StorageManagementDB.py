27b8e06a6b66d3b16a5a7743ad8cb252dd219fd5
Ricardo Graciani <Ricardo.Graciani@gmail.com>
2011-10-05 11:16:38 +0200

    BUGFIX: add RetryIntervalHour Option for the StageMonitorAgent

--- StorageManagementSystem/DB/StorageManagementDB.py
+++ StorageManagementSystem/DB/StorageManagementDB.py
@@ -828,14 +828,22 @@ class StorageManagementDB( DB ):
     gLogger.debug( "StorageManagementDB.setStageComplete: Successfully updated %s StageRequests table with StageStatus=Staged for ReplicaIDs: %s." % ( res['Value'], replicaIDs ) )
     return res
 
-  def wakeupOldRequests( self, replicaIDs , connection = False ):
+  def wakeupOldRequests( self, replicaIDs , retryInterval, connection = False ):
     """
-    get only StageRequests with StageRequestSubmitTime older than 1 day AND are still not staged
+    get only StageRequests with StageRequestSubmitTime older than retryInterval hours AND are still not staged
     delete these requests
     reset Replicas with corresponding ReplicaIDs to Status='New'
     """
+    try:
+      retryInterval = max( retryInterval, 2 )
+      retryInterval = min( retryInterval, 24 )
+      retryInterval = int( retryInterval )
+    except Exception:
+      errorString = 'Wrong argument type'
+      gLogger.exception( errorString )
+      return S_ERROR( errorString )
     if( replicaIDs ) > 0:
-      req = "SELECT ReplicaID FROM StageRequests WHERE ReplicaID IN (%s) AND StageStatus='StageSubmitted' AND DATE_ADD( StageRequestSubmitTime, INTERVAL 1 DAY ) < UTC_TIMESTAMP();" % intListToString( replicaIDs )
+      req = "SELECT ReplicaID FROM StageRequests WHERE ReplicaID IN (%s) AND StageStatus='StageSubmitted' AND DATE_ADD( StageRequestSubmitTime, INTERVAL %s HOUR ) < UTC_TIMESTAMP();" % ( intListToString( replicaIDs ), retryInterval )
       res = self._query( req )
       if not res['OK']:
         gLogger.error( "StorageManagementDB.wakeupOldRequests: Failed to select old StageRequests.", res['Message'] )
