a4d45d815113cfba9df3976e7c97f93067735760
Mario <mario.ubeda.garcia@cern.ch>
2011-10-05 16:01:04 +0200

    Added RSSExceptions to provide finer granularity

--- ResourceStatusSystem/Utilities/Exceptions.py
+++ ResourceStatusSystem/Utilities/Exceptions.py
@@ -6,8 +6,7 @@
 from DIRAC.ResourceStatusSystem import ValidRes, ValidStatus, ValidSiteType, \
     ValidServiceType, ValidResourceType, PolicyTypes
 
-
-#############################################################################
+################################################################################
 
 class RSSException(Exception):
 
@@ -18,7 +17,7 @@ class RSSException(Exception):
   def __str__(self):
     return "Generic Exception in the RSS: \n" + repr(self.message)
 
-#############################################################################
+################################################################################
 
 class InvalidRes(RSSException):
 
@@ -29,7 +28,7 @@ class InvalidRes(RSSException):
   def __str__(self):
     return "Invalid resource type: \nshould be in " + repr(ValidRes) + repr(self.message)
 
-#############################################################################
+################################################################################
 
 class InvalidStatus(RSSException):
 
@@ -40,7 +39,7 @@ class InvalidStatus(RSSException):
   def __str__(self):
     return "Invalid status type: \nshould be in " + repr(ValidStatus) + repr(self.message)
 
-#############################################################################
+################################################################################
 
 class InvalidSiteType(RSSException):
 
@@ -51,7 +50,7 @@ class InvalidSiteType(RSSException):
   def __str__(self):
     return "Invalid resource type: \nshould be in " + repr(ValidSiteType) + repr(self.message)
 
-#############################################################################
+################################################################################
 
 class InvalidServiceType(RSSException):
 
@@ -62,7 +61,7 @@ class InvalidServiceType(RSSException):
   def __str__(self):
     return "Invalid service type: \nshould be in " + repr(ValidServiceType) + repr(self.message)
 
-#############################################################################
+################################################################################
 
 class InvalidResourceType(RSSException):
 
@@ -73,7 +72,7 @@ class InvalidResourceType(RSSException):
   def __str__(self):
     return "Invalid resource type: \nshould be in " + repr(ValidResourceType) + repr(self.message)
 
-#############################################################################
+################################################################################
 
 class InvalidPolicyType(RSSException):
 
@@ -84,7 +83,7 @@ class InvalidPolicyType(RSSException):
   def __str__(self):
     return "Invalid policy type: \nshould be in " + repr(PolicyTypes) + repr(self.message)
 
-#############################################################################
+################################################################################
 
 class InvalidStateValueDict(RSSException):
 
@@ -95,4 +94,49 @@ class InvalidStateValueDict(RSSException):
   def __str__(self):
     return "Invalid dictionnary for the state values. " + repr(self.message)
 
-#############################################################################
+################################################################################
+
+class InvalidSite(RSSException):
+
+  def __init__(self,  message = ""):
+    self.message = message
+    RSSException.__init__(self, message)
+
+  def __str__(self):
+    return "Invalid Site. " + repr(self.message)
+
+################################################################################
+
+class InvalidGridSite(RSSException):
+
+  def __init__(self,  message = ""):
+    self.message = message
+    RSSException.__init__(self, message)
+
+  def __str__(self):
+    return "Invalid GridSite. " + repr(self.message)
+
+################################################################################
+
+class InvalidDate(RSSException):
+
+  def __init__(self,  message = ""):
+    self.message = message
+    RSSException.__init__(self, message)
+
+  def __str__(self):
+    return "Invalid Date. " + repr(self.message)
+
+################################################################################
+
+class InvalidFormat(RSSException):
+
+  def __init__(self,  message = ""):
+    self.message = message
+    RSSException.__init__(self, message)
+
+  def __str__(self):
+    return "Invalid Format. " + repr(self.message)
+  
+################################################################################
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
