3ff67bf30fb05f8e8c1192dcf16e43faf709650a
Mario <mario.ubeda.garcia@cern.ch>
2011-10-19 14:33:00 +0200

    added headUrls, rcsid, initAPIs

--- ResourceStatusSystem/Command/GGUSTickets_Command.py
+++ ResourceStatusSystem/Command/GGUSTickets_Command.py
@@ -1,24 +1,23 @@
-""" The GGUSTickets_Command class is a command class to know about 
-    the number of active present opened tickets
+################################################################################
+# $HeadURL $
+################################################################################
+__RCSID__ = "$Id:  $"
+
+""" 
+  The GGUSTickets_Command class is a command class to know about 
+  the number of active present opened tickets
 """
 
 import urllib2
 
 from DIRAC import gLogger
+from DIRAC.ResourceStatusSystem.Command.knownAPIs import initAPIs
 from DIRAC.Core.Utilities.SitesDIRACGOCDBmapping import getGOCSiteName
 
 from DIRAC.ResourceStatusSystem.Command.Command import *
-#from DIRAC.ResourceStatusSystem.Utilities.Exceptions import RSSException
 
-def callClient(args, clientIn = None):
-
-  if clientIn is not None:
-    c = clientIn
-  else:
-    # use standard GGUSTickets Client
-    from DIRAC.Core.LCG.GGUSTicketsClient import GGUSTicketsClient   
-    c = GGUSTicketsClient()
-    
+def callClient( args, clientIn ):
+   
   name = args[1]
   name = getGOCSiteName(name)
   if not name['OK']:
@@ -26,7 +25,7 @@ def callClient(args, clientIn = None):
   name = name['Value']
   
   try:
-    openTickets = c.getTicketsList(name)
+    openTickets = clientIn.getTicketsList(name)
     if not openTickets['OK']:
       return 'Unknown'
     return openTickets['Value']
@@ -37,51 +36,63 @@ def callClient(args, clientIn = None):
     gLogger.exception("Exception when calling GGUSTicketsClient for " + name)
     return 'Unknown'
     
-        
-#############################################################################
+################################################################################
+################################################################################
 
-class GGUSTickets_Open(Command):
+class GGUSTickets_Open( Command ):
   
-  def doCommand(self):
+  __APISs__ = [ 'GGUSTicketsClient' ]
+  
+  def doCommand( self ):
     """ 
     Return getTicketsList from GGUSTickets Client  
     `args`: 
       - args[0]: string: should be the name of the site
     """
-    super(GGUSTickets_Open, self).doCommand()
-
-    openTickets = callClient(self.args, self.client)
-    if openTickets == 'Unknown':
-      return {'Result':'Unknown'}
     
-    return {'Result': openTickets[0]['open']} 
+    super( GGUSTickets_Open, self ).doCommand()
+    self.APIs = initAPIs( self.__APIs__, self.APIs )
+
+    openTickets = callClient( self.args, self.APIs[ 'GGUSTicketsClient' ] )
+    if openTickets == 'Unknown':
+      return { 'Result' : 'Unknown' }
+    
+    return { 'Result' : openTickets[ 0 ][ 'open' ] } 
 
   doCommand.__doc__ = Command.doCommand.__doc__ + doCommand.__doc__
     
-#############################################################################
+################################################################################
+################################################################################
 
 class GGUSTickets_Link(Command):
   
-  def doCommand(self):
+  __APISs__ = [ 'GGUSTicketsClient' ]
+  
+  def doCommand( self ):
     """ 
     Use CallClient to get GGUS link  
 
    :attr:`args`: 
      - args[0]: string: should be the name of the site
     """
-    super(GGUSTickets_Link, self).doCommand()
-
-    openTickets = callClient(self.args, self.client)
-    if openTickets == 'Unknown':
-      return {'GGUS_Link':'Unknown'}
     
-    return {'Result': openTickets[1]}
+    super( GGUSTickets_Link, self ).doCommand()
+    self.APIs = initAPIs( self.__APIs__, self.APIs )
+
+    openTickets = callClient( self.args, self.APIs[ 'GGUSTicketsClient' ] )
+    if openTickets == 'Unknown':
+      return { 'GGUS_Link':'Unknown' }
+    
+    return { 'Result': openTickets[1] }
 
   doCommand.__doc__ = Command.doCommand.__doc__ + doCommand.__doc__
     
-#############################################################################
+################################################################################
+################################################################################
 
 class GGUSTickets_Info(Command):
+  
+  __APISs__ = [ 'GGUSTicketsClient' ]
   
   def doCommand(self):
     """ 
@@ -90,14 +101,27 @@ class GGUSTickets_Info(Command):
    :attr:`args`: 
      - args[0]: string: should be the name of the site
     """
-    super(GGUSTickets_Info, self).doCommand()
-
-    openTickets = callClient(self.args, self.client)
-    if openTickets == 'Unknown':
-      return {'GGUS_Info':'Unknown'}
     
-    return {'Result': openTickets[2]}
+    super( GGUSTickets_Info, self ).doCommand()
+    self.APIs = initAPIs( self.__APIs__, self.APIs )
+
+    openTickets = callClient( self.args, self.APIs[ 'GGUSTicketsClient' ] )
+    if openTickets == 'Unknown':
+      return { 'GGUS_Info' : 'Unknown' }
+    
+    return { 'Result' : openTickets[ 2 ] }
 
   doCommand.__doc__ = Command.doCommand.__doc__ + doCommand.__doc__
     
-#############################################################################
+################################################################################
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #  
+################################################################################
+
+'''
+  HOW DOES THIS WORK.
+    
+    will come soon...
+'''
+
+################################################################################
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
