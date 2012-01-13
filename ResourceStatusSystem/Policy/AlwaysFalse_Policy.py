2d1d83a622ca5e9c245d353c784f477ad353be9f
ubeda <mario.ubeda.garcia@cern.ch>
2012-01-13 16:15:27 +0100

    Policies updated ( commands returning S_OK/S_ERROR forced this update )

--- ResourceStatusSystem/Policy/AlwaysFalse_Policy.py
+++ ResourceStatusSystem/Policy/AlwaysFalse_Policy.py
@@ -22,6 +22,6 @@ class AlwaysFalse_Policy( PolicyBase ):
         }
     """
 
-    return {'Status':"Active", 'Reason':'AlwaysFalse_Policy'}
+    return { 'Status' : 'Active', 'Reason' : 'This is the AlwasyFalse policy' }
 
   evaluate.__doc__ = PolicyBase.evaluate.__doc__ + evaluate.__doc__
