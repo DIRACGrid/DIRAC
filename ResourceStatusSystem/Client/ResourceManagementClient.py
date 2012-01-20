c2b8be04cf4959b787c22026aa5f1c34bfa8768a
ubeda <mario.ubeda.garcia@cern.ch>
2012-01-20 09:23:18 +0100

    SystemExit exception catched

--- ResourceStatusSystem/Client/ResourceManagementClient.py
+++ ResourceStatusSystem/Client/ResourceManagementClient.py
@@ -48,7 +48,7 @@ class ResourceManagementClient:
     if not serviceIn:
       try:
         self.gate = ResourceManagementDB()
-      except:
+      except SystemExit:
         self.gate = RPCClient( "ResourceStatus/ResourceManagement" )        
     else:
       self.gate = serviceIn    
