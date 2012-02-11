4d5e170dd61f3dafb83099aa58dd9684b64b34fc
Andrei Tsaregorodtsev <atsareg@gmail.com>
2012-02-11 16:49:35 +0100

    Moved ResolveCatalog utilities to the main class module

--- Resources/Catalog/LcgFileCatalogCombinedClient.py
+++ Resources/Catalog/LcgFileCatalogCombinedClient.py
@@ -4,8 +4,60 @@ import DIRAC
 from DIRAC                                              import gLogger, gConfig, S_OK, S_ERROR
 from DIRAC.Resources.Catalog.LcgFileCatalogClient       import LcgFileCatalogClient
 from DIRAC.Core.Utilities.Subprocess                    import pythonCall
-from DIRAC.Core.Utilities.ResolveCatalog                import getLocationOrderedCatalogs
+from DIRAC.Core.Utilities.CountryMapping                import getCountryMappingTier1
+from DIRAC.Core.Utilities.List                          import randomize
 import random, time,os
+
+#######################################################################################
+#
+# Utility functions 
+#
+def getActiveCatalogs():
+  res = gConfig.getSections( "/Resources/FileCatalogs/LcgFileCatalogCombined" )
+  if not res['OK']:
+    gLogger.error( "Failed to get Active Catalogs", "%s" % res['Message'] )
+    return res
+  readDict = {}
+  for site in res['Value']:
+    res = gConfig.getOptionsDict( "/Resources/FileCatalogs/LcgFileCatalogCombined/%s" % site )
+    if not res['OK']:
+      gLogger.error( "Failed to get Tier1 catalog options", "%s %s" % ( site, res['Message'] ) )
+      continue
+    siteDict = res['Value']
+    if siteDict['Status'] == 'Active':
+      readDict[site] = siteDict['ReadOnly']
+  return S_OK( readDict )
+
+def getLocationOrderedCatalogs( siteName = '' ):
+  # First get a list of the active catalogs and their location
+  res = getActiveCatalogs()
+  if not res['OK']:
+    gLogger.error( "Failed to get list of active catalogs", res['Message'] )
+    return res
+  catalogDict = res['Value']
+  # Get the tier1 associated to the current location
+  if not siteName:
+    import DIRAC
+    siteName = DIRAC.siteName()
+  countryCode = siteName.split( '.' )[-1]
+  res = getCountryMappingTier1( countryCode )
+  if not res['OK']:
+    gLogger.error( "Failed to resolve closest Tier1", res['Message'] )
+    return res
+  tier1 = res['Value']
+  # Create a sorted list of the active readonly catalogs
+  catalogList = []
+  if catalogDict.has_key( tier1 ):
+    catalogList.append( catalogDict[tier1] )
+    catalogDict.pop( tier1 )
+  for catalogURL in randomize( catalogDict.values() ):
+    catalogList.append( catalogURL )
+  return S_OK( catalogList )
+
+##############################################################################################
+#
+#  Now the main class
+#
 
 class LcgFileCatalogCombinedClient:
 
