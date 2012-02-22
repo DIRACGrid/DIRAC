d8669f567b41e36273fbd55b00891efadbf606b3
ubeda <mario.ubeda.garcia@cern.ch>
2012-02-22 15:25:30 +0100

    FIX: removed LongestCommonSubstring

--- ResourceStatusSystem/Utilities/Utils.py
+++ ResourceStatusSystem/Utilities/Utils.py
@@ -166,21 +166,6 @@ def typedobj_of_string(s):
 
 # String utils
 
-# http://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Longest_common_substring#Python
-def LongestCommonSubstring(S1, S2):
-  M = [[0]*(1+len(S2)) for i in xrange(1+len(S1))]
-  longest, x_longest = 0, 0
-  for x in xrange(1,1+len(S1)):
-    for y in xrange(1,1+len(S2)):
-      if S1[x-1] == S2[y-1]:
-        M[x][y] = M[x-1][y-1] + 1
-        if M[x][y]>longest:
-          longest = M[x][y]
-          x_longest  = x
-      else:
-        M[x][y] = 0
-  return S1[x_longest-longest: x_longest]
-
 def CountCommonLetters(S1, S2):
   count = 0
   target = S2
