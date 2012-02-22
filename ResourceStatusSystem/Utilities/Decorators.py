8600b58a2d62c83c98396a23baae0b9a3551fd94
ubeda <mario.ubeda.garcia@cern.ch>
2012-02-22 16:24:48 +0100

    FIX: removed HandlerDecCredentials decorator

--- ResourceStatusSystem/Utilities/Decorators.py
+++ ResourceStatusSystem/Utilities/Decorators.py
@@ -77,58 +77,6 @@ class HandlerDec( BaseDec ):
       return S_ERROR( x )
     
     return resQuery   
-
-################################################################################
-
-class HandlerDecCredentials( object ):
-  
-  def __init__( self, f, *args, **kwargs ):
-    self.f           = f
-    self.db          = None
-    self.credentials = None
-    
-  def processArgs( self, *args ):
-    
-    _db, _credentials = self.f( *args )
-    
-    self.db          = _db
-    self.credentials = _credentials  
-    
-  def __get__( self, obj, objtype = None ):
-    return types.MethodType( self, obj, objtype )
-    
-  def __call__( self, *args, **kwargs ):  
-      
-    fname = self.f.__name__.replace( 'export_', '' )    
-    if self.db is None:
-      self.processArgs( *args )
-    
-    ins = inspect.getargspec( self.f )    
-    if ins.args[-1] == 'meta':       
-      kwargs = list( args )[ -1 ]
-      args   = tuple( list( args )[ :-1 ] )
-
-    args = tuple( list( args )[ 1:] )[ 0 ] 
-
-    try:
-      dbFunction = getattr( self.db, fname )
-    except Exception, x:
-      gLogger.exception( 'Unable to find function %s \n %s' % ( fname, x ) )
-      return S_ERROR( x )    
-
-    gLogger.info( 'Attempting to %s' % fname )
-
-    gLogger.info( '%s.args: %s'   % ( fname, args )   )  
-    gLogger.info( '%s.kwargs: %s' % ( fname, kwargs ) )
-        
-    try:
-      resQuery = dbFunction( args, kwargs )
-      gLogger.info( 'Done %s' % fname )
-    except Exception, x:
-      gLogger.exception( 'Something went wrong executing %s \n %s' % ( fname, x ) )    
-      return S_ERROR( x )
-    
-    return resQuery   
            
 ################################################################################
 #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF    
