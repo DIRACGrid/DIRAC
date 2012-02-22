07e06d3bb805d78e5d982aa16d9a34d0f57370d7
ubeda <mario.ubeda.garcia@cern.ch>
2012-02-22 16:27:16 +0100

    FIX: removed HandlerDec decorator

--- ResourceStatusSystem/Service/mock/ResourceStatusHandler.py
+++ ResourceStatusSystem/Service/mock/ResourceStatusHandler.py
@@ -1,29 +1,20 @@
 from DIRAC import S_OK
 
-from DIRAC.ResourceStatusSystem.DB.mock.ResourceStatusDB    import ResourceStatusDB
 from DIRAC.ResourceStatusSystem.Service.mock.RequestHandler import RequestHandler
-
-from DIRAC.ResourceStatusSystem.Utilities.Decorators        import HandlerDec
-
-db = ResourceStatusDB()
 
 class ResourceStatusHandler( RequestHandler ):
   
   def __init__( self ):
     pass
   
-  @HandlerDec
   def insert( self, args, kwargs ):
-    return db
+    return S_OK()
   
-  @HandlerDec
   def update( self, args, kwargs ):
-    return db
+    return S_OK()
   
-  @HandlerDec
   def get( self, args, kwargs ):
-    return db
+    return S_OK()
   
-  @HandlerDec
   def delete( self, args, kwargs ):
-    return db
+    return S_OK()
--- ResourceStatusSystem/Utilities/Decorators.py
+++ ResourceStatusSystem/Utilities/Decorators.py
@@ -41,42 +41,6 @@ class ValidateDBTypes( BaseDec ):
     if not isinstance( args[2], dict ):
       return S_ERROR( 'kwargs MUST be a dict, not %s' % type( args[2] ))
     return self.f( *args, **kwargs )
-
-################################################################################
-
-class HandlerDec( BaseDec ):
-    
-  def __call__( self, *args, **kwargs ):  
-      
-    fname = self.f.__name__.replace( 'export_', '' ) 
-    db    = self.f( *args )   
-    
-    ins = inspect.getargspec( self.f )    
-    if ins.args[-1] == 'meta':       
-      kwargs = list( args )[ -1 ]
-      args   = tuple( list( args )[ :-1 ] )
-
-    args = tuple( list( args )[ 1:] )[ 0 ] 
-
-    try:
-      dbFunction = getattr( db, fname )
-    except Exception, x:
-      gLogger.exception( 'Unable to find function %s \n %s' % ( fname, x ) )
-      return S_ERROR( x )    
-
-    gLogger.info( 'Attempting to %s' % fname )
-
-    gLogger.info( '%s.args: %s'   % ( fname, args )   )  
-    gLogger.info( '%s.kwargs: %s' % ( fname, kwargs ) )
-        
-    try:
-      resQuery = dbFunction( args, kwargs )
-      gLogger.info( 'Done %s' % fname )
-    except Exception, x:
-      gLogger.exception( 'Something went wrong executing %s \n %s' % ( fname, x ) )    
-      return S_ERROR( x )
-    
-    return resQuery   
            
 ################################################################################
 #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF    
