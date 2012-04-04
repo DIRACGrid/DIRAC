93f3e954fc474b233f52a346b77b2e2952f8616e
Andrei Tsaregorodtsev <atsareg@gmail.com>
2012-04-04 14:56:11 +0200

    NEW: added getStorageElementStatus in Resources helper CHANGE: Use ResourcesStatus and Resources helpers in the InputDataAgent logic

--- ConfigurationSystem/Client/Helpers/Resources.py
+++ ConfigurationSystem/Client/Helpers/Resources.py
@@ -4,6 +4,8 @@ __RCSID__ = "$Id$"
 from DIRAC                                              import S_OK, S_ERROR, gConfig
 from DIRAC.ConfigurationSystem.Client.Helpers.Path      import cfgPath
 
+import re
+
 gBaseResourcesSection = "/Resources"
 
 def getSiteTier( site ):
@@ -34,6 +36,31 @@ def getSiteGrid( site ):
   if len( sitetuple ) != 3:
     return S_ERROR( 'Wrong Site Name format' )
   return S_OK( sitetuple[0] )
+
+def getStorageElementOptions( seName ):
+  """ Get the CS StorageElementOptions
+  """
+  storageConfigPath = '/Resources/StorageElements/%s' % seName
+  result = gConfig.getOptionsDict( storageConfigPath )
+  
+  print result
+  
+  if not result['OK']:
+    return result
+  options = result['Value']
+  
+  # Help distinguishing storage type
+  diskSE = True
+  tapeSE = False
+  if options.has_key( 'SEType' ):
+    # Type should follow the convention TXDY
+    seType = options['SEType']
+    diskSE = re.search( 'D[1-9]', seType ) != None
+    tapeSE = re.search( 'T[1-9]', seType ) != None
+  options['DiskSE'] = diskSE
+  options['TapeSE'] = tapeSE
+      
+  return S_OK( options )  
 
 def getCatalogPath(catalogName):
   """  Return the configuration path of the description for a a given catalog
