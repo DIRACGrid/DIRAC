4f1a92c69085f1e7adb8355ba647774f2a38b155
ubeda <mario.ubeda.garcia@cern.ch>
2012-04-16 13:32:49 +0200

    NEW: moved up two commands from LHCbDIRAC

---
+++ ResourceStatusSystem/Command/SpaceTokenOccupancyCommand.py
@@ -0,0 +1,47 @@
+# $HeadURL: $
+''' SpaceTokenOccupancyCommand
+  
+  The Command gets information of the SpaceTokenOccupancy from the lcg_utils
+  
+'''
+
+import lcg_util
+
+from DIRAC                                      import S_OK, S_ERROR
+from DIRAC.ResourceStatusSystem.Command.Command import Command
+
+__RCSID__ = '$Id: $'
+
+class SpaceTokenOccupancyCommand( Command ):
+  '''
+  Uses lcg_util to query status of endpoint for a given token.
+  ''' 
+
+  def doCommand( self ):
+    '''
+    Run the command.
+    '''
+    super( SpaceTokenOccupancyCommand, self ).doCommand()
+
+    spaceTokenEndpoint = self.args[ 0 ]
+    spaceToken         = self.args[ 1 ]  
+    
+    occupancy = lcg_util.lcg_stmd( spaceToken, spaceTokenEndpoint, True, 0 )
+           
+    if occupancy[ 0 ] == 0:
+    
+      output     = occupancy[1][0]
+      total      = float( output[ 'totalsize' ] ) / 1e12 # Bytes to Terabytes
+      guaranteed = float( output[ 'guaranteedsize' ] ) / 1e12
+      free       = float( output[ 'unusedsize' ] ) / 1e12
+      
+      res = S_OK( { 'total' : total, 'free' : free, 'guaranteed' : guaranteed } )
+          
+    else:  
+
+      res = S_ERROR( occupancy )
+
+    return { 'Result' : res }
+    
+################################################################################
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
---
+++ ResourceStatusSystem/Command/VOBOXAvailabilityCommand.py
@@ -0,0 +1,50 @@
+# $HeadURL: $
+''' VOBOXAvailabilityCommand
+  
+  The Command pings a service on a vobox.
+  
+'''
+
+from DIRAC                                      import S_OK
+from DIRAC.Core.DISET.RPCClient                 import RPCClient
+from DIRAC.ResourceStatusSystem.Command.Command import Command
+
+__RCSID__ = '$Id: $'
+
+class VOBOXAvailabilityCommand( Command ):
+  '''
+  Given an url pointing to a service on a vobox, use DIRAC ping against it.
+  ''' 
+  
+  __APIs__ = [ 'ResourceManagementClient' ]
+  
+  def doCommand( self ):
+    '''
+    Run the command.
+    '''
+    super( VOBOXAvailabilityCommand, self ).doCommand()
+    
+    serviceURL   = self.args[ 0 ]
+    
+    pinger  = RPCClient( serviceURL )
+    resPing = pinger.ping()
+    
+    if resPing[ 'OK' ]: 
+      
+      serviceUpTime = resPing[ 'Value' ].get( 'service uptime', 0 )
+      machineUpTime = resPing[ 'Value' ].get( 'host uptime', 0 )
+      
+      res = S_OK( 
+                  { 
+                    'serviceUpTime' : serviceUpTime,
+                    'machineUpTime' : machineUpTime
+                   }
+                 )
+    else:
+      
+      res = resPing
+      
+    return { 'Result' : res }       
+    
+################################################################################      
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF  
