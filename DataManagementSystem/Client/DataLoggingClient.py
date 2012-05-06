e60a5ea54596b0c4fc6c6ccc66adab5d03a284c2
ricardo <Ricardo.Graciani@gmail.com>
2012-05-06 07:06:48 +0200

    Added a minimal test

--- DataManagementSystem/Client/DataLoggingClient.py
+++ DataManagementSystem/Client/DataLoggingClient.py
@@ -20,7 +20,56 @@ class DataLoggingClient( Client ):
     :param str url: service URL
     :param useCertificates: flag to use certificates
     """
+    Client.__init__( self )
     self.setServer( 'DataManagement/DataLogging' )
     if url:
       self.setServer( url )
     self.setTimeout( 120 )
+
+def testDataLoggingClient():
+  """ basic test of the module
+  """
+  import os
+  import sys
+  from DIRAC.Core.Base.Script import parseCommandLine
+  from DIRAC import gLogger, S_OK
+  parseCommandLine()
+  gLogger.setLevel( 'VERBOSE' )
+
+  if 'PYTHONOPTIMIZE' in os.environ and os.environ['PYTHONOPTIMIZE']:
+    gLogger.info( 'Unset pyhthon optimization "PYTHONOPTIMIZE"' )
+    sys.exit( 0 )
+
+  gLogger.info( 'Testing DataLoggingClient class...' )
+
+  try:
+
+    result = S_OK()
+
+    dlc = DataLoggingClient()
+
+    gLogger.info( 'DataLoggingClient instantiated' )
+
+    server = dlc.getServer()
+    assert server == 'DataManagement/DataLogging'
+
+    gLogger.info( ' Connecting to ', server )
+
+    timeout = dlc.timeout
+    assert timeout == 120
+
+    result = dlc.ping()
+    assert result['OK']
+
+    gLogger.info( 'Server is alive' )
+
+  except AssertionError, x:
+    if result['OK']:
+      gLogger.error( x )
+      sys.exit( 1 )
+    else:
+      gLogger.info( 'Test OK, but could not connect to server' )
+      gLogger.info( result['Message'] )
+
+if __name__ == '__main__':
+  testDataLoggingClient()
--- Resources/Catalog/LcgFileCatalogProxyClient.py
+++ Resources/Catalog/LcgFileCatalogProxyClient.py
@@ -1,6 +1,10 @@
+########################################################################
+# $HeadURL $
+# File: LcgFileCatalogProxyClient.py
+########################################################################
 """ File catalog client for LCG File Catalog proxy service """
+__RCSID__ = "$Id$"
 
-from DIRAC                                      import gLogger, gConfig, S_OK, S_ERROR
 from DIRAC.Core.Base.Client                     import Client
 
 class LcgFileCatalogProxyClient( Client ):
@@ -11,18 +15,22 @@ class LcgFileCatalogProxyClient( Client ):
     """ Constructor of the LCGFileCatalogProxy client class
     """
     Client.__init__( self )
+    self.method = None
     self.name = 'LFCProxy'
     self.valid = False
     self.setServer( 'DataManagement/LcgFileCatalogProxy' )
     if url:
       self.setServer( url )
     self.setTimeout( 120 )
-    self.valid = self.ping()['OK']
+    self.call = 'ping'
+    self.valid = self.executeRPC()['OK']
 
   def isOK( self ):
+    """ Is the Catalog available?
+    """
     return self.valid
 
-  def getName( self, DN = '' ):
+  def getName( self ):
     """ Get the file catalog type name
     """
     return self.name
@@ -33,5 +41,57 @@ class LcgFileCatalogProxyClient( Client ):
 
   def execute( self, *parms, **kws ):
     """ Magic method dispatcher """
-    self.call = callProxyMethod
+    self.call = 'callProxyMethod'
     return self.executeRPC( self.method, parms, kws )
+
+def testLcgFileCatalogProxyClient():
+  """ basic test of the module
+  """
+  import os
+  import sys
+  import pprint
+  from DIRAC.Core.Base.Script import parseCommandLine
+  from DIRAC import gLogger, S_OK
+  parseCommandLine()
+  gLogger.setLevel( 'VERBOSE' )
+
+  if 'PYTHONOPTIMIZE' in os.environ and os.environ['PYTHONOPTIMIZE']:
+    gLogger.info( 'Unset pyhthon optimization "PYTHONOPTIMIZE"' )
+    sys.exit( 0 )
+
+  gLogger.info( 'Testing LcgFileCatalogProxyClient class...' )
+
+  try:
+
+    result = S_OK()
+
+    lfcpc = LcgFileCatalogProxyClient()
+
+    gLogger.info( 'LcgFileCatalogProxyClient instantiated' )
+
+    server = lfcpc.getServer()
+    assert server == 'DataManagement/LcgFileCatalogProxy'
+
+    gLogger.info( ' Connecting to ', server )
+
+    timeout = lfcpc.timeout
+    assert timeout == 120
+
+    result = lfcpc.listDirectory( '/' )
+    assert result['OK']
+
+    gLogger.info( pprint.pformat( result['Value']['Successful'] ) )
+
+    gLogger.info( 'Server is alive' )
+
+  except AssertionError, x:
+    if result['OK']:
+      gLogger.error( x )
+      sys.exit( 1 )
+    else:
+      gLogger.info( 'Test OK, but could not connect to server' )
+      gLogger.info( result['Message'] )
+
+if __name__ == '__main__':
+  testLcgFileCatalogProxyClient()
+
