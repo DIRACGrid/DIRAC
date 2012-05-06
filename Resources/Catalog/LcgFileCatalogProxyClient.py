dbb1a23c1e72fa738d379ba8dc9082b1eb9dd964
ricardo <Ricardo.Graciani@gmail.com>
2012-05-06 05:57:21 +0200

    Reformat

--- Resources/Catalog/LcgFileCatalogProxyClient.py
+++ Resources/Catalog/LcgFileCatalogProxyClient.py
@@ -8,7 +8,7 @@ class LcgFileCatalogProxyClient:
   """ File catalog client for LCG File Catalog proxy service
   """
 
-  def __init__(self, url = False, useCertificates = False):
+  def __init__( self, url = False, useCertificates = False ):
     """ Constructor of the LCGFileCatalogProxy client class
     """
     self.name = 'LFCProxy'
@@ -17,35 +17,35 @@ class LcgFileCatalogProxyClient:
       if url:
         self.url = url
       else:
-        url = PathFinder.getServiceURL('DataManagement/LcgFileCatalogProxy')
+        url = PathFinder.getServiceURL( 'DataManagement/LcgFileCatalogProxy' )
         if not url:
           return
         self.url = url
-      self.server  = RPCClient(self.url,timeout=120,useCertificates=useCertificates)
-      if not self.server: 
+      self.server = RPCClient( self.url, timeout = 120, useCertificates = useCertificates )
+      if not self.server:
         return
       else:
         self.valid = True
-    except Exception,x:
-      gLogger.exception('Exception while creating connection to LcgFileCatalog proxy server','',x)
+    except Exception, x:
+      gLogger.exception( 'Exception while creating connection to LcgFileCatalog proxy server', '', x )
       return
 
-  def isOK(self):
+  def isOK( self ):
     return self.valid
 
-  def getName(self,DN=''):
+  def getName( self, DN = '' ):
     """ Get the file catalog type name
     """
     return self.name
 
-  def __getattr__(self, name):
+  def __getattr__( self, name ):
     self.call = name
     return self.execute
 
-  def execute(self, *parms, **kws):
+  def execute( self, *parms, **kws ):
     """ Magic method dispatcher """
     try:
-      result = self.server.callProxyMethod(self.call,parms,kws)
-    except Exception,x:
-      return S_ERROR('Exception while calling the server '+str(x))
+      result = self.server.callProxyMethod( self.call, parms, kws )
+    except Exception, x:
+      return S_ERROR( 'Exception while calling the server ' + str( x ) )
     return result
