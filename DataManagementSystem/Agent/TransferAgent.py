63ea190408d0f7fef6b06eedf4d335f791084d21
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2012-05-16 15:46:38 +0200

    TransferAgent: add AcceptableFailedFiles to StrategyHandler to ban FTS channel from scheduling

--- DataManagementSystem/Agent/TransferAgent.py
+++ DataManagementSystem/Agent/TransferAgent.py
@@ -156,9 +156,11 @@ class TransferAgent( RequestAgentBase ):
       SchedulingType = File
      - list of active strategies
       ActiveStrategies = MinimiseTotalWait 
-     - acceptable failure rate
-      AcceptableFailureRate = 75  
-  
+     - acceptable failure rate to ban/unban FST channel 
+      AcceptableFailureRate = 75
+     - acceptable number of dictinct files failed in FTS to ban/unban FTS channel 
+     AcceptableFailedFiles = 5
+
   """
   ## placeholder for ReplicaManager instance
   __replicaManager = None
@@ -321,12 +323,20 @@ class TransferAgent( RequestAgentBase ):
       return S_ERROR( errStr )
     bandwidths = res["Value"] or False
 
+    res = self.transferDB().getCountFileToFTS(  self.__throughputTimescale, "Failed" )
+    if not res["OK"]:
+      errStr = "setupStrategyHandler: Failed to get Failed files counters from TransferDB."
+      self.log.error( errStr, res['Message'] )
+      return S_ERROR( errStr )
+    failedFiles = res["Value"] or False
+
     ## neither channels nor bandwidths 
     if not ( channels and bandwidths ):
       return S_ERROR( "setupStrategyHandler: No active channels found for replication" )
     
     self.strategyHandler().setBandwiths( bandwidths )
     self.strategyHandler().setChannels( channels )
+    self.strategyHandler().setFailedFiles( failedFiles )
     self.strategyHandler().reset()
 
     return S_OK()
@@ -600,8 +610,8 @@ class TransferAgent( RequestAgentBase ):
     ## check request owner
     ownerGroup = requestObj.getAttribute( "OwnerGroup" )
     if ownerGroup["OK"] and ownerGroup["Value"] in self.__ftsDisabledOwnerGroups:
-      self.log.info("excuteFTS: request %s OwnerGroup=%s is not allowed to execute FTS transfer" % ( requestDict["requestName"], 
-                                                                                                     ownerGroup["Value"] ) )
+      self.log.info("excuteFTS: request %s OwnerGroup=%s is banned from FTS" % ( requestDict["requestName"], 
+                                                                                 ownerGroup["Value"] ) )
       return S_OK( False )
 
     ## check request owner
@@ -856,7 +866,7 @@ class TransferAgent( RequestAgentBase ):
       ## set target SEs for this file
       waitingFileTargets = [ targetSE for targetSE in targetSEs if targetSE not in waitingFileReplicas ]
       if not waitingFileTargets:
-        self.log.info( "scheduleFiles: %s is present at all targets, setting its status to 'Done'" % waitingFileLFN )
+        self.log.info( "scheduleFiles: %s is replicated, setting its status to 'Done'" % waitingFileLFN )
         requestObj.setSubRequestFileAttributeValue( index, "transfer", waitingFileLFN, "Status", "Done" )
         continue
         
@@ -872,7 +882,8 @@ class TransferAgent( RequestAgentBase ):
                                                               strategy )
 
       if not tree["OK"] or not tree["Value"]:
-        self.log.error( "scheduleFiles: failed to determine replication tree", tree["Message"] if "Message" in tree else "replication tree is empty")
+        self.log.error( "scheduleFiles: failed to determine replication tree: ", 
+                        tree["Message"] if "Message" in tree else "tree is empty")
         raise StrategyHandlerChannelNotDefined( "FTS" )
       tree = tree["Value"]
       self.log.debug( "scheduleFiles: replicationTree: %s" % tree )
@@ -1068,7 +1079,7 @@ class StrategyHandler( object ):
   source files, their replicas and target storage elements.
   """
 
-  def __init__( self, configSection, bandwidths=None, channels=None ):
+  def __init__( self, configSection, bandwidths=None, channels=None, failedFiles=None ):
     """c'tor
 
     :param self: self reference
@@ -1095,9 +1106,12 @@ class StrategyHandler( object ):
     self.log.debug( "Number of active strategies = %s" % self.numberOfStrategies )
     self.acceptableFailureRate = gConfig.getValue( self.configSection + '/AcceptableFailureRate', 75 )
     self.log.debug( "AcceptableFailureRate = %s" % self.acceptableFailureRate )
-            
+    self.acceptableFailedFiles = gConfig.getValue( self.configSection + "/AcceptableFailedFiles", 5 )
+    self.log.debug( "AcceptableFailedFiles = %s" % self.acceptableFailedFiles )
+
     self.bandwidths = bandwidths
     self.channels = channels
+    self.failedFiles = failedFiles
     self.chosenStrategy = 0
 
     # dispatcher
@@ -1120,6 +1134,14 @@ class StrategyHandler( object ):
     :param self: self reference
     """
     self.chosenStrategy = 0
+
+  def setFailedFiles( self, failedFiles ):
+    """ set the failed FTS files counters
+
+    :param self: self reference
+    :param failedFiles: observed distinct failed files
+    """
+    self.failedFiles = failedFiles
 
   def setBandwiths( self, bandwidths ):
     """ set the bandwidths 
@@ -1366,8 +1388,6 @@ class StrategyHandler( object ):
     siteAncestor = {}              # Maintains the ancestor channel for a site
     primarySources = sourceSEs
 
-    
-
     while destSEs:
       try:
         minTotalTimeToStart = float( "inf" )
@@ -1456,13 +1476,17 @@ class StrategyHandler( object ):
         channelFileSuccess = bandwidth["SuccessfulFiles"]
         channelFileFailed = bandwidth["FailedFiles"]
         attempted = channelFileSuccess + channelFileFailed
+        
 
         successRate = 100.0
         if attempted != 0:
           successRate = 100.0 * ( channelFileSuccess / float( attempted ) )
-        
-        ## success rate too low?, make channel unattractive
-        if successRate < self.acceptableFailureRate:
+    
+        ## get distinct failed files counter
+        distinctFailedFiles = self.failedFiles.get( channelID, 0 )      
+    
+        ## success rate too low and more than acceptable distinct files are affected?, make channel unattractive
+        if ( successRate < self.acceptableFailureRate ) and ( distinctFailedFiles > self.acceptableFailedFiles ):
           timeToStart = float( "inf" ) 
         else:
 
@@ -1474,7 +1498,7 @@ class StrategyHandler( object ):
           if self.schedulingType == "File":
             transferSpeed = bandwidth["Fileput"] 
             waitingTransfers = channelDict["Files"]
-            
+
           if transferSpeed > 0:
             timeToStart = waitingTransfers / float( transferSpeed )
             
