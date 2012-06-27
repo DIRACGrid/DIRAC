b8262241587234341dabf84d94bd9d5c5a4776ac
ubeda <mario.ubeda.garcia@cern.ch>
2012-06-27 10:24:33 +0200

    FIX: new transition sql files

--- ResourceStatusSystem/DB/ResourceStatusDB.sql
+++ ResourceStatusSystem/DB/ResourceStatusDB.sql
@@ -2,17 +2,4 @@
 -- Resource Status database definition
 -- -----------------------------------------------------------
 
-DROP DATABASE IF EXISTS ResourceStatusDB;
-
-CREATE DATABASE ResourceStatusDB;
---
--- Must set passwords for database user by replacing "must_be_set".
---
--- Create user DIRAC
-USE mysql;
-GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON ResourceStatusDB.* TO 'Dirac'@'localhost' IDENTIFIED BY 'must_be_set';
-GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON ResourceStatusDB.* TO 'Dirac'@'%' IDENTIFIED BY 'must_be_set';
-
--- -----------------------------------------------------------
-
 USE ResourceStatusDB;
