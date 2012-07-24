a553c065b7aa75cfd4d2f86a23883759bf37e4d1
ubeda <mario.ubeda.garcia@cern.ch>
2012-07-24 15:26:10 +0200

    FIX: Configurations unittest

---
+++ ResourceStatusSystem/Policy/test/Test_RSS_Policy_Configurations.py
@@ -0,0 +1,47 @@
+# $HeadURL:  $
+''' Test_RSS_Policy_Configurations
+
+'''
+
+import unittest
+
+import DIRAC.ResourceStatusSystem.Policy.Configurations as moduleTested
+
+__RCSID__ = '$Id: $'
+
+################################################################################
+
+class Configurations_TestCase( unittest.TestCase ):
+  
+  def setUp( self ):
+    '''
+    Setup
+    '''
+        
+    self.moduleTested = moduleTested
+    
+  def tearDown( self ):
+    '''
+    TearDown
+    '''
+    del self.moduleTested
+    
+################################################################################
+# Tests
+
+class Configurations_Success( Configurations_TestCase ):
+  
+  def test_policiesMeta( self ):
+    ''' tests that the configuration does not have any funny key
+    '''
+    
+    self.assertEqual( True, hasattr( self.moduleTested, 'policiesMeta' ) )
+    
+    policiesMeta = self.moduleTested.policiesMeta
+    
+    for _policyName, policyMeta in policiesMeta.items():
+      
+      self.assertEquals( [ 'args', 'command', 'description', 'module' ], policyMeta.keys() )
+    
+################################################################################
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
