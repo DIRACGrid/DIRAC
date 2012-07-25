162df4cba9401ca3d3454d83d216953141b1d046
ubeda <mario.ubeda.garcia@cern.ch>
2012-07-25 08:20:29 +0200

    FIX: last unittests for DTPolicy

--- ResourceStatusSystem/Policy/test/Test_RSS_Policy_DTPolicy.py
+++ ResourceStatusSystem/Policy/test/Test_RSS_Policy_DTPolicy.py
@@ -46,7 +46,6 @@ class DTPolicy_Success( DTPolicy_TestCase ):
     
     policy = self.testClass()
     res = policy.evaluate()
-    
     self.assertEqual( False, res[ 'OK' ] )
     
     # command mock
@@ -56,9 +55,48 @@ class DTPolicy_Success( DTPolicy_TestCase ):
     policy.command = mock_command
     
     res = policy.evaluate()
-    
     self.assertEqual( False, res[ 'OK' ] )
     self.assertEqual( 'Grumpy command', res[ 'Message' ] )
+    
+    # command mock
+    mock_command.doCommand.return_value = { 'OK' : True, 'Value' : None }
+    policy.command = mock_command
+    
+    res = policy.evaluate()
+    self.assertEqual( True, res[ 'OK' ] )
+    self.assertEqual( 'Active', res[ 'Value' ][ 'Status' ] )
+    self.assertEqual( 'No DownTime announced', res[ 'Value' ][ 'Reason' ] )
+    self.assertEqual( False, 'EndDate' in res[ 'Value' ] )
+    
+    # command mock
+    mock_command.doCommand.return_value = { 'OK' : True, 'Value' : { 'DT'      : 'OUTAGE',
+                                                                     'EndDate' : 'Y' } }
+    policy.command = mock_command
+    
+    res = policy.evaluate()
+    self.assertEqual( True, res[ 'OK' ] )
+    self.assertEqual( 'Banned', res[ 'Value' ][ 'Status' ] )
+    self.assertEqual( 'DownTime found: OUTAGE', res[ 'Value' ][ 'Reason' ] )
+    self.assertEqual( 'Y', res[ 'Value' ][ 'EndDate' ] )
+
+    # command mock
+    mock_command.doCommand.return_value = { 'OK' : True, 'Value' : { 'DT'      : 'WARNING',
+                                                                     'EndDate' : 'Y' } }
+    policy.command = mock_command
+    
+    res = policy.evaluate()
+    self.assertEqual( True, res[ 'OK' ] )
+    self.assertEqual( 'Bad', res[ 'Value' ][ 'Status' ] )
+    self.assertEqual( 'DownTime found: WARNING', res[ 'Value' ][ 'Reason' ] )
+    self.assertEqual( 'Y', res[ 'Value' ][ 'EndDate' ] )
+
+    # command mock
+    mock_command.doCommand.return_value = { 'OK' : True, 'Value' : { 'DT'      : 'XYZ',
+                                                                     'EndDate' : 'Y' } }
+    policy.command = mock_command
+    
+    res = policy.evaluate()
+    self.assertEqual( False, res[ 'OK' ] )
         
 ################################################################################
 #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
