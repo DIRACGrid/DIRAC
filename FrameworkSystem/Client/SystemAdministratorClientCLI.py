65a5fae516df073c03b3f133ff377dcf2f7b6141
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2012-07-31 16:57:20 +0200

    Revert back previous change

--- FrameworkSystem/Client/SystemAdministratorClientCLI.py
+++ FrameworkSystem/Client/SystemAdministratorClientCLI.py
@@ -417,9 +417,6 @@ class SystemAdministratorClientCLI( cmd.Cmd ):
       system = argss[0]
       del argss[0]
       component = argss[0]
-      if '_' in component:
-        print "ERROR: component name can not contain '_' character"
-        return 
       del argss[0]
       
       specialOptions = {}
