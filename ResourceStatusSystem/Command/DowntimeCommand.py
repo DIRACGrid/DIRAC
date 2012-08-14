79034e54252236fc69aac6bb5e0d018f15564b64
ubeda <mario.ubeda.garcia@cern.ch>
2012-08-14 14:25:55 +0200

    FIX: DowntimeCommand as MasterCommand

--- ResourceStatusSystem/Command/DowntimeCommand.py
+++ ResourceStatusSystem/Command/DowntimeCommand.py
@@ -3,13 +3,166 @@
 
 '''
 
-from DIRAC                                       import S_OK
-from DIRAC.Core.Utilities.SitesDIRACGOCDBmapping import getGOCSiteName, getDIRACSiteName
-from DIRAC.Core.LCG.GOCDBClient                  import GOCDBClient
-from DIRAC.ResourceStatusSystem.Command.Command  import Command
-from DIRAC.ResourceStatusSystem.Utilities        import CSHelpers
+from DIRAC                                                      import S_OK, S_ERROR
+from DIRAC.Core.Utilities.SitesDIRACGOCDBmapping                import getGOCSiteName, getDIRACSiteName
+from DIRAC.Core.LCG.GOCDBClient                                 import GOCDBClient
+from DIRAC.ResourceStatusSystem.Client.ResourceManagementClient import ResourceManagementClient
+from DIRAC.ResourceStatusSystem.Command.Command                 import Command
+from DIRAC.ResourceStatusSystem.Utilities                       import CSHelpers
 
 __RCSID__ = '$Id:  $'
+
+class DowntimeCommand( Command ):
+  '''
+    As the API provided by GOCDB is incomplete, this command will look little
+    bit terrible
+  '''
+
+  def __init__( self, args = None, clients = None ):
+    
+    super( DowntimeCommand, self ).__init__( args, clients )
+
+    if 'GOCDBClient' in self.apis:
+      self.gClient = self.apis[ 'GOCDBClient' ]
+    else:
+      self.gClient = GOCDBClient() 
+
+    if 'ResourceManagementClient' in self.apis:
+      self.rmClient = self.apis[ 'ResourceManagementClient' ]
+    else:
+      self.rmClient = ResourceManagementClient()
+      
+  def _storeCommand( self, result ):
+
+    for dt in result:
+      
+      resQuery = self.rmClient.addOrModifyDowntimeCache( dt[ 'DowntimeID' ], 
+                                                         dt[ 'Element' ], 
+                                                         dt[ 'Name' ], 
+                                                         dt[ 'StartDate' ], 
+                                                         dt[ 'EndDate' ], 
+                                                         dt[ 'Severity' ], 
+                                                         dt[ 'Description' ], 
+                                                         dt[ 'Link' ] )  
+      if not resQuery[ 'OK' ]:
+        return resQuery
+    return S_OK()
+  
+  def _prepareCommand( self ):
+    
+    if 'name' not in self.args:
+      return S_ERROR( '"name" not found in self.args' )
+    name = self.args[ 'name' ]      
+    
+    if 'element' not in self.args:
+      return S_ERROR( '"element" not found in self.args' )
+    element = self.args[ 'element' ]
+    
+    if not element in [ 'Site', 'Resource' ]:
+      return S_ERROR( 'element is not Site nor Resource' )
+       
+    return S_OK( ( element, name ) )
+
+  def doNew( self, masterParams = None ):
+    
+    if masterParams is not None:
+      element, elementNames = masterParams
+      
+    else:
+      params = self._prepareCommand()
+      if not params[ 'OK' ]:
+        return params
+      element, elementNames = params[ 'Value' ]
+      
+    if element == 'Site':
+      
+      if not isinstance( elementNames, list ):
+        elementNames = [ elementNames ]
+      
+      gocSites = []
+        
+      for siteName in elementNames:
+        gocSite = getGOCSiteName( siteName )      
+        if not gocSite[ 'OK' ]:
+          #FIXME: not all sites are in GOC, only LCG sites. 
+          #We have to filter them somehow.
+          continue
+          #return gocSite
+        gocSites.append( gocSite[ 'Value' ] )        
+      
+      elementNames = gocSites  
+        
+    results = self.gClient.getStatus( element, elementNames, None, 120 )        
+    if not results[ 'OK' ]:
+      return results
+    results = results[ 'Value' ]
+  
+    uniformResult = [] 
+
+    for downtime, downDic in results.items():
+
+      dt                  = {}
+      dt[ 'DowntimeID' ]  = downtime
+      dt[ 'Element' ]     = element
+      dt[ 'StartDate' ]   = downDic[ 'FORMATED_START_DATE' ]
+      dt[ 'EndDate' ]     = downDic[ 'FORMATED_END_DATE' ]
+      dt[ 'Severity' ]    = downDic[ 'SEVERITY' ]
+      dt[ 'Description' ] = downDic[ 'DESCRIPTION' ].replace( '\'', '' )
+      dt[ 'Link' ]        = downDic[ 'GOCDB_PORTAL_URL' ]
+      if element == 'Resource':
+        dt[ 'Name' ]        = downDic[ 'HOSTNAME' ]
+      else:
+        dt[ 'Name' ] = downDic[ 'SITENAME' ]
+      
+      uniformResult.append( dt )  
+          
+    storeRes = self._storeCommand( uniformResult )
+    if not storeRes[ 'OK' ]:
+      return storeRes
+           
+    return S_OK( uniformResult )            
+
+  def doCache( self ):
+
+    params = self._prepareCommand()
+    if not params[ 'OK' ]:
+      return params
+    element, elementNames = params[ 'Value' ]
+    
+    result = self.rmClient.selectDowntimeCache( element, elementNames )  
+    if result[ 'OK' ]:
+      result = S_OK( dict( zip( result[ 'Columns' ], result[ 'Value' ] ) ) )
+           
+    return result          
+
+  def doMaster( self ):
+    
+    sites = CSHelpers.getSites()
+    if not sites[ 'OK' ]:
+      return sites
+    sites = sites[ 'Value' ]
+    
+    resources = CSHelpers.getResources()
+    if not resources[ 'OK' ]:
+      return resources
+    resources = resources[ 'Value' ]
+    
+    siteRes = self.doNew( 'Site', sites )
+    if not siteRes[ 'OK' ]:
+      self.metrics[ 'failed' ].append( siteRes[ 'Message' ] )
+
+    resourceRes = self.doNew( 'Resources', resources )
+    if not resourceRes[ 'OK' ]:
+      self.metrics[ 'failed' ].append( resourceRes[ 'Message' ] )
+    
+    return S_OK( self.metrics )
+
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+################################################################################
 
 class DowntimeSitesCommand( Command ):
 
