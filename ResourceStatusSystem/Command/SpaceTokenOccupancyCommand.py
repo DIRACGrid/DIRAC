7fac5a8562e83c059f2484eb105c44e5ffea94aa
ubeda <mario.ubeda.garcia@cern.ch>
2012-08-15 11:46:52 +0200

    FIX: logged csv instead of list

--- ResourceStatusSystem/Command/SpaceTokenOccupancyCommand.py
+++ ResourceStatusSystem/Command/SpaceTokenOccupancyCommand.py
@@ -155,7 +155,7 @@ class SpaceTokenOccupancyCommand( Command ):
 #
 #    elementsToQuery = list( set( elementsToCheck ).difference( set( resQuery ) ) )
     
-    gLogger.info( 'Processing %s' % elementsToCheck )
+    gLogger.info( 'Processing %s' % ( ', '.join( elementsToCheck ) ) )
     
     for elementToQuery in elementsToCheck:
 
