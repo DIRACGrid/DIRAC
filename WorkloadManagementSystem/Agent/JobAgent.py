efc7d58d98ec13950bd99d41c76438518899e547
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2012-08-25 23:42:19 +0200

    Use __finish when stopping after several empty matcher queries

--- WorkloadManagementSystem/Agent/JobAgent.py
+++ WorkloadManagementSystem/Agent/JobAgent.py
@@ -151,13 +151,13 @@ class JobAgent( AgentModule ):
         self.log.info( 'Job request OK: %s' % ( jobRequest['Message'] ) )
         self.matchFailedCount += 1
         if self.matchFailedCount > self.stopAfterFailedMatches:
-          return S_ERROR( 'Nothing to do for more than %d cycles' %  self.stopAfterFailedMatches )
+          return self.__finish( 'Nothing to do for more than %d cycles' %  self.stopAfterFailedMatches )
         return S_OK( jobRequest['Message'] )
       elif jobRequest['Message'].find( "seconds timeout" ) != -1:
         self.log.error( jobRequest['Message'] )
         self.matchFailedCount += 1
         if self.matchFailedCount > self.stopAfterFailedMatches:
-          return S_ERROR( 'Nothing to do for more than %d cycles' %  self.stopAfterFailedMatches )
+          return self.__finish( 'Nothing to do for more than %d cycles' %  self.stopAfterFailedMatches )
         return S_OK( jobRequest['Message'] )
       elif jobRequest['Message'].find( "Pilot version does not match" ) != -1 :
         self.log.error( jobRequest['Message'] )
@@ -166,7 +166,7 @@ class JobAgent( AgentModule ):
         self.log.info( 'Failed to get jobs: %s' % ( jobRequest['Message'] ) )
         self.matchFailedCount += 1
         if self.matchFailedCount > self.stopAfterFailedMatches:
-          return S_ERROR( 'Nothing to do for more than %d cycles' %  self.stopAfterFailedMatches )
+          return self.__finish( 'Nothing to do for more than %d cycles' %  self.stopAfterFailedMatches )
         return S_OK( jobRequest['Message'] )
 
     # Reset the Counter
