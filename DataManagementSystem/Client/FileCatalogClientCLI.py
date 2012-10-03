fce509b29e2a82657a5a12d9c54930efb16f0aea
lint <lintao@ihep.ac.cn>
2012-10-03 11:31:19 +0800

    LFN auto completion for rmreplica.

--- DataManagementSystem/Client/FileCatalogClientCLI.py
+++ DataManagementSystem/Client/FileCatalogClientCLI.py
@@ -447,6 +447,23 @@ File Catalog Client $Revision: 1.17 $Date:
         print result['Message']
     except Exception, x:
       print "Error: rmreplica failed with exception: ", x
+
+  def complete_rmreplica(self, text, line, begidx, endidx):
+    result = []
+    args = line.split()
+
+    # the first argument -- LFN.
+    if (1<=len(args)<=2):
+      # If last char is ' ',
+      # this can be a new parameter.
+      if (len(args) == 1) or (len(args)==2 and (not line.endswith(' '))):
+        cur_path = ""
+        if (len(args) == 2):
+          cur_path = args[1]
+        result = self.lfn_dc.parse_text_line(text, cur_path, self.cwd)
+
+    return result
+
     
   def do_rm(self,args):
     """ Remove file from the storage and from the File Catalog
