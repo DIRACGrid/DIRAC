9ba3332d26822b214c44b1f7a767a7791f665f5d
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2012-11-21 21:15:40 +0100

    Moved to use remote control scripts partially

--- Resources/Computing/SSHLSFComputingElement.py
+++ Resources/Computing/SSHLSFComputingElement.py
@@ -15,9 +15,10 @@
 
 __RCSID__ = "$Id$"
 
-from DIRAC.Resources.Computing.ComputingElement          import ComputingElement
+from DIRAC.Resources.Computing.SSHComputingElement       import SSH, SSHComputingElement 
 from DIRAC.Core.Utilities.Subprocess                     import shellCall
 from DIRAC.Core.Utilities.List                           import breakListIntoChunks
+from DIRAC.Core.Utilities.Pfn                            import pfnparse 
 from DIRAC                                               import S_OK, S_ERROR
 from DIRAC                                               import systemCall, rootPath
 from DIRAC                                               import gConfig
@@ -30,147 +31,17 @@ import string, shutil, bz2, base64, tempfile
 CE_NAME = 'SSHLSF'
 MANDATORY_PARAMETERS = [ 'Queue' ]
 
-class SSHLSFComputingElement( ComputingElement ):
+class SSHLSFComputingElement( SSHComputingElement ):
 
   #############################################################################
   def __init__( self, ceUniqueID ):
     """ Standard constructor.
     """
-    ComputingElement.__init__( self, ceUniqueID )
+    SSHComputingElement.__init__( self, ceUniqueID )
 
     self.ceType = CE_NAME
-    self.submittedJobs = 0
+    self.controlScript = 'lsfce'
     self.mandatoryParameters = MANDATORY_PARAMETERS
-
-  #############################################################################
-  def _addCEConfigDefaults( self ):
-    """Method to make sure all necessary Configuration Parameters are defined
-    """
-    # First assure that any global parameters are loaded
-    ComputingElement._addCEConfigDefaults( self )
-    # Now Torque specific ones
-    if 'ExecQueue' not in self.ceParameters:
-      self.ceParameters['ExecQueue'] = self.ceParameters.get( 'Queue', '' )
-
-    if 'SharedArea' not in self.ceParameters:
-      self.ceParameters['SharedArea'] = ''
-
-    if 'BatchOutput' not in self.ceParameters:
-      self.ceParameters['BatchOutput'] = os.path.join( gConfig.getValue( '/LocalSite/InstancePath', rootPath ), 'data' )
-
-    if 'BatchError' not in self.ceParameters:
-      self.ceParameters['BatchError'] = os.path.join( gConfig.getValue( '/LocalSite/InstancePath', rootPath ), 'data' )
-
-    if 'ExecutableArea' not in self.ceParameters:
-      self.ceParameters['ExecutableArea'] = os.path.join( gConfig.getValue( '/LocalSite/InstancePath', rootPath ), 'data' )
-
-
-  def reset( self ):
-
-    self.queue = self.ceParameters['Queue']
-    if 'ExecQueue' not in self.ceParameters or not self.ceParameters['ExecQueue']:
-      self.ceParameters['ExecQueue'] = self.ceParameters.get( 'Queue', '' )
-    self.execQueue = self.ceParameters['ExecQueue']
-    self.log.info( "Using queue: ", self.queue )
-    self.hostname = socket.gethostname()
-    self.sharedArea = self.ceParameters['SharedArea']
-    self.batchOutput = self.ceParameters['BatchOutput']
-    self.batchError = self.ceParameters['BatchError']
-    self.executableArea = self.ceParameters['ExecutableArea']
-    self.submitOptions = ''
-    if 'SubmitOptions' in self.ceParameters:
-      self.submitOptions = self.ceParameters['SubmitOptions']
-    self.removeOutput = True
-    if 'RemoveOutput' in self.ceParameters:
-      if self.ceParameters['RemoveOutput'].lower()  in ['no', 'false', '0']:
-        self.removeOutput = False
-
-  #############################################################################
-  def submitJob( self, executableFile, proxy, numberOfJobs = 1 ):
-    """ Method to submit job
-    """
-
-    self.log.verbose( "Executable file path: %s" % executableFile )
-    if not os.access( executableFile, 5 ):
-      os.chmod( executableFile, 0755 )
-
-    # if no proxy is supplied, the executable can be submitted directly
-    # otherwise a wrapper script is needed to get the proxy to the execution node
-    # The wrapper script makes debugging more complicated and thus it is
-    # recommended to transfer a proxy inside the executable if possible.
-    if proxy:
-      self.log.verbose( 'Setting up proxy for payload' )
-
-      compressedAndEncodedProxy = base64.encodestring( bz2.compress( proxy.dumpAllToString()['Value'] ) ).replace( '\n', '' )
-      compressedAndEncodedExecutable = base64.encodestring( bz2.compress( open( executableFile, "rb" ).read(), 9 ) ).replace( '\n', '' )
-
-      wrapperContent = """#!/usr/bin/env python
-# Wrapper script for executable and proxy
-import os, tempfile, sys, base64, bz2, shutil
-try:
-  workingDirectory = tempfile.mkdtemp( suffix = '_wrapper', prefix= 'LSF_' )
-  os.chdir( workingDirectory )
-  open( 'proxy', "w" ).write(bz2.decompress( base64.decodestring( "%(compressedAndEncodedProxy)s" ) ) )
-  open( '%(executable)s', "w" ).write(bz2.decompress( base64.decodestring( "%(compressedAndEncodedExecutable)s" ) ) )
-  os.chmod('proxy',0600)
-  os.chmod('%(executable)s',0700)
-  os.environ["X509_USER_PROXY"]=os.path.join(workingDirectory, 'proxy')
-except Exception, x:
-  print >> sys.stderr, x
-  sys.exit(-1)
-cmd = "./%(executable)s"
-print 'Executing: ', cmd
-sys.stdout.flush()
-os.system( cmd )
-
-shutil.rmtree( workingDirectory )
-
-""" % { 'compressedAndEncodedProxy': compressedAndEncodedProxy, \
-        'compressedAndEncodedExecutable': compressedAndEncodedExecutable, \
-        'executable': os.path.basename( executableFile ) }
-
-      fd, name = tempfile.mkstemp( suffix = '_wrapper.py', prefix = 'LSF_', dir = os.getcwd() )
-      wrapper = os.fdopen( fd, 'w' )
-      wrapper.write( wrapperContent )
-      wrapper.close()
-
-      submitFile = name
-
-    else: # no proxy
-      submitFile = executableFile
-
-    ssh = SSH( parameters = self.ceParameters )
-    # Copy the executable
-    os.chmod( submitFile, stat.S_IRUSR | stat.S_IXUSR )
-    sFile = os.path.basename( submitFile )
-    result = ssh.scpCall( 100, submitFile, '%s/%s' % ( self.executableArea, os.path.basename( submitFile ) ) )
-    # submit submitFile to the batch system
-    cmd = "i=0; while [ $i -lt %(numberOfJobs)d ]; do bsub -o %(output)s -e %(error)s -q %(queue)s -J DIRACPilot %(submitOptions)s %(executable)s; let i=i+1; done;" % \
-      {'numberOfJobs': numberOfJobs, \
-       'output': self.batchOutput, \
-       'error': self.batchError, \
-       'queue': self.queue, \
-       'submitOptions': self.submitOptions, \
-       'executable': '%s/%s' % ( self.executableArea, os.path.basename( submitFile ) ) }
-
-    self.log.verbose( 'CE submission command: %s' % ( cmd ) )
-
-    result = ssh.sshCall( 100, cmd )
-
-    if not result['OK'] or result['Value'][0] != 0:
-      self.log.warn( '===========> SSHLSF CE result NOT OK' )
-      self.log.debug( result )
-      return S_ERROR( result['Value'] )
-    else:
-      self.log.debug( 'LSF CE result OK' )
-
-    batchIDList = []
-    lines = result['Value'][1].strip().replace( '\r', '' ).split( '\n' )
-    for line in lines:
-      batchIDList.append( line.split( "<" )[1].split( ">" )[0] )
-    self.submittedJobs += 1
-
-    return S_OK( batchIDList )
 
   #############################################################################
   def getCEStatus( self ):
@@ -227,15 +98,16 @@ shutil.rmtree( workingDirectory )
     for jobList in breakListIntoChunks( jobIDList, 100 ):
       jobDict = {}
       for job in jobList:
-        jobNumber = job.split( '.' )[0]
+        result = pfnparse( job )
+        jobNumber = result['Value']['FileName']
         if jobNumber:
           jobDict[jobNumber] = job
 
-      cmd = [ 'bjobs', ' '.join( jobList ) ]
+      jobStamps = jobDict.keys()
+      cmd = [ 'bjobs', ' '.join( jobStamps ) ]
       result = ssh.sshCall( 100, cmd )
       if not result['OK']:
         return result
-
       output = result['Value'][1].replace( '\r', '' )
       lines = output.split( '\n' )
       for job in jobDict:
@@ -254,46 +126,19 @@ shutil.rmtree( workingDirectory )
                 resultDict[jobDict[job]] = 'Waiting'
 
     return S_OK( resultDict )
-
-  def getJobOutput( self, jobID, localDir = None ):
-    """ Get the specified job standard output and error files. If the localDir is provided,
-        the output is returned as file in this directory. Otherwise, the output is returned 
-        as strings. 
+  
+  def _getJobOutputFiles( self, jobID ):
+    """ Get output file names for the specific CE 
     """
-    jobNumber = jobID.split( '.' )[0]
-
-    if not localDir:
-      tempDir = tempfile.mkdtemp()
-    else:
-      tempDir = localDir
-
-    ssh = SSH( parameters = self.ceParameters )
-    result = ssh.scpCall( 200, '%s/%s.out' % ( tempDir, jobID ), '%s/*%s*' % ( self.batchOutput, jobNumber ), upload = False )
+    result = pfnparse( jobID )
     if not result['OK']:
       return result
-    if not os.path.exists( '%s/%s.out' % ( tempDir, jobID ) ):
-      os.system( 'touch %s/%s.out' % ( tempDir, jobID ) )
-    result = ssh.scpCall( 200, '%s/%s.err' % ( tempDir, jobID ), '%s/*%s*' % ( self.batchError, jobNumber ), upload = False )
-    if not result['OK']:
-      return result
-    if not os.path.exists( '%s/%s.err' % ( tempDir, jobID ) ):
-      os.system( 'touch %s/%s.err' % ( tempDir, jobID ) )
+    jobStamp = result['Value']['FileName']
+    host = result['Value']['Host']
 
-    # The result is OK, we can remove the output
-    if self.removeOutput:
-      result = ssh.sshCall( 100, 'rm -f %s/*%s* %s/*%s*' % ( self.batchOutput, jobNumber, self.batchError, jobNumber ) )
+    output = '%s/DIRACPilot.o%s' % ( self.batchOutput, jobStamp )
+    error = '%s/DIRACPilot.e%s' % ( self.batchError, jobStamp )
 
-    if localDir:
-      return S_OK( ( '%s/%s.out' % ( tempDir, jobID ), '%s/%s.err' % ( tempDir, jobID ) ) )
-    else:
-      # Return the output as a string
-      outputFile = open( '%s/%s.out' % ( tempDir, jobID ), 'r' )
-      output = outputFile.read()
-      outputFile.close()
-      outputFile = open( '%s/%s.err' % ( tempDir, jobID ), 'r' )
-      error = outputFile.read()
-      outputFile.close()
-      shutil.rmtree( tempDir )
-      return S_OK( ( output, error ) )
+    return S_OK( (jobStamp,host,output,error) )
 
 #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
