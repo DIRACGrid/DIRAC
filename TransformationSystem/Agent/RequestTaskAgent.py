c45358850324ec46c9511a0cae59cd9c6009fb4c
FedericoStagni <fstagni@cern.ch>
2012-11-28 16:00:14 +0100

    Pylint, and better code in general more easily extensible

--- TransformationSystem/Agent/RequestTaskAgent.py
+++ TransformationSystem/Agent/RequestTaskAgent.py
@@ -1,31 +1,20 @@
-########################################################################
-# $HeadURL$
-########################################################################
-"""  The Request Task Agent takes request tasks created in the transformation database and submits to the request management system. """
-__RCSID__ = "$Id$"
+''' The Request Task Agent takes request tasks created in the transformation database
+    and submits to the request management system
+'''
 
-from DIRAC                                                          import S_OK, S_ERROR, gConfig, gMonitor, gLogger, rootPath
 from DIRAC.TransformationSystem.Agent.TaskManagerAgentBase          import TaskManagerAgentBase
 from DIRAC.TransformationSystem.Client.TaskManager                  import RequestTasks
 
 AGENT_NAME = 'Transformation/RequestTaskAgent'
 
 class RequestTaskAgent( TaskManagerAgentBase ):
-  """ An AgentModule to submit requests tasks
-  """
+  ''' An AgentModule to submit requests tasks
+  '''
+  def __init__( self, agentName, loadName, baseAgentName, properties ):
+    ''' c'tor
+    '''
+    TaskManagerAgentBase.__init__( self, agentName, loadName, baseAgentName, properties )
 
-  #############################################################################
-  def initialize( self ):
-    """ Sets defaults """
-    
-    taskManager = RequestTasks()
-    
-    TaskManagerAgentBase.initialize( self, taskManager = taskManager )
+    self.taskManager = RequestTasks( transClient = self.transClient )
+    self.shifterProxy = 'ProductionManager'
     self.transType = ['Replication', 'Removal']
-
-    # This sets the Default Proxy to used as that defined under 
-    # /Operations/Shifter/ProductionManager
-    # the shifterProxy option in the Configuration can be used to change this default.
-    self.am_setOption( 'shifterProxy', 'ProductionManager' )
-
-    return S_OK()
