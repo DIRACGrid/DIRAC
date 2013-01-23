1e1adda038c422ab054edb2cb62c7aa3ba1767f0
Andrei Tsaregorodtsev <atsareg@diracgrid.org>
2013-01-24 00:04:42 +0100

    Update Resources/Catalog/FileCatalogClient.py

--- Resources/Catalog/FileCatalogClient.py
+++ Resources/Catalog/FileCatalogClient.py
@@ -20,7 +20,6 @@ class FileCatalogClient(Client):
     if url:
       self.setServer(url)
     self.available = False
-    gConfig.setOptionValue( '/DIRAC/Security/UseServerCertificate', 'no' )
 #    res = self.isOK()
 #    if res['OK']:
 #      self.available = res['Value']
