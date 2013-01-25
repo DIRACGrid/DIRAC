c3551202230abbba4af395a7c22008127824b895
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2013-01-25 22:13:05 +0100

    Add optional vo argument

--- Resources/Catalog/FileCatalog.py
+++ Resources/Catalog/FileCatalog.py
@@ -24,7 +24,7 @@ class FileCatalog:
                    'removeFile', 'setReplicaStatus', 'setReplicaHost', 'createDirectory', 'setDirectoryStatus',
                    'removeDirectory', 'removeDataset', 'removeFileFromDataset', 'createDataset']
 
-  def __init__( self, catalogs = [] ):
+  def __init__( self, catalogs = [], vo = None  ):
     """ Default constructor
     """
     self.valid = True
@@ -32,6 +32,7 @@ class FileCatalog:
     self.readCatalogs = []
     self.writeCatalogs = []
     self.rootConfigPath = '/Resources/FileCatalogs'
+    self.vo = vo
 
     if type( catalogs ) in types.StringTypes:
       catalogs = [catalogs]
@@ -209,10 +210,12 @@ class FileCatalog:
     
     # Get the eligible catalogs first
     # First, look in the Operations, if nothing defined look in /Resources for backward compatibility
-    result = getVOfromProxyGroup()
-    if not result['OK']:
-      return result
-    vo = result['Value']
+    vo = self.vo
+    if not vo:
+      result = getVOfromProxyGroup()
+      if not result['OK']:
+        return result
+      vo = result['Value']
     opHelper = Operations( vo = vo )
     result = opHelper.getSections( '/Services/FileCatalogs' )
     fileCatalogs = []
@@ -295,3 +298,4 @@ class FileCatalog:
     """ Create a file catalog object from its name and CS description
     """
     return FileCatalogFactory().createCatalog(catalogName)
+
