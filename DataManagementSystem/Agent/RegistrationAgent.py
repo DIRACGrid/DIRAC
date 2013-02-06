a3407f656da927b31950a6ae26ad79f9d692bf61
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2013-02-06 12:48:59 +0100

    fix __init__

--- DataManagementSystem/Agent/RegistrationAgent.py
+++ DataManagementSystem/Agent/RegistrationAgent.py
@@ -38,14 +38,14 @@ class RegistrationAgent( RequestAgentBase ):
     shifterProxy = DataManager
 
   """
-  def __init__( self, agentName, loadName, baseAgentName = False, properties = dict() ):
+  def __init__( self, *args, **kwargs ):
     """ agen c'tor
 
     :param self: self reference
     """
     self.setRequestType( "register" )
     self.setRequestTask( RegistrationTask )
-    RequestAgentBase.__init__( self, agentName, loadName, baseAgentName, properties )
+    RequestAgentBase.__init__( self, *args, **kwargs )
     self.log.info("%s has been constructed" % agentName  )
     
   
