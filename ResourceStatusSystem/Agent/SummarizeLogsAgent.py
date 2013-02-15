7768ac349ab43ed8c5ea86aa53598dc36e77ed09
ubeda <mario.ubeda.garcia@cern.ch>
2013-02-15 09:55:40 +0100

    FIX: variable should not be a tuple

--- ResourceStatusSystem/Agent/SummarizeLogsAgent.py
+++ ResourceStatusSystem/Agent/SummarizeLogsAgent.py
@@ -116,7 +116,7 @@ class SummarizeLogsAgent( AgentModule ):
       return selectedRes
     selectedRes = selectedRes[ 'Value' ]
 
-    selectedStatus = None,
+    selectedStatus = None
     if selectedRes:
 
       # Get the last selectedRes, which will be the newest one. Each selectedRes
