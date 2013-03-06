ca1c03e86eda35cce35a2d4219d05401e0ab3d12
FedericoStagni <fstagni@cern.ch>
2013-03-06 17:04:33 +0100

    CHANGE: picking transformation list from correct place

--- WorkloadManagementSystem/Client/JobDescription.py
+++ WorkloadManagementSystem/Client/JobDescription.py
@@ -159,8 +159,10 @@ class JobDescription:
     result = self.__checkMaxInputData( [maxInputData] )
     if not result[ 'OK' ]:
       return result
-    result = self.__checkMultiChoiceInDescription( "JobType",
-                                                   Operations().getValue( "JobDescription/AllowedJobTypes", [] ) )
+    transformationTypes = Operations().getValue( "Transformations/DataProcessing", [] )
+    if transformationTypes:
+      transformationTypes = transformationTypes.replace( ' ', '' ).split( ',' )
+    result = self.__checkMultiChoiceInDescription( "JobType", ['User', 'SAM', 'Hospital'] + transformationTypes )
     return S_OK()
 
   def setVar( self, varName, varValue ):
