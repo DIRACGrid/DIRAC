e3a595b7db21feecabd4f8422039efcba22560a2
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2013-03-06 01:07:12 +0100

    Pylint import fixes

--- FrameworkSystem/Agent/SystemLoggingDBCleaner.py
+++ FrameworkSystem/Agent/SystemLoggingDBCleaner.py
@@ -6,8 +6,7 @@ __RCSID__ = "$Id$"
 """
 
 from DIRAC.Core.Base.AgentModule import AgentModule
-from DIRAC  import S_OK, S_ERROR, gConfig
-from DIRAC.ConfigurationSystem.Client.PathFinder import getDatabaseSection
+from DIRAC  import S_OK
 from DIRAC.FrameworkSystem.DB.SystemLoggingDB import SystemLoggingDB
 from DIRAC.Core.Utilities import dateTime, toString, day
 
