c36131fd609e1913a36cdbc6697b58bac29407d9
Philippe Charpentier <Philippe.Charpentier@cern.ch>
2013-04-15 14:38:48 +0200

    CHANGE: clear replica cache when flushing or setting a file in the workdirectory

--- TransformationSystem/Agent/TransformationAgent.py
+++ TransformationSystem/Agent/TransformationAgent.py
@@ -216,7 +216,7 @@ class TransformationAgent( AgentModule, TransformationAgentsUtilities ):
     unusedFiles = len( lfns )
 
     # Check the data is available with replicas
-    res = self.__getDataReplicas( transID, lfns, clients, active = not replicateOrRemove )
+    res = self.__getDataReplicas( transDict, lfns, clients, active = not replicateOrRemove )
     if not res['OK']:
       self._logError( "Failed to get data replicas: %s" % res['Message'],
                        method = "processTransformation", transID = transID )
@@ -326,10 +326,25 @@ class TransformationAgent( AgentModule, TransformationAgentsUtilities ):
 
     return lfns
 
-  def __getDataReplicas( self, transID, lfns, clients, active = True ):
+  def __getDataReplicas( self, transDict, lfns, clients, active = True ):
     """ Get the replicas for the LFNs and check their statuses. It first looks within the cache.
     """
     method = '__getDataReplicas'
+    transID = transDict['TransformationID']
+    clearCacheFile = os.path.join( self.workDirectory, 'ClearCache_%s' % str( transID ) )
+    try:
+      clearCache = os.path.exists( clearCacheFile )
+      if clearCache:
+        os.remove( clearCacheFile )
+    except:
+      pass
+    if clearCache or transDict['Status'] == 'Flush':
+      self._logInfo( "Replica cache cleared", method = method, transID = transID )
+      # We may need to get new replicas
+      self.__clearCacheForTrans( transID )
+    else:
+      # If the cache needs to be cleaned
+      self.__cleanCache()
     startTime = time.time()
     dataReplicas = {}
     lfns.sort()
@@ -390,7 +405,6 @@ class TransformationAgent( AgentModule, TransformationAgentsUtilities ):
       self._logInfo( "Obtained %d replicas from catalog in %.1f seconds" \
                       % ( len( newReplicas ), time.time() - startTime ),
                       method = method, transID = transID )
-    self.__cleanCache()
     return S_OK( dataReplicas )
 
   def __getDataReplicasRM( self, transID, lfns, clients, active = True ):
@@ -425,8 +439,7 @@ class TransformationAgent( AgentModule, TransformationAgentsUtilities ):
     # Create a dictionary containing all the file replicas
     failoverLfns = []
     for lfn, replicaDict in replicas['Successful'].items():
-      ses = replicaDict.keys()
-      for se in ses:
+      for se in replicaDict:
         #### This should definitely be included in the SE definition (i.e. not used for transformations)
         if active and re.search( 'failover', se.lower() ):
           self._logVerbose( "Ignoring failover replica for %s." % lfn, method = method, transID = transID )
@@ -453,20 +466,30 @@ class TransformationAgent( AgentModule, TransformationAgentsUtilities ):
 
   @gSynchro
   def __updateCache( self, transID, newReplicas ):
+    """ Add replicas to the cache
+    """
     self.replicaCache.setdefault( transID, {} )[datetime.datetime.utcnow()] = newReplicas
+
+  @gSynchro
+  def __clearCacheForTrans( self, transID ):
+    """ Remove all replicas for a transformation
+    """
+    self.replicaCache.pop( transID , None )
 
   @gSynchro
   def __cleanCache( self ):
     """ Cleans the cache
     """
+    cacheChanged = False
     try:
       timeLimit = datetime.datetime.utcnow() - datetime.timedelta( days = self.replicaCacheValidity )
-      for transID in self.replicaCache.keys():
-        for updateTime in self.replicaCache[transID].keys():
+      for transID in self.replicaCache:
+        for updateTime in self.replicaCache[transID]:
           if updateTime < timeLimit or not self.replicaCache[transID][updateTime]:
             self._logVerbose( "Clear %d cached replicas for transformation %s" % ( len( self.replicaCache[transID][updateTime] ),
                                                                                     str( transID ) ), method = '__cleanCache' )
             self.replicaCache[transID].pop( updateTime )
+            cacheChanged = True
         # Remove empty transformations
         if not self.replicaCache[transID]:
           self.replicaCache.pop( transID )
@@ -475,7 +498,8 @@ class TransformationAgent( AgentModule, TransformationAgentsUtilities ):
 
     # Write the cache file
     try:
-      self.__writeCache()
+      if cacheChanged:
+        self.__writeCache()
     except Exception:
       self._logException( "While writing replica cache" )
 
