675bff714da6b48a7a709d7192c7ce6d34440b01
MS <matvey.sapunov@gmail.com>
2013-04-25 16:21:56 +0200

    add example

--- FrameworkSystem/scripts/dirac-sys-sendmail.py
+++ FrameworkSystem/scripts/dirac-sys-sendmail.py
@@ -19,8 +19,9 @@
   Options:
     There are no options.
 
-  Example:
+  Examples:
     dirac-sys-sendmail "From: source@email.com\\nTo: destination@email.com\\nSubject: Test\\n\\nMessage body"
+    echo "From: source@email.com\\nSubject: Test\\n\\nMessage body" | dirac-sys-sendmail destination@email.com
 """
 
 __RCSID__ = "$Id$"
