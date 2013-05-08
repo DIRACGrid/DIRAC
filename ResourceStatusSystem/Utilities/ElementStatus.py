597127c5b481348a056851e0687ecdc92b2e0d22
ubeda <mario.ubeda.garcia@cern.ch>
2013-05-08 16:51:48 +0200

    FIX: wrong loop level

--- ResourceStatusSystem/Utilities/ElementStatus.py
+++ ResourceStatusSystem/Utilities/ElementStatus.py
@@ -149,12 +149,11 @@ class ElementStatus( object ):
     
     usableElements = []
     
-    for elementDict in elementStatuses:
-      for elementName, statusDict in elementDict.items():
+    for elementName, statusDict in elementStatuses.items():
         
-        if statusDict[ statusType ] in ( 'Active', 'Degraded' ):
+      if statusDict[ statusType ] in ( 'Active', 'Degraded' ):
         
-          usableElements.append( elementName )
+        usableElements.append( elementName )
     
     return S_OK( usableElements )  
 
