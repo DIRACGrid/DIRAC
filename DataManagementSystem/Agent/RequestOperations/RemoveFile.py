477ab6649a1384fcc64fbccba45be3c371e587fd
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2013-05-23 11:22:57 +0200

    save

--- DataManagementSystem/Agent/RequestOperations/RemoveFile.py
+++ DataManagementSystem/Agent/RequestOperations/RemoveFile.py
@@ -97,6 +97,7 @@ class RemoveFile( BaseOperation ):
     :return: S_ERROR or S_OK( { lfn: opFile, ... } ) -- dict with files still waiting to be removed
     """
     bulkRemoval = self.replicaManager().removeFile( toRemoveDict.keys(), force = True )
+    self.log.always( bulkRemoval )
     if not bulkRemoval["OK"]:
       self.log.error( "unable to remove files: %s" % bulkRemoval["Message"] )
       self.operation.Error = bulkRemoval["Message"]
