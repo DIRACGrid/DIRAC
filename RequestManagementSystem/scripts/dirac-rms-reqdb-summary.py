7e7ee4a4ba221fc14ce2161f2f4dda0958946206
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2013-05-27 13:55:17 +0200

    save

--- RequestManagementSystem/scripts/dirac-rms-reqdb-summary.py
+++ RequestManagementSystem/scripts/dirac-rms-reqdb-summary.py
@@ -27,6 +27,20 @@ if __name__ == "__main__":
     DIRAC.gLogger.info("ReqDB is empty!")
     DIRAC.exit(0)
 
-  print dbSummary
+  reqs = dbSummary.get("Request", {} )
+  ops = dbSummary.get( "Operation", {} )
+  fs = dbSummary.get( "File", {} )
+
+  print opts
+  print fs
+
+  if not reqs:
+    DIRAC.gLogger.always("no requests in DB")
+    DIRAC.exit(0)
+  else:
+    for reqState, reqCount in sorted( reqs.items() ):
+      DIRAC.gLogger.always( "%s Requests with %s status" % ( reqCount, reqState ) )
+    for opState, opCount in sorted( ops.items() ):
+      DIRAC.gLogger( "%s Operations " )
 
 
