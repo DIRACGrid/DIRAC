0ad937863b9a4e3ff79e63808ca9e29f3fbdbfc4
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2013-05-27 11:47:20 +0200

    save

--- RequestManagementSystem/private/RequestTask.py
+++ RequestManagementSystem/private/RequestTask.py
@@ -292,12 +292,7 @@ class RequestTask( object ):
     if not shifter:
       os.unlink( proxyFile )
 
-    self.log.always( "aaaaaaaaaaaaaa %s %s" % ( self.request.Status, self.request.subStatusList() ) )
-
     gMonitor.flush()
-
-    # # just in case allow state propagation again
-    # self.request._notify()
 
     # # request done?
     if self.request.Status == "Done":
