78a408c296b4aeadfb4bdbd2b9e84d4f51fb6361
ubeda <mario.ubeda.garcia@cern.ch>
2013-05-27 11:18:20 +0200

    NEW: PilotsEfficiencyPolicy

---
+++ ResourceStatusSystem/Policy/PilotEfficiencyPolicy.py
@@ -0,0 +1,69 @@
+# $HeadURL: $
+""" PilotEfficiencyPolicy
+
+  ... blah ...
+
+"""
+
+from DIRAC                                              import S_OK
+from DIRAC.ResourceStatusSystem.PolicySystem.PolicyBase import PolicyBase
+
+__RCSID__ = '$Id: $'
+
+class PilotEfficiencyPolicy( PolicyBase ):
+  
+  @staticmethod
+  def _evaluate( commandResult ):
+
+    result = { 
+               'Status' : None,
+               'Reason' : None
+              }
+
+    if not commandResult[ 'OK' ]:
+      result[ 'Status' ] = 'Error'
+      result[ 'Reason' ] = commandResult[ 'Message' ]
+      return S_OK( result )    
+
+    commandResult = commandResult[ 'Value' ]
+
+    if not commandResult:
+      result[ 'Status' ] = 'Unknown'
+      result[ 'Reason' ] = 'No values to take a decision'
+      return S_OK( result )
+    
+    commandResult = commandResult[ 0 ]
+
+    if not commandResult:
+      result[ 'Status' ] = 'Unknown'
+      result[ 'Reason' ] = 'No values to take a decision'
+      return S_OK( result )
+
+    aborted = float( commandResult[ 'Aborted' ] )
+    #deleted = float( commandResult[ 'Deleted' ] )
+    done    = float( commandResult[ 'Done' ] )
+    failed  = float( commandResult[ 'Failed' ] )
+
+    #total     = aborted + deleted + done + failed
+    total     = aborted + done + failed
+
+    #we want a minimum amount of pilots to take a decision ( at least 10 pilots )
+    if total < 10:
+      result[ 'Status' ] = 'Unknown'
+      result[ 'Reason' ] = 'Not enough pilots to take a decision'
+      return S_OK( result )
+    
+    efficiency = done / total
+
+    if efficiency < 0.65:
+      result[ 'Status' ] = 'Banned'
+    elif efficiency < 0.90:
+      result[ 'Status' ] = 'Degraded'  
+    else:   
+      result[ 'Status' ] = 'Active'    
+          
+    result[ 'Reason' ] = 'Pilots Efficiency of %.2f' % efficiency
+    return S_OK( result )
+       
+#...............................................................................
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
