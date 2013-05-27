ae5db1fa73f478dc8fa1e2a8acbb911e8a36c189
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2013-05-27 14:02:19 +0200

    save

--- RequestManagementSystem/scripts/dirac-rms-reqdb-summary.py
+++ RequestManagementSystem/scripts/dirac-rms-reqdb-summary.py
@@ -1,4 +1,4 @@
-#!/bin/env python  
+#!/bin/env python
 """ show ReqDB summary """
 __RCSID__ = "$Id: $"
 
@@ -16,33 +16,32 @@ if __name__ == "__main__":
 
   from DIRAC.RequestManagementSystem.Client.ReqClient import ReqClient
   reqClient = ReqClient()
-  
-  dbSummary = reqClient.getDBSummary() 
+
+  dbSummary = reqClient.getDBSummary()
   if not dbSummary["OK"]:
     DIRAC.gLogger.error( dbSumamry["Message"] )
-    DIRAC.exit(-1)
+    DIRAC.exit( -1 )
 
-  dbSumamry = dbSummary["Value"]
+  dbSummary = dbSummary["Value"]
   if not dbSumamry:
-    DIRAC.gLogger.info("ReqDB is empty!")
-    DIRAC.exit(0)
+    DIRAC.gLogger.info( "ReqDB is empty!" )
+    DIRAC.exit( 0 )
 
   reqs = dbSummary.get( "Request", {} )
   ops = dbSummary.get( "Operation", {} )
   fs = dbSummary.get( "File", {} )
 
-  print dbSummary["Request"]
-  print reqs
-  print ops
-  print fs
-
   if not reqs:
-    DIRAC.gLogger.always("no requests in DB")
-    DIRAC.exit(0)
+    DIRAC.gLogger.always( "no requests in DB" )
+    DIRAC.exit( 0 )
   else:
     for reqState, reqCount in sorted( reqs.items() ):
       DIRAC.gLogger.always( "%s Requests with %s status" % ( reqCount, reqState ) )
-    for opState, opCount in sorted( ops.items() ):
-      DIRAC.gLogger( "%s Operations " )
+    for opType, opDict in sorted( ops ):
+      for opState, opCount in sorted( opDict.items() ):
+        DIRAC.gLogger( "%s Operations of type %s with status %s" % ( opCount, opType, opState ) )
+    for fState, fCount in sorted( fs ):
+      DIRAC.gLogger( "%s Files with status %s" % ( fCount, fState ) )
+
 
 
