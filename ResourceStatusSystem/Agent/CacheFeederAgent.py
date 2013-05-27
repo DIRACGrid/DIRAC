ce6505ee74dac30fcb088e4b2ec7cdf7fbc54a47
ubeda <mario.ubeda.garcia@cern.ch>
2013-05-27 10:34:23 +0200

    FIX: added PilotAgentsDB to CacheFeeder agent

--- ResourceStatusSystem/Agent/CacheFeederAgent.py
+++ ResourceStatusSystem/Agent/CacheFeederAgent.py
@@ -5,7 +5,7 @@
 
 '''
 
-from DIRAC                                                      import S_OK#, S_ERROR, gConfig
+from DIRAC                                                      import S_OK
 from DIRAC.AccountingSystem.Client.ReportsClient                import ReportsClient
 from DIRAC.Core.Base.AgentModule                                import AgentModule
 from DIRAC.Core.DISET.RPCClient                                 import RPCClient
@@ -13,7 +13,7 @@ from DIRAC.Core.LCG.GOCDBClient                                 import GOCDBClie
 from DIRAC.ResourceStatusSystem.Client.ResourceManagementClient import ResourceManagementClient
 from DIRAC.ResourceStatusSystem.Client.ResourceStatusClient     import ResourceStatusClient
 from DIRAC.ResourceStatusSystem.Command                         import CommandCaller
-#from DIRAC.ResourceStatusSystem.Utilities                       import CSHelpers
+from DIRAC.WorkloadManagementSystem.DB.PilotAgentsDB            import PilotAgentsDB
 
 __RCSID__  = '$Id:  $'
 AGENT_NAME = 'ResourceStatus/CacheFeederAgent'
@@ -84,6 +84,7 @@ class CacheFeederAgent( AgentModule ):
     self.clients[ 'ReportsClient' ]            = ReportsClient()
     self.clients[ 'ResourceStatusClient' ]     = ResourceStatusClient()
     self.clients[ 'ResourceManagementClient' ] = ResourceManagementClient()
+    self.clients[ 'PilotsDB' ]                 = PilotAgentsDB()
     self.clients[ 'WMSAdministrator' ]         = RPCClient( 'WorkloadManagement/WMSAdministrator' )
 
     self.cCaller = CommandCaller
