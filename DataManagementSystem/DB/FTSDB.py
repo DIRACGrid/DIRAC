6e9e80f6578a7a3170b026855623175cc8da377b
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2013-06-04 19:40:10 +0200

    save

--- DataManagementSystem/DB/FTSDB.py
+++ DataManagementSystem/DB/FTSDB.py
@@ -266,7 +266,8 @@ class FTSDB( DB ):
 
     ftsJobs = [ FTSJob( ftsJobDict ) for ftsJobDict in ftsJobs  ]
     for ftsJob in ftsJobs:
-      query = "SELECT * FROM `FTSFile` WHERE `FTSGUID` = '%s;'" % ftsJob.FTSGUID
+      query = "SELECT * FROM `FTSFile` WHERE `FTSGUID` = '%s;' AND `RequestID`=%s" % ( ftsJob.FTSGUID,
+                                                                                       requestID )
       ftsFiles = self._transaction( [ query ] )
       if not ftsFiles["OK"]:
         self.log.error( "getFTSJobsForRequest: %s" % ftsFiles["Message"] )
