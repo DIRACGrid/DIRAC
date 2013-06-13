527626c022f12fb8a8ae6193263f8f746ed4d643
Daniela <remenska@gmail.com>
2013-06-13 14:33:23 +0200

    Cache usage per SE (current pins)

--- StorageManagementSystem/scripts/dirac-stager-show-stats.py
+++ StorageManagementSystem/scripts/dirac-stager-show-stats.py
@@ -6,6 +6,7 @@
 ########################################################################
 """
 Reports breakdown of file(s) number/size in different staging states across storage elements 
+Currently used Cache per SE is also reported. (active pins)
 """
 
 __RCSID__ = "$Id$"
@@ -36,14 +37,33 @@ if stagerInfo:
     outStr = "%s %s\n" %(outStr, str(stagerInfo[sid]['SumFiles']).ljust( 20 ))
 else:
   outStr ="%s %s" % (outStr, "Nothing to see here...Bye")
-outStr ="%s %s" % (outStr, "\nWARNING: the Size for files with Status=New is not yet determined at the point of selection!\n")
+outStr ="%s %s" % (outStr, "\nWARNING: the Size for files with Status=New is not yet determined at the point of selection!\n\n")
+outStr ="%s %s" %(outStr,"--------------------- current status of the SE Caches from the DB-----------")
+res = client.getSubmittedStagePins()
+if not res['OK']:
+  print res['Message']
+  DIRACExit(2)
+storageElementUsage = res['Value']
+if storageElementUsage:
+  for storageElement in storageElementUsage.keys():
+    seDict = storageElementUsage[storageElement]
+    seDict['TotalSize'] = seDict['TotalSize'] / ( 1000 * 1000 * 1000.0 )
+    outStr ="%s\n %s: %s replicas with a size of %.3f GB.\n" % (outStr, storageElement.ljust( 15 ), str( seDict['Replicas'] ).rjust( 6 ), seDict['TotalSize'] ) 
+else:
+  outStr ="%s %s" % (outStr, "StageRequest.getStorageUsage: No active stage/pin requests found." )    
 print outStr
 DIRACExit( 0 )
 '''Example:
- dirac-stager-show-stats.py 
+dirac-stager-show-stats.py 
 
  Status               SE                   NumberOfFiles        Size(GB)            
 --------------------------------------------------------------------------
  Staged               GRIDKA-RDST          1                    4.5535              
- StageSubmitted       GRIDKA-RDST          5                    22.586  
- '''      
+ StageSubmitted       GRIDKA-RDST          5                    22.586              
+ Waiting              PIC-RDST             3                    13.6478             
+ 
+WARNING: the Size for files with Status=New is not yet determined at the point of selection!
+
+ --------------------- current status of the SE Caches from the DB-----------
+ GRIDKA-RDST    :      6 replicas with a size of 29.141 GB.
+'''
