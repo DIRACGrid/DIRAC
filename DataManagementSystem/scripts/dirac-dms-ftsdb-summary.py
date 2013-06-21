eda1ad6909279ad83e5d537b95bd77f5de646d7e
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2013-06-21 14:22:25 +0200

    save

--- DataManagementSystem/scripts/dirac-dms-ftsdb-summary.py
+++ DataManagementSystem/scripts/dirac-dms-ftsdb-summary.py
@@ -30,33 +30,33 @@ if __name__ == "__main__":
 
   ftsSites = ret.get( "FTSSite", None )
   if ftsSites:
-    gLogger.info( "[%d] FTSSites:" % ic )
+    gLogger.always( "[%d] FTSSites:" % ic )
     ic += 1
     for i, ftsSite in enumerate( ftsSites ):
-      gLogger.info( "[%02d] FTSSite: %s using %s FTSServer" % ( i, ftsSite["Name"], ftsSite["FTSServer"] ) )
+      gLogger.always( "- %s using FTS server at %s" % ( i, ftsSite["Name"], ftsSite["FTSServer"] ) )
 
   ftsJobs = ret.get( "FTSJob", None )
   if ftsJobs:
-    gLogger.info( "[%d] FTSJobs:" % ic )
+    gLogger.always( "[%d] FTSJobs:" % ic )
     ic += 1
     for status, count in sorted( ftsJobs.items() ):
-       gLogger.info( "- '%s' %s" % ( status, count ) )
+       gLogger.always( "- '%s' %s" % ( status, count ) )
 
   ftsFiles = ret.get( "FTSFile", None )
   if ftsFiles:
-    gLogger.info( "[%d] FTSFiles:" % ic )
+    gLogger.always( "[%d] FTSFiles:" % ic )
     ic += 1
     for status, count in sorted( ftsFiles.items() ):
-      gLogger.info( "- '%s' %s" % ( status, count ) )
+      gLogger.always( "- '%s' %s" % ( status, count ) )
 
   ftsHistory = ret.get( "FTSHistory", None )
   if ftsHistory:
     gLogger.info( "[%d] Last hour transfer history" % ic )
     ic += 1
     keys = ( "SourceSE", "TargetSE", "Status", "FTSJobs", "Files", "Size" )
-    gLogger.info( "%-20s %-20s %20s %20s %20s %20s" % keys )
+    gLogger.always( "%-20s %-20s %20s %20s %20s %20s" % keys )
     for view in sorted( ftsHistory, key = itemgetter( "SourceSE" ) ):
       vals = tuple( [ view.get( key, "" ) for key in keys ] )
-      gLogger.info( "%-20s %-20s %20s %20s %20s %20s" % vals )
+      gLogger.always( "%-20s %-20s %20s %20s %20s %20s" % vals )
 
   DIRAC.exit( -1 )
