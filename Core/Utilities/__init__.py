85e78dc68d8dd9f30a0acb8a4e201547e67a96e2
ubeda <mario.ubeda.garcia@cern.ch>
2013-06-27 13:51:28 +0200

    FIX: cleared ThreadPool and Tests

--- Core/Utilities/__init__.py
+++ Core/Utilities/__init__.py
@@ -10,9 +10,9 @@ from DIRAC.Core.Utilities.Os                 import uniquePath, getDiskSpace, ge
 from DIRAC.Core.Utilities.Subprocess         import shellCall, systemCall, pythonCall
 from DIRAC.Core.Utilities                    import Time
 #from DIRAC.Core.Utilities.Time               import microsecond, second, minute, hour, day, week, dt, dateTime, date, time, toEpoch, fromEpoch, to2K, from2K, toString, fromString, timeInterval
-from DIRAC.Core.Utilities.ThreadPool         import WorkingThread, ThreadedJob, ThreadPool, gThreadPool, getGlobalThreadPool
-from DIRAC.Core.Utilities.Tests              import run
+#from DIRAC.Core.Utilities.ThreadPool         import WorkingThread, ThreadedJob, ThreadPool, gThreadPool, getGlobalThreadPool
+#from DIRAC.Core.Utilities.Tests              import run
 from DIRAC.Core.Utilities.ExitCallback       import gCallbackList, registerSignals, execute, registerExitCallback
-from DIRAC.Core.Utilities.ThreadSafe         import Synchronizer, WORM
+#from DIRAC.Core.Utilities.ThreadSafe         import Synchronizer, WORM
 #from DIRAC.Core.Utilities.DEncode            import encode, decode
 from DIRAC.Core.Utilities                    import List
