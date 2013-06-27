f57bc55ea37312a485411f10b261ac251a974953
ubeda <mario.ubeda.garcia@cern.ch>
2013-06-27 14:52:37 +0200

    FIX: blindly commented out List and Time

--- Core/Utilities/__init__.py
+++ Core/Utilities/__init__.py
@@ -7,11 +7,11 @@ __RCSID__ = "$Id$"
 #from DIRAC.Core.Utilities.Network            import discoverInterfaces, getAllInterfaces, getAddressFromInterface, getMACFromInterface, getFQDN, splitURL, getIPsForHostName, checkHostsMatch
 from DIRAC.Core.Utilities.Os                 import uniquePath, getDiskSpace, getDirectorySize, sourceEnv, unifyLdLibraryPath, which, DEBUG
 from DIRAC.Core.Utilities.Subprocess         import shellCall, systemCall, pythonCall
-from DIRAC.Core.Utilities                    import Time
+#from DIRAC.Core.Utilities                    import Time
 #from DIRAC.Core.Utilities.Time               import microsecond, second, minute, hour, day, week, dt, dateTime, date, time, toEpoch, fromEpoch, to2K, from2K, toString, fromString, timeInterval
 #from DIRAC.Core.Utilities.ThreadPool         import WorkingThread, ThreadedJob, ThreadPool, gThreadPool, getGlobalThreadPool
 #from DIRAC.Core.Utilities.Tests              import run
 #from DIRAC.Core.Utilities.ExitCallback       import gCallbackList, registerSignals, execute, registerExitCallback
 #from DIRAC.Core.Utilities.ThreadSafe         import Synchronizer, WORM
 #from DIRAC.Core.Utilities.DEncode            import encode, decode
-from DIRAC.Core.Utilities                    import List
+#from DIRAC.Core.Utilities                    import List
