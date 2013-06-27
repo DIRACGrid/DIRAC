c0baa39054eddfd6c2cd477f2dda4d3bff163770
ubeda <mario.ubeda.garcia@cern.ch>
2013-06-27 15:26:37 +0200

    FIX: cleared Os * imports

--- Core/Utilities/__init__.py
+++ Core/Utilities/__init__.py
@@ -6,7 +6,6 @@ __RCSID__ = "$Id$"
 #from DIRAC.Core.Utilities.File               import makeGuid, checkGuid, getSize, getGlobbedTotalSize, getGlobbedFiles, getCommonPath, getMD5ForFiles
 #from DIRAC.Core.Utilities.Network            import discoverInterfaces, getAllInterfaces, getAddressFromInterface, getMACFromInterface, getFQDN, splitURL, getIPsForHostName, checkHostsMatch
 #from DIRAC.Core.Utilities.Os                 import uniquePath, getDiskSpace, getDirectorySize, sourceEnv, unifyLdLibraryPath, which, DEBUG
-from DIRAC.Core.Utilities.Os                 import which, DEBUG
 #from DIRAC.Core.Utilities.Subprocess         import shellCall, systemCall, pythonCall
 #from DIRAC.Core.Utilities                    import Time
 #from DIRAC.Core.Utilities.Time               import microsecond, second, minute, hour, day, week, dt, dateTime, date, time, toEpoch, fromEpoch, to2K, from2K, toString, fromString, timeInterval
