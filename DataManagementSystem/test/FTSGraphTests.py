5a96e5d0125267e1985bab357790da4b475b5edb
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2013-07-03 12:32:23 +0200

    save

--- DataManagementSystem/test/FTSGraphTests.py
+++ DataManagementSystem/test/FTSGraphTests.py
@@ -41,38 +41,40 @@ class FTSGraphTests( unittest.TestCase ):
 
   def setUp( self ):
     """ test set up """
-    self.ftsSites = [ FTSSite( { "FTSServer": "https://fts22-t0-export.cern.ch:8443/glite-data-transfer-fts/services/FileTransfer",
-                                "Name": "CERN.ch",
-                                "FTSSiteID": 1 } ),
-                      FTSSite( { "FTSServer": "https://fts.pic.es:8443/glite-data-transfer-fts/services/FileTransfer",
-                                "Name": "PIC.es",
-                                "FTSSiteID": 2 } ),
-                      FTSSite( { "FTSServer": "https://lcgfts.gridpp.rl.ac.uk:8443/glite-data-transfer-fts/services/FileTransfer",
-                                "Name": "RAL.uk",
-                                "FTSSiteID": 3 } ),
-                      FTSSite( { "FTSServer": "https://fts.grid.sara.nl:8443/glite-data-transfer-fts/services/FileTransfer",
-                                "Name": "SARA.nl",
-                                "FTSSiteID": 4 } ),
-                      FTSSite( { "FTSServer": "https://fts.cr.cnaf.infn.it:8443/glite-data-transfer-fts/services/FileTransfer",
-                                "Name": "CNAF.it",
-                                "FTSSiteID": 5 } ),
-                      FTSSite( { "FTSServer": "https://fts.grid.sara.nl:8443/glite-data-transfer-fts/services/FileTransfer",
-                                "Name": "NIKHEF.nl",
-                                "FTSSiteID": 6 } ),
-                      FTSSite( { "FTSServer": "https://fts-fzk.gridka.de:8443/glite-data-transfer-fts/services/FileTransfer",
-                                "Name": "GRIDKA.de",
-                                "FTSSiteID": 7 } ),
-                      FTSSite( { "FTSServer": "https://cclcgftsprod.in2p3.fr:8443/glite-data-transfer-fts/services/FileTransfer",
-                                "Name": "IN2P3.fr",
-                                "FTSSiteID": 8 } ) ]
-    self.ftsHistoryViews = [ FTSHistoryView( { "TargetSE": "RAL-USER",
-                                               "SourceSE": "CERN-USER",
-                                               "FTSJobs": 10,
-                                               "FTSServer": "https://fts22-t0-export.cern.ch:8443/glite-data-transfer-fts/services/FileTransfer",
-                                               "Status": "Active",
-                                               "Files" : 1000,
-                                               "Size": 10000000 } ) ]
-
+    self.ftsSites = [ 
+      FTSSite( { "FTSServer": "https://fts22-t0-export.cern.ch:8443/glite-data-transfer-fts/services/FileTransfer",
+                 "Name": "CERN.ch",
+                 "FTSSiteID": 1 } ),
+      FTSSite( { "FTSServer": "https://fts.pic.es:8443/glite-data-transfer-fts/services/FileTransfer",
+                 "Name": "PIC.es",
+                 "FTSSiteID": 2 } ),
+      FTSSite( { "FTSServer": "https://lcgfts.gridpp.rl.ac.uk:8443/glite-data-transfer-fts/services/FileTransfer",
+                 "Name": "RAL.uk",
+                 "FTSSiteID": 3 } ),
+      FTSSite( { "FTSServer": "https://fts.grid.sara.nl:8443/glite-data-transfer-fts/services/FileTransfer",
+                 "Name": "SARA.nl",
+                 "FTSSiteID": 4 } ),
+      FTSSite( { "FTSServer": "https://fts.cr.cnaf.infn.it:8443/glite-data-transfer-fts/services/FileTransfer",
+                 "Name": "CNAF.it",
+                 "FTSSiteID": 5 } ),
+      FTSSite( { "FTSServer": "https://fts.grid.sara.nl:8443/glite-data-transfer-fts/services/FileTransfer",
+                 "Name": "NIKHEF.nl",
+                 "FTSSiteID": 6 } ),
+      FTSSite( { "FTSServer": "https://fts-fzk.gridka.de:8443/glite-data-transfer-fts/services/FileTransfer",
+                 "Name": "GRIDKA.de",
+                 "FTSSiteID": 7 } ),
+      FTSSite( { "FTSServer": "https://cclcgftsprod.in2p3.fr:8443/glite-data-transfer-fts/services/FileTransfer",
+                 "Name": "IN2P3.fr",
+                 "FTSSiteID": 8 } ) ]
+    self.ftsHistoryViews = [ 
+      FTSHistoryView( { "TargetSE": "RAL-USER",
+                        "SourceSE": "CERN-USER",
+                        "FTSJobs": 10,
+                        "FTSServer": "https://fts22-t0-export.cern.ch:8443/glite-data-transfer-fts/services/FileTransfer",
+                        "Status": "Active",
+                        "Files" : 1000,
+                        "Size": 10000000 } ) ]
+    
   def tearDown( self ):
     """ test case tear down """
     del self.ftsSites
@@ -83,12 +85,6 @@ class FTSGraphTests( unittest.TestCase ):
     graph = FTSGraph( "ftsGraph", self.ftsSites, self.ftsHistoryViews )
 
     self.assertEqual( type( graph ), FTSGraph, "c'tor failed" )
-
-    for node in graph.nodes():
-      print node
-
-    for route in graph.edges():
-      print route
 
     site = graph.findSiteForSE( "CERN-FOO" )
     self.assertEqual( site["OK"], False, "findSiteForSE call failed for unknown SE" )
@@ -108,7 +104,7 @@ class FTSGraphTests( unittest.TestCase ):
 
 # # test execution
 if __name__ == "__main__":
-  testLoader = unittest.TestLoader()
-  suite = testLoader.loadTestsFromTestCase( FTSGraphTests )
-  suite = unittest.TestSuite( [ suite ] )
+  gTestLoader = unittest.TestLoader()
+  gSuite = testLoader.loadTestsFromTestCase( FTSGraphTests )
+  gSuite = unittest.TestSuite( [ suite ] )
   unittest.TextTestRunner( verbosity = 3 ).run( suite )
