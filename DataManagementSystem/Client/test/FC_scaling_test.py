9c28805f2fcfbad24330ffdfe3ae0bd867f8b561
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2013-07-29 18:26:10 +0200

    updated

--- DataManagementSystem/Client/test/FC_scaling_test.py
+++ DataManagementSystem/Client/test/FC_scaling_test.py
@@ -15,17 +15,43 @@ Script.setUsageMessage( """
 """                        )
 
 testType = 'noTest'
-def setTestType():
+def setTestType( value ):
   global testType 
-  testType = 'listDir'
+  testType = value
   return S_OK()
 
+testDir = ''
+def setTestDirectory( value ):
+  global testDir
+  testDir = value
+  return S_OK()  
+
+nClients = 1
+def setNumberOfClients( value ):
+  global nClients
+  nClients = int( value )
+  return S_OK()  
+
+nQueries = 100  
+def setNumberOfQueries( value ):
+  global nQueries
+  nQueries = int( value )
+  return S_OK() 
+
+catalog = 'AugerTestFileCatalog'
+def setCatalog( value ):
+  global catalog
+  catalog = value
+  return S_OK()   
+
 Script.registerSwitch( "t:", "type=", "test type", setTestType )
+Script.registerSwitch( "D:", "directory=", "test directory", setTestDirectory )
+Script.registerSwitch( "N:", "clients=", "number of parallel clients", setNumberOfClients )
+Script.registerSwitch( "Q:", "queries=", "number of queries", setNumberOfQueries )
+Script.registerSwitch( "C:", "catalog=", "catalog to use", setCatalog )
 
 
 Script.parseCommandLine( ignoreErrors = True )
-
-
 
 from DIRAC.Resources.Catalog.FileCatalog import FileCatalog
 from DIRAC.Core.Utilities.ProcessPool import ProcessPool
@@ -33,43 +59,99 @@ from DIRAC.Core.Utilities.ProcessPool import ProcessPool
 from DIRAC import S_OK
 import time
 
-fc = FileCatalog( catalogs=['AugerTestFileCatalog'] )
+fc = FileCatalog( catalogs=[catalog] )
 
-def test():
+lfnList = []
+
+def listDirectory( n_queries, lfn_list ):
 
   start = time.time()
-  for i in range(200) :
+  sCount = 0
+  fCount = 0
+  for i in range( n_queries ) :
     result = fc.listDirectory('/auger/prod/ComPhotonQGSjetII_gr139/en16.500/th0.65/048279')
-    #if result['OK']:
-    #  print '.',
-    #else:
-    #  print '-',
+    if result['OK']:
+      sCount += 1
+    else:
+      fCount += 1
 
-    if i % 100 == 0:
-      print i
+    if i % 50 == 0:
+      print '\033[1D' + ".",
 
   total = time.time() - start
 
-  print "Total", total
+  #print "Total", total, 'Success', sCount, 'Failure', fCount
 
-  result = S_OK(total)
-  #print result
+  result = S_OK( (total,sCount,fCount) )
   return result
+
+resultTest = []
+
+def getBulkReplicas( n_queries, lfn_list ):
+
+  start = time.time()
+  sCount = 0
+  fCount = 0
+  for i in range( n_queries ) :
+    result = fc.getReplicas( lfnList )
+    if result['OK']:
+      sCount += 1
+    else:
+      fCount += 1
+
+    if i % 50 == 0:
+      print '\033[1D' + ".",
+
+  total = time.time() - start
+
+  #print "Total", total, 'Success', sCount, 'Failure', fCount
+
+  result = S_OK( (total,sCount,fCount) )
+  return result
+
 
 def finalize(task,result):
 
-  if result['OK']:
-    print "Test time ", result['Value'], task
-  else:
-    print "Error:", result['Message']
+  global resultTest 
+
+  #if result['OK']:
+  #  print "Test time ", result['Value'], task.getTaskID()
+  #else:
+  #  print "Error:", result['Message']
+
+  resultTest.append( result['Value'] ) 
 
 def doException( expt ):
   print "Exception", expt
 
 
-pp = ProcessPool( 10 )
+pp = ProcessPool( nClients )
 
-for c in range( 10  ):
-  pp.createAndQueueTask( test, [], callback=finalize, exceptionCallback=doException )
+testFunction = eval( testType )
 
-pp.processAllResults()
+for c in range( nClients ):
+  pp.createAndQueueTask( testFunction, [nQueries,lfnList], 
+                         callback=finalize, 
+                         exceptionCallback=doException )
+
+pp.processAllResults(3600)
+pp.finalize()
+
+average = 0.
+
+print "\nFinal results:"
+
+sum = 0.
+count = 0
+for testTime,success,failure in resultTest:
+  #print testTime,success,failure
+  count += 1
+  sum += testTime
+
+if count != 0:
+  average = sum/count
+  print "Average time %.2f sec" % average
+  print "Average response time %.2f sec" % (average/nQueries)
+  print "Average query rate %.2f Hz" % (nQueries*nClients/average)   
+else:
+  print "Test failed", sum,count 
