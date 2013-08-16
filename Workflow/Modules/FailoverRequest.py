a4c0e9d9c59fc7d10df9fef695861413783e5f03
FedericoStagni <fstagni@cern.ch>
2013-08-16 17:18:40 +0200

    Bugfix and slight change in the logic
    Files are normally set as "Processed" unless failures are previously
    detected

--- Workflow/Modules/FailoverRequest.py
+++ Workflow/Modules/FailoverRequest.py
@@ -20,8 +20,6 @@ class FailoverRequest( ModuleBase ):
     self.log = gLogger.getSubLogger( "FailoverRequest" )
     super( FailoverRequest, self ).__init__( self.log )
 
-    self.stepInputData = []
-
   #############################################################################
 
   def _resolveInputVariables( self ):
@@ -42,27 +40,20 @@ class FailoverRequest( ModuleBase ):
 
   def _execute( self ):
 
-    # report on the status of the input data
-    if self.stepInputData:
-      inputFiles = self.fileReport.getFiles()
-      for lfn in self.stepInputData:
-        if not lfn in inputFiles:
-          self.log.verbose( "No status populated for input data %s, setting to 'Unused'" % lfn )
-          self.fileReport.setFileStatus( int( self.production_id ), lfn, 'Unused' )
-
+    # report on the status of the input data, by default they are 'Processed', unless the job failed
+    # failures happening before are not touched
+    filesInFileReport = self.fileReport.getFiles()
     if not self._checkWFAndStepStatus( noPrint = True ):
-      inputFiles = self.fileReport.getFiles()
-      for lfn in inputFiles:
-        self.log.info( "Forcing status to 'Unused' due to workflow failure for: %s" % ( lfn ) )
-        self.fileReport.setFileStatus( int( self.production_id ), lfn, 'Unused' )
+      for lfn in self.inputDataList:
+        if not lfn in filesInFileReport:
+          self.log.info( "Forcing status to 'Unused' due to workflow failure for: %s" % ( lfn ) )
+          self.fileReport.setFileStatus( int( self.production_id ), lfn, 'Unused' )
     else:
-      inputFiles = self.fileReport.getFiles()
-
-      if inputFiles:
-        self.log.info( "Workflow status OK, setting input file status to Processed" )
-      for lfn in inputFiles:
-        self.log.info( "Setting status to 'Processed' for: %s" % ( lfn ) )
-        self.fileReport.setFileStatus( int( self.production_id ), lfn, 'Processed' )
+      filesInFileReport = self.fileReport.getFiles()
+      for lfn in self.inputDataList:
+        if not lfn in filesInFileReport:
+          self.log.verbose( "No status populated for input data %s, setting to 'Processed'" % lfn )
+          self.fileReport.setFileStatus( int( self.production_id ), lfn, 'Processed' )
 
     result = self.fileReport.commit()
     if not result['OK']:
