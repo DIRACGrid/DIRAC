174cf0829d21f20168bb6eecec473f88924a6bf7
sposs <stephposs@gmail.com>
2013-09-17 12:12:54 +0200

    NEW: SSHOSGComputingElement and condorgce script
    To submit to OSG, a variation of the condor client must be provided. In
    particular, it requires the "grid" Universe, and the "grid_resource"
    parameter. The condor_q is also different: the user is not needed as the
    submitter is the submission host (apparently, from the doc), but adding
    a -globus command line option allows for grid job monitoring.
    
    This is untested as ILCDIRAC does not have an OSG client installed
    (would be nice though).

---
+++ Resources/Computing/SSHOSGComputingElement.py
@@ -0,0 +1,25 @@
+########################################################################
+# $HeadURL$
+# File : CondorComputingElement.py
+# Author : A.Tsaregorodtsev
+########################################################################
+
+from DIRAC.Resources.Computing.SSHComputingElement  import SSH, SSHComputingElement 
+from DIRAC.Resources.Computing.PilotBundle          import bundleProxy 
+from DIRAC import rootPath, S_OK, S_ERROR
+
+CE_NAME = 'SSHOSG'
+MANDATORY_PARAMETERS = [ 'Queue' ]
+
+class SSHOSGComputingElement( SSHComputingElement ):
+       
+  #############################################################################
+  def __init__( self, ceUniqueID ):
+    """ Standard constructor.
+    """
+    SSHComputingElement.__init__( self, ceUniqueID )
+
+    self.ceType = CE_NAME
+    self.controlScript = 'condorgce'
+    self.mandatoryParameters = MANDATORY_PARAMETERS         
+    
