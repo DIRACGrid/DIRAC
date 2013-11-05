b5c870694c605e53a9931ed633a871fd5a40c784
sposs <stephposs@gmail.com>
2013-11-05 10:25:19 +0100

    beautify

--- StorageManagementSystem/DB/MigrationMonitoringDB.py
+++ StorageManagementSystem/DB/MigrationMonitoringDB.py
@@ -4,65 +4,64 @@
 
 __RCSID__ = "$Id$"
 
-from DIRAC                        import gLogger, gConfig, S_OK, S_ERROR
+from DIRAC                        import gLogger, S_OK
 from DIRAC.Core.Base.DB           import DB
-from DIRAC.Core.Utilities.List    import stringListToString,intListToString
-from types                        import ListType
+from DIRAC.Core.Utilities.List    import stringListToString, intListToString
 
 class MigrationMonitoringDB(DB):
 
   def __init__(self, systemInstance='Default', maxQueueSize=10 ):
-    DB.__init__(self,'StorageManagementDB','StorageManagement/StorageManagementDB',maxQueueSize)
+    DB.__init__(self, 'StorageManagementDB', 'StorageManagement/StorageManagementDB', maxQueueSize)
 
   #################################################################
   #
   # The methods for adding/removing files to/from the database
   #
 
-  def addMigratingReplicas(self,fileList):
+  def addMigratingReplicas(self, fileList):
     """ Insert replicas into the database
     """
     gLogger.info("addMigratingReplicas: Attempting to add %d replicas to database." % len(fileList))
     req = "INSERT INTO MigratingReplicas (Status,LFN,PFN,Size,SE,GUID,FileChecksum,SubmitTime) VALUES"
-    for lfn,pfn,size,se,guid,checksum in fileList:
+    for lfn, pfn, size, se, guid, checksum in fileList:
       status = 'New'
       if (size and checksum):
         status = 'Migrating'
-      req = "%s ('%s','%s','%s',%s,'%s','%s','%s',UTC_TIMESTAMP())," % (req,status,lfn,pfn,size,se,guid,checksum)
+      req = "%s ('%s','%s','%s',%s,'%s','%s','%s',UTC_TIMESTAMP())," % (req, status, lfn, pfn, size, se, guid, checksum)
     req = req.rstrip(',')
     res = self._update(req)
     if not res['OK']:
-      gLogger.error("addMigratingReplicas: Failed update add replicas to database.",res['Message'])
+      gLogger.error("addMigratingReplicas: Failed update add replicas to database.", res['Message'])
     else:
       gLogger.info("addMigratingReplicas: Successfully added replicas.")
     return res
 
-  def removeMigratingFiles(self,lfns):
+  def removeMigratingFiles(self, lfns):
     """ Remove all replicas with associated LFNs from the database
     """
     gLogger.info("removeMigratingFiles: Attempting to remove %d files from the database." % len(lfns))
     req = "DELETE FROM MigratingReplicas WHERE LFN IN (%s);" % stringListToString(lfns)
     res = self._update(req)
     if not res['OK']:
-      gLogger.error("removeMigratingFiles: Failed removing files from database.",res['Message'])
+      gLogger.error("removeMigratingFiles: Failed removing files from database.", res['Message'])
     else:
       gLogger.info("removeMigratingFiles: Successfully removed files.")
     return res
 
-  def removeMigratingReplicas(self,replicaList):
+  def removeMigratingReplicas(self, replicaList):
     """ Remove replicas from the database
     """
     gLogger.info("removeMigratingReplicas: Attempting to remove %d replicas from the database." % len(replicaList))
     successful = {}
     failed = {}
-    for lfn,pfn,se in replicaList:
-      req = "DELETE FROM MigratingReplicas WHERE SE='%s' AND LFN = '%s';" % (se,lfn)
+    for lfn, pfn, se in replicaList:
+      req = "DELETE FROM MigratingReplicas WHERE SE='%s' AND LFN = '%s';" % (se, lfn)
       res = self._update(req)
       if not res['OK']:
         failed[lfn] = res['Message']
       else:
         successful[lfn] = True
-    resDict = {'Failed':failed,'Successful':successful}
+    resDict = {'Failed':failed, 'Successful':successful}
     return S_OK(resDict)
 
   #################################################################
@@ -70,31 +69,31 @@ class MigrationMonitoringDB(DB):
   # The methods for retrieving data files from the database
   #
 
-  def getMigratingReplicas(self,se,status):
+  def getMigratingReplicas(self, se, status):
     """ Obtain all the replicas of a given status in the database along with all their associated metadata
     """
     if se:
-      req = "SELECT ReplicaID,LFN,PFN,SE,Size,FileChecksum,SubmitTime FROM MigratingReplicas WHERE Status = '%s' AND SE = '%s';" % (status,se)
+      req = "SELECT ReplicaID,LFN,PFN,SE,Size,FileChecksum,SubmitTime FROM MigratingReplicas WHERE Status = '%s' AND SE = '%s';" % (status, se)
     else:
       req = "SELECT ReplicaID,LFN,PFN,SE,Size,FileChecksum,SubmitTime FROM MigratingReplicas WHERE Status = '%s';" % (status)
     res = self._query(req)
     if not res['OK']:
-      gLogger.error("getMigratingReplicas: Failed to get replicas from database.",res['Message'])
+      gLogger.error("getMigratingReplicas: Failed to get replicas from database.", res['Message'])
       return res
     fileDict = {}
-    for id,lfn,pfn,se,size,checksum,submitTime in res['Value']:
-      fileDict[id] = {'LFN':lfn,'PFN':pfn,'SE':se,'Size':size,'Checksum':checksum,'SubmitTime':submitTime}
-    gLogger.info("getMigratingReplicas: Obtained %d replicas with status %s." % (len(fileDict.keys()),status))
+    for fid, lfn, pfn, se, size, checksum, submitTime in res['Value']:
+      fileDict[fid] = {'LFN' : lfn, 'PFN' : pfn, 'SE' : se, 'Size' : size, 'Checksum' : checksum, 'SubmitTime' : submitTime}
+    gLogger.info("getMigratingReplicas: Obtained %d replicas with status %s." % (len(fileDict.keys()), status))
     return S_OK(fileDict)
 
-  def setMigratingReplicaStatus(self,fileIDs,status):
+  def setMigratingReplicaStatus(self, fileIDs, status):
     """ Update the status of the replica in the database
     """
-    gLogger.info("setMigratingReplicaStatus: Attempting to update status of %d replicas to '%s'." % (len(fileIDs),status))
-    req = "UPDATE MigratingReplicas SET Status='%s', LastUpdate=UTC_TIMESTAMP() WHERE ReplicaID IN (%s);" % (status,intListToString(fileIDs))
+    gLogger.info("setMigratingReplicaStatus: Attempting to update status of %d replicas to '%s'." % (len(fileIDs), status))
+    req = "UPDATE MigratingReplicas SET Status='%s', LastUpdate=UTC_TIMESTAMP() WHERE ReplicaID IN (%s);" % (status, intListToString(fileIDs))
     res = self._update(req)
     if not res['OK']:
-      gLogger.error("setMigratingReplicaStatus: Failed update replica statuses.",res['Message'])
+      gLogger.error("setMigratingReplicaStatus: Failed update replica statuses.", res['Message'])
     else:
       gLogger.info("setMigratingReplicaStatus: Successfully updated replica statuses.")
     return res
@@ -107,23 +106,23 @@ class MigrationMonitoringDB(DB):
   def getMigratingReplicasStatistics(self):
     """ Get the count of the file statutes and storage elements in the DB
     """
-    return self.getCounters('MigratingReplicas', ['Status','SE'], {})
+    return self.getCounters('MigratingReplicas', ['Status', 'SE'], {})
 
   def getDistinctMigratingReplicasSelections(self):
     """ Get the unique values of the selection fields
     """
     selDict = {}
-    res = self.getDistinctAttributeValues('MigratingReplicas','SE')
+    res = self.getDistinctAttributeValues('MigratingReplicas', 'SE')
     if not res['OK']:
       return res
     selDict['SE'] = res['Value']
-    res = self.getDistinctAttributeValues('MigratingReplicas','Status')
+    res = self.getDistinctAttributeValues('MigratingReplicas', 'Status')
     if not res['OK']:
       return res
     selDict['Status'] = res['Value']
     return S_OK(selDict)
 
-  def selectMigratingReplicasRows(self,selectDict, orderAttribute='LFN',newer=None, older=None, limit=None):
+  def selectMigratingReplicasRows(self, selectDict, orderAttribute='LFN', newer=None, older=None, limit=None):
     """ Select the rows which match the selection criteria.
     """
     condition = self.buildCondition(selectDict, older=older, newer=newer)
