45d8d501867c8897dfb7899b4f3d6230fe47c3d8
FedericoStagni <fstagni@cern.ch>
2013-11-15 10:35:02 +0100

    DBs definition: portings from integration

--- WorkloadManagementSystem/DB/JobLoggingDB.sql
+++ WorkloadManagementSystem/DB/JobLoggingDB.sql
@@ -33,12 +33,21 @@ USE JobLoggingDB;
 DROP TABLE IF EXISTS LoggingInfo;
 CREATE TABLE LoggingInfo (
     JobID INTEGER NOT NULL,
+    SeqNum INTEGER NOT NULL DEFAULT 0,
     Status VARCHAR(32) NOT NULL DEFAULT '',
     MinorStatus VARCHAR(128) NOT NULL DEFAULT '',
     ApplicationStatus varchar(256) NOT NULL DEFAULT '', 
     StatusTime DATETIME NOT NULL ,
-    StatusTimeOrder DOUBLE(11,3) NOT NULL,  
+    StatusTimeOrder DOUBLE(12,3) NOT NULL,  
     StatusSource VARCHAR(32) NOT NULL DEFAULT 'Unknown',
-    INDEX (JobID)
+    PRIMARY KEY (JobID, SeqNum)
 ) ENGINE = InnoDB;
 
+-- -----------------------------------------------------------------------------
+--
+-- Trigger to manage the new LoggingInfo structure: SeqNum is a sequential number within the same JobID
+--    the trigger generates a proper sequence for each JobID
+--
+
+CREATE TRIGGER SeqNumGenerator BEFORE INSERT ON LoggingInfo
+FOR EACH ROW SET NEW.SeqNum= (SELECT IFNULL(MAX(SeqNum) + 1,1) FROM LoggingInfo WHERE JobID=NEW.JobID);
