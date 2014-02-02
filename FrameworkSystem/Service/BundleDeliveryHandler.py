781026a7b790aa66c3648979ab976f09cc33cffe
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2014-02-02 23:58:43 +0100

    Protect against empty bundle case

--- FrameworkSystem/Service/BundleDeliveryHandler.py
+++ FrameworkSystem/Service/BundleDeliveryHandler.py
@@ -66,20 +66,23 @@ class BundleManager:
     for bId in dirsToBundle:
       bundlePaths = dirsToBundle[ bId ]
       gLogger.info( "Updating %s bundle %s" % ( bId, bundlePaths ) )
-      buffer = cStringIO.StringIO()
+      buffer_ = cStringIO.StringIO()
       filesToBundle = sorted( File.getGlobbedFiles( bundlePaths ) )
-      commonPath = File.getCommonPath( filesToBundle )
-      commonEnd = len( commonPath )
-      gLogger.info( "Bundle will have %s files with common path %s" % ( len( filesToBundle ), commonPath ) )
-      tarBuffer = tarfile.open( 'dummy', "w:gz", buffer )
-      for filePath in filesToBundle:
-        tarBuffer.add( filePath, filePath[ commonEnd: ] )
-      tarBuffer.close()
-      zippedData = buffer.getvalue()
-      buffer.close()
-      hash = File.getMD5ForFiles( filesToBundle )
-      gLogger.info( "Bundled %s : %s bytes (%s)" % ( bId, len( zippedData ), hash ) )
-      self.__bundles[ bId ] = ( hash, zippedData )
+      if filesToBundle:
+        commonPath = File.getCommonPath( filesToBundle )
+        commonEnd = len( commonPath )
+        gLogger.info( "Bundle will have %s files with common path %s" % ( len( filesToBundle ), commonPath ) )
+        tarBuffer = tarfile.open( 'dummy', "w:gz", buffer_ )
+        for filePath in filesToBundle:
+          tarBuffer.add( filePath, filePath[ commonEnd: ] )
+        tarBuffer.close()
+        zippedData = buffer_.getvalue()
+        buffer_.close()
+        hash_ = File.getMD5ForFiles( filesToBundle )
+        gLogger.info( "Bundled %s : %s bytes (%s)" % ( bId, len( zippedData ), hash_ ) )
+        self.__bundles[ bId ] = ( hash_, zippedData )
+      else:
+        self.__bundles[ bId ] = ( None, None )  
 
 gBundleManager = False
 
@@ -119,13 +122,17 @@ class BundleDeliveryHandler( RequestHandler ):
       return S_ERROR( "Unknown bundle %s" % bId )
 
     bundleVersion = gBundleManager.getBundleVersion( bId )
+    if bundleVersion is None:
+      fileHelper.markAsTransferred()
+      return S_ERROR( "Empty bundle %s" % bId )
+    
     if version == bundleVersion:
       fileHelper.markAsTransferred()
       return S_OK( bundleVersion )
 
-    buffer = cStringIO.StringIO( gBundleManager.getBundleData( bId ) )
-    result = fileHelper.DataSourceToNetwork( buffer )
-    buffer.close()
+    buffer_ = cStringIO.StringIO( gBundleManager.getBundleData( bId ) )
+    result = fileHelper.DataSourceToNetwork( buffer_ )
+    buffer_.close()
     if not result[ 'OK' ]:
       return result
     return S_OK( bundleVersion )
