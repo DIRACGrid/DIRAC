9e90a6123633c4218683c34650a193bdd65ab1ea
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2014-03-03 12:26:51 +0100

    Removed obsoleted stuff

--- Resources/Computing/SSHTorqueComputingElement.py
+++ Resources/Computing/SSHTorqueComputingElement.py
@@ -10,18 +10,13 @@
 
 __RCSID__ = "$Id$"
 
-from DIRAC.Resources.Computing.SSHComputingElement       import SSH, SSHComputingElement 
-from DIRAC.Core.Utilities.Subprocess                     import shellCall
-from DIRAC.Core.Utilities.List                           import breakListIntoChunks
+from DIRAC.Resources.Computing.SSHComputingElement       import SSHComputingElement 
 from DIRAC.Core.Utilities.Pfn                            import pfnparse
 from DIRAC                                               import S_OK, S_ERROR
-from DIRAC                                               import systemCall, rootPath
-from DIRAC                                               import gConfig
-from DIRAC.Core.Security.ProxyInfo                       import getProxyInfo
 from DIRAC.Resources.Computing.SSHComputingElement       import SSH 
+from DIRAC.Core.Utilities.List                           import breakListIntoChunks
 
-import os, sys, time, re, socket, stat, shutil
-import string, shutil, bz2, base64, tempfile
+import re
 
 CE_NAME = 'SSHTorque'
 MANDATORY_PARAMETERS = [ 'Queue' ]
@@ -37,134 +32,6 @@ class SSHTorqueComputingElement( SSHComputingElement ):
     self.ceType = CE_NAME
     self.controlScript = 'torquece'
     self.mandatoryParameters = MANDATORY_PARAMETERS
-
-  #############################################################################
-  def _addCEConfigDefaults_old( self ):
-    """Method to make sure all necessary Configuration Parameters are defined
-    """
-    # First assure that any global parameters are loaded
-    ComputingElement._addCEConfigDefaults( self )
-    # Now Torque specific ones
-    if 'ExecQueue' not in self.ceParameters:
-      self.ceParameters['ExecQueue'] = self.ceParameters.get( 'Queue', '' )
-
-    if 'SharedArea' not in self.ceParameters:
-      self.ceParameters['SharedArea'] = ''
-
-    if 'BatchOutput' not in self.ceParameters:
-      self.ceParameters['BatchOutput'] = os.path.join( gConfig.getValue( '/LocalSite/InstancePath', rootPath ), 'data' )
-
-    if 'BatchError' not in self.ceParameters:
-      self.ceParameters['BatchError'] = os.path.join( gConfig.getValue( '/LocalSite/InstancePath', rootPath ), 'data' )
-
-    if 'ExecutableArea' not in self.ceParameters:
-      self.ceParameters['ExecutableArea'] = os.path.join( gConfig.getValue( '/LocalSite/InstancePath', rootPath ), 'data' )
-
-
-  def reset_old( self ):
-
-    self.queue = self.ceParameters['Queue']
-    if 'ExecQueue' not in self.ceParameters or not self.ceParameters['ExecQueue']:
-      self.ceParameters['ExecQueue'] = self.ceParameters.get( 'Queue', '' )
-    self.execQueue = self.ceParameters['ExecQueue']
-    self.log.info( "Using queue: ", self.queue )
-    self.hostname = socket.gethostname()
-    self.sharedArea = self.ceParameters['SharedArea']
-    self.batchOutput = self.ceParameters['BatchOutput']
-    self.batchError = self.ceParameters['BatchError']
-    self.executableArea = self.ceParameters['ExecutableArea']
-    self.submitOptions = ''
-    if 'SubmitOptions' in self.ceParameters:
-      self.submitOptions = self.ceParameters['SubmitOptions']
-    self.removeOutput = True
-    if 'RemoveOutput' in self.ceParameters:
-      if self.ceParameters['RemoveOutput'].lower()  in ['no', 'false', '0']:
-        self.removeOutput = False
-
-  #############################################################################
-  def submitJob_old( self, executableFile, proxy, numberOfJobs = 1 ):
-    """ Method to submit job
-    """
-
-    self.log.verbose( "Executable file path: %s" % executableFile )
-    if not os.access( executableFile, 5 ):
-      os.chmod( executableFile, 0755 )
-
-    # if no proxy is supplied, the executable can be submitted directly
-    # otherwise a wrapper script is needed to get the proxy to the execution node
-    # The wrapper script makes debugging more complicated and thus it is
-    # recommended to transfer a proxy inside the executable if possible.
-    if proxy:
-      self.log.verbose( 'Setting up proxy for payload' )
-
-      compressedAndEncodedProxy = base64.encodestring( bz2.compress( proxy.dumpAllToString()['Value'] ) ).replace( '\n', '' )
-      compressedAndEncodedExecutable = base64.encodestring( bz2.compress( open( executableFile, "rb" ).read(), 9 ) ).replace( '\n', '' )
-
-      wrapperContent = """#!/usr/bin/env python
-# Wrapper script for executable and proxy
-import os, tempfile, sys, base64, bz2, shutil
-try:
-  workingDirectory = tempfile.mkdtemp( suffix = '_wrapper', prefix= 'TORQUE_' )
-  os.chdir( workingDirectory )
-  open( 'proxy', "w" ).write(bz2.decompress( base64.decodestring( "%(compressedAndEncodedProxy)s" ) ) )
-  open( '%(executable)s', "w" ).write(bz2.decompress( base64.decodestring( "%(compressedAndEncodedExecutable)s" ) ) )
-  os.chmod('proxy',0600)
-  os.chmod('%(executable)s',0700)
-  os.environ["X509_USER_PROXY"]=os.path.join(workingDirectory, 'proxy')
-except Exception, x:
-  print >> sys.stderr, x
-  sys.exit(-1)
-cmd = "./%(executable)s"
-print 'Executing: ', cmd
-sys.stdout.flush()
-os.system( cmd )
-
-shutil.rmtree( workingDirectory )
-
-""" % { 'compressedAndEncodedProxy': compressedAndEncodedProxy, \
-        'compressedAndEncodedExecutable': compressedAndEncodedExecutable, \
-        'executable': os.path.basename( executableFile ) }
-
-      fd, name = tempfile.mkstemp( suffix = '_wrapper.py', prefix = 'TORQUE_', dir = os.getcwd() )
-      wrapper = os.fdopen( fd, 'w' )
-      wrapper.write( wrapperContent )
-      wrapper.close()
-
-      submitFile = name
-
-    else: # no proxy
-      submitFile = executableFile
-
-    ssh = SSH( parameters = self.ceParameters )
-    # Copy the executable
-    os.chmod( submitFile, stat.S_IRUSR | stat.S_IXUSR )
-    sFile = os.path.basename( submitFile )
-    result = ssh.scpCall( 10, submitFile, '%s/%s' % ( self.executableArea, os.path.basename( submitFile ) ) )
-    # submit submitFile to the batch system
-    cmd = "i=0; while [ $i -lt %(numberOfJobs)d ]; do qsub -o %(output)s -e %(error)s -q %(queue)s -N DIRACPilot %(submitOptions)s %(executable)s; let i=i+1; done; rm -f %(executable)s" % \
-      {'numberOfJobs': numberOfJobs, \
-       'output': self.batchOutput, \
-       'error': self.batchError, \
-       'queue': self.queue, \
-       'submitOptions': self.submitOptions, \
-       'executable': '%s/%s' % ( self.executableArea, os.path.basename( submitFile ) ) }
-
-    self.log.verbose( 'CE submission command: %s' % ( cmd ) )
-
-    result = ssh.sshCall( 10, cmd )
-
-    if not result['OK'] or result['Value'][0] != 0:
-      self.log.warn( '===========> SSHTorque CE result NOT OK' )
-      self.log.debug( result )
-      return S_ERROR( result['Value'] )
-    else:
-      self.log.debug( 'Torque CE result OK' )
-
-    batchIDList = result['Value'][1].strip().replace( '\r', '' ).split( '\n' )
-
-    self.submittedJobs += 1
-
-    return S_OK( batchIDList )
 
   #############################################################################
   def getCEStatus( self ):
