0a5ddfec2a9d6af38eeefe5f746eea91cab93cce
Adria Casajus <adriancasajus@gmail.com>
2014-03-27 15:08:24 +0100

    Serveral accounting fixes:
    - NEW: All buckets are inserted at the same time using on DUPLICATE REPLACE
    - FIX: Even more restrictive job records for protecting against YANDEX crap. Fixes #1878

--- AccountingSystem/Client/Types/Job.py
+++ AccountingSystem/Client/Types/Job.py
@@ -52,6 +52,8 @@ class Job( BaseAccountingType ):
     cpuTime = result[ 'Value' ]
     if cpuTime > execTime * 100:
       return DIRAC.S_ERROR( "OOps. CPUTime seems to be more than 100 times the ExecTime. Smells fishy!" )
+    if execTime > 33350400: # 1 year
+      return DIRAC.S_ERROR( "OOps. More than 1 year of cpu time smells fishy!" )
     return DIRAC.S_OK()
 
 
