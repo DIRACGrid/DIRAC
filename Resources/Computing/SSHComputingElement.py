85a581c5b2d6b2f6355a841fa452c29e5629cab6
zimmer <zimmer@fysik.su.se>
2014-04-28 20:31:46 +0200

    fix

--- Resources/Computing/SSHComputingElement.py
+++ Resources/Computing/SSHComputingElement.py
@@ -25,7 +25,7 @@ MANDATORY_PARAMETERS = [ 'Queue' ]
 
 class SSH:
 
-  def __init__( self, user = None, host = None, password = None, key = None, parameters = {}, options = None ):
+  def __init__( self, user = None, host = None, password = None, key = None, parameters = {}, options = "" ):
 
     self.user = user
     if not user:
@@ -41,6 +41,7 @@ class SSH:
       self.key = parameters.get( 'SSHKey', '' )
     if not options:
       self.options = parameters.get( 'SSHOptions', '' )
+    self.options = options
     self.log = gLogger.getSubLogger( 'SSH' )  
 
   def __ssh_call( self, command, timeout ):
@@ -106,10 +107,9 @@ class SSH:
     key = ''
     if self.key:
       key = ' -i %s ' % self.key
-    if self.options:
-      key+= ' %s ' % self.options
+
     pattern = "'===><==='"
-    command = 'ssh -q %s -l %s %s "echo %s;%s"' % ( key, self.user, self.host, pattern, command )    
+    command = 'ssh -q %s -l %s %s %s "echo %s;%s"' % ( key, self.user, self.host, self.options, pattern, command )    
     self.log.debug( "SSH command %s" % command )
     result = self.__ssh_call( command, timeout )    
     self.log.debug( "SSH command result %s" % str( result ) )
@@ -137,10 +137,8 @@ class SSH:
     key = ''
     if self.key:
       key = ' -i %s ' % self.key
-    if self.options:
-      key+= ' %s ' % self.options
     if upload:
-      command = "scp %s %s %s@%s:%s" % ( key, localFile, self.user, self.host, destinationPath )
+      command = "scp %s %s %s %s@%s:%s" % ( key, localFile, self.options, self.user, self.host, destinationPath )
     else:
       command = "scp %s %s@%s:%s %s" % ( key, self.user, self.host, destinationPath, localFile )
     self.log.debug( "SCP command %s" % command )
