57833ff4ceb23a49adf2bd22b7b8aa92e5c81511
antonio <antonio79it@gmail.com>
2014-05-20 17:36:17 +0200

    Test case for Consistency checks

---
+++ DataManagementSystem/Client/test/TestConsistencyChecks.py
@@ -0,0 +1,12 @@
+import unittest
+from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
+
+class ConsistencyChecksTestCase(unittest.TestCase):
+  """ Base class for the Consistency Checks test cases
+  """
+  
+
+if __name__ == '__main__':
+  suite = unittest.defaultTestLoader.loadTestsFromTestCase(ConsistencyChecksTestCase)
+  #suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(DirectoryTestCase))
+  testResult = unittest.TextTestRunner(verbosity=2).run(suite)
