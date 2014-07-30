b79edefa41363bf5153d6d8ebc75f60a2b36cc35
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2014-07-30 23:22:35 +0200

    Added doc string

--- WorkloadManagementSystem/PilotAgent/dirac-pilot-2.py
+++ WorkloadManagementSystem/PilotAgent/dirac-pilot-2.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python
-# $HeadURL$
-"""
- Perform initial sanity checks on WN, installs and configures DIRAC and runs
- Job Agent to execute pending workload on WMS.
+
+#################################################
+# $Id$
+#################################################
+
+""" The dirac-pilot-2.py script is a steering script to execute a series of
+ pilot commands. The commands are provided in the pilot input sandbox in
+ the pilotCommands.py module or in any <EXTENSION>Commands.py module.
+ The pilot script defines two switches in order to choose a set of commands
+ for the pilot:
+ 
+ -E, --commandExtensions value
+    where the value is a comma separated list of extension names. Modules
+    with names <EXTENSION>Commands.py will be searched for the commands in
+    the order defined in the value. By default no extensions are given
+ -X, --commands value
+    where value is a comma separated list of pilot commands. By default
+    the list is InstallDIRAC,ConfigureDIRAC,LaunchAgent      
+  
+ The pilot script performs initial sanity checks on WN, installs and configures 
+ DIRAC and runs the Job Agent to execute pending workloads in the DIRAC WMS.
  It requires dirac-install script to be sitting in the same directory.
 """
+
 __RCSID__ = "$Id$"
 
 import os
