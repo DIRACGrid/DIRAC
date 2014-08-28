50bfba2daedbe34326cc412013bc4a74e87a3886
FedericoStagni <fstagni@cern.ch>
2014-08-28 12:26:54 +0200

    removed which

--- Core/Utilities/__init__.py
+++ Core/Utilities/__init__.py
@@ -6,7 +6,7 @@ __RCSID__ = "$Id$"
 from DIRAC.Core.Utilities.File               import makeGuid, checkGuid, getSize, getGlobbedTotalSize, getGlobbedFiles, getCommonPath, getMD5ForFiles
 from DIRAC.Core.Utilities.List               import uniqueElements, appendUnique, fromChar, randomize, sortList, pop, stringListToString, intListToString, getChunk, breakListIntoChunks, removeEmptyElements
 from DIRAC.Core.Utilities.Network            import discoverInterfaces, getAllInterfaces, getAddressFromInterface, getMACFromInterface, getFQDN, splitURL, getIPsForHostName, checkHostsMatch
-from DIRAC.Core.Utilities.Os                 import uniquePath, getDiskSpace, getDirectorySize, sourceEnv, unifyLdLibraryPath, which, DEBUG
+from DIRAC.Core.Utilities.Os                 import uniquePath, getDiskSpace, getDirectorySize, sourceEnv, unifyLdLibraryPath, DEBUG
 from DIRAC.Core.Utilities.Subprocess         import shellCall, systemCall, pythonCall
 from DIRAC.Core.Utilities                    import Time
 from DIRAC.Core.Utilities.Time               import microsecond, second, minute, hour, day, week, dt, dateTime, date, time, toEpoch, fromEpoch, to2K, from2K, toString, fromString, timeInterval
