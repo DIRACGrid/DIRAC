16ac0a0f01dbb16f1ab42716981391b8912307c5
FedericoStagni <fstagni@cern.ch>
2014-09-16 12:41:31 +0200

    Removed sql lines dangerous when installing via dirac tools

--- WorkloadManagementSystem/DB/PilotAgentsDB.sql
+++ WorkloadManagementSystem/DB/PilotAgentsDB.sql
@@ -7,24 +7,20 @@
 -- -
 -- ------------------------------------------------------------------------------
 
-DROP DATABASE IF EXISTS PilotAgentsDB;
-
-CREATE DATABASE PilotAgentsDB;
-
+-- When installing via dirac tools, the following is not needed (still here for reference)
+-- 
+-- DROP DATABASE IF EXISTS PilotAgentsDB;
+-- CREATE DATABASE PilotAgentsDB;
 -- ------------------------------------------------------------------------------
 -- Database owner definition
-
-USE mysql;
-DELETE FROM user WHERE user='Dirac';
-
+-- USE mysql;
+-- DELETE FROM user WHERE user='Dirac';
 --
 -- Must set passwords for database user by replacing "must_be_set".
 --
-
-GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON PilotAgentsDB.* TO Dirac@localhost IDENTIFIED BY 'must_be_set';
-GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON PilotAgentsDB.* TO Dirac@'%' IDENTIFIED BY 'must_be_set';
-
-FLUSH PRIVILEGES;
+-- GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON PilotAgentsDB.* TO Dirac@localhost IDENTIFIED BY 'must_be_set';
+-- GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON PilotAgentsDB.* TO Dirac@'%' IDENTIFIED BY 'must_be_set';
+-- FLUSH PRIVILEGES;
 
 -- -----------------------------------------------------------------------------
 USE PilotAgentsDB;
