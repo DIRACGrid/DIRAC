647f21151b0076a88e5bee83b0866f731e14df63
FedericoStagni <fstagni@cern.ch>
2014-10-14 18:18:41 +0200

    Reverted

--- WorkloadManagementSystem/DB/JobDB.sql
+++ WorkloadManagementSystem/DB/JobDB.sql
@@ -26,16 +26,6 @@
 USE JobDB;
 
 -- ------------------------------------------------------------------------------
-DROP TABLE IF EXISTS `JobJDLs`;
-CREATE TABLE `JobJDLs` (
-  `JobID` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `JDL` BLOB NOT NULL,
-  `JobRequirements` BLOB NOT NULL,
-  `OriginalJDL` BLOB NOT NULL,
-  PRIMARY KEY (`JobID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ------------------------------------------------------------------------------
 DROP TABLE IF EXISTS `Jobs`;
 CREATE TABLE `Jobs` (
   `JobID` INT(11) UNSIGNED NOT NULL DEFAULT 0,
@@ -86,6 +76,16 @@ CREATE TABLE `Jobs` (
   KEY `ApplicationStatus` (`ApplicationStatus`),
   KEY `StatusSite` (`Status`,`Site`),
   KEY `LastUpdateTime` (`LastUpdateTime`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ------------------------------------------------------------------------------
+DROP TABLE IF EXISTS `JobJDLs`;
+CREATE TABLE `JobJDLs` (
+  `JobID` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `JDL` BLOB NOT NULL,
+  `JobRequirements` BLOB NOT NULL,
+  `OriginalJDL` BLOB NOT NULL,
+  PRIMARY KEY (`JobID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ------------------------------------------------------------------------------
