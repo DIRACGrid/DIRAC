8a992bfeb3d5ec1853f622964f2d2f5546d9a07b
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2014-10-15 22:56:11 +0200

    Create common catalogs in multi-VO case if VO is not specified

--- Resources/Catalog/FileCatalog.py
+++ Resources/Catalog/FileCatalog.py
@@ -30,19 +30,17 @@ class FileCatalog:
     self.writeCatalogs = []
     self.rootConfigPath = '/Resources/FileCatalogs'
     self.vo = vo if vo else getVOfromProxyGroup().get( 'Value', None )
-    if self.vo:
-      self.opHelper = Operations( vo = self.vo )
-      if type( catalogs ) in types.StringTypes:
-        catalogs = [catalogs]
-      if catalogs:
-        res = self._getSelectedCatalogs( catalogs )
-      else:
-        res = self._getCatalogs()
-      if not res['OK']:
-        self.valid = False
-      elif ( len( self.readCatalogs ) == 0 ) and ( len( self.writeCatalogs ) == 0 ):
-        self.valid = False
+
+    self.opHelper = Operations( vo = self.vo )
+    if type( catalogs ) in types.StringTypes:
+      catalogs = [catalogs]
+    if catalogs:
+      res = self._getSelectedCatalogs( catalogs )
     else:
+      res = self._getCatalogs()
+    if not res['OK']:
+      self.valid = False
+    elif ( len( self.readCatalogs ) == 0 ) and ( len( self.writeCatalogs ) == 0 ):
       self.valid = False
 
   def isOK( self ):
