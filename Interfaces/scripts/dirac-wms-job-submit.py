86e2c4c616b197b253478383e2d6632b4d61da33
Daniela Bauer <daniela.bauer.grid@googlemail.com>
2014-10-29 18:56:11 +0100

    added logfile option to match other dirac-wms-job commands

--- Interfaces/scripts/dirac-wms-job-submit.py
+++ Interfaces/scripts/dirac-wms-job-submit.py
@@ -11,11 +11,15 @@ __RCSID__ = "$Id$"
 import DIRAC
 from DIRAC.Core.Base import Script
 
+import os
+
 Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                      'Usage:',
                                      '  %s [option|cfgfile] ... JDL ...' % Script.scriptName,
                                      'Arguments:',
                                      '  JDL:      Path to JDL file' ] ) )
+
+Script.registerSwitch( "f:", "File=", "Writes job ids to file <value>" )
 Script.parseCommandLine( ignoreErrors = True )
 args = Script.getPositionalArgs()
 
@@ -27,14 +31,28 @@ dirac = Dirac()
 exitCode = 0
 errorList = []
 
+jFile = None
+for sw, value in Script.getUnprocessedSwitches():
+  if sw.lower() in ( 'f', 'file' ):
+    if os.path.isfile( value ):
+      print 'Appending job ids to existing logfile: %s' %value
+      if not os.access( value , os.W_OK ):
+        print 'Existing logfile %s must be writable by user.' %value
+    jFile = open( value, 'a' )
+
 for jdl in args:
 
   result = dirac.submit( jdl )
   if result['OK']:
     print 'JobID = %s' % ( result['Value'] )
+    if jFile != None:
+      jFile.write( str( result['Value'] )+'\n' )
   else:
     errorList.append( ( jdl, result['Message'] ) )
     exitCode = 2
+
+if jFile != None:
+  jFile.close()
 
 for error in errorList:
   print "ERROR %s: %s" % error
