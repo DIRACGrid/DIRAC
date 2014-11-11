b176de33840758d2631b2420a1007cf97c1545d4
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2014-11-11 20:50:57 +0100

    Bug fixed

--- DataManagementSystem/scripts/dirac-dms-remove-replicas.py
+++ DataManagementSystem/scripts/dirac-dms-remove-replicas.py
@@ -47,6 +47,7 @@ Usage:
       res = dm.removeReplica( storageElementName, lfnList )
       if not res['OK']:
         print 'Error:', res['Message']
+        continue
       for lfn in sortList( res['Value']['Successful'].keys() ):
         print 'Successfully removed %s replica of %s' % ( storageElementName, lfn )
       for lfn in sortList( res['Value']['Failed'].keys() ):
