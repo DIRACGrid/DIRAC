41e6fb48e7d63959ce7175fd8f842080a5ada99f
chaen <chaen@pclhcb31.cern.ch>
2014-11-18 08:56:17 +0100

    Remove unused files

--- RequestManagementSystem/private/RMSBase.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-
-# This is the common class instance all the RMS objects (Request, Operation, Files) have to inherit from
-RMSBase = declarative_base()
-RMSBase._datetimeFormat = '%Y-%m-%d %H:%M:%S'
-RMSBase.__table_args__ = {'mysql_engine':'InnoDB'}
