f26a95cca36dd613de6dd3bb818f32c90e934782
FedericoStagni <fstagni@cern.ch>
2015-02-16 10:38:29 +0100

    Added default RequestID

--- RequestManagementSystem/Client/Request.py
+++ RequestManagementSystem/Client/Request.py
@@ -89,6 +89,8 @@ class Request( object ):
     self.OwnerGroup = None
     self.SourceComponent = None
 
+    self.RequestID = 0
+
     proxyInfo = getProxyInfo()
     if proxyInfo["OK"]:
       proxyInfo = proxyInfo["Value"]
