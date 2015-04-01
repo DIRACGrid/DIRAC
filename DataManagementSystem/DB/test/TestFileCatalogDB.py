46c854de34485d3ab0d5cc5c7102aabad73f91a8
chaen <chaen@pclhcb31.cern.ch>
2015-04-01 11:18:06 +0200

    Bug fix with badly calculated storage usage tables

--- DataManagementSystem/DB/test/TestFileCatalogDB.py
+++ DataManagementSystem/DB/test/TestFileCatalogDB.py
@@ -477,6 +477,320 @@ class DirectoryCase( FileCatalogDBTestCase ):
 
 
 
+
+
+class DirectoryUsageCase ( FileCatalogDBTestCase ):
+
+  def getPhysicalSize(self, sizeDict, dirName, seName):
+    """ Extract the information from a ret dictionary
+        and return the tuple (files, size) for a given
+        directory and a se
+    """
+    try:
+      val = sizeDict[dirName]['PhysicalSize'][seName]
+      files = val['Files']
+      size = val['Size']
+      return ( files, size )
+    
+    except Exception as e:
+      raise
+
+  def getLogicalSize( self, sizeDict, dirName ):
+    """ Extract the information from a ret dictionary
+        and return the tuple (files, size) for a given
+        directory and a se
+    """
+    try:
+      files = sizeDict[dirName]['LogicalFiles']
+      size = sizeDict[dirName]['LogicalSize']
+      return ( files, size )
+
+    except Exception as e:
+#       print e
+#       print "sizeDict %s" % sizeDict
+#       print "dirName %s" % dirName
+      raise
+
+  def test_directoryUsage( self ):
+    """Testing DirectoryUsage related operation"""
+    # create SE
+    
+    d1 = '/sizeTest/d1'
+    d2 = '/sizeTest/d2'
+    f1 = d1 + '/f1'
+    f2 = d1 + '/f2'
+    f3 = d2 + '/f3'
+
+    f1Size = 3000000000
+    f2Size = 3000000001
+    f3Size = 3000000002
+
+#     f1Size = 1
+#     f2Size = 2
+#     f3Size = 5
+
+    for sen in ['se1', 'se2', 'se3']:
+      ret = self.db.addSE( sen, credDict )
+      self.assert_( ret["OK"] )
+
+    for din in [d1, d2]:
+      ret = self.db.createDirectory( din, credDict )
+      self.assert_( ret["OK"] )
+
+
+    ret = self.db.addFile( { f1: { 'PFN': 'f1se1',
+                                         'SE': 'se1' ,
+                                         'Size':f1Size,
+                                         'GUID':'1000',
+                                         'Checksum':'1' },
+                             f2: { 'PFN': 'f2se2',
+                                         'SE': 'se2' ,
+                                         'Size':f2Size,
+                                         'GUID':'1001',
+                                         'Checksum':'2' } }, credDict )
+
+
+    self.assert_( ret["OK"] )
+
+    ret = self.db.getDirectorySize( [d1, d2], True, False, credDict )
+    self.assert_( ret["OK"] )
+    val = ret['Value']['Successful']
+
+    d1s1 = self.getPhysicalSize( val, d1, 'se1' )
+    d1s2 = self.getPhysicalSize( val, d1, 'se2' )
+    d1l = self.getLogicalSize( val, d1 )
+
+    self.assertEqual( d1s1 , ( 1, f1Size ), "Unexpected size %s, expected %s" % ( d1s1, ( 1, f1Size ) ) )
+    self.assertEqual( d1s2 , ( 1, f2Size ), "Unexpected size %s, expected %s" % ( d1s2, ( 1, f2Size ) ) )
+    self.assertEqual( d1l , ( 2, f1Size + f2Size ), "Unexpected size %s, expected %s" % ( d1l, ( 2, f1Size + f2Size ) ) )
+
+    ret = self.db.addReplica( {f1 : {"PFN" : "f1se2", "SE" : "se2"},
+                               f2 : {"PFN" : "f1se3", "SE" : "se3"}},
+                              credDict )
+
+    self.assert_( ret['OK'] )
+
+    ret = self.db.getDirectorySize( [d1, d2], True, False, credDict )
+    self.assert_( ret["OK"] )
+    val = ret['Value']['Successful']
+
+    d1s1 = self.getPhysicalSize( val, d1, 'se1' )
+    d1s2 = self.getPhysicalSize( val, d1, 'se2' )
+    d1s3 = self.getPhysicalSize( val, d1, 'se3' )
+    d1l = self.getLogicalSize( val, d1 )
+
+    self.assertEqual( d1s1 , ( 1, f1Size ), "Unexpected size %s, expected %s" % ( d1s1, ( 1, f1Size ) ) )
+    self.assertEqual( d1s2 , ( 2, f1Size + f2Size ), "Unexpected size %s, expected %s" % ( d1s2, ( 2, f1Size + f2Size ) ) )
+    self.assertEqual( d1s3 , ( 1, f2Size ), "Unexpected size %s, expected %s" % ( d1s3, ( 1, f2Size ) ) )
+    self.assertEqual( d1l , ( 2, f1Size + f2Size ), "Unexpected size %s, expected %s" % ( d1l, ( 2, f1Size + f2Size ) ) )
+    
+    
+    ret = self.db.removeFile( [f1], credDict )
+    self.assert_( ret['OK'] )
+
+    ret = self.db.getDirectorySize( [d1, d2], True, False, credDict )
+    self.assert_( ret["OK"] )
+    val = ret['Value']['Successful']
+
+    # Here we should have the KeyError, since there are no files left on s1 in principle
+    try:
+      d1s1 = self.getPhysicalSize( val, d1, 'se1' )
+    except KeyError:
+      d1s1 = ( 0, 0 )
+    d1s2 = self.getPhysicalSize( val, d1, 'se2' )
+    d1s3 = self.getPhysicalSize( val, d1, 'se3' )
+    d1l = self.getLogicalSize( val, d1 )
+
+    self.assertEqual( d1s1 , ( 0, 0 ), "Unexpected size %s, expected %s" % ( d1s1, ( 0, 0 ) ) )
+    self.assertEqual( d1s2 , ( 1, f2Size ), "Unexpected size %s, expected %s" % ( d1s2, ( 1, f2Size ) ) )
+    self.assertEqual( d1s3 , ( 1, f2Size ), "Unexpected size %s, expected %s" % ( d1s3, ( 1, f2Size ) ) )
+    self.assertEqual( d1l , ( 1, f2Size ), "Unexpected size %s, expected %s" % ( d1l, ( 1, f2Size ) ) )
+
+
+    ret = self.db.removeReplica( {f2 : { "SE" : "se2"}}, credDict )
+    self.assert_( ret['OK'] )
+
+    ret = self.db.getDirectorySize( [d1, d2], True, False, credDict )
+    self.assert_( ret["OK"] )
+    val = ret['Value']['Successful']
+
+    # Here we should have the KeyError, since there are no files left on s1 in principle
+    try:
+      d1s2 = self.getPhysicalSize( val, d1, 'se1' )
+    except KeyError:
+      d1s2 = ( 0, 0 )
+    d1s3 = self.getPhysicalSize( val, d1, 'se3' )
+    d1l = self.getLogicalSize( val, d1 )
+
+    self.assertEqual( d1s2 , ( 0, 0 ), "Unexpected size %s, expected %s" % ( d1s2, ( 0, 0 ) ) )
+    self.assertEqual( d1s3 , ( 1, f2Size ), "Unexpected size %s, expected %s" % ( d1s3, ( 1, f2Size ) ) )
+    self.assertEqual( d1l , ( 1, f2Size ), "Unexpected size %s, expected %s" % ( d1l, ( 1, f2Size ) ) )
+    
+
+    ret = self.db.addFile( { f1: { 'PFN': 'f1se1',
+                                         'SE': 'se1' ,
+                                         'Size':f1Size,
+                                         'GUID':'1000',
+                                         'Checksum':'1' },
+                             f3: { 'PFN': 'f3se3',
+                                         'SE': 'se3' ,
+                                         'Size':f3Size,
+                                         'GUID':'1002',
+                                         'Checksum':'3' } }, credDict )
+
+
+    self.assert_( ret["OK"] )
+
+    ret = self.db.getDirectorySize( [d1, d2], True, False, credDict )
+    self.assert_( ret["OK"] )
+    val = ret['Value']['Successful']
+
+    d1s1 = self.getPhysicalSize( val, d1, 'se1' )
+    d1s3 = self.getPhysicalSize( val, d1, 'se3' )
+    d2s3 = self.getPhysicalSize( val, d2, 'se3' )
+    d1l = self.getLogicalSize( val, d1 )
+    d2l = self.getLogicalSize( val, d2 )
+
+    self.assertEqual( d1s1 , ( 1, f1Size ), "Unexpected size %s, expected %s" % ( d1s1, ( 1, f1Size ) ) )
+    self.assertEqual( d1s3 , ( 1, f2Size ), "Unexpected size %s, expected %s" % ( d1s3, ( 1, f2Size ) ) )
+    self.assertEqual( d2s3 , ( 1, f3Size ), "Unexpected size %s, expected %s" % ( d2s3, ( 1, f3Size ) ) )
+    self.assertEqual( d1l , ( 2, f1Size + f2Size ), "Unexpected size %s, expected %s" % ( d1l, ( 2, f1Size + f2Size ) ) )
+    self.assertEqual( d2l , ( 1, f3Size ), "Unexpected size %s, expected %s" % ( d2l, ( 1, f3Size ) ) )
+
+
+    ret = self.db.removeReplica( {f1 : { "SE" : "se1"}}, credDict )
+    self.assert_( ret['OK'] )
+
+    ret = self.db.getDirectorySize( [d1, d2], True, False, credDict )
+    self.assert_( ret["OK"] )
+    val = ret['Value']['Successful']
+
+
+    try:
+      d1s1 = self.getPhysicalSize( val, d1, 'se1' )
+    except KeyError:
+      d1s1 = ( 0, 0 )
+    d1s3 = self.getPhysicalSize( val, d1, 'se3' )
+    d2s3 = self.getPhysicalSize( val, d2, 'se3' )
+    d1l = self.getLogicalSize( val, d1 )
+    d2l = self.getLogicalSize( val, d2 )
+
+    self.assertEqual( d1s1 , ( 0, 0 ), "Unexpected size %s, expected %s" % ( d1s1, ( 0, 0 ) ) )
+    self.assertEqual( d1s3 , ( 1, f2Size ), "Unexpected size %s, expected %s" % ( d1s3, ( 1, f2Size ) ) )
+    self.assertEqual( d2s3 , ( 1, f3Size ), "Unexpected size %s, expected %s" % ( d2s3, ( 1, f3Size ) ) )
+    # This one is silly... there are no replicas of f1, but since the file is still there,
+    # the logical size does not change
+    self.assertEqual( d1l , ( 2, f1Size + f2Size ), "Unexpected size %s, expected %s" % ( d1l, ( 2, f1Size + f2Size ) ) )
+    self.assertEqual( d2l , ( 1, f3Size ), "Unexpected size %s, expected %s" % ( d2l, ( 1, f3Size ) ) )
+
+
+    ret = self.db.removeFile( [f1], credDict )
+    self.assert_( ret['OK'] )
+
+    ret = self.db.getDirectorySize( [d1, d2], True, False, credDict )
+    self.assert_( ret["OK"] )
+    val = ret['Value']['Successful']
+
+    try:
+      d1s1 = self.getPhysicalSize( val, d1, 'se1' )
+    except KeyError:
+      d1s1 = ( 0, 0 )
+
+    d1s3 = self.getPhysicalSize( val, d1, 'se3' )
+    d2s3 = self.getPhysicalSize( val, d2, 'se3' )
+    d1l = self.getLogicalSize( val, d1 )
+    d2l = self.getLogicalSize( val, d2 )
+
+    self.assertEqual( d1s1 , ( 0, 0 ), "Unexpected size %s, expected %s" % ( d1s1, ( 0, 0 ) ) )
+    self.assertEqual( d1s3 , ( 1, f2Size ), "Unexpected size %s, expected %s" % ( d1s3, ( 1, f2Size ) ) )
+    self.assertEqual( d2s3 , ( 1, f3Size ), "Unexpected size %s, expected %s" % ( d2s3, ( 1, f3Size ) ) )
+    self.assertEqual( d1l , ( 1, f2Size ), "Unexpected size %s, expected %s" % ( d1l, ( 1, f2Size ) ) )
+    self.assertEqual( d2l , ( 1, f3Size ), "Unexpected size %s, expected %s" % ( d2l, ( 1, f3Size ) ) )
+
+
+
+    ret = self.db.removeReplica( {f2 : { "SE" : "se3"},
+                                  f3 : { "SE" : "se3"}}, credDict )
+    self.assert_( ret['OK'] )
+
+    ret = self.db.getDirectorySize( [d1, d2], True, False, credDict )
+    self.assert_( ret["OK"] )
+    val = ret['Value']['Successful']
+
+
+    try:
+      d1s1 = self.getPhysicalSize( val, d1, 'se1' )
+    except KeyError:
+      d1s1 = ( 0, 0 )
+    try:
+      d1s3 = self.getPhysicalSize( val, d1, 'se3' )
+    except KeyError:
+      d1s3 = ( 0, 0 )
+    try:
+      d2s3 = self.getPhysicalSize( val, d2, 'se3' )
+    except KeyError:
+      d2s3 = ( 0, 0 )
+    d1l = self.getLogicalSize( val, d1 )
+    d2l = self.getLogicalSize( val, d2 )
+
+    self.assertEqual( d1s1 , ( 0, 0 ), "Unexpected size %s, expected %s" % ( d1s1, ( 0, 0 ) ) )
+    self.assertEqual( d1s3 , ( 0, 0 ), "Unexpected size %s, expected %s" % ( d1s3, ( 0, 0 ) ) )
+    self.assertEqual( d2s3 , ( 0, 0 ), "Unexpected size %s, expected %s" % ( d2s3, ( 0, 0 ) ) )
+    # This one is silly... there are no replicas of f1, but since the file is still there,
+    # the logical size does not change
+    self.assertEqual( d1l , ( 1, f2Size ), "Unexpected size %s, expected %s" % ( d1l, ( 1, f2Size ) ) )
+    self.assertEqual( d2l , ( 1, f3Size ), "Unexpected size %s, expected %s" % ( d2l, ( 1, f3Size ) ) )
+
+
+
+    ret = self.db.removeFile( [f2, f3], credDict )
+    self.assert_( ret['OK'] )
+
+    ret = self.db.getDirectorySize( [d1, d2], True, False, credDict )
+    self.assert_( ret["OK"] )
+    val = ret['Value']['Successful']
+
+
+    try:
+      d1s1 = self.getPhysicalSize( val, d1, 'se1' )
+    except KeyError:
+      d1s1 = ( 0, 0 )
+    try:
+      d1s3 = self.getPhysicalSize( val, d1, 'se3' )
+    except KeyError:
+      d1s3 = ( 0, 0 )
+    try:
+      d2s3 = self.getPhysicalSize( val, d2, 'se3' )
+    except KeyError:
+      d2s3 = ( 0, 0 )
+    d1l = self.getLogicalSize( val, d1 )
+    d2l = self.getLogicalSize( val, d2 )
+
+    self.assertEqual( d1s1 , ( 0, 0 ), "Unexpected size %s, expected %s" % ( d1s1, ( 0, 0 ) ) )
+    self.assertEqual( d1s3 , ( 0, 0 ), "Unexpected size %s, expected %s" % ( d1s3, ( 0, 0 ) ) )
+    self.assertEqual( d2s3 , ( 0, 0 ), "Unexpected size %s, expected %s" % ( d2s3, ( 0, 0 ) ) )
+    # This one is silly... there are no replicas of f1, but since the file is still there,
+    # the logical size does not change
+    self.assertEqual( d1l , ( 0, 0 ), "Unexpected size %s, expected %s" % ( d1l, ( 0, 0 ) ) )
+    self.assertEqual( d2l , ( 0, 0 ), "Unexpected size %s, expected %s" % ( d2l, ( 0, 0 ) ) )
+
+
+#     self.assert_( testDir in result["Value"]["Successful"], "getDirectorySize : %s should be in Successful %s" % ( testDir, result ) )
+#     self.assertEqual( result["Value"]["Successful"][testDir], {'LogicalFiles': 1, 'LogicalDirectories': 0, 'LogicalSize': 123}, "getDirectorySize got incorrect directory size %s" % result )
+#     self.assert_( nonExistingDir in result["Value"]["Failed"], "getDirectorySize : %s should be in Failed %s" % ( nonExistingDir, result ) )
+#
+#
+#     result = self.db.getDirectorySize( [testDir, nonExistingDir], False, True, credDict )
+#     self.assert_( result["OK"], "getDirectorySize (calc) failed: %s" % result )
+#     self.assert_( testDir in result["Value"]["Successful"], "getDirectorySize (calc): %s should be in Successful %s" % ( testDir, result ) )
+#     self.assertEqual( result["Value"]["Successful"][testDir], {'LogicalFiles': 1, 'LogicalDirectories': 0, 'LogicalSize': 123}, "getDirectorySize got incorrect directory size %s" % result )
+#     self.assert_( nonExistingDir in result["Value"]["Failed"], "getDirectorySize (calc) : %s should be in Failed %s" % ( nonExistingDir, result ) )
+
+
+
+
+
+
 if __name__ == '__main__':
 
   managerTypes = MANAGER_TO_TEST.keys()
@@ -496,6 +810,7 @@ if __name__ == '__main__':
     suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( FileCase ) )
     suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( ReplicaCase ) )
     suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( DirectoryCase ) )
+    suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( DirectoryUsageCase ) )
 
     testResult = unittest.TextTestRunner( verbosity = 2 ).run( suite )
 
