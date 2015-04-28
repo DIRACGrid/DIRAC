1efa8ce5eb6bbeec0d3e37d7cee4794d5717c594
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2015-04-28 20:15:05 +0200

    More undesirable metaKeys to be eliminated from replicas dict

--- WorkloadManagementSystem/Client/InputDataByProtocol.py
+++ WorkloadManagementSystem/Client/InputDataByProtocol.py
@@ -31,7 +31,9 @@ class InputDataByProtocol( object ):
     self.jobID = None
     self.storageElements = {}
     # This is because  replicas contain SEs and metadata keys!
-    self.metaKeys = set( ['ChecksumType', 'Checksum', 'NumberOfLinks', 'Mode', 'GUID', 'Status', 'ModificationDate', 'CreationDate', 'Size'] )
+    self.metaKeys = set( ['ChecksumType', 'Checksum', 'NumberOfLinks', 'Mode', 'GUID',
+                          'Status', 'ModificationDate', 'CreationDate', 'Size',
+                          'Owner', 'OwnerGroup', 'GID', 'UID'] )
 
   def __storageElement( self, seName ):
     return self.storageElements.setdefault( seName, StorageElement( seName ) )
