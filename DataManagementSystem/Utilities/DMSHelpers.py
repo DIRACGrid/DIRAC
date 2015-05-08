00b0595073efa1d1ea6907fc440aa38e1ca99e14
Philippe Charpentier <Philippe.Charpentier@cern.ch>
2015-05-08 14:39:28 +0200

    Change name to DMSHelpers Location still under discussion, but functionality is there

---
+++ DataManagementSystem/Utilities/DMSHelpers.py
@@ -0,0 +1,234 @@
+"""
+  This module contains helper methods for accessing operational attributes or parameters of DMS objects
+
+"""
+
+from DIRAC.ConfigurationSystem.Client.Helpers.Path      import cfgPath
+from DIRAC.ConfigurationSystem.Client.Helpers.Operations import Operations
+from DIRAC import gConfig, gLogger, S_OK, S_ERROR
+# from Core.Utilities import SitesDIRACGOCDBmapping
+
+LOCAL = 1
+PROTOCOL = 2
+DOWNLOAD = 3
+
+def resolveSEGroup( seGroupList ):
+  seList = []
+  if type( seGroupList ) != type( [] ):
+    seGroupList = [seGroupList]
+  for se in seGroupList:
+    seConfig = gConfig.getValue( '/Resources/StorageElementGroups/%s' % se, se )
+    if seConfig != se:
+      seList += [se.strip() for se in seConfig.split( ',' )]
+      # print seList
+    else:
+      seList.append( se )
+    res = gConfig.getSections( '/Resources/StorageElements' )
+    if not res['OK']:
+      gLogger.fatal( 'Error getting list of SEs from CS', res['Message'] )
+      return []
+    for se in seList:
+      if se not in res['Value']:
+        gLogger.fatal( '%s is not a valid SE' % se )
+        seList = []
+        break
+
+  return seList
+
+
+class DMSHelpers():
+
+  def __init__( self ):
+    self.local = LOCAL
+    self.protocol = PROTOCOL
+    self.download = DOWNLOAD
+    self.siteSEMapping = {}
+    self.storageElementSet = set()
+    self.siteSet = set()
+    self.__opsHelper = Operations()
+
+
+  def getSiteSEMapping( self ):
+    """ Returns a dictionary of all sites and their localSEs as a list, e.g.
+        {'LCG.CERN.ch':['CERN-RAW','CERN-RDST',...]}
+        If gridName is specified, result is restricted to that Grid type.
+    """
+    if self.siteSEMapping:
+      return S_OK( self.siteSEMapping )
+
+    siteSEMapping = {}
+    gridTypes = gConfig.getSections( 'Resources/Sites/' )
+    if not gridTypes['OK']:
+      gLogger.warn( 'Problem retrieving sections in /Resources/Sites' )
+      return gridTypes
+
+    gridTypes = gridTypes['Value']
+
+    gLogger.debug( 'Grid Types are: %s' % ( ', '.join( gridTypes ) ) )
+    # Get a list of sites and their local SEs
+    siteSet = set()
+    storageElementSet = set()
+    siteSEMapping[self.local] = {}
+    for grid in gridTypes:
+      result = gConfig.getSections( '/Resources/Sites/%s' % grid )
+      if not result['OK']:
+        gLogger.warn( 'Problem retrieving /Resources/Sites/%s section' % grid )
+        return result
+      sites = result['Value']
+      siteSet.update( sites )
+      for site in sites:
+        candidateSEs = gConfig.getValue( '/Resources/Sites/%s/%s/SE' % ( grid, site ), [] )
+        if candidateSEs:
+          siteSEMapping[self.local].setdefault( site, set() ).update( candidateSEs )
+          storageElementSet.update( candidateSEs )
+        else:
+          gLogger.debug( 'No SEs defined for site %s' % site )
+
+    # Add Sites from the SiteSEMappingByProtocol in the CS
+    siteSEMapping[self.protocol] = {}
+    cfgLocalSEPath = cfgPath( 'SiteSEMappingByProtocol' )
+    result = self.__opsHelper.getOptionsDict( cfgLocalSEPath )
+    if result['OK']:
+      sites = result['Value']
+      for site in sites:
+        candidates = self.__opsHelper.getValue( cfgPath( cfgLocalSEPath, site ), [] )
+        ses = set( candidates )
+        # If a candidate is a site, then all local SEs are eligible
+        for candidate in ses & siteSet:
+          ses.remove( candidate )
+          ses.update( siteSEMapping[self.local][candidate] )
+        siteSEMapping[self.protocol].setdefault( site, set() ).update( ses )
+
+    # Add Sites from the SiteSEMappingByDownload in the CS, else SiteLocalSEMapping (old convention)
+    siteSEMapping[self.download] = {}
+    cfgLocalSEPath = cfgPath( 'SiteSEMappingByDownload' )
+    result = self.__opsHelper.getOptionsDict( cfgLocalSEPath )
+    if not result['OK']:
+      cfgLocalSEPath = cfgPath( 'SiteLocalSEMapping' )
+      result = self.__opsHelper.getOptionsDict( cfgLocalSEPath )
+    if result['OK']:
+      sites = result['Value']
+      for site in sites:
+        candidates = self.__opsHelper.getValue( cfgPath( cfgLocalSEPath, site ), [] )
+        ses = set( candidates )
+        for candidate in ses & siteSet:
+          ses.remove( candidate )
+          ses.update( siteSEMapping[self.local][candidate] )
+        siteSEMapping[ self.download].setdefault( site, set() ).update( ses )
+
+    self.siteSEMapping = siteSEMapping
+    self.storageElementSet = storageElementSet
+    self.siteSet = siteSet
+    return S_OK( siteSEMapping )
+
+
+  def isSEFailover( self, storageElement ):
+    self.getSiteSEMapping()
+    if storageElement not in self.storageElementSet:
+      return False
+    seList = resolveSEGroup( self.__opsHelper.getValue( 'DataManagement/SEsUsedForFailover', [] ) )
+    return storageElement in resolveSEGroup( seList )
+
+  def isSEForJobs( self, storageElement ):
+    self.getSiteSEMapping()
+    if storageElement not in self.storageElementSet:
+      return False
+    seList = resolveSEGroup( self.__opsHelper.getValue( 'DataManagement/SEsNotToBeUsedForJobs', [] ) )
+    return storageElement not in resolveSEGroup( seList )
+
+  def isSEArchive( self, storageElement ):
+    self.getSiteSEMapping()
+    if storageElement not in self.storageElementSet:
+      return False
+    seList = resolveSEGroup( self.__opsHelper.getValue( 'DataManagement/SEsUsedForArchive', [] ) )
+    return storageElement in resolveSEGroup( seList )
+
+  def getSitesForSE( self, storageElement, connectionLevel = None ):
+    if not connectionLevel:
+      connectionLevel = self.download
+    if connectionLevel == self.local:
+      return self._getLocalSitesForSE( storageElement )
+    if connectionLevel == self.protocol:
+      return self.getProtocolSitesForSE( storageElement )
+    if connectionLevel == self.download:
+      return self.getDownloadSitesForSE( storageElement )
+    return S_ERROR( "Unknown connection level, connectionLevel" )
+
+  def getLocalSiteForSE( self, se ):
+    sites = self._getLocalSitesForSE( se )
+    if not sites['OK']:
+      return sites
+    return S_OK( sites['Value'][0] )
+
+  def _getLocalSitesForSE( self, se ):
+    mapping = self.getSiteSEMapping()
+    if not mapping['OK']:
+      return mapping
+    if se not in self.storageElementSet:
+      return S_ERROR( 'Non-existing SE' )
+    mapping = mapping['Value'][self.local]
+    sites = [site for site in mapping if se in mapping[site]]
+    if len( sites ) != 1:
+      return S_ERROR( 'SE is at more than one site' )
+    return S_OK( sites )
+
+  def getProtocolSitesForSE( self, se ):
+    mapping = self.getSiteSEMapping()
+    if not mapping['OK']:
+      return mapping
+    if se not in self.storageElementSet:
+      return S_ERROR( 'Non-existing SE' )
+    mapping = mapping['Value'][self.protocol]
+    sites = self._getLocalSitesForSE( se )
+    if not sites['OK']:
+      return sites
+    sites = set( sites['Value'] )
+    sites.update( [site for site in mapping if se in mapping[site]] )
+    return S_OK( sorted( sites ) )
+
+  def getDownloadSitesForSE( self, se ):
+    mapping = self.getSiteSEMapping()
+    if not mapping['OK']:
+      return mapping
+    if se not in self.storageElementSet:
+      return S_ERROR( 'Non-existing SE' )
+    mapping = mapping['Value'][self.download]
+    sites = self.getProtocolSitesForSE( se )
+    if not sites['OK']:
+      return sites
+    sites = set( sites['Value'] )
+    sites.update( [site for site in mapping if se in mapping[site]] )
+    return S_OK( sorted( sites ) )
+
+  def getSEsAtSite( self, site ):
+    mapping = self.getSiteSEMapping()
+    if not mapping['OK']:
+      return mapping
+    if site not in self.siteSet:
+      site = None
+      for s in self.siteSet:
+        if '.%s.' in s:
+          site = s
+          break
+    if site is None:
+      return S_ERROR( "Onknown site" )
+    ses = mapping['Value'][self.local].get( site, [] )
+    if not ses:
+      return S_ERROR( 'No SE at site' )
+    return S_OK( sorted( ses ) )
+
+  def getSEInGroupAtSite( self, seGroup, site ):
+    if type( seGroup ) == type( '' ):
+      seList = gConfig.getValue( '/Resources/StorageElementGroups/%s' % seGroup, [] )
+    else:
+      seList = list( seGroup )
+    if not seList:
+      return S_ERROR( 'SEGroup does not exist' )
+    sesAtSite = self.getSEsAtSite( site )
+    if not sesAtSite['OK']:
+      return sesAtSite
+    sesAtSite = sesAtSite['Value']
+    se = set( seList ) & set( sesAtSite )
+    if not se:
+      return S_ERROR( 'No SE found at that site' )
+    return S_OK( list( se )[0] )
