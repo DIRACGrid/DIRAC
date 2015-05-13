d522a5573f8a83315bed1478338fc863b0493aa1
Sbalbp <sbalbp@gmail.com>
2015-05-13 20:58:32 +0200

    Modified DB populating script
    The script now allows the -e/--exclude flag followed by a
    comma-separated list of hosts that will not be taken into consideration
    by the script. Also, the script will no longer attempt to register
    components from hosts that are unreachable and will send a notification
    mail in this case.

--- FrameworkSystem/scripts/dirac-populate-component-db.py
+++ FrameworkSystem/scripts/dirac-populate-component-db.py
@@ -2,8 +2,8 @@
 
 """
 Populates the database with the current installations of components
-This script assumes that both the InstalledComponentsDB and the
-ComponentMonitoring service are installed and running
+This script assumes that the InstalledComponentsDB, the
+ComponentMonitoring service and the Notification service are installed and running
 """
 
 __RCSID__ = "$Id$"
@@ -11,76 +11,92 @@ __RCSID__ = "$Id$"
 import sys
 from datetime import datetime
 from DIRAC import exit as DIRACexit
+from DIRAC import S_OK, gLogger
 from DIRAC.ConfigurationSystem.Client.CSAPI import CSAPI
 from DIRAC.ConfigurationSystem.Client.Helpers.CSGlobals import getSetup
 from DIRAC.Core.Base import Script
+from DIRAC.FrameworkSystem.Client.NotificationClient    import NotificationClient
 from DIRAC.FrameworkSystem.Client.SystemAdministratorIntegrator \
   import SystemAdministratorIntegrator
 from DIRAC.FrameworkSystem.Client.ComponentMonitoringClient \
   import ComponentMonitoringClient
+from DIRAC.ConfigurationSystem.Client.Helpers.Operations import Operations
+
+global excludedHosts
+excludedHosts = []
+
+def setExcludedHosts( value ):
+  global excludedHosts
+
+  excludedHosts = value.split( ',' )
+  return S_OK()
+
+Script.registerSwitch( "e:", "exclude=", "Comma separated list of hosts to be excluded from the scanning process", setExcludedHosts )
+
 
 Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                         'Usage:',
-                        '  %s [option|cfgfile] ... [debug]' % Script.scriptName,
-                        'Arguments:',
-                        '  debug:  Debug mode on/off'] ) )
+                        '  %s [option|cfgfile] ... [debug]' % Script.scriptName ] ) )
 
-Script.parseCommandLine()
+Script.parseCommandLine( ignoreErrors = False )
 args = Script.getPositionalArgs()
-
-if len( args ) > 0 and args[0] == 'debug':
-  debug = True
-else:
-  debug = False
 
 componentType = ''
 
 # Retrieve information from all the hosts
 client = SystemAdministratorIntegrator()
 resultAll = client.getOverallStatus()
+
+for host in resultAll[ 'Value' ]:
+  if not resultAll[ 'Value' ][ host ][ 'OK' ]:
+    # If the host cannot be contacted, exclude it and send message
+    excludedHosts.append( host )
+    notificationClient = NotificationClient()
+
+    result = notificationClient.sendMail( Operations().getValue( 'EMail/Production', [] ), 'Unreachable host', '\ndirac-populate-component-db: Could not fill the database with the components from unreachable host %s\n' % host )
+    if not result[ 'OK' ]:
+      gLogger.error( 'Can not send unreachable host notification mail: %s' % result[ 'Message' ] )
+
 if not resultAll[ 'OK' ]:
-  print resultAll[ 'Message' ]
+  gLogger.error( resultAll[ 'Message' ] )
   DIRACexit( -1 )
 resultHosts = client.getHostInfo()
 if not resultHosts[ 'OK' ]:
-  print resultHosts[ 'Message' ]
+  gLogger.error( resultHosts[ 'Message' ] )
   DIRACexit( -1 )
 resultInfo = client.getInfo()
 if not resultInfo[ 'OK' ]:
-  print resultInfo[ 'Message' ]
+  gLogger.error( resultInfo[ 'Message' ] )
   DIRACexit( -1 )
 resultAllDB = client.getDatabases()
 if not resultAllDB[ 'OK' ]:
-  print resultAllDB[ 'Message' ]
+  gLogger.error( resultAllDB[ 'Message' ] )
   DIRACexit( -1 )
 resultAvailableDB = client.getAvailableDatabases()
 if not resultAvailableDB[ 'OK' ]:
-  print resultAvailableDB[ 'Message' ]
+  gLogger.error( resultAvailableDB[ 'Message' ] )
   DIRACexit( -1 )
 
 
 records = []
-for host in resultAll[ 'Value' ]:
+finalSet = list( set( resultAll[ 'Value' ] ) - set( excludedHosts ) )
+for host in finalSet:
   result = resultAll[ 'Value' ][ host ]
   hostResult = resultHosts[ 'Value' ][ host ]
   allDBResult = resultAllDB[ 'Value' ][ host ]
   availableDBResult = resultAvailableDB[ 'Value' ][ host ]
 
   if not result[ 'OK' ]:
-    if debug:
-      print 'Host %s: %s' % ( host, result[ 'Message' ] )
+    gLogger.error( 'Host %s: %s' % ( host, result[ 'Message' ] ) )
     continue
   elif not hostResult[ 'OK' ]:
-    if debug:
-      print 'Host %s: %s' % ( host, hostResult[ 'Message' ] )
+    gLogger.error( 'Host %s: %s' % ( host, hostResult[ 'Message' ] ) )
     continue
   elif not allDBResult[ 'OK' ]:
-    if debug:
-      print 'Host %s: %s' % ( host, allDBResult[ 'Message' ] )
+    gLogger.error( 'Host %s: %s' % ( host, allDBResult[ 'Message' ] ) )
     continue
   elif not availableDBResult[ 'OK' ]:
-    if debug:
-      print 'Host %s: %s' % ( host, availableDBResult[ 'Message' ] )
+    gLogger.error( 'Host %s: %s' % ( host, availableDBResult[ 'Message' ] ) )
     continue
 
   cpu = hostResult[ 'Value' ][ 'CPUModel' ].strip()
@@ -139,11 +155,12 @@ for host in resultAll[ 'Value' ]:
         record[ 'Installation' ][ 'InstallationTime' ] = datetime.utcnow()
         records.append( record )
 
+
 monitoringClient = ComponentMonitoringClient()
 
 # Add the installations to the database
 for record in records:
   result = monitoringClient.addInstallation \
     ( record[ 'Installation' ], record[ 'Component' ], record[ 'Host' ], True )
-  if not result[ 'OK' ] and debug:
-    print result[ 'Message' ]
+  if not result[ 'OK' ]:
+    gLogger.error( result[ 'Message' ] )
