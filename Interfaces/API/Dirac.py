b1facca7e8360be3bbb34080e08100cf5aa9bf18
FedericoStagni <fstagni@cern.ch>
2015-06-03 10:49:14 +0200

    pylint

--- Interfaces/API/Dirac.py
+++ Interfaces/API/Dirac.py
@@ -15,8 +15,18 @@
 """
 __RCSID__ = "$Id$"
 
-import re, os, sys, time, shutil, tempfile, glob, tarfile, urllib
+import re
+import os
+import sys
+import time
+import shutil
+import tempfile
+import glob
+import tarfile
+import urllib
+
 import DIRAC
+from DIRAC                                               import gConfig, gLogger, S_OK, S_ERROR
 
 from DIRAC.Core.Base.API                                 import API
 from DIRAC.Interfaces.API.JobRepository                  import JobRepository
@@ -25,7 +35,6 @@ from DIRAC.Core.Utilities.Subprocess                     import shellCall
 from DIRAC.Core.Utilities.ModuleFactory                  import ModuleFactory
 from DIRAC.WorkloadManagementSystem.Client.WMSClient     import WMSClient
 from DIRAC.WorkloadManagementSystem.Client.SandboxStoreClient     import SandboxStoreClient
-# from DIRAC.DataManagementSystem.Client.ReplicaManager    import ReplicaManager
 from DIRAC.DataManagementSystem.Client.DataManager       import DataManager
 from DIRAC.Resources.Storage.StorageElement              import StorageElement
 from DIRAC.Resources.Catalog.FileCatalog                 import FileCatalog
@@ -40,7 +49,6 @@ from DIRAC.Core.Base.AgentReactor                        import AgentReactor
 from DIRAC.Core.Security.X509Chain                       import X509Chain
 from DIRAC.Core.Security                                 import Locations
 from DIRAC.Core.Utilities                                import Time
-from DIRAC                                               import gConfig, gLogger, S_OK, S_ERROR
 from DIRAC.Core.Utilities.PrettyPrint                    import printTable
 
 
