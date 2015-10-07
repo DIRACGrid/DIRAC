4584b83cf50711541ed1d941feabfaf9f5d130e6
Zoltan Mathe <zoltan.mathe@cern.ch>
2015-10-07 11:15:07 +0200

    change the name of the Monitoring package

--- MonitoringSystem/service/ReportGeneratorHandler.py
+++ MonitoringSystem/service/ReportGeneratorHandler.py
@@ -9,10 +9,20 @@ It creates the reports using Elasticsearch
 
 from DIRAC.Core.DISET.RequestHandler import RequestHandler
 from DIRAC import gLogger, gConfig, S_OK, S_ERROR
+from type import StringType
 
 class ReportGeneratorHandler( RequestHandler ):
-
+  
+  __db = None
+  
   @classmethod
   def initializeHandler( cls, serviceInfo ):
+    cls.__db = None
     return S_OK()
-
+  
+  types_echo = [StringType]
+  @staticmethod
+  def export_echo( inputstring ):
+    """ Echo input to output
+    """
+    return S_OK( inputstring )
