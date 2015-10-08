7c44ce2000e4d9506e0ed32a0374cff44aec5b6d
Zoltan Mathe <zoltan.mathe@cern.ch>
2015-10-08 15:07:31 +0200

    improve the database management

--- MonitoringSystem/DB/MonitoringDB.py
+++ MonitoringSystem/DB/MonitoringDB.py
@@ -3,9 +3,61 @@
 __RCSID__ = "$Id$"
 
 from DIRAC.Core.Base.ElasticDB import ElasticDB
+from DIRAC.MonitoringSystem.private.TypeLoader import TypeLoader
 from DIRAC import S_OK, S_ERROR, gConfig
 
 class MonitoringDB( ElasticDB ):
 
   def __init__( self, name = 'Monitoring/MonitoringDB' ):
     ElasticDB.__init__( self, 'MonitoringDB', name )
+    self.__loadIndexes()
+    
+  def __loadIndexes(self):
+    print 'DSDHSJDGJSH'
+    retVal = gConfig.getSections( "/DIRAC/Setups" )
+    if not retVal[ 'OK' ]:
+      return S_ERROR( "Can't get a list of setups: %s" % retVal[ 'Message' ] )
+    setupsList = retVal[ 'Value' ]
+    objectsLoaded = TypeLoader().getTypes()
+    print "DSDSDS", objectsLoaded
+
+    
+    #Load the files
+    for pythonClassName in sorted( objectsLoaded ):
+      typeClass = objectsLoaded[ pythonClassName ]
+      for setup in setupsList:
+        typeName = "%s_%s" % ( setup, pythonClassName )
+        
+        print typeName
+        """
+        typeDef = typeClass().getDefinition()
+        #dbTypeName = "%s_%s" % ( setup, typeName )
+        definitionKeyFields, definitionAccountingFields, bucketsLength = typeDef[1:]
+        #If already defined check the similarities
+        if typeName in self.dbCatalog:
+          bucketsLength.sort()
+          if bucketsLength != self.dbBucketsLength[ typeName ]:
+            bucketsLength = self.dbBucketsLength[ typeName ]
+            self.log.warn( "Bucket length has changed for type %s" % typeName )
+          keyFields = [ f[0] for f in definitionKeyFields ]
+          if keyFields != self.dbCatalog[ typeName ][ 'keys' ]:
+            keyFields = self.dbCatalog[ typeName ][ 'keys' ]
+            self.log.error( "Definition fields have changed", "Type %s" % typeName )
+          valueFields = [ f[0] for f in definitionAccountingFields ]
+          if valueFields != self.dbCatalog[ typeName ][ 'values' ]:
+            valueFields = self.dbCatalog[ typeName ][ 'values' ]
+            self.log.error( "Accountable fields have changed", "Type %s" % typeName )
+        #Try to re register to check all the tables are there
+        retVal = self.registerType( typeName, definitionKeyFields,
+                                    definitionAccountingFields, bucketsLength )
+        if not retVal[ 'OK' ]:
+          self.log.error( "Can't register type", "%s: %s" % ( typeName, retVal[ 'Message' ] ) )
+        #If it has been properly registered, update info
+        elif retVal[ 'Value' ]:
+          #Set the timespan
+          self.dbCatalog[ typeName ][ 'dataTimespan' ] = typeClass().getDataTimespan()
+          self.dbCatalog[ typeName ][ 'definition' ] = { 'keys' : definitionKeyFields,
+                                                         'values' : definitionAccountingFields }
+                                                         """
+                                                         
+    
---
+++ MonitoringSystem/private/TypeLoader.py
@@ -0,0 +1,52 @@
+########################################################################
+# $Id: $
+########################################################################
+
+"""
+It is used to load the monitoring types. 
+
+"""
+
+__RCSID__ = "$Id$"
+
+
+from DIRAC.Core.Utilities                           import DIRACSingleton
+from DIRAC.MonitoringSystem.Client.Types.BaseType   import BaseType
+from DIRAC.AccountingSystem.private.ObjectLoader    import loadObjects
+
+import re
+
+########################################################################
+class TypeLoader( object ):
+  
+  """
+  .. class:: BaseType
+  :param DIRACSingleton metaclass : this is a singleton
+  :param dict loaded: it stores the loaded classes
+  :param srt path: The location of the classes
+  :param BaseType parentCls: it is the parent class
+  :param regexp: regular expretion...
+  """
+  __metaclass__ = DIRACSingleton.DIRACSingleton
+  __loaded = {}
+  __path = ""
+  __parentCls = None
+  __reFilter = None
+  
+  ########################################################################
+  def __init__( self ):
+    """c'tor
+    """
+    self.__loaded = {}
+    self.__path = "MonitoringSystem/Client/Types"
+    self.__parentCls = BaseType
+    self.__reFilter = re.compile( ".*[a-z1-9]\.py$" )
+
+  ########################################################################
+  def getTypes( self ):
+    """
+    It returns all monitoring classes
+    """
+    if not self.__loaded:
+      self.__loaded = loadObjects( self.__path, self.__reFilter, self.__parentCls )
+    return self.__loaded
