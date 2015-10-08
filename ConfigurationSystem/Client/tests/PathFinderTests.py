03004ed331e233e8e671d1e76fd0e48569e3ac7b
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2015-10-08 12:02:37 +0200

    Change tests for PathFinder to use any setup

--- ConfigurationSystem/Client/tests/PathFinderTests.py
+++ ConfigurationSystem/Client/tests/PathFinderTests.py
@@ -2,51 +2,55 @@
 """
 
 import unittest
-from DIRAC.ConfigurationSystem.Client.PathFinder import getEntitySection
+from DIRAC.ConfigurationSystem.Client.PathFinder import getComponentSection
 from DIRAC.ConfigurationSystem.Client.PathFinder import getConsumerSection
+from DIRAC import gConfig
 
 class TestPathFinder( unittest.TestCase ):
   def setUp( self ):
-    pass
+    self.setup = gConfig.getValue( '/DIRAC/Setup', '' )
+    self.wm = gConfig.getValue('DIRAC/Setups/' + self.setup +'/WorkloadManagement', '')
   def tearDown( self ):
     pass
 
-class TestGetEntitySection( TestPathFinder ):
-
+class TestGetComponentSection( TestPathFinder ):
   def test_success( self ):
-    result = getEntitySection("WorkloadManagement/SimpleLogConsumer",False, False,"Consumers")
-    self.assertEqual(result, "/Systems/WorkloadManagement/DevInstance/Consumers/SimpleLogConsumer")
+    result = getComponentSection('WorkloadManagement/SimpleLogConsumer',False, False,'Consumers')
+    correctResult = '/Systems/WorkloadManagement/' + self.wm + '/Consumers/SimpleLogConsumer'
+    self.assertEqual(result, correctResult)
 
   def test_success_2( self ):
-    result = getEntitySection("WorkloadManagement/SandboxStoreHandler",False, False,"Services")
-    self.assertEqual(result, "/Systems/WorkloadManagement/DevInstance/Services/SandboxStoreHandler")
+    result = getComponentSection('WorkloadManagement/SandboxStoreHandler',False, False,'Services')
+    correctResult = '/Systems/WorkloadManagement/' + self.wm + '/Services/SandboxStoreHandler'
+    self.assertEqual(result, correctResult)
 
   def test_successConsumerDoesNotExist( self ):
-    """ tricky case one could expect that if the consumer module does exist
+    """ tricky case one could expect that if the consumer module does not exist
         than some kind of error will be returned, but it is not the case
     """
-    result = getEntitySection("WorkloadManagement/NoExistantConsumer",False, False,"Consumers")
-    self.assertEqual(result,"/Systems/WorkloadManagement/DevInstance/Consumers/NoExistantConsumer")
+    result = getComponentSection('WorkloadManagement/NoExistantConsumer',False, False,'Consumers')
+    correctResult = '/Systems/WorkloadManagement/' + self.wm + '/Consumers/NoExistantConsumer'
+    self.assertEqual(result, correctResult)
 
-  def test_sucessEntityStringDoesNotExist( self ):
+  def test_sucessComponentStringDoesNotExist( self ):
     """ tricky case one could expect that if entity string is wrong
         than some kind of error will be returned, but it is not the case
     """
-    result = getEntitySection("WorkloadManagement/SimpleLogConsumer",False, False,"NonRonsumersNon")
-    self.assertEqual(result,"/Systems/WorkloadManagement/DevInstance/NonRonsumersNon/SimpleLogConsumer")
+    result = getComponentSection('WorkloadManagement/SimpleLogConsumer',False, False,'NonRonsumersNon')
+    correctResult = '/Systems/WorkloadManagement/' + self.wm + '/NonRonsumersNon/SimpleLogConsumer'
+    self.assertEqual(result, correctResult)
 
   def test_failureSystemDoesNotExist( self ):
-    self.assertRaises(RuntimeError,lambda:getEntitySection("NullSystem/SimpleLogConsumer",False, False,"Consumers"))
-
+    self.assertRaises(RuntimeError,lambda:getComponentSection('NullSystem/SimpleLogConsumer',False, False,'Consumers'))
 
 class TestGetConsumerSection( TestPathFinder ):
-
   def test_success( self ):
-    result = getConsumerSection("WorkloadManagement/SimpleLogConsumer",False, False)
-    self.assertEqual(result, "/Systems/WorkloadManagement/DevInstance/Consumers/SimpleLogConsumer")
+    result = getConsumerSection('WorkloadManagement/SimpleLogConsumer',False, False)
+    correctResult = '/Systems/WorkloadManagement/' + self.wm + '/Consumers/SimpleLogConsumer'
+    self.assertEqual(result, correctResult)
 
 if __name__ == '__main__':
   suite = unittest.defaultTestLoader.loadTestsFromTestCase( TestPathFinder )
-  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( TestGetEntitySection ) )
+  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( TestGetComponentSection ) )
   suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( TestGetConsumerSection ) )
   testResult = unittest.TextTestRunner( verbosity = 2 ).run( suite )
