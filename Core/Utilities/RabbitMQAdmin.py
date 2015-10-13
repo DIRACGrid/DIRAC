99bf4243ce66fceced7aa912ae5b48c0cfed826b
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2015-10-13 17:03:47 +0200

    Add stub of RabbitMQAdmin module to manage the internal RabbitMQ database

---
+++ Core/Utilities/RabbitMQAdmin.py
@@ -0,0 +1,28 @@
+"""RabbitMQAdmin module serves for the management of the internal RabbitMQ
+   users database. It uses rabbitmqctl command. Only the user with the right
+   permissions can execute those commands
+"""
+from subprocess import call
+
+def executeCommand(command, args):
+  #ret = call(['rabbitmqctl', 'list_users'])
+  #print ret
+  pass 
+
+def addUser(user):
+  pass 
+
+def deleteUser(user):
+  pass
+
+def getAllUsers():
+  pass
+
+def addUsers(users):
+  pass
+
+def deleteUsers(users):
+  pass
+
+def setPermissions(user, permissions):
+  pass 
