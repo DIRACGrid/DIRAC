eda15694f369b3ee948f7ccdd21814d47c481fce
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2015-10-14 16:11:23 +0200

    Clean  RabbitMQSyncHandler. I am not sure if this is enough to make it work.

--- WorkloadManagementSystem/Service/RabbitMQSyncHandler.py
+++ WorkloadManagementSystem/Service/RabbitMQSyncHandler.py
@@ -1,37 +1,31 @@
-""" RabbitMQSync Service is an example of how to build services in the DIRAC framework
+""" RabbitMQSync service to synchronize the internal RabbitMQ database.
+    according to CS content. The whole work is done by the RabbitMQSynchronizer
+    that is activated when the CS was changed.
 """
 
 __RCSID__ = "$Id: $"
 
-import types
 from DIRAC.Core.DISET.RequestHandler import RequestHandler
-from DIRAC import gLogger, S_OK, S_ERROR
-from DIRAC.Core.Utilities import Time
+from DIRAC import S_OK
 from DIRAC import gConfig
-from DIRAC.ResourceStatusSystem.Utilities import Synchronizer
+from DIRAC.WorkloadManagementSystem.Utilities import RabbitMQSynchronizer
+
 class RabbitMQSyncHandler( RequestHandler ):
+  """Service to synchronize the content of internal RabbitMQ database
+     with the CS content. The work is done by the RabbitMQSynchronizer
+     that acts when the CS is changed.
+  """
 
   @classmethod
-  def initializeHandler( cls, serviceInfo ):
+  def initializeHandler( cls ):
     """ Handler initialization
     """
-    syncObject = Synchronizer.Synchronizer()
+    syncObject = RabbitMQSynchronizer.RabbitMQSynchronizer()
     gConfig.addListenerToNewVersionEvent( syncObject.sync )
-
-    cls.defaultWhom = "World"
     return S_OK()
 
   def initialize(self):
     """ Response initialization
     """
-    self.requestDefaultWhom = self.srv_getCSOption( "DefaultWhom", RabbitMQSyncHandler.defaultWhom )
-
-  auth_sayRabbitMQSync = [ 'all' ]
-  types_sayRabbitMQSync = [ types.StringTypes ]
-  def export_sayRabbitMQSync( self, whom ):
-    """ Say hello to somebody
-    """
-    if not whom:
-      whom = self.requestDefaultWhom
-    return S_OK( "RabbitMQSync " + whom )
+    pass
 
