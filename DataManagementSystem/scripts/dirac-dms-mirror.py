9121ecdb383bd4b96f639b0983aa6f087a726065
Marko Petric <marko.petric@cern.ch>
2015-10-15 17:12:48 +0200

    Cleanup

--- DataManagementSystem/scripts/dirac-dms-mirror.py
+++ DataManagementSystem/scripts/dirac-dms-mirror.py
@@ -5,8 +5,11 @@
 ########################################################################
 """
 Provides basic rsync functionality for DIRAC
+
 Mirrors the source destination folder recursivly into the target destination
-If option --sync is used contend that is not in the source directory but is only in the target directory will be deleted.
+
+If option --sync is used contend that is not in the source directory but is
+only in the target directory will be deleted.
 """
 
 __RCSID__ = "$Id$"
@@ -32,7 +35,7 @@ Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                  )
                       )
 
-Script.registerSwitch( "D" , "sync" , "     Make target directory identical to source. Files in target directory will be deleted" )
+Script.registerSwitch( "D" , "sync" , "Make target directory identical to source" )
 Script.parseCommandLine( ignoreErrors = False )
 
 args = Script.getPositionalArgs()
@@ -44,7 +47,7 @@ for switch in Script.getUnprocessedSwitches():
   if switch[0].lower() == "s" or switch[0].lower() == "sync":
     sync = True
 
-  
+
 from DIRAC import S_OK, S_ERROR
 from DIRAC import gConfig
 from DIRAC.Resources.Catalog.FileCatalogFactory import FileCatalogFactory
@@ -93,24 +96,24 @@ def getFileCatalog():
     Returns the DIRAC file catalog
   """
   fcType = gConfig.getValue("/LocalSite/FileCatalog","")
-  
+
   res = gConfig.getSections("/Resources/FileCatalogs",listOrdered = True)
   if not res['OK']:
     return S_ERROR(res['Message'])
-    
+
   if not fcType:
     if res['OK']:
       fcType = res['Value'][0]
-  
+
   if not fcType:
     return S_ERROR("No file catalog given and defaults could not be obtained")
-  
+
   result = FileCatalogFactory().createCatalog(fcType)
   if not result['OK']:
     return S_ERROR(result['Message'])
-    
+
   fc = result['Value']
-  
+
   return S_OK(fc)
 
 
@@ -151,22 +154,22 @@ def getSetOfRemoteDirectoriesAndFiles(fc, path):
 
   for myfile in files:
     return_files.add((myfile[0].replace(path,'').lstrip('/'),myfile[1]))
-    
+
   for mydirectory in directories:
     return_directories.add(mydirectory.replace(path,'').lstrip('/'))
 
   tree = {}
   tree["Directories"]=return_directories
   tree["Files"]=return_files
-  
+
   return S_OK(tree)
 
 def isInFileCatalog(fc, path ):
   """
   Check if the file is in the File Catalog
   """
-  
-  result = fc.listDirectory(path) 
+
+  result = fc.listDirectory(path)
   if result['OK']:
     if result['Value']['Successful']:
       return S_OK()
@@ -183,27 +186,27 @@ def getContentToSync(upload, fc, source_dir, dest_dir):
       return S_ERROR(res['Message'])
     to_dirs = res['Value']['Directories']
     to_files =  res['Value']['Files']
-    
+
     res = getSetOfLocalDirectoriesAndFiles(source_dir)
     if not res['OK']:
       return S_ERROR(res['Message'])
     from_dirs = res['Value']['Directories']
     from_files =  res['Value']['Files']
-    
+
   else:
     res = getSetOfLocalDirectoriesAndFiles(dest_dir)
     if not res['OK']:
       return S_ERROR(res['Message'])
     to_dirs = res['Value']['Directories']
     to_files =  res['Value']['Files']
-    
+
     res = getSetOfRemoteDirectoriesAndFiles(fc, source_dir)
     if not res['OK']:
       return S_ERROR(res['Message'])
     from_dirs = res['Value']['Directories']
     from_files =  res['Value']['Files']
-    
-  
+
+
   dirs_delete = list(to_dirs - from_dirs)
   dirs_delete.sort(key = lambda s: -s.count('/'))
   dirs_create = list(from_dirs - to_dirs)
@@ -223,10 +226,10 @@ def getContentToSync(upload, fc, source_dir, dest_dir):
   tree = {}
   tree["Create"]=create
   tree["Delete"]=delete
-  
+
   return S_OK(tree)
-  
-  
+
+
 def removeRemoteFiles(dm,lfns):
   """
   Remove file from the catalog
@@ -237,8 +240,8 @@ def removeRemoteFiles(dm,lfns):
       return S_ERROR( "Failed to remove data", res['Message'] )
     else:
       return S_OK()
-    
-  
+
+
 def uploadLocalFile(dm, lfn, localfile, storage):
   """
     Upload a local file to a storage element
@@ -258,17 +261,17 @@ def downloadRemoteFile(dm, lfn, destination):
     return S_ERROR( 'Error: failed to download %s ' % lfn )
   else:
     return S_OK( 'Successfully uploaded file %s' % lfn )
-  
+
 def removeStorageDirectoryFromSE( directory, storageElement ):
   """
   Delete directory on selected storage element
   """
-  
+
   se = StorageElement( storageElement, False )
   res = returnSingleResult( se.exists( directory ) )
 
   if not res['OK']:
-    return S_ERROR( "Failed to obtain existence of directory", res['Message'] )
+    return S_ERROR( "Failed to obtain existence of directory" + res['Message'] )
 
   exists = res['Value']
   if not exists:
@@ -276,16 +279,16 @@ def removeStorageDirectoryFromSE( directory, storageElement ):
 
   res = returnSingleResult( se.removeDirectory( directory, recursive = True ) )
   if not res['OK']:
-    return S_ERROR( "Failed to remove storage directory", res['Message'] )
-  
+    return S_ERROR( "Failed to remove storage directory" + res['Message'] )
+
   return S_OK()
-    
+
 def removeRemoteDirectory(fc,lfn):
   """
   Remove file from the catalog
   """
   storageElements = gConfig.getValue( 'Resources/StorageElementGroups/SE_Cleaning_List', [] )
-  
+
   for storageElement in sorted( storageElements ):
     res = removeStorageDirectoryFromSE( lfn, storageElement )
     if not res['OK']:
@@ -293,7 +296,7 @@ def removeRemoteDirectory(fc,lfn):
   res = returnSingleResult( fc.removeDirectory( lfn, recursive = True ) )
   if not res['OK']:
     return S_ERROR( "Failed to clean storage directory at all SE:" + res['Message'] )
-  
+
   return S_OK("Successfully removed directory")
 
 
@@ -359,23 +362,23 @@ def doUpload(fc, dm, result, source_dir, dest_dir, storage, delete):
       res = removeRemoteFiles(dm,lfns)
       if not res['OK']:
         return S_ERROR('Failed to remove files: ' + lfns + res['Message'])
-    
+
     for _directory in result['Value']['Delete']['Directories']:
       res = removeRemoteDirectory(fc, dest_dir + "/" + _directory)
       if not res['OK']:
         return S_ERROR('Failed to remove directory: '+ _directory + res['Message'])
-        
-  
+
+
   for _directory in result['Value']['Create']['Directories']:
     res = createRemoteDirectory(fc, dest_dir+"/"+ _directory)
     if not res['OK']:
       return S_ERROR('Directory creation failed: ' + res['Message'])
-        
+
   for _file in result['Value']['Create']['Files']:
     res = uploadLocalFile(dm, dest_dir+"/"+_file, source_dir+"/"+_file, storage)
     if not res['OK']:
       return S_ERROR('Upload of file: ' + _file + ' failed ' + res['Message'])
-  
+
   return S_OK('Upload finished successfully')
 
 def doDownload(dm, result, source_dir, dest_dir, delete):
@@ -387,40 +390,40 @@ def doDownload(dm, result, source_dir, dest_dir, delete):
       res = removeLocalFile(dest_dir+"/"+ _file)
       if not res['OK']:
         return S_ERROR('Deleting of file: ' + _file + ' failed ' + res['Message'])
-      
+
     for _directory in result['Value']['Delete']['Directories']:
       res = removeLocaDirectory( dest_dir + "/" + _directory )
       if not res['OK']:
         return S_ERROR('Deleting of directory: ' + _directory + ' failed ' + res['Message'])
-    
+
   for _directory in result['Value']['Create']['Directories']:
     res = createLocalDirectory( dest_dir+"/"+ _directory )
     if not res['OK']:
       return S_ERROR('Creation of directory: ' + _directory + ' failed ' + res['Message'])
-        
+
   for _file in result['Value']['Create']['Files']:
     res = downloadRemoteFile(dm, source_dir + "/" + _file, dest_dir + ("/" + _file).rsplit("/", 1)[0])
     if not res['OK']:
       return S_ERROR('Download of file: ' + _file + ' failed ' + res['Message'])
-  
+
   return S_OK('Upload finished successfully')
 
 def syncDestinations(upload, source_dir, dest_dir, storage, delete ):
   """
   Top level wrapper to execute functions
   """
-  
+
   result = getFileCatalog()
   if not result['OK']:
     return S_ERROR(result['Message'])
   fc = result['Value']
-    
+
   dm = DataManager()
-  
+
   result = getContentToSync(upload,fc,source_dir,dest_dir)
   if not result['OK']:
     return S_ERROR(result['Message'])
-  
+
   if upload:
     res = doUpload(fc, dm, result, source_dir, dest_dir, storage, delete)
     if not res['OK']:
@@ -429,19 +432,19 @@ def syncDestinations(upload, source_dir, dest_dir, storage, delete ):
     res = doDownload(dm, result, source_dir, dest_dir, delete)
     if not res['OK']:
       return S_ERROR('Download failed: ' + res['Message'])
-  
+
   return S_OK('Mirroring successfully finished')
-  
+
 def run( parameters , delete ):
   """
   The main user interface
   """
-  
+
   source_dir = parameters[0]
   dest_dir = parameters[1]
   upload = False
   storage = None
-  
+
   if len( parameters ) == 3:
     storage = parameters[2]
     source_dir = os.path.abspath(source_dir)
@@ -450,7 +453,7 @@ def run( parameters , delete ):
     if not os.path.isdir(source_dir):
       print "Source directory does not exist"
       sys.exit(1)
-     
+
   if len (parameters ) == 2:
     dest_dir = os.path.abspath(dest_dir)
     source_dir = source_dir.rstrip('/')
@@ -461,9 +464,9 @@ def run( parameters , delete ):
   res = syncDestinations( upload, source_dir, dest_dir, storage, delete )
   if not res['OK']:
     return S_ERROR(res['Message'])
-  
+
   return S_OK("Successfully mirrored " + source_dir + " into " + dest_dir)
-    
+
 if __name__ == "__main__":
   message = run( args , sync )
   print message['Value']
