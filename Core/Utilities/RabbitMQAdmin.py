c3dde8e76891321074fa276e005952fa6be4f3a6
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2015-10-19 13:28:37 +0200

    Remove empty hasPermissionsToAdd

--- Core/Utilities/RabbitMQAdmin.py
+++ Core/Utilities/RabbitMQAdmin.py
@@ -5,29 +5,26 @@
 import subprocess
 import re
 
-def hasPermissionsToAdmin():
-  pass 
-
 def executeRabbitmqctl(arg, *argv):
   command =['sudo','/usr/sbin/rabbitmqctl','-q', arg] + list(argv)
   res = subprocess.Popen(command, stdout = subprocess.PIPE)
   cmd_out, cmd_err = res.communicate()
-  return cmd_out 
+  return cmd_out
 
 def addUserWithoutPassword(user):
   addUser(user)
   clearUserPassword(user)
 def addUser(user, password = 'password'):
-  ret = executeRabbitmqctl('add_user', user, password) 
+  ret = executeRabbitmqctl('add_user', user, password)
 
 def deleteUser(user):
-  ret = executeRabbitmqctl('delete_user', user) 
+  ret = executeRabbitmqctl('delete_user', user)
 
 def getAllUsers():
   users = executeRabbitmqctl('list_users')
   users = users.split('\n')
   # the rabbitMQ user list is given in the format:
-  # user_name [usr_tag] 
+  # user_name [usr_tag]
   # I remove [usr_tag] part.
   # Also only non-empty users are proceeded further.
   # Empty users can appear, cause every new line was
@@ -36,7 +33,7 @@ def getAllUsers():
   return users
 
 def setUserPermission(user):
-  ret = executeRabbitmqctl('set_permissions','-p','/', user, '\".*\"','\".*\"','\".*\"') 
+  ret = executeRabbitmqctl('set_permissions','-p','/', user, '\".*\"','\".*\"','\".*\"')
 
 def clearUserPassword(user):
   ret = executeRabbitmqctl('clear_password', user)
@@ -45,7 +42,7 @@ def clearUserPassword(user):
 def setUsersPermissions(users):
   for u in users:
     setUserPermission(u)
-  
+
 def addUsersWithoutPasswords(users):
   for u in users:
     addUserWithoutPassword(u)
