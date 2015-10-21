2dd31aefc1e623f03434252c2b456111b61f6985
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2015-10-22 00:32:49 +0200

    Added stabdard methods

--- Resources/Catalog/TSCatalogClient.py
+++ Resources/Catalog/TSCatalogClient.py
@@ -4,28 +4,62 @@
 
 __RCSID__ = "$Id$"
 
-from DIRAC                                                         import S_OK
-from DIRAC.Core.Base.Client                                        import Client
-from DIRAC.Core.Utilities.List                                     import breakListIntoChunks
-from DIRAC.DataManagementSystem.Utilities.CatalogUtilities         import checkCatalogArguments
+from DIRAC                                                  import S_OK
+from DIRAC.Core.Utilities.List                              import breakListIntoChunks
+from DIRAC.DataManagementSystem.Utilities.CatalogUtilities  import checkCatalogArguments
+from DIRAC.Core.DISET.RPCClient                             import RPCClient
 
 # List of common File Catalog methods implemented by this client
 CATALOG_METHODS = [ "addFile", "removeFile" ]
 
-class TSCatalogClient( Client ):
+class TSCatalogClient( object ):
 
-  """ Exposes the functionality available in the DIRAC/TransformationHandler
+  """ Exposes the catalog functionality available in the DIRAC/TransformationHandler
 
   """
+  def __init__( self, url = None, **kwargs ):
+
+    self.__kwargs = kwargs
+    self.valid = True
+    self.serverURL = "Transformation/TransformationManager"
+    if url is not None:
+      self.serverURL = url
+
+  def __getRPC( self, rpc = None, url = '', timeout = 600 ):
+    """ Return RPCClient object to url
+    """
+    if not rpc:
+      if not url:
+        url = self.serverURL
+      self.__kwargs.setdefault( 'timeout', timeout )
+      rpc = RPCClient( url, **self.__kwargs )
+    return rpc
+
+  def isOK( self ):
+    return self.valid
+
+  def hasCatalogMethod( self, methodName ):
+    """ Check of a method with the given name is implemented
+    :param str methodName: the name of the method to check
+    :return: boolean Flag
+    """
+    return methodName in CATALOG_METHODS
+
+  def getInterfaceMethods( self ):
+    """ Get the methods implemented by the File Catalog client
+
+    :return tuple: ( read_methods_list, write_methods_list, nolfn_methods_list )
+    """
+    return ( [], CATALOG_METHODS, [] )
 
   @checkCatalogArguments
   def addFile( self, lfns, force = False ):
-    rpcClient = self._getRPC()
+    rpcClient = self.__getRPC()
     return rpcClient.addFile( lfns, force )
 
   @checkCatalogArguments
   def removeFile( self, lfns ):
-    rpcClient = self._getRPC()
+    rpcClient = self.__getRPC()
     successful = {}
     failed = {}
     listOfLists = breakListIntoChunks( lfns, 100 )
