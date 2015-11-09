637b6526621de5e7a41ddfc1966a9001f9b3d00c
Sbalbp <sbalbp@gmail.com>
2015-11-09 11:03:25 +0100

    Bugfix
    execCommand is now a ComponentInstaller method

--- FrameworkSystem/Client/ComponentInstaller.py
+++ FrameworkSystem/Client/ComponentInstaller.py
@@ -1141,7 +1141,7 @@ class ComponentInstaller( object ):
       runDict['RSS'] = -1
       if pid: # check the process CPU usage and memory
         # PID %CPU %MEM VSZ
-        result = execCommand( 0, ['ps', '-q', pid, 'au'] )
+        result = self.execCommand( 0, ['ps', '-q', pid, 'au'] )
         if result['OK'] and len( result['Value'] ) > 0:
           stats = result['Value'][1]
           values = re.findall( r"\d*\.\d+|\d+", stats )
