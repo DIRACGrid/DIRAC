fc910134b41b4fc4ad0546fe4b5b2478ab405551
Philippe Charpentier <Philippe.Charpentier@cern.ch>
2015-11-26 10:50:39 +0100

    Consider files to reschedule and submit when they are Failed in FTS

--- DataManagementSystem/Agent/FTSAgent.py
+++ DataManagementSystem/Agent/FTSAgent.py
@@ -548,9 +548,13 @@ class FTSAgent( AgentModule ):
 
             # Recover files that are Failed but were not spotted
             for ftsFile in [f for f in ftsFiles if f.Status == 'Failed' and f.TargetSE in missingReplicas.get( f.LFN, [] )]:
-              _r, _s, fail = self.__checkFailed( ftsFile )
-              if fail:
+              reschedule, submit, fail = self.__checkFailed( ftsFile )
+              if fail and ftsFile not in ftsFilesDict['toFail']:
                 ftsFilesDict['toFail'].append( ftsFile )
+              elif reschedule and ftsFile not in ftsFilesDict['toReschedule']:
+                ftsFilesDict['toReschedule'].append( ftsFile )
+              elif submit and ftsFile not in ftsFilesDict['toSubmit']:
+                ftsFilesDict['toSubmit'].append( ftsFile )
 
             # If all transfers are finished for unregistered files and there is already a registration operation, set it Done
             for lfn in missingReplicas:
