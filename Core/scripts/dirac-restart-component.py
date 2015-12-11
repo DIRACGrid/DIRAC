65576200dac3da922fa12e2c48fa36421bfeee0d
Sbalbp <sbalbp@gmail.com>
2015-12-11 14:43:07 +0100

    Adaptation to ComponentInstaller
    Replaced references to InstallTools with the appropriate calls to
    ComponentInstaller. InstallTools has not been deleted yet, just in case.

--- Core/scripts/dirac-restart-component.py
+++ Core/scripts/dirac-restart-component.py
@@ -31,13 +31,13 @@ if system != '*':
   if len( args ) > 1:
     component = args[1]
 #
-from DIRAC.Core.Utilities import InstallTools
+from FrameworkSystem.Client.ComponentInstaller import gComponentInstaller
 #
-InstallTools.exitOnError = True
+gComponentInstaller.exitOnError = True
 #
-result = InstallTools.runsvctrlComponent( system, component, 't' )
+result = gComponentInstaller.runsvctrlComponent( system, component, 't' )
 if not result['OK']:
   print 'ERROR:', result['Message']
   exit( -1 )
 
-InstallTools.printStartupStatus( result['Value'] )
+gComponentInstaller.printStartupStatus( result['Value'] )
--- Core/scripts/dirac-stop-mysql.py
+++ Core/scripts/dirac-stop-mysql.py
@@ -17,8 +17,8 @@ Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                      ] ) )
 Script.parseCommandLine()
 #
-from DIRAC.Core.Utilities import InstallTools
+from FrameworkSystem.Client.ComponentInstaller import gComponentInstaller
 #
-InstallTools.exitOnError = True
+gComponentInstaller.exitOnError = True
 #
-print InstallTools.stopMySQL()['Value'][1]
+print gComponentInstaller.stopMySQL()['Value'][1]
