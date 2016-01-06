46d0f18b527b339717c0a436e972e40910415eef
FedericoStagni <federico.stagni@cern.ch>
2016-01-06 16:21:09 +0100

    minor

--- Core/scripts/install_site.sh
+++ Core/scripts/install_site.sh
@@ -8,10 +8,10 @@
 function usage {
   echo Usage:
   echo "    install_site.sh [Options] ... CFG_file"
-  echo 
+  echo
   echo "CFG_file - is the name of the installation configuration file which contains"
   echo "           all the instructions for the DIRAC installation. See DIRAC Administrator "
-  echo "           Guide for the details"        
+  echo "           Guide for the details"
   echo "Options:"
   echo "    -d, --debug    debug mode"
   echo "    -h, --help     print this"
@@ -28,7 +28,7 @@ do
     exit
   ;;
   -d | --debug )
-    DEBUG='-o LogLevel=DEBUG' 
+    DEBUG='-o LogLevel=DEBUG'
 # -v | --version )
 #    switch=$1
 #    shift
@@ -50,7 +50,6 @@ fi
 
 # Get the latest version of dirac-install
 #
-#wget -O dirac-install 'http://svnweb.cern.ch/guest/dirac/DIRAC/trunk/DIRAC/Core/scripts/dirac-install.py' | exit
 wget --no-check-certificate -O dirac-install 'https://github.com/DIRACGrid/DIRAC/raw/integration/Core/scripts/dirac-install.py' || exit
 #
 # define the target Dir
@@ -61,5 +60,5 @@ mkdir -p $installDir || exit
 #
 python dirac-install -t server $installCfg
 source $installDir/bashrc
-dirac-configure $installCfg $DEBUG 
+dirac-configure $installCfg $DEBUG
 dirac-setup-site $DEBUG
