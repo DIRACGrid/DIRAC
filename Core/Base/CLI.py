399c1958d2e75274cabe2f82f5c4d53b2154c497
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2016-02-04 16:17:24 +0100

    CLI base class for all the CLI consoles

---
+++ Core/Base/CLI.py
@@ -0,0 +1,117 @@
+########################################################################
+# File :   CLI.py
+# Author : Andrei Tsaregorodtsev
+########################################################################
+
+""" CLI is the base class for all the DIRAC consoles ( CLIs ). It contains
+    several utilities and signal handlers of general purpose.
+"""
+
+__RCSID__ = "$Id$"
+
+import cmd
+import sys
+import os
+import signal
+
+from DIRAC import gLogger
+
+gColors = { 'red':1, 'green':2, 'yellow':3, 'blue':4 }
+
+def colorEnabled():
+  if os.environ.has_key( 'TERM' ):
+    if os.environ['TERM'] in ( 'xterm', 'xterm-color' ):
+      return True
+  return False
+
+def colorize( text, color ):
+  """Return colorized text"""
+  if not colorEnabled():
+    return text
+
+  startCode = '\033[;3'
+  endCode = '\033[0m'
+  if isinstance( color, ( int, long ) ):
+    return "%s%sm%s%s" % ( startCode, color, text, endCode )
+  try:
+    return "%s%sm%s%s" % ( startCode, gColors[ color ], text, endCode )
+  except:
+    return text
+
+class CLI( cmd.Cmd ):
+
+  def __init__( self ):
+
+    cmd.Cmd.__init__( self )
+    self._initSignals()
+
+  def _handleSignal( self, sig, frame ):
+
+    print "\nReceived signal", sig, ", exiting ..."
+    self.do_quit( self )
+
+  def _initSignals( self ):
+    """
+    Registers signal handlers
+    """
+    for sigNum in ( signal.SIGINT, signal.SIGQUIT, signal.SIGKILL, signal.SIGTERM ):
+      try:
+        signal.signal( sigNum, self._handleSignal )
+      except:
+        pass
+
+  def _errMsg( self, errMsg ):
+    """
+    Print out a colorized error log message
+
+    :param str errMsg: error message string
+    :return: nothing
+    """
+    gLogger.error( "%s %s" % ( colorize( "[ERROR]", "red" ), errMsg ) )
+
+  def emptyline( self ):
+    pass
+
+  def do_exit( self, args ):
+    """ Exit the shell.
+
+    usage: exit
+    """
+    self.do_quit( self )
+
+  def do_quit( self, args ):
+    """ Exit the shell.
+
+    usage: quit
+    """
+    gLogger.notice( '' )
+    sys.exit( 0 )
+
+  def do_EOF( self, args ):
+    """ Handler for EOF ( Ctrl D ) signal - perform quit
+    """
+    self.do_quit( args )
+
+  def do_execfile( self, args ):
+    """ Execute a series of CLI commands from a given file
+
+        usage:
+
+          execfile <filename>
+    """
+
+    argss = args.split()
+    fname = argss[0]
+    if not os.path.exists(fname):
+      print "Error: File not found %s" % fname
+      return
+    with open(fname, "r") as input_cmd:
+      contents = input_cmd.readlines()
+    for line in contents:
+      try:
+        gLogger.notice( "\n--> Executing %s\n" % line )
+        self.onecmd(line)
+      except Exception as error:
+        self._errMsg( str( error ) )
+        break
+    return
