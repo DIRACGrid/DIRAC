6bf8175ba2a08477c91e3a397078f4f60a2a7262
FedericoStagni <federico.stagni@cern.ch>
2016-03-03 17:25:28 +0100

    minor fix

--- Core/scripts/dirac-stop-component.py
+++ Core/scripts/dirac-stop-component.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC/Core/scripts/dirac-install.py $
 """
 Do the initial installation and configuration of the DIRAC MySQL server
 """
-__RCSID__ = "$Id: dirac-install.py 26844 2010-07-16 08:44:22Z rgracian $"
+__RCSID__ = "$Id$"
 #
 from DIRAC.Core.Base import Script
 Script.disableCS()
