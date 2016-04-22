07340beed74dd4ae60771a23a309ca87499969c5
Federico Stagni <federico.stagni@cern.ch>
2016-04-22 16:02:21 +0200

    Failing jobs when request is Invalid

--- WorkloadManagementSystem/JobWrapper/JobWrapper.py
+++ WorkloadManagementSystem/JobWrapper/JobWrapper.py
@@ -1233,7 +1233,7 @@ class JobWrapper( object ):
       isValid = RequestValidator().validate( request )
       if not isValid["OK"]:
         self.log.error( "Failover request is not valid", isValid["Message"] )
-        self.__report( minorStatus = 'Failover Request Failed' )
+        self.__report( status = 'Failed', minorStatus = 'Failover Request Failed' )
       else:
         # We try several times to put the request before failing the job: it's very important that requests go through,
         # or the job will be in an unclear status (workflow ok, but, e.g., the output files won't be registered).
@@ -1252,10 +1252,7 @@ class JobWrapper( object ):
             del requestClient
             time.sleep( counter ** 3 )
         if not result['OK']:
-          self.__report( 'Failed', 'Failover Request Failed' )
-        return result
-
-    return S_OK()
+          self.__report( status = 'Failed', minorStatus = 'Failover Request Failed' )
 
   #############################################################################
   def __getRequestFiles( self ):
