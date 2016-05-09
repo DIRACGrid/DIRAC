0f0888bb1e4b5b997da07d2791b7742f78a77e21
Miłosz Zdybał <milosz.zdybal@ifj.edu.pl>
2016-05-09 12:41:46 +0200

    Function auth moved to ConfigTemplate.cfg + typo spotted

--- WorkloadManagementSystem/Service/PilotsLoggingHandler.py
+++ WorkloadManagementSystem/Service/PilotsLoggingHandler.py
@@ -27,32 +27,27 @@ class PilotsLoggingHandler( RequestHandler ):
   def initialize(self):
     self.pilotsLogging = PilotsLoggingDB()
 
-  auth_addPilotsLogging = [ 'Operator' ]
   types_addPilotsLogging = [ basestring, basestring, basestring, float, basestring ]
   def export_addPilotsLogging( self, pilotUUID, status, minorStatus, timeStamp, source ):
 
     return self.pilotsLogging.addPilotsLogging( pilotUUID, status, minorStatus, timeStamp, source )
 
-  auth_getPilotsLogging = [ 'authenticated' ]
   types_getPilotsLogging = [ [int, long] ]
   def export_getPilotsLogging( self, pilotID ):
 
     return self.pilotsLogging.getPilotsLogging( pilotID )
 
-  auth_setPilotsUUIDtoIDMapping = [ 'Operator' ]
   types_setPilotsUUIDtoIDMapping = [ basestring, [int, long] ]
   def export_setPilotsUUIDtoIDMapping( self, pilotUUID, pilotID ):
 
     return self.pilotsLogging.setPilotsUUIDtoIDMapping( pilotUUID, pilotID )
 
-  auth_addPilotsUUID = [ 'Operator' ]
   types_addPilotsUUID = [ basestring ]
   def export_addPilotsUUID(self, pilotUUID ):
 
     return self.pilotsLogging.addPilotsUUID( pilotUUID )
 
-  auth_detelePilotsLogging = [ 'Operator' ]
-  types_detelePilotsLogging = [ [int, long,  list] ]
-  def export_detelePilotsLogging( self, pilotID ):
+  types_deletePilotsLogging = [ [int, long,  list] ]
+  def export_deletePilotsLogging( self, pilotID ):
 
     return self.pilotsLogging.deletePilotsLogging( pilotID )
