104df6f7c111a92f6cbc2834631a626ec72526c1
Federico Stagni <federico.stagni@cern.ch>
2016-05-09 16:50:50 +0200

    cp dest

--- tests/Jenkins/utilities.sh
+++ tests/Jenkins/utilities.sh
@@ -472,7 +472,7 @@ function generateUserCredentials(){
 			return
 		fi
 
-    cp $CI_CONFIG/openssl_config openssl_config
+    cp $CI_CONFIG/openssl_config openssl_config .
     sed -i 's/#hostname#/ciuser/g' openssl_config
     openssl genrsa -out client.key 1024 2&>1 /dev/null
     openssl req -key client.key -new -out client.req -config openssl_config
