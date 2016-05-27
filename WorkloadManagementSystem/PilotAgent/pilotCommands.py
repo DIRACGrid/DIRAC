c08bbff25cb33e88a3ca0c1206b26195a5e95e22
cinziaLu <cinzia.luzzi@cern.ch>
2016-05-27 11:18:14 +0200

    Protecting the parsing of pilot commands output.

--- WorkloadManagementSystem/PilotAgent/pilotCommands.py
+++ WorkloadManagementSystem/PilotAgent/pilotCommands.py
@@ -384,9 +384,12 @@ class CheckCECapabilities( CommandBase ):
                                                                         self.pp.queueName,
                                                                         " ".join( self.cfg ) )
     retCode, resourceDict = self.executeAndGetOutput( checkCmd, self.pp.installEnv )
-
-    import json
-    resourceDict = json.loads( resourceDict )
+    try:
+      import json
+      resourceDict = json.loads( resourceDict )
+    except ValueError:
+      self.log.error( "The pilot command output is not json compatible." )
+      sys.exit( 1 )
     if retCode:
       self.log.warn( "Could not get resource parameters [ERROR %d]" % retCode )
     if resourceDict.get( 'Tag' ):
@@ -432,11 +435,15 @@ class CheckWNCapabilities( CommandBase ):
     checkCmd = 'dirac-wms-get-wn-parameters -S %s -N %s -Q %s %s' % ( self.pp.site, self.pp.ceName, self.pp.queueName,
                                                                        " ".join( self.cfg ) )
     retCode, result = self.executeAndGetOutput( checkCmd, self.pp.installEnv )
-
-    result = result.split( ' ' )
-    numberOfProcessor = int( result[0] )
-    maxRAM = int( result[1] )
-
+    if retCode:
+      self.log.warn( "Could not get resource parameters [ERROR %d]" % retCode )
+    try:
+      result = result.split( ' ' )
+      numberOfProcessor = int( result[0] )
+      maxRAM = int( result[1] )
+    except ValueError:
+      self.log.error( "Wrong Command output %s" % result )
+      sys.exit( 1 )
     if numberOfProcessor or maxRAM:
       self.cfg.append( '-FDMH' )
 
@@ -778,9 +785,12 @@ class ConfigureCPURequirements( CommandBase ):
     self.log.info( "CPUTime left (in seconds) is %s" % cpuTime )
 
     # HS06s = seconds * HS06
-    self.pp.jobCPUReq = float( cpuTime ) * float( cpuNormalizationFactor )
-    self.log.info( "Queue length (which is also set as CPUTimeLeft) is %f" % self.pp.jobCPUReq )
-
+    try:
+      self.pp.jobCPUReq = float( cpuTime ) * float( cpuNormalizationFactor )
+      self.log.info( "Queue length (which is also set as CPUTimeLeft) is %f" % self.pp.jobCPUReq )
+    except ValueError:
+      self.log.error( 'Pilot command output does not have the correct format' )
+      sys.exit( 1 )
     # now setting this value in local file
     cfg = ['-FDMH']
     if self.pp.useServerCertificate:
