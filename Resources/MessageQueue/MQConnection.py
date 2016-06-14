b43f349f92007e44ce3d800a7dc8a82f7e76785e
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2016-06-14 15:57:48 +0200

    Added tests, bug fixes, doc strings

--- Resources/MessageQueue/MQConnection.py
+++ Resources/MessageQueue/MQConnection.py
@@ -2,10 +2,16 @@
 Abstract class for management of MQ connections
 """
 
+import time
+
+from DIRAC import gLogger, S_OK, S_ERROR
+from DIRAC.Core.Utilities.DErrno import EMQUKN
+
 __RCSID__ = "$Id$"
 
-import time
-from DIRAC import gLogger, S_OK, S_ERROR
+class MQConnectionError( Exception ):
+    def __init__(self,*args,**kwargs):
+        Exception.__init__(self,*args,**kwargs)
 
 class MQConnection( object ):
   """
@@ -14,81 +20,84 @@ class MQConnection( object ):
   """
 
   def __init__( self ):
-    raise NotImplementedError( 'Cannot instantiate an abstract class' )
+    """ Standard constructor
+    """
 
-  # Callback functions for message receiving mode
+    self.alive = False
 
   def on_error( self, headers, message ):
-    """
-    Callback function called when an error happens
+    """ Default callback function called when an error happens
     """
     gLogger.error( message )
 
   # Rest of functions
 
   def setParameters( self, parameters ):
-    """
-    Reads the MessageQueue parameters from the CS and sets the appropriate variables in the class with their values
-    system is the DIRAC system where the queue works
-    queueName is the name of the queue
-    parameters is a dictionary with the parameters for the queue. It should include the following parameters:
+    """ Sets the parameters attribute
+
+    :param dict parameters: dictionary with the parameters for the queue. It should include the following parameters:
     'Host', 'Port', 'User', 'VH' and 'Type'. Otherwise, the function will return an error
     """
 
     if not parameters:
-      return S_ERROR( 'Queue parameters are not provided' )
+      return S_ERROR( EMQUKN, 'Queue parameters are not provided' )
+
+    for param in [ 'Host', 'Port', 'User', 'VHost' and 'Type' ]:
+      if not param in parameters:
+        return S_ERROR( EMQUKN, "Parameter %s not provided" % param )
 
     self.parameters = parameters
 
     return S_OK( 'Queue parameters set successfully' )
 
-  def setupConnection( self, system, queueName, receive = False, messageCallback = None ):
+  def setupConnection( self, parameters = {}, receive = False, messageCallback = None ):
     """
     Establishes a new non-blocking connection to the message queue
-    system indicates in which System the queue works
-    queueName is the name of the queue to read from/write to
-    receive indicates whether this object will read from the queue or read from it
-    exchange indicates whether the destination will be a exchange (True) or a queue (False). Only taken into account if receive = True
-    messageCallback is the function to be called when a new message is received from the queue ( only receiver mode ). If None, the defaultCallback method is used instead
+
+    :param dict parameters: dictionary with additional MQ parameters if any
+    :param bool receive: flag to enable the MQ connection for getting message
+    :param func messageCallback: function to be called when a new message is received from the queue ( only receiver mode ).
+                                If None, the defaultCallback method is used instead
+    :return: S_OK/S_ERROR
     """
     raise NotImplementedError( 'This method should be implemented by child class' )
 
-  def blockingConnection( self, system, queueName, receive = False, messageCallback = None ):
+  def run( self, parameters = {}, receive = True, messageCallback = None ):
     """
     Establishes a new blocking connection to the message queue
-    system indicates in which System the queue works
-    queueName is the name of the queue to read from/write to
-    receive indicates whether this object will read from the queue or read from it
-    exchange indicates whether the destination will be a exchange (True) or a queue (False). Only taken into account if receive = True
-    messageCallback is the function to be called when a new message is received from the queue ( only receiver mode ). If None, the defaultCallback method is used instead
+    :param dict parameters: dictionary with additional MQ parameters if any
+    :param bool receive: flag to enable the MQ connection for getting message
+    :param func messageCallback: function to be called when a new message is received from the queue
+                                 ( only receiver mode ).
+    :return: S_OK/S_ERROR
     """
-    result = self.setupConnection( system, queueName, receive, messageCallback )
+    result = self.setupConnection( parameters, receive, messageCallback )
     if not result[ 'OK' ]:
       return result
 
-    while 1:
+    while self.alive:
       time.sleep( 1 )
-
-    self.unsetupConnection()
-
-  def __del__(self):
 
     self.disconnect()
 
   def put( self, message ):
+    """ Send message to a MQ server
+
+    :param message: any json encodable structure
+    :return: S_OK/S_ERROR
     """
-    Sends a message to the queue
-    message contains the body of the message
-    """
+
     raise NotImplementedError( 'This method should be implemented by child class' )
 
   def get( self ):
+    """ Get one message, if any, from the MQ server
+
+    :return: S_OK( message )/ S_ERROR if not message available or connection error
     """
-    Retrieves a message from the queue ( if any )
-    """
+
     raise NotImplementedError( 'This method should be implemented by child class' )
 
-  def unsetupConnection( self ):
+  def disconnect( self ):
     """
     Disconnects from the message queue server
     """
--- Resources/MessageQueue/MQConnectionFactory.py
+++ Resources/MessageQueue/MQConnectionFactory.py
@@ -1,60 +1,10 @@
-"""  The MQ creates MQConnection objects
+"""  The MQ Factory creates MQConnection objects
 """
-from DIRAC                 import S_OK, S_ERROR, gLogger, gConfig
+from DIRAC                 import S_OK, S_ERROR, gLogger
 from DIRAC.Core.Utilities  import ObjectLoader
+from DIRAC.Resources.MessageQueue.Utilities import getMQueue
 
 __RCSID__ = "$Id$"
-
-def getMQueue( queueName ):
-
-  mqService = None
-  elements = queueName.split( '::' )
-  if len( elements ) == 2:
-    mqService, queue = elements
-  else:
-    queue = queueName
-
-
-  result = gConfig.getSections( '/Resources/MQServices' )
-  if not result['OK']:
-    return result
-  sections = result['Value']
-  if mqService and not mqService in sections:
-    return S_ERROR( 'Requested MQService %s not found in the CS' % mqService )
-  elif not mqService and len( sections ) == 1:
-    mqService = sections[0]
-
-  queuePath = ''
-  servicePath = ''
-  if mqService:
-    servicePath = '/Resources/MQServices/%s' % mqService
-    result = gConfig.getSections( '/Resources/MQServices/%s/Queues' % mqService )
-    if result['OK'] and queue in result['Value']:
-      queuePath = '/Resources/MQServices/%s/Queues/%s' % ( mqService, queue )
-  else:
-    for section in sections:
-      result = gConfig.getSections( '/Resources/MQServices/%s/Queues' % section )
-      if result['OK']:
-        if queue in result['Value']:
-          if queuePath:
-            return S_ERROR( 'Ambiguous queue %s definition' % queue )
-          else:
-            servicePath = '/Resources/MQServices/%s' % section
-            queuePath = '/Resources/MQServices/%s/Queues/%s' % ( section, queue )
-
-  result = gConfig.getOptionsDict( servicePath )
-  if not result['OK']:
-    return result
-  serviceDict = result['Value']
-
-  if queuePath:
-    result = gConfig.getOptionsDict( queuePath )
-    if not result['OK']:
-      return result
-    serviceDict.update( result['Value'] )
-  serviceDict['Queue'] = queue
-
-  return S_OK( serviceDict )
 
 class MQConnectionFactory( object ):
 
@@ -67,8 +17,12 @@ class MQConnectionFactory( object ):
 
   #############################################################################
   def __getMQConnection( self, queueName = None, parameters = {} ):
-    """This method returns the CE instance corresponding to the supplied
-       CEUniqueID.  If no corresponding CE is available, this is indicated.
+    """ This method returns the MQConnection instance corresponding to the parameters and queue
+
+       :param str queueName: name of the queue. Can be provided as just queueName or <MQServer>::<queueName>
+                             forms
+       :param dict parameters: dictionary of connection parameters
+       :return: S_OK(MQconnectionObject)/ S_ERROR
     """
 
     queueParameters = {}
@@ -106,21 +60,29 @@ class MQConnectionFactory( object ):
 
     return S_OK( mqConnection )
 
-  def getMQListener( self, queueName = None, parameters = {}, messageCallback = None ):
+  def getMQListener( self, queueName = None, parameters = {} ):
+    """ Get a MQConnection object in a Listener mode without connection
+        initialized
 
+    :param str queueName: queueName
+    :param dict parameters: MQ connection parameters
+    :return: S_OK( MQconnectionObject )/ S_ERROR
+    """
     result = self.__getMQConnection( queueName = queueName,
                                      parameters = parameters )
     if not result['OK']:
       return result
 
     mqConnection = result['Value']
-    result = mqConnection.setupConnection( receive = True, messageCallback = messageCallback )
-    if not result['OK']:
-      return result
-
     return S_OK( mqConnection )
 
   def getMQPublisher( self, queueName = None, parameters = {} ):
+    """ Get a MQConnection object in a Publisher mode
+
+    :param str queueName: queueName
+    :param dict parameters: MQ connection parameters
+    :return: S_OK( MQconnectionObject )/ S_ERROR
+    """
 
     result = self.__getMQConnection( queueName = queueName,
                                      parameters = parameters )
