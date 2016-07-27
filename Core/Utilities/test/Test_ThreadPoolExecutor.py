087af63274a32121d1c54aa51f63860fa7042657
Zoltan Mathe <zoltan.mathe@cern.ch>
2016-07-27 12:03:45 +0200

    hope now it is fixed

--- Core/Utilities/test/Test_ThreadPoolExecutor.py
+++ Core/Utilities/test/Test_ThreadPoolExecutor.py
@@ -7,30 +7,23 @@ Unit test for DIRAC.Core.Utilities.ThreadPoolExecutor
 import unittest
 import random
 import time
-#import functools
+import functools
 import os
-import pytest
 import logging
 logging.basicConfig()
 
 from DIRAC import gLogger
 from DIRAC.Core.Utilities.ThreadPoolExecutor import ThreadPoolExecutor
+ 
 
-def testFunc( timeWait ):
+def tFunc( timeWait ):
   """ function to be executed by a future """
-  print "pid=%s will sleep " % ( os.getpid() )
-  time.sleep( 1 )
-  
+  print "pid=%s will sleep for %s s" % ( os.getpid(), timeWait )
+  time.sleep( timeWait )
+  return timeWait
 
-#def testFunc( timeWait ):
-#  """ function to be executed by a future """
-#  print "pid=%s will sleep for %s s" % ( os.getpid(), timeWait )
-#  time.sleep( timeWait )
-#  return timeWait
-
-@pytest.fixture(autouse=True)
-def testCallback(fn):
-  print "callback: pid=%s" % ( os.getpid() )
+def tCallback( timeWait, futures ):
+  print "callback: pid=%s will sleep for %s s" % ( os.getpid(), timeWait )
     
 class ThreadPoolExecutorTests( unittest.TestCase ):
   
@@ -42,7 +35,7 @@ class ThreadPoolExecutorTests( unittest.TestCase ):
   def test_generateJobAndQueueItI( self ):  
     for _ in xrange( 20 ):
       timeWait = random.randint( 0, 10 )
-      self.threadPool.generateJobAndQueueIt( testFunc, args = ( timeWait, ) )
+      self.threadPool.generateJobAndQueueIt( tFunc, args = ( timeWait, ) )
 
     self.assertEqual( 4, self.threadPool.numWorkingThreads() )
     self.assert_( not self.threadPool.numWaitingThreads () < 4 )
@@ -51,11 +44,11 @@ class ThreadPoolExecutorTests( unittest.TestCase ):
   def test_generateJobAndQueueItICallback( self ):
     for i in xrange( 20 ):
       timeWait = random.randint( 0, 10 )
-      self.threadPool.generateJobAndQueueIt( testFunc, args = ( timeWait, ) )
+      self.threadPool.generateJobAndQueueIt( tFunc, args = ( timeWait, ) )
       if i % 2:
-        self.threadPool.generateJobAndQueueIt( testFunc, args = ( timeWait, ), oCallback = testCallback ) 
+        self.threadPool.generateJobAndQueueIt( tFunc, args = ( timeWait, ), oCallback = functools.partial( tCallback, "Future:%s" % timeWait ) ) 
       else:
-        self.threadPool.generateJobAndQueueIt( testFunc, args = ( timeWait, ) )
+        self.threadPool.generateJobAndQueueIt( tFunc, args = ( timeWait, ) )
 
     self.assertEqual( 4, self.threadPool.numWorkingThreads() )
     self.assert_( not self.threadPool.numWaitingThreads () < 4 )
