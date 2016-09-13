2b676ee515914c7e33ab3dca2ffb1cc49c4e0d64
Zoltan Mathe <zoltan.mathe@cern.ch>
2016-09-13 10:40:40 +0200

    every time when we process the data we crate a new listener

--- MonitoringSystem/Client/MonitoringReporter.py
+++ MonitoringSystem/Client/MonitoringReporter.py
@@ -20,7 +20,7 @@ class MonitoringReporter( object ):
     self.__commitTimer = None
     
     try:
-      self.__mqListener = MQListener( monitoringType ) #, callback = self.consumeRecords )
+      #self.__mqListener = MQListener( monitoringType ) #, callback = self.consumeRecords )
       #self.__mqListener.setCallback(self.consumeRecords)
       self.__mqPublisher = MQPublisher( monitoringType )
       self.__mq = True
@@ -36,10 +36,41 @@ class MonitoringReporter( object ):
     print '!!!!', records[0]
     return S_ERROR()
   
+  def processRecords( self ):
+    print 'start'
+    try:
+      mqListener = MQListener( self.__monitoringType )
+    except MQConnectionError as exc:
+      gLogger.error( "Fail to create Listener: %s" % exc )
+      return S_ERROR( "Fail to create Listener: %s" % exc )
+    
+    result = S_OK()
+    while result['OK']:
+      print 'while'
+      result = mqListener.get()
+      print '!!!!', result['OK']
+      if result['OK']:
+        records = json.loads( result['Value'] )
+        retVal = self.__db.put( list( records ), self.__monitoringType )
+        if not retVal['OK']:
+          mqListener.stop()
+          res = self.publishRecords( records )
+          print res
+          if not res['OK']:
+            return res
+          break
+      else:
+        print 'SS@@#######', result['Message']
+        mqListener.stop()
+        break
+    print 'end'
+    return S_OK()
+    
   def addRecord( self, rec ):
     self.__documents.append( rec )
        
   def publishRecords( self, records ):
+    print 'publishRecords'
     return self.__mqPublisher.put( json.dumps(records) )
       
   def commit( self ):
@@ -58,7 +89,7 @@ class MonitoringReporter( object ):
           del documents[ :self.__maxRecordsInABundle ]          
         else:
           if self.__mq:
-            res = self.publishRecords( recordsToSend[:2] )
+            res = self.publishRecords( recordsToSend )
             documents = []
             #if we managed to publish the records we can delete from the list
             if res['OK']:
@@ -71,26 +102,12 @@ class MonitoringReporter( object ):
       return S_ERROR( "Error committing %s" % repr( e ).replace( ',)', ')' ) )    
     finally:
       self.__documents.extend( documents )
-    
-    print 'start'
-    
-    result = S_OK()
-    while result['OK']:
-      result = self.__mqListener.get()
-      print '!!!!', result['OK']
-      if result['OK']:
-        records = json.loads( result['Value'] )
-        retVal = self.__db.put( list(records), self.__monitoringType )
-        if not retVal['OK']:
-          self.__mqListener.stop()
-          res = self.publishRecords( records )
-          if not res['OK']:
-            return res
-          break
-      else:
-        print 'SS@@#######', result['Message']
-        self.__mqListener.stop()
-        break
+        
+    if self.__mq:
+      result = self.processRecords()
+      if not result['OK']:
+        gLogger.error( "Unable to insert data from the MQ", result['Message'] )
+        
     
     '''
     result = self.__mqListener.get()
@@ -99,6 +116,6 @@ class MonitoringReporter( object ):
       records = json.loads( result['Value'] )
       print 'records', records
     '''
-    print 'END'
+  
     return S_OK( recordSent ) 
   
