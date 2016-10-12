37d4018a7f96fca1dd1cebd93938c683c4a3c9f8
Stavros Moiras <steve@vipersec.com>
2016-10-12 11:35:30 +0200

    Added test for GOCDBSyncCommand

--- ResourceStatusSystem/Command/test/Test_RSS_Command_GOCDBSyncCommand.py
+++ ResourceStatusSystem/Command/test/Test_RSS_Command_GOCDBSyncCommand.py
@@ -1,6 +1,68 @@
-''' This is a test of the chain
-    ResourceManagementClient -> ResourceManagementHandler -> ResourceManagementDB
-    It supposes that the DB is present, and that the service is running
+"""
+Test_RSS_Command_GOCDBStatusCommand
+"""
 
-    this is pytest!
-'''
+import mock
+import unittest
+import importlib
+
+from DIRAC.ResourceStatusSystem.Command.GOCDBSyncCommand import GOCDBSyncCommand
+from DIRAC import gLogger, S_OK
+
+__RCSID__ = '$Id:  $'
+
+################################################################################
+
+class GOCDBSyncCommand_TestCase( unittest.TestCase ):
+
+  def setUp( self ):
+    """
+    Setup
+    """
+    gLogger.setLevel( 'DEBUG' )
+    self.CSHelpersMock = mock.MagicMock()
+    self.GOCDBSyncCommandModule = importlib.import_module( 'DIRAC.ResourceStatusSystem.Command.GOCDBSyncCommand' )
+    self.GOCDBSyncCommandModule.CSHelpers = self.CSHelpersMock
+    self.mock_GOCDBClient = mock.MagicMock()
+
+  def tearDown( self ):
+    """
+    TearDown
+    """
+    del self.mock_GOCDBClient
+    del self.GOCDBSyncCommandModule
+
+################################################################################
+# Tests
+
+class GOCDBSyncCommand_Success( GOCDBSyncCommand_TestCase ):
+
+  def test_instantiate( self ):
+    """ tests that we can instantiate the object
+    """
+
+    command = GOCDBSyncCommand()
+    self.assertEqual( 'GOCDBSyncCommand', command.__class__.__name__ )
+
+  def test_init( self ):
+    """ tests that the init method
+    """
+
+    command = GOCDBSyncCommand()
+    self.assertEqual( {}, command.apis )
+
+    command = GOCDBSyncCommand( clients = {'GOCDBClient': self.mock_GOCDBClient} )
+    self.assertEqual( {'onlyCache': False}, command.args )
+    self.assertEqual( {'GOCDBClient': self.mock_GOCDBClient}, command.apis )
+
+
+  def test_doNew( self ):
+    """ tests the doNew method
+    """
+
+    command = GOCDBSyncCommand()
+    res = command.doNew()
+    self.assertEqual( False, res['OK'] )
+
+    res = command.doNew('dummy.host.dummy')
+    self.assertEqual( True, res['OK'] )
