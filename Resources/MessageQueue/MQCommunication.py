548e45356058f73d6db3a74968e601d9c8fcde33
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2016-12-01 15:05:57 +0100

    Change setupConnection to _setupConnection

--- Resources/MessageQueue/MQCommunication.py
+++ Resources/MessageQueue/MQCommunication.py
@@ -21,7 +21,7 @@ def createConsumer(mqURI, callback = generateDefaultCallback()):
   Returns:
     S_OK/S_ERROR: with the consumer object in S_OK.
   """
-  result = setupConnection(mqURI = mqURI, mType = "consumer")
+  result = _setupConnection(mqURI = mqURI, mType = "consumer")
   if not result['OK']:
     gLogger.error( 'Failed to createConsumer:', result['Message'] )
     return result
@@ -38,13 +38,13 @@ def createProducer(mqURI):
   Returns:
     S_OK/S_ERROR: with the producer object in S_OK.
   """
-  result = setupConnection(mqURI = mqURI, mType = "producer")
+  result = _setupConnection(mqURI = mqURI, mType = "producer")
   if not result['OK']:
     gLogger.error( 'Failed to createProducer:', result['Message'] )
     return result
   return S_OK(MQProducer(mqManager = connectionManager, mqURI  = mqURI, producerId = result['Value']))
 
-def setupConnection(mqURI, mType):
+def _setupConnection(mqURI, mType):
   """ Function sets up the active MQ connection. All parameters are taken
       from the Configuration Service based on the mqURI
       value and the messenger Type mType.
