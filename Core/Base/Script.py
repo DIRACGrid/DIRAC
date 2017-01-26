a9494ec678fc28be0a1de4b85ac03236444c4516
Stavros Moiras <steve@vipersec.com>
2017-01-26 10:07:47 +0100

    Reduced tab length

--- Core/Base/Script.py
+++ Core/Base/Script.py
@@ -99,20 +99,20 @@ def confirmDecision(question, default="no"):
     valid = {"yes": True, "y": True, "ye": True,
              "no": False, "n": False}
     if default is None:
-        prompt = " [y/n] "
+      prompt = " [y/n] "
     elif default == "yes":
-        prompt = " [Y/n] "
+      prompt = " [Y/n] "
     elif default == "no":
-        prompt = " [y/N] "
+      prompt = " [y/N] "
     else:
-        raise ValueError("invalid default answer: '%s'" % default)
+      raise ValueError("invalid default answer: '%s'" % default)
 
     while True:
-        print question + prompt
-        choice = raw_input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
-            print "Please respond with 'yes' or 'no' (or 'y' or 'n').\n"
+      print question + prompt
+      choice = raw_input().lower()
+      if default is not None and choice == '':
+        return valid[default]
+      elif choice in valid:
+        return valid[choice]
+      else:
+        print "Please respond with 'yes' or 'no' (or 'y' or 'n').\n"
