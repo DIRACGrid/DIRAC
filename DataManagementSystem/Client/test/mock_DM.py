5276d8c9d7ee63545d064d59287d25d47029ce6a
Philippe Charpentier <Philippe.Charpentier@cern.ch>
2017-02-08 14:03:42 +0100

    add getReplicasForJobs

--- DataManagementSystem/Client/test/mock_DM.py
+++ DataManagementSystem/Client/test/mock_DM.py
@@ -1,7 +1,7 @@
 """ A mock of the DataManager, used for testing purposes
 """
 
-#pylint: disable=protected-access, missing-docstring, invalid-name, line-too-long
+# pylint: disable=protected-access, missing-docstring, invalid-name, line-too-long
 
 from mock import MagicMock
 
@@ -12,6 +12,7 @@ dm_mock.getReplicas.return_value = S_OK( {'Successful': {'/a/lfn/1.txt':{'SE1':'
                                                                          'SE2':'/a/lfn/at/SE2.1.txt'},
                                                          '/a/lfn/2.txt':{'SE1':'/a/lfn/at/SE1.1.txt'}},
                                           'Failed':{}} )
+dm_mock.getReplicasForJobs.return_value = dm_mock.getReplicas.return_value
 dm_mock.getCatalogFileMetadata.return_value = {'OK': True, 'Value':{'Successful':{'pippo':'metadataPippo'},
                                                                     'Failed':None}}
 dm_mock.removeFile.return_value = {'OK': True, 'Value': {'Failed':False}}
