64d4d382a0e3972896e31802af6845e9fc0c6b11
Christophe Haen <christophe.haen@cern.ch>
2017-03-20 11:27:07 +0100

    Reorder the import

--- RequestManagementSystem/scripts/dirac-rms-list-req-cache.py
+++ RequestManagementSystem/scripts/dirac-rms-list-req-cache.py
@@ -1,17 +1,18 @@
 #!/bin/env python
 """ List the number of requests in the caches of all the ReqProxyies """
 __RCSID__ = "$Id: $"
+import DIRAC
 
 from DIRAC.Core.Base import Script
-Script.registerSwitch( '', 'Full', '   Print full list of requests' )
-Script.setUsageMessage( '\n'.join( [ __doc__,
-                                     'Usage:',
-                                     ' %s [option|cfgfile]' % Script.scriptName ] ) )
+from DIRAC.Core.Base.Script import parseCommandLine
+
+Script.registerSwitch('', 'Full', '   Print full list of requests')
+Script.setUsageMessage('\n'.join([__doc__, 'Usage:', ' %s [option|cfgfile]' % Script.scriptName]))
+parseCommandLine()
+from DIRAC.RequestManagementSystem.Client.ReqClient import ReqClient
+
 
 if __name__ == "__main__":
-
-  from DIRAC.Core.Base.Script import parseCommandLine
-  parseCommandLine()
 
   fullPrint = False
 
@@ -19,14 +20,10 @@ if __name__ == "__main__":
     if switch[0] == 'Full':
       fullPrint = True
 
-  import DIRAC
-
-  from DIRAC.RequestManagementSystem.Client.ReqClient import ReqClient
   reqClient = ReqClient()
 
-
   for server, rpcClient in reqClient.requestProxies().iteritems():
-    DIRAC.gLogger.always("Checking request cache at %s"%server)
+    DIRAC.gLogger.always("Checking request cache at %s" % server)
     reqCache = rpcClient.listCacheDir()
     if not reqCache['OK']:
       DIRAC.gLogger.error("Cannot list request cache", reqCache)
@@ -38,5 +35,4 @@ if __name__ == "__main__":
     else:
       DIRAC.gLogger.always("Number of requests in the cache", len(reqCache))
 
-
-  DIRAC.exit( 0 )
+  DIRAC.exit(0)
