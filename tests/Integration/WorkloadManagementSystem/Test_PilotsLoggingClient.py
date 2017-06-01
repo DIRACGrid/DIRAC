7028308a7a00f30f5327b313d055c9c1fc7bb187
Miłosz Zdybał <milosz.zdybal@gmail.com>
2017-06-01 11:46:57 +0200

    Test updated to new logging format

--- tests/Integration/WorkloadManagementSystem/Test_PilotsLoggingClient.py
+++ tests/Integration/WorkloadManagementSystem/Test_PilotsLoggingClient.py
@@ -16,76 +16,68 @@ class TestPilotsLogging( unittest.TestCase ):
     pass
 
 class PilotsLogging( TestPilotsLogging ):
-
+pilotUUID, timestamp, source, phase, status, messageContent
   def test_PilotsLoggingAddGetDelete( self ):
-
-    resp = self.pilotsLoggingClient.addPilotsLogging('11111111-1111-1111-1111-111111111111', 'status1', 'minorStatus1', 1427721819.0, 'test', 0)
+    resp = self.pilotsLoggingClient.addPilotsLogging('11111111-1111-1111-1111-111111111111', 'timestamp', 'test', 'phase', 'status', 'messageContent')
     self.assert_(resp['OK'], 'Failed to add PilotsLogging')
-    resp = self.pilotsLoggingClient.setPilotsUUIDtoIDMapping('11111111-1111-1111-1111-111111111111', 1)
-    self.assert_(resp['OK'], 'Failed to add PilotsUUIDtoIDMapping')
-    resp = self.pilotsLoggingClient.getPilotsLogging(1)
+    resp = self.pilotsLoggingClient.getPilotsLogging('11111111-1111-1111-1111-111111111111')
     self.assert_(resp['OK'], 'Failed to get PilotsLogging')
     test_sample = {
-                   'PilotUUID': '11111111-1111-1111-1111-111111111111',
-                   'PilotID': 1,
-                   'Status': 'status1',
-                   'MinorStatus': 'minorStatus1',
-                   'TimeStamp': 1427721819.0,
-                   'Source': 'test'
+                   'pilotUUID': '11111111-1111-1111-1111-111111111111',
+                   'timestamp': 'timestamp',
+                   'source': 'test',
+                   'phase': 'phase',
+                   'status': 'status',
+                   'messageContent': 'messageContent',
                    }
     self.assertEqual(resp['Value'], [ test_sample ], 'Wrong data comes out of Service')
-    resp = self.pilotsLoggingClient.deletePilotsLogging(1)
+    resp = self.pilotsLoggingClient.deletePilotsLogging('11111111-1111-1111-1111-111111111111')
     self.assert_(resp['OK'], 'Failed to delete PilotsLogging')
-    resp = self.pilotsLoggingClient.getPilotsLogging(1)
+    resp = self.pilotsLoggingClient.getPilotsLogging('11111111-1111-1111-1111-111111111111')
     self.assert_(resp['OK'], 'Failed to get PilotsLogging')
     self.assertEqual(resp['Value'], [], 'PilotsLogging was not really deleted')
 
   def test_PilotsLoggingEmptyGetDelete( self ):
 
-    resp = self.pilotsLoggingClient.getPilotsLogging( 1 )
+    resp = self.pilotsLoggingClient.getPilotsLogging( '11111111-1111-1111-1111-111111111111' )
     self.assert_( resp['OK'], 'Failed to get PilotsLogging' )
-    resp = self.pilotsLoggingClient.deletePilotsLogging( 1 )
+    resp = self.pilotsLoggingClient.deletePilotsLogging( '11111111-1111-1111-1111-111111111111' )
     self.assert_( resp['OK'], 'Failed to delete PilotsLogging' )
 
   def test_PilotsLoggingDeleteList( self ):
 
     test_sample1 = {
-                   'PilotUUID': '11111111-1111-1111-1111-111111111111',
-                   'PilotID': 1,
-                   'Status': 'status1',
-                   'MinorStatus': 'minorStatus1',
-                   'TimeStamp': 1427721819.0,
-                   'Source': 'test'
+                   'pilotUUID': '11111111-1111-1111-1111-111111111111',
+                   'timestamp': 'timestamp1',
+                   'source': 'test',
+                   'phase': 'phase1',
+                   'status': 'status1',
+                   'messageContent': 'messageContent1',
                    }
     test_sample2 = {
-                   'PilotUUID': '22222222-2222-2222-2222-222222222222',
-                   'PilotID': 2,
-                   'Status': 'status2',
-                   'MinorStatus': 'minorStatus2',
-                   'TimeStamp': 1427721820.0,
-                   'Source': 'test'
+                   'pilotUUID': '22222222-2222-2222-2222-222222222222',
+                   'timestamp': 'timestamp2',
+                   'source': 'test',
+                   'phase': 'phase2',
+                   'status': 'status2',
+                   'messageContent': 'messageContent2',
                    }
 
-    resp = self.pilotsLoggingClient.addPilotsLogging('11111111-1111-1111-1111-111111111111', 'status1', 'minorStatus1', 1427721819.0, 'test', 0)
+    resp = self.pilotsLoggingClient.addPilotsLogging('11111111-1111-1111-1111-111111111111', 'timestamp1', 'test', 'phase1', 'status1', 'messageContent1')
     self.assert_(resp['OK'], 'Failed to add PilotsLogging')
-    resp = self.pilotsLoggingClient.addPilotsLogging('22222222-2222-2222-2222-222222222222', 'status2', 'minorStatus2', 1427721820.0, 'test', 0)
-    self.assert_(resp['OK'], 'Failed to add PilotsLogging')
-    resp = self.pilotsLoggingClient.setPilotsUUIDtoIDMapping('11111111-1111-1111-1111-111111111111', 1)
-    self.assert_(resp['OK'], 'Failed to add PilotsUUIDtoIDMapping')
-    resp = self.pilotsLoggingClient.setPilotsUUIDtoIDMapping('22222222-2222-2222-2222-222222222222', 2)
-    self.assert_(resp['OK'], 'Failed to add PilotsUUIDtoIDMapping')
-    resp = self.pilotsLoggingClient.getPilotsLogging(1)
+    resp = self.pilotsLoggingClient.addPilotsLogging('22222222-2222-2222-2222-222222222222', 'timestamp2', 'test', 'phase2', 'status2', 'messageContent2')
+    resp = self.pilotsLoggingClient.getPilotsLogging('11111111-1111-1111-1111-111111111111')
     self.assert_(resp['OK'], 'Failed to get PilotsLogging')
     self.assertEqual(resp['Value'], [ test_sample1 ], 'Wrong data comes out of Service')
-    resp = self.pilotsLoggingClient.getPilotsLogging(2)
+    resp = self.pilotsLoggingClient.getPilotsLogging('22222222-2222-2222-2222-222222222222')
     self.assert_(resp['OK'], 'Failed to get PilotsLogging')
     self.assertEqual(resp['Value'], [ test_sample2 ], 'Wrong data comes out of Service')
-    resp = self.pilotsLoggingClient.deletePilotsLogging( [1, 2] )
+    resp = self.pilotsLoggingClient.deletePilotsLogging( ['11111111-1111-1111-1111-111111111111', '22222222-2222-2222-2222-222222222222'] )
     self.assert_(resp['OK'], 'Failed to delete PilotsLogging')
-    resp = self.pilotsLoggingClient.getPilotsLogging(1)
+    resp = self.pilotsLoggingClient.getPilotsLogging('11111111-1111-1111-1111-111111111111')
     self.assert_(resp['OK'], 'Failed to get PilotsLogging')
     self.assertEqual(resp['Value'], [], 'PilotsLogging was not really deleted')
-    resp = self.pilotsLoggingClient.getPilotsLogging(2)
+    resp = self.pilotsLoggingClient.getPilotsLogging('22222222-2222-2222-2222-222222222222')
     self.assert_(resp['OK'], 'Failed to get PilotsLogging')
     self.assertEqual(resp['Value'], [], 'PilotsLogging was not really deleted')
 
