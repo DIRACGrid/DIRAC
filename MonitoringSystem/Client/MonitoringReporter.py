2c3d2de62a51842d8ddbd80807fa7f2fbdb9ff32
Zoltan Mathe <zoltan.mathe@cern.ch>
2017-07-31 16:14:10 +0200

    use different connections to connect the MQ service

--- MonitoringSystem/Client/MonitoringReporter.py
+++ MonitoringSystem/Client/MonitoringReporter.py
@@ -33,27 +33,16 @@ class MonitoringReporter( object ):
   :param threading.RLock __documentLock: is used to lock the local store when it is being modified.
   :param __documents: contains the recods which will be inserted to the db\
   :type __documents: python:list
-  :param bool __mq: we can use MQ if it is available... By default it is not allowed.
   :param str __monitoringType: type of the records which will be inserted to the db. For example: WMSHistory.
-  """
+    """
 
   def __init__( self, monitoringType = '' ):
 
     self.__maxRecordsInABundle = 5000
     self.__documentLock = threading.RLock()
     self.__documents = []
-    self.__mq = False
     self.__monitoringType = None
-    
-    #check the existence of a MQ
-    retVal = getMQParamsFromCS ( "Monitoring::Queue::%s" % monitoringType )
-    if retVal['OK']:
-      result = createProducer( "Monitoring::Queue::%s" % monitoringType )
-      if not result['OK']:
-        gLogger.warn( "Fail to create Producer:", result['Message'] )
-      else:
-        self.__mq = True
-
+   
     self.__monitoringType = monitoringType
 
   def processRecords( self ):
@@ -61,7 +50,7 @@ class MonitoringReporter( object ):
     It consumes all messaged from the MQ (these are failover messages). In case of failure, the messages
     will be inserted to the MQ again.
     """
-    retVal = monitoringDB.ping()  # if the db is not accessible, the records will be not processed
+    retVal = monitoringDB.ping()  # if the db is not accessible, the records will be not processed from MQ
     if retVal['OK']:
       if not retVal['Value']:  # false if we can not connect to the db
         return retVal
@@ -85,8 +74,8 @@ class MonitoringReporter( object ):
         retVal = monitoringDB.put( list( records ), self.__monitoringType )
         if not retVal['OK']:
           failedToProcess.append( records )
-    
-    mqConsumer.close()  # make sure that we will not proccess any more messages.
+             
+    mqConsumer.close()  # make sure that we will not process any more messages.
     # the db is not available and we publish again the data to MQ
     for records in failedToProcess:
       res = self.publishRecords( records )
@@ -97,21 +86,24 @@ class MonitoringReporter( object ):
   def addRecord( self, rec ):
     """
     It inserts the record to the list
-    :param: dict rec it kontains a key/value pair.
+    :param dict rec: it kontains a key/value pair.
     """
     self.__documents.append( rec )
 
-  def publishRecords( self, records ):
+  def publishRecords( self, records, mqProducer = None ):
     """
-    send data to the MQ
-    :param: list records contains a list of key/value pairs (dictionaries)
+    send data to the MQ. If the mqProducer instance is provided, it will be used for publishing the data to MQ.
+    :param list records: contains a list of key/value pairs (dictionaries)
+    :param object mqProducer: We can provide the instance of a producer, which will be used to publish the data
     """
-    if self.__mq:
-      result = createProducer( "Monitoring::Queue::%s" % self.__monitoringType )
-      if not result['OK']:
-        return result
-      else:
-        return result['Value'].put( json.dumps( records ) )
+    
+    if not mqProducer:
+      mqProducer = self.__createProducer()
+      result = mqProducer.put( json.dumps( records ) )
+      mqProducer.close()
+      return result
+    else:
+      return mqProducer.put( json.dumps( records ) )
     
 
   def commit( self ):
@@ -119,12 +111,13 @@ class MonitoringReporter( object ):
     It inserts the accumulated data to the db. In case of failure
     it keeps in memory/MQ
     """
-    #before we try to insert the data to the db, we process all the data 
-    #which are already in the queue
-    if self.__mq:
+    # before we try to insert the data to the db, we process all the data 
+    # which are already in the queue
+    mqProducer = self.__createProducer()  # we are sure that we can connect to MQ
+    if mqProducer:
       result = self.processRecords()
       if not result['OK']:
-        gLogger.error( "Unable to insert data from the MQ", result['Message'] )
+        gLogger.error( "Unable to insert data to the db:", result['Message'] )
         
     self.__documentLock.acquire()
     documents = self.__documents
@@ -140,8 +133,8 @@ class MonitoringReporter( object ):
           del documents[ :self.__maxRecordsInABundle ]
           gLogger.info( "%d records inserted to the db" % ( recordSent ) )
         else:
-          if self.__mq:
-            res = self.publishRecords( recordsToSend )
+          if mqProducer:
+            res = self.publishRecords( recordsToSend, mqProducer )
             # if we managed to publish the records we can delete from the list
             if res['OK']:
               recordSent += len( recordsToSend )
@@ -154,6 +147,20 @@ class MonitoringReporter( object ):
       gLogger.exception( "Error committing", lException = e )
       return S_ERROR( "Error committing %s" % repr( e ).replace( ',)', ')' ) )
     finally:
+      mqProducer.close()
       self.__documents.extend( documents )
 
     return S_OK( recordSent )
+  
+  def __createProducer( self ):
+    """
+    This method is used to create a producer
+    """
+    mqProducer = None
+    result = createProducer( "Monitoring::Queue::%s" % self.__monitoringType )
+    if not result['OK']:
+      gLogger.warn( "Fail to create Producer:", result['Message'] )
+    else:
+      mqProducer = result['Value']
+    
+    return mqProducer
