d68b967277aac44fda9aaa91526d0305a22d1310
Zoltan Mathe <zoltan.mathe@cern.ch>
2017-08-01 15:03:20 +0200

    add scripts used to generate the bundles

---
+++ Core/scripts/genRevokedCerts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+  gsCerts=/etc/grid-security/certificates
+
+  allF="/opt/dirac/etc/grid-security/allRevokedCerts.pem"
+  copiedCAs=0
+  invalidCAs=0
+  echo "Copying revoked certificates into $allF"
+  for cert in $gsCerts/*.r0
+  do
+     openssl crl -in ${cert} >> $allF.gen
+     copiedCAs=`expr "${copiedCAs}" + "1"`
+  done
+  echo " + Copied ${copiedCAs} revoked certificates into $allF"
+  mv $allF.gen $allF
