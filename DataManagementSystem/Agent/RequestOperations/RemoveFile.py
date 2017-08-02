39ce20d0ae2dcd768f8cae17154328da8c331262
Philippe Charpentier <Philippe.Charpentier@cern.ch>
2017-08-02 14:35:02 +0200

    allow checkSEsRSS to not fail if SE is banned

--- DataManagementSystem/Agent/RequestOperations/RemoveFile.py
+++ DataManagementSystem/Agent/RequestOperations/RemoveFile.py
@@ -79,20 +79,31 @@ class RemoveFile( DMSRequestOperationsBase ):
     replicas = res['Value']['Successful']
     targetSEs = set( [se for lfn in replicas for se in replicas[lfn] ] )
 
-    bannedTargets = set()
     if targetSEs:
-      bannedTargets = self.checkSEsRSS( targetSEs, access = 'RemoveAccess' )
+      # Check if SEs are allowed for remove but don't fail yet the operation if SEs are always banned
+      bannedTargets = self.checkSEsRSS( targetSEs, access = 'RemoveAccess', failIfBanned = False )
       if not bannedTargets['OK']:
         gMonitor.addMark( "RemoveFileAtt" )
         gMonitor.addMark( "RemoveFileFail" )
         return bannedTargets
       bannedTargets = set( bannedTargets['Value'] )
-      if bannedTargets and 'always banned' in self.operation.Error:
-        return S_OK( "%s targets are always banned for removal" % ",".join( sorted( bannedTargets ) ) )
+    else:
+      bannedTargets = set()
 
     # # prepare waiting file dict
     # # We take only files that have no replica at the banned SEs... If no replica, don't
-    toRemoveDict = dict( ( opFile.LFN, opFile ) for opFile in waitingFiles if not bannedTargets.intersection( replicas.get( opFile.LFN, [] ) ) )
+    toRemoveDict = dict( ( opFile.LFN, opFile ) for opFile in waitingFiles \
+                         if not bannedTargets or not bannedTargets.intersection( replicas.get( opFile.LFN, [] ) ) )
+    # If some SEs are always banned, set Failed the files that cannot be removed
+    if bannedTargets and 'always banned' in self.operation.Error:
+      for opFile in waitingFiles:
+        if opFile.LFN not in toRemoveDict:
+          # Set the files that cannot be removed Failed
+          opFile.Error = self.operation.Error
+          opFile.Status = "Failed"
+      if not toRemoveDict:
+        # If there are no files that can be removed, exit, else try once to remove them anyway
+        return S_OK( "%s targets are always banned for removal" % ",".join( sorted( bannedTargets ) ) )
 
     if toRemoveDict:
       gMonitor.addMark( "RemoveFileAtt", len( toRemoveDict ) )
@@ -106,7 +117,7 @@ class RemoveFile( DMSRequestOperationsBase ):
         toRemoveDict = bulkRemoval["Value"]
 
       # # 2nd step - single file removal
-      for lfn, opFile in toRemoveDict.items():
+      for lfn, opFile in toRemoveDict.iteritems():
         self.log.info( "removing single file %s" % lfn )
         singleRemoval = self.singleRemoval( opFile )
         if not singleRemoval["OK"]:
