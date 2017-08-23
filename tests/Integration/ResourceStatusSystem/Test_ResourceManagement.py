b6f0ee2ae69d5f430b8c1cf3aff0dc361d4ff940
Federico Stagni <federico.stagni@cern.ch>
2017-08-23 19:41:06 +0200

    minor fixes

--- tests/Integration/ResourceStatusSystem/Test_ResourceManagement.py
+++ tests/Integration/ResourceStatusSystem/Test_ResourceManagement.py
@@ -1,6 +1,8 @@
 ''' This is a test of the chain
     ResourceManagementClient -> ResourceManagementHandler -> ResourceManagementDB
     It supposes that the DB is present, and that the service is running
+
+    The DB is supposed to be empty when the test starts
 '''
 
 #pylint: disable=invalid-name,wrong-import-position,missing-docstring
@@ -23,7 +25,7 @@ lastCheckTime = datetime.datetime.now()
 class TestClientResourceManagementTestCase( unittest.TestCase ):
 
   def setUp( self ):
-    self.rsClient = ResourceManagementClient()
+    self.rmClient = ResourceManagementClient()
 
   def tearDown( self ):
     pass
@@ -31,37 +33,41 @@ class TestClientResourceManagementTestCase( unittest.TestCase ):
 
 class ResourceManagementClientChain( TestClientResourceManagementTestCase ):
 
-  def test_addAndRemove(self):
+  def test_AccountingCache(self):
+    """
+    AccountingCache table
+    """
 
-    self.rsClient.deleteAccountingCache('TestName12345')
+    res = self.rmClient.deleteAccountingCache('TestName12345')  # just making sure it's not there (yet)
+    self.assertTrue(res['OK'])
+
 
     # TEST addOrModifyAccountingCache
-    # ...............................................................................
-
-    res = self.rsClient.addOrModifyAccountingCache('TestName12345', 'plotType', 'plotName', 'result',
-                                                   dateEffective, lastCheckTime)
+    res = self.rmClient.addOrModifyAccountingCache('TestName12345', 'plotType', 'plotName', 'result',
+						   datetime.datetime.now(), datetime.datetime.now())
     self.assertTrue(res['OK'])
 
-    res = self.rsClient.selectAccountingCache('TestName12345')
+    res = self.rmClient.selectAccountingCache('TestName12345')
     self.assertTrue(res['OK'])
     #check if the name that we got is equal to the previously added 'TestName12345'
+    print res
     self.assertEqual(res['Value'][0][0], 'TestName12345')
 
-    res = self.rsClient.addOrModifyAccountingCache('TestName12345', 'plotType', 'plotName', 'changedresult',
+    res = self.rmClient.addOrModifyAccountingCache('TestName12345', 'plotType', 'plotName', 'changedresult',
                                                    dateEffective, lastCheckTime)
     self.assertTrue(res['OK'])
 
-    res = self.rsClient.selectAccountingCache('TestName12345')
+    res = self.rmClient.selectAccountingCache('TestName12345')
     #check if the result has changed
-    self.assertEqual(res['Value'][0][3], 'changedresult')
+    self.assertEqual(res['Value'][0][4], 'changedresult')
 
 
     # TEST deleteAccountingCache
     # ...............................................................................
-    res = self.rsClient.deleteAccountingCache('TestName12345')
+    res = self.rmClient.deleteAccountingCache('TestName12345')
     self.assertTrue(res['OK'])
 
-    res = self.rsClient.selectAccountingCache('TestName12345')
+    res = self.rmClient.selectAccountingCache('TestName12345')
     self.assertTrue(res['OK'])
     self.assertFalse(res['Value'])
 
