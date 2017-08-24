4745829a7bf51a18f8de1efb3a036bb96d051051
Federico Stagni <federico.stagni@cern.ch>
2017-08-24 19:07:36 +0200

    added couple tables

--- tests/Integration/ResourceStatusSystem/Test_ResourceManagement.py
+++ tests/Integration/ResourceStatusSystem/Test_ResourceManagement.py
@@ -35,7 +35,7 @@ class ResourceManagementClientChain( TestClientResourceManagementTestCase ):
 
   def test_AccountingCache(self):
     """
-    AccountingCache table
+    DowntimeCache table
     """
 
     res = self.rmClient.deleteAccountingCache('TestName12345')  # just making sure it's not there (yet)
@@ -50,7 +50,6 @@ class ResourceManagementClientChain( TestClientResourceManagementTestCase ):
     res = self.rmClient.selectAccountingCache('TestName12345')
     self.assertTrue(res['OK'])
     #check if the name that we got is equal to the previously added 'TestName12345'
-    print res
     self.assertEqual(res['Value'][0][0], 'TestName12345')
 
     res = self.rmClient.addOrModifyAccountingCache('TestName12345', 'plotType', 'plotName', 'changedresult',
@@ -70,6 +69,156 @@ class ResourceManagementClientChain( TestClientResourceManagementTestCase ):
     res = self.rmClient.selectAccountingCache('TestName12345')
     self.assertTrue(res['OK'])
     self.assertFalse(res['Value'])
+
+
+  def test_DowntimeCache(self):
+    """
+    DowntimeCache table
+    """
+
+    res = self.rmClient.deleteDowntimeCache('TestName12345')  # just making sure it's not there (yet)
+    self.assertTrue(res['OK'])
+
+
+    # TEST addOrModifyDowntimeCache
+    res = self.rmClient.addOrModifyDowntimeCache( 'TestName12345', 'element', 'name',
+						  datetime.datetime.now(), datetime.datetime.now(),
+						  'severity', 'description', 'link',
+						  datetime.datetime.now(), datetime.datetime.now(),
+						  'gOCDBServiceType')
+    self.assertTrue(res['OK'])
+
+    res = self.rmClient.selectDowntimeCache('TestName12345')
+    self.assertTrue(res['OK'])
+    #check if the name that we got is equal to the previously added 'TestName12345'
+    self.assertEqual(res['Value'][0][0], 'TestName12345')
+
+    res = self.rmClient.addOrModifyDowntimeCache('TestName12345', 'element', 'name', severity = 'changedSeverity')
+    self.assertTrue(res['OK'])
+
+    res = self.rmClient.selectDowntimeCache('TestName12345')
+    #check if the result has changed
+    self.assertEqual(res['Value'][0][4], 'changedSeverity')
+
+
+    # TEST deleteDowntimeCache
+    # ...............................................................................
+    res = self.rmClient.deleteDowntimeCache('TestName12345')
+    self.assertTrue(res['OK'])
+
+    res = self.rmClient.selectDowntimeCache('TestName12345')
+    self.assertTrue(res['OK'])
+    self.assertFalse(res['Value'])
+
+
+  def test_GGUSTicketsCache(self):
+    """
+    GGUSTicketsCache table
+    """
+
+    res = self.rmClient.deleteGGUSTicketsCache('TestName12345')  # just making sure it's not there (yet)
+    self.assertTrue(res['OK'])
+
+
+    # TEST addOrModifyGGUSTicketsCache
+    res = self.rmClient.addOrModifyGGUSTicketsCache( 'TestName12345', 'link', 0, 'tickets', datetime.datetime.now() )
+    self.assertTrue(res['OK'])
+
+    res = self.rmClient.selectGGUSTicketsCache('TestName12345')
+    self.assertTrue(res['OK'])
+    #check if the name that we got is equal to the previously added 'TestName12345'
+    self.assertEqual(res['Value'][0][0], 'TestName12345')
+
+    res = self.rmClient.addOrModifyGGUSTicketsCache('TestName12345', 'newLink')
+    self.assertTrue(res['OK'])
+
+    res = self.rmClient.selectGGUSTicketsCache('TestName12345')
+    #check if the result has changed
+    self.assertEqual(res['Value'][0][3], 'newLink')
+
+
+    # TEST deleteGGUSTicketsCache
+    # ...............................................................................
+    res = self.rmClient.deleteGGUSTicketsCache('TestName12345')
+    self.assertTrue(res['OK'])
+
+    res = self.rmClient.selectGGUSTicketsCache('TestName12345')
+    self.assertTrue(res['OK'])
+    self.assertFalse(res['Value'])
+
+
+  def test_JobCache(self):
+    """
+    JobCache table
+    """
+
+    res = self.rmClient.deleteJobCache('TestName12345')  # just making sure it's not there (yet)
+    self.assertTrue(res['OK'])
+
+
+    # TEST addOrModifyJobCache
+    res = self.rmClient.addOrModifyJobCache( 'TestName12345', 'maskstatus', 50.89, 'status', datetime.datetime.now() )
+    self.assertTrue(res['OK'])
+
+    res = self.rmClient.selectJobCache('TestName12345')
+    self.assertTrue(res['OK'])
+    #check if the name that we got is equal to the previously added 'TestName12345'
+    self.assertEqual(res['Value'][0][0], 'TestName12345')
+
+    res = self.rmClient.addOrModifyJobCache('TestName12345', status = 'newStatus')
+    self.assertTrue(res['OK'])
+
+    res = self.rmClient.selectJobCache('TestName12345')
+    #check if the result has changed
+    self.assertEqual(res['Value'][0][1], 'newStatus')
+
+
+    # TEST deleteJobCache
+    # ...............................................................................
+    res = self.rmClient.deleteJobCache('TestName12345')
+    self.assertTrue(res['OK'])
+
+    res = self.rmClient.selectJobCache('TestName12345')
+    self.assertTrue(res['OK'])
+    self.assertFalse(res['Value'])
+
+  def test_PilotCache(self):
+    """
+    PilotCache table
+    """
+
+    res = self.rmClient.deletePilotCache('TestName12345')  # just making sure it's not there (yet)
+    self.assertTrue(res['OK'])
+
+
+    # TEST addOrModifyPilotCache
+    res = self.rmClient.addOrModifyPilotCache( 'TestName12345', 'CE', 0.0, 25.5, 'status', datetime.datetime.now() )
+    self.assertTrue(res['OK'])
+
+    res = self.rmClient.selectPilotCache('TestName12345')
+    self.assertTrue(res['OK'])
+    #check if the name that we got is equal to the previously added 'TestName12345'
+    self.assertEqual(res['Value'][0][0], 'TestName12345')
+
+    res = self.rmClient.addOrModifyPilotCache('TestName12345', status = 'newStatus')
+    self.assertTrue(res['OK'])
+
+    res = self.rmClient.selectPilotCache('TestName12345')
+    #check if the result has changed
+    self.assertEqual(res['Value'][0][2], 'newStatus')
+
+
+    # TEST deletePilotCache
+    # ...............................................................................
+    res = self.rmClient.deletePilotCache('TestName12345')
+    self.assertTrue(res['OK'])
+
+    res = self.rmClient.selectPilotCache('TestName12345')
+    self.assertTrue(res['OK'])
+    self.assertFalse(res['Value'])
+
+# FIXME: add other tables
+
 
 if __name__ == '__main__':
   suite = unittest.defaultTestLoader.loadTestsFromTestCase( TestClientResourceManagementTestCase )
