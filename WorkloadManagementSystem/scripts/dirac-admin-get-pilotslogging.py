de06fb251094b49e6f072c4fdf3b39dfe94cc376
Miłosz <milosz.zdybal@gmail.com>
2017-10-17 10:07:45 +0200

    Utility admin script to get PilotsLogging

---
+++ WorkloadManagementSystem/scripts/dirac-admin-get-pilotslogging.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+"""
+  Get Pilots Logging for specific Pilot UUID or Job ID.
+"""
+__RCSID__ = "$Id$"
+
+import sys
+from DIRAC import S_OK
+from DIRAC.Core.Base import Script
+from DIRAC.WorkloadManagementSystem.Client.PilotsLoggingClient import PilotsLoggingClient
+from DIRAC.Core.Utilities.PrettyPrint import printTable
+
+def getByUUID( optVal ):
+  pilotsLogging = PilotsLoggingClient()
+  result = pilotsLogging.getPilotsLogging( optVal )
+  if not result['OK']:
+    print 'ERROR: %s' % result['Message']
+    sys.exit( 1 )
+  labels = ['pilotUUID', 'timestamp', 'source', 'phase', 'status', 'messageContent']
+  content = []
+  for log in result['Value']:
+    content.append( [ log['pilotUUID'], log['timestamp'], log['source'], log['phase'], log['status'], log['messageContent'] ] )
+  printTable( labels, content, numbering = False, columnSeparator=' | ' )
+  return S_OK()
+
+Script.registerSwitch( 'u:', 'uuid=', 'get PilotsLogging for given Pilot UUID', getByUUID )
+#Script.registerSwitch( 'j:', 'jobid=', 'get PilotsLogging for given Job ID', getByJobID )
+
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                     'Usage:',
+                                     '  %s [option|cfgfile] ' % Script.scriptName ] ) )
+
+Script.parseCommandLine()
