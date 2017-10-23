5f1eaa0f26048e502d288c9d6c3f5b0d804bd792
Federico Stagni <federico.stagni@cern.ch>
2017-10-23 10:58:29 +0200

    application log can be in 'logFile'

--- Workflow/Modules/Script.py
+++ Workflow/Modules/Script.py
@@ -68,7 +68,8 @@ class Script( ModuleBase ):
     if os.path.exists( os.path.basename( self.executable ) ):
       self.executable = os.path.basename( self.executable )
       if not os.access( '%s/%s' % ( os.getcwd(), self.executable ), 5 ):
-        os.chmod( '%s/%s' % ( os.getcwd(), self.executable ), stat.S_IRWXU | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH )
+        os.chmod( '%s/%s' % ( os.getcwd(), self.executable ),
+                  stat.S_IRWXU | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH )
       self.command = '%s/%s' % ( os.getcwd(), self.executable )
     elif re.search( '.py$', self.executable ):
       self.command = '%s %s' % ( sys.executable, self.executable )
@@ -105,11 +106,10 @@ class Script( ModuleBase ):
     if os.path.exists( self.applicationLog ):
       self.log.verbose( 'Removing existing %s' % self.applicationLog )
       os.remove( self.applicationLog )
-    fopen = open( '%s/%s' % ( os.getcwd(), self.applicationLog ), 'w' )
-    fopen.write( "<<<<<<<<<< %s Standard Output >>>>>>>>>>\n\n%s " % ( self.executable, stdout ) )
-    if stderr:
-      fopen.write( "<<<<<<<<<< %s Standard Error >>>>>>>>>>\n\n%s " % ( self.executable, stderr ) )
-    fopen.close()
+    with open( '%s/%s' % ( os.getcwd(), self.applicationLog ), 'w' ) as fopen:
+      fopen.write( "<<<<<<<<<< %s Standard Output >>>>>>>>>>\n\n%s " % ( self.executable, stdout ) )
+      if stderr:
+        fopen.write( "<<<<<<<<<< %s Standard Error >>>>>>>>>>\n\n%s " % ( self.executable, stderr ) )
     self.log.info( "Output written to %s, execution complete." % ( self.applicationLog ) )
 
     if failed:
