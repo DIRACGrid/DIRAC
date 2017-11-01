57bde81f60bae47efc3f0d16b84fff85a31efb89
Miłosz <milosz.zdybal@gmail.com>
2017-11-01 13:33:10 +0100

    autopep8

--- WorkloadManagementSystem/scripts/dirac-admin-get-pilotslogging.py
+++ WorkloadManagementSystem/scripts/dirac-admin-get-pilotslogging.py
@@ -14,7 +14,8 @@ from DIRAC.Core.Utilities.PrettyPrint import printTable
 uuid = None
 jobid = None
 
-def setUUID( optVal ):
+
+def setUUID(optVal):
   """
   Set UUID from arguments
   """
@@ -22,7 +23,8 @@ def setUUID( optVal ):
   uuid = optVal
   return S_OK()
 
-def setJobID( optVal ):
+
+def setJobID(optVal):
   """
   Set JobID from arguments
   """
@@ -30,46 +32,49 @@ def setJobID( optVal ):
   jobid = optVal
   return S_OK()
 
-Script.registerSwitch( 'u:', 'uuid=', 'get PilotsLogging for given Pilot UUID', setUUID )
-Script.registerSwitch( 'j:', 'jobid=', 'get PilotsLogging for given Job ID', setJobID )
 
-Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
-                                     'Usage:',
-                                     '  %s option value ' % Script.scriptName ] ) )
+Script.registerSwitch('u:', 'uuid=', 'get PilotsLogging for given Pilot UUID', setUUID)
+Script.registerSwitch('j:', 'jobid=', 'get PilotsLogging for given Job ID', setJobID)
+
+Script.setUsageMessage('\n'.join([__doc__.split('\n')[1],
+                                  'Usage:',
+                                  '  %s option value ' % Script.scriptName]))
 
 Script.parseCommandLine()
 
-def printPilotsLogging( logs ):
+
+def printPilotsLogging(logs):
   """
   Print results using printTable from PrettyPrint
   """
   content = []
   labels = ['pilotUUID', 'timestamp', 'source', 'phase', 'status', 'messageContent']
   for log in logs:
-    content.append( [log[label] for label in labels] )
-  printTable( labels, content, numbering = False, columnSeparator=' | ' )
+    content.append([log[label] for label in labels])
+  printTable(labels, content, numbering=False, columnSeparator=' | ')
+
 
 if uuid:
   pilotsLogging = PilotsLoggingClient()
-  result = pilotsLogging.getPilotsLogging( uuid )
+  result = pilotsLogging.getPilotsLogging(uuid)
   if not result['OK']:
     print 'ERROR: %s' % result['Message']
-    DIRAC.exit( 1 )
-  printPilotsLogging( result['Value'] )
-  DIRAC.exit( 0 )
+    DIRAC.exit(1)
+  printPilotsLogging(result['Value'])
+  DIRAC.exit(0)
 else:
   pilotDB = PilotAgentsDB()
   pilotsLogging = PilotsLoggingClient()
-  pilots = pilotDB.getPilotsForJobID( jobid )
-  if not pilots[ 'OK ']:
+  pilots = pilotDB.getPilotsForJobID(jobid)
+  if not pilots['OK ']:
     print pilots['Message']
   for pilotID in pilots:
-    info = pilotDB.getPilotInfo( pilotID = pilotID )
+    info = pilotDB.getPilotInfo(pilotID=pilotID)
     if not info['OK']:
       print info['Message']
     for pilot in info:
-      logging = pilotsLogging.getPilotsLogging( pilot['PilotJobReference'] )
+      logging = pilotsLogging.getPilotsLogging(pilot['PilotJobReference'])
       if not logging['OK']:
         print logging['Message']
-      printPilotsLogging( logging )
-  DIRAC.exit( 0 )
+      printPilotsLogging(logging)
+  DIRAC.exit(0)
