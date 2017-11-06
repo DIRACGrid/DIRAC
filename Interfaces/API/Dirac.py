da4d611b059d8201f1c2ce8c54ef096b5e3b9bdd
Andre Sailer <andre.philippe.sailer@cern.ch>
2017-11-06 11:39:35 +0100

    API.Dirac.__getJDLParameters: use hasattr instead of import+isinstance

--- Interfaces/API/Dirac.py
+++ Interfaces/API/Dirac.py
@@ -2600,9 +2600,7 @@ class Dirac( API ):
     :param jdl: a JDL
     :type jdl: ~DIRAC.Interfaces.API.Job.Job or str or file
     """
-    # import here, becase Job module imports this module
-    from DIRAC.Interfaces.API.Job import Job
-    if isinstance(jdl, Job):
+    if hasattr(jdl, '_toJDL'):
       jdl = jdl._toJDL()
     elif os.path.exists( jdl ):
       with open( jdl, 'r' ) as jdlFile:
