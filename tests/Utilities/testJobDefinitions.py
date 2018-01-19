9fb48fbb28e06f28e2ef4b0bd0ee19535a48c796
Federico Stagni <federico.stagni@cern.ch>
2018-01-19 15:21:38 +0100

    try..except

--- tests/Utilities/testJobDefinitions.py
+++ tests/Utilities/testJobDefinitions.py
@@ -1,63 +1,76 @@
 """ Collection of user jobs for testing purposes
 """
 
-from DIRAC.tests.Utilities.utils import find_all
+import os
+
+from DIRAC import rootPath
 from DIRAC.Interfaces.API.Job import Job
 from DIRAC.Interfaces.API.Dirac import Dirac
-from DIRAC import rootPath
+
+from DIRAC.tests.Utilities.utils import find_all
 
 # parameters
 
 # Common functions
 
-def getJob( jobClass = None ):
+
+def getJob(jobClass=None):
   if not jobClass:
     jobClass = Job
   oJob = jobClass()
   return oJob
 
-def getDIRAC( diracClass = None ):
+
+def getDIRAC(diracClass=None):
   if not diracClass:
     diracClass = Dirac
   oDirac = diracClass()
   return oDirac
 
 
-def baseToAllJobs( jName, jobClass = None ):
+def baseToAllJobs(jName, jobClass=None):
 
   print "**********************************************************************************************************"
   print "\n Submitting job ", jName
 
-  J = getJob( jobClass )
-  J.setName( jName )
-  J.setCPUTime( 17800 )
+  J = getJob(jobClass)
+  J.setName(jName)
+  J.setCPUTime(17800)
   return J
 
 
-def endOfAllJobs( J ):
-  result = getDIRAC().submit( J )
+def endOfAllJobs(J):
+  result = getDIRAC().submit(J)
   print "Job submission result:", result
   if result['OK']:
-    jobID = int( result['Value'] )
+    jobID = int(result['Value'])
     print "Submitted with job ID:", jobID
 
   return result
-
 
 
 # List of jobs
 
 def helloWorld():
 
-  J = baseToAllJobs( 'helloWorld' )
-  J.setInputSandbox( [find_all( 'exe-script.py', rootPath, 'DIRAC/tests/Workflow' )[0]] )
-  J.setExecutable( "exe-script.py", "", "helloWorld.log" )
-  return endOfAllJobs( J )
+  J = baseToAllJobs('helloWorld')
+  try:
+    J.setInputSandbox([find_all('exe-script.py', rootPath, 'DIRAC/tests/Workflow')[0]])
+  except IndexError:  # we are in Jenkins
+    J.setInputSandbox([find_all('exe-script.py', os.environ['WORKSPACE'], 'DIRAC/tests/Workflow')[0]])
+  J.setExecutable("exe-script.py", "", "helloWorld.log")
+  return endOfAllJobs(J)
+
 
 def mpJob():
-  J = baseToAllJobs( 'mpJob' )
-  J.setInputSandbox( [find_all( 'mpTest.py', rootPath, 'DIRAC/tests/Utilities' )[0]] +
-                     [find_all( 'testMpJob.sh', rootPath, 'DIRAC/tests/Utilities' )[0]])
-  J.setExecutable( 'testMpJob.sh mpTest.py' )
-  J.setTag( 'MultiProcessor' )
-  return endOfAllJobs( J )
+  J = baseToAllJobs('mpJob')
+  try:
+    J.setInputSandbox([find_all('mpTest.py', rootPath, 'DIRAC/tests/Utilities')[0]] +
+                      [find_all('testMpJob.sh', rootPath, 'DIRAC/tests/Utilities')[0]])
+  except IndexError:  # we are in Jenkins
+    J.setInputSandbox([find_all('mpTest.py', os.environ['WORKSPACE'], 'DIRAC/tests/Utilities')[0]] +
+                      [find_all('testMpJob.sh', os.environ['WORKSPACE'], 'DIRAC/tests/Utilities')[0]])
+
+  J.setExecutable('testMpJob.sh mpTest.py')
+  J.setTag('MultiProcessor')
+  return endOfAllJobs(J)
