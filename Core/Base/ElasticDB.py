5ecd98e7768cefc0dd2238ea201fe926976cfa35
Federico Stagni <federico.stagni@cern.ch>
2018-01-30 16:42:34 +0100

    attempting to connect with useSSL=False in case of failures

--- Core/Base/ElasticDB.py
+++ Core/Base/ElasticDB.py
@@ -8,7 +8,8 @@ from DIRAC.ConfigurationSystem.Client.Utilities import getElasticDBParameters
 
 __RCSID__ = "$Id$"
 
-class ElasticDB( ElasticSearchDB ):
+
+class ElasticDB(ElasticSearchDB):
 
   """
   .. class:: ElasticDB
@@ -19,7 +20,7 @@ class ElasticDB( ElasticSearchDB ):
   """
 
   ########################################################################
-  def __init__( self, dbname, fullName, indexPrefix = '' ):
+  def __init__(self, dbname, fullName, indexPrefix=''):
     """ c'tor
 
     :param self: self reference
@@ -29,37 +30,50 @@ class ElasticDB( ElasticSearchDB ):
     """
 
     database_name = dbname
-    self.log = gLogger.getSubLogger( database_name )
+    self.log = gLogger.getSubLogger(database_name)
 
-    result = getElasticDBParameters( fullName )
-    if not result['OK'] :
-      raise RuntimeError( 'Cannot get database parameters: %s' % result['Message'] )
+    result = getElasticDBParameters(fullName)
+    if not result['OK']:
+      raise RuntimeError('Cannot get database parameters: %s' % result['Message'])
 
-    dbParameters = result[ 'Value' ]
-    self.__dbHost = dbParameters[ 'Host' ]
-    self.__dbPort = dbParameters[ 'Port' ]
-    #we can have db which does not have any authentication...
+    dbParameters = result['Value']
+    self.__dbHost = dbParameters['Host']
+    self.__dbPort = dbParameters['Port']
+    # we can have db which does not have any authentication...
     self.__user = ''
     if 'User' in dbParameters:
-      self.__user = dbParameters[ 'User' ]
+      self.__user = dbParameters['User']
     self.__dbPassword = ''
     if 'Password' in dbParameters:
-      self.__dbPassword = dbParameters[ 'Password' ]
+      self.__dbPassword = dbParameters['Password']
 
-    super( ElasticDB, self ).__init__( self.__dbHost, self.__dbPort, self.__user, self.__dbPassword, indexPrefix )
+    try:
+      super(ElasticDB, self).__init__(self.__dbHost,
+                                      self.__dbPort,
+                                      self.__user,
+                                      self.__dbPassword,
+                                      indexPrefix)
+    except AttributeError as e:
+      self.log.warn("Attempt to connect with useSSL=True failed, trying without")
+      self.log.warn(repr(e))
+      super(ElasticDB, self).__init__(self.__dbHost,
+                                      self.__dbPort,
+                                      self.__user,
+                                      self.__dbPassword,
+                                      indexPrefix,
+                                      useSSL=False)
 
     if not self._connected:
-      raise RuntimeError( 'Can not connect to DB %s, exiting...' % self.clusterName )
+      raise RuntimeError('Can not connect to DB %s, exiting...' % self.clusterName)
 
-
-    self.log.info( "==================================================" )
-    self.log.info( "Host: %s " % self.__dbHost )
-    self.log.info( "Port: %d "  % self.__dbPort )
-    self.log.info( "ClusterName: %s   " % self.clusterName )
-    self.log.info( "==================================================" )
+    self.log.info("==================================================")
+    self.log.info("Host: %s " % self.__dbHost)
+    self.log.info("Port: %d " % self.__dbPort)
+    self.log.info("ClusterName: %s   " % self.clusterName)
+    self.log.info("==================================================")
 
   ########################################################################
-  def setDbHost( self, hostName ):
+  def setDbHost(self, hostName):
     """
      It is used to set the cluster host
 
@@ -68,14 +82,14 @@ class ElasticDB( ElasticSearchDB ):
     self.__dbHost = hostName
 
   ########################################################################
-  def getDbHost( self ):
+  def getDbHost(self):
     """
      It returns the elasticsearch database host
     """
     return self.__dbHost
 
   ########################################################################
-  def setDbPort( self, port ):
+  def setDbPort(self, port):
     """
      It is used to set the cluster port
 
@@ -85,7 +99,7 @@ class ElasticDB( ElasticSearchDB ):
     self.__dbPort = port
 
   ########################################################################
-  def getDbPort( self ):
+  def getDbPort(self):
     """
        It returns the database port
 
