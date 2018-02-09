6d023a2c2e2b9031d39f3968e7a18e2e97ecc6f5
Federico Stagni <federico.stagni@cern.ch>
2018-02-09 14:51:32 +0100

    fix TypeError

--- tests/System/unitTestUserJobs.py
+++ tests/System/unitTestUserJobs.py
@@ -46,7 +46,7 @@ class submitSuccess(GridSubmissionTestCase):
     self.assertTrue(res['OK'])
     jobsSubmittedList.append(res['Value'])
 
-    print "submitted %d jobs: %s" % (len(jobsSubmittedList), ','.join(jobsSubmittedList))
+    print "submitted %d jobs: %s" % (len(jobsSubmittedList), ','.join(str(js) for js in jobsSubmittedList))
 
 
 # FIXME: This is also in the extension...? To try!
