4f81d765dc8e73c7b20b2c9f9b096fdc0f5588bd
Federico Stagni <federico.stagni@cern.ch>
2018-02-22 17:21:47 +0100

    style

--- FrameworkSystem/Client/SecurityLogClient.py
+++ FrameworkSystem/Client/SecurityLogClient.py
@@ -1,55 +1,57 @@
+""" For reporting messages to security log service
+"""
 
 import syslog
+
 from DIRAC import gLogger, gConfig
 from DIRAC.Core.DISET.RPCClient import RPCClient
 from DIRAC.Core.Utilities import Time
 from DIRAC.Core.Utilities.ThreadScheduler import gThreadScheduler
 
-class SecurityLogClient:
+
+class SecurityLogClient(object):
 
   __securityLogStore = []
 
-  def __init__( self ):
+  def __init__(self):
     self.__messagesList = []
     self.__maxMessagesInBundle = 1000
     self.__maxMessagesWaiting = 10000
-    self.__taskId = gThreadScheduler.addPeriodicTask( 30, self.__sendData )
+    self.__taskId = gThreadScheduler.addPeriodicTask(30, self.__sendData)
 
-  def addMessage( self, success, sourceIP, sourcePort, sourceIdentity,
-                  destinationIP, destinationPort, destinationService,
-                  action, timestamp = False ):
+  def addMessage(self, success, sourceIP, sourcePort, sourceIdentity,
+                 destinationIP, destinationPort, destinationService,
+                 action, timestamp=False):
     if not timestamp:
       timestamp = Time.dateTime()
-    msg = ( timestamp, success, sourceIP, sourcePort, sourceIdentity,
-            destinationIP, destinationPort, destinationService, action )
-    if gConfig.getValue( "/Registry/EnableSysLog", False ):
+    msg = (timestamp, success, sourceIP, sourcePort, sourceIdentity,
+           destinationIP, destinationPort, destinationService, action)
+    if gConfig.getValue("/Registry/EnableSysLog", False):
       strMsg = "Time=%s Accept=%s Source=%s:%s SourceID=%s Destination=%s:%s Service=%s Action=%s"
-      syslog.syslog( strMsg % msg )
-    while len( self.__messagesList ) > self.__maxMessagesWaiting:
-      self.__messagesList.pop( 0 )
+      syslog.syslog(strMsg % msg)
+    while len(self.__messagesList) > self.__maxMessagesWaiting:
+      self.__messagesList.pop(0)
     if not self.__securityLogStore:
-      self.__messagesList.append( msg )
+      self.__messagesList.append(msg)
     else:
-      self.__securityLogStore[0].logAction( msg )
+      self.__securityLogStore[0].logAction(msg)
 
-  def setLogStore( self, logStore ):
+  def setLogStore(self, logStore):
     while self.__securityLogStore:
       self.__securityLogStore.pop()
-    self.__securityLogStore.append( logStore )
-    gThreadScheduler.addPeriodicTask( 10, self.__sendData, executions = 1 )
+    self.__securityLogStore.append(logStore)
+    gThreadScheduler.addPeriodicTask(10, self.__sendData, executions=1)
 
-  def __sendData( self ):
-    gLogger.verbose( "Sending records to security log service..." )
+  def __sendData(self):
+    gLogger.verbose("Sending records to security log service...")
     msgList = self.__messagesList
     self.__messagesList = []
-    rpcClient = RPCClient( "Framework/SecurityLogging" )
-    for i in range( 0, len( msgList ), self.__maxMessagesInBundle ):
-      msgsToSend = msgList[ :self.__maxMessagesInBundle ]
-      result = rpcClient.logActionBundle( msgsToSend )
-      if not result[ 'OK' ]:
-        self.__messagesList.extend( msgList )
+    rpcClient = RPCClient("Framework/SecurityLogging")
+    for _i in range(0, len(msgList), self.__maxMessagesInBundle):
+      msgsToSend = msgList[:self.__maxMessagesInBundle]
+      result = rpcClient.logActionBundle(msgsToSend)
+      if not result['OK']:
+        self.__messagesList.extend(msgList)
         break
-      msgList = msgList[ self.__maxMessagesInBundle: ]
-    gLogger.verbose( "Data sent to security log service" )
-
-
+      msgList = msgList[self.__maxMessagesInBundle:]
+    gLogger.verbose("Data sent to security log service")
