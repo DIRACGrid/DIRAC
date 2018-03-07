a27b361dfc7a84037f74d9f74d557007c7cdf30d
Federico Stagni <federico.stagni@cern.ch>
2018-03-07 13:27:29 +0100

    added documentation

--- ResourceStatusSystem/Command/FreeDiskSpaceCommand.py
+++ ResourceStatusSystem/Command/FreeDiskSpaceCommand.py
@@ -59,7 +59,7 @@ class FreeDiskSpaceCommand(Command):
     of ResourceManagementDB database.
 
     The result is also returned to the caller, not only inserted.
-    What is inserted in the DB will be in MB,
+    What is inserted in the DB will normally be in MB,
     what is returned will be in the specified unit.
     """
 
