62e1ae30c13dccc82d7cad4c95b2fbd75b02f720
Federico Stagni <federico.stagni@cern.ch>
2018-05-28 18:17:59 +0200

    style

--- docs/source/DeveloperGuide/Systems/Framework/stableconns/service.py
+++ docs/source/DeveloperGuide/Systems/Framework/stableconns/service.py
@@ -5,13 +5,14 @@ from DIRAC import S_OK
 from DIRAC.Core.DISET.RequestHandler import RequestHandler
 
 
-class PingPongHandler( RequestHandler ):
+class PingPongHandler(RequestHandler):
 
-  MSG_DEFINITIONS = { 'Ping' : { 'id' : ( int, long ) },
-                      'Pong' : { 'id' : ( int, long ) } }
+  MSG_DEFINITIONS = {'Ping': {'id': (int, long)},
+                     'Pong': {'id': (int, long)}}
 
-  auth_conn_connected = [ 'all' ]
-  def conn_connected( self, trid, identity, kwargs ):
+  auth_conn_connected = ['all']
+
+  def conn_connected(self, trid, identity, kwargs):
     """
     This function will be called when a new client connects.
     It is not mandatory to have this function
@@ -21,29 +22,31 @@ class PingPongHandler( RequestHandler ):
       @identity: Unique for each client even if it reconnects
       @kwargs: Arguments sent by the client for the connection
     """
-    #Do something with trid/identity/kwargs if needed
+    # Do something with trid/identity/kwargs if needed
     return S_OK()
 
-  auth_conn_drop = [ 'all' ]
-  def conn_drop( self, trid ):
+  auth_conn_drop = ['all']
+
+  def conn_drop(self, trid):
     """
     This function will be called when a client disconnects.
     It is not mandatory to have this function
     """
     return S_OK()
 
-  auth_msg_Ping = [ 'all' ]
-  def msg_Ping( self, msgObj ):
+  auth_msg_Ping = ['all']
+
+  def msg_Ping(self, msgObj):
     """
     Callback for Ping message
     """
     pingid = msgObj.id
-    result = self.srv_msgCreate( "Pong" )
-    if not result[ 'OK' ]:
-      #Something went wrong :P
+    result = self.srv_msgCreate("Pong")
+    if not result['OK']:
+      # Something went wrong :P
       return result
-    pongObj = result[ 'Value' ]
+    pongObj = result['Value']
     pongObj.id = pingid
-    #Could have been
-    #return self.srv_msgReply( pongObj )
-    return self.srv_msgSend( self.srv_getTransportID(), pongObj )
+    # Could have been
+    # return self.srv_msgReply( pongObj )
+    return self.srv_msgSend(self.srv_getTransportID(), pongObj)
