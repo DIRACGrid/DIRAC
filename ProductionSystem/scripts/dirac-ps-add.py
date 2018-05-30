0d243c74a4b92d0d79cdd9987452a4f1a924f383
arrabito <arrabito@in2p3.fr>
2018-05-30 12:15:21 +0200

    first implementation of the production system

--- ProductionSystem/scripts/dirac-ps-add.py
+++ ProductionSystem/scripts/dirac-ps-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-  Delete the production
+  Create a production
 """
 
 __RCSID__ = "$Id$"
@@ -11,9 +11,9 @@ from DIRAC.Core.Base import Script
 
 Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                      'Usage:',
-                                     '  %s prodID' % Script.scriptName,
+                                     '  %s prodName' % Script.scriptName,
                                      'Arguments:',
-                                     '  prodID: Production ID'
+                                     '  prodName: Production Name'
                                      ] ) )
 
 
@@ -21,21 +21,25 @@ Script.parseCommandLine()
 
 from DIRAC.ProductionSystem.Client.ProductionClient import ProductionClient
 
+prodClient = ProductionClient()
+
+# get arguments
 args = Script.getPositionalArgs()
 if ( len( args ) != 1 ):
   Script.showHelp()
 
-# get arguments
-prodID = args[0]
+prodName = args[0]
 
-prodClient = ProductionClient()
+### Create a production
+res = prodClient.addProduction( prodName )
 
-res = prodClient.deleteProduction( prodID )
-if res['OK']:
-  DIRAC.gLogger.notice ( 'Production %s successully deleted' % prodID )
-else:
-  DIRAC.gLogger.error ( res['Message'] )
+if not res['OK']:
+  DIRAC.gLogger.error( res['Message'] )
   DIRAC.exit( -1 )
+
+prodID = res['Value']
+
+DIRAC.gLogger.notice( 'Production %s successfully created' % prodID )
 
 DIRAC.exit( 0 )
 
--- ProductionSystem/scripts/dirac-ps-get-trans-from-prod.py
+++ ProductionSystem/scripts/dirac-ps-get-trans-from-prod.py
@@ -10,13 +10,13 @@ import DIRAC
 from DIRAC.Core.Base import Script
 from DIRAC.Core.Utilities.PrettyPrint import printTable
 
-Script.setUsageMessage('\n'.join([__doc__.split('\n')[1],
-                                  'Usage:',
-                                  '  %s prodID' % Script.scriptName,
-                                  'Arguments:',
-                                  '  prodID: Production ID',
-                                  '\ne.g: %s 381' % Script.scriptName,
-                                  ]))
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                     'Usage:',
+                                     '  %s prodID' % Script.scriptName,
+                                     'Arguments:',
+                                     '  prodID: Production ID',
+                                     '\ne.g: %s 381' % Script.scriptName,
+                                     ] ) )
 
 
 Script.parseCommandLine()
@@ -24,27 +24,29 @@ Script.parseCommandLine()
 from DIRAC.ProductionSystem.Client.ProductionClient import ProductionClient
 
 args = Script.getPositionalArgs()
-if (len(args) != 1):
+if ( len( args ) != 1 ):
   Script.showHelp()
 
 # get arguments
 prodID = args[0]
 
 prodClient = ProductionClient()
-res = prodClient.getProductionTransformations(prodID)
+res = prodClient.getProductionTransformations( prodID )
 
-fields = ['ProductionID', 'TransformationID', 'ParentTransformationID', 'LastUpdate', 'InsertedTime']
+fields = ['ProductionID','TransformationID','ParentTransformationID','LastUpdate','InsertedTime']
 records = []
 
 if res['OK']:
   transList = res['Value']
   for trans in transList:
-    records.append([str(trans['ProductionID']), str(trans['TransformationID']), str(trans['ParentTransformationID']),
-                    str(trans['LastUpdate']), str(trans['InsertedTime'])])
+    records.append( [str(trans['ProductionID']), str(trans['TransformationID']), str(trans['ParentTransformationID']), \
+                     str(trans['LastUpdate']), str(trans['InsertedTime'])] )
 else:
-  DIRAC.gLogger.error(res['Message'])
-  DIRAC.exit(-1)
+  DIRAC.gLogger.error ( res['Message'] )
+  DIRAC.exit( -1 )
 
-printTable(fields, records)
+printTable( fields, records )
 
-DIRAC.exit(0)
+DIRAC.exit( 0 )
+
+
