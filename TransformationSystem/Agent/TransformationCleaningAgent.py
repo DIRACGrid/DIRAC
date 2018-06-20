e9b95abac03820150dcde782f84aa9a7403d01de
Zoltan Mathe <zoltan.mathe@cern.ch>
2018-06-20 10:39:56 +0200

    handle exception when the result is not a string

--- TransformationSystem/Agent/TransformationCleaningAgent.py
+++ TransformationSystem/Agent/TransformationCleaningAgent.py
@@ -204,7 +204,11 @@ class TransformationCleaningAgent(AgentModule):
       transDirectories = []
       if res['Value']:
         if not isinstance(res['Value'], list):
-          transDirectories = ast.literal_eval(res['Value'])
+          try:
+            transDirectories = ast.literal_eval(res['Value'])
+          except:
+            # It can happen if the res['Value'] is '/a/b/c' instead of '["/a/b/c"]'
+            transDirectories = list(res['Value'])
         else:
           transDirectories = res['Value']
       directories = self._addDirs(transID, transDirectories, directories)
