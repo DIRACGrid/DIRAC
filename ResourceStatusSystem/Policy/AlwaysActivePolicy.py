f1dd9339a26168713d47429a8d4e62ad2a56c370
Federico Stagni <federico.stagni@cern.ch>
2018-07-17 12:35:09 +0200

    autopep8

--- ResourceStatusSystem/Policy/AlwaysActivePolicy.py
+++ ResourceStatusSystem/Policy/AlwaysActivePolicy.py
@@ -3,27 +3,27 @@
 
 __RCSID__ = '$Id$'
 
-from DIRAC                                              import S_OK
+from DIRAC import S_OK
 from DIRAC.ResourceStatusSystem.PolicySystem.PolicyBase import PolicyBase
 
 
-class AlwaysActivePolicy( PolicyBase ):
+class AlwaysActivePolicy(PolicyBase):
   '''
     The AlwaysActivePolicy is a dummy module that can be used as example, it
     always returns Active status.
   '''
 
   @staticmethod
-  def _evaluate( commandResult ):
+  def _evaluate(commandResult):
     '''
       It returns Active status, evaluates the default command, but its output
       is completely ignored.
     '''
 
-    policyResult = {'Status' : 'Active',
-                    'Reason' : 'AlwaysActive'}
+    policyResult = {'Status': 'Active',
+                    'Reason': 'AlwaysActive'}
 
-    return S_OK( policyResult )
+    return S_OK(policyResult)
 
 ################################################################################
-#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
+# EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
--- ResourceStatusSystem/Policy/AlwaysBannedPolicy.py
+++ ResourceStatusSystem/Policy/AlwaysBannedPolicy.py
@@ -3,27 +3,27 @@
 
 __RCSID__ = '$Id$'
 
-from DIRAC                                              import S_OK
+from DIRAC import S_OK
 from DIRAC.ResourceStatusSystem.PolicySystem.PolicyBase import PolicyBase
 
 
-class AlwaysBannedPolicy( PolicyBase ):
+class AlwaysBannedPolicy(PolicyBase):
   '''
     The AlwaysBannedPolicy is a dummy module that can be used as example, it
     always returns Banned status.
   '''
 
   @staticmethod
-  def _evaluate( commandResult ):
+  def _evaluate(commandResult):
     '''
       It returns Banned status, evaluates the default command, but its output
       is completely ignored.
     '''
 
-    policyResult = {'Status' : 'Banned',
-                    'Reason' : 'AlwaysBanned'}
+    policyResult = {'Status': 'Banned',
+                    'Reason': 'AlwaysBanned'}
 
-    return S_OK( policyResult )
+    return S_OK(policyResult)
 
 ################################################################################
-#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
+# EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
