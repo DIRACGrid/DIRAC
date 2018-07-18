3a998a4fa23e16c5258e926e550676fcf2309d4e
Federico Stagni <federico.stagni@cern.ch>
2018-07-18 18:13:53 +0200

    autopep8

--- TransformationSystem/Agent/test/Test_Agent_TransformationSystem.py
+++ TransformationSystem/Agent/test/Test_Agent_TransformationSystem.py
@@ -15,11 +15,11 @@ from DIRAC import gLogger
 from DIRAC.TransformationSystem.Agent.TaskManagerAgentBase import TaskManagerAgentBase
 from DIRAC.TransformationSystem.Agent.TransformationAgent import TransformationAgent
 
-gLogger.setLevel( 'DEBUG' )
+gLogger.setLevel('DEBUG')
 
 
 mockAM = MagicMock()
-tmab_m = importlib.import_module( 'DIRAC.TransformationSystem.Agent.TaskManagerAgentBase' )
+tmab_m = importlib.import_module('DIRAC.TransformationSystem.Agent.TaskManagerAgentBase')
 tmab_m.AgentModule = mockAM
 tmab_m.FileReport = MagicMock()
 tmab = TaskManagerAgentBase()
@@ -27,11 +27,12 @@ tmab.log = gLogger
 tmab.am_getOption = mockAM
 tmab.log.setLevel('DEBUG')
 
+
 @pytest.mark.parametrize("operationsOnTransformationsDict, expected", [
-    ({1:{'Operations':['op1', 'op2'], 'Body':'veryBigBody'}}, ([1], 1)),
-    ({2:{'Operations':['op3', 'op2'], 'Body':'veryveryBigBody'}}, ([1, 2], 2)),
-    ({2:{'Operations':['op3', 'op2'], 'Body':'veryveryBigBody'}}, ([1, 2], 2))
-    ])
+    ({1: {'Operations': ['op1', 'op2'], 'Body':'veryBigBody'}}, ([1], 1)),
+    ({2: {'Operations': ['op3', 'op2'], 'Body':'veryveryBigBody'}}, ([1, 2], 2)),
+    ({2: {'Operations': ['op3', 'op2'], 'Body':'veryveryBigBody'}}, ([1, 2], 2))
+])
 def test__fillTheQueue(operationsOnTransformationsDict, expected):
   tmab._fillTheQueue(operationsOnTransformationsDict)
   assert tmab.transInQueue == expected[0]
@@ -42,29 +43,27 @@ def test__fillTheQueue(operationsOnTransformationsDict, expected):
 
 tc_mock = MagicMock()
 tm_mock = MagicMock()
-clients = {'TransformationClient':tc_mock, 'TaskManager':tm_mock}
+clients = {'TransformationClient': tc_mock, 'TaskManager': tm_mock}
 
-transIDOPBody = {1:{'Operations':['op1', 'op2'], 'Body':'veryBigBody'}}
+transIDOPBody = {1: {'Operations': ['op1', 'op2'], 'Body': 'veryBigBody'}}
 tasks = {'OK': True, 'Value': [{'CreationTime': None,
                                 'ExternalID': '1',
                                 'ExternalStatus': 'Reserved',
                                 'LastUpdateTime': None,
-                                'RunNumber': 0L,
+                                'RunNumber': 0,
                                 'TargetSE': 'Unknown',
-                                'TaskID': 1L,
-                                'TransformationID': 101L},
-                               {'CreationTime': datetime.datetime( 2014, 7, 21, 14, 19, 3 ),
+                                'TaskID': 1,
+                                'TransformationID': 101},
+                               {'CreationTime': datetime.datetime(2014, 7, 21, 14, 19, 3),
                                 'ExternalID': '0',
                                 'ExternalStatus': 'Reserved',
-                                'LastUpdateTime': datetime.datetime( 2014, 7, 21, 14, 19, 3 ),
-                                'RunNumber': 0L,
+                                'LastUpdateTime': datetime.datetime(2014, 7, 21, 14, 19, 3),
+                                'RunNumber': 0,
                                 'TargetSE': 'Unknown',
-                                'TaskID': 2L,
-                                'TransformationID': 101L}]}
+                                'TaskID': 2,
+                                'TransformationID': 101}]}
 sOk = {'OK': True, 'Value': []}
 sError = {'OK': False, 'Message': 'a mess'}
-
-
 
 
 @pytest.mark.parametrize("tcMockReturnValue, tmMockGetSubmittedTaskStatusReturnvalue, expected", [
@@ -73,23 +72,22 @@ sError = {'OK': False, 'Message': 'a mess'}
     (tasks, sError, False),  # tasks, fail in update
     (tasks, {'OK': True, 'Value': {}}, True),  # tasks, nothing to update
     (tasks, {'OK': True, 'Value': {'Running': [1, 2], 'Done': [3]}}, True)  # tasks, to update, no errors
-    ])
+])
 def test_updateTaskStatusSuccess(tcMockReturnValue, tmMockGetSubmittedTaskStatusReturnvalue, expected):
   tc_mock.getTransformationTasks.return_value = tcMockReturnValue
   tm_mock.getSubmittedTaskStatus.return_value = tmMockGetSubmittedTaskStatusReturnvalue
-  res = tmab.updateTaskStatus( transIDOPBody, clients )
+  res = tmab.updateTaskStatus(transIDOPBody, clients)
   assert res['OK'] == expected
-
 
 
 @pytest.mark.parametrize("tcMockGetTransformationFilesReturnValue, tmMockGetSubmittedFileStatusReturnValue, expected", [
     (sError, None, False),  # errors
     (sOk, None, True),  # no files
-    ({'OK': True, 'Value': [{'file1': 'boh', 'TaskID':1}]}, sError, False),  # files, failing to update
-    ({'OK': True, 'Value': [{'file1': 'boh', 'TaskID':1}]}, sOk, True),  # files, nothing to update
-    ({'OK': True, 'Value': [{'file1': 'boh', 'TaskID':1}]},
-     {'OK': True, 'Value': {'file1': 'OK', 'file2': 'NOK'}}, True ),  # files, something to update
-    ])
+    ({'OK': True, 'Value': [{'file1': 'boh', 'TaskID': 1}]}, sError, False),  # files, failing to update
+    ({'OK': True, 'Value': [{'file1': 'boh', 'TaskID': 1}]}, sOk, True),  # files, nothing to update
+    ({'OK': True, 'Value': [{'file1': 'boh', 'TaskID': 1}]},
+     {'OK': True, 'Value': {'file1': 'OK', 'file2': 'NOK'}}, True),  # files, something to update
+])
 def test_updateFileStatusSuccess(tcMockGetTransformationFilesReturnValue,
                                  tmMockGetSubmittedFileStatusReturnValue,
                                  expected):
@@ -99,17 +97,15 @@ def test_updateFileStatusSuccess(tcMockGetTransformationFilesReturnValue,
   assert res['OK'] == expected
 
 
-
-
 @pytest.mark.parametrize("tcMockGetTransformationTasksReturnValue, tmMockUpdateTransformationReservedTasksReturnValue, tcMockSetTaskStatusAndWmsIDReturnValue, expected", [
     (sError, None, None, False),  # errors getting
     (sOk, None, None, True),  # no tasks
     (tasks, sError, None, False),  # tasks, failing to update
-    (tasks, {'OK': True, 'Value': {'NoTasks': [], 'TaskNameIDs': {'1_1':123, '2_1':456}}},
+    (tasks, {'OK': True, 'Value': {'NoTasks': [], 'TaskNameIDs': {'1_1': 123, '2_1': 456}}},
      sError, False),  # tasks, something to update, fail
-    (tasks, {'OK': True, 'Value': {'NoTasks': ['3_4', '5_6'], 'TaskNameIDs': {'1_1':123, '2_1':456}}},
+    (tasks, {'OK': True, 'Value': {'NoTasks': ['3_4', '5_6'], 'TaskNameIDs': {'1_1': 123, '2_1': 456}}},
      {'OK': True}, True),  # tasks, something to update, no fail
-    ])
+])
 def test_checkReservedTasks(tcMockGetTransformationTasksReturnValue,
                             tmMockUpdateTransformationReservedTasksReturnValue,
                             tcMockSetTaskStatusAndWmsIDReturnValue,
@@ -121,12 +117,11 @@ def test_checkReservedTasks(tcMockGetTransformationTasksReturnValue,
   assert res['OK'] == expected
 
 
-
-
 transIDOPBody = {1: {'Operations': ['op1', 'op2'], 'Body': 'veryBigBody',
                      'Owner': 'prodMan', 'OwnerDN': '/ca=man/user=prodMan', 'OwnerGroup': 'prodMans'}}
 sOkJobDict = {'OK': True, 'Value': {'JobDictionary': {123: 'foo', 456: 'bar'}}}
 sOkJobs = {'OK': True, 'Value': {123: 'foo', 456: 'bar'}}
+
 
 @pytest.mark.parametrize("tcMockGetTasksToSubmitReturnValue, tmMockPrepareTransformationTasksReturnValue, tmMockSubmitTransformationTasksReturnValue, tmMockUpdateDBAfterTaskSubmissionReturnValue, expected", [
     (sError, None, None, None, False),  # errors getting
@@ -135,7 +130,7 @@ sOkJobs = {'OK': True, 'Value': {123: 'foo', 456: 'bar'}}
     (sOkJobDict, sOkJobs, sError, None, False),  # tasks, still errors
     (sOkJobDict, sOkJobs, sOk, sError, False),  # tasks, still errors
     (sOkJobDict, sOkJobs, sOk, sOk, True),  # tasks, no errors
-    ])
+])
 def test_submitTasks(tcMockGetTasksToSubmitReturnValue,
                      tmMockPrepareTransformationTasksReturnValue,
                      tmMockSubmitTransformationTasksReturnValue,
@@ -145,52 +140,51 @@ def test_submitTasks(tcMockGetTasksToSubmitReturnValue,
   tm_mock.prepareTransformationTasks.return_value = tmMockPrepareTransformationTasksReturnValue
   tm_mock.submitTransformationTasks.return_value = tmMockSubmitTransformationTasksReturnValue
   tm_mock.updateDBAfterTaskSubmission.return_value = tmMockUpdateDBAfterTaskSubmissionReturnValue
-  res = tmab.submitTasks( transIDOPBody, clients )
+  res = tmab.submitTasks(transIDOPBody, clients)
   assert res['OK'] == expected
 
 
+# TransformationAgent
 
 
-### TransformationAgent
-
-
-ta_m = importlib.import_module( 'DIRAC.TransformationSystem.Agent.TransformationAgent' )
+ta_m = importlib.import_module('DIRAC.TransformationSystem.Agent.TransformationAgent')
 ta_m.AgentModule = mockAM
 ta = TransformationAgent()
 ta.log = gLogger
 ta.am_getOption = mockAM
 
-goodFiles = {'OK':True,
-             'Value':[{'ErrorCount': 1L,
-                       'FileID': 17990660L,
-                       'InsertedTime': datetime.datetime( 2012, 3, 15, 17, 5, 50 ),
-                       'LFN': '/00012574_00000239_1.charmcompleteevent.dst',
-                       'LastUpdate': datetime.datetime( 2012, 3, 16, 23, 43, 26 ),
-                       'RunNumber': 90269L,
-                       'Status': 'Unused',
-                       'TargetSE': 'Unknown',
-                       'TaskID': '222',
-                       'TransformationID': 17042L,
-                       'UsedSE': 'CERN-DST,IN2P3-DST,PIC-DST,RAL-DST'},
-                      {'ErrorCount': 1L,
-                       'FileID': 17022945L,
-                       'InsertedTime': datetime.datetime( 2012, 3, 15, 17, 5, 50 ),
-                       'LFN': '/00012574_00000119_1.charmcompleteevent.dst',
-                       'LastUpdate': datetime.datetime( 2012, 3, 16, 23, 54, 59 ),
-                       'RunNumber': 90322L,
-                       'Status': 'Unused',
-                       'TargetSE': 'Unknown',
-                       'TaskID': '82',
-                       'TransformationID': 17042L,
-                       'UsedSE': 'CERN-DST,CNAF-DST,RAL-DST,SARA-DST'}]
-            }
-noFiles = {'OK':True, 'Value':[]}
+goodFiles = {'OK': True,
+             'Value': [{'ErrorCount': 1,
+                        'FileID': 17990660,
+                        'InsertedTime': datetime.datetime(2012, 3, 15, 17, 5, 50),
+                        'LFN': '/00012574_00000239_1.charmcompleteevent.dst',
+                        'LastUpdate': datetime.datetime(2012, 3, 16, 23, 43, 26),
+                        'RunNumber': 90269,
+                        'Status': 'Unused',
+                        'TargetSE': 'Unknown',
+                        'TaskID': '222',
+                        'TransformationID': 17042,
+                        'UsedSE': 'CERN-DST,IN2P3-DST,PIC-DST,RAL-DST'},
+                       {'ErrorCount': 1,
+                        'FileID': 17022945,
+                        'InsertedTime': datetime.datetime(2012, 3, 15, 17, 5, 50),
+                        'LFN': '/00012574_00000119_1.charmcompleteevent.dst',
+                        'LastUpdate': datetime.datetime(2012, 3, 16, 23, 54, 59),
+                        'RunNumber': 90322,
+                        'Status': 'Unused',
+                        'TargetSE': 'Unknown',
+                        'TaskID': '82',
+                        'TransformationID': 17042,
+                        'UsedSE': 'CERN-DST,CNAF-DST,RAL-DST,SARA-DST'}]
+             }
+noFiles = {'OK': True, 'Value': []}
 
-@pytest.mark.parametrize("transDict, getTFiles, expected",[
-    ({'TransformationID': 123, 'Status': 'Stopped', 'Type': 'Replication' }, goodFiles, True),
-    ({'TransformationID': 123, 'Status': 'Stopped', 'Type': 'Removal' }, noFiles, True)
-    ])
+
+@pytest.mark.parametrize("transDict, getTFiles, expected", [
+    ({'TransformationID': 123, 'Status': 'Stopped', 'Type': 'Replication'}, goodFiles, True),
+    ({'TransformationID': 123, 'Status': 'Stopped', 'Type': 'Removal'}, noFiles, True)
+])
 def test__getTransformationFiles(transDict, getTFiles, expected):
   tc_mock.getTransformationFiles.return_value = getTFiles
-  res = ta._getTransformationFiles( transDict, {'TransformationClient': tc_mock} )
+  res = ta._getTransformationFiles(transDict, {'TransformationClient': tc_mock})
   assert res['OK'] == expected
