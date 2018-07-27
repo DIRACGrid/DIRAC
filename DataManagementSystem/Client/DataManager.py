fca3721a6f65cb1c4008b1ebb50d1dc73020e8c2
Federico Stagni <federico.stagni@cern.ch>
2018-07-27 10:31:54 +0200

    using cmpError

--- DataManagementSystem/Client/DataManager.py
+++ DataManagementSystem/Client/DataManager.py
@@ -184,7 +184,7 @@ class DataManager(object):
                 "%s %s" % (lfn, reason))
     res = returnSingleResult(self.removeFile(['%s/dirac_directory' % folder]))
     if not res['OK']:
-      if "No such file" not in res['Message']:
+      if not DErrno.cmpError(res, errno.ENOENT):
         log.warn('Failed to delete dirac_directory placeholder file')
 
     storageElements = gConfig.getValue(
