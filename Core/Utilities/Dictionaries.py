8bd02d57c04a4ba5d90d1198a7a280f944554d28
Federico Stagni <federico.stagni@cern.ch>
2018-08-09 13:10:23 +0200

    added utility for breaking dictionaries into chunks

---
+++ Core/Utilities/Dictionaries.py
@@ -0,0 +1,13 @@
+""" Collecting utilties for dictionaries
+"""
+
+from itertools import islice
+
+def breakDictionaryIntoChunks(aDict, chunkSize):
+  """ This function is a generator of chunks of dictionaries of size chunkSize
+  """
+  if chunkSize < 1:
+    raise RuntimeError("chunkSize cannot be less than 1")
+  iterDict = iter(aDict)
+  for _ in xrange(0, len(aDict), chunkSize):
+    yield  {k:aDict[k] for k in islice(iterDict, chunkSize)}
---
+++ Core/Utilities/test/Test_Dictionaries.py
@@ -0,0 +1,19 @@
+""" pytest of Core.Utilities.Dictionaries
+"""
+
+import pytest
+
+# sut
+from DIRAC.Core.Utilities.Dictionaries import breakDictionaryIntoChunks
+
+
+@pytest.mark.parametrize("aDict, chunkSize, expected", [
+    ({}, 1, []),
+    ({'a':'aa', 'b':'bb', 'c':'cc', 'd':'dd'}, 5, [{'a':'aa', 'b':'bb', 'c':'cc', 'd':'dd'}]),
+    ({'a':'aa', 'b':'bb', 'c':'cc', 'd':'dd'}, 2, [{'a':'aa', 'c':'cc'}, {'b':'bb', 'd':'dd'}]),
+    ({'a':'aa', 'b':'bb', 'c':'cc', 'd':'dd'}, 1, [{'a':'aa'}, {'c':'cc'}, {'b':'bb'}, {'d':'dd'}]),
+    ({'a':'aa', 'b':'bb', 'c':'cc', 'd':'dd'}, 3, [{'a':'aa', 'b':'bb', 'c':'cc'}, {'d':'dd'}]),
+    ])
+def test_breakDictionaryIntoChunks_normal(aDict, chunkSize, expected):
+  """ breakDictIntoChunks tests"""
+  assert list(breakDictionaryIntoChunks(aDict, chunkSize)) == expected
