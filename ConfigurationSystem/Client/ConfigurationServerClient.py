fe2f8df3741f9fb37db24e03d98ff2d382a2a02f
louisjdmartin <louis.jd.martin@gmail.com>
2018-08-16 15:10:44 +0200

    Core: use url in Base.Client constructor CS: add specific client and use it in CSAPI

---
+++ ConfigurationSystem/Client/ConfigurationServerClient.py
@@ -0,0 +1,13 @@
+"""
+  Custom Client for Configuration System
+"""
+
+from DIRAC.Core.Base.Client import Client
+
+
+class ConfigurationServerClient(Client):
+  """
+    Placeholder client to speak with ConfigurationServer.
+  """
+
+  pass
