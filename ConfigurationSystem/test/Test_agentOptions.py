a0063a07c69b0f3a6ed2b8c24c06e793aa5ad536
Andre Sailer <andre.philippe.sailer@cern.ch>
2018-08-17 14:32:34 +0200

    Test: add tests for options of all agents.

---
+++ ConfigurationSystem/test/Test_agentOptions.py
@@ -0,0 +1,135 @@
+"""Check options for all agents."""
+
+import inspect
+import importlib
+import logging
+import pytest
+
+from mock import patch, MagicMock as Mock
+
+from DIRAC.tests.Utilities.assertingUtils import checkAgentOptions
+
+
+AGENTS = [('DIRAC.ConfigurationSystem.Agent.Bdii2CSAgent', ['BannedCEs', 'BannedSEs', 'DryRun', 'AlternativeBDIIs',
+                                                            'VO']),
+          ('DIRAC.ConfigurationSystem.Agent.VOMS2CSAgent', ['mailFrom', 'DryRun', 'VO']),
+          ('DIRAC.ConfigurationSystem.Agent.GOCDB2CSAgent', ['Cycles', 'DryRun']),
+          ('DIRAC.RequestManagementSystem.Agent.CleanReqDBAgent', ['KickLimit', 'KickGraceHours', 'DeleteGraceDays']),
+          ('DIRAC.RequestManagementSystem.Agent.RequestExecutingAgent', ['MaxProcess', 'ProcessTaskTimeout',
+                                                                         'RequestsPerCycle', 'OperationHandlers',
+                                                                         'MinProcess', 'MaxAttempts',
+                                                                         'ProcessPoolQueueSize',
+                                                                         'ProcessPoolSleep',
+                                                                         'FTSMode', 'OperationHandlers']),
+          ('DIRAC.FrameworkSystem.Agent.CAUpdateAgent', []),
+          ('DIRAC.FrameworkSystem.Agent.MyProxyRenewalAgent', ['MinValidity', 'ValidityPeriod',
+                                                               'MinimumLifeTime',
+                                                               'RenewedLifeTime']),
+          ('DIRAC.StorageManagementSystem.Agent.RequestPreparationAgent', []),
+          ('DIRAC.StorageManagementSystem.Agent.RequestFinalizationAgent', []),
+          ('DIRAC.StorageManagementSystem.Agent.StageMonitorAgent', []),
+          ('DIRAC.StorageManagementSystem.Agent.StageRequestAgent', ['PinLifetime']),
+          ('DIRAC.AccountingSystem.Agent.NetworkAgent', ['MaxCycles', 'MessageQueueURI', 'BufferTimeout']),
+          ('DIRAC.WorkloadManagementSystem.Agent.JobCleaningAgent', ['ProductionTypes', 'MaxJobsAtOnce', 'JobByJob',
+                                                                     'ThrottlingPeriod']),
+          ('DIRAC.WorkloadManagementSystem.Agent.JobAgent', ['FillingModeFlag', 'JobWrapperTemplate',
+                                                             'MinimumTimeLeft']),
+          ('DIRAC.WorkloadManagementSystem.Agent.StatesAccountingAgent', []),
+          ('DIRAC.WorkloadManagementSystem.Agent.StalledJobAgent', ['StalledTimeHours', 'FailedTimeHours',
+                                                                    'StalledJobsTolerantSites', 'Enable']),
+          ('DIRAC.WorkloadManagementSystem.Agent.PilotStatusAgent', ['PilotAccountingEnabled', 'ClearPilotsDelay',
+                                                                     'ClearAbortedPilotsDelay']),
+          ('DIRAC.WorkloadManagementSystem.Agent.StatesMonitoringAgent', []),
+          ('DIRAC.ResourceStatusSystem.Agent.SummarizeLogsAgent', []),
+          ('DIRAC.ResourceStatusSystem.Agent.ElementInspectorAgent', ['elementType', 'maxNumberOfThreads',
+                                                                      'limitQueueFeeder']),
+          ('DIRAC.ResourceStatusSystem.Agent.EmailAgent', ['Status']),
+          ('DIRAC.ResourceStatusSystem.Agent.TokenAgent', ['notifyHours', 'adminMail']),
+          ('DIRAC.ResourceStatusSystem.Agent.CacheFeederAgent', []),
+          ('DIRAC.ResourceStatusSystem.Agent.SiteInspectorAgent', ['elementType', 'maxNumberOfThreads',
+                                                                   'limitQueueFeeder']),
+          ('DIRAC.DataManagementSystem.Agent.FTSAgent', ['StageFiles', 'UseProxies', 'shifterProxy',
+                                                         'FTSPlacementValidityPeriod', 'SubmitCommand',
+                                                         'MonitorCommand', 'PinTime', 'MaxActiveJobsPerRoute',
+                                                         'MaxRequests', 'MonitoringInterval', 'ProcessJobRequests']),
+          ('DIRAC.DataManagementSystem.Agent.CleanFTSDBAgent', ['DeleteGraceDays']),
+          ('DIRAC.DataManagementSystem.Agent.FTS3Agent', []),
+          ('DIRAC.TransformationSystem.Agent.InputDataAgent', ['DateKey', 'TransformationTypes']),
+          # ('DIRAC.TransformationSystem.Agent.WorkflowTaskAgent', []),  # not inheriting from AgentModule
+          # ('DIRAC.TransformationSystem.Agent.RequestTaskAgent', []),  # not inheriting from AgentModule
+          ('DIRAC.TransformationSystem.Agent.TaskManagerAgentBase', ['PluginLocation', 'BulkSubmission', 'shifterProxy',
+                                                                     'ShifterCredentials', 'maxNumberOfThreads']),
+          ('DIRAC.TransformationSystem.Agent.MCExtensionAgent', ['TransformationTypes', 'TasksPerIteration',
+                                                                 'MaxFailureRate', 'MaxWaitingJobs']),
+          ('DIRAC.TransformationSystem.Agent.TransformationCleaningAgent', ['EnableFlag', 'shifterProxy']),
+          ('DIRAC.TransformationSystem.Agent.ValidateOutputDataAgent', ['TransformationTypes', 'DirectoryLocations',
+                                                                        'ActiveSEs', 'TransfIDMeta']),
+          ('DIRAC.TransformationSystem.Agent.TransformationAgent', ['PluginLocation', 'transformationStatus',
+                                                                    'MaxFiles', 'MaxFilesToProcess',
+                                                                    'TransformationTypes', 'ReplicaCacheValidity',
+                                                                    'NoUnusedDelay', 'maxThreadsInPool']),
+          ]
+
+LOG = logging.getLogger('Test')
+
+
+@pytest.mark.parametrize('agentPath, ignoreOptions', AGENTS)
+def test_AgentOptions(caplog, agentPath, ignoreOptions):
+  """Check that all options in ConfigTemplate are found in the initialize method, including default values."""
+  caplog.set_level(logging.DEBUG)
+
+  agentPathSplit = agentPath.split('.')
+  systemName = agentPathSplit[1]
+  agentName = agentPathSplit[-1]
+
+  agentModule = importlib.import_module(agentPath)
+  LOG.info("Agents: %s %s", agentPath, agentModule)
+  agentClass = None
+
+  # mock everything but the agentClass
+  for name, member in inspect.getmembers(agentModule):
+    LOG.info("Mocking? %s, %s, %s, isclass(%s)", name, callable(member), type(member), inspect.isclass(member))
+    if name != 'AgentModule' and '_AgentModule__executeModuleCycle' in dir(member):
+      LOG.info("Found the agent class %s, %s", name, member)
+      agentClass = member
+      continue
+    elif name == 'AgentModule':
+      continue
+    if callable(member) or inspect.ismodule(member):
+      LOG.info("Mocking: %s, %s, %s", name, member, type(member))
+      agentModule.__dict__[name] = Mock(name=name)
+
+  agentModule.__dict__['gConfig'] = Mock()
+  agentModule.__dict__['gConfig'].getSections.return_value = dict(OK=True, Value=[])
+
+  def returnDefault(*args):
+    LOG.debug("ReturningDefault: %s, %s", args, type(args[1]))
+    return args[1]
+
+  getOptionMock = Mock(name="am_getOption", side_effect=returnDefault)
+
+  def instrument(*args, **kwargs):
+    """Mock some functions that come from the AgentModule and are not present otherwise."""
+    args[0].am_getOption = getOptionMock
+    args[0].log = Mock()
+    args[0].am_getModuleParam = Mock()
+    args[0].am_setOption = Mock()
+    args[0].am_getWorkDirectory = Mock()
+    args[0].am_getControlDirectory = Mock()
+    return None
+  initMock = Mock(side_effect=instrument)
+
+  class MockAgentModule(object):
+    def __init__(self, *args, **kwargs):
+      instrument(self)
+
+  patchBase = patch.object(agentClass, '__bases__', (MockAgentModule,))
+  with \
+          patchBase, \
+          patch(agentPath + ".AgentModule.__init__", new=initMock), \
+          patch("DIRAC.Core.Base.AgentModule.AgentModule.am_getOption", new=getOptionMock):
+    patchBase.is_local = True
+    agentInstance = agentClass(agentName="sys/name", loadName="sys/name")
+    instrument(agentInstance)
+  agentInstance.initialize()
+  checkAgentOptions(getOptionMock, systemName, agentName, ignoreOptions=ignoreOptions)
--- tests/Utilities/assertingUtils.py
+++ tests/Utilities/assertingUtils.py
@@ -12,6 +12,31 @@ from DIRAC.Core.Utilities.CFG import CFG
 LOG = logging.getLogger(__name__)
 
 
+def _parseOption(outDict, inDict, optionPrefix=''):
+  """Parse the ConfigTemplates options.
+
+  Handle some special cases.
+  """
+  LOG.info("Parsing into %s, from %s, prefix %r", outDict, inDict, optionPrefix)
+  for option, value in inDict.iteritems():
+    optionName = "/".join([optionPrefix, option]).strip('/')
+    LOG.info("Parsing %r with %r", optionName, value)
+    if isinstance(value, basestring) and value.lower() in ('no', 'false'):
+      outDict[optionName] = False
+    elif isinstance(value, basestring) and value.lower() in ('yes', 'true'):
+      outDict[optionName] = True
+    elif isinstance(value, basestring) and ',' in value:
+      outDict[optionName] = [val.strip() for val in value.split(',')]
+    elif isinstance(value, dict):
+      _parseOption(outDict, value, optionPrefix=optionName)
+    elif isinstance(value, basestring):
+      outDict[optionName] = value
+      try:
+        outDict[optionName] = int(value)
+      except ValueError:
+        pass
+
+
 def checkAgentOptions(getOptionMock, systemName, agentName,
                       ignoreOptions=None, extension='DIRAC'):
   """Ensure that all the agent options are properly documented.
@@ -25,31 +50,48 @@ def checkAgentOptions(getOptionMock, systemName, agentName,
   if ignoreOptions is None:
     ignoreOptions = []
 
+  # add some options that can be set, see the AgentModule for all of them
+  ignoreOptions.extend(['PollingTime', 'Status', 'Enabled', 'MaxCycles', 'LogOutputs', 'ControlDirectory'])
+  ignoreOptions = list(set(ignoreOptions))
   config = CFG()
+
+  LOG.info("Testing %s/%s, ignoring options %s", systemName, agentName, ignoreOptions)
+
   # get the location where DIRAC is in from basefolder/DIRAC/__ini__.py
   configFilePath = os.path.join(os.path.dirname(os.path.dirname(DIRAC.__file__)),
                                 extension, systemName, 'ConfigTemplate.cfg')
   config.loadFromFile(configFilePath)
   optionsDict = config.getAsDict('Agents/%s' % agentName)
-  for option, value in optionsDict.iteritems():
-    if isinstance(value, basestring) and value.lower() in ('no', 'false'):
-      optionsDict[option] = False
-    if isinstance(value, basestring) and value.lower() in ('yes', 'true'):
-      optionsDict[option] = True
-    if isinstance(value, basestring) and ',' in value:
-      optionsDict[option] = [val.strip() for val in value.split(',')]
+  outDict = {}
+  _parseOption(outDict, optionsDict)
+  optionsDict = outDict
+  LOG.info("Calls: %s", pformat(getOptionMock.call_args_list))
+  LOG.info("Options found in ConfigTemplate: %s ", list(optionsDict.keys()))
 
-    try:
-      optionsDict[option] = int(value)
-    except ValueError:
-      pass
-
-  LOG.info("Calls: %s", pformat(getOptionMock.mock_calls))
+  # check that values in ConfigTemplate are used
   for option, value in optionsDict.iteritems():
-    LOG.info("Checking option %s with value %r", option, value)
-    if option in ignoreOptions:
+    if any(ignoreOp in option for ignoreOp in ignoreOptions):
+      LOG.info("From Agent: ignoring option %r with value %r, (%s)", option, value, type(value))
       continue
+    LOG.info("Looking for call to option %r with value %r, (%s)", option, value, type(value))
     if not isinstance(value, bool) and not value:  # empty string, list, dict ...
-      assert any(call(option, null) in getOptionMock.mock_calls for null in ({}, set(), [], ''))
+      assert any(call(option, null) in getOptionMock.call_args_list for null in ({}, set(), [], '', 0))
     else:
-      assert call(option, value) in getOptionMock.mock_calls
+      assert call(option, value) in getOptionMock.call_args_list
+
+  # check that options used in the agent are in the ConfigTemplates
+  for opCall in getOptionMock.call_args_list:
+    optionArguments = opCall[0]
+    if len(optionArguments) != 2:
+      continue
+    optionName = optionArguments[0]
+    optionValue = optionArguments[1]
+    if optionName in ignoreOptions:
+      LOG.info("From Template: ignoring option %r with %r", optionName, optionValue)
+      continue
+    LOG.info("Checking Template option %r with %r", optionName, optionValue)
+    assert optionName in optionsDict
+    if not optionsDict[optionName]:
+      assert not optionValue
+      continue
+    assert optionsDict[optionName] == optionValue
