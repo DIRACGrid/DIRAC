92eef77fe97e7c7a03e016625a56f6c362cc0fc9
arrabito <arrabito@in2p3.fr>
2018-08-23 14:45:14 +0200

    conflict resolved

--- TransformationSystem/DB/TransformationDB.py
+++ TransformationSystem/DB/TransformationDB.py
@@ -406,7 +406,7 @@ class TransformationDB(DB):
     """ Get filters for all defined input streams in all the transformations.
     """
     resultList = []
-    req = "SELECT TransformationID,InputMetaQuery FROM Transformations;"
+    req = "SELECT TransformationID,InputMetaQuery FROM Transformations where Status in ('New','Active','Stopped','Flush','Completing');"
     res = self._query( req, connection )
     if not res['OK']:
       return res
@@ -1730,13 +1730,13 @@ class TransformationDB(DB):
     typeDict.update(res['Value']['DirectoryMetaFields'])
 
     for transID, query in queries:
-      gLogger.info("Check the transformation status")
-      res = self.getTransformationParameters(transID, 'Status')
-      if res['Value'] not in ['New', 'Active', 'Stopped', 'Completing', 'Flush']:
+      gLogger.info( "Check the transformation status" )
+      res = self.getTransformationParameters( transID, 'Status' )
+      if res['Value'] not in ['New','Active','Stopped','Completing','Flush']:
         continue
-      mq = MetaQuery(query, typeDict)
-      gLogger.info("Apply query %s to metadata %s" % (mq.getMetaQuery(), metadatadict))
-      res = mq.applyQuery(metadatadict)
+      mq = MetaQuery( query, typeDict )
+      gLogger.info( "Apply query %s to metadata %s" % ( mq.getMetaQuery(), metadatadict ) )
+      res = mq.applyQuery( metadatadict )
       if not res['OK']:
         gLogger.error("Error in applying query: %s" % res['Message'])
         return res
