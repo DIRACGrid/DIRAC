cdfeb241a4bded32382daac4e83b8bff223228eb
Andre Sailer <andre.philippe.sailer@cern.ch>
2018-09-25 11:46:37 +0200

    Tests: add test for Dirac.runLocal input sandbox resolution

--- Interfaces/API/test/Test_DIRAC.py
+++ Interfaces/API/test/Test_DIRAC.py
@@ -32,13 +32,16 @@ def dirac():
 def job():
   from DIRAC.Interfaces.API.Job import Job
   job = Job(stdout='printer', stderr='/dev/null')
+  job.setInputSandbox(['LFN:/vo/user/i/initial/important.tar.gz',
+                       '/abspath/absfile.xml',
+                       'file_in_pwd.xml',
+                       ])
   return job
 
 
 @pytest.fixture
 def osmock():
   os = MagicMock(return_value=False, name="OS")
-  os.getcwd.return_value = '/root'
   os.environ = dict()
 
   def expandMock(*args, **kwargs):
@@ -46,7 +49,29 @@ def osmock():
        'DIRACROOT' in args[0]:
       return args[0].replace('$DIRACROOT', os.environ['DIRACROOT'])
     return args[0]
+
+  def existsMock(*args, **kwargs):
+    print "exists", args
+    if any(f in args[0] for f in ('jobDescription', '/pwd/file_in_pwd.xml')):
+      return True
+    if 'absfile' in args[0] and args[0].startswith('/abspath'):
+      return True
+    return False
+
+  def joinMock(*args):
+    path = '/'
+    for l in args:
+      path += l.strip('/') + '/'
+    return path.rstrip('/')
+
+  os.getcwd.side_effect = ['/pwd/'] + ['/pwd/tempFolder/'] * 100
+
   os.path.expandvars.side_effect = expandMock
+  os.path.exists.side_effect = existsMock
+  os.path.isabs.side_effect = lambda x: x.startswith('/')
+  os.path.isdir.return_value = False
+  os.path.join.side_effect = joinMock
+  os.path.basename.side_effect = lambda x: x.rsplit('/', 1)[-1]
   return os
 
 
@@ -83,13 +108,15 @@ def test_JobJob(dirac, job):
 
 def test_runLocal(dirac, job, mocker, osmock, confMock):
   mocker.patch('DIRAC.Interfaces.API.Dirac.os', new=osmock)
-  mocker.patch('DIRAC.Interfaces.API.Dirac.tempfile')
-  mocker.patch('DIRAC.Interfaces.API.Dirac.shutil')
-  mocker.patch('DIRAC.Interfaces.API.Dirac.gConfig', new=confMock)
-  sysMock = mocker.patch('DIRAC.Interfaces.API.Dirac.systemCall')
-  sysMock.return_value = S_OK([0, 'No output', 'No errors'])
   mocker.patch('DIRAC.Interfaces.API.Dirac.tarfile', new=MagicMock(return_value=False))
   mocker.patch('__builtin__.open')
+  mocker.patch('DIRAC.Interfaces.API.Dirac.gConfig', new=confMock)
+  tempMock = mocker.patch('DIRAC.Interfaces.API.Dirac.tempfile')
+  tempMock.mkdtemp.return_value = 'tempFolder'
+  shMock = mocker.patch('DIRAC.Interfaces.API.Dirac.shutil', new=MagicMock(name="Shutil"))
+  sysMock = mocker.patch('DIRAC.Interfaces.API.Dirac.systemCall')
+  sysMock.return_value = S_OK([0, 'No output', 'No errors'])
+  dirac.getFile = MagicMock(return_value=S_OK())
   ret = dirac.runLocal(job)
   LOG.info("dirac log calls: %s", dirac.log.call_args_list)
   LOG.info("CallStack: %s", pformat(ret.get('CallStack', {})))
@@ -97,3 +124,6 @@ def test_runLocal(dirac, job, mocker, osmock, confMock):
                                                     'jobDescription.xml', '-o', 'LogLevel=info']
   assert ret.get('Message', None) is None
   assert ret['OK']
+  assert call('/abspath/absfile.xml', '/pwd/tempFolder/') in shMock.copy.call_args_list
+  assert call('/pwd/file_in_pwd.xml', '/pwd/tempFolder/') in shMock.copy.call_args_list
+  assert call('/vo/user/i/initial/important.tar.gz') in dirac.getFile.call_args_list
