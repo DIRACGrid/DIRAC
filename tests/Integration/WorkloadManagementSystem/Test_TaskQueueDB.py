a7194caffdca2e0f90167f73b8a75ce14859dd18
Federico Stagni <federico.stagni@cern.ch>
2018-10-04 16:34:40 +0200

    moved to pytest

--- tests/Integration/WorkloadManagementSystem/Test_TaskQueueDB.py
+++ tests/Integration/WorkloadManagementSystem/Test_TaskQueueDB.py
@@ -1,9 +1,12 @@
 """ This integration test only need the TaskQueueDB
     (which should of course be properly defined also in the configuration),
     and connects directly to it
-"""
 
-import unittest
+
+    Run this test with::
+
+        "python -m pytest tests/Integration/WorkloadManagementSystem/Test_TaskQueueDB.py"
+"""
 
 from DIRAC.Core.Base.Script import parseCommandLine
 parseCommandLine()
@@ -12,141 +15,122 @@ from DIRAC import gLogger
 from DIRAC.WorkloadManagementSystem.DB.TaskQueueDB import TaskQueueDB
 
 
-class TQDBTestCase(unittest.TestCase):
-  """ Base class for the JobDB test cases
+gLogger.setLevel('DEBUG')
+tqDB = TaskQueueDB()
+
+
+def test_basicChain():
+  """ a basic put - remove
   """
+  tqDefDict = {'OwnerDN': '/my/DN', 'OwnerGroup': 'myGroup', 'Setup': 'aSetup', 'CPUTime': 50000}
+  result = tqDB.insertJob(123, tqDefDict, 10)
+  assert result['OK'] is True
+  result = tqDB.getTaskQueueForJobs([123])
+  assert result['OK'] is True
+  assert 123 in result['Value']
+  tq = result['Value'][123]
+  result = tqDB.deleteJob(123)
+  assert result['OK'] is True
+  result = tqDB.cleanOrphanedTaskQueues()
+  assert result['OK'] is True
+  result = tqDB.deleteTaskQueueIfEmpty(tq)
+  assert result['OK'] is True
 
-  def setUp(self):
-    gLogger.setLevel('DEBUG')
-    self.tqDB = TaskQueueDB()
 
-  def tearDown(self):
-    pass
-
-
-class TQChain(TQDBTestCase):
-  """ chaining some commands
+def test_chainWithParameter():
+  """ put - remove with parameters
   """
+  tqDefDict = {'OwnerDN': '/my/DN', 'OwnerGroup': 'myGroup', 'Setup': 'aSetup', 'CPUTime': 50000}
 
-  def test_basicChain(self):
-    """ a basic put - remove
-    """
-    tqDefDict = {'OwnerDN': '/my/DN', 'OwnerGroup': 'myGroup', 'Setup': 'aSetup', 'CPUTime': 50000}
-    result = self.tqDB.insertJob(123, tqDefDict, 10)
-    self.assertTrue(result['OK'])
-    result = self.tqDB.getTaskQueueForJobs([123])
-    self.assertTrue(result['OK'])
-    self.assertTrue(123 in result['Value'].keys())
-    tq = result['Value'][123]
-    result = self.tqDB.deleteJob(123)
-    self.assertTrue(result['OK'])
-    result = self.tqDB.cleanOrphanedTaskQueues()
-    self.assertTrue(result['OK'])
-    result = self.tqDB.deleteTaskQueueIfEmpty(tq)
-    self.assertTrue(result['OK'])
+  # first job
+  result = tqDB.insertJob(123, tqDefDict, 10)
+  assert result['OK'] is True
+  result = tqDB.getTaskQueueForJobs([123])
+  assert result['OK'] is True
+  tq = result['Value'][123]
+  result = tqDB.deleteTaskQueue(tq)
+  assert result['OK'] is False  # This will fail because of the foreign key
+  result = tqDB.cleanOrphanedTaskQueues()
+  assert result['OK'] is True
+  result = tqDB.deleteTaskQueueIfEmpty(tq)  # this won't delete anything
+  assert result['OK'] is True
 
-  def test_chainWithParameter(self):
-    """ put - remove with parameters
-    """
-    tqDefDict = {'OwnerDN': '/my/DN', 'OwnerGroup': 'myGroup', 'Setup': 'aSetup', 'CPUTime': 50000}
+  # second job
+  result = tqDB.insertJob(125, tqDefDict, 10)
+  assert result['OK'] is True
+  result = tqDB.getTaskQueueForJobs([125])
+  tq = result['Value'][125]
+  result = tqDB.deleteTaskQueue(tq)
+  assert result['OK'] is False  # This will fail because of the foreign key
+  result = tqDB.deleteTaskQueueIfEmpty(tq)  # this won't delete anything, as both 123 and 125 are in
+  assert result['OK'] is True  # but still it won't fail
+  assert result['Value'] is False
+  result = tqDB.retrieveTaskQueues()
+  assert result['OK'] is True
+  assert result['Value'].values()[0] == {'OwnerDN': '/my/DN', 'Jobs': 2, 'OwnerGroup': 'myGroup',
+                                         'Setup': 'aSetup', 'CPUTime': 86400, 'Priority': 1.0}
 
-    # first job
-    result = self.tqDB.insertJob(123, tqDefDict, 10)
-    self.assertTrue(result['OK'])
-    result = self.tqDB.getTaskQueueForJobs([123])
-    self.assertTrue(result['OK'])
-    tq = result['Value'][123]
-    result = self.tqDB.deleteTaskQueue(tq)
-    self.assertFalse(result['OK'])  # This will fail because of the foreign key
-    result = self.tqDB.cleanOrphanedTaskQueues()
-    self.assertTrue(result['OK'])
-    result = self.tqDB.deleteTaskQueueIfEmpty(tq)  # this won't delete anything
-    self.assertTrue(result['OK'])
-
-    # second job
-    result = self.tqDB.insertJob(125, tqDefDict, 10)
-    self.assertTrue(result['OK'])
-    result = self.tqDB.getTaskQueueForJobs([125])
-    tq = result['Value'][125]
-    result = self.tqDB.deleteTaskQueue(tq)
-    self.assertFalse(result['OK'])  # This will fail because of the foreign key
-    result = self.tqDB.deleteTaskQueueIfEmpty(tq)  # this won't delete anything, as both 123 and 125 are in
-    self.assertTrue(result['OK'])  # but still it won't fail
-    self.assertFalse(result['Value'])
-    result = self.tqDB.retrieveTaskQueues()
-    self.assertTrue(result['OK'])
-    self.assertEqual(result['Value'].values()[0],
-                     {'OwnerDN': '/my/DN', 'Jobs': 2, 'OwnerGroup': 'myGroup',
-                      'Setup': 'aSetup', 'CPUTime': 86400, 'Priority': 1.0})
-
-    # now we will try to delete
-    result = self.tqDB.deleteJob(123)
-    self.assertTrue(result['OK'])
-    result = self.tqDB.deleteJob(125)
-    self.assertTrue(result['OK'])
-    result = self.tqDB.deleteTaskQueueIfEmpty(tq)  # this should now delete tq
-    self.assertTrue(result['OK'])
-    result = self.tqDB.retrieveTaskQueues()
-    self.assertTrue(result['OK'])
-    self.assertEqual(result['Value'], {})
-
-  def test_chainWithParametersComplex(self):
-    """ put - remove with parameters
-    """
-    tqDefDict = {'OwnerDN': '/my/DN', 'OwnerGroup': 'myGroup', 'Setup': 'aSetup', 'CPUTime': 50000,
-                 'BannedSites': ['LCG.CERN.ch', 'CLOUD.IN2P3.fr']}
-    result = self.tqDB.insertJob(127, tqDefDict, 10)
-    self.assertTrue(result['OK'])
-    result = self.tqDB.getTaskQueueForJobs([127])
-    tq = result['Value'][127]
-    result = self.tqDB.deleteTaskQueueIfEmpty(tq)  # this won't delete anything, as 127 is in
-    self.assertTrue(result['OK'])  # but still it won't fail
-    self.assertFalse(result['Value'])
-    result = self.tqDB.deleteJob(127)
-    self.assertTrue(result['OK'])
-    result = self.tqDB.deleteTaskQueueIfEmpty(tq)  # this should now delete tq
-    self.assertTrue(result['OK'])
-    result = self.tqDB.retrieveTaskQueues()
-    self.assertTrue(result['OK'])
-    self.assertEqual(result['Value'], {})
+  # now we will try to delete
+  result = tqDB.deleteJob(123)
+  assert result['OK'] is True
+  result = tqDB.deleteJob(125)
+  assert result['OK'] is True
+  result = tqDB.deleteTaskQueueIfEmpty(tq)  # this should now delete tq
+  assert result['OK'] is True
+  result = tqDB.retrieveTaskQueues()
+  assert result['OK'] is True
+  assert result['Value'] == {}
 
 
-class TQTests(TQDBTestCase):
-  """ Various other tests
+def test_chainWithParametersComplex():
+  """ put - remove with parameters
   """
-
-  def test_TQ(self):
-    """ test of various functions
-    """
-    tqDefDict = {'OwnerDN': '/my/DN', 'OwnerGroup': 'myGroup', 'Setup': 'aSetup', 'CPUTime': 50000}
-    self.tqDB.insertJob(123, tqDefDict, 10)
-
-    result = self.tqDB.getNumTaskQueues()
-    self.assertTrue(result['OK'])
-    self.assertEqual(result['Value'], 1)
-    result = self.tqDB.retrieveTaskQueues()
-    self.assertTrue(result['OK'])
-    self.assertEqual(result['Value'].values()[0],
-                     {'OwnerDN': '/my/DN', 'Jobs': 1, 'OwnerGroup': 'myGroup',
-                      'Setup': 'aSetup', 'CPUTime': 86400, 'Priority': 1.0})
-    result = self.tqDB.findOrphanJobs()
-    self.assertTrue(result['OK'])
-    result = self.tqDB.recalculateTQSharesForAll()
-    self.assertTrue(result['OK'])
-
-    # this will also remove the job
-    result = self.tqDB.matchAndGetJob({'Setup': 'aSetup', 'CPUTime': 300000})
-    self.assertTrue(result['OK'])
-    self.assertTrue(result['Value']['matchFound'])
-    self.assertTrue(result['Value']['jobId'] in [123, 125])
-    tq = result['Value']['taskQueueId']
-
-    result = self.tqDB.deleteTaskQueueIfEmpty(tq)
-    self.assertTrue(result['OK'])
+  tqDefDict = {'OwnerDN': '/my/DN', 'OwnerGroup': 'myGroup', 'Setup': 'aSetup', 'CPUTime': 50000,
+               'BannedSites': ['LCG.CERN.ch', 'CLOUD.IN2P3.fr']}
+  result = tqDB.insertJob(127, tqDefDict, 10)
+  assert result['OK'] is True
+  result = tqDB.getTaskQueueForJobs([127])
+  tq = result['Value'][127]
+  result = tqDB.deleteTaskQueueIfEmpty(tq)  # this won't delete anything, as 127 is in
+  assert result['OK'] is True  # but still it won't fail
+  assert result['Value'] is False
+  result = tqDB.deleteJob(127)
+  assert result['OK'] is True
+  result = tqDB.deleteTaskQueueIfEmpty(tq)  # this should now delete tq
+  assert result['OK'] is True
+  result = tqDB.retrieveTaskQueues()
+  assert result['OK'] is True
+  assert result['Value'] == {}
 
 
-if __name__ == '__main__':
-  suite = unittest.defaultTestLoader.loadTestsFromTestCase(TQDBTestCase)
-  suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TQChain))
-  suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TQTests))
-  testResult = unittest.TextTestRunner(verbosity=2).run(suite)
+""" Various other tests
+"""
+
+
+def test_TQ():
+  """ test of various functions
+  """
+  tqDefDict = {'OwnerDN': '/my/DN', 'OwnerGroup': 'myGroup', 'Setup': 'aSetup', 'CPUTime': 50000}
+  tqDB.insertJob(123, tqDefDict, 10)
+
+  result = tqDB.getNumTaskQueues()
+  assert result['OK'] is True
+  assert result['Value'] == 1
+  result = tqDB.retrieveTaskQueues()
+  assert result['OK'] is True
+  assert result['Value'].values()[0] == {'OwnerDN': '/my/DN', 'Jobs': 1, 'OwnerGroup': 'myGroup',
+                                         'Setup': 'aSetup', 'CPUTime': 86400, 'Priority': 1.0}
+  result = tqDB.findOrphanJobs()
+  assert result['OK'] is True
+  result = tqDB.recalculateTQSharesForAll()
+  assert result['OK'] is True
+
+  # this will also remove the job
+  result = tqDB.matchAndGetJob({'Setup': 'aSetup', 'CPUTime': 300000})
+  assert result['OK'] is True
+  assert result['Value']['matchFound'] is True
+  assert result['Value']['jobId'] in [123, 125]
+  tq = result['Value']['taskQueueId']
+
+  result = tqDB.deleteTaskQueueIfEmpty(tq)
+  assert result['OK'] is True
