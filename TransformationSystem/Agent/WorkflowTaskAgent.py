754caa04336354aa186c24b916b10a6e8dbbaa9a
Federico Stagni <federico.stagni@cern.ch>
2018-10-23 17:02:18 +0200

    options for WorkflowTaskAgent in ConfigTemplate.cfg

--- TransformationSystem/Agent/WorkflowTaskAgent.py
+++ TransformationSystem/Agent/WorkflowTaskAgent.py
@@ -2,54 +2,14 @@
 
 The following options can be set for the WorkflowTaskAgent.
 
-::
+.. literalinclude:: ../ConfigTemplate.cfg
+  :start-after: ##BEGIN WorkflowTaskAgent
+  :end-before: ##END
+  :dedent: 2
+  :caption: WorkflowTaskAgent options
 
-  WorkflowTaskAgent
-  {
-    # Use a dedicated proxy to submit jobs to the WMS
-    shifterProxy = ProductionManager
-    # Use delegated credentials. Use this instead of the shifterProxy option (New in v6r20p5)
-    ShifterCredentials =
-    # Transformation types to be taken into account by the agent
-    TransType = MCSimulation,DataReconstruction,DataStripping,MCStripping,Merge
-    # Location of the transformation plugins
-    PluginLocation = DIRAC.TransformationSystem.Client.TaskManagerPlugin
-    # maximum number of threads to use in this agent
-    maxNumberOfThreads = 15
-
-    # Give this option a value if the agent should submit Requests
-    SubmitTasks = yes
-    # Status of transformations for which to submit Requests
-    SubmitStatus = Active,Completing
-    # Number of tasks to submit in one execution cycle per transformation
-    TasksPerLoop = 50
-
-    # Give this option a value if the agent should monitor tasks
-    MonitorTasks = yes
-    # Status of transformations for which to monitor tasks
-    UpdateTasksStatus = Active,Completing,Stopped
-    # Task statuses considered transient that should be monitored for updates
-    TaskUpdateStatus = Submitted,Received,Waiting,Running,Matched,Completed,Failed
-    # Number of tasks to be updated in one call
-    TaskUpdateChunkSize = 0
-
-    # Give this option a value if the agent should monitor files
-    MonitorFiles =
-    # Status of transformations for which to monitor Files
-    UpdateFilesStatus = Active,Completing,Stopped
-
-    # Give this option a value if the agent should check Reserved tasks
-    CheckReserved =
-    # Status of transformations for which to check reserved tasks
-    CheckReservedStatus = Active,Completing,Stopped
-
-    # Fill in this option if you want to activate bulk submission (for speed up)
-    BulkSubmission = yes
-
-  }
-
-* The options *SubmitTasks*, *MonitorTasks*, *MonitorFiles*, and *CheckReserved*
-  need to be assigned any non-empty value to be activated
+The options *SubmitTasks*, *MonitorTasks*, *MonitorFiles*, and *CheckReserved*
+need to be assigned any non-empty value to be activated
 
 * .. versionadded:: v6r20p5
 
