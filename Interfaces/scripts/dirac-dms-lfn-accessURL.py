89b9a9ec3fd1e13fae3a5e34a489a318c49cf9d2
Simon Fayer <sf105@ic.ac.uk>
2018-10-29 10:20:56 +0000

    Tidy up dirac-dms-lfn-accessURL to new standards

--- Interfaces/scripts/dirac-dms-lfn-accessURL.py
+++ Interfaces/scripts/dirac-dms-lfn-accessURL.py
@@ -11,22 +11,24 @@ __RCSID__ = "$Id$"
 import DIRAC
 from DIRAC.Core.Base import Script
 
-Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
-                                     'Usage:',
-                                     '  %s [option|cfgfile] ... LFN SE' % Script.scriptName,
-                                     'Arguments:',
-                                     '  LFN:      Logical File Name or file containing LFNs',
-                                     '  SE:       Valid DIRAC SE' ] ) )
-Script.parseCommandLine( ignoreErrors = True )
+Script.setUsageMessage('\n'.join([ __doc__.split('\n')[1],
+                                   'Usage:',
+                                   '  %s [option|cfgfile] ... LFN SE' % Script.scriptName,
+                                   'Arguments:',
+                                   '  LFN:      Logical File Name or file containing LFNs',
+                                   '  SE:       Valid DIRAC SE' ]))
+Script.parseCommandLine(ignoreErrors = True)
 args = Script.getPositionalArgs()
 
-if len( args ) < 2:
+# pylint: disable=wrong-import-position
+from DIRAC.Interfaces.API.Dirac import Dirac
+
+if len(args) < 2:
   Script.showHelp()
 
-if len( args ) > 2:
+if len(args) > 2:
   print 'Only one LFN SE pair will be considered'
 
-from DIRAC.Interfaces.API.Dirac                         import Dirac
 dirac = Dirac()
 exitCode = 0
 
@@ -34,16 +36,16 @@ lfn = args[0]
 seName = args[1]
 
 try:
-  f = open( lfn, 'r' )
+  f = open(lfn, 'r')
   lfns = f.read().splitlines()
   f.close()
-except:
+except IOError:
   lfns = [lfn]
 
 for lfn in lfns:
-  result = dirac.getAccessURL( lfn, seName, printOutput = True )
+  result = dirac.getAccessURL(lfn, seName, printOutput=True)
   if not result['OK']:
     print 'ERROR: ', result['Message']
     exitCode = 2
 
-DIRAC.exit( exitCode )
+DIRAC.exit(exitCode)
