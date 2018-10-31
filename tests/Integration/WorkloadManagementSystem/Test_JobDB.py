da4f2d6b3d00215010fec95309af0c691cb5c027
Federico Stagni <federico.stagni@cern.ch>
2018-10-31 15:40:30 +0100

    rewritten test as pytest

--- tests/Integration/WorkloadManagementSystem/Test_JobDB.py
+++ tests/Integration/WorkloadManagementSystem/Test_JobDB.py
@@ -1,12 +1,10 @@
 """ This tests only need the JobDB, and connects directly to it
+
+    Suggestion: for local testing, run this with::
+        python -m pytest -c ../pytest.ini  -vv tests/Integration/WorkloadManagementSystem/Test_JobDB.py
 """
 
 # pylint: disable=invalid-name,wrong-import-position
-
-import unittest
-
-from DIRAC.Core.Base.Script import parseCommandLine
-parseCommandLine()
 
 from DIRAC import gLogger
 from DIRAC.WorkloadManagementSystem.DB.JobDB import JobDB
@@ -42,73 +40,51 @@ jdl = """
 """
 
 
-class JobDBTestCase(unittest.TestCase):
-  """ Base class for the JobDB test cases
-  """
-
-  def setUp(self):
-    gLogger.setLevel('DEBUG')
-    self.jobDB = JobDB()
-
-  def tearDown(self):
-    result = self.jobDB.selectJobs({})
-    self.assertTrue(result['OK'], 'Status after selectJobs')
-    jobs = result['Value']
-    for job in jobs:
-      result = self.jobDB.removeJobFromDB(job)
-      self.assertTrue(result['OK'])
+gLogger.setLevel('DEBUG')
+jobDB = JobDB()
 
 
-class JobSubmissionCase(JobDBTestCase):
-  """  TestJobDB represents a test suite for the JobDB database front-end
-  """
+def test_insertAndRemoveJobIntoDB():
 
-  def test_insertAndRemoveJobIntoDB(self):
+  res = jobDB.insertNewJobIntoDB(jdl, 'owner', '/DN/OF/owner', 'ownerGroup', 'someSetup')
+  assert res['OK'] is True
+  jobID = res['JobID']
+  res = jobDB.getJobAttribute(jobID, 'Status')
+  assert res['OK'] is True
+  assert res['Value'] == 'Received'
+  res = jobDB.getJobAttribute(jobID, 'MinorStatus')
+  assert res['OK'] is True
+  assert res['Value'] == 'Job accepted'
+  res = jobDB.getJobOptParameters(jobID)
+  assert res['OK'] is True
+  assert res['Value'] == {}
 
-    res = self.jobDB.insertNewJobIntoDB(jdl, 'owner', '/DN/OF/owner', 'ownerGroup', 'someSetup')
-    self.assertTrue(res['OK'])
-    jobID = res['JobID']
-    res = self.jobDB.getJobAttribute(jobID, 'Status')
-    self.assertTrue(res['OK'])
-    self.assertEqual(res['Value'], 'Received')
-    res = self.jobDB.getJobAttribute(jobID, 'MinorStatus')
-    self.assertTrue(res['OK'])
-    self.assertEqual(res['Value'], 'Job accepted')
-    res = self.jobDB.getJobOptParameters(jobID)
-    self.assertTrue(res['OK'])
-    self.assertEqual(res['Value'], {})
-
-
-class JobRescheduleCase(JobDBTestCase):
-
-  def test_rescheduleJob(self):
-
-    res = self.jobDB.insertNewJobIntoDB(jdl, 'owner', '/DN/OF/owner', 'ownerGroup', 'someSetup')
-    self.assertTrue(res['OK'])
-    jobID = res['JobID']
-
-    result = self.jobDB.rescheduleJob(jobID)
-    self.assertTrue(result['OK'])
-
-    res = self.jobDB.getJobAttribute(jobID, 'Status')
-    self.assertTrue(res['OK'])
-    self.assertEqual(res['Value'], 'Received')
-    result = self.jobDB.getJobAttribute(jobID, 'MinorStatus')
-    self.assertTrue(result['OK'])
-    self.assertEqual(result['Value'], 'Job Rescheduled')
+  res = jobDB.selectJobs({})
+  assert res['OK'] is True
+  jobs = res['Value']
+  for job in jobs:
+    res = jobDB.removeJobFromDB(job)
+    assert res['OK'] is True
 
 
-class CountJobsCase(JobDBTestCase):
+def test_rescheduleJob():
 
-  def test_getCounters(self):
+  res = jobDB.insertNewJobIntoDB(jdl, 'owner', '/DN/OF/owner', 'ownerGroup', 'someSetup')
+  assert res['OK'] is True
+  jobID = res['JobID']
 
-    result = self.jobDB.getCounters('Jobs', ['Status', 'MinorStatus'], {}, '2007-04-22 00:00:00')
-    self.assertTrue(result['OK'], 'Status after getCounters')
+  res = jobDB.rescheduleJob(jobID)
+  assert res['OK'] is True
+
+  res = jobDB.getJobAttribute(jobID, 'Status')
+  assert res['OK'] is True
+  assert res['Value'] == 'Received'
+  res = jobDB.getJobAttribute(jobID, 'MinorStatus')
+  assert res['OK'] is True
+  assert res['Value'] == 'Job Rescheduled'
 
 
-if __name__ == '__main__':
+def test_getCounters():
 
-  suite = unittest.defaultTestLoader.loadTestsFromTestCase(JobSubmissionCase)
-  suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(JobRescheduleCase))
-  suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(CountJobsCase))
-  testResult = unittest.TextTestRunner(verbosity=2).run(suite)
+  res = jobDB.getCounters('Jobs', ['Status', 'MinorStatus'], {}, '2007-04-22 00:00:00')
+  assert res['OK'] is True
