22432a79f109cd46f9631f378fc0c261e334731c
Federico Stagni <federico.stagni@cern.ch>
2018-11-26 18:27:27 +0100

    bugfix: call the init of super class

--- FrameworkSystem/Client/NotificationClient.py
+++ FrameworkSystem/Client/NotificationClient.py
@@ -11,9 +11,11 @@ from DIRAC.Core.Utilities.Mail import Mail
 
 class NotificationClient(Client):
 
-  def __init__(self):
+  def __init__(self, **kwargs):
     """ Notification Client constructor
     """
+    Client.__init__(self, **kwargs)
+
     self.log = gLogger.getSubLogger('NotificationClient')
     self.setServer('Framework/Notification')
 
