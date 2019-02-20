225d42250fcd1e5e0fb41c6dd3229c067e9f3ac7
Federico Stagni <federico.stagni@cern.ch>
2019-02-20 15:01:04 +0100

    style

--- FrameworkSystem/private/logging/SubSystemLogger.py
+++ FrameworkSystem/private/logging/SubSystemLogger.py
@@ -2,16 +2,17 @@ __RCSID__ = "$Id$"
 
 from DIRAC.FrameworkSystem.Client.Logger import Logger
 
-class SubSystemLogger( Logger ):
 
-  def __init__( self, subName, masterLogger, child = True ):
-    Logger.__init__( self )
+class SubSystemLogger(Logger):
+
+  def __init__(self, subName, masterLogger, child=True):
+    Logger.__init__(self)
     self.__child = child
     self._minLevel = masterLogger._minLevel
-    for attrName in dir( masterLogger ):
-      attrValue = getattr( masterLogger, attrName )
-      if isinstance( attrValue, basestring ):
-        setattr( self, attrName, attrValue )
+    for attrName in dir(masterLogger):
+      attrValue = getattr(masterLogger, attrName)
+      if isinstance(attrValue, basestring):
+        setattr(self, attrName, attrValue)
     self.__masterLogger = masterLogger
     self._subName = subName
 
@@ -21,9 +22,11 @@ class SubSystemLogger( Logger ):
     """
     return self._subName
 
-  def processMessage( self, messageObject ):
+  def processMessage(self, messageObject):
     if self.__child:
-      messageObject.setSubSystemName( self._subName )
+      messageObject.setSubSystemName(self._subName)
     else:
-      messageObject.setSystemName( self._subName )
-self.__masterLogger.processMessage( messageObject )
+      messageObject.setSystemName(self._subName)
+
+
+self.__masterLogger.processMessage(messageObject)
