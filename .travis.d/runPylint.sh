89c89177348383c388ba812bcaca14f0aad93cdf
Marko Petric <marko.petric@cern.ch>
2019-03-21 14:46:27 +0100

    Enable pylint python3 check

--- .travis.d/runPylint.sh
+++ .travis.d/runPylint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-find . -name "*.py" -and -not -name 'pep8_*' -exec pylint -E --rcfile=.pylintrc --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" --extension-pkg-whitelist=GSI,numpy {} +
+if [[ "${CHECK}" == "pylintPY3K"  ]]; then
+  sed -i "s/load-plugins=/load-plugins=caniusepython3.pylint_checker/" .pylintrc
+  find . -name "*.py" -and -not -name 'pep8_*' -exec pylint -E --rcfile=.travis.d/.pylintrc3k --py3k --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" --extension-pkg-whitelist=GSI,numpy {} +
+else
+  find . -name "*.py" -and -not -name 'pep8_*' -exec pylint -E --rcfile=.pylintrc --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" --extension-pkg-whitelist=GSI,numpy {} +
+fi
