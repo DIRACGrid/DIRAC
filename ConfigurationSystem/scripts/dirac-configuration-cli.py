271907c8ef61a743aa53426680347807e08c484f
Federico Stagni <federico.stagni@cern.ch>
2019-03-26 14:29:34 +0100

    pycodestyle

--- ConfigurationSystem/scripts/dirac-configuration-cli.py
+++ ConfigurationSystem/scripts/dirac-configuration-cli.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 ########################################################################
-# $HeadURL$
 # File :   dirac-configuration-cli
 # Author : Adria Casajus
 ########################################################################
 """
   Command line interface to DIRAC Configuration Server
 """
-__RCSID__   = "$Id$"
+__RCSID__ = "$Id$"
 
 from DIRAC.Core.Base import Script
 from DIRAC.ConfigurationSystem.Client.CSCLI import CSCLI
 
-Script.localCfg.addDefaultEntry( "LogLevel", "fatal" )
-Script.setUsageMessage('\n'.join( [ __doc__.split( '\n' )[1],
-                                    'Usage:',
-                                    '  %s [option|cfgfile] ...' % Script.scriptName, ] )   )
+Script.localCfg.addDefaultEntry("LogLevel", "fatal")
+Script.setUsageMessage('\n'.join([__doc__.split('\n')[1],
+                                  'Usage:',
+                                  '  %s [option|cfgfile] ...' % Script.scriptName, ]))
 Script.parseCommandLine()
 
 CSCLI().start()
--- ConfigurationSystem/scripts/dirac-configuration-dump-local-cache.py
+++ ConfigurationSystem/scripts/dirac-configuration-dump-local-cache.py
@@ -1,46 +1,51 @@
 #!/usr/bin/env python
 ########################################################################
-# $HeadURL$
 # File :   dirac-configuration-cli
 # Author : Adria Casajus
 ########################################################################
 """
   Dump DIRAC Configuration data
 """
-__RCSID__   = "$Id$"
+__RCSID__ = "$Id$"
 
 import sys
 import DIRAC
 from DIRAC.Core.Base import Script
 
-Script.localCfg.addDefaultEntry( "LogLevel", "fatal" )
+Script.localCfg.addDefaultEntry("LogLevel", "fatal")
 
 fileName = ""
-def setFilename( args ):
+
+
+def setFilename(args):
   global fileName
   fileName = args
   return DIRAC.S_OK()
 
+
 raw = False
-def setRaw( args ):
+
+
+def setRaw(args):
   global raw
   raw = True
   return DIRAC.S_OK()
 
-Script.registerSwitch( "f:", "file=", "Dump Configuration data into <file>", setFilename )
-Script.registerSwitch( "r", "raw", "Do not make any modification to the data", setRaw )
-Script.setUsageMessage('\n'.join( [ __doc__.split( '\n' )[1],
-                                    'Usage:',
-                                    '  %s [option|cfgfile] ...' % Script.scriptName, ] )   )
+
+Script.registerSwitch("f:", "file=", "Dump Configuration data into <file>", setFilename)
+Script.registerSwitch("r", "raw", "Do not make any modification to the data", setRaw)
+Script.setUsageMessage('\n'.join([__doc__.split('\n')[1],
+                                  'Usage:',
+                                  '  %s [option|cfgfile] ...' % Script.scriptName, ]))
 Script.parseCommandLine()
 
 from DIRAC import gConfig, gLogger
-result = gConfig.dumpCFGAsLocalCache( fileName, raw )
-if not result[ 'OK' ]:
-  print "Error: %s" % result[ 'Message' ]
+result = gConfig.dumpCFGAsLocalCache(fileName, raw)
+if not result['OK']:
+  print "Error: %s" % result['Message']
   sys.exit(1)
 
 if not fileName:
-  print result[ 'Value' ]
+  print result['Value']
 
 sys.exit(0)
