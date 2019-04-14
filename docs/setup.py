59ac6856bbe1ca275715d80600ad53620d81941a
Andre Sailer <andre.philippe.sailer@cern.ch>
2019-04-14 19:34:42 +0200

    Docs: clean up setup.py

--- docs/setup.py
+++ docs/setup.py
@@ -1,49 +1,32 @@
-""" Basic setuptools script for DIRACDocs.
-    Does not contain any dependency
-"""
-
-import sys
+"""Basic setuptools script for DIRACDocs."""
 import os
 import glob
+
 # Actual setuptools
 from setuptools import setup, find_packages
 
 # Find the base dir where the setup.py lies
-base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'diracdoctools'))
-print 'BASE DIR', base_dir
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'diracdoctools'))
+
 # Take all the packages but the scripts and tests
-ALL_PACKAGES = find_packages(where=base_dir, exclude=['*test*'])
-print 'CHRIS allPackages %s' % ALL_PACKAGES
+ALL_PACKAGES = find_packages(where=BASE_DIR, exclude=['*test*'])
 
-# Because we want to have a 'DIRAC' base module and that the setup.py
-# is lying inside it, we need to define a mapping
-# < module name : directory >
-# e.g. DIRAC.DataManagementSystem is base_dir/DataManagementSystem
+PACKAGE_DIR = dict(("%s" % p, os.path.join(BASE_DIR, p.replace('.', '/'))) for p in ALL_PACKAGES)
 
-package_dir = dict(("%s" % p, os.path.join(base_dir, p.replace('.', '/'))) for p in ALL_PACKAGES)
-print 'CHRIS package_dir %s' % package_dir
-
-# We also rename the packages so that they contain diracdoctools
+# We rename the packages so that they contain diracdoctools
 ALL_PACKAGES = ['diracdoctools.%s' % p for p in ALL_PACKAGES]
 ALL_PACKAGES.insert(0, 'diracdoctools')
 
-print 'CHRIS allPackages %s' % ALL_PACKAGES
-package_dir['diracdoctools'] = base_dir
+PACKAGE_DIR['diracdoctools'] = BASE_DIR
 # The scripts to be distributed
-SCRIPTS = glob.glob('%s/scripts/*.py' % base_dir)
+SCRIPTS = glob.glob('%s/scripts/*.py' % BASE_DIR)
 
 setup(
     name='diracdoctools',
     version='6.19.2',
     url='https://github.com/DIRACGRID/DIRAC/docs',
     license='GPLv3',
-    package_dir=package_dir,
+    package_dir=PACKAGE_DIR,
     packages=ALL_PACKAGES,
     scripts=SCRIPTS,
-    # entry_points={'console_scripts': [
-    #   'dirac-docs-build-commands.py = diracdoctools.cmd.commandReference:run',
-    #   'dirac-docs-concatenate-diraccfg.py = diracdoctools.cmd.concatcfg:run',
-    #   'dirac-docs-get-release-notes.py = diracdoctools.cmd.getrelnotes:run',
-    #   'dirac-docs-build-code.py = diracdoctools.cmd.codedoc:run',
-    # ]},
 )
