30201bfc6c1aee8b222a8ea626f53333b531ec38
Federico Stagni <federico.stagni@cern.ch>
2019-04-16 18:23:42 +0200

    removed script not useful

--- AccountingSystem/Client/ReportCLI.py
+++ AccountingSystem/Client/ReportCLI.py
@@ -1,6 +1,25 @@
 """
 ReportCLI class implementing command line interface to DIRAC Accounting
 ReportGenerator Service. It is not complete yet
+
+Once ready it could be used with a script as simple as:
+
+
+from DIRAC.Core.Base import Script
+
+Script.localCfg.addDefaultEntry( "LogLevel", "info" )
+Script.setUsageMessage('\n'.join( [ __doc__.split( '\n' )[1],
+                                    'Usage:',
+                                    '  %s [option|cfgfile] ...' % Script.scriptName, ] )   )
+Script.parseCommandLine()
+
+from DIRAC.AccountingSystem.Client.ReportCLI import ReportCLI
+
+if __name__=="__main__":
+    reli = ReportCLI()
+    reli.start()
+
+
 """
 
 __RCSID__ = "$Id$"
