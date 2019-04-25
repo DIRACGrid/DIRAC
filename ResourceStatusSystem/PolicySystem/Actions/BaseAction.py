1d1768c39c06baa4e9fbc886c4bc22a68bc388b5
Federico Stagni <federico.stagni@cern.ch>
2019-04-25 13:17:37 +0200

    pycodestyle

--- ResourceStatusSystem/PolicySystem/Actions/BaseAction.py
+++ ResourceStatusSystem/PolicySystem/Actions/BaseAction.py
@@ -9,12 +9,12 @@ from DIRAC import gLogger
 __RCSID__ = '$Id$'
 
 
-class BaseAction( object ):
+class BaseAction(object):
   '''
     Base class for all actions. It defines a constructor an a run main method.
   '''
 
-  def __init__( self, name, decisionParams, enforcementResult, singlePolicyResults, clients ):
+  def __init__(self, name, decisionParams, enforcementResult, singlePolicyResults, clients):
 
     # enforcementResult supposed to look like:
     # {
@@ -35,20 +35,17 @@ class BaseAction( object ):
     #   'tokenOwner'  : None
     # }
 
-    self.actionName          = name # 'BaseAction'
+    self.actionName = name  # 'BaseAction'
     self.decisionParams = decisionParams
-    self.enforcementResult   = enforcementResult
+    self.enforcementResult = enforcementResult
     self.singlePolicyResults = singlePolicyResults
-    self.clients             = clients
-    self.log = gLogger.getSubLogger( self.__class__.__name__ )
-    self.log.verbose( "Running %s action" % self.__class__.__name__ )
+    self.clients = clients
+    self.log = gLogger.getSubLogger(self.__class__.__name__)
+    self.log.verbose("Running %s action" % self.__class__.__name__)
 
-  def run( self ):
+  def run(self):
     '''
       Method to be over written by the real actions
     '''
 
-    self.log.warn( '%s: you may want to overwrite this method' % self.actionName )
-
-################################################################################
-#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
+    self.log.warn('%s: you may want to overwrite this method' % self.actionName)
