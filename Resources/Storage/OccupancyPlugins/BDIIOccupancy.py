c50ca886dc8fc2dd09ff071d938a3e35af5d9c91
Hideki Miyake <hideki.miyake@kek.jp>
2019-06-05 20:20:26 +0900

    Get vo and host info assuming SRM

--- Resources/Storage/OccupancyPlugins/BDIIOccupancy.py
+++ Resources/Storage/OccupancyPlugins/BDIIOccupancy.py
@@ -6,22 +6,35 @@ from DIRAC.Core.Utilities.Grid import ldapsearchBDII
 
 
 class BDIIOccupancy(object):
-  def __init__(self):
-    self.host = 'lcg-bdii.cern.ch:2170'
+  def __init__(self, se):
+    self.isUsable = False
+    self.log = se.log.getSubLogger('BDIIOccupancy')
+    self.bdii = 'lcg-bdii.cern.ch:2170'
     if 'LCG_GFAL_INFOSYS' in os.environ:
-      self.host = os.environ['LCG_GFAL_INFOSYS']
+      self.bdii = os.environ['LCG_GFAL_INFOSYS']
+    self.vo = se.vo
+    # assume the SE speaks SRM
+    ret = se.getStorageParameters(protocol='srm')
+    if not ret['OK']:
+      self.log.error(ret['Message'])
+      return
+    if 'Host' not in ret['Value']:
+      self.log.error('No Host is found from StorageParameters')
+      return
+    self.host = ret['Value']['Host']
+    self.isUsable = True
 
-  def getOccupancy(self, sePlugin):
+  def getOccupancy(self):
     sTokenDict = {'Total': 0, 'Free': 0}
-    bdiiAttr = ['GlueSATotalOnlineSize', 'GlueSAFreeOnlineSize']
-    filt = "(&(GlueSAAccessControlBaseRule=VO:%s)" % sePlugin.voName
-    filt += "(GlueChunkKey=GlueSEUniqueID=%s))" % sePlugin.protocolParameters['Host']
-    ret = ldapsearchBDII(filt, bdiiAttr, host=self.host)
+    BDIIAttr = ['GlueSATotalOnlineSize', 'GlueSAFreeOnlineSize']
+
+    filt = "(&(GlueSAAccessControlBaseRule=VO:%s)(GlueChunkKey=GlueSEUniqueID=%s))" % (self.vo, self.host)
+    ret = ldapsearchBDII(filt, BDIIAttr, host=self.bdii)
     if not ret['OK']:
       return ret
     if len(ret['Value']) > 0:
       if 'attr' in ret['Value'][0]:
         attr = ret['Value'][0]['attr']
-        sTokenDict['Total'] = float(attr.get(bdiiAttr[0], 0)) * 1024 * 1024 * 1024
-        sTokenDict['Free'] = float(attr.get(bdiiAttr[1], 0)) * 1024 * 1024 * 1024
+        sTokenDict['Total'] = float(attr.get(BDIIAttr[0], 0)) * 1024 * 1024 * 1024
+        sTokenDict['Free'] = float(attr.get(BDIIAttr[1], 0)) * 1024 * 1024 * 1024
     return S_OK(sTokenDict)
