20cc477ef172c14d76b0fb5c0ab6f033a701ee81
Federico Stagni <federico.stagni@cern.ch>
2019-06-18 12:52:42 +0200

    added basic unit tests

---
+++ ResourceStatusSystem/Agent/test/Test_Agent_ElementInspectorAgent.py
@@ -0,0 +1,51 @@
+""" Test class for ElementInspectorAgent
+"""
+import Queue
+
+# imports
+import pytest
+from mock import MagicMock
+
+# DIRAC Components
+from DIRAC.ResourceStatusSystem.Agent.ElementInspectorAgent import ElementInspectorAgent
+from DIRAC import gLogger
+
+gLogger.setLevel('DEBUG')
+
+# Mock Objects
+mockAM = MagicMock()
+mockNone = MagicMock()
+mockNone.return_value = None
+mockSM = MagicMock()
+
+queueFilled = Queue.Queue()
+queueFilled.put({'status': 'status',
+		 'name': 'site',
+		 'site': 'site',
+		 'element': 'Site',
+		 'statusType': 'all',
+		 'elementType': 'Site'})
+
+
+@pytest.mark.parametrize(
+    "elementsToBeCheckedValue", [
+	(Queue.Queue()),
+	(queueFilled)
+    ])
+def test__execute(mocker, elementsToBeCheckedValue):
+  """ Testing JobCleaningAgent()._getAllowedJobTypes()
+  """
+
+  mocker.patch("DIRAC.ResourceStatusSystem.Agent.ElementInspectorAgent.AgentModule.__init__")
+  mocker.patch("DIRAC.ResourceStatusSystem.Agent.ElementInspectorAgent.AgentModule.am_getOption", side_effect=mockAM)
+
+  elementInspectorAgent = ElementInspectorAgent()
+  elementInspectorAgent.log = gLogger
+  elementInspectorAgent.log.setLevel('DEBUG')
+  elementInspectorAgent._AgentModule__configDefaults = mockAM
+  elementInspectorAgent.initialize()
+  elementInspectorAgent.elementsToBeChecked = elementsToBeCheckedValue
+
+  result = elementInspectorAgent._execute()
+
+  assert result == {'OK': True, 'Value': None}
