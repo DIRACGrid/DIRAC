27e2a1ca1886a7b0c48a3ebb9ce38f5bbd2240b1
Anton <anton.lu@cern.ch>
2019-06-19 09:49:58 +0200

    tuto script almost runnable twice

--- docs/source/AdministratorGuide/Tutorials/basicTutoSetup.sh
+++ docs/source/AdministratorGuide/Tutorials/basicTutoSetup.sh
@@ -13,7 +13,7 @@ HOSTNAME=`hostname`
 
 # avoid calling passwd, which might be the cern passwd in /usr/sue/bin
 # START add_dirac
-adduser -s /bin/bash -d /home/dirac dirac
+adduser -s /bin/bash -d /home/dirac dirac || echo "User dirac already exists."
 echo password | /usr/bin/passwd --stdin dirac
 mkdir -p /opt/dirac/sbin
 chown -R dirac:dirac /opt/dirac/
@@ -62,7 +62,7 @@ systemctl start runsvdir-start
 ## SETUP FOR MYSQL
 # remove mysql
 # START mysqlInstall
-yum remove -y $(rpm -qa | grep -i mysql | paste -sd ' ')
+yum remove -y $(rpm -qa | grep -i -e mysql -e mariadb | paste -sd ' ') || echo  "MySQL is not yet installed"
 rm -rf /var/lib/mysql/*
 yum install -y \
     https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-community-devel-5.7.25-1.el7.x86_64.rpm \
@@ -73,7 +73,7 @@ yum install -y \
 # END mysqlInstall
 
 # START mysqlStart
-systemctl start mysqld
+systemctl start mysqld || systemctl restart mysqld
 # END mysqlStart
 
 cat > mysqlSetup.sql <<EOF
@@ -209,9 +209,9 @@ sudo -u dirac /home/dirac/installDirac
 # add user to be used as a user
 
 # START user_diracuser
-adduser -s /bin/bash -d /home/diracuser diracuser
+adduser -s /bin/bash -d /home/diracuser diracuser || echo "User diracuser already exists."
 echo password | /usr/bin/passwd --stdin diracuser
-mkdir ~diracuser/.globus/
+mkdir -p ~diracuser/.globus/
 cp /opt/dirac/user/client.pem ~diracuser/.globus/usercert.pem
 cp /opt/dirac/user/client.key ~diracuser/.globus/userkey.pem
 chown -R diracuser:diracuser ~diracuser/.globus/
@@ -219,14 +219,14 @@ chown -R diracuser:diracuser ~diracuser/.globus/
 
 cat > InstallDiracClient <<EOF
 # START installClient1
-mkdir ~/DiracInstallation && cd ~/DiracInstallation
+mkdir -p ~/DiracInstallation && cd ~/DiracInstallation
 curl -O -L https://github.com/DIRACGrid/DIRAC/raw/integration/Core/scripts/dirac-install.py
 chmod +x dirac-install.py
 ./dirac-install.py -r v6r21p5 --dirac-os
 # END installClient1
 # START installClient2
 mkdir -p ~/DiracInstallation/etc/grid-security/
-ln -s /opt/dirac/etc/grid-security/certificates/ ~/DiracInstallation/etc/grid-security/certificates
+ln -fs /opt/dirac/etc/grid-security/certificates/ ~/DiracInstallation/etc/grid-security/certificates
 # END installClient2
 EOF
 
