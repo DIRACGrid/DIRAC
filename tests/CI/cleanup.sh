364684c9631681b60d757109e3e8397c33727590
Anton <anton.lu@cern.ch>
2019-06-28 12:10:33 +0200

    removed -e flag from cleanup script, source CONFIG before dirac_ci

--- tests/CI/cleanup.sh
+++ tests/CI/cleanup.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 # 
 
-set -ex
-
 SCRIPT_DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 
 source $SCRIPT_DIR/CONFIG
 source $SCRIPT_DIR/utils.sh
-export CONFIGFILE=$SCRIPT_DIR/tmp/CONFIG
 
 parseCommandLine
 
