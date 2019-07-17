94cb873406eaa5500642e0c465fc7d98c7f81f2f
arrabito <arrabito@in2p3.fr>
2019-07-17 12:07:36 +0200

    autopep8

--- tests/System/dirac-test-prod-sys.py
+++ tests/System/dirac-test-prod-sys.py
@@ -7,10 +7,6 @@ import os
 import json
 
 from DIRAC.Core.Base import Script
-Script.setUsageMessage('\n'.join([__doc__.split('\n')[1],
-                                  'Usage:',
-                                  '  %s test directory' % Script.scriptName
-                                  ]))
 
 Script.parseCommandLine()
 
@@ -23,32 +19,36 @@ from DIRAC.Interfaces.API.Job import Job
 from DIRAC.Core.Workflow.Parameter import Parameter
 from DIRAC.Resources.Catalog.FileCatalog import FileCatalog
 
+
 def createWorkflowBodyStep1():
   job = Job()
   job.setName('mandelbrot raw')
-  job.setOutputSandbox( ['*log'] )
+  job.setOutputSandbox(['*log'])
   # this is so that the JOB_ID within the transformation can be evaluated on the fly in the job application, see below
-  job.workflow.addParameter( Parameter( "JOB_ID", "000000", "string", "", "", True, False, "Initialize JOB_ID" ) )
-  ## define the job workflow in 3 steps
+  job.workflow.addParameter(Parameter("JOB_ID", "000000", "string", "", "", True, False, "Initialize JOB_ID"))
+  # define the job workflow in 3 steps
   # job step1: setup software
   job.setExecutable('git clone https://github.com/bregeon/mandel4ts.git')
   # job step2: run mandelbrot application
   # note how the JOB_ID (within the transformation) is passed as an argument and will be evaluated on the fly
-  job.setExecutable('./mandel4ts/mandelbrot.py',arguments="-P 0.0005 -M 1000 -L @{JOB_ID} -N 200")
+  job.setExecutable('./mandel4ts/mandelbrot.py', arguments="-P 0.0005 -M 1000 -L @{JOB_ID} -N 200")
   # job step3: upload data and set metadata
   outputPath = os.path.join('/dirac/prodsys/mandelbrot/images/raw')
   outputPattern = 'data_*txt'
-  outputSE = 'DIRAC-USER'
-  outputMetadata = json.dumps( {"application":"mandelbrot","image_format":"ascii", "image_width":7680, "image_height":200} )
-  job.setExecutable( './mandel4ts/dirac-add-files.py', arguments = "%s '%s' %s '%s'" % (outputPath, outputPattern, outputSE, outputMetadata ) )
+  outputSE = 'RAL-SE'
+  outputMetadata = json.dumps({"application": "mandelbrot", "image_format": "ascii",
+                               "image_width": 7680, "image_height": 200})
+  job.setExecutable('./mandel4ts/dirac-add-files.py', arguments="%s '%s' %s '%s'" %
+                    (outputPath, outputPattern, outputSE, outputMetadata))
   return job.workflow.toXML()
+
 
 def createWorkflowBodyStep2():
   job = Job()
   job.setName('merge mandelbrot')
-  job.setOutputSandbox( ['*log'] )
+  job.setOutputSandbox(['*log'])
 
-  ## define the job workflow in 3 steps
+  # define the job workflow in 3 steps
   # job step1: setup software
   job.setExecutable('git clone https://github.com/bregeon/mandel4ts.git')
   # job step2: run mandelbrot merge
@@ -56,11 +56,14 @@ def createWorkflowBodyStep2():
   # job step3: upload data and set metadata
   outputPath = os.path.join('/dirac/prodsys/mandelbrot/images/merged')
   outputPattern = 'data_merged*txt'
-  outputSE = 'DIRAC-USER'
+  outputSE = 'RAL-SE'
   nb_input_files = 7
-  outputMetadata = json.dumps( {"application":"mandelbrot","image_format":"ascii", "image_width":7680, "image_height":200*nb_input_files} )
-  job.setExecutable( './mandel4ts/dirac-add-files.py', arguments = "%s '%s' %s '%s'" % (outputPath, outputPattern, outputSE, outputMetadata ) )
+  outputMetadata = json.dumps({"application": "mandelbrot", "image_format": "ascii",
+                               "image_width": 7680, "image_height": 200 * nb_input_files})
+  job.setExecutable('./mandel4ts/dirac-add-files.py', arguments="%s '%s' %s '%s'" %
+                    (outputPath, outputPattern, outputSE, outputMetadata))
   return job.workflow.toXML()
+
 
 def createProductionStep(name, type, inputQuery=None, outputQuery=None):
   # create a production step
@@ -71,9 +74,14 @@ def createProductionStep(name, type, inputQuery=None, outputQuery=None):
   prodStep.Outputquery = outputQuery
   return prodStep
 
+
 # Set meta data fields in the DFC
 fc = FileCatalog()
-MDFieldDict = {'application': 'VARCHAR(128)', 'image_format': 'VARCHAR(128)', 'image_width': 'int', 'image_height': 'int'}
+MDFieldDict = {
+    'application': 'VARCHAR(128)',
+    'image_format': 'VARCHAR(128)',
+    'image_width': 'int',
+    'image_height': 'int'}
 for MDField in MDFieldDict.keys():
   MDFieldType = MDFieldDict[MDField]
   res = fc.addMetadataField(MDField, MDFieldType)
@@ -87,7 +95,7 @@ prodClient = ProductionClient()
 prod = Production()
 
 # Create the first production step and add it to the Production
-outputquery = {"application":"mandelbrot","image_format":"ascii", "image_width":7680, "image_height":200}
+outputquery = {"application": "mandelbrot", "image_format": "ascii", "image_width": 7680, "image_height": 200}
 prodStep1 = createProductionStep('ImageProd', 'MCSimulation', outputQuery=outputquery)
 body = createWorkflowBodyStep1()
 prodStep1.Body = body
@@ -97,8 +105,8 @@ if not res['OK']:
   exit(-1)
 
 # Create the second production step and add it to the Production
-inputquery = {"application":"mandelbrot","image_format":"ascii", "image_width":7680, "image_height":200}
-outputquery = {"application":"mandelbrot","image_format":"ascii", "image_width":7680, "image_height":1400}
+inputquery = {"application": "mandelbrot", "image_format": "ascii", "image_width": 7680, "image_height": 200}
+outputquery = {"application": "mandelbrot", "image_format": "ascii", "image_width": 7680, "image_height": 1400}
 prodStep2 = createProductionStep('MergeImage', 'DataProcessing', inputQuery=inputquery, outputQuery=outputquery)
 body = createWorkflowBodyStep2()
 prodStep2.Body = body
@@ -123,4 +131,3 @@ res = prodClient.startProduction(prodName)
 if not res['OK']:
   gLogger.error("Failed to start production", res['Message'])
   exit(-1)
-  
