c414e529427f8596e78b71333a84aa27f04d3765
arrabito <arrabito@in2p3.fr>
2019-07-17 17:09:13 +0200

    remove escapeString for ProdDescription

--- ProductionSystem/DB/ProductionDB.py
+++ ProductionSystem/DB/ProductionDB.py
@@ -85,11 +85,6 @@ class ProductionDB(DB):
 
     self.lock.acquire()
 
-    res = self._escapeString(prodDescription)
-    if not res['OK']:
-      return res
-    prodDescription = res['Value']
-
     req = "INSERT INTO Productions (ProductionName,Description,CreationDate,LastUpdate, \
                                     AuthorDN,AuthorGroup,Status)\
                                 VALUES ('%s','%s',UTC_TIMESTAMP(),UTC_TIMESTAMP(),'%s','%s','New');" % \
@@ -262,6 +257,7 @@ class ProductionDB(DB):
     if not res['OK']:
       return res
 
+    print res['Value']
     prodDescription = json.loads(res['Value'])
     transIDs = []
     for step in sorted(prodDescription):
