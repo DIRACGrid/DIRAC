5be37a010e035415dbe7c71c4b06d95945a1bf8f
Anton <anton.lu@cern.ch>
2019-07-22 17:11:31 +0200

    syntax fixes for sh

--- tests/CI/install_client.sh
+++ tests/CI/install_client.sh
@@ -8,7 +8,7 @@
 
 set -e
 
-source CONFIG
+source ./CONFIG
 
 CSURL=dips://$SERVER_HOST:9135/Configuration/Server
 
@@ -37,6 +37,7 @@ cd ../..
 
 echo -e '***' $(date -u) "**** Got the DIRAC tests ****\n"
 
+source ./TestCode/DIRAC/tests/Jenkins/dirac_ci.sh
 
 echo -e '***' $(date -u) "**** Client INSTALLATION START ****\n"
 
