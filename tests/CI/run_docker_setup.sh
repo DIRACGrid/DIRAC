30f1ff4541bc397240b3d2cfcc7f50ca723b6361
Anton <anton.lu@cern.ch>
2019-07-23 14:28:09 +0200

    add documentation, rename main script

--- tests/CI/run_docker_setup.sh
+++ tests/CI/run_docker_setup.sh
@@ -1,8 +1,5 @@
-#!/usr/bin/env bash
-# set -euo pipefail
-set -eo pipefail
-IFS=$'\n\t'
-#........................................................................
+#!/bin/sh
+#
 #    Executable script to set up DIRAC server and client instances with
 #        ElasticSearch and MySQL services, all in docker containers.
 #
@@ -15,141 +12,105 @@ IFS=$'\n\t'
 #    `docker login gitlab-registry.cern.ch` and following the prompts
 #
 #    Edit environment variables (settings) in the CONFIG file
+#
 #........................................................................
 
-BUILD_DIR=$PWD/integration_test_results
-mkdir -p "${BUILD_DIR}"
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export HOST_OS=${HOST_OS:-cc7}
+set -e
 
-# shellcheck source=tests/CI/CONFIG
-source "$SCRIPT_DIR/CONFIG"
-# shellcheck source=tests/CI/utils.sh
-source "$SCRIPT_DIR/utils.sh"
-cd "$SCRIPT_DIR"
+CWD=$PWD
+SCRIPT_DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 
-function prepareEnvironment() {
-  if [ -z "$TMP" ]; then
-      TMP=/tmp/DIRAC_CI_$(date +"%Y%m%d%I%M%p")
-      mkdir -p "$TMP"
-  fi
-  if [ -z "$CLIENTCONFIG" ]; then
-      CLIENTCONFIG=$PWD/CLIENTCONFIG
-  fi
-  if [ -z "$SERVERCONFIG" ]; then
-      SERVERCONFIG=$PWD/SERVERCONFIG
-  fi
+if [ -z $TMP ]; then
+    TMP=/tmp/DIRAC_CI_`date +"%Y%m%d%I%M%p"`
+    mkdir -p $TMP
+fi
+if [ -z $CLIENTCONFIG ]; then
+    CLIENTCONFIG=$TMP/CLIENTCONFIG
+fi
+if [ -z $SERVERCONFIG ]; then
+    SERVERCONFIG=$TMP/SERVERCONFIG
+fi
 
-  # GitLab variables
-  cp ./CONFIG "${SERVERCONFIG}"
-  if [[ -n $CI_PROJECT_DIR ]]; then
-      echo "I guess we're in GitLab CI, using local repository in branch ${CI_COMMIT_REF_NAME}"
-      export TESTREPO=$CI_PROJECT_DIR
-      export ALTERNATIVE_MODULES=$CI_PROJECT_DIR
+source $SCRIPT_DIR/CONFIG
+source $SCRIPT_DIR/utils.sh
+cd $SCRIPT_DIR
 
-      # find the latest version
-      majorVersion=$(grep "majorVersion =" "${TESTREPO}/__init__.py" | cut -d "=" -f 2)
-      minorVersion=$(grep "minorVersion =" "${TESTREPO}/__init__.py" | cut -d "=" -f 2)
-      export DIRACBRANCH=v${majorVersion// }r${minorVersion// }
-      echo "Deduced DIRACBRANCH ${DIRACBRANCH} from __init__.py"
+parseArguments
 
-      {
-        echo "export TESTREPO=${TESTREPO}"
-        echo "export ALTERNATIVE_MODULES=${ALTERNATIVE_MODULES}"
-        echo "export DIRACBRANCH=${DIRACBRANCH}"
-      } >> "${SERVERCONFIG}"
-  fi
-  cp "${SERVERCONFIG}" "${CLIENTCONFIG}"
+docker-compose -f ./docker-compose.yml up -d
 
-  docker-compose -f ./docker-compose.yml up -d
+echo -e "\n****" $(date -u) "Creating user and copying scripts ****"
 
-  echo -e "\n**** $(date -u) Creating user and copying scripts ****"
+# DIRAC server user and scripts
+docker exec server adduser -s /bin/bash -d $USER_HOME $USER
 
-  # DIRAC server user and scripts
-  docker exec server adduser -s /bin/bash -d "$USER_HOME" "$DOCKER_USER"
-  docker exec client adduser -s /bin/bash -d "$USER_HOME" "$DOCKER_USER"
+docker cp ./install_server.sh server:$USER_HOME
+docker cp $SERVERCONFIG server:$USER_HOME/CONFIG
 
-  # Create database user
-  docker exec mysql mysql --password=password -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-  docker exec mysql mysql --password=password -e "CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
-  docker exec mysql mysql --password=password -e "CREATE USER '${DB_USER}'@'mysql' IDENTIFIED BY '${DB_PASSWORD}';"
+# DIRAC client user and scripts
+docker exec client adduser -s /bin/bash -d $USER_HOME $USER
 
-  docker cp ./install_server.sh server:"$WORKSPACE"
-  docker cp ./install_client.sh client:"$WORKSPACE"
+docker cp ./install_client.sh client:$USER_HOME
+docker cp $CLIENTCONFIG client:$USER_HOME/CONFIG
 
-  copyLocalSource server "${SERVERCONFIG}"
-  copyLocalSource client "${CLIENTCONFIG}"
+copyLocalSource
 
-  docker cp "$SERVERCONFIG" server:"$WORKSPACE/CONFIG"
-  docker cp "$CLIENTCONFIG" client:"$WORKSPACE/CONFIG"
-}
 
-function installServer() {
-  docker exec -e TERM=xterm-color -u "$DOCKER_USER" -w "$WORKSPACE" server bash ./install_server.sh 2>&1 | tee "${BUILD_DIR}/log_server_install.txt"
+echo -e "\n****" $(date -u) "Installing DIRAC server ****"
+docker exec -u $USER \
+       -w $USER_HOME \
+       server \
+       bash -c "./install_server.sh > log.txt 2>&1" 
 
-  echo -e "\n**** $(date -u) Copying credentials and certificates ****"
-  docker exec client bash -c "mkdir -p $WORKSPACE/ServerInstallDIR/user $WORKSPACE/ClientInstallDIR/etc /home/dirac/.globus"
-  docker cp server:"$WORKSPACE/ServerInstallDIR/etc/grid-security" - | docker cp - client:"$WORKSPACE/ClientInstallDIR/etc/"
-  docker cp server:"$WORKSPACE/ServerInstallDIR/user/client.pem" - | docker cp - client:"$WORKSPACE/ServerInstallDIR/user/"
-  docker cp server:"$WORKSPACE/ServerInstallDIR/user/client.key" - | docker cp - client:"$WORKSPACE/ServerInstallDIR/user/"
-  docker exec client bash -c "cp $WORKSPACE/ServerInstallDIR/user/client.* $USER_HOME/.globus/"
-  docker cp server:/tmp/x509up_u1000 - | docker cp - client:/tmp/
-  docker exec client bash -c "chown -R dirac:dirac /home/dirac"
-  docker exec client bash -c "chown -R dirac:dirac /tmp/x509up_u1000"
-}
+echo -e "\n****" $(date -u) "Copying user certificates and installing client ****"
 
-function installClient() {
-  docker exec -e TERM=xterm-color -u "$DOCKER_USER" -w "$WORKSPACE" client bash ./install_client.sh 2>&1 | tee "${BUILD_DIR}/log_client_install.txt"
-}
+# Copy generated user credentials from server to temp folder
+docker cp server:$USER_HOME/ServerInstallDIR/user/client.pem $TMP/usercert.pem
+docker cp server:$USER_HOME/ServerInstallDIR/user/client.key $TMP/userkey.pem
+docker cp server:$USER_HOME/ServerInstallDIR/etc/grid-security $TMP/
 
-function testServer() {
-  docker exec -e TERM=xterm-color -u "$DOCKER_USER" -w "$WORKSPACE" -e INSTALLROOT="$WORKSPACE" -e INSTALLTYPE=server server \
-      bash TestCode/DIRAC/tests/CI/run_tests.sh || SERVER_CODE=$?
-  echo ${SERVER_CODE:-0} > "${BUILD_DIR}/server_test_status"
-}
+# install client
+docker exec -u $USER \
+       -w $USER_HOME \
+       client \
+       bash -c "./install_client.sh  > log.txt 2>&1"
 
-function testClient() {
-  docker exec -e TERM=xterm-color -u "$DOCKER_USER" -w "$WORKSPACE" -e INSTALLROOT="$WORKSPACE" -e INSTALLTYPE=client client \
-      bash TestCode/DIRAC/tests/CI/run_tests.sh || CLIENT_CODE=$?
-  echo ${CLIENT_CODE:-0} > "${BUILD_DIR}/client_test_status"
-}
+# copy credentials to client
+docker exec client bash -c "mkdir /home/dirac/.globus"
+docker cp $TMP/usercert.pem client:$USER_HOME/.globus/
+docker cp $TMP/userkey.pem client:$USER_HOME/.globus/
+docker cp $TMP/grid-security client:$USER_HOME/ClientInstallDIR/etc/
 
-function checkErrors() {
-  set +x
-  EXIT_CODE=0
+docker exec client bash -c "chown -R dirac:dirac /home/dirac/"
 
-  # Server
-  if [ ! -f "${BUILD_DIR}/server_test_status" ]; then
-    echo "ERROR: Server integration have not been ran"
-    EXIT_CODE=1
-  elif [ "$(cat "${BUILD_DIR}/server_test_status")" = "0" ]; then
-    echo "Server integration tests passed"
-  else
-    echo "ERROR: Server integration tests failed with $(cat "${BUILD_DIR}/server_test_status")"
-    EXIT_CODE=1
-  fi
 
-  # Client
-  if [ ! -f "${BUILD_DIR}/client_test_status" ]; then
-    echo "ERROR: Client integration have not been ran"
-    EXIT_CODE=1
-  elif [ "$(cat "${BUILD_DIR}/client_test_status")" = "0" ]; then
-    echo "Client integration tests passed"
-  else
-    echo "ERROR: Client integration tests failed with $(cat "${BUILD_DIR}/client_test_status")"
-    EXIT_CODE=1
-  fi
+set +e
 
-  exit $EXIT_CODE
-}
+echo -e "\n****" $(date -u) "Starting server tests ****"
+docker exec -u $USER \
+       -w $USER_HOME \
+       -e INSTALLROOT=$USER_HOME \
+       -e INSTALLTYPE=server \
+       server \
+       bash TestCode/DIRAC/tests/CI/run_tests.sh
+SERVER_ERR=$?
 
-if [ "${0}" = "${BASH_SOURCE[0]}" ]; then
-  prepareEnvironment
-  installServer
-  installClient
-  testServer
-  testClient
-  checkErrors
+echo -e "\n****" $(date -u) "Starting client tests ****"
+docker exec -u $USER \
+       -w $USER_HOME \
+       -e INSTALLROOT=$USER_HOME \
+       -e INSTALLTYPE=client \
+       client \
+       bash TestCode/DIRAC/tests/CI/run_tests.sh
+CLIENT_ERR=$?
+
+echo -e "\n****" $(date -u) "ALL DONE ****"
+
+
+if [ $SERVER_ERR -eq 0 ] && [ $CLIENT_ERR -eq 0 ]; then
+    echo "SUCCESS: All tests succeded" 
+    exit 0
 else
-  echo "Skipping setup, installation and testing as ${BASH_SOURCE[0]} is being sourced"
+    echo "At least one unit test failed. Check the logs for more info. "
+    exit 1
 fi
