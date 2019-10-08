047f2839c09f6ccdd5a2f216b8bb47fdce9609ce
Federico Stagni <federico.stagni@cern.ch>
2019-10-08 15:52:39 +0200

    style

--- Core/scripts/genAllCAs.sh
+++ Core/scripts/genAllCAs.sh
@@ -6,7 +6,7 @@ allF="/opt/dirac/etc/grid-security/cas.pem"
  
 narg=$#
 nar=0
-while [ $nar -lt $narg ] ; do
+while [ $nar -lt "$narg" ] ; do
   case "$1" in
     -h )
       echo "Usage : $0 [-h] [-i] [-o] "
@@ -18,7 +18,7 @@ while [ $nar -lt $narg ] ; do
     -i )
       shift
       gsCerts=$1
-      if [ ! -d $gsCerts ] ; then
+      if [ ! -d "$gsCerts" ] ; then
         echo "$gsCerts does not exist"
         exit
       fi
@@ -27,14 +27,14 @@ while [ $nar -lt $narg ] ; do
     -o )
       shift
       allF=$1
-      if [ ! -d $allF ] ; then
-        mkdir $allF
+      if [ ! -d "$allF" ] ; then
+        mkdir "$allF"
       fi
       allF=$allF/cas.pem
       shift
       ;;
   esac
-  if [ $nar -lt $narg ] ; then
+  if [ $nar -lt "$narg" ] ; then
     nar=$(( ++nar ))
   else
     break
@@ -49,13 +49,13 @@ for cert in $gsCerts/*.0
 do
   ossle="openssl x509 -noout -in ${cert}"
   if ${ossle} -checkend 3600; then
-    openssl x509 -in ${cert} >> $allF.gen
-    copiedCAs=`expr "${copiedCAs}" + "1"`
+    openssl x509 -in "${cert}" >> "$allF".gen
+    copiedCAs=$(expr "${copiedCAs}" + "1")
   else
     echo " - CA ${cert} is expired"
-    invalidCAs=`expr "${invalidCAs}" + "1"`
+    invalidCAs=$(expr "${invalidCAs}" + "1")
   fi
 done
 echo " + There are ${invalidCAs} invalid CA certificates in $gsCerts"
 echo " + Copied ${copiedCAs} CA certificates into $allF"
-mv $allF.gen $allF
+mv "$allF".gen "$allF"
