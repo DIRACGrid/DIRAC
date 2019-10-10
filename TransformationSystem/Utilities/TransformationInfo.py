LCDsoft/ILCDIRAC

ILCDIRAC-561: Add third implementation and rewrites in Utilities

ILCTransformationSystem/Utilities/TransformationInfo.py
@@ -2,9 +2,11 @@
 __RCSID__ = "$Id$"
 
 from DIRAC import gLogger, S_OK
-
 from DIRAC.Core.Workflow.Workflow                              import fromXMLString
+
 from ILCDIRAC.Core.Utilities.ProductionData                    import constructProductionLFNs
+
+from ILCDIRAC.ILCTransformationSystem.Utilities.JobInfo import ENABLED
 
 class TransformationInfo( object ):
   """ hold information about transformations """
@@ -12,14 +14,14 @@ class TransformationInfo( object ):
     self.log = gLogger.getSubLogger( "TInfo" )
     self.tID = transformationID
     self.transName = transName
-    self.transClient = tClient
+    self.tClient = tClient
     self.jobDB = jobDB
     self.logDB = logDB
     self.olist = self.__getOutputList()
 
   def __getTransformationWorkflow( self ):
     """return the workflow for the transformation"""
-    res = self.transClient.getTransformationParameters( self.tID, ['Body'] )
+    res = self.tClient.getTransformationParameters( self.tID, ['Body'] )
     if not res['OK']:
       self.log.error( 'Could not get Body from TransformationDB' )
       return res
@@ -56,3 +58,83 @@ class TransformationInfo( object ):
     expectedlfns = resFiles['Value']['ProductionOutputData']
     return expectedlfns 
 
+
+  def checkTasksStatus( self ):
+    """Check the status for the task of given transformation and taskID"""
+
+    res = self.tClient.getTransformationFiles( condDict = { 'TransformationID': self.tID } )
+    if not res['OK']:
+      raise RuntimeError( "Failed to get transformation tasks", res['Message'] )
+
+    tasksDict = {}
+    for task in res['Value']:
+      taskID = task['TaskID']
+      lfn = task['LFN']
+      status = task['Status']
+      fileID = task['FileID']
+      tasksDict[taskID] = dict( FileID=fileID, LFN=lfn, Status=status )
+
+    return tasksDict
+
+  def setJobDone( self, job ):
+    """ set the taskID to Done"""
+    if not ENABLED:
+      return
+    taskID = job.taskID
+    res = self.tClient.setTaskStatus( self.transName, taskID, "Done" )
+    if not res['OK']:
+      raise RuntimeError( "Failed updating task status" )
+    self.__updateJobStatus( "Done", "Job Finished Successfully" )
+
+  def setJobFailed( self, job ):
+    """ set the taskID to Done"""
+    if not ENABLED:
+      return
+    taskID = job.taskID
+    res = self.tClient.setTaskStatus( self.transName, taskID, "Failed" )
+    if not res['OK']:
+      raise RuntimeError( "Failed updating task status" )
+    self.__updateJobStatus( "Failed", "Job forced to Failed" )
+
+  def setInputUnused( self, job ):
+    """set the inputfile to unused"""
+    self.__setInputStatus( job, "Unused" )
+
+  def setInputProcessed( self, job ):
+    """set the inputfile to processed"""
+    self.__setInputStatus( job, "Processed" )
+
+  def __setInputStatus( self, job, status ):
+    """set the input file to status"""
+    if ENABLED:
+      result = self.tClient.setFileStatusForTransformation(self.tID, status, [job.inputFile], force = True)
+      if not result['OK']:
+        gLogger.error( "Failed updating status", result['Message'] )
+        raise RuntimeError( "Failed updating file status" )
+
+  def __updateJobStatus( self, jobID , status, minorstatus = None ):
+    """ This method updates the job status in the JobDB
+    """
+    self.log.verbose( "self.jobDB.setJobAttribute(%s,'Status','%s',update=True)" % ( jobID, status ) )
+
+    if ENABLED:
+      result = self.jobDB.setJobAttribute( jobID, 'Status', status, update = True )
+    else:
+      return S_OK( 'DisabledMode' )
+
+    if result['OK']:
+      if minorstatus:
+        self.log.verbose( "self.jobDB.setJobAttribute(%s,'MinorStatus','%s',update=True)" % ( jobID, minorstatus ) )
+        result = self.jobDB.setJobAttribute( jobID, 'MinorStatus', minorstatus, update = True )
+
+    if not minorstatus: #Retain last minor status for stalled jobs
+      result = self.jobDB.getJobAttributes( jobID, ['MinorStatus'] )
+      if result['OK']:
+        minorstatus = result['Value']['MinorStatus']
+
+    logStatus = status
+    result = self.logDB.addLoggingRecord( jobID, status = logStatus, minor = minorstatus, source = 'DataRecoveryAgent' )
+    if not result['OK']:
+      self.log.warn( result )
+
+    return result

DIRACGrid/DIRAC

TransformationSystem/Utilities/TransformationInfo.py
@@ -2,9 +2,11 @@
 __RCSID__ = "$Id$"
 
 from DIRAC import gLogger, S_OK
-
 from DIRAC.Core.Workflow.Workflow import fromXMLString
+
 from ILCDIRAC.Core.Utilities.ProductionData import constructProductionLFNs
+
+from DIRAC.TransformationSystem.Utilities.JobInfo import ENABLED
 
 
 class TransformationInfo(object):
@@ -14,14 +16,14 @@ class TransformationInfo(object):
     self.log = gLogger.getSubLogger("TInfo")
     self.tID = transformationID
     self.transName = transName
-    self.transClient = tClient
+    self.tClient = tClient
     self.jobDB = jobDB
     self.logDB = logDB
     self.olist = self.__getOutputList()
 
   def __getTransformationWorkflow(self):
     """return the workflow for the transformation"""
-    res = self.transClient.getTransformationParameters(self.tID, ['Body'])
+    res = self.tClient.getTransformationParameters(self.tID, ['Body'])
     if not res['OK']:
       self.log.error('Could not get Body from TransformationDB')
       return res
@@ -57,3 +59,83 @@ class TransformationInfo(object):
       raise RuntimeError("Failed to create productionLFNs")
     expectedlfns = resFiles['Value']['ProductionOutputData']
     return expectedlfns
+
+  def checkTasksStatus(self):
+    """Check the status for the task of given transformation and taskID"""
+
+    res = self.tClient.getTransformationFiles(condDict={'TransformationID': self.tID})
+    if not res['OK']:
+      raise RuntimeError("Failed to get transformation tasks", res['Message'])
+
+    tasksDict = {}
+    for task in res['Value']:
+      taskID = task['TaskID']
+      lfn = task['LFN']
+      status = task['Status']
+      fileID = task['FileID']
+      tasksDict[taskID] = dict(FileID=fileID, LFN=lfn, Status=status)
+
+    return tasksDict
+
+  def setJobDone(self, job):
+    """ set the taskID to Done"""
+    if not ENABLED:
+      return
+    taskID = job.taskID
+    res = self.tClient.setTaskStatus(self.transName, taskID, "Done")
+    if not res['OK']:
+      raise RuntimeError("Failed updating task status")
+    self.__updateJobStatus("Done", "Job Finished Successfully")
+
+  def setJobFailed(self, job):
+    """ set the taskID to Done"""
+    if not ENABLED:
+      return
+    taskID = job.taskID
+    res = self.tClient.setTaskStatus(self.transName, taskID, "Failed")
+    if not res['OK']:
+      raise RuntimeError("Failed updating task status")
+    self.__updateJobStatus("Failed", "Job forced to Failed")
+
+  def setInputUnused(self, job):
+    """set the inputfile to unused"""
+    self.__setInputStatus(job, "Unused")
+
+  def setInputProcessed(self, job):
+    """set the inputfile to processed"""
+    self.__setInputStatus(job, "Processed")
+
+  def __setInputStatus(self, job, status):
+    """set the input file to status"""
+    if ENABLED:
+      result = self.tClient.setFileStatusForTransformation(self.tID, status, [job.inputFile], force=True)
+      if not result['OK']:
+        gLogger.error("Failed updating status", result['Message'])
+        raise RuntimeError("Failed updating file status")
+
+  def __updateJobStatus(self, jobID, status, minorstatus=None):
+    """ This method updates the job status in the JobDB
+    """
+    self.log.verbose("self.jobDB.setJobAttribute(%s,'Status','%s',update=True)" % (jobID, status))
+
+    if ENABLED:
+      result = self.jobDB.setJobAttribute(jobID, 'Status', status, update=True)
+    else:
+      return S_OK('DisabledMode')
+
+    if result['OK']:
+      if minorstatus:
+        self.log.verbose("self.jobDB.setJobAttribute(%s,'MinorStatus','%s',update=True)" % (jobID, minorstatus))
+        result = self.jobDB.setJobAttribute(jobID, 'MinorStatus', minorstatus, update=True)
+
+    if not minorstatus:  # Retain last minor status for stalled jobs
+      result = self.jobDB.getJobAttributes(jobID, ['MinorStatus'])
+      if result['OK']:
+        minorstatus = result['Value']['MinorStatus']
+
+    logStatus = status
+    result = self.logDB.addLoggingRecord(jobID, status=logStatus, minor=minorstatus, source='DataRecoveryAgent')
+    if not result['OK']:
+      self.log.warn(result)
+
+    return result
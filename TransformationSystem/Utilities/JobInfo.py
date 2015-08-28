01f0490c7161a79363d8aeb307add6d08a6ce5da
Andre Sailer <andre.philippe.sailer@cern.ch>
2015-08-28 13:40:39 +0200

    ILCDIRAC-561: Add third implementation and rewrites in Utilities

---
+++ TransformationSystem/Utilities/JobInfo.py
@@ -0,0 +1,200 @@
+"""Job Information"""
+__RCSID__ = "$Id$"
+
+ENABLED = False
+
+from itertools import izip_longest
+import re
+
+
+class JobInfo(object):
+  """ hold information about jobs"""
+
+  def __init__(self, jobID, status, tID):
+    self.tID = int(tID)
+    self.jobID = int(jobID)
+    self.status = status
+    self.inputFile = None
+    self.inputFileExists = False
+    self.outputFiles = None
+    self.outputFileStatus = []
+    self.taskID = None
+    self.taskStatus = None
+    self.taskFileID = None
+    self.pendingRequest = False
+    self.finalTask = None
+
+  def __str__(self):
+    info = "%d: %s" % (self.jobID, self.status)
+    if self.tID and self.taskID:
+      info += " Transformation: %d -- %d " % (self.tID, self.taskID)
+    if self.taskStatus:
+      info += "TaskStatus: %s " % self.taskStatus
+      if self.finalTask:
+        info += "(File was processed in task %d)" % self.finalTask
+    if self.inputFile:
+      info += "\n---> inputFile: %s (%s)" % (self.inputFile, self.inputFileExists)
+    if self.outputFiles:
+      info += "\n---> OutputFiles: "
+      efInfo = ["%s (%s)" % _ for _ in izip_longest(self.outputFiles, self.outputFileStatus)]
+      info += ", ".join(efInfo)
+    if self.pendingRequest:
+      info += "\n PENDING REQUEST IGNORE THIS JOB!!!"
+    else:
+      info += "\n No Pending Requests"
+
+    return info
+
+  def allFilesExist(self):
+    """check if all files exists"""
+    return all("Exists" in status for status in self.outputFileStatus)
+
+  def allFilesMissing(self):
+    """check if all files are missing"""
+    return all("Missing" in status for status in self.outputFileStatus)
+
+  def someFilesMissing(self):
+    """check if some files are missing and therefore some files exist """
+    return not (self.allFilesExist() or self.allFilesMissing())
+
+  def getJobInformation(self, jobMon):
+    """get all the information for the job"""
+    jdlString = self.__getJDL(jobMon)
+    self.__getOutputFiles(jdlString)
+    self.__getTaskID(jdlString)
+    self.__getInputFile(jdlString)
+
+  def getTaskInfo(self, tasksDict, lfnTaskDict):
+    """extract the task information from the taskDict"""
+    if self.taskID not in tasksDict:
+      #print "taskID %d not in tasksDict" % self.taskID
+      taskDict = tasksDict[lfnTaskDict[self.inputFile]]
+      self.finalTask = lfnTaskDict[self.inputFile]
+    else:
+      taskDict = tasksDict[self.taskID]
+
+    #dict( FileID=fileID, LFN=lfn, Status=status )
+    if self.inputFile != taskDict['LFN']:
+      raise RuntimeError("Task info does not fit with job info: \n %s" % str(self))
+    self.taskStatus = taskDict['Status']
+    self.taskFileID = taskDict['FileID']
+
+  def checkFileExistance(self, fcClient):
+    """check if input and outputfile still exist"""
+    lfns = [self.inputFile] + self.outputFiles
+    reps = fcClient.exists(lfns)
+    if not reps['OK']:
+      raise RuntimeError("Failed to check existance: %s" % reps['Message'])
+    statuses = reps['Value']
+    success = statuses['Successful']
+
+    self.inputFileExists = self.inputFile in success
+    for lfn in self.outputFiles:
+      if lfn in success and success[lfn]:
+        self.outputFileStatus.append("Exists")
+      elif lfn in success:
+        self.outputFileStatus.append("Missing")
+      else:
+        self.outputFileStatus.append("Unknown")
+
+  def checkRequests(self, reqClient):
+    """check if there are pending Requests"""
+    result = reqClient.readRequestsForJobs([self.jobID])
+    if not result['OK']:
+      raise RuntimeError("Failed to check Requests: %s " % result['Message'])
+    if self.jobID in result['Value']['Successful']:
+      request = result['Value']['Successful'][self.jobID]
+      self.pendingRequest = request.Status != "Done"
+
+  def __getJDL(self, jobMon):
+    """return jdlstring for this job"""
+    res = jobMon.getJobJDL(int(self.jobID), False)
+    if not res['OK']:
+      raise RuntimeError("Failed to get jobJDL: %s" % res['Message'])
+    jdlString = res['Value']
+    return jdlString
+
+  def __getOutputFiles(self, jdlString):
+    """get the Production Outputfiles for the given Job"""
+    if 'ProductionOutputData = "' in jdlString:
+      lfns = JobInfo.__getSingleLFN(jdlString)
+    else:
+      lfns = JobInfo.__getMultiLFN(jdlString)
+    self.outputFiles = lfns
+
+  def __getInputFile(self, jdlString):
+    """get the Inputdata for the given job"""
+    for val in jdlString.split('\n'):
+      if 'InputData' in val:
+        lfn = re.search('".*"', val)
+        lfn = lfn.group(0).strip('"')
+        self.inputFile = lfn
+
+  def __getTaskID(self, jdlString):
+    """get the taskID """
+    for val in jdlString.split('\n'):
+      if 'TaskID' in val:
+        try:
+          self.taskID = int(val.strip(";").split("=")[1].strip(' "'))
+        except ValueError:
+          print "*" * 80
+          print "ERROR"
+          print val
+          print self
+          print "*" * 80
+          raise
+        break
+
+  @staticmethod
+  def __getSingleLFN(jdlString):
+    """get the only productionOutputData LFN from the jdlString"""
+    for val in jdlString.split('\n'):
+      if 'ProductionOutputData' in val:
+        lfn = re.search('".*"', val)
+        lfn = lfn.group(0).strip('"')
+        return [lfn]
+
+  @staticmethod
+  def __getMultiLFN(jdlString):
+    """ get multiple outputfiles """
+    lfns = []
+    counter = 0
+    getEm = False
+    for val in jdlString.split('\n'):
+      if 'ProductionOutputData' in val:
+        counter += 1
+        continue
+      if counter == 1 and '{' in val:
+        getEm = True
+        continue
+      if '}' in val and getEm:
+        break
+      if getEm:
+        lfn = re.search('".*"', val)
+        lfn = lfn.group(0).strip('"')
+        lfns.append(lfn)
+    return lfns
+
+  def setJobDone(self, tInfo):
+    """mark job as done in wms and transformationsystem"""
+    if ENABLED:
+      tInfo.setJobDone(self.taskID)
+
+  def setJobFailed(self, tInfo):
+    """mark job as failed in  wms and transformationsystem"""
+    if ENABLED:
+      tInfo.setJobFailed(self.taskID)
+
+  def setInputProcessed(self, tInfo):
+    """mark input file as Processed"""
+    if ENABLED:
+      tInfo.setInputProcessed(self)
+
+  def setInputUnused(self, tInfo):
+    """mark input file as Unused"""
+    if ENABLED:
+      tInfo.setInputProcessed(self)
+
+  def cleanOutputs(self, tInfo):
+    """remove all job outputs"""
+    pass
--- TransformationSystem/Utilities/TransformationInfo.py
+++ TransformationSystem/Utilities/TransformationInfo.py
@@ -2,9 +2,11 @@
 __RCSID__ = "$Id$"
 
 from DIRAC import gLogger, S_OK
-
 from DIRAC.Core.Workflow.Workflow import fromXMLString
+
 from ILCDIRAC.Core.Utilities.ProductionData import constructProductionLFNs
+
+from DIRAC.TransformationSystem.Utilities.JobInfo import ENABLED
 
 
 class TransformationInfo(object):
@@ -14,14 +16,14 @@ class TransformationInfo(object):
     self.log = gLogger.getSubLogger("TInfo")
     self.tID = transformationID
     self.transName = transName
-    self.transClient = tClient
+    self.tClient = tClient
     self.jobDB = jobDB
     self.logDB = logDB
     self.olist = self.__getOutputList()
 
   def __getTransformationWorkflow(self):
     """return the workflow for the transformation"""
-    res = self.transClient.getTransformationParameters(self.tID, ['Body'])
+    res = self.tClient.getTransformationParameters(self.tID, ['Body'])
     if not res['OK']:
       self.log.error('Could not get Body from TransformationDB')
       return res
@@ -57,3 +59,83 @@ class TransformationInfo(object):
       raise RuntimeError("Failed to create productionLFNs")
     expectedlfns = resFiles['Value']['ProductionOutputData']
     return expectedlfns
+
+  def checkTasksStatus(self):
+    """Check the status for the task of given transformation and taskID"""
+
+    res = self.tClient.getTransformationFiles(condDict={'TransformationID': self.tID})
+    if not res['OK']:
+      raise RuntimeError("Failed to get transformation tasks", res['Message'])
+
+    tasksDict = {}
+    for task in res['Value']:
+      taskID = task['TaskID']
+      lfn = task['LFN']
+      status = task['Status']
+      fileID = task['FileID']
+      tasksDict[taskID] = dict(FileID=fileID, LFN=lfn, Status=status)
+
+    return tasksDict
+
+  def setJobDone(self, job):
+    """ set the taskID to Done"""
+    if not ENABLED:
+      return
+    taskID = job.taskID
+    res = self.tClient.setTaskStatus(self.transName, taskID, "Done")
+    if not res['OK']:
+      raise RuntimeError("Failed updating task status")
+    self.__updateJobStatus("Done", "Job Finished Successfully")
+
+  def setJobFailed(self, job):
+    """ set the taskID to Done"""
+    if not ENABLED:
+      return
+    taskID = job.taskID
+    res = self.tClient.setTaskStatus(self.transName, taskID, "Failed")
+    if not res['OK']:
+      raise RuntimeError("Failed updating task status")
+    self.__updateJobStatus("Failed", "Job forced to Failed")
+
+  def setInputUnused(self, job):
+    """set the inputfile to unused"""
+    self.__setInputStatus(job, "Unused")
+
+  def setInputProcessed(self, job):
+    """set the inputfile to processed"""
+    self.__setInputStatus(job, "Processed")
+
+  def __setInputStatus(self, job, status):
+    """set the input file to status"""
+    if ENABLED:
+      result = self.tClient.setFileStatusForTransformation(self.tID, status, [job.inputFile], force=True)
+      if not result['OK']:
+        gLogger.error("Failed updating status", result['Message'])
+        raise RuntimeError("Failed updating file status")
+
+  def __updateJobStatus(self, jobID, status, minorstatus=None):
+    """ This method updates the job status in the JobDB
+    """
+    self.log.verbose("self.jobDB.setJobAttribute(%s,'Status','%s',update=True)" % (jobID, status))
+
+    if ENABLED:
+      result = self.jobDB.setJobAttribute(jobID, 'Status', status, update=True)
+    else:
+      return S_OK('DisabledMode')
+
+    if result['OK']:
+      if minorstatus:
+        self.log.verbose("self.jobDB.setJobAttribute(%s,'MinorStatus','%s',update=True)" % (jobID, minorstatus))
+        result = self.jobDB.setJobAttribute(jobID, 'MinorStatus', minorstatus, update=True)
+
+    if not minorstatus:  # Retain last minor status for stalled jobs
+      result = self.jobDB.getJobAttributes(jobID, ['MinorStatus'])
+      if result['OK']:
+        minorstatus = result['Value']['MinorStatus']
+
+    logStatus = status
+    result = self.logDB.addLoggingRecord(jobID, status=logStatus, minor=minorstatus, source='DataRecoveryAgent')
+    if not result['OK']:
+      self.log.warn(result)
+
+    return result
