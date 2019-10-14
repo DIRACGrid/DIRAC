LCDsoft/ILCDIRAC

ILCTrans tests: Add spec/autospec to Mocks/patches
will check that proper functions and signatures are used when calling Mocks

ILCTransformationSystem/Tests/Test_DRA.py
@@ -7,6 +7,7 @@ from collections import defaultdict
 
 from mock import MagicMock as Mock, patch
 
+import DIRAC
 from DIRAC import S_OK, S_ERROR, gLogger
 
 from ILCDIRAC.ILCTransformationSystem.Agent.DataRecoveryAgent import DataRecoveryAgent
@@ -24,10 +25,10 @@ class TestDRA( unittest.TestCase ):
   @patch("ILCDIRAC.ILCTransformationSystem.Agent.DataRecoveryAgent.ReqClient", new=Mock() )
   def setUp ( self ):
     self.dra = DataRecoveryAgent( agentName="ILCTransformationSystem/DataRecoveryAgent", loadName="TestDRA" )
-    self.dra.reqClient = Mock()
-    self.dra.tClient = Mock()
-    self.dra.fcClient = Mock()
-    self.dra.jobMon = Mock()
+    self.dra.reqClient=Mock( name="reqMock", spec=DIRAC.RequestManagementSystem.Client.ReqClient.ReqClient )
+    self.dra.tClient=Mock( name="transMock", spec=DIRAC.TransformationSystem.Client.TransformationClient.TransformationClient )
+    self.dra.fcClient=Mock( name="fcMock", spec=DIRAC.Resources.Catalog.FileCatalogClient.FileCatalogClient )
+    self.dra.jobMon=Mock( name="jobMonMock", spec=DIRAC.WorkloadManagementSystem.Client.JobMonitoringClient.JobMonitoringClient)
     self.dra.printEveryNJobs = 10
 
   def tearDown ( self ):
@@ -35,7 +36,14 @@ class TestDRA( unittest.TestCase ):
 
   def getTestMock( self ):
     """create a JobInfo object with mocks"""
-    testJob = Mock ( name = "jobInfoMock")
+    from ILCDIRAC.ILCTransformationSystem.Utilities.JobInfo import JobInfo
+    testJob = Mock ( name = "jobInfoMock", spec=JobInfo )
+    testJob.jobID = 1234567
+    testJob.tType = "testType"
+    testJob.otherTasks = None
+    testJob.inputFileExists = True
+    testJob.status = "Done"
+    testJob.fileStatus = "Assigned"
     testJob.outputFiles = ["/my/stupid/file.lfn", "/my/stupid/file2.lfn"]
     testJob.outputFileStatus = ["Exists", "Exists"]
     testJob.inputFile = "inputfile.lfn"
@@ -108,13 +116,14 @@ class TestDRA( unittest.TestCase ):
     """test for DataRecoveryAgent treatProduction skip.............................................."""
     getJobMock = Mock( name = "getJobMOck" )
     getJobMock.getJobs.return_value = ( Mock( name = "jobsMock" ), 50, 50 )
-    tinfoMock = Mock( name = "infoMock", return_value = getJobMock )
     self.dra.checkAllJobs = Mock()
     self.dra.jobCache[1234] = (50, 50)
     #catch the printout to check path taken
     out = StringIO()
     sys.stdout = out
-    with patch("ILCDIRAC.ILCTransformationSystem.Agent.DataRecoveryAgent.TransformationInfo", new=tinfoMock ):
+    with patch("ILCDIRAC.ILCTransformationSystem.Agent.DataRecoveryAgent.TransformationInfo",
+               autospec=True,
+               return_value=getJobMock ):
       self.dra.treatProduction( prodID=1234, transName="TestProd12", transType="MCReconstruction" ) ##returns None
     self.assertIn( "Skipping production 1234", out.getvalue().strip().splitlines()[0] )
 
@@ -122,7 +131,8 @@ class TestDRA( unittest.TestCase ):
   def test_checkJob( self ):
     """test for DataRecoveryAgent checkJob MCGeneration............................................."""
 
-    tInfoMock = Mock( name = "tInfoMock" )
+    from ILCDIRAC.ILCTransformationSystem.Utilities.TransformationInfo import TransformationInfo
+    tInfoMock = Mock( name = "tInfoMock", spec=TransformationInfo )
     
     from ILCDIRAC.ILCTransformationSystem.Utilities.JobInfo import JobInfo
 
@@ -160,7 +170,8 @@ class TestDRA( unittest.TestCase ):
   def test_checkJob_others( self ):
     """test for DataRecoveryAgent checkJob other ProductionTypes ..................................."""
 
-    tInfoMock = Mock( name = "tInfoMock" )
+    from ILCDIRAC.ILCTransformationSystem.Utilities.TransformationInfo import TransformationInfo
+    tInfoMock = Mock( name = "tInfoMock", spec=TransformationInfo )
     
     from ILCDIRAC.ILCTransformationSystem.Utilities.JobInfo import JobInfo
 
@@ -543,7 +554,8 @@ class TestDRA( unittest.TestCase ):
     ### test with additional task dicts
     out = StringIO()
     sys.stdout = out
-    tInfoMock = Mock( name = "tInfoMock" )
+    from ILCDIRAC.ILCTransformationSystem.Utilities.TransformationInfo import TransformationInfo
+    tInfoMock = Mock( name = "tInfoMock", spec=TransformationInfo )
     mockJobs = dict([ (i, self.getTestMock() ) for i in xrange(11) ] )
     mockJobs[2].pendingRequest = True
     mockJobs[3].getJobInformation = Mock( side_effect = ( RuntimeError("ARGJob1"), None ) )

ILCTransformationSystem/Tests/Test_TransformationInfo.py
@@ -8,6 +8,7 @@ from collections import OrderedDict
 from mock import MagicMock as Mock, patch
 
 from DIRAC import S_OK, S_ERROR, gLogger
+import DIRAC
 
 from ILCDIRAC.ILCTransformationSystem.Utilities.JobInfo import JobInfo
 from ILCDIRAC.ILCTransformationSystem.Utilities.TransformationInfo import TransformationInfo
@@ -22,10 +23,10 @@ class TestTI( unittest.TestCase ):
 
   def setUp( self ):
 
-    tMock = Mock( name = "transMock")
-    tMock.setFileStatusForTransformation = Mock( name = "setFileStat" )
-    fcMock = Mock( name = "fcMock" )
-    jmMock = Mock( name = "jobMonMock" )
+    tMock = Mock( name = "transMock",spec=DIRAC.TransformationSystem.Client.TransformationClient.TransformationClient)
+    tMock.setFileStatusForTransformation = Mock( name = "setFileStat")
+    fcMock = Mock( name = "fcMock" , spec=DIRAC.Resources.Catalog.FileCatalogClient.FileCatalogClient )
+    jmMock = Mock( name = "jobMonMock" , spec=DIRAC.WorkloadManagementSystem.Client.JobMonitoringClient.JobMonitoringClient)
 
     self.tri = TransformationInfo( transformationID = 1234,
                                    transName = "TestTrans",
@@ -75,7 +76,7 @@ class TestTI( unittest.TestCase ):
 
   def test_setJob_Status( self ):
     """ILCDIRAC.ILCTransformationSystem.Utilities.TransformationInfo setJob functions..............."""
-    job = Mock()
+    job = Mock( spec=JobInfo )
     job.jobID = 5678
     self.tri.enabled = False
     self.tri._TransformationInfo__setTaskStatus = Mock()
@@ -127,7 +128,7 @@ class TestTI( unittest.TestCase ):
     """ILCDIRAC.ILCTransformationSystem.Utilities.TransformationInfo setInput functions............."""
     self.tri._TransformationInfo__setInputStatus = Mock()
     self.tri.enabled = False
-    job = Mock()
+    job = Mock( spec=JobInfo )
 
     self.tri._TransformationInfo__setInputStatus.reset_mock()
     self.tri.setInputUnused( job )
@@ -143,7 +144,8 @@ class TestTI( unittest.TestCase ):
 
   def test_setInputStatus( self ):
     """ILCDIRAC.ILCTransformationSystem.Utilities.TransformationInfo setInputStatus................."""
-    job = Mock()
+    job = Mock( spec=JobInfo )
+    job.inputFile = "dummylfn"
     status = "Unused"
 
     self.tri.enabled = False
@@ -160,7 +162,7 @@ class TestTI( unittest.TestCase ):
 
   def test_setTaskStatus( self ):
     """ILCDIRAC.ILCTransformationSystem.Utilities.TransformationInfo setTaskStatus.................."""
-    job = Mock()
+    job = Mock( spec=JobInfo )
     job.taskID = 1234
     self.tri.tClient.setTaskStatus = Mock( return_value = S_OK("Done") )
     self.tri._TransformationInfo__setTaskStatus( job, "Processed" )
@@ -185,7 +187,9 @@ class TestTI( unittest.TestCase ):
     jobDBMock.return_value = dbMock
     logDBMock = Mock()
     logDBMock.return_value = logMock
+#    with patch("DIRAC.WorkloadManagementSystem.DB.JobDB", new=Mock() ):
     with patch("DIRAC.WorkloadManagementSystem.DB.JobDB.JobDB", new=jobDBMock ):
+#      with patch("DIRAC.WorkloadManagementSystem.DB.JobLoggingDB", new=Mock() ):
       with patch("DIRAC.WorkloadManagementSystem.DB.JobLoggingDB.JobLoggingDB", new=logDBMock ):
         self.tri.enabled = False
         res = self.tri._TransformationInfo__updateJobStatus( 1234, "Failed", minorstatus=None )
@@ -262,12 +266,11 @@ class TestTI( unittest.TestCase ):
     descList = self.tri._TransformationInfo__findAllDescendants( lfnList = [] )
     self.assertEqual( descList, [] )
 
-  @patch("DIRAC.DataManagementSystem.Client.DataManager.DataManager", new=Mock() )
   def test_cleanOutputs( self ):
     """ILCDIRAC.ILCTransformationSystem.Utilities.TransformationInfo cleanOutputs..................."""
     descList = ["lfnDD1", "lfnDD2","lfnD1", "lfnD2"]
 
-    jobInfo = Mock()
+    jobInfo = Mock( spec=JobInfo )
     jobInfo.outputFiles = ["lfn1", "lfn2"]
     jobInfo.outputFileStatus = ["Exists", "Missing"]
 
@@ -282,13 +285,14 @@ class TestTI( unittest.TestCase ):
 
     remMock = Mock( name = "remmock" )
     remMock.removeFile.return_value = S_ERROR( "arg" )
-    dataMock = Mock( return_value = remMock )
 
     self.tri.enabled = True
     self.tri._TransformationInfo__findAllDescendants = Mock( return_value = descList )
     out = StringIO()
     sys.stdout = out
-    with patch("ILCDIRAC.ILCTransformationSystem.Utilities.TransformationInfo.DataManager", new=dataMock ):
+    with patch("ILCDIRAC.ILCTransformationSystem.Utilities.TransformationInfo.DataManager",
+               return_value=remMock,
+               autospec=True ):
       with self.assertRaisesRegexp( RuntimeError, "Failed to remove LFNs: arg" ):
         self.tri.cleanOutputs( jobInfo )
 
@@ -298,21 +302,22 @@ class TestTI( unittest.TestCase ):
                                                         "lfnD3":"SomeReason",
                                                         "lfnDD2":"SomeOtherReason"} } )
 
-    dataMock = Mock( return_value = remMock )
     self.tri.enabled = True
-    with patch("ILCDIRAC.ILCTransformationSystem.Utilities.TransformationInfo.DataManager", new=dataMock ):
+    with patch("ILCDIRAC.ILCTransformationSystem.Utilities.TransformationInfo.DataManager",
+               autospec=True,
+               return_value=remMock ):
       self.tri.cleanOutputs( jobInfo )
     self.assertIn( "Successfully removed 2 files", out.getvalue() )
 
     ### nothing to remove
-    jobInfo = Mock()
+    jobInfo = Mock( spec=JobInfo )
     jobInfo.outputFiles = []
     self.tri._TransformationInfo__findAllDescendants = Mock( return_value = descList )
     self.tri.cleanOutputs( jobInfo )
     self.tri._TransformationInfo__findAllDescendants.assert_not_called()
 
     ### nothing to remove
-    jobInfo = Mock()
+    jobInfo = Mock( spec=JobInfo )
     jobInfo.outputFiles = ["lfn1", "lfn2"]
     jobInfo.outputFileStatus = ["Missing", "Missing"]
     self.tri._TransformationInfo__findAllDescendants = Mock( return_value = [] )

DIRACGrid/DIRAC

TransformationSystem/test/Test_DRA.py
@@ -7,6 +7,7 @@ from collections import defaultdict
 
 from mock import MagicMock as Mock, patch
 
+import DIRAC
 from DIRAC import S_OK, S_ERROR, gLogger
 
 from DIRAC.TransformationSystem.Agent.DataRecoveryAgent import DataRecoveryAgent
@@ -25,10 +26,14 @@ class TestDRA(unittest.TestCase):
   @patch("DIRAC.TransformationSystem.Agent.DataRecoveryAgent.ReqClient", new=Mock())
   def setUp(self):
     self.dra = DataRecoveryAgent(agentName="ILCTransformationSystem/DataRecoveryAgent", loadName="TestDRA")
-    self.dra.reqClient = Mock()
-    self.dra.tClient = Mock()
-    self.dra.fcClient = Mock()
-    self.dra.jobMon = Mock()
+    self.dra.reqClient = Mock(name="reqMock", spec=DIRAC.RequestManagementSystem.Client.ReqClient.ReqClient)
+    self.dra.tClient = Mock(
+        name="transMock",
+        spec=DIRAC.TransformationSystem.Client.TransformationClient.TransformationClient)
+    self.dra.fcClient = Mock(name="fcMock", spec=DIRAC.Resources.Catalog.FileCatalogClient.FileCatalogClient)
+    self.dra.jobMon = Mock(
+        name="jobMonMock",
+        spec=DIRAC.WorkloadManagementSystem.Client.JobMonitoringClient.JobMonitoringClient)
     self.dra.printEveryNJobs = 10
 
   def tearDown(self):
@@ -36,7 +41,14 @@ class TestDRA(unittest.TestCase):
 
   def getTestMock(self):
     """create a JobInfo object with mocks"""
-    testJob = Mock(name="jobInfoMock")
+    from DIRAC.TransformationSystem.Utilities.JobInfo import JobInfo
+    testJob = Mock(name="jobInfoMock", spec=JobInfo)
+    testJob.jobID = 1234567
+    testJob.tType = "testType"
+    testJob.otherTasks = None
+    testJob.inputFileExists = True
+    testJob.status = "Done"
+    testJob.fileStatus = "Assigned"
     testJob.outputFiles = ["/my/stupid/file.lfn", "/my/stupid/file2.lfn"]
     testJob.outputFileStatus = ["Exists", "Exists"]
     testJob.inputFile = "inputfile.lfn"
@@ -111,7 +123,6 @@ class TestDRA(unittest.TestCase):
     """test for DataRecoveryAgent treatProduction skip.............................................."""
     getJobMock = Mock(name="getJobMOck")
     getJobMock.getJobs.return_value = (Mock(name="jobsMock"), 50, 50)
-    tinfoMock = Mock(name="infoMock", return_value=getJobMock)
     self.dra.checkAllJobs = Mock()
     self.dra.jobCache[1234] = (50, 50)
     # catch the printout to check path taken
@@ -124,10 +135,12 @@ class TestDRA(unittest.TestCase):
       self.dra.treatProduction(prodID=1234, transInfoDict=transInfoDict)  # returns None
     self.dra.log.notice.assert_called_with(MatchStringWith("Skipping production 1234"))
 
+
   def test_checkJob(self):
     """test for DataRecoveryAgent checkJob MCGeneration............................................."""
 
-    tInfoMock = Mock(name="tInfoMock")
+    from DIRAC.TransformationSystem.Utilities.TransformationInfo import TransformationInfo
+    tInfoMock = Mock(name="tInfoMock", spec=TransformationInfo)
 
     from DIRAC.TransformationSystem.Utilities.JobInfo import JobInfo
 
@@ -163,7 +176,8 @@ class TestDRA(unittest.TestCase):
   def test_checkJob_others(self):
     """test for DataRecoveryAgent checkJob other ProductionTypes ..................................."""
 
-    tInfoMock = Mock(name="tInfoMock")
+    from DIRAC.TransformationSystem.Utilities.TransformationInfo import TransformationInfo
+    tInfoMock = Mock(name="tInfoMock", spec=TransformationInfo)
 
     from DIRAC.TransformationSystem.Utilities.JobInfo import JobInfo
 
@@ -576,7 +590,8 @@ class TestDRA(unittest.TestCase):
     ### test with additional task dicts
     out = StringIO()
     sys.stdout = out
-    tInfoMock = Mock(name="tInfoMock")
+    from DIRAC.TransformationSystem.Utilities.TransformationInfo import TransformationInfo
+    tInfoMock = Mock(name="tInfoMock", spec=TransformationInfo)
     mockJobs = dict([(i, self.getTestMock()) for i in xrange(11)])
     mockJobs[2].pendingRequest = True
     mockJobs[3].getJobInformation = Mock(side_effect=(RuntimeError("ARGJob1"), None))

TransformationSystem/test/Test_TransformationInfo.py
@@ -8,6 +8,7 @@ from collections import OrderedDict
 from mock import MagicMock as Mock, patch
 
 from DIRAC import S_OK, S_ERROR, gLogger
+import DIRAC
 
 from DIRAC.TransformationSystem.Utilities.JobInfo import JobInfo
 from DIRAC.TransformationSystem.Utilities.TransformationInfo import TransformationInfo
@@ -22,10 +23,10 @@ class TestTI(unittest.TestCase):
 
   def setUp(self):
 
-    tMock = Mock(name="transMock")
+    tMock = Mock(name="transMock", spec=DIRAC.TransformationSystem.Client.TransformationClient.TransformationClient)
     tMock.setFileStatusForTransformation = Mock(name="setFileStat")
-    fcMock = Mock(name="fcMock")
-    jmMock = Mock(name="jobMonMock")
+    fcMock = Mock(name="fcMock", spec=DIRAC.Resources.Catalog.FileCatalogClient.FileCatalogClient)
+    jmMock = Mock(name="jobMonMock", spec=DIRAC.WorkloadManagementSystem.Client.JobMonitoringClient.JobMonitoringClient)
 
     self.tri = TransformationInfo(transformationID=1234,
                                   transName="TestTrans",
@@ -74,7 +75,7 @@ class TestTI(unittest.TestCase):
 
   def test_setJob_Status(self):
     """DIRAC.TransformationSystem.Utilities.TransformationInfo setJob functions..............."""
-    job = Mock()
+    job = Mock(spec=JobInfo)
     job.jobID = 5678
     self.tri.enabled = False
     self.tri._TransformationInfo__setTaskStatus = Mock()
@@ -126,7 +127,7 @@ class TestTI(unittest.TestCase):
     """DIRAC.TransformationSystem.Utilities.TransformationInfo setInput functions............."""
     self.tri._TransformationInfo__setInputStatus = Mock()
     self.tri.enabled = False
-    job = Mock()
+    job = Mock(spec=JobInfo)
 
     self.tri._TransformationInfo__setInputStatus.reset_mock()
     self.tri.setInputUnused(job)
@@ -142,7 +143,8 @@ class TestTI(unittest.TestCase):
 
   def test_setInputStatus(self):
     """DIRAC.TransformationSystem.Utilities.TransformationInfo setInputStatus................."""
-    job = Mock()
+    job = Mock(spec=JobInfo)
+    job.inputFile = "dummylfn"
     status = "Unused"
 
     self.tri.enabled = False
@@ -159,7 +161,7 @@ class TestTI(unittest.TestCase):
 
   def test_setTaskStatus(self):
     """DIRAC.TransformationSystem.Utilities.TransformationInfo setTaskStatus.................."""
-    job = Mock()
+    job = Mock(spec=JobInfo)
     job.taskID = 1234
     self.tri.tClient.setTaskStatus = Mock(return_value=S_OK("Done"))
     self.tri._TransformationInfo__setTaskStatus(job, "Processed")
@@ -183,7 +185,9 @@ class TestTI(unittest.TestCase):
     jobDBMock.return_value = dbMock
     logDBMock = Mock()
     logDBMock.return_value = logMock
+#    with patch("DIRAC.WorkloadManagementSystem.DB.JobDB", new=Mock() ):
     with patch("DIRAC.WorkloadManagementSystem.DB.JobDB.JobDB", new=jobDBMock):
+      #      with patch("DIRAC.WorkloadManagementSystem.DB.JobLoggingDB", new=Mock() ):
       with patch("DIRAC.WorkloadManagementSystem.DB.JobLoggingDB.JobLoggingDB", new=logDBMock):
         self.tri.enabled = False
         res = self.tri._TransformationInfo__updateJobStatus(1234, "Failed", minorstatus=None)
@@ -261,12 +265,11 @@ class TestTI(unittest.TestCase):
     descList = self.tri._TransformationInfo__findAllDescendants(lfnList=[])
     self.assertEqual(descList, [])
 
-  @patch("DIRAC.DataManagementSystem.Client.DataManager.DataManager", new=Mock())
   def test_cleanOutputs(self):
     """DIRAC.TransformationSystem.Utilities.TransformationInfo cleanOutputs..................."""
     descList = ["lfnDD1", "lfnDD2", "lfnD1", "lfnD2"]
 
-    jobInfo = Mock()
+    jobInfo = Mock(spec=JobInfo)
     jobInfo.outputFiles = ["lfn1", "lfn2"]
     jobInfo.outputFileStatus = ["Exists", "Missing"]
 
@@ -281,13 +284,14 @@ class TestTI(unittest.TestCase):
 
     remMock = Mock(name="remmock")
     remMock.removeFile.return_value = S_ERROR("arg")
-    dataMock = Mock(return_value=remMock)
 
     self.tri.enabled = True
     self.tri._TransformationInfo__findAllDescendants = Mock(return_value=descList)
     out = StringIO()
     sys.stdout = out
-    with patch("DIRAC.TransformationSystem.Utilities.TransformationInfo.DataManager", new=dataMock):
+    with patch("DIRAC.TransformationSystem.Utilities.TransformationInfo.DataManager",
+               return_value=remMock,
+               autospec=True):
       with self.assertRaisesRegexp(RuntimeError, "Failed to remove LFNs: arg"):
         self.tri.cleanOutputs(jobInfo)
 
@@ -297,21 +301,22 @@ class TestTI(unittest.TestCase):
                                                        "lfnD3": "SomeReason",
                                                        "lfnDD2": "SomeOtherReason"}})
 
-    dataMock = Mock(return_value=remMock)
     self.tri.enabled = True
-    with patch("DIRAC.TransformationSystem.Utilities.TransformationInfo.DataManager", new=dataMock):
+    with patch("DIRAC.TransformationSystem.Utilities.TransformationInfo.DataManager",
+               autospec=True,
+               return_value=remMock):
       self.tri.cleanOutputs(jobInfo)
     self.assertIn("Successfully removed 2 files", out.getvalue())
 
     ### nothing to remove
-    jobInfo = Mock()
+    jobInfo = Mock(spec=JobInfo)
     jobInfo.outputFiles = []
     self.tri._TransformationInfo__findAllDescendants = Mock(return_value=descList)
     self.tri.cleanOutputs(jobInfo)
     self.tri._TransformationInfo__findAllDescendants.assert_not_called()
 
     ### nothing to remove
-    jobInfo = Mock()
+    jobInfo = Mock(spec=JobInfo)
     jobInfo.outputFiles = ["lfn1", "lfn2"]
     jobInfo.outputFileStatus = ["Missing", "Missing"]
     self.tri._TransformationInfo__findAllDescendants = Mock(return_value=[])