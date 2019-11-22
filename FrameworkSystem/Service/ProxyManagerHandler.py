88b809d977175e8e1d13f7e5dc132bbffabb48ef
Zoltan Mathe <zoltan.mathe@cern.ch>
2019-11-22 16:00:06 +0100

    allow to download proxy for PRIVATE_LIMITED_DELEGATION role

--- FrameworkSystem/Service/ProxyManagerHandler.py
+++ FrameworkSystem/Service/ProxyManagerHandler.py
@@ -128,16 +128,16 @@ class ProxyManagerHandler( RequestHandler ):
     Check the properties and return if they can only download limited proxies if authorized
     """
     credDict = self.getRemoteCredentials()
-    if Properties.FULL_DELEGATION in credDict[ 'properties' ]:
-      return S_OK( False )
-    if Properties.LIMITED_DELEGATION in credDict[ 'properties' ]:
-      return S_OK( True )
-    if Properties.PRIVATE_LIMITED_DELEGATION in credDict[ 'properties' ]:
-      if credDict[ 'DN' ] != requestedUserDN:
-        return S_ERROR( "You are not allowed to download any proxy" )
-      if Properties.PRIVATE_LIMITED_DELEGATION in Registry.getPropertiesForGroup( requestedUserGroup ):
-        return S_ERROR( "You can't download proxies for that group" )
-      return S_OK( True )
+    if Properties.FULL_DELEGATION in credDict['properties']:
+      return S_OK(False)
+    if Properties.LIMITED_DELEGATION in credDict['properties']:
+      return S_OK(True)
+    if Properties.PRIVATE_LIMITED_DELEGATION in credDict['properties']:
+      if credDict['DN'] != requestedUserDN:
+        return S_ERROR("You are not allowed to download any proxy")
+      if Properties.PRIVATE_LIMITED_DELEGATION not in Registry.getPropertiesForGroup(requestedUserGroup):
+        return S_ERROR("You can't download proxies for that group")
+      return S_OK(True)
     # Not authorized!
     return S_ERROR( "You can't get proxies! Bad boy!" )
 
