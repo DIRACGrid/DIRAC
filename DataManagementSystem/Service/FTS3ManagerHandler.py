d40b40d676066d2548c925a101279726bef057b4
Christophe Haen <christophe.haen@cern.ch>
2019-11-28 16:18:41 +0100

    FTS3Manager: strengthen security of persistOperation

--- DataManagementSystem/Service/FTS3ManagerHandler.py
+++ DataManagementSystem/Service/FTS3ManagerHandler.py
@@ -13,7 +13,10 @@ __RCSID__ = "$Id$"
 
 # from DIRAC
 from DIRAC import S_OK, S_ERROR, gLogger
+from DIRAC.ConfigurationSystem.Client.Helpers.Registry import getDNForUsername
 from DIRAC.Core.DISET.RequestHandler import RequestHandler, getServiceOption
+from DIRAC.Core.Security.Properties import FULL_DELEGATION, LIMITED_DELEGATION, TRUSTED_HOST
+from DIRAC.Core.Utilities import DErrno
 
 
 from DIRAC.DataManagementSystem.DB.FTS3DB import FTS3DB
@@ -43,10 +46,49 @@ class FTS3ManagerHandler(RequestHandler):
     # # create tables for empty db
     return cls.fts3db.createTables()
 
+  @staticmethod
+  def _isAllowed(opObj, remoteCredentials):
+    """
+        Make sure the client is allowed to persist an operation
+        (FULL_DELEGATION or LIMITED_DELEGATION). This is the case of pilots,
+        the RequestExecutingAgent or the FTS3Agent
+
+        :param opObj: the FTS3Operation object
+        :param remoteCredentials: credentials from the clients
+
+        :returns: True if everything is fine, False otherwise
+    """
+
+    credDN = remoteCredentials['DN']
+    credGroup = remoteCredentials['group']
+    credProperties = remoteCredentials['properties']
+
+    # First, get the DN matching the username
+    res = getDNForUsername(opObj.username)
+    # if we have an error, do not allow
+    if not res['OK']:
+      gLogger.error("Error retrieving DN for username", res)
+      return False
+
+    # List of DN matching the username
+    dnList = res['Value']
+
+    # If the credentials in the Request match those from the credentials, it's OK
+    if credDN in dnList and opObj.userGroup == credGroup:
+      return True
+
+    # From here, something/someone is putting a request on behalf of someone else
+
+    # Only allow this if the credentials have Full or Limited delegation properties
+
+    if FULL_DELEGATION in credProperties or LIMITED_DELEGATION in credProperties:
+      return True
+
+    return False
+
   types_persistOperation = [basestring]
 
-  @classmethod
-  def export_persistOperation(cls, opJSON):
+  def export_persistOperation(self, opJSON):
     """ update or insert request into db
 
         :param opJSON: json string representing the operation
@@ -55,7 +97,13 @@ class FTS3ManagerHandler(RequestHandler):
     """
 
     opObj, _size = decode(opJSON)
-    return cls.fts3db.persistOperation(opObj)
+
+    isAuthorized = FTS3ManagerHandler._isAllowed(opObj, self.getRemoteCredentials())
+
+    if not isAuthorized:
+      return S_ERROR(DErrno.ENOAUTH, "Credentials in the requests are not allowed")
+
+    return self.fts3db.persistOperation(opObj)
 
   types_getOperation = [(long, int)]
 
@@ -101,10 +149,9 @@ class FTS3ManagerHandler(RequestHandler):
     return S_OK(activeJobsJSON)
 
   types_updateFileStatus = [dict]
-
   @classmethod
   def export_updateFileStatus(cls, fileStatusDict, ftsGUID):
-    """ Update the file ftsStatus and error
+    """ Update the file ftsStatus and error.
 
        :param fileStatusDict: { fileID : { status , error } }
        :param ftsGUID: (not mandatory) If specified, only update the rows where the ftsGUID matches this value.
@@ -113,7 +160,6 @@ class FTS3ManagerHandler(RequestHandler):
     return cls.fts3db.updateFileStatus(fileStatusDict, ftsGUID)
 
   types_updateJobStatus = [dict]
-
   @classmethod
   def export_updateJobStatus(cls, jobStatusDict):
     """ Update the job Status and error
