9d13b9dc5e9cf8d6699bbb41d17653a8364a0b71
Federico Stagni <federico.stagni@cern.ch>
2019-12-04 10:35:13 +0100

    v7r0p8,v7r1-pre11

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ scripts = glob.glob('%s/*/scripts/*.py' % base_dir)
 
 setup(
     name="DIRAC",
-    version="7.1.pre11",
+    version="7.2.pre2",
     url="https://github.com/DIRACGRID/DIRAC",
     license="GPLv3",
     package_dir=package_dir,
