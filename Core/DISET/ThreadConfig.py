2681654c327992dbfcf978a323a5a36971ee4cac
TaykYoku <yokutayk@gmail.com>
2020-01-14 12:23:39 +0100

    Core: ChreadConfig - use getID/setID to get/set unique ID from some IdP

--- Core/DISET/ThreadConfig.py
+++ Core/DISET/ThreadConfig.py
@@ -1,9 +1,10 @@
 
 import threading
 import functools
+
 from DIRAC.Core.Utilities.DIRACSingleton import DIRACSingleton
 
-class ThreadConfig( threading.local ):
+class ThreadConfig(threading.local):
   """ This class allows to contain extra information when a call is done on behalf of
       somebody else. Typically, when a host performs the request on behalf of a user.
       It is not used inside DIRAC, but is used in WebAppDIRAC for example
@@ -12,77 +13,128 @@ class ThreadConfig( threading.local ):
       however the attributes are thread locals (because of the threading.local inheritance).
 
       Also, this class has to be populated manually, no Client class will do it for you.
-
   """
-
   __metaclass__ = DIRACSingleton
 
-
-  def __init__( self ):
+  def __init__(self):
     self.reset()
 
-  def reset( self ):
+  def reset(self):
+    """ Reset extra information
+    """
     self.__DN = False
+    self.__ID = False
     self.__group = False
     self.__deco = False
     self.__setup = False
 
-  def setDecorator( self, deco ):
+  def setDecorator(self, deco):
+    """ Set decorator
+
+        :param deco: decorator
+    """
     self.__deco = deco
 
-  def getDecorator( self ):
+  def getDecorator(self):
+    """ Return decorator
+
+        :return: decorator
+    """
     return self.__deco
 
-  def setDN( self, DN ):
+  def setDN(self, DN):
+    """ Set DN
+
+        :param str DN: DN
+    """
     self.__DN = DN
 
-  def getDN( self ):
-    return self.__DN
+  def getDN(self):
+    """ Return DN
 
-  def setGroup( self, group ):
+        :return: str
+    """
+    return self.__DN
+  
+  def setID(self, ID):
+    """ Set user ID
+
+        :param str ID: user ID
+    """
+    self.__ID = ID
+
+  def getID(self):
+    """ Return user ID
+
+        :return: str
+    """
+    return self.__ID
+
+  def setGroup(self, group):
+    """ Set group
+
+        :param str group: group name
+    """
     self.__group = group
 
-  def getGroup( self ):
+  def getGroup(self):
+    """ Return group name
+
+        :return str
+    """
     return self.__group
 
-  def setID( self, DN, group ):
-    self.__DN = DN
-    self.__group = group
+  def setSetup(self, setup):
+    """ Set setup name
 
-  def getID( self ):
-    return ( self.__DN, self.__group )
-
-  def setSetup( self, setup ):
+        :param str setup: setup name
+    """
     self.__setup = setup
 
-  def getSetup( self ):
+  def getSetup(self):
+    """ Return setup name
+
+        :return: str
+    """
     return self.__setup
 
-  def dump( self ):
-    return ( self.__DN, self.__group, self.__setup )
+  def dump(self):
+    """ Return extra information
 
-  def load( self, tp ):
-    if tp[0]:
-      self.__DN = tp[0]
-    if tp[1]:
-      self.__group = tp[1]
-    if tp[2]:
-      self.__setup = tp[2]
+        :return: tuple
+    """
+    return (self.__ID, self.__DN, self.__group, self.__setup)
 
+  def load(self, tp):
+    """ Save extra information
 
-def threadDeco( method ):
+        :param tuple tp: contain DN, group name, setup name
+    """
+    self.__ID = tp[0] or self.__ID
+    self.__DN = tp[1] or self.__DN
+    self.__group = tp[2] or self.__group
+    self.__setup = tp[3] or self.__setup
 
+def threadDeco(method):
+  """ Tread decorator
+
+      :param method: method
+
+      :return: wrapped method
+  """
   tc = ThreadConfig()
 
-  @functools.wraps( method )
-  def wrapper( *args, **kwargs ):
-    """
-    THIS IS SPARTAAAAAAAAAA
+  @functools.wraps(method)
+  def wrapper(*args, **kwargs):
+    """ Wrapper
+
+        :return: wrapped method
     """
     deco = tc.getDecorator()
     if not deco:
-      return method( *args, **kwargs )
-    #Deco is a decorator sooo....
-    return deco( method )( *args, **kwargs )
+      return method(*args, **kwargs)
+
+    # Deco is a decorator sooo....
+    return deco(method)(*args, **kwargs)
 
   return wrapper
