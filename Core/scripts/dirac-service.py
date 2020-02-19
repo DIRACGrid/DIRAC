2176298a484eecb99b34ea6998820e67e04f69d7
Federico Stagni <federico.stagni@cern.ch>
2020-02-19 15:25:48 +0100

    autopep8

--- Core/scripts/dirac-service.py
+++ Core/scripts/dirac-service.py
@@ -14,33 +14,33 @@ from DIRAC.Core.Utilities.DErrno import includeExtensionErrors
 localCfg = LocalConfiguration()
 
 positionalArgs = localCfg.getPositionalArguments()
-if len( positionalArgs ) == 0:
-  gLogger.fatal( "You must specify which server to run!" )
-  sys.exit( 1 )
+if len(positionalArgs) == 0:
+  gLogger.fatal("You must specify which server to run!")
+  sys.exit(1)
 
 serverName = positionalArgs[0]
-localCfg.setConfigurationForServer( serverName )
-localCfg.addMandatoryEntry( "Port" )
+localCfg.setConfigurationForServer(serverName)
+localCfg.addMandatoryEntry("Port")
 #localCfg.addMandatoryEntry( "HandlerPath" )
-localCfg.addMandatoryEntry( "/DIRAC/Setup" )
-localCfg.addDefaultEntry( "/DIRAC/Security/UseServerCertificate", "yes" )
-localCfg.addDefaultEntry( "LogLevel", "INFO" )
-localCfg.addDefaultEntry( "LogColor", True )
+localCfg.addMandatoryEntry("/DIRAC/Setup")
+localCfg.addDefaultEntry("/DIRAC/Security/UseServerCertificate", "yes")
+localCfg.addDefaultEntry("LogLevel", "INFO")
+localCfg.addDefaultEntry("LogColor", True)
 resultDict = localCfg.loadUserData()
-if not resultDict[ 'OK' ]:
-  gLogger.initialize( serverName, "/" )
-  gLogger.error( "There were errors when loading configuration", resultDict[ 'Message' ] )
-  sys.exit( 1 )
+if not resultDict['OK']:
+  gLogger.initialize(serverName, "/")
+  gLogger.error("There were errors when loading configuration", resultDict['Message'])
+  sys.exit(1)
 
 includeExtensionErrors()
 
 
 serverToLaunch = ServiceReactor()
-result = serverToLaunch.initialize( positionalArgs )
-if not result[ 'OK' ]:
-  gLogger.error( result[ 'Message' ] )
-  sys.exit( 1 )
+result = serverToLaunch.initialize(positionalArgs)
+if not result['OK']:
+  gLogger.error(result['Message'])
+  sys.exit(1)
 result = serverToLaunch.serve()
-if not result[ 'OK' ]:
-  gLogger.error( result[ 'Message' ] )
-  sys.exit( 1 )
+if not result['OK']:
+  gLogger.error(result['Message'])
+  sys.exit(1)
