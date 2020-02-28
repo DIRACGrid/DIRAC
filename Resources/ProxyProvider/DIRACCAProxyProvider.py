f8ec2275b4b7f7b708f51d31c103e1a94d7da65b
TaykYoku <yokutayk@gmail.com>
2020-02-28 16:18:33 +0100

    Resources: add getFakeProxy method to DIRACCAProxyProvider

--- Resources/ProxyProvider/DIRACCAProxyProvider.py
+++ Resources/ProxyProvider/DIRACCAProxyProvider.py
@@ -164,22 +164,13 @@ class DIRACCAProxyProvider(ProxyProvider):
       return S_ERROR('Cannot generate proxy with %s DN.' % userDN)
 
     result = self.__createCertM2Crypto()
-    if not result['OK']:
-      return result
-    certStr, keyStr = result['Value']
-
-    chain = X509Chain()
-    result = chain.loadChainFromString(certStr)
-    if not result['OK']:
-      return result
-    result = chain.loadKeyFromString(keyStr)
-    if not result['OK']:
-      return result
-
-    result = chain.generateProxyToString(365 * 24 * 3600, rfc=True)
-    if not result['OK']:
-      return result
-    return S_OK({'proxy': result['Value']})
+    if result['OK']:
+      certStr, keyStr = result['Value']
+      chain = X509Chain()
+      if chain.loadChainFromString(certStr)['OK'] and chain.loadKeyFromString(keyStr)['OK']:
+        result = chain.generateProxyToString(365 * 24 * 3600, rfc=True)
+    
+    return S_OK({'proxy': result['Value']}) if result['OK'] else result
 
   def getUserDN(self, **kwargs):
     """ Get DN of the user certificate that will be created
@@ -293,8 +284,10 @@ class DIRACCAProxyProvider(ProxyProvider):
         return S_ERROR('Cannot set "%s" field.' % field)
     return S_OK()
 
-  def __createCertM2Crypto(self):
+  def __createCertM2Crypto(self, extensions=None):
     """ Create new certificate for user
+
+        :param list extensions: list of X509.new_extension()
 
         :return: S_OK(tuple)/S_ERROR() -- tuple contain certificate and pulic key as strings
     """
@@ -307,9 +300,11 @@ class DIRACCAProxyProvider(ProxyProvider):
     userCert.set_version(2)
     userCert.set_subject(self.__X509Name)
     userCert.set_serial_number(int(random.random() * 10 ** 10))
-    # Add extentionals
+    # Add extensions
     userCert.add_ext(X509.new_extension('basicConstraints', 'CA:' + str(False).upper()))
     userCert.add_ext(X509.new_extension('extendedKeyUsage', 'clientAuth', critical=1))
+    for ext in extensions or []:
+      userCert.add_ext(ext)
     # Set livetime
     validityTime = datetime.timedelta(days=400)
     notBefore = ASN1.ASN1_UTCTIME()
@@ -334,3 +329,29 @@ class DIRACCAProxyProvider(ProxyProvider):
     userCertStr = userCert.as_pem()
     userPubKeyStr = userPubKey.as_pem(cipher=None, callback=util.no_passphrase_callback)
     return S_OK((userCertStr, userPubKeyStr))
+
+  def getFakeProxy(self, dn, vo, **kwargs):
+    """ Get fake proxy for tests
+
+        :param str dn: fake DN
+        :param str vo: fake VOMS VO name
+        :param dict kwargs: fake VO description dictionary with possible fields:
+               - Expired time
+               - Role, etc..
+        
+        :return: S_OK(dict)/S_ERROR() -- dict contain 'proxy' field with is a fake proxy string
+    """
+    self.__X509Name = X509.X509_Name()
+    for field, value in [field.split('=') for field in dn.lstrip('/').split('/')]:
+      result = self.__fillX509Name(field, value)
+      if not result['OK']:
+        return result
+
+    result = self.__createCertM2Crypto([X509.new_extension('vomsExtensions', vo)])
+    if result['OK']:
+      certStr, keyStr = result['Value']
+      chain = X509Chain()
+      if chain.loadChainFromString(certStr)['OK'] and chain.loadKeyFromString(keyStr)['OK']:
+        result = chain.generateProxyToString(365 * 24 * 3600, rfc=True)
+    
+    return S_OK({'proxy': result['Value']}) if result['OK'] else result
