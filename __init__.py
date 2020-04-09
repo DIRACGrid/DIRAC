8b3380b8abe1f42ac8272af1c9f134b8707f5d7b
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2020-04-09 10:09:04 +0200

    v7r1-pre23 notes and tags

--- __init__.py
+++ __init__.py
@@ -93,7 +93,7 @@ import _strptime
 majorVersion = 7
 minorVersion = 1
 patchLevel = 0
-preVersion = 22
+preVersion = 23
 
 version = "v%sr%s" % (majorVersion, minorVersion)
 buildVersion = "v%dr%d" % (majorVersion, minorVersion)
