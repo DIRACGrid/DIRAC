5e45468b18bb5739867c472c592bfcb8f510d1dd
Federico Stagni <federico.stagni@cern.ch>
2020-04-15 12:24:10 +0200

    expand+double-quote vars in tests/Integration/all*.sh files

--- tests/Integration/all_integration_client_tests.sh
+++ tests/Integration/all_integration_client_tests.sh
@@ -2,7 +2,7 @@
 #-------------------------------------------------------------------------------
 # A convenient way to run all the integration tests for client -> server interaction
 #
-# It supposes that DIRAC client is installed in $CLIENTINSTALLDIR
+# It supposes that DIRAC client is installed in "${CLIENTINSTALLDIR}"
 # and that there's a DIRAC server running with all the services running.
 #-------------------------------------------------------------------------------
 
@@ -10,70 +10,70 @@ echo -e '****************************************'
 echo -e '********' "client -> server tests" '********\n'
 
 echo -e "*** $(date -u)  Getting a non privileged user\n" 2>&1 | tee -a clientTestOutputs.txt
-dirac-proxy-init -C $SERVERINSTALLDIR/user/client.pem -K $SERVERINSTALLDIR/user/client.key $DEBUG 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+dirac-proxy-init -C "${SERVERINSTALLDIR}/user/client.pem" -K "${SERVERINSTALLDIR}/user/client.key" "${DEBUG}" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
 
 #-------------------------------------------------------------------------------#
 echo -e "*** $(date -u) **** Accounting TESTS ****\n"
-pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/AccountingSystem/Test_DataStoreClient.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
-pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/AccountingSystem/Test_ReportsClient.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+pytest "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/AccountingSystem/Test_DataStoreClient.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+pytest "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/AccountingSystem/Test_ReportsClient.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
 
 
 #-------------------------------------------------------------------------------#
 echo -e "*** $(date -u)  **** RMS TESTS ****\n"
 
 echo -e "*** $(date -u)  Getting a non privileged user\n" 2>&1 | tee -a clientTestOutputs.txt
-dirac-proxy-init -C $SERVERINSTALLDIR/user/client.pem -K $SERVERINSTALLDIR/user/client.key $DEBUG 2>&1 | tee -a clientTestOutputs.txt
+dirac-proxy-init -C "${SERVERINSTALLDIR}/user/client.pem" -K "${SERVERINSTALLDIR}/user/client.key" "${DEBUG}" 2>&1 | tee -a clientTestOutputs.txt
 
 echo -e "*** $(date -u)  Starting RMS Client test as a non privileged user\n" 2>&1 | tee -a clientTestOutputs.txt
-pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/RequestManagementSystem/Test_Client_Req.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+pytest "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/RequestManagementSystem/Test_Client_Req.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
 
 echo -e "*** $(date -u)  getting the prod role again\n" 2>&1 | tee -a clientTestOutputs.txt
-dirac-proxy-init -g prod -C $SERVERINSTALLDIR/user/client.pem -K $SERVERINSTALLDIR/user/client.key $DEBUG 2>&1 | tee -a clientTestOutputs.txt
+dirac-proxy-init -g prod -C "${SERVERINSTALLDIR}/user/client.pem" -K "${SERVERINSTALLDIR}/user/client.key" "${DEBUG}" 2>&1 | tee -a clientTestOutputs.txt
 echo -e "*** $(date -u)  Starting RMS Client test as an admin user\n" 2>&1 | tee -a clientTestOutputs.txt
-pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/RequestManagementSystem/Test_Client_Req.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+pytest "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/RequestManagementSystem/Test_Client_Req.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
 
 
 #-------------------------------------------------------------------------------#
 echo -e "*** $(date -u)  **** RSS TESTS ****\n"
-pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/ResourceStatusSystem/Test_ResourceManagement.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
-pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/ResourceStatusSystem/Test_ResourceStatus.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
-pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/ResourceStatusSystem/Test_SiteStatus.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
-pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/ResourceStatusSystem/Test_Publisher.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+pytest "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/ResourceStatusSystem/Test_ResourceManagement.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+pytest "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/ResourceStatusSystem/Test_ResourceStatus.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+pytest "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/ResourceStatusSystem/Test_SiteStatus.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+pytest "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/ResourceStatusSystem/Test_Publisher.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
 
 
 #-------------------------------------------------------------------------------#
 echo -e "*** $(date -u)  **** WMS TESTS ****\n"
-# pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/WorkloadManagementSystem/Test_PilotsLoggingClient.py 2>&1 | tee -a clientTestOutputs.txt
-python $CLIENTINSTALLDIR/DIRAC/tests/Integration/WorkloadManagementSystem/Test_SandboxStoreClient.py $WORKSPACE/TestCode/DIRAC/tests/Integration/WorkloadManagementSystem/sb.cfg 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
-pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/WorkloadManagementSystem/Test_JobWrapper.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
-pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/WorkloadManagementSystem/Test_PilotsClient.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+# pytest "${CLIENTINSTALLDIR}"/DIRAC/tests/Integration/WorkloadManagementSystem/Test_PilotsLoggingClient.py 2>&1 | tee -a clientTestOutputs.txt
+python "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/WorkloadManagementSystem/Test_SandboxStoreClient.py" "$WORKSPACE/TestCode/DIRAC/tests/Integration/WorkloadManagementSystem/sb.cfg" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+pytest "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/WorkloadManagementSystem/Test_JobWrapper.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+pytest "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/WorkloadManagementSystem/Test_PilotsClient.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
 
 ## no real tests
-python $CLIENTINSTALLDIR/DIRAC/tests/Integration/WorkloadManagementSystem/createJobXMLDescriptions.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
-$CLIENTINSTALLDIR/DIRAC/tests/Integration/WorkloadManagementSystem/Test_dirac-jobexec.sh 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
-$CLIENTINSTALLDIR/DIRAC/tests/Integration/WorkloadManagementSystem/Test_TimeLeft.sh 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+python "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/WorkloadManagementSystem/createJobXMLDescriptions.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+"${CLIENTINSTALLDIR}/DIRAC/tests/Integration/WorkloadManagementSystem/Test_dirac-jobexec.sh" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+"${CLIENTINSTALLDIR}/DIRAC/tests/Integration/WorkloadManagementSystem/Test_TimeLeft.sh" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
 
 #-------------------------------------------------------------------------------#
 echo -e "*** $(date -u)  **** MONITORING TESTS ****\n"
-pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/Monitoring/Test_MonitoringSystem.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+pytest "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/Monitoring/Test_MonitoringSystem.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
 
 
 #-------------------------------------------------------------------------------#
 echo -e "*** $(date -u)  **** TS TESTS ****\n"
-pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/TransformationSystem/Test_Client_Transformation.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
-# pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/TransformationSystem/Test_TS_DFC_Catalog.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+pytest "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/TransformationSystem/Test_Client_Transformation.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+# pytest "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/TransformationSystem/Test_TS_DFC_Catalog.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
 
 
 #-------------------------------------------------------------------------------#
 echo -e "*** $(date -u)  **** PS TESTS ****\n"
-pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/ProductionSystem/Test_Client_Production.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
-pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/ProductionSystem/Test_Client_TS_Prod.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+pytest "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/ProductionSystem/Test_Client_Production.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+pytest "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/ProductionSystem/Test_Client_TS_Prod.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
 
 #-------------------------------------------------------------------------------#
 echo -e "*** $(date -u) **** DataManager TESTS ****\n"
 
 echo -e "*** $(date -u)  Getting a privileged user\n" 2>&1 | tee -a clientTestOutputs.txt
-dirac-proxy-init -g jenkins_fcadmin -C $SERVERINSTALLDIR/user/client.pem -K $SERVERINSTALLDIR/user/client.key $DEBUG 2>&1 | tee -a clientTestOutputs.txt
+dirac-proxy-init -g jenkins_fcadmin -C "${SERVERINSTALLDIR}/user/client.pem" -K "${SERVERINSTALLDIR}/user/client.key" "${DEBUG}" 2>&1 | tee -a clientTestOutputs.txt
 
 cat >> dataManager_create_folders <<EOF
 
@@ -89,9 +89,9 @@ EOF
 dirac-dms-filecatalog-cli -f FileCatalog < dataManager_create_folders
 
 echo -e "*** $(date -u)  Getting a non privileged user\n" 2>&1 | tee -a clientTestOutputs.txt
-dirac-proxy-init -g jenkins_user -C $SERVERINSTALLDIR/user/client.pem -K $SERVERINSTALLDIR/user/client.key $DEBUG 2>&1 | tee -a clientTestOutputs.txt
+dirac-proxy-init -g jenkins_user -C "${SERVERINSTALLDIR}/user/client.pem" -K "${SERVERINSTALLDIR}/user/client.key" "${DEBUG}" 2>&1 | tee -a clientTestOutputs.txt
 
-pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/DataManagementSystem/Test_DataManager.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+pytest "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/DataManagementSystem/Test_DataManager.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
 
 #-------------------------------------------------------------------------------#
 # MultiVO File Catalog tests are configured to use MultiVOFileCatalog module with a separate DB.
@@ -100,15 +100,15 @@ pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/DataManagementSystem/Test_DataM
 
 # need the dirac_admin user, so we can make a change in the CS
 echo -e "*** $(date -u)  Getting a dirac_admin user in order to modify the configuration \n" 2>&1 | tee -a clientTestOutputs.txt
-if ! dirac-proxy-init -g dirac_admin -C "$SERVERINSTALLDIR/user/client.pem" -K "$SERVERINSTALLDIR/user/client.key" "$DEBUG" --rfc; then
+if ! dirac-proxy-init -g dirac_admin -C "${SERVERINSTALLDIR}/user/client.pem" -K "${SERVERINSTALLDIR}/user/client.key" "${DEBUG}" --rfc; then
   echo 'ERROR: dirac-proxy-init for dirac_admin failed'
   exit 1
 fi
-python $CLIENTINSTALLDIR/DIRAC/tests/Jenkins/dirac-cfg-update-filecatalog.py  2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+python "${CLIENTINSTALLDIR}/DIRAC/tests/Jenkins/dirac-cfg-update-filecatalog.py"  2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
 # now back to a normal user
-dirac-proxy-init -g jenkins_user -C $SERVERINSTALLDIR/user/client.pem -K $SERVERINSTALLDIR/user/client.key $DEBUG 2>&1 | tee -a clientTestOutputs.txt
+dirac-proxy-init -g jenkins_user -C "${SERVERINSTALLDIR}/user/client.pem" -K "${SERVERINSTALLDIR}/user/client.key" "${DEBUG}" 2>&1 | tee -a clientTestOutputs.txt
 echo -e "*** $(date -u) **** MultiVO User Metadata TESTS ****\n"
-python -m pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/DataManagementSystem/Test_UserMetadata.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+python -m pytest "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/DataManagementSystem/Test_UserMetadata.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
 
 echo -e "*** $(date -u) **** S3 TESTS ****\n"
-pytest $CLIENTINSTALLDIR/DIRAC/tests/Integration/Resources/Storage/Test_Resources_S3.py 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
+pytest "${CLIENTINSTALLDIR}/DIRAC/tests/Integration/Resources/Storage/Test_Resources_S3.py" 2>&1 | tee -a clientTestOutputs.txt; (( ERR |= "${?}" ))
--- tests/Integration/all_integration_server_tests.sh
+++ tests/Integration/all_integration_server_tests.sh
@@ -2,7 +2,7 @@
 #-------------------------------------------------------------------------------
 # A convenient way to run all the integration tests for servers
 #
-# It supposes that DIRAC is installed in $SERVERINSTALLDIR
+# It supposes that DIRAC is installed in ${SERVERINSTALLDIR}
 #-------------------------------------------------------------------------------
 
 echo -e '****************************************'
@@ -10,85 +10,85 @@ echo -e '*******' "integration server tests" '*******\n'
 
 #-------------------------------------------------------------------------------#
 echo -e "*** $(date -u) **** Configuration TESTS ****\n"
-pytest $SERVERINSTALLDIR/DIRAC/tests/Integration/ConfigurationSystem/Test_Helpers.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
+pytest "${SERVERINSTALLDIR}/DIRAC/tests/Integration/ConfigurationSystem/Test_Helpers.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
 
 #-------------------------------------------------------------------------------#
 echo -e "*** $(date -u) **** Core TESTS ****\n"
-pytest $SERVERINSTALLDIR/DIRAC/tests/Integration/Core/Test_ElasticsearchDB.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
-pytest $SERVERINSTALLDIR/DIRAC/tests/Integration/Core/Test_MySQLDB.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
+pytest "${SERVERINSTALLDIR}/DIRAC/tests/Integration/Core/Test_ElasticsearchDB.py 2>&1" | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
+pytest "${SERVERINSTALLDIR}/DIRAC/tests/Integration/Core/Test_MySQLDB.py 2>&1" | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
 
 #-------------------------------------------------------------------------------#
 echo -e "*** $(date -u) **** FRAMEWORK TESTS (partially skipped) ****\n"
-pytest $SERVERINSTALLDIR/DIRAC/tests/Integration/Framework/Test_InstalledComponentsDB.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
-python $SERVERINSTALLDIR/DIRAC/tests/Integration/Framework/Test_ProxyDB.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
-#pytest $SERVERINSTALLDIR/DIRAC/tests/Integration/Framework/Test_LoggingDB.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
+pytest "${SERVERINSTALLDIR}/DIRAC/tests/Integration/Framework/Test_InstalledComponentsDB.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
+python "${SERVERINSTALLDIR}/DIRAC/tests/Integration/Framework/Test_ProxyDB.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
+#pytest ${SERVERINSTALLDIR}/DIRAC/tests/Integration/Framework/Test_LoggingDB.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
 
 
 #-------------------------------------------------------------------------------#
 echo -e "*** $(date -u)  **** RSS TESTS ****\n"
-pytest $SERVERINSTALLDIR/DIRAC/tests/Integration/ResourceStatusSystem/Test_FullChain.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
+pytest "${SERVERINSTALLDIR}/DIRAC/tests/Integration/ResourceStatusSystem/Test_FullChain.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
 
 #-------------------------------------------------------------------------------#
 echo -e "*** $(date -u)  **** WMS TESTS ****\n"
-pytest $SERVERINSTALLDIR/DIRAC/tests/Integration/WorkloadManagementSystem/Test_JobDB.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
-pytest $SERVERINSTALLDIR/DIRAC/tests/Integration/WorkloadManagementSystem/Test_JobLoggingDB.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
-pytest $SERVERINSTALLDIR/DIRAC/tests/Integration/WorkloadManagementSystem/Test_TaskQueueDB.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
-pytest $SERVERINSTALLDIR/DIRAC/tests/Integration/WorkloadManagementSystem/Test_ElasticJobDB.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
-pytest $SERVERINSTALLDIR/DIRAC/tests/Integration/WorkloadManagementSystem/Test_JobParameters_MySQLandES.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
-python $SERVERINSTALLDIR/DIRAC/tests/Integration/WorkloadManagementSystem/Test_Client_WMS.py $WORKSPACE/TestCode/DIRAC/tests/Integration/WorkloadManagementSystem/sb.cfg 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
+pytest "${SERVERINSTALLDIR}/DIRAC/tests/Integration/WorkloadManagementSystem/Test_JobDB.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
+pytest "${SERVERINSTALLDIR}/DIRAC/tests/Integration/WorkloadManagementSystem/Test_JobLoggingDB.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
+pytest "${SERVERINSTALLDIR}/DIRAC/tests/Integration/WorkloadManagementSystem/Test_TaskQueueDB.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
+pytest "${SERVERINSTALLDIR}/DIRAC/tests/Integration/WorkloadManagementSystem/Test_ElasticJobDB.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
+pytest "${SERVERINSTALLDIR}/DIRAC/tests/Integration/WorkloadManagementSystem/Test_JobParameters_MySQLandES.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
+python "${SERVERINSTALLDIR}/DIRAC/tests/Integration/WorkloadManagementSystem/Test_Client_WMS.py" "${WORKSPACE}/TestCode/DIRAC/tests/Integration/WorkloadManagementSystem/sb.cfg" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
 
 #-------------------------------------------------------------------------------#
 echo -e "*** $(date -u)  **** DMS TESTS ****\n"
 ## DFC
-pytest $SERVERINSTALLDIR/DIRAC/tests/Integration/DataManagementSystem/Test_DataIntegrityDB.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
+pytest "${SERVERINSTALLDIR}/DIRAC/tests/Integration/DataManagementSystem/Test_DataIntegrityDB.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
 
-echo "Test DFC DB" 2>&1 | tee -a $SERVER_TEST_OUTPUT
-python $SERVERINSTALLDIR/DIRAC/tests/Integration/DataManagementSystem/Test_FileCatalogDB.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
+echo "Test DFC DB" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"
+python "${SERVERINSTALLDIR}/DIRAC/tests/Integration/DataManagementSystem/Test_FileCatalogDB.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
 
-echo -e "*** $(date -u)  Reinitialize the DFC DB\n" 2>&1 | tee -a $SERVER_TEST_OUTPUT
-diracDFCDB 2>&1 | tee -a $SERVER_TEST_OUTPUT
+echo -e "*** $(date -u)  Reinitialize the DFC DB\n" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"
+diracDFCDB 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"
 
-echo -e "*** $(date -u)  Run the DFC client tests as user without admin privileges" 2>&1 | tee -a $SERVER_TEST_OUTPUT
-echo -e "*** $(date -u)  Getting a non privileged user\n" 2>&1 | tee -a $SERVER_TEST_OUTPUT
-dirac-proxy-init -C $WORKSPACE/ServerInstallDIR/user/client.pem -K $WORKSPACE/ServerInstallDIR/user/client.key $DEBUG
-python $SERVERINSTALLDIR/DIRAC/tests/Integration/DataManagementSystem/Test_Client_DFC.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
-diracDFCDB 2>&1 | tee -a $SERVER_TEST_OUTPUT
-python $SERVERINSTALLDIR/DIRAC/tests/Integration/DataManagementSystem/Test_FileCatalogDB.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
+echo -e "*** $(date -u)  Run the DFC client tests as user without admin privileges" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"
+echo -e "*** $(date -u)  Getting a non privileged user\n" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"
+dirac-proxy-init -C "${WORKSPACE}/ServerInstallDIR/user/client.pem" -K "${WORKSPACE}/ServerInstallDIR/user/client.key" "${DEBUG}"
+python "${SERVERINSTALLDIR}/DIRAC/tests/Integration/DataManagementSystem/Test_Client_DFC.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
+diracDFCDB 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"
+python "${SERVERINSTALLDIR}/DIRAC/tests/Integration/DataManagementSystem/Test_FileCatalogDB.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
 
-echo "Reinitialize the DFC DB" 2>&1 | tee -a $SERVER_TEST_OUTPUT
-diracDFCDB 2>&1 | tee -a $SERVER_TEST_OUTPUT
+echo 'Reinitialize the DFC DB' 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"
+diracDFCDB 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"
 
-echo -e "*** $(date -u)  Restart the DFC service\n" 2>&1 | tee -a $SERVER_TEST_OUTPUT
-dirac-restart-component DataManagement FileCatalog $DEBUG 2>&1 | tee -a $SERVER_TEST_OUTPUT
+echo -e "*** $(date -u)  Restart the DFC service\n" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"
+dirac-restart-component DataManagement FileCatalog "${DEBUG}" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"
 
-echo -e "*** $(date -u)  Run it with the admin privileges" 2>&1 | tee -a $SERVER_TEST_OUTPUT
-echo -e "*** $(date -u)  getting the prod role again\n" 2>&1 | tee -a $SERVER_TEST_OUTPUT
-dirac-proxy-init -g prod -C $WORKSPACE/ServerInstallDIR/user/client.pem -K $WORKSPACE/ServerInstallDIR/user/client.key $DEBUG 2>&1 | tee -a $SERVER_TEST_OUTPUT
-python $SERVERINSTALLDIR/DIRAC/tests/Integration/DataManagementSystem/Test_Client_DFC.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
-diracDFCDB 2>&1 | tee -a $SERVER_TEST_OUTPUT
-python $SERVERINSTALLDIR/DIRAC/tests/Integration/DataManagementSystem/Test_FileCatalogDB.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
+echo -e "*** $(date -u)  Run it with the admin privileges" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"
+echo -e "*** $(date -u)  getting the prod role again\n" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"
+dirac-proxy-init -g prod -C "${WORKSPACE}/ServerInstallDIR/user/client.pem" -K "${WORKSPACE}/ServerInstallDIR/user/client.key" "${DEBUG}" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"
+python "${SERVERINSTALLDIR}/DIRAC/tests/Integration/DataManagementSystem/Test_Client_DFC.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
+diracDFCDB 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"
+python "${SERVERINSTALLDIR}/DIRAC/tests/Integration/DataManagementSystem/Test_FileCatalogDB.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
 
 
 #-------------------------------------------------------------------------------#
 echo -e "*** $(date -u)  **** FTS TESTS ****\n"
 # I know, it says Client, but it also instaciates a DB, so it needs to be here
-pytest $SERVERINSTALLDIR/DIRAC/tests/Integration/DataManagementSystem/Test_Client_FTS3.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
+pytest "${SERVERINSTALLDIR}/DIRAC/tests/Integration/DataManagementSystem/Test_Client_FTS3.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
 
 #-------------------------------------------------------------------------------#
 echo -e "*** $(date -u)  **** RMS TESTS ****\n"
-pytest $SERVERINSTALLDIR/DIRAC/tests/Integration/RequestManagementSystem/Test_ReqDB.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
+pytest "${SERVERINSTALLDIR}/DIRAC/tests/Integration/RequestManagementSystem/Test_ReqDB.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
 
 #-------------------------------------------------------------------------------#
 echo -e "*** $(date -u)  **** MONITORING TESTS ****\n"
-pytest $SERVERINSTALLDIR/DIRAC/tests/Integration/Monitoring/Test_MonitoringReporter.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
+pytest "${SERVERINSTALLDIR}/DIRAC/tests/Integration/Monitoring/Test_MonitoringReporter.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
 
 
 #-------------------------------------------------------------------------------#
 echo -e "*** $(date -u)  **** Resources TESTS ****\n"
 
-python $SERVERINSTALLDIR/DIRAC/tests/Integration/Resources/Storage/Test_Resources_GFAL2StorageBase.py ProductionSandboxSE 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
-python $SERVERINSTALLDIR/DIRAC/tests/Integration/Resources/ProxyProvider/Test_DIRACCAProxyProvider.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
+python "${SERVERINSTALLDIR}/DIRAC/tests/Integration/Resources/Storage/Test_Resources_GFAL2StorageBase.py ProductionSandboxSE" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
+python "${SERVERINSTALLDIR}/DIRAC/tests/Integration/Resources/ProxyProvider/Test_DIRACCAProxyProvider.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
 
 # Can only run if there's a Stomp MQ local...
 # TODO Enable
-# pytest $SERVERINSTALLDIR/DIRAC/tests/Integration/Resources/MessageQueue/Test_ActiveClose.py 2>&1 | tee -a $SERVER_TEST_OUTPUT; (( ERR |= "${?}" ))
+# pytest "${SERVERINSTALLDIR}/DIRAC/tests/Integration/Resources/MessageQueue/Test_ActiveClose.py" 2>&1 | tee -a "${SERVER_TEST_OUTPUT}"; (( ERR |= "${?}" ))
