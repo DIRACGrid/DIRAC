25ec25294eab63a2fe893789f32a8765c04b3bf7
Federico Stagni <federico.stagni@cern.ch>
2020-05-07 17:53:39 +0200

    not basestring

--- WorkloadManagementSystem/Utilities/PilotWrapper.py
+++ WorkloadManagementSystem/Utilities/PilotWrapper.py
@@ -78,16 +78,16 @@ def pilotWrapperScript(pilotFilesCompressedEncodedDict=None,
                         the proxy can be part of this, and of course the pilot files
      :type pilotFilesCompressedEncodedDict: dict
      :param pilotOptions: options with which to start the pilot
-     :type pilotOptions: basestring
+     :type pilotOptions: string
      :param pilotExecDir: pilot execution directory
-     :type pilotExecDir: basestring
+     :type pilotExecDir: string
      :param envVariables: dictionary of environment variables
      :type envVariables: dict
      :param location: location where to get the pilot files
-     :type location: basestring
+     :type location: string
 
      :returns: content of the pilot wrapper
-     :rtype: basestring
+     :rtype: string
   """
 
   if pilotFilesCompressedEncodedDict is None:
@@ -289,12 +289,12 @@ def _writePilotWrapperFile(workingDirectory=None, localPilot=''):
   """ write the localPilot string to a file, rurn the file name
 
      :param workingDirectory: the directory where to store the pilot wrapper file
-     :type workingDirectory: basestring
+     :type workingDirectory: string
      :param localPilot: content of the pilot wrapper
-     :type localPilot: basestring
+     :type localPilot: string
 
      :returns: file name of the pilot wrapper
-     :rtype: basestring
+     :rtype: string
   """
 
   fd, name = tempfile.mkstemp(suffix='_pilotwrapper.py', prefix='DIRAC_', dir=workingDirectory)
