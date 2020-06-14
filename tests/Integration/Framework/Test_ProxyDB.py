b79412efe96a8f1f32e01a43cf863f9e52b942f2
TaykYoku <yokutayk@gmail.com>
2020-06-14 13:19:54 +0200

    fix bug

--- tests/Integration/Framework/Test_ProxyDB.py
+++ tests/Integration/Framework/Test_ProxyDB.py
@@ -373,8 +373,8 @@ class testDB(ProxyDBTestCase):
       self.assertTrue(result['OK'], '\n' + result.get('Message', 'Error message is absent.'))
       usersList = []
       for line in result['Value']:
-        if line['Name'] in ['user', 'user_2', 'user_3']:
-          usersList.append(line['Name'])
+        if line['UserName'] in ['user', 'user_2', 'user_3']:
+          usersList.append(line['UserName'])
       self.assertEqual(set(expect), set(usersList), str(usersList) + ', when expected ' + str(expect))
 
   def test_purgeExpiredProxies(self):
