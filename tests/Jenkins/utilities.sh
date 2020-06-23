d021d5d9fbc6b1a13b98a4638779484c49637150
Christophe Haen <christophe.haen@cern.ch>
2020-06-23 18:55:28 +0200

    Tests: ignore Tornado services when doing installation in CI

--- tests/Jenkins/utilities.sh
+++ tests/Jenkins/utilities.sh
@@ -752,7 +752,8 @@ diracAddSite() {
 diracServices(){
   echo '==> [diracServices]'
 
-  local services=$(cut -d '.' -f 1 < services | grep -v PilotsLogging | grep -v StorageElementHandler | grep -v ^ConfigurationSystem | grep -v Plotting | grep -v RAWIntegrity | grep -v RunDBInterface | grep -v ComponentMonitoring | sed 's/System / /g' | sed 's/Handler//g' | sed 's/ /\//g')
+  # Ignore tornado services
+  local services=$(cut -d '.' -f 1 < services | grep -v Tornado |  grep -v PilotsLogging | grep -v StorageElementHandler | grep -v ^ConfigurationSystem | grep -v Plotting | grep -v RAWIntegrity | grep -v RunDBInterface | grep -v ComponentMonitoring | sed 's/System / /g' | sed 's/Handler//g' | sed 's/ /\//g')
 
   # group proxy, will be uploaded explicitly
   #  echo '==> getting/uploading proxy for prod'
@@ -802,7 +803,8 @@ diracUninstallServices(){
 
   findServices
 
-  local services=$(cut -d '.' -f 1 <services | grep -v IRODSStorageElementHandler | grep -v ^ConfigurationSystem | grep -v Plotting | grep -v RAWIntegrity | grep -v RunDBInterface | grep -v ComponentMonitoring | sed 's/System / /g' | sed 's/Handler//g' | sed 's/ /\//g')
+  # Ignore tornado services
+  local services=$(cut -d '.' -f 1 <services | grep -v IRODSStorageElementHandler | grep -v ^ConfigurationSystem | grep -v Plotting | grep -v RAWIntegrity | grep -v RunDBInterface | grep -v ComponentMonitoring | grep -v Tornado | sed 's/System / /g' | sed 's/Handler//g' | sed 's/ /\//g')
 
   # group proxy, will be uploaded explicitly
   #  echo '==> getting/uploading proxy for prod'
