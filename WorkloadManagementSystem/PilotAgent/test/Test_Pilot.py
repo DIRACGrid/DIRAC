dc3f58165e79bb844c7c83fe04f0d4d68a1fecfb
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2020-06-25 11:14:17 +0200

    autopep8

--- WorkloadManagementSystem/PilotAgent/test/Test_Pilot.py
+++ WorkloadManagementSystem/PilotAgent/test/Test_Pilot.py
@@ -7,26 +7,28 @@ import os
 import sys
 
 if "--no-cov" in sys.argv:
-  del sys.argv[sys.argv.index( '--no-cov' )]
+  del sys.argv[sys.argv.index('--no-cov')]
 
 from DIRAC.WorkloadManagementSystem.PilotAgent.pilotTools import PilotParams, CommandBase
 from DIRAC.WorkloadManagementSystem.PilotAgent.pilotCommands import GetPilotVersion
 
+
 def test_GetPilotVersion():
   pp = PilotParams()
   # Now defining a local file for test, and all the necessary parameters
-  fp = open( 'pilot.json', 'w' )
-  json.dump( { 'TestSetup': { 'Version': ['v1r1', 'v2r2'] } }, fp )
+  fp = open('pilot.json', 'w')
+  json.dump({'TestSetup': {'Version': ['v1r1', 'v2r2']}}, fp)
   fp.close()
   pp.setup = 'TestSetup'
   pp.pilotCFGFileLocation = 'file://%s' % os.getcwd()
-  gpv = GetPilotVersion( pp )
+  gpv = GetPilotVersion(pp)
   result = gpv.execute()
   assert result is None
   assert gpv.pp.releaseVersion == 'v1r1'
 
+
 def test_commandBase():
   pp = PilotParams()
-  cb = CommandBase( pp )
-  returnCode, _outputData = cb.executeAndGetOutput( "ls" )
+  cb = CommandBase(pp)
+  returnCode, _outputData = cb.executeAndGetOutput("ls")
   assert returnCode == 0
