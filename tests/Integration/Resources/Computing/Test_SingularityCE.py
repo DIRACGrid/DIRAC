23f509438588876f108d0efc42c66b6be699b665
Federico Stagni <federico.stagni@cern.ch>
2020-08-04 17:22:38 +0200

    added SIngularityCE integration test

---
+++ tests/Integration/Resources/Computing/Test_SingularityCE.py
@@ -0,0 +1,29 @@
+""" This integration test is for "Inner" Computing Element SingularityComputingElement
+"""
+
+from DIRAC.tests.Utilities.utils import find_all
+from DIRAC import gLogger
+
+
+# sut
+from DIRAC.Resources.Computing.SingularityComputingElement import SingularityComputingElement
+
+gLogger.setLevel('DEBUG')
+
+# executable file
+executableFile = 'hello.sh'
+
+
+def test_submit():
+  jobDesc = {"jobID": 123,
+	     "jobParams": {},
+	     "resourceParams": {},
+	     "optimizerParams": {}}
+
+  ce = SingularityComputingElement('InProcess')
+
+  res = ce.submitJob(find_all(executableFile, 'tests')[0],
+		     jobDesc=jobDesc,
+		     log=gLogger.getSubLogger('job_log'),
+		     logLevel='DEBUG')
+  assert res['OK'] is True
