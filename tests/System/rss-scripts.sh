3071fc194dfcf9bf95f349080ee21076acfa6e8d
fstagni <federico.stagni@cern.ch>
2020-08-06 16:02:16 +0200

    fix syntax

--- tests/System/rss-scripts.sh
+++ tests/System/rss-scripts.sh
@@ -8,7 +8,7 @@
 declare -a commands=(
 'dirac-admin-get-banned-sites'
 'dirac-admin-get-site-mask'
-"test $( dirac-admin-site-info LCG.CERN.cern | wc -l ) = 7"
+'test $( dirac-admin-site-info LCG.CERN.cern | wc -l ) = 7'
 'dirac-dms-show-se-status'
 'dirac-rss-list-status --element=Site'
 'dirac-rss-list-status --element=Resource --name=RAL-SE'
@@ -17,17 +17,17 @@ declare -a commands=(
 'dirac-rss-list-status --name=test123 --element=Resource -dd'
 'dirac-rss-set-token --name=test123 --element=Resource --reason=RSStest --releaseToken -dd'
 'dirac-rss-query-db --name=test123 delete resource status -dd'
-"dirac-rss-query-dtcache --name=dtest123 --element=Site --downtimeID=4354354789 --severity=OUTAGE --description='just a test DT' add --startDate='2019-06-12 15:00:00' --endDate='2020-06-12 15:00:00' -dd"
-"test $( dirac-rss-query-dtcache --name=dtest123 select | grep dtest123 | wc -l ) = 1"
-"test $( dirac-rss-query-dtcache --name=dtest123 --element=Site --downtimeID=4354354789 delete -dd | grep 'successfully executed' | wc -l ) = 1"
-"test $( dirac-rss-query-dtcache --name=dtest123 select | grep 'request successfully executed' | grep 'number: 0' )"
+'dirac-rss-query-dtcache --name=dtest123 --element=Site --downtimeID=4354354789 --severity=OUTAGE --description="just a test DT" add --startDate="2019-06-12 15:00:00" --endDate="2020-06-12 15:00:00" -dd'
+'test $( dirac-rss-query-dtcache --name=dtest123 select | grep dtest123 | wc -l ) = 1'
+'test $( dirac-rss-query-dtcache --name=dtest123 --element=Site --downtimeID=4354354789 delete -dd | grep "successfully executed" | wc -l ) = 1'
+'test $( dirac-rss-query-dtcache --name=dtest123 select | grep "request successfully executed" | grep "number: 0" )'
 )
 
 for command in "${commands[@]}"
 do
   echo "************************************************"
   echo " "
-  if ! ${command}; then
+  if ! bash -c "${command}"; then
     echo "${command}" gives error
     exit 1
   fi
