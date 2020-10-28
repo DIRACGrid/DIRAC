59cc7893d5bb81656ac4f39df462df271a70112f
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2020-10-28 13:15:18 +0100

    v7r2-pre18 notes and tags

--- __init__.py
+++ __init__.py
@@ -94,7 +94,7 @@ import _strptime
 majorVersion = 7
 minorVersion = 2
 patchLevel = 0
-preVersion = 17
+preVersion = 18
 
 version = "v%sr%s" % (majorVersion, minorVersion)
 if patchLevel:
--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ scripts = glob.glob('%s/*/scripts/*.py' % base_dir)
 
 setup(
     name="DIRAC",
-    version="7.2.pre17",
+    version="7.2.pre18",
     url="https://github.com/DIRACGRID/DIRAC",
     license="GPLv3",
     package_dir=package_dir,
