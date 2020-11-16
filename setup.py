65dacbce6761ceede6f337b47218b49678f3d676
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2020-11-16 21:26:46 +0100

    v7r1p19 notes and tags

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
     Does not contain any dependency
 """
 
-import sys
 import os
 import glob
 # Actual setuptools
@@ -34,7 +33,7 @@ scripts = glob.glob('%s/*/scripts/*.py' % base_dir)
 
 setup(
     name="DIRAC",
-    version="7.1.18",
+    version="7.1.19",
     url="https://github.com/DIRACGRID/DIRAC",
     license="GPLv3",
     package_dir=package_dir,
