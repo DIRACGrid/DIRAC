6625209f4ad95451e092f4ab7135b18e54f4ce26
Philippe Charpentier <Philippe.Charpentier@cern.ch>
2020-11-25 11:31:10 +0100

    remove test of obsolete method

--- tests/Integration/WorkloadManagementSystem/Test_Client_WMS.py
+++ tests/Integration/WorkloadManagementSystem/Test_Client_WMS.py
@@ -292,8 +292,6 @@ class JobMonitoring(TestWMSTestCase):
     self.assertTrue(res['OK'], res.get('Message'))
     res = jobMonitor.getAtticJobParameters(jobID)
     self.assertTrue(res['OK'], res.get('Message'))
-    res = jobStateUpdate.setJobsStatus([jobID], 'Done', 'MinorStatus', 'Unknown')
-    self.assertTrue(res['OK'], res.get('Message'))
     res = jobMonitor.getJobSummary(jobID)
     self.assertTrue(res['OK'], res.get('Message'))
     self.assertEqual(res['Value']['Status'], 'Done', msg="Got %s" % str(res['Value']['Status']))
