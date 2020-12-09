b7841cda8a295cb1557134c552e15c749c1139b3
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2020-12-09 12:10:13 +0100

    v7r2-pre24 notes and tags

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ scripts = glob.glob('%s/*/scripts/*.py' % base_dir)
 
 setup(
     name="DIRAC",
-    version="7.2.pre23",
+    version="7.2.pre24",
     url="https://github.com/DIRACGRID/DIRAC",
     license="GPLv3",
     package_dir=package_dir,
