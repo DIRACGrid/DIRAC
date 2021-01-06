06729c489636a26d364a46e2148b485ccc505616
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2021-01-06 19:50:45 +0100

    v7r2-pre27 notes and tags

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ scripts = glob.glob('%s/*/scripts/*.py' % base_dir)
 
 setup(
     name="DIRAC",
-    version="7.2.pre26",
+    version="7.2.pre27",
     url="https://github.com/DIRACGRID/DIRAC",
     license="GPLv3",
     package_dir=package_dir,
