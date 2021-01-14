88c61502d5e5183ed2cca692f859dd5ec1210411
Chris Burr <christopher.burr@cern.ch>
2021-01-14 10:08:34 +0100

    PY3: Convert tornado scripts to be setup.cfg compatible

--- src/DIRAC/Core/Tornado/scripts/tornado_start_CS.py
+++ src/DIRAC/Core/Tornado/scripts/tornado_start_CS.py
@@ -12,37 +12,46 @@ from __future__ import print_function
 
 __RCSID__ = "$Id$"
 
-# Must be define BEFORE any dirac import
 import os
 import sys
-os.environ['DIRAC_USE_TORNADO_IOLOOP'] = "True"
-
-from DIRAC.ConfigurationSystem.Client.ConfigurationData import gConfigurationData
-from DIRAC.ConfigurationSystem.Client.LocalConfiguration import LocalConfiguration
-from DIRAC.ConfigurationSystem.private.Refresher import gRefresher
-from DIRAC.Core.Utilities.DErrno import includeExtensionErrors
-from DIRAC.Core.Tornado.Server.TornadoServer import TornadoServer
-from DIRAC.FrameworkSystem.Client.Logger import gLogger
-
-if gConfigurationData.isMaster():
-  gRefresher.disable()
-
-localCfg = LocalConfiguration()
-localCfg.addMandatoryEntry("/DIRAC/Setup")
-localCfg.addDefaultEntry("/DIRAC/Security/UseServerCertificate", "yes")
-localCfg.addDefaultEntry("LogLevel", "INFO")
-localCfg.addDefaultEntry("LogColor", True)
-resultDict = localCfg.loadUserData()
-if not resultDict['OK']:
-  gLogger.initialize("Tornado-CS", "/")
-  gLogger.error("There were errors when loading configuration", resultDict['Message'])
-  sys.exit(1)
-
-includeExtensionErrors()
+from DIRAC.Core.Utilities.DIRACScript import DIRACScript
 
 
-gLogger.initialize('Tornado-CS', "/")
+@DIRACScript()
+def main():
+  # Must be defined BEFORE any dirac import
+  os.environ['DIRAC_USE_TORNADO_IOLOOP'] = "True"
+
+  from DIRAC.ConfigurationSystem.Client.ConfigurationData import gConfigurationData
+  from DIRAC.ConfigurationSystem.Client.LocalConfiguration import LocalConfiguration
+  from DIRAC.ConfigurationSystem.private.Refresher import gRefresher
+  from DIRAC.Core.Utilities.DErrno import includeExtensionErrors
+  from DIRAC.Core.Tornado.Server.TornadoServer import TornadoServer
+  from DIRAC.FrameworkSystem.Client.Logger import gLogger
+
+  if gConfigurationData.isMaster():
+    gRefresher.disable()
+
+  localCfg = LocalConfiguration()
+  localCfg.addMandatoryEntry("/DIRAC/Setup")
+  localCfg.addDefaultEntry("/DIRAC/Security/UseServerCertificate", "yes")
+  localCfg.addDefaultEntry("LogLevel", "INFO")
+  localCfg.addDefaultEntry("LogColor", True)
+  resultDict = localCfg.loadUserData()
+  if not resultDict['OK']:
+    gLogger.initialize("Tornado-CS", "/")
+    gLogger.error("There were errors when loading configuration", resultDict['Message'])
+    sys.exit(1)
+
+  includeExtensionErrors()
 
 
-serverToLaunch = TornadoServer(services='Configuration/Server')
-serverToLaunch.startTornado()
+  gLogger.initialize('Tornado-CS', "/")
+
+
+  serverToLaunch = TornadoServer(services='Configuration/Server')
+  serverToLaunch.startTornado()
+
+
+if __name__ == "__main__":
+  main()
--- src/DIRAC/Core/Tornado/scripts/tornado_start_all.py
+++ src/DIRAC/Core/Tornado/scripts/tornado_start_all.py
@@ -15,44 +15,47 @@ __RCSID__ = "$Id$"
 # Must be define BEFORE any dirac import
 import os
 import sys
-os.environ['DIRAC_USE_TORNADO_IOLOOP'] = "True"
+from DIRAC.Core.Utilities.DIRACScript import DIRACScript
 
 
-from DIRAC import gConfig
-from DIRAC.ConfigurationSystem.Client import PathFinder
-from DIRAC.ConfigurationSystem.Client.ConfigurationData import gConfigurationData
-from DIRAC.ConfigurationSystem.Client.LocalConfiguration import LocalConfiguration
-from DIRAC.Core.Tornado.Server.TornadoServer import TornadoServer
-from DIRAC.Core.Utilities.DErrno import includeExtensionErrors
-from DIRAC.FrameworkSystem.Client.Logger import gLogger
+@DIRACScript()
+def main():
+  os.environ['DIRAC_USE_TORNADO_IOLOOP'] = "True"
+
+  from DIRAC import gConfig
+  from DIRAC.ConfigurationSystem.Client import PathFinder
+  from DIRAC.ConfigurationSystem.Client.ConfigurationData import gConfigurationData
+  from DIRAC.ConfigurationSystem.Client.LocalConfiguration import LocalConfiguration
+  from DIRAC.Core.Tornado.Server.TornadoServer import TornadoServer
+  from DIRAC.Core.Utilities.DErrno import includeExtensionErrors
+  from DIRAC.FrameworkSystem.Client.Logger import gLogger
+
+  # We check if there is no configuration server started as master
+  # If you want to start a master CS you should use Configuration_Server.cfg and
+  # use tornado-start-CS.py
+  key = '/Systems/Configuration/%s/Services/Server/Protocol' % PathFinder.getSystemInstance('Configuration')
+  if gConfigurationData.isMaster() and gConfig.getValue(key, 'dips').lower() == 'https':
+    gLogger.fatal("You can't run the CS and services in the same server!")
+    sys.exit(0)
+
+  localCfg = LocalConfiguration()
+  localCfg.addMandatoryEntry("/DIRAC/Setup")
+  localCfg.addDefaultEntry("/DIRAC/Security/UseServerCertificate", "yes")
+  localCfg.addDefaultEntry("LogLevel", "INFO")
+  localCfg.addDefaultEntry("LogColor", True)
+  resultDict = localCfg.loadUserData()
+  if not resultDict['OK']:
+    gLogger.initialize("Tornado", "/")
+    gLogger.error("There were errors when loading configuration", resultDict['Message'])
+    sys.exit(1)
+
+  includeExtensionErrors()
+
+  gLogger.initialize('Tornado', "/")
+
+  serverToLaunch = TornadoServer()
+  serverToLaunch.startTornado()
 
 
-# We check if there is no configuration server started as master
-# If you want to start a master CS you should use Configuration_Server.cfg and
-# use tornado-start-CS.py
-if gConfigurationData.isMaster() and gConfig.getValue(
-    '/Systems/Configuration/%s/Services/Server/Protocol' %
-    PathFinder.getSystemInstance('Configuration'),
-   'dips').lower() == 'https':
-  gLogger.fatal("You can't run the CS and services in the same server!")
-  sys.exit(0)
-
-localCfg = LocalConfiguration()
-localCfg.addMandatoryEntry("/DIRAC/Setup")
-localCfg.addDefaultEntry("/DIRAC/Security/UseServerCertificate", "yes")
-localCfg.addDefaultEntry("LogLevel", "INFO")
-localCfg.addDefaultEntry("LogColor", True)
-resultDict = localCfg.loadUserData()
-if not resultDict['OK']:
-  gLogger.initialize("Tornado", "/")
-  gLogger.error("There were errors when loading configuration", resultDict['Message'])
-  sys.exit(1)
-
-includeExtensionErrors()
-
-
-gLogger.initialize('Tornado', "/")
-
-
-serverToLaunch = TornadoServer()
-serverToLaunch.startTornado()
+if __name__ == "__main__":
+  main()
