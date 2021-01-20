e1997049f6763b38de1916a68b516b0be3f533d8
fstagni <federico.stagni@cern.ch>
2021-01-20 18:16:41 +0100

    moving towards a single agent for WMS history

--- src/DIRAC/WorkloadManagementSystem/Agent/StatesAccountingAgent.py
+++ src/DIRAC/WorkloadManagementSystem/Agent/StatesAccountingAgent.py
@@ -1,34 +1,32 @@
-########################################################################
-# File :    StatesAccountingAgent.py
-# Author :  A.T.
-########################################################################
-
 """  StatesAccountingAgent sends periodically numbers of jobs in various states for various
      sites to the Monitoring system to create historical plots.
+
+.. literalinclude:: ../ConfigTemplate.cfg
+  :start-after: ##BEGIN StatesAccountingAgent
+  :end-before: ##END
+  :dedent: 2
+  :caption: StatesAccountingAgent options
 """
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
 __RCSID__ = "$Id$"
 
-
-from DIRAC import gConfig, S_OK
+from DIRAC import S_OK, S_ERROR
 from DIRAC.Core.Base.AgentModule import AgentModule
-from DIRAC.WorkloadManagementSystem.DB.JobDB import JobDB
+from DIRAC.Core.Utilities import Time
 from DIRAC.AccountingSystem.Client.Types.WMSHistory import WMSHistory
 from DIRAC.AccountingSystem.Client.DataStoreClient import DataStoreClient
-from DIRAC.Core.Utilities import Time
+from DIRAC.MonitoringSystem.Client.MonitoringReporter import MonitoringReporter
+from DIRAC.WorkloadManagementSystem.DB.JobDB import JobDB
 
 
 class StatesAccountingAgent(AgentModule):
-  """
-      The specific agents must provide the following methods:
-        - initialize() for initial settings
-        - beginExecution()
-        - execute() - the main method called in the agent cycle
-        - endExecution()
-        - finalize() - the graceful exit of the method, this one is usually used
-                   for the agent restart
+  """ Agent that every 15 minutes will report
+      to the AccountingDB (MySQL) or the Monitoring DB (ElasticSearch), or both,
+      a snapshot of the JobDB.
   """
 
   __summaryKeyFieldsMapping = ['Status',
@@ -37,23 +35,32 @@ class StatesAccountingAgent(AgentModule):
                                'UserGroup',
                                'JobGroup',
                                'JobType',
-                               ]
-  __summaryDefinedFields = [('ApplicationStatus', 'unset'), ('MinorStatus', 'unset')]
+			       'ApplicationStatus',
+			       'MinorStatus']
+  __summaryDefinedFields = [('ApplicationStatus', 'unset'),
+			    ('MinorStatus', 'unset')]
   __summaryValueFieldsMapping = ['Jobs',
-                                 'Reschedules',
-                                 ]
+				 'Reschedules']
   __renameFieldsMapping = {'JobType': 'JobSplitType'}
 
   def initialize(self):
-    """ Standard constructor
+    """ Standard initialization
     """
-    self.dsClients = {}
-    self.jobDB = JobDB()
-    self.retryOnce = False
-    self.retryValues = []
+    # This agent will always loop every 15 minutes
+    self.am_setOption("PollingTime", 900)
 
-    self.reportPeriod = 850
-    self.am_setOption("PollingTime", self.reportPeriod)
+    self.backends = self.am_getOption("Backends", "Accounting").replace(' ', '').split(',')
+    messageQueue = self.am_getOption("MessageQueue", "dirac.wmshistory")
+
+    self.datastores = {}  # For storing the clients to Accounting and Monitoring
+
+    if 'Accounting' in self.backends:
+      self.datastores['Accounting'] = DataStoreClient(retryGraceTime=900)
+    if 'Monitoring' in self.backends:
+      self.datastores['Monitoring'] = MonitoringReporter(
+	  monitoringType="WMSHistory",
+	  failoverQueueName=messageQueue)
+
     self.__jobDBFields = []
     for field in self.__summaryKeyFieldsMapping:
       if field == 'User':
@@ -66,76 +73,51 @@ class StatesAccountingAgent(AgentModule):
   def execute(self):
     """ Main execution method
     """
-    result = gConfig.getSections("/DIRAC/Setups")
-    if not result['OK']:
-      return result
-    validSetups = result['Value']
-    self.log.info("Valid setups for this cycle are %s" % ", ".join(validSetups))
     # Get the WMS Snapshot!
-    result = self.jobDB.getSummarySnapshot(self.__jobDBFields)
+    result = JobDB().getSummarySnapshot(self.__jobDBFields)
     now = Time.dateTime()
     if not result['OK']:
       self.log.error("Can't get the JobDB summary", "%s: won't commit at this cycle" % result['Message'])
-    else:
-      values = result['Value'][1]
+      return S_ERROR()
 
-      if self.retryOnce:
-        self.log.verbose("Adding to records to commit those not committed within the previous cycle")
-      acWMSListAdded = []
+    # Now we try to commit
+    values = result['Value'][1]
 
-      for record in values:
-        recordSetup = record[0]
-        if recordSetup not in validSetups:
-          self.log.error("Setup %s is not valid" % recordSetup)
-          continue
-        if recordSetup not in self.dsClients:
-          self.log.info("Creating DataStore client for %s" % recordSetup)
-          self.dsClients[recordSetup] = DataStoreClient(retryGraceTime=900)
-        record = record[1:]
-        rD = {}
-        for fV in self.__summaryDefinedFields:
-          rD[fV[0]] = fV[1]
-        for iP in range(len(self.__summaryKeyFieldsMapping)):
-          fieldName = self.__summaryKeyFieldsMapping[iP]
-          rD[self.__renameFieldsMapping.get(fieldName, fieldName)] = record[iP]
-        record = record[len(self.__summaryKeyFieldsMapping):]
-        for iP in range(len(self.__summaryValueFieldsMapping)):
-          rD[self.__summaryValueFieldsMapping[iP]] = int(record[iP])
-        acWMS = WMSHistory()
-        acWMS.setStartTime(now)
-        acWMS.setEndTime(now)
-        acWMS.setValuesFromDict(rD)
-        retVal = acWMS.checkValues()
-        if not retVal['OK']:
-          self.log.error("Invalid accounting record ", "%s -> %s" % (retVal['Message'], rD))
-        else:
-          self.dsClients[recordSetup].addRegister(acWMS)
-          acWMSListAdded.append(acWMS)
+    self.log.info("Start sending records")
+    for record in values:
+      record = record[1:]
+      rD = {}
+      for fV in self.__summaryDefinedFields:
+	rD[fV[0]] = fV[1]
+      for iP in range(len(self.__summaryKeyFieldsMapping)):
+	fieldName = self.__summaryKeyFieldsMapping[iP]
+	rD[self.__renameFieldsMapping.get(fieldName, fieldName)] = record[iP]
+      record = record[len(self.__summaryKeyFieldsMapping):]
+      for iP in range(len(self.__summaryValueFieldsMapping)):
+	rD[self.__summaryValueFieldsMapping[iP]] = int(record[iP])
 
-      if self.retryOnce and self.retryValues:
-        for acWMSCumulated in self.retryValues:
-          retVal = acWMSCumulated.checkValues()
+      for backend in self.datastores:
+	if backend.lower() == 'monitoring':
+	  rD['timestamp'] = int(Time.toEpoch(now))
+	  self.datastores['Monitoring'].addRecord(rD)
+
+	elif backend.lower() == 'accounting':
+	  acWMS = WMSHistory()
+	  acWMS.setStartTime(now)
+	  acWMS.setEndTime(now)
+	  acWMS.setValuesFromDict(rD)
+	  retVal = acWMS.checkValues()
           if not retVal['OK']:
-            self.log.error("Invalid accounting record ", "%s" % (retVal['Message']))
+	    self.log.error("Invalid accounting record ", "%s -> %s" % (retVal['Message'], rD))
           else:
-            self.dsClients[recordSetup].addRegister(acWMSCumulated)
+	    self.datastores['Accounting'].addRegister(acWMS)
 
-      for setup in self.dsClients:
-        self.log.info("Sending records for setup %s" % setup)
-        result = self.dsClients[setup].commit()
-        if not result['OK']:
-          self.log.error("Couldn't commit wms history for setup %s" % setup, result['Message'])
-          # Re-creating the client: for new connection, and for avoiding accumulating too large of a backlog
-          self.dsClients[setup] = DataStoreClient(retryGraceTime=900)
-          if not self.retryOnce:
-            self.log.info("Will try again at next cycle")
-            self.retryOnce = True
-            self.retryValues = acWMSListAdded
-          else:
-            self.log.warn("Won't retry one more time")
-            self.retryOnce = False
-            self.retryValues = []
-        else:
-          self.log.info("Sent %s records for setup %s" % (result['Value'], setup))
-          self.retryOnce = False
+    for backend, datastore in self.datastores.items():
+      self.log.info("Committing to %s backend" % backend)
+      result = datastore.commit()
+      if not result['OK']:
+	self.log.error("Couldn't commit WMS history to %s" % backend, result['Message'])
+	return S_ERROR()
+      self.log.verbose("Done committing to %s backend" % backend)
+
     return S_OK()
