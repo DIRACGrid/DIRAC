c80db26fe8a19b1c0dabc0ba4a675a81df916426
Marko Petric <marko.petric@cern.ch>
2021-02-02 19:25:25 +0100

    should be elif and indent correctly

--- Core/Security/VOMS.py
+++ Core/Security/VOMS.py
@@ -197,15 +197,15 @@ class VOMS(BaseSecurity):
     if 'DIRAC_VOMSES' not in os.environ and 'X509_VOMSES' not in os.environ:
       os.environ["X509_VOMSES"] = os.path.join(rootPath, "etc", "grid-security", "vomses")
       gLogger.notice("You did not set X509_VOMSES in your bashrc. DIRAC searches $DIRAC/etc/grid-security/vomses . "
-                      "Please use X509_VOMSES, this auto discovery will be dropped.")
-    if 'DIRAC_VOMSES' in os.environ and 'X509_VOMSES' in os.environ:
+                     "Please use X509_VOMSES, this auto discovery will be dropped.")
+    elif 'DIRAC_VOMSES' in os.environ and 'X509_VOMSES' in os.environ:
       os.environ["X509_VOMSES"] = "%s:%s" % (os.environ['DIRAC_VOMSES'], os.environ["X509_VOMSES"])
       gLogger.notice("You set both variables DIRAC_VOMSES and X509_VOMSES in your bashrc. "
-                      "DIRAC_VOMSES will be dropped in a future version, please use only X509_VOMSES")
-    if 'DIRAC_VOMSES' in os.environ and 'X509_VOMSES' not in os.environ:
+                     "DIRAC_VOMSES will be dropped in a future version, please use only X509_VOMSES")
+    elif 'DIRAC_VOMSES' in os.environ and 'X509_VOMSES' not in os.environ:
       os.environ["X509_VOMSES"] = os.environ['DIRAC_VOMSES']
       gLogger.notice("You set the variables DIRAC_VOMSES in your bashrc. "
-                      "DIRAC_VOMSES will be dropped in a future version, please use X509_VOMSES")
+                     "DIRAC_VOMSES will be dropped in a future version, please use X509_VOMSES")
     # End of transition code
     if 'X509_VOMSES' not in os.environ:
       raise Exception("The env variable X509_VOMSES is not set. "
