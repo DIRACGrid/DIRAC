f01b2297e0a47f3fc5c5d6b69a322d89b5a8caca
fstagni <federico.stagni@cern.ch>
2021-02-04 11:21:00 +0100

    fix test

--- src/DIRAC/WorkloadManagementSystem/Agent/test/Test_Agent_SiteDirector.py
+++ src/DIRAC/WorkloadManagementSystem/Agent/test/Test_Agent_SiteDirector.py
@@ -63,8 +63,7 @@ def test__getPilotOptions(mocker):
                                                 'Setup': 'LHCb-Production',
                                                 'Site': 'LCG.CERN.cern'}}}
   res = sd._getPilotOptions('aQueue', 10)
-  assert res[0] == ['-S TestSetup', '-V 123', '-l 123', '-C T,e,s,t,S,e,t,u,p',
-                    '-e 1,2,3', '-N aCE', '-Q aQueue', '-n LCG.CERN.cern']
+  assert set(['-S TestSetup', '-V 123', '-l 123', '-n LCG.CERN.cern']) <= set(res[0])
   assert res[1] == 10
 
 
