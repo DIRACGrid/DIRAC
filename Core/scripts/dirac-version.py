9787436c9fe68903ac34ec2aa8f50fba81ac7d00
fstagni <federico.stagni@cern.ch>
2021-02-08 17:13:18 +0100

    using argparse

--- Core/scripts/dirac-version.py
+++ Core/scripts/dirac-version.py
@@ -19,18 +19,10 @@ from __future__ import print_function
 
 __RCSID__ = "$Id$"
 
-import sys
-import getopt
+import argparse
 
-cmdOpt = ('h', 'help', 'help doc string')
-
-optList, args = getopt.getopt(sys.argv[1:], cmdOpt[0], cmdOpt[1])
-for opt, value in optList:
-  if opt in ('-h', '--help'):
-    print(__doc__)
-    print('Options::\n\n')
-    print("  %s %s : %s" % (cmdOpt[0].ljust(3), cmdOpt[1].ljust(20), cmdOpt[2]))
-    sys.exit(0)
+parser = argparse.ArgumentParser(usage=__doc__)
+parser.parse_known_args()
 
 import DIRAC
 print(DIRAC.version)
