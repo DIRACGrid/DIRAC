812ba851c62727f3760095ddedd8354e579a286d
TaykYoku <yokutayk@gmail.com>
2021-02-25 00:17:20 +0100

    fix

--- src/DIRAC/FrameworkSystem/DB/AuthDB2.py
+++ src/DIRAC/FrameworkSystem/DB/AuthDB2.py
@@ -17,6 +17,50 @@ from sqlalchemy.orm import scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, Text, BigInteger, String
 from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
+from sqlalchemy.types import TypeDecorator, VARCHAR
+import json
+from sqlalchemy.ext.mutable import Mutable
+
+class MutableDict(Mutable, dict):
+    @classmethod
+    def coerce(cls, key, value):
+        "Convert plain dictionaries to MutableDict."
+
+        if not isinstance(value, MutableDict):
+            if isinstance(value, dict):
+                return MutableDict(value)
+
+            # this call will raise ValueError
+            return Mutable.coerce(key, value)
+        else:
+            return value
+
+    def __setitem__(self, key, value):
+        "Detect dictionary set events and emit change events."
+
+        dict.__setitem__(self, key, value)
+        self.changed()
+
+    def __delitem__(self, key):
+        "Detect dictionary del events and emit change events."
+
+        dict.__delitem__(self, key)
+        self.changed()
+
+class JSONEncodedDict(TypeDecorator):
+    "Represents an immutable structure as a json-encoded string."
+
+    impl = VARCHAR
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            value = json.dumps(value)
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = json.loads(value)
+        return value
 
 Model = declarative_base()
 
@@ -25,6 +69,8 @@ class Client(Model, OAuth2ClientMixin):
   __table_args__ = {'mysql_engine': 'InnoDB',
                     'mysql_charset': 'utf8'}
   id = Column(Integer, primary_key=True, nullable=False)
+  client_metadata = Column('client_metadata', MutableDict.as_mutable(JSONEncodedDict))
+
 
 class Token(Model, OAuth2TokenMixin):
   __tablename__ = 'Tokens'
