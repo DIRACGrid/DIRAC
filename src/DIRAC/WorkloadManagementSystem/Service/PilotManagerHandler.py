5a0b6c0b9a1f3d9f6226e7b5e37c7d91ae88d3ad
martynia <martynia@users.noreply.github.com>
2021-03-05 13:25:46 +0100

    Update WorkloadManagementSystem/Service/PilotManagerHandler.py
    Co-authored-by: fstagni <federico.stagni@cern.ch>

--- src/DIRAC/WorkloadManagementSystem/Service/PilotManagerHandler.py
+++ src/DIRAC/WorkloadManagementSystem/Service/PilotManagerHandler.py
@@ -202,8 +202,7 @@ class PilotManagerHandler(RequestHandler):
     :param columnList: a list of columns to GROUP BY (less status column)
     :return: a dictionary containing column names and data records
     """
-    result = pilotDB.getGroupedPilotSummary(selectDict, columnList)
-    return result
+    return pilotDB.getGroupedPilotSummary(selectDict, columnList)
 
   ##############################################################################
   types_getPilots = [six.string_types + six.integer_types]
