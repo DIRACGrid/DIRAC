3e1a884e4eb8ec8958b23b07b98d232065cec08c
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2021-03-17 13:12:41 +0100

    v7r3-pre1 notes and tags

--- src/DIRAC/__init__.py
+++ src/DIRAC/__init__.py
@@ -103,9 +103,9 @@ if six.PY3:
     version = "Unknown"
 else:
   majorVersion = 7
-  minorVersion = 2
+  minorVersion = 3
   patchLevel = 0
-  preVersion = 0
+  preVersion = 1
 
   version = "v%sr%s" % (majorVersion, minorVersion)
   # Make it so that __version__ is always PEP-440 style
