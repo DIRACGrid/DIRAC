32b8b72b41dcd8280b1309c10da271d8ba34dea0
TaykYoku <yokutayk@gmail.com>
2021-03-22 23:09:02 +0100

    fix logs

--- src/DIRAC/FrameworkSystem/API/AuthHandler.py
+++ src/DIRAC/FrameworkSystem/API/AuthHandler.py
@@ -303,7 +303,7 @@ class AuthHandler(TornadoREST):
           &chooseScope=..  to specify new scope(group in our case) (optional)
     """
     # Redirect endpoint for response
-    self.log.info('REDIRECT RESPONSE:\n', '\n'.join([self.request, self.request.uri, self.request.query, self.request.body, self.request.headers]))
+    self.log.debug('REDIRECT RESPONSE:\n', '\n'.join([self.request.uri, self.request.query, self.request.body, self.request.headers]))
 
     # Try to parse IdP session id
     session = self.get_argument('state')
