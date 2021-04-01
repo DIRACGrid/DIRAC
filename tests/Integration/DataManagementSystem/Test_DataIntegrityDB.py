6eb064bcb1d1fb50e7a138acc8d43b6af14a4e38
Chris Burr <christopher.burr@cern.ch>
2021-04-01 08:55:36 +0200

    Support running Test_DataIntegrityDB multiple times

--- tests/Integration/DataManagementSystem/Test_DataIntegrityDB.py
+++ tests/Integration/DataManagementSystem/Test_DataIntegrityDB.py
@@ -9,6 +9,8 @@ from __future__ import division
 from __future__ import print_function
 
 # pylint: disable=invalid-name,wrong-import-position
+import copy
+import time
 
 from DIRAC import gLogger
 
@@ -21,19 +23,31 @@ from DIRAC.DataManagementSystem.DB.DataIntegrityDB import DataIntegrityDB
 def test_DataIntegrityDB():
   """ Some test cases
   """
-
-  diDB = DataIntegrityDB()
-
   source = 'Test'
   prognosis = 'TestError'
   prodID = 1234
-  lfn = '/Test/%08d/File1' % prodID
-  fileMetadata1 = {lfn: {'Prognosis': prognosis, 'PFN': 'File1', 'SE': 'Test-SE'}}
-  fileOut1 = {'FileID': 1, 'LFN': lfn, 'PFN': 'File1', 'Prognosis': prognosis,
+  timestamp = int(time.time())
+  lfn = '/Test/%08d/File1/%d' % (prodID, timestamp)
+  pfn = 'File1/%d' % (timestamp)
+  fileMetadata1 = {lfn: {'Prognosis': prognosis, 'PFN': pfn, 'SE': 'Test-SE'}}
+  fileOut1 = {'LFN': lfn, 'PFN': pfn, 'Prognosis': prognosis,
               'GUID': None, 'SE': 'Test-SE', 'Size': None}
   newStatus = 'Solved'
   newPrognosis = 'AnotherError'
 
+  diDB = DataIntegrityDB()
+
+  # Clean up the database if required
+  result = diDB.getTransformationProblematics(1234)
+  assert result['OK']
+  for fileID in result['Value'].values():
+    result = diDB.removeProblematic(fileID)
+    assert result['OK']
+  result = diDB.getProblematicsSummary()
+  assert result['OK']
+  assert result['Value'] == {}
+
+  # Run the actual test
   result = diDB.insertProblematic(source, fileMetadata1)
   assert result['OK']
   assert result['Value'] == {'Successful': {lfn: True}, 'Failed': {}}
@@ -52,6 +66,7 @@ def test_DataIntegrityDB():
 
   result = diDB.getProblematic()
   assert result['OK']
+  fileOut1["FileID"] = result["Value"]["FileID"]
   assert result['Value'] == fileOut1
 
   result = diDB.incrementProblematicRetry(result['Value']['FileID'])
@@ -68,13 +83,13 @@ def test_DataIntegrityDB():
 
   result = diDB.getTransformationProblematics(prodID)
   assert result['OK']
-  assert result['Value'][lfn] == 1
+  assert result['Value'][lfn] == fileOut1["FileID"]
 
-  result = diDB.setProblematicStatus(1, newStatus)
+  result = diDB.setProblematicStatus(fileOut1["FileID"], newStatus)
   assert result['OK']
   assert result['Value'] == 1
 
-  result = diDB.changeProblematicPrognosis(1, newPrognosis)
+  result = diDB.changeProblematicPrognosis(fileOut1["FileID"], newPrognosis)
   assert result['OK']
   assert result['Value'] == 1
 
@@ -82,7 +97,7 @@ def test_DataIntegrityDB():
   assert result['OK']
   assert result['Value'] == []
 
-  result = diDB.removeProblematic(1)
+  result = diDB.removeProblematic(fileOut1["FileID"])
   assert result['OK']
   assert result['Value'] == 1
 
