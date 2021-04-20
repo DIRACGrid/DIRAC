7bca19398d9a22999c29699e6522fb4d416f997e
Chris Burr <christopher.burr@cern.ch>
2021-04-20 15:56:41 +0200

    Add hacky support for bytes to RMSEncoder

--- src/DIRAC/RequestManagementSystem/private/JSONUtils.py
+++ src/DIRAC/RequestManagementSystem/private/JSONUtils.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
 import json
+
+import six
 
 
 class RMSEncoder(json.JSONEncoder):
@@ -12,5 +15,7 @@ class RMSEncoder(json.JSONEncoder):
 
     if hasattr(obj, '_getJSONData'):
       return obj._getJSONData()
+    elif six.PY3 and isinstance(obj, bytes):
+      return obj.decode()
     else:
       return json.JSONEncoder.default(self, obj)
