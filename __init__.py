06c6aefd72308e0c87b771bb21d7a093b0c404a2
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2021-04-21 21:24:02 +0200

    v7r1p38 notes and tags

--- __init__.py
+++ __init__.py
@@ -95,7 +95,7 @@ if pythonVersion[0] == "3":
 else:
   majorVersion = 7
   minorVersion = 1
-  patchLevel = 37
+  patchLevel = 38
   preVersion = 0
 
 version = "v%sr%s" % (majorVersion, minorVersion)
