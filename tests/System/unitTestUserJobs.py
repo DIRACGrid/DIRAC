02272f0edc489ce5056ec9eddd9eb9b49217447c
fstagni <federico.stagni@cern.ch>
2021-05-20 12:41:12 +0200

    just better code

--- tests/System/unitTestUserJobs.py
+++ tests/System/unitTestUserJobs.py
@@ -3,7 +3,8 @@
 
 # pylint: disable=wrong-import-position, invalid-name
 
-from __future__ import print_function, absolute_import
+from __future__ import print_function
+from __future__ import absolute_import
 from __future__ import division
 
 __RCSID__ = "$Id$"
