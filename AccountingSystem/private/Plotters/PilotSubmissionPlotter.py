d9f7d3aaf0be71af9050bf050277f7ee48b57768
Yuji Kato <kato@hepl.phys.nagoya-u.ac.jp>
2021-05-20 16:50:10 +0900

    Removed futures

--- AccountingSystem/private/Plotters/PilotSubmissionPlotter.py
+++ AccountingSystem/private/Plotters/PilotSubmissionPlotter.py
@@ -1,6 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
 from DIRAC import S_OK
 from DIRAC.AccountingSystem.private.Plotters.BaseReporter import BaseReporter
