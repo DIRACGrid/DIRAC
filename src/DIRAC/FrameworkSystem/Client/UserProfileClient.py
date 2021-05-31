92ed947ad2f0706cd2ac5af5f5dc25cc04f69d3f
Chris Burr <christopher.burr@cern.ch>
2021-05-31 14:13:20 +0200

    Remove unused dataTypeRE argument in UserProfileClient

--- src/DIRAC/FrameworkSystem/Client/UserProfileClient.py
+++ src/DIRAC/FrameworkSystem/Client/UserProfileClient.py
@@ -2,11 +2,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import six
-import re
 from DIRAC import S_OK, S_ERROR
 from DIRAC.Core.DISET.RPCClient import RPCClient
-from DIRAC.Core.Utilities import DEncode, Time
+from DIRAC.Core.Utilities import DEncode
 
 
 class UserProfileClient(object):
@@ -21,35 +19,6 @@ class UserProfileClient(object):
   def __getRPCClient(self):
     return self.rpcClientFunctor("Framework/UserProfileManager")
 
-  def __generateTypeDest(self, dataObj):
-    if isinstance(dataObj, bool):
-      return "b"
-    if dataObj is None:
-      return "o"
-    if isinstance(dataObj, six.integer_types + (float,)):
-      return "n"
-    if isinstance(dataObj, six.string_types):
-      return "s"
-    # Not even trying here...
-    if isinstance(dataObj, Time._allTypes):
-      return "t"
-    if isinstance(dataObj, (list, tuple)):
-      return "l%se" % "".join([self.__generateTypeDest(so) for so in dataObj])
-    if isinstance(dataObj, dict):
-      return "d%se" % "".join(["%s%s" % (self.__generateTypeDest(k),
-                                         self.__generateTypeDest(dataObj[k])) for k in dataObj])
-    return ""
-
-  def checkTypeRe(self, dataObj, typeRE):
-    if typeRE[0] != "^":
-      typeRE = "^%s" % typeRE
-    if typeRE[-1] != "$":
-      typeRE = "%s$" % typeRE
-    typeDesc = self.__generateTypeDest(dataObj)
-    if not re.match(typeRE, typeDesc):
-      return S_ERROR("Stored data does not match typeRE: %s vs %s" % (typeDesc, typeRE))
-    return S_OK()
-
   def storeVar(self, varName, data, perms={}):
     try:
       stub = DEncode.encode(data)
@@ -57,30 +26,26 @@ class UserProfileClient(object):
       return S_ERROR("Cannot encode data:%s" % str(e))
     return self.__getRPCClient().storeProfileVar(self.profile, varName, stub, perms)
 
-  def __decodeVar(self, data, dataTypeRE):
+  def __decodeVar(self, data):
     try:
       dataObj, lenData = DEncode.decode(data)
     except Exception as e:
       return S_ERROR("Cannot decode data: %s" % str(e))
-    if dataTypeRE:
-      result = self.checkTypeRe(dataObj, dataTypeRE)
-      if not result['OK']:
-        return result
     return S_OK(dataObj)
 
-  def retrieveVar(self, varName, dataTypeRE=False):
+  def retrieveVar(self, varName):
     rpcClient = self.__getRPCClient()
     result = rpcClient.retrieveProfileVar(self.profile, varName)
     if not result['OK']:
       return result
-    return self.__decodeVar(result['Value'], dataTypeRE)
+    return self.__decodeVar(result['Value'])
 
-  def retrieveVarFromUser(self, ownerName, ownerGroup, varName, dataTypeRE=False):
+  def retrieveVarFromUser(self, ownerName, ownerGroup, varName):
     rpcClient = self.__getRPCClient()
     result = rpcClient.retrieveProfileVarFromUser(ownerName, ownerGroup, self.profile, varName)
     if not result['OK']:
       return result
-    return self.__decodeVar(result['Value'], dataTypeRE)
+    return self.__decodeVar(result['Value'])
 
   def retrieveAllVars(self):
     rpcClient = self.__getRPCClient()
