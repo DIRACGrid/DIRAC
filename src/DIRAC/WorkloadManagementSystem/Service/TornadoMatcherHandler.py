ee4b1e788cdae9c665d05d843e6f093b11e960dd
fstagni <federico.stagni@cern.ch>
2021-06-21 17:39:29 +0200

    added basic imports

--- src/DIRAC/WorkloadManagementSystem/Service/TornadoMatcherHandler.py
+++ src/DIRAC/WorkloadManagementSystem/Service/TornadoMatcherHandler.py
@@ -1,9 +1,20 @@
-""" Tornado-based Matcher service.
+""" Tornado-based HTTPs Matcher service.
 """
 
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__RCSID__ = "$Id$"
+
+from DIRAC import gLogger
 from DIRAC.Core.Tornado.Server.TornadoService import TornadoService
 from DIRAC.WorkloadManagementSystem.Service.MatcherHandler import MatcherHandlerMixin
 
 
+sLog = gLogger.getSubLogger(__name__)
+
+
 class TornadoMatcherHandler(MatcherHandlerMixin, TornadoService):
-  pass
+  log = sLog
