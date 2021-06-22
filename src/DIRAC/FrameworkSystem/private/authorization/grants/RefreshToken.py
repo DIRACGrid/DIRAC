002f22482965359294b7ff940e5eee020b4b425d
TaykYoku <yokutayk@gmail.com>
2021-06-22 20:37:03 +0200

    fix py3 things

--- src/DIRAC/FrameworkSystem/private/authorization/grants/RefreshToken.py
+++ src/DIRAC/FrameworkSystem/private/authorization/grants/RefreshToken.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from authlib.jose import JsonWebKey, jwt
 from authlib.oauth2.base import OAuth2Error
 from authlib.oauth2.rfc6749.grants import RefreshTokenGrant as _RefreshTokenGrant
 
