dd1494ccf68b122c10f95876f4e6553e1dd363b0
TaykYoku <yokutayk@gmail.com>
2021-07-06 19:50:56 +0200

    comment sslDebug

--- src/DIRAC/Core/Tornado/Server/TornadoServer.py
+++ src/DIRAC/Core/Tornado/Server/TornadoServer.py
@@ -186,7 +186,8 @@ class TornadoServer(object):
         'keyfile': certs[1],
         'cert_reqs': M2Crypto.SSL.verify_peer,
         'ca_certs': ca,
-        'sslDebug': False,  # Set to true if you want to see the TLS debug messages
+        # Failed in tornado '5.1.1', 'sslDebug' not in m2netutil._SSL_CONTEXT_KEYWORDS
+        #'sslDebug': False,  # Set to true if you want to see the TLS debug messages
     }
 
     # Init monitoring
