55fdb641e176bdafd0431c9826a8aec0008d998a
ARRABITO Luisa <arrabito@in2p3.fr>
2021-07-09 10:26:46 +0200

    fix group by syntax in summaryWeb

--- RequestManagementSystem/DB/RequestDB.py
+++ RequestManagementSystem/DB/RequestDB.py
@@ -575,7 +575,10 @@ class RequestDB(object):
 
           if key == 'Type':
             summaryQuery = summaryQuery.join(Request.__operations__)\
-                                       .group_by(Request.RequestID, Operation.Type)
+                                       .group_by(Request.RequestID, Request.RequestName,
+                                                 Request.JobID, Request.OwnerDN, Request.OwnerGroup,
+                                                 Request._Status, Request.Error,
+                                                 Request._CreationTime, Request._LastUpdate)
             tableName = 'Operation'
           elif key == 'Status':
             key = '_Status'
