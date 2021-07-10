db4c9492c9db66cf446f7b91aef71ccbc9224cbf
TaykYoku <yokutayk@gmail.com>
2021-07-11 00:25:20 +0200

    script initialize

--- src/DIRAC/Core/Utilities/DIRACScript.py
+++ src/DIRAC/Core/Utilities/DIRACScript.py
@@ -28,17 +28,18 @@ class DIRACScript(object):
   wrapped in this decorator to allow extensions to override any entry_point.
   """
   def __init__(self):
-    """ c'tor
-    """
+    """ c'tor """
+    self.scriptDoc = None
+    self.scriptName = None
     self.alreadyInitialized = False
-    self.scriptName = inspect.currentframe().f_back.f_globals['__name__'].split('.')[-1].replace('_', '-')
-
-    # Parse script __doc__'s to create a draft help message
     self.localCfg = LocalConfiguration()
-    self.localCfg.setUsageMessage(inspect.currentframe().f_back.f_globals['__doc__'])
-
     self.showHelp = self.localCfg.showHelp
     self.initParameters()
+
+  def __scriptInitialize(self):
+    """ Script c'tor
+    """
+    self.localCfg.setUsageMessage(self.scriptDoc)
 
   def initParameters(self):
     """ Script initialization """
@@ -55,7 +56,12 @@ class DIRACScript(object):
     # If func is provided then the decorator is being applied to a function
     if func is not None:
       self._func = func
+      # Find the name of the command and its documentation
+      self.scriptDoc = func.__globals__['__doc__']
+      self.scriptName = os.path.basename(func.__globals__['__file__'])[:-3].replace('_', '-')
       return functools.wraps(func)(self)
+
+    self.__scriptInitialize()
 
     # Setuptools based installations aren't supported with Python 2
     if six.PY2:
@@ -64,35 +70,27 @@ class DIRACScript(object):
     # This is only available in Python 3.8+ so it has to be here for now
     from importlib import metadata  # pylint: disable=no-name-in-module
 
-    # Iterate through all known entry_points looking for DIRACScripts
-    matches = defaultdict(list)
-    function_name = None
-    for entrypoint in metadata.entry_points()['console_scripts']:
-      if self.scriptName == entrypoint.name:
-        entrypointFunc = entrypoint.load()
-        if not isinstance(entrypointFunc, DIRACScript):
-          raise ImportError(
-              "Invalid dirac- console_scripts entry_point: " + repr(entrypoint) + "\n" +
-              "All dirac- console_scripts should be wrapped in the DiracScript " +
-              "decorator to ensure extension overlays are applied correctly."
-          )
-        matches[entrypoint.name].append(entrypoint)
-        # If the entrypoint.name is self.scriptName then we've found the currently called function
-        function_name = entrypoint.name
+    # Iterate through all known entry_points looking for self.scriptName
+    matches = [ep for ep in metadata.entry_points()['console_scripts'] if ep.name == self.scriptName]
 
-    if function_name is None:
+    if not matches:
       # TODO: This should an error once the integration tests modified to use pip install
       return self._func(self)  # pylint: disable=not-callable
       # raise NotImplementedError("Something is very wrong")
 
     # Call the entry_point from the extension with the highest priority
     rankedExtensions = extensionsByPriority()
-    entrypoint = max(
-        matches[function_name],
-        key=lambda e: rankedExtensions.index(entrypointToExtension(e)),
-    )
+    entrypoint = max(matches, key=lambda e: rankedExtensions.index(entrypointToExtension(e)),)
+    entrypointFunc = entrypoint.load()
 
-    return entrypoint.load()._func(self)
+    # Check if entrypoint is DIRACScript
+    if not isinstance(entrypointFunc, DIRACScript):
+      raise ImportError(
+          "Invalid dirac- console_scripts entry_point: " + repr(entrypoint) + "\n" +
+          "All dirac- console_scripts should be wrapped in the DiracScript " +
+          "decorator to ensure extension overlays are applied correctly."
+      )
+    return entrypointFunc._func(self)
 
   def parseCommandLine(self, script=False, ignoreErrors=False, initializeMonitor=False):
     """ Parse command line
