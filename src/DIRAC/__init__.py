84c4d72f836176cf4d8dcee7e006dea35d267658
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2021-07-19 16:36:19 +0200

    v7r3-pre16 notes and tags

--- src/DIRAC/__init__.py
+++ src/DIRAC/__init__.py
@@ -106,7 +106,7 @@ else:
   majorVersion = 7
   minorVersion = 3
   patchLevel = 0
-  preVersion = 15
+  preVersion = 16
 
   version = "v%sr%s" % (majorVersion, minorVersion)
   # Make it so that __version__ is always PEP-440 style
