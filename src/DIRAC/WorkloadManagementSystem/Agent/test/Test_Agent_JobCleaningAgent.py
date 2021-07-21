45abde865babb42aba57b7493b185d2170135630
Federico Stagni <federico.stagni@cern.ch>
2021-07-21 17:09:50 +0200

    using fixtures to initialize the agents

--- src/DIRAC/WorkloadManagementSystem/Agent/test/Test_Agent_JobCleaningAgent.py
+++ src/DIRAC/WorkloadManagementSystem/Agent/test/Test_Agent_JobCleaningAgent.py
@@ -5,6 +5,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
 import pytest
 from mock import MagicMock
 
@@ -22,35 +23,42 @@ mockNone.return_value = None
 mockJMC = MagicMock()
 
 
-@pytest.mark.parametrize(
-    "mockReplyInput, expected", [
-        ({'OK': True, 'Value': ''}, {'OK': True, 'Value': []}),
-        ({'OK': False, 'Message': ''}, {'OK': False, 'Message': ''})
-    ])
-def test__getAllowedJobTypes(mocker, mockReplyInput, expected):
-  """ Testing JobCleaningAgent()._getAllowedJobTypes()
-  """
-
-  mockReply.return_value = mockReplyInput
-
+@pytest.fixture
+def jca(mocker):
   mocker.patch("DIRAC.WorkloadManagementSystem.Agent.JobCleaningAgent.AgentModule.__init__")
   mocker.patch(
       "DIRAC.WorkloadManagementSystem.Agent.JobCleaningAgent.AgentModule._AgentModule__moduleProperties",
       side_effect=lambda x, y=None: y, create=True
   )
+  mocker.patch("DIRAC.WorkloadManagementSystem.Agent.JobCleaningAgent.AgentModule.am_getOption", return_value=mockAM)
   mocker.patch(
       "DIRAC.WorkloadManagementSystem.Agent.JobCleaningAgent.JobDB.getDistinctJobAttributes",
       side_effect=mockReply)
+  mocker.patch("DIRAC.WorkloadManagementSystem.Agent.JobCleaningAgent.JobDB.selectJobs", side_effect=mockReply)
   mocker.patch("DIRAC.WorkloadManagementSystem.Agent.JobCleaningAgent.JobDB.__init__", side_effect=mockNone)
+  mocker.patch("DIRAC.WorkloadManagementSystem.Agent.JobCleaningAgent.ReqClient", return_value=mockNone)
+  mocker.patch("DIRAC.WorkloadManagementSystem.Agent.JobCleaningAgent.JobMonitoringClient", return_value=mockJMC)
 
-  jobCleaningAgent = JobCleaningAgent()
-  jobCleaningAgent.log = gLogger
-  jobCleaningAgent.log.setLevel('DEBUG')
-  jobCleaningAgent._AgentModule__configDefaults = mockAM
-  jobCleaningAgent.initialize()
+  jca = JobCleaningAgent()
+  jca.log = gLogger
+  jca.log.setLevel('DEBUG')
+  jca._AgentModule__configDefaults = mockAM
+  jca.initialize()
 
-  result = jobCleaningAgent._getAllowedJobTypes()
+  return jca
 
+
+@pytest.mark.parametrize(
+    "mockReplyInput, expected", [
+        ({'OK': True, 'Value': ''}, {'OK': True, 'Value': []}),
+        ({'OK': False, 'Message': ''}, {'OK': False, 'Message': ''})
+    ])
+def test__getAllowedJobTypes(jca, mockReplyInput, expected):
+  """ Testing JobCleaningAgent()._getAllowedJobTypes()
+  """
+
+  mockReply.return_value = mockReplyInput
+  result = jca._getAllowedJobTypes()
   assert result == expected
 
 
@@ -59,28 +67,12 @@ def test__getAllowedJobTypes(mocker, mockReplyInput, expected):
         ({'OK': True, 'Value': ''}, {'OK': True, 'Value': None}),
         ({'OK': False, 'Message': ''}, {'OK': False, 'Message': ''})
     ])
-def test_removeJobsByStatus(mocker, mockReplyInput, expected):
+def test_removeJobsByStatus(jca, mockReplyInput, expected):
   """ Testing JobCleaningAgent().removeDeletedJobs()
   """
 
   mockReply.return_value = mockReplyInput
-
-  mocker.patch("DIRAC.WorkloadManagementSystem.Agent.JobCleaningAgent.AgentModule.__init__")
-  mocker.patch(
-      "DIRAC.WorkloadManagementSystem.Agent.JobCleaningAgent.AgentModule._AgentModule__moduleProperties",
-      side_effect=lambda x, y=None: y, create=True
-  )
-  mocker.patch("DIRAC.WorkloadManagementSystem.Agent.JobCleaningAgent.JobDB.selectJobs", side_effect=mockReply)
-  mocker.patch("DIRAC.WorkloadManagementSystem.Agent.JobCleaningAgent.JobDB.__init__", side_effect=mockNone)
-
-  jobCleaningAgent = JobCleaningAgent()
-  jobCleaningAgent.log = gLogger
-  jobCleaningAgent.log.setLevel('DEBUG')
-  jobCleaningAgent._AgentModule__configDefaults = mockAM
-  jobCleaningAgent.initialize()
-
-  result = jobCleaningAgent.removeDeletedJobs()
-
+  result = jca.removeDeletedJobs()
   assert result == expected
 
 
@@ -92,28 +84,12 @@ def test_removeJobsByStatus(mocker, mockReplyInput, expected):
         ({'JobType': ['some', 'status'],
           'Status': ['Deleted', 'Cancelled']}, {'OK': True, 'Value': ''}, {'OK': True, 'Value': None})
     ])
-def test_deleteJobsByStatus(mocker, conditions, mockReplyInput, expected):
+def test_deleteJobsByStatus(jca, conditions, mockReplyInput, expected):
   """ Testing JobCleaningAgent().deleteJobsByStatus()
   """
 
   mockReply.return_value = mockReplyInput
-
-  mocker.patch("DIRAC.WorkloadManagementSystem.Agent.JobCleaningAgent.AgentModule.__init__")
-  mocker.patch(
-      "DIRAC.WorkloadManagementSystem.Agent.JobCleaningAgent.AgentModule._AgentModule__moduleProperties",
-      side_effect=lambda x, y=None: y, create=True
-  )
-  mocker.patch("DIRAC.WorkloadManagementSystem.Agent.JobCleaningAgent.JobDB.selectJobs", side_effect=mockReply)
-  mocker.patch("DIRAC.WorkloadManagementSystem.Agent.JobCleaningAgent.JobDB.__init__", side_effect=mockNone)
-
-  jobCleaningAgent = JobCleaningAgent()
-  jobCleaningAgent.log = gLogger
-  jobCleaningAgent.log.setLevel('DEBUG')
-  jobCleaningAgent._AgentModule__configDefaults = mockAM
-  jobCleaningAgent.initialize()
-
-  result = jobCleaningAgent.deleteJobsByStatus(conditions)
-
+  result = jca.deleteJobsByStatus(conditions)
   assert result == expected
 
 
