6693d53d35bb2e66c0db85a2b2c084f8035b6093
TaykYoku <yokutayk@gmail.com>
2021-08-28 00:28:14 +0200

    fix show jobs

--- WorkloadManagementSystem/Service/JobMonitoringHandler.py
+++ WorkloadManagementSystem/Service/JobMonitoringHandler.py
@@ -44,6 +44,8 @@ class JobMonitoringHandler(RequestHandler):
         '/Services/JobMonitoring/useESForJobParametersFlag', False)
     if useESForJobParametersFlag:
       cls.gElasticJobParametersDB = ElasticJobParametersDB()
+    
+    cls.pilotManager = PilotManagerClient()
     return S_OK()
 
   def initialize(self):
@@ -62,6 +64,44 @@ class JobMonitoringHandler(RequestHandler):
     self.jobPolicy.jobDB = self.gJobDB
 
     return S_OK()
+
+  @classmethod
+  def parseSelectors(cls, selectDict=None):
+    """ Parse selectors before DB query
+
+        :param dict selectDict: selectors
+
+        :return: str, str, dict -- start/end date, selectors
+    """
+    selectDict = selectDict or {}
+
+    # Get time period
+    startDate = selectDict.get('FromDate', None)
+    if startDate:
+      del selectDict['FromDate']
+    # For backward compatibility
+    if startDate is None:
+      startDate = selectDict.get('LastUpdate', None)
+      if startDate:
+        del selectDict['LastUpdate']
+    endDate = selectDict.get('ToDate', None)
+    if endDate:
+      del selectDict['ToDate']
+    
+    # Provide JobID bound to a specific PilotJobReference
+    # There is no reason to have both PilotJobReference and JobID in selectDict
+    # If that occurs, use the JobID instead of the PilotJobReference
+    pilotJobRefs = selectDict.get('PilotJobReference')
+    if pilotJobRefs:
+      del selectDict['PilotJobReference']
+      if not selectDict.get('JobID'):
+        for pilotJobRef in [pilotJobRefs] if isinstance(pilotJobRefs, six.string_types) else pilotJobRefs:
+          res = cls.pilotManager.getPilotInfo(pilotJobRef)
+          if res['OK'] and 'Jobs' in res['Value'][pilotJobRef]:
+            selectDict['JobID'] = selectDict.get('JobID', [])
+            selectDict['JobID'].extend(res['Value'][pilotJobRef]['Jobs'])
+
+    return startDate, endDate, selectDict
 
   @classmethod
   def getAttributesForJobList(cls, *args, **kwargs):
@@ -183,27 +223,8 @@ class JobMonitoringHandler(RequestHandler):
     Return a list. Each item is a list with 2 items, the list of distinct
     attribute values and the counter
     """
-
-    # Check that Attributes in attrList and attrDict, they must be in
-    # self.queryAttributes.
-
-    # for attr in attrList:
-    #  try:
-    #    self.queryAttributes.index(attr)
-    #  except:
-    #    return S_ERROR( 'Requested Attribute not Allowed: %s.' % attr )
-    #
-    # for attr in attrDict:
-    #  try:
-    #    self.queryAttributes.index(attr)
-    #  except:
-    #    return S_ERROR( 'Condition Attribute not Allowed: %s.' % attr )
-
-    cutDate = str(cutDate)
-    if not attrDict:
-      attrDict = {}
-
-    return cls.gJobDB.getCounters('Jobs', attrList, attrDict, newer=cutDate, timeStamp='LastUpdateTime')
+    _, _, attrDict = cls.parseSelectors(attrDict)
+    return cls.gJobDB.getCounters('Jobs', attrList, attrDict, newer=str(cutDate), timeStamp='LastUpdateTime')
 
 ##############################################################################
   types_getCurrentJobCounters = []
@@ -213,9 +234,7 @@ class JobMonitoringHandler(RequestHandler):
     """ Get job counters per Status with attrDict selection. Final statuses are given for
         the last day.
     """
-
-    if not attrDict:
-      attrDict = {}
+    _, _, attrDict = cls.parseSelectors(attrDict)
     result = cls.gJobDB.getCounters('Jobs', ['Status'], attrDict, timeStamp='LastUpdateTime')
     if not result['OK']:
       return result
@@ -361,32 +380,8 @@ class JobMonitoringHandler(RequestHandler):
         job monitor in a generic format
     """
     resultDict = {}
-    startDate = selectDict.get('FromDate', None)
-    if startDate:
-      del selectDict['FromDate']
-    # For backward compatibility
-    if startDate is None:
-      startDate = selectDict.get('LastUpdate', None)
-      if startDate:
-        del selectDict['LastUpdate']
-    endDate = selectDict.get('ToDate', None)
-    if endDate:
-      del selectDict['ToDate']
 
-    # Provide JobID bound to a specific PilotJobReference
-    # There is no reason to have both PilotJobReference and JobID in selectDict
-    # If that occurs, use the JobID instead of the PilotJobReference
-    pilotJobRefs = selectDict.get('PilotJobReference')
-    if pilotJobRefs:
-      del selectDict['PilotJobReference']
-      if 'JobID' not in selectDict or not selectDict['JobID']:
-        if not isinstance(pilotJobRefs, list):
-          pilotJobRefs = [pilotJobRefs]
-        selectDict['JobID'] = []
-        for pilotJobRef in pilotJobRefs:
-          res = PilotManagerClient().getPilotInfo(pilotJobRef)
-          if res['OK'] and 'Jobs' in res['Value'][pilotJobRef]:
-            selectDict['JobID'].extend(res['Value'][pilotJobRef]['Jobs'])
+    startDate, endDate, selectDict = self.parseSelectors(selectDict)
 
     result = self.jobPolicy.getControlledUsers(RIGHT_GET_INFO)
     if not result['OK']:
@@ -489,18 +484,7 @@ class JobMonitoringHandler(RequestHandler):
   def export_getJobStats(cls, attribute, selectDict):
     """ Get job statistics distribution per attribute value with a given selection
     """
-    startDate = selectDict.get('FromDate', None)
-    if startDate:
-      del selectDict['FromDate']
-    # For backward compatibility
-    if startDate is None:
-      startDate = selectDict.get('LastUpdate', None)
-      if startDate:
-        del selectDict['LastUpdate']
-    endDate = selectDict.get('ToDate', None)
-    if endDate:
-      del selectDict['ToDate']
-
+    startDate, endDate, selectDict = self.parseSelectors(selectDict)
     result = cls.gJobDB.getCounters('Jobs', [attribute], selectDict,
                                     newer=startDate,
                                     older=endDate,
