b9d26ce9618615c984c831695f19b17193510676
TaykYoku <yokutayk@gmail.com>
2021-10-15 01:01:45 +0200

    fix (Core): fix dirac-cert-convert script

--- src/DIRAC/Core/scripts/dirac_cert_convert.py
+++ src/DIRAC/Core/scripts/dirac_cert_convert.py
@@ -12,7 +12,6 @@ __RCSID__ = "$Id$"
 import os
 import sys
 import shutil
-from getpass import getpass
 from datetime import datetime
 
 from DIRAC import gLogger
@@ -22,12 +21,8 @@ from DIRAC.Core.Utilities.DIRACScript import DIRACScript as Script
 
 @Script()
 def main():
-    Script.registerArgument("P12:  certificate in the p12")
+    Script.registerArgument("P12: user certificate in the p12")
     _, args = Script.parseCommandLine(ignoreErrors=True)
-
-    if len(args) == 0:
-        gLogger.fatal("User Certificate P12 file is not given.")
-        sys.exit(1)
 
     p12 = args[0]
     if not os.path.isfile(p12):
@@ -36,46 +31,38 @@ def main():
 
     globus = os.path.join(os.environ["HOME"], ".globus")
     if not os.path.isdir(globus):
-        gLogger.notice("Creating '~/.globus' directory")
+        gLogger.notice(f"Creating {globus} directory")
         os.mkdir(globus)
 
-    p12Path = os.path.join(globus, os.path.basename(p12))
     cert = os.path.join(globus, "usercert.pem")
     key = os.path.join(globus, "userkey.pem")
 
     nowPrefix = "." + datetime.now().isoformat()
-    for old in [p12Path, cert, key]:
+    for old in [cert, key]:
         if os.path.isfile(old):
-            gLogger.notice("Back up %s file." % old)
-            shutil.copy(old, old + nowPrefix)
-
-    shutil.copy(p12, p12Path)
-
-    password = getpass("Enter password for p12:")
-    password = " -password pass:%s" % password
+            gLogger.notice(f"Back up {old} file to {old + nowPrefix}.")
+            shutil.move(old, old + nowPrefix)
 
     # new OpenSSL version require OPENSSL_CONF to point to some accessible location',
-    gLogger.notice("echo Converting p12 key to pem format")
-    result = shellCall(
-        900, 'export OPENSSL_CONF=/tmp && openssl pkcs12 -nocerts -in "%s" -out "%s"%s' % (p12Path, key, password)
-    )
-    if result["OK"]:
+    gLogger.notice("Converting p12 key to pem format")
+    result = shellCall(900, f"export OPENSSL_CONF=/tmp && openssl pkcs12 -nocerts -in {p12} -out {key}")
+    # The last command was successful
+    if result["OK"] and result["Value"][0] == 0:
         gLogger.notice("Converting p12 certificate to pem format")
-        result = shellCall(
-            900,
-            'export OPENSSL_CONF=/tmp && openssl pkcs12 -clcerts -nokeys -in "%s" -out "%s"%s'
-            % (p12Path, cert, password),
-        )
-    if not result["OK"]:
-        gLogger.fatal(result["Message"])
-        for old in [p12Path, cert, key]:
+        result = shellCall(900, f"export OPENSSL_CONF=/tmp && openssl pkcs12 -clcerts -nokeys -in {p12} -out {cert}")
+    # Something went wrong
+    if not result["OK"] or result["Value"][0] != 0:
+        gLogger.fatal(result.get("Message", result["Value"][2]))
+        for old in [cert, key]:
             if os.path.isfile(old + nowPrefix):
-                gLogger.notice("Move %s file back to %s" % (old + nowPrefix, old))
+                gLogger.notice(f"Restore {old} file from the {old + nowPrefix}")
                 shutil.move(old + nowPrefix, old)
         sys.exit(1)
 
     os.chmod(key, 0o400)
     os.chmod(cert, 0o644)
+
+    gLogger.notice(f"{os.path.basename(cert)} and {os.path.basename(key)} was created in the {globus}")
 
 
 if __name__ == "__main__":
