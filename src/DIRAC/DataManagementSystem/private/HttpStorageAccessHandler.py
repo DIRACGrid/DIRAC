6fcd99a172870fb4696d776b45c5e5004173c3fd
fstagni <federico.stagni@cern.ch>
2021-10-18 12:31:48 +0200

    fix: inheriting from BaseHTTPRequestHandler

--- src/DIRAC/DataManagementSystem/private/HttpStorageAccessHandler.py
+++ src/DIRAC/DataManagementSystem/private/HttpStorageAccessHandler.py
@@ -22,7 +22,7 @@ from http import server
 from DIRAC.Core.Utilities.DictCache import DictCache
 
 
-class HttpStorageAccessHandler(server):
+class HttpStorageAccessHandler(server.BaseHTTPRequestHandler):
 
     register = DictCache()
     basePath = ""
