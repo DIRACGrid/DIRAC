6c59d3e586bf8534652354acbc09097d967da749
Federico Stagni <federico.stagni@cern.ch>
2021-12-02 16:38:44 +0100

    fix: WMS Utilities and scripts to py3

--- src/DIRAC/WorkloadManagementSystem/scripts/dirac_vm_get_pilot_output.py
+++ src/DIRAC/WorkloadManagementSystem/scripts/dirac_vm_get_pilot_output.py
@@ -2,13 +2,6 @@
 """
   Get pilot output from a VM
 """
-
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-
-__RCSID__ = "$Id$"
-
 from DIRAC import gLogger, exit as DIRACExit
 from DIRAC.Core.Utilities.DIRACScript import DIRACScript as Script
 
