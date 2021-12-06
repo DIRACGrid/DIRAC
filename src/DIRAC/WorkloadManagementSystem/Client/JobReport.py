51fe7e9b3efb9ad1f3e9efe8b10d40fe36959f0b
fstagni <federico.stagni@cern.ch>
2021-12-06 17:16:17 +0100

    fix: only reporting changes on major status when needed

--- src/DIRAC/WorkloadManagementSystem/Client/JobReport.py
+++ src/DIRAC/WorkloadManagementSystem/Client/JobReport.py
@@ -36,7 +36,7 @@ class JobReport(object):
         self.jobID = jobID
 
     def setJobStatus(self, status="", minorStatus="", applicationStatus="", sendFlag=True):
-        """Send job status information to the JobState service for jobID"""
+	"""Accumulate and possibly send job status information to the JobState service"""
 
         timeStamp = Time.toString()
         # add job status record
