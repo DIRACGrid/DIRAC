cb49c1ee6a5f668d4fa7c87d7c460692b2e8aaa1
fstagni <federico.stagni@cern.ch>
2022-03-01 10:32:19 +0100

    fix: added TornadoProxyManager to ConfigTemplate.cfg

--- src/DIRAC/FrameworkSystem/Service/TornadoProxyManagerHandler.py
+++ src/DIRAC/FrameworkSystem/Service/TornadoProxyManagerHandler.py
@@ -1,14 +1,18 @@
 """ TornadoProxyManager is the implementation of the ProxyManagement service in HTTPS
 
     .. literalinclude:: ../ConfigTemplate.cfg
-      :start-after: ##BEGIN ProxyManager:
+      :start-after: ##BEGIN TornadoProxyManager:
       :end-before: ##END
       :dedent: 2
-      :caption: ProxyManager options
+      :caption: TornadoProxyManager options
 """
+from DIRAC import gLogger
 from DIRAC.Core.Tornado.Server.TornadoService import TornadoService
 from DIRAC.FrameworkSystem.Service.ProxyManagerHandler import ProxyManagerHandlerMixin
 
 
+sLog = gLogger.getSubLogger(__name__)
+
+
 class TornadoProxyManagerHandler(ProxyManagerHandlerMixin, TornadoService):
-    pass
+    log = sLog
