13e4682995f1c30e2a66ea82076914706282b8fc
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2022-05-10 22:48:44 +0200

    docs: v7r2p44 notes and tags

--- src/DIRAC/__init__.py
+++ src/DIRAC/__init__.py
@@ -105,7 +105,7 @@ if six.PY3:
 else:
     majorVersion = 7
     minorVersion = 2
-    patchLevel = 43
+    patchLevel = 44
     preVersion = 0
 
     version = "v%sr%s" % (majorVersion, minorVersion)
