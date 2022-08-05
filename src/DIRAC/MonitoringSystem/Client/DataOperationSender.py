edfa9c15351c06c4de07cf9758e845483a4fc90d
Christophe Haen <christophe.haen@cern.ch>
2022-08-05 11:06:12 +0200

    fix (DataOperationSender): initialize dynamic list of methods only once, and fail if master accounting fails

--- src/DIRAC/MonitoringSystem/Client/DataOperationSender.py
+++ src/DIRAC/MonitoringSystem/Client/DataOperationSender.py
@@ -27,16 +27,75 @@ class DataOperationSender:
     # Initialize the object so that the Reporters are created only once
     def __init__(self):
         monitoringType = "DataOperation"
-        # Will use the `MonitoringBackends/Default` value as monitoring backend unless a flag for `MonitoringBackends/DataOperation` is set.
+        # Will use the `MonitoringBackends/Default` value
+        # as monitoring backend unless a flag for `MonitoringBackends/DataOperation` is set.
         self.monitoringOptions = Operations().getMonitoringBackends(monitoringType)
         if "Monitoring" in self.monitoringOptions:
             self.dataOperationReporter = MonitoringReporter(monitoringType)
         if "Accounting" in self.monitoringOptions:
             self.dataOp = DataOperation()
 
+        self._sendDataMethods = []
+        self._commitMethods = []
+        for backend in self.monitoringOptions:
+            self._sendDataMethods.append(getattr(self, f"_sendData{backend}"))
+            self._commitMethods.append(getattr(self, f"_commit{backend}"))
+
+    def _sendDataMonitoring(self, baseDict, commitFlag=False, delayedCommit=False, startTime=False, endTime=False):
+        """Send the data to the monitoring system"""
+        # Some fields added here are not known to the Accounting, so we have to make a copy
+        monitoringDict = copy.deepcopy(baseDict)
+
+        monitoringDict["Channel"] = monitoringDict["Source"] + "->" + monitoringDict["Destination"]
+        # Add timestamp if not already added
+        if "timestamp" not in monitoringDict:
+            monitoringDict["timestamp"] = int(toEpoch())
+        self.dataOperationReporter.addRecord(monitoringDict)
+        if commitFlag:
+            result = self.dataOperationReporter.commit()
+            sLog.debug("Committing data operation to monitoring")
+            if not result["OK"]:
+                sLog.error("Could not commit data operation to monitoring", result["Message"])
+            else:
+                sLog.debug("Done committing to monitoring")
+            return result
+        return S_OK()
+
+    @convertToReturnValue
+    def _sendDataAccounting(self, baseDict, commitFlag=False, delayedCommit=False, startTime=False, endTime=False):
+        """Send the data to the accounting system"""
+        returnValueOrRaise(self.dataOp.setValuesFromDict(baseDict))
+
+        if startTime:
+            self.dataOp.setStartTime(startTime)
+            self.dataOp.setEndTime(endTime)
+        else:
+            self.dataOp.setStartTime()
+            self.dataOp.setEndTime()
+        # Adding only to register
+        if not commitFlag and not delayedCommit:
+            return gDataStoreClient.addRegister(self.dataOp)
+
+        # Adding to register and committing
+        if commitFlag and not delayedCommit:
+            gDataStoreClient.addRegister(self.dataOp)
+            sLog.debug("Committing data operation to accounting")
+            result = gDataStoreClient.commit()
+
+            if not result["OK"]:
+                sLog.error("Could not commit data operation to accounting", result["Message"])
+                return result
+            sLog.debug("Done committing to accounting")
+        # Only late committing
+        else:
+            result = self.dataOp.delayedCommit()
+            if not result["OK"]:
+                sLog.error("Could not delay-commit data operation to accounting")
+        return result
+
     def sendData(self, baseDict, commitFlag=False, delayedCommit=False, startTime=False, endTime=False):
         """
-        Sends the input to Monitoring or Acconting based on the monitoringOptions
+        Sends the input to Monitoring or Accounting based on the monitoringOptions
 
         :param dict baseDict: contains a key/value pair
         :param bool commitFlag: decides whether to commit the record or not.
@@ -47,85 +106,49 @@ class DataOperationSender:
 
         baseDict["ExecutionSite"] = DIRAC.siteName()
 
-        def _sendMonitoring():
-            monitoringDict = copy.deepcopy(baseDict)
-
-            monitoringDict["Channel"] = monitoringDict["Source"] + "->" + monitoringDict["Destination"]
-            # Add timestamp if not already added
-            if "timestamp" not in monitoringDict:
-                monitoringDict["timestamp"] = int(toEpoch())
-            self.dataOperationReporter.addRecord(monitoringDict)
-            if commitFlag:
-                result = self.dataOperationReporter.commit()
-                sLog.debug("Committing data operation to monitoring")
-                if not result["OK"]:
-                    sLog.error("Could not commit data operation to monitoring", result["Message"])
-                else:
-                    sLog.debug("Done committing to monitoring")
-                return result
-            return S_OK()
-
-        @convertToReturnValue
-        def _sendAccounting():
-            returnValueOrRaise(self.dataOp.setValuesFromDict(baseDict))
-
-            if startTime:
-                self.dataOp.setStartTime(startTime)
-                self.dataOp.setEndTime(endTime)
-            else:
-                self.dataOp.setStartTime()
-                self.dataOp.setEndTime()
-            # Adding only to register
-            if not commitFlag and not delayedCommit:
-                return gDataStoreClient.addRegister(self.dataOp)
-
-            # Adding to register and committing
-            if commitFlag and not delayedCommit:
-                gDataStoreClient.addRegister(self.dataOp)
-                result = gDataStoreClient.commit()
-                sLog.debug("Committing data operation to accounting")
-                if not result["OK"]:
-                    sLog.error("Could not commit data operation to accounting", result["Message"])
-                    return result
-                sLog.debug("Done committing to accounting")
-            # Only late committing
-            else:
-                result = self.dataOp.delayedCommit()
-                if not result["OK"]:
-                    sLog.error("Could not delay-commit data operation to accounting")
-            return result
-
         # Send data and commit prioritizing the first monitoring option in the list
-        for backend in self.monitoringOptions:
-            func = locals()[f"_send{backend}"]
-            res = func()
+        for methId, _sendDataMeth in enumerate(self._sendDataMethods):
+            res = _sendDataMeth(
+                baseDict, commitFlag=commitFlag, delayedCommit=delayedCommit, startTime=startTime, endTime=endTime
+            )
             if not res["OK"]:
-                sLog.error("Could not send or commit data", f"Backend: {backend}, {res}")
+                sLog.error("DataOperationSender.sendData: could not send data", f"{res}")
+                # If this is the first backend, we stop
+                if methId == 0:
+                    sLog.error(
+                        "DataOperationSender.sendData: failure of the master accounting system, not trying the others"
+                    )
+                    return res
 
         return S_OK()
 
+    def _commitAccounting(self):
+        result = gDataStoreClient.commit()
+        sLog.debug("Concluding the sending and committing data operation to accounting")
+        if not result["OK"]:
+            sLog.error("Could not commit data operation to accounting", result["Message"])
+        sLog.debug("Committing to accounting concluded")
+        return result
+
+    def _commitMonitoring(self):
+        result = self.dataOperationReporter.commit()
+        sLog.debug("Committing data operation to monitoring")
+        if not result["OK"]:
+            sLog.error("Could not commit data operation to monitoring", result["Message"])
+        sLog.debug("Committing to monitoring concluded")
+        return result
+
     # Call this method in order to commit all records added but not yet committed to Accounting and Monitoring
     def concludeSending(self):
-        def _commitAccounting():
-            result = gDataStoreClient.commit()
-            sLog.debug("Concluding the sending and committing data operation to accounting")
-            if not result["OK"]:
-                sLog.error("Could not commit data operation to accounting", result["Message"])
-            sLog.debug("Committing to accounting concluded")
-            return result
-
-        def _commitMonitoring():
-            result = self.dataOperationReporter.commit()
-            sLog.debug("Committing data operation to monitoring")
-            if not result["OK"]:
-                sLog.error("Could not commit data operation to monitoring", result["Message"])
-            sLog.debug("Committing to monitoring concluded")
-            return result
-
-        # Commit data prioritizing first monitoring option in the list
-        for backend in self.monitoringOptions:
-            func = locals()[f"_commit{backend}"]
-            res = func()
+        """Flush to the services what is still queued"""
+        for methId, _commitMeth in enumerate(self._commitMethods):
+            res = _commitMeth()
             if not res["OK"]:
-                return res
+                sLog.error("DataOperationSender.concludeSending: could not commit data", f"{res}")
+                # If this is the first backend, we stop
+                if methId == 0:
+                    sLog.error(
+                        "DataOperationSender.sendData: failure of the master accounting system, not trying the others"
+                    )
+                    return res
         return S_OK()
