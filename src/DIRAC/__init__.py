4f94c2cd8f0c7254aa2087087e94c77f4c64da5d
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2022-08-05 15:33:51 +0200

    docs: DIRAC release v7r3p27 notes and tags

--- src/DIRAC/__init__.py
+++ src/DIRAC/__init__.py
@@ -106,7 +106,7 @@ if six.PY3:
 else:
     majorVersion = 7
     minorVersion = 3
-    patchLevel = 26
+    patchLevel = 27
     preVersion = 0
 
     version = "v%sr%s" % (majorVersion, minorVersion)
