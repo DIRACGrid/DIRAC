306d02171a1a6cd20b2cee6f23a9ed3e894e46f8
Simon Fayer <sf105@ic.ac.uk>
2022-10-10 10:54:59 +0100

    feat: Merge COMDIRAC
    Co-authored-by: Daniela Bauer <daniela.bauer.grid@googlemail.com>
    Co-authored-by: fstagni <federico.stagni@cern.ch>
    Co-authored-by: Chris Burr <chrisburr@users.noreply.github.com>

---
+++ src/DIRAC/Interfaces/scripts/dcd.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+"""
+Change current DIRAC File Catalog working directory
+
+Examples:
+    $ dcd /dirac/user
+    $ dcd
+"""
+import DIRAC
+from DIRAC.Interfaces.Utilities.DCommands import DSession
+from DIRAC.Interfaces.Utilities.DCommands import DCatalog
+from DIRAC.Interfaces.Utilities.DCommands import pathFromArgument
+from DIRAC.Interfaces.Utilities.DConfigCache import ConfigCache
+from DIRAC.Core.Base.Script import Script
+from DIRAC import gLogger
+
+
+@Script()
+def main():
+    Script.registerArgument(
+        "Path:     path to new working directory (defaults to home directory)",
+        mandatory=False,
+    )
+
+    configCache = ConfigCache()
+    Script.parseCommandLine(ignoreErrors=True)
+    configCache.cacheConfig()
+
+    args = Script.getPositionalArgs()
+
+    session = DSession()
+
+    if len(args) > 1:
+        gLogger.notice("Error: too many arguments provided\n%s:" % Script.scriptName)
+        Script.showHelp(exitCode=-1)
+
+    if len(args):
+        arg = pathFromArgument(session, args[0])
+    else:
+        arg = session.homeDir()
+
+    catalog = DCatalog()
+
+    if catalog.isDir(arg):
+        if session.getCwd() != arg:
+            session.setCwd(arg)
+            session.write()
+    else:
+        print('Error: "%s" not a valid directory' % arg)
+        DIRAC.exit(-1)
+
+
+if __name__ == "__main__":
+    main()
