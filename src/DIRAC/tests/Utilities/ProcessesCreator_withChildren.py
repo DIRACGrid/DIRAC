6ffeb40607acc0c6556939e5e137fee1d32e9e20
fstagni <federico.stagni@cern.ch>
2022-11-22 11:38:23 +0100

    test: ProcessCreator_withChildern less hungry

--- src/DIRAC/tests/Utilities/ProcessesCreator_withChildren.py
+++ src/DIRAC/tests/Utilities/ProcessesCreator_withChildren.py
@@ -15,10 +15,10 @@ def do_sum(li):
 
 
 if __name__ == "__main__":
-    my_list = list(range(150000000))
+    my_list = list(range(15000000))
 
     pool = Pool(3)
-    pool.map(do_sum, [my_list[:50000000], my_list[50000000:100000000], my_list[100000000:]])
+    pool.map(do_sum, [my_list[:5000000], my_list[5000000:10000000], my_list[10000000:]])
     pool.close()
     pool.join()
 
