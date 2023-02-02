98089fbb31af35e5606d45080862757ac01e247e
Chris Burr <christopher.burr@cern.ch>
2023-02-02 16:03:33 +0100

    refactor: Revert Python 2 compatibility from Test_entrypoints.py

--- src/DIRAC/Core/Utilities/test/Test_entrypoints.py
+++ src/DIRAC/Core/Utilities/test/Test_entrypoints.py
@@ -1,16 +1,13 @@
 import importlib_metadata as metadata
-import pytest
-import six
 
 
-@pytest.mark.skipif(six.PY2, reason="Only makes sense for Python 3 installs")
 def test_entrypoints():
     """Make sure all console_scripts defined by DIRAC are importable."""
     errors = []
-    for ep in metadata.entry_points(group="console_scripts"):  # pylint: disable=unexpected-keyword-arg
+    for ep in metadata.entry_points(group="console_scripts"):
         if ep.module.startswith("DIRAC"):
             try:
                 ep.load()
-            except ModuleNotFoundError as e:  # pylint: disable=undefined-variable
+            except ModuleNotFoundError as e:
                 errors.append(str(e))
     assert not errors, errors
