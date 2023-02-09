db998b0fafe0301e806a137ad9bec699799e19a7
Federico Stagni <federico.stagni@cern.ch>
2023-02-09 16:45:10 +0100

    test: rewrote test as pytest

--- src/DIRAC/RequestManagementSystem/private/test/Test_RequestValidator.py
+++ src/DIRAC/RequestManagementSystem/private/test/Test_RequestValidator.py
@@ -7,8 +7,9 @@
 
     test cases for RequestValidator
 """
-import unittest
-from unittest.mock import MagicMock as Mock, patch
+import os
+import pytest
+from unittest.mock import MagicMock
 
 from DIRAC.RequestManagementSystem.Client.Request import Request
 from DIRAC.RequestManagementSystem.Client.Operation import Operation
@@ -18,109 +19,98 @@ from DIRAC.RequestManagementSystem.Client.File import File
 from DIRAC.RequestManagementSystem.private.RequestValidator import RequestValidator
 
 
-########################################################################
-class RequestValidatorTests(unittest.TestCase):
-    """
-    .. class:: RequestValidatorTests
+@pytest.fixture
+def setup():
 
-    """
+    yield setup
 
-    def setUp(self):
-        """test setup"""
-        self.request = Request()
-        self.operation = Operation()
-        self.file = File()
-
-    def tearDown(self):
-        """test tear down"""
-        del self.request
-        del self.operation
-        del self.file
-
-    @patch("DIRAC.ConfigurationSystem.Client.PathFinder.getSystemInstance", new=Mock())
-    def testValidator(self):
-        """validator test"""
-
-        # create validator
-        validator = RequestValidator()
-        self.assertEqual(isinstance(validator, RequestValidator), True)
-
-        # RequestName not set
-        ret = validator.validate(self.request)
-        self.assertFalse(ret["OK"])
-        self.request.RequestName = "test_request"
-
-        # # no operations
-        ret = validator.validate(self.request)
-        self.assertFalse(ret["OK"])
-        self.request.addOperation(self.operation)
-
-        # # type not set
-        ret = validator.validate(self.request)
-        self.assertFalse(ret["OK"])
-        self.operation.Type = "ReplicateAndRegister"
-
-        # # files not present
-        ret = validator.validate(self.request)
-        self.assertFalse(ret["OK"])
-        self.operation.addFile(self.file)
-
-        # # targetSE not set
-        ret = validator.validate(self.request)
-        self.assertFalse(ret["OK"])
-        self.operation.TargetSE = "CERN-USER"
-
-        # # missing LFN
-        ret = validator.validate(self.request)
-        self.assertFalse(ret["OK"])
-        self.file.LFN = "/a/b/c"
-
-        # # no owner
-        # force no owner because it takes the one of the current user
-        self.request.Owner = ""
-        ret = validator.validate(self.request)
-        self.assertFalse(ret["OK"])
-        self.request.Owner = "foo"
-
-        # # no owner group
-        # same, force it
-        self.request.OwnerGroup = ""
-        ret = validator.validate(self.request)
-        self.assertFalse(ret["OK"])
-        self.request.OwnerGroup = "dirac_user"
-
-        # Checksum set, ChecksumType not set
-        self.file.Checksum = "abcdef"
-        ret = validator.validate(self.request)
-        self.assertFalse(ret["OK"])
-
-        # ChecksumType set, Checksum not set
-        self.file.Checksum = ""
-        self.file.ChecksumType = "adler32"
-
-        ret = validator.validate(self.request)
-        self.assertFalse(ret["OK"])
-
-        # both set
-        self.file.Checksum = "abcdef"
-        self.file.ChecksumType = "adler32"
-        ret = validator.validate(self.request)
-        self.assertEqual(ret, {"OK": True, "Value": None})
-
-        # both unset
-        self.file.Checksum = ""
-        self.file.ChecksumType = None
-        ret = validator.validate(self.request)
-        self.assertEqual(ret, {"OK": True, "Value": None})
-
-        # all OK
-        ret = validator.validate(self.request)
-        self.assertEqual(ret, {"OK": True, "Value": None})
+    try:
+        os.remove("1.txt")
+        os.remove("InputData_*")
+    except OSError:
+        pass
 
 
-# test suite execution
-if __name__ == "__main__":
-    gTestLoader = unittest.TestLoader()
-    gSuite = gTestLoader.loadTestsFromTestCase(RequestValidatorTests)
-    gSuite = unittest.TestSuite([gSuite])
-    unittest.TextTestRunner(verbosity=3).run(gSuite)
+def test_Validator(mocker, setup):
+    """validator test"""
+    mocker.patch("DIRAC.ConfigurationSystem.Client.PathFinder.getSystemInstance", new=MagicMock())
+
+    request = Request()
+    operation = Operation()
+    file = File()
+
+    # create validator
+    validator = RequestValidator()
+    assert isinstance(validator, RequestValidator)
+
+    # RequestName not set
+    ret = validator.validate(request)
+    assert ret["OK"] is False
+    request.RequestName = "test_request"
+
+    # # no operations
+    ret = validator.validate(request)
+    assert ret["OK"] is False
+    request.addOperation(operation)
+
+    # # type not set
+    ret = validator.validate(request)
+    assert ret["OK"] is False
+    operation.Type = "ReplicateAndRegister"
+
+    # # files not present
+    ret = validator.validate(request)
+    assert ret["OK"] is False
+    operation.addFile(file)
+
+    # # targetSE not set
+    ret = validator.validate(request)
+    assert ret["OK"] is False
+    operation.TargetSE = "CERN-USER"
+
+    # # missing LFN
+    ret = validator.validate(request)
+    assert ret["OK"] is False
+    file.LFN = "/a/b/c"
+
+    # # no owner
+    # force no owner because it takes the one of the current user
+    request.Owner = ""
+    ret = validator.validate(request)
+    assert ret["OK"] is False
+    request.Owner = "foo"
+
+    # # no owner group
+    # same, force it
+    request.OwnerGroup = ""
+    ret = validator.validate(request)
+    assert ret["OK"] is False
+    request.OwnerGroup = "dirac_user"
+
+    # Checksum set, ChecksumType not set
+    file.Checksum = "abcdef"
+    ret = validator.validate(request)
+    assert ret["OK"] is False
+
+    # ChecksumType set, Checksum not set
+    file.Checksum = ""
+    file.ChecksumType = "adler32"
+
+    ret = validator.validate(request)
+    assert ret["OK"] is False
+
+    # both set
+    file.Checksum = "abcdef"
+    file.ChecksumType = "adler32"
+    ret = validator.validate(request)
+    assert ret == {"OK": True, "Value": None}
+
+    # both unset
+    file.Checksum = ""
+    file.ChecksumType = None
+    ret = validator.validate(request)
+    assert ret == {"OK": True, "Value": None}
+
+    # all OK
+    ret = validator.validate(request)
+    assert ret == {"OK": True, "Value": None}
