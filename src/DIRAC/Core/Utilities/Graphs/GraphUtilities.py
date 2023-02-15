91642e334bc0aa499a2b2e6905ce28f8b9ba3b2a
Christophe Haen <christophe.haen@cern.ch>
2023-02-15 15:37:03 +0100

    fix (GraphUtilities): stop using matplotlib deprecated methods

--- src/DIRAC/Core/Utilities/Graphs/GraphUtilities.py
+++ src/DIRAC/Core/Utilities/Graphs/GraphUtilities.py
@@ -366,8 +366,8 @@ class PrettyDateLocator(AutoDateLocator):
         locator = RRuleLocator(rrule, self.tz)
         locator.set_axis(self.axis)
 
-        locator.set_view_interval(*self.axis.get_view_interval())
-        locator.set_data_interval(*self.axis.get_data_interval())
+        locator.axis.set_view_interval(*self.axis.get_view_interval())
+        locator.axis.set_data_interval(*self.axis.get_data_interval())
         return locator
 
 
