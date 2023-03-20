a551437764f9bfd60284248ef9082767bf8bee58
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2023-03-20 19:12:43 +0100

    fix: get CAs location with getCAsLocation() utility

--- src/DIRAC/Resources/Computing/HTCondorCEComputingElement.py
+++ src/DIRAC/Resources/Computing/HTCondorCEComputingElement.py
@@ -62,7 +62,7 @@ from DIRAC.WorkloadManagementSystem.Client import PilotStatus
 from DIRAC.WorkloadManagementSystem.Client.PilotManagerClient import PilotManagerClient
 from DIRAC.Core.Utilities.File import makeGuid
 from DIRAC.FrameworkSystem.private.authorization.utils.Tokens import writeToTokenFile
-
+from DIRAC.Core.Security.Locations import getCAsLocation
 from DIRAC.Resources.Computing.BatchSystems.Condor import parseCondorStatus
 
 MANDATORY_PARAMETERS = ["Queue"]
@@ -272,8 +272,9 @@ Queue stamp in %(pilotStampList)s
             htcEnv = {
                 "_CONDOR_SEC_CLIENT_AUTHENTICATION_METHODS": "SCITOKENS",
                 "_CONDOR_SCITOKENS_FILE": self.tokenFile.name,
-                "_CONDOR_AUTH_SSL_CLIENT_CADIR": os.environ.get("X509_CERT_DIR", "/etc/grid-security/certificates"),
             }
+            if cas := getCAsLocation():
+                htcEnv["_CONDOR_AUTH_SSL_CLIENT_CADIR"] = cas
         else:
             htcEnv = {"_CONDOR_SEC_CLIENT_AUTHENTICATION_METHODS": "GSI"}
 
