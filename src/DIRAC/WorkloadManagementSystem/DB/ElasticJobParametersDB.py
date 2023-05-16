b2c99f2a1e4fe383ca50da799acfe059fd048056
Federico Stagni <federico.stagni@cern.ch>
2023-05-16 15:07:22 +0200

    feat: removed old elasticJobParametersDB

--- src/DIRAC/WorkloadManagementSystem/DB/ElasticJobParametersDB.py
+++ src/DIRAC/WorkloadManagementSystem/DB/ElasticJobParametersDB.py
@@ -33,13 +33,6 @@ from DIRAC.ConfigurationSystem.Client.PathFinder import getDatabaseSection
 from DIRAC.ConfigurationSystem.Client.Helpers import CSGlobals
 from DIRAC.Core.Base.ElasticDB import ElasticDB
 
-try:
-    from opensearchpy.exceptions import NotFoundError, RequestError
-except ImportError:
-    from elasticsearch.exceptions import NotFoundError, RequestError
-
-name = "ElasticJobParametersDB"  # (base for) old index name
-
 
 mapping = {
     "properties": {
@@ -68,16 +61,12 @@ class ElasticJobParametersDB(ElasticDB):
             indexPrefix = gConfig.getValue(f"{section}/IndexPrefix", CSGlobals.getSetup()).lower()
 
             # Connecting to the ES cluster
-            super().__init__(name, "WorkloadManagement/ElasticJobParametersDB", indexPrefix, parentLogger=parentLogger)
+            super().__init__("WorkloadManagement/ElasticJobParametersDB", indexPrefix, parentLogger=parentLogger)
         except Exception as ex:
             self.log.error("Can't connect to ElasticJobParametersDB", repr(ex))
-            raise RuntimeError("Can't connect to ElasticJobParametersDB")
+            raise RuntimeError("Can't connect to ElasticJobParametersDB") from ex
 
-        self.oldIndexName = f"{self.getIndexPrefix()}_{name.lower()}"
         self.indexName_base = f"{self.getIndexPrefix()}_elasticjobparameters_index"
-
-        self.dslSearch = self._Search(self.oldIndexName)
-        self.dslSearch.extra(track_total_hits=True)
 
     def _indexName(self, jobID: int) -> str:
         """construct the index name
@@ -114,46 +103,15 @@ class ElasticJobParametersDB(ElasticDB):
         if isinstance(paramList, str):
             paramList = paramList.replace(" ", "").split(",")
         self.log.debug(f"JobDB.getParameters: Getting Parameters for job {jobID}")
-        resultDict = {}
-        inNewIndex = self.existsDoc(self._indexName(jobID), jobID)
-        inOldIndex = self._isInOldIndex(self.oldIndexName, jobID)
-        # Case 1: the parameters are stored in both indices
-        if inNewIndex and inOldIndex:
-            # First we get the parameters from the old index
-            self.log.debug(
-                f"A document with JobID {jobID} was found in the old index {self.oldIndexName} and in the new index {self._indexName(jobID)}"
-            )
-            resultDict = self._searchInOldIndex(jobID, paramList)
 
-            # Now we get the parameters from the new index
-            res = self.getDoc(self._indexName(jobID), str(jobID))
-            if not res["OK"]:
-                self.log.error("Could not retrieve the data from the new index!", res["Message"])
-            else:
-                for key in res["Value"]:
-                    if paramList and key not in paramList:
-                        continue
-                    # Add new parameters or overwrite the old ones
-                    resultDict[key] = res["Value"][key]
-
-        # Case 2: only in the old index
-        elif inOldIndex:
-            self.log.debug(f"A document with JobID {jobID} was found in the old index {self.oldIndexName}")
-            resultDict = self._searchInOldIndex(jobID, paramList)
-
-        # Case 3: only in the new index
-        else:
-            self.log.debug(
-                f"The searched parameters with JobID {jobID} exists in the new index {self._indexName(jobID)}"
-            )
-            res = self.getDoc(self._indexName(jobID), str(jobID))
-            if not res["OK"]:
-                return res
-            resultDict = res["Value"]
-            if paramList:
-                for k in list(resultDict):
-                    if k not in paramList:
-                        resultDict.pop(k)
+        res = self.getDoc(self._indexName(jobID), str(jobID))
+        if not res["OK"]:
+            return res
+        resultDict = res["Value"]
+        if paramList:
+            for k in list(resultDict):
+                if k not in paramList:
+                    resultDict.pop(k)
 
         return S_OK({jobID: resultDict})
 
@@ -224,111 +182,7 @@ class ElasticJobParametersDB(ElasticDB):
 
         if isinstance(paramList, str):
             paramList = paramList.replace(" ", "").split(",")
-        inNewIndex = self.existsDoc(self._indexName(jobID), str(jobID))
-        inOldIndex = self._isInOldIndex(self.oldIndexName, jobID)
 
-        # 3 Cases as in GetJobParameters
-        if inNewIndex and inOldIndex:
-            # Delete first in the old index, then in the new one
-            self._deleteInOldIndex(jobID, paramList)
-            res = self._deleteInNewIndex(jobID, paramList)
-            if not res["OK"]:
-                return S_ERROR(res)
-
-        elif inOldIndex:
-            self._deleteInOldIndex(jobID, paramList)
-
-        else:
-            self.log.debug(
-                f"The searched parameters with JobID {jobID} exists in the new index {self._indexName(jobID)}"
-            )
-            res = self._deleteInNewIndex(jobID, paramList)
-            if not res["OK"]:
-                return S_ERROR(res)
-
-        return S_OK()
-
-    def _isInOldIndex(self, old_index: str, jobID: int) -> bool:
-        """Checks if a document with this jobID exists in the old index"""
-        query = {
-            "query": {
-                "bool": {
-                    "filter": {  # no scoring
-                        "term": {"JobID": jobID}  # term level query, does not pass through the analyzer
-                    }
-                }
-            }
-        }
-        try:
-            # See a document with this jobID is stored in the old index
-            self.query(old_index, query)
-            return True
-        except (RequestError, NotFoundError):
-            return False
-
-    def _searchInOldIndex(self, jobID: int, paramList: list) -> bool:
-        """Searches for a document with this jobID in the old index"""
-        if paramList:
-            if isinstance(paramList, str):
-                paramList = paramList.replace(" ", "").split(",")
-        else:
-            paramList = []
-
-        resultDict = {}
-
-        # the following should be equivalent to
-        # {
-        #   "query": {
-        #     "bool": {
-        #       "filter": {  # no scoring
-        #         "term": {"JobID": jobID}  # term level query, does not pass through the analyzer
-        #       }
-        #     }
-        #   }
-        # }
-
-        s = self.dslSearch.query("bool", filter=self._Q("term", JobID=jobID))
-
-        res = s.scan()
-
-        for hit in res:
-            pname = hit.Name
-            if paramList and pname not in paramList:
-                continue
-            resultDict[pname] = hit.Value
-        return resultDict
-
-    def _deleteInOldIndex(self, jobID: int, paramList: list) -> dict:
-        """Deletes a document with this jobID in the old index"""
-        self.log.debug(f"A document with JobID {jobID} was found in the old index {self.oldIndexName}")
-        jobFilter = self._Q("term", JobID=jobID)
-
-        if not paramList:
-            s = self.dslSearch.query("bool", filter=jobFilter)
-            s.delete()
-            return S_OK()
-
-        # the following should be equivalent to
-        # {
-        #   "query": {
-        #     "bool": {
-        #       "filter": [  # no scoring
-        #         {"term": {"JobID": jobID}},  # term level query, does not pass through the analyzer
-        #         {"term": {"Name": param}},  # term level query, does not pass through the analyzer
-        #       ]
-        #     }
-        #   }
-        # }
-
-        for param in paramList:
-            paramFilter = self._Q("term", Name=param)
-            combinedFilter = jobFilter & paramFilter
-
-            s = self.dslSearch.query("bool", filter=combinedFilter)
-            s.delete()
-
-    def _deleteInNewIndex(self, jobID: int, paramList=None) -> dict:
-        """Deletes a document with this jobID in the new index"""
         if not paramList:
             # Deleting the whole record
             self.log.debug("Deleting record of job {jobID}")
