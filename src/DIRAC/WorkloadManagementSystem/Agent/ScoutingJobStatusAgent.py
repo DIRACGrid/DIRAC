84090c73c6b3bb2e4f4b2480b6ae56ff47d037f8
qcampagn <quinn.campagna@desy.de>
2023-09-29 01:12:39 -0500

    remove __future__ imports

--- src/DIRAC/WorkloadManagementSystem/Agent/ScoutingJobStatusAgent.py
+++ src/DIRAC/WorkloadManagementSystem/Agent/ScoutingJobStatusAgent.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function
-from __future__ import absolute_import
-from __future__ import division
-
 """ Agent for scout job framework to monitor scout status
     and update main job status according to scout status.
 """
