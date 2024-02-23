90354180104692b6cd9cf4f628a2e465e6bbc53d
Christophe Haen <christophe.haen@cern.ch>
2024-02-23 15:04:12 +0100

    fix: plotting TypeLoader supports editable install

--- src/DIRAC/Core/Utilities/Plotting/TypeLoader.py
+++ src/DIRAC/Core/Utilities/Plotting/TypeLoader.py
@@ -1,9 +1,10 @@
 """ Utility for loading plotting types.
     Works both for Accounting and Monitoring.
 """
+
 import re
 
-from DIRAC.Core.Utilities.ObjectLoader import loadObjects
+from DIRAC.Core.Utilities.ObjectLoader import ObjectLoader
 
 from DIRAC.AccountingSystem.Client.Types.BaseAccountingType import BaseAccountingType
 from DIRAC.MonitoringSystem.Client.Types.BaseType import BaseType
@@ -26,12 +27,11 @@ class TypeLoader:
         """c'tor"""
         self.__loaded = {}
         if plottingFamily == "Accounting":
-            self.__path = "AccountingSystem/Client/Types"
+            self.__path = "AccountingSystem.Client.Types"
             self.__parentCls = BaseAccountingType
         elif plottingFamily == "Monitoring":
-            self.__path = "MonitoringSystem/Client/Types"
+            self.__path = "MonitoringSystem.Client.Types"
             self.__parentCls = BaseType
-        self.__reFilter = re.compile(r".*[a-z1-9]\.py$")
 
     ########################################################################
     def getTypes(self):
@@ -39,5 +39,9 @@ class TypeLoader:
         It returns all monitoring classes
         """
         if not self.__loaded:
-            self.__loaded = loadObjects(self.__path, self.__reFilter, self.__parentCls)
+            allObjects = ObjectLoader().getObjects(self.__path, parentClass=self.__parentCls)["Value"]
+            for _objectModule, objectClass in allObjects.items():
+                if objectClass.__name__ not in self.__loaded and objectClass != self.__parentCls:
+                    self.__loaded[objectClass.__name__] = objectClass
+
         return self.__loaded
