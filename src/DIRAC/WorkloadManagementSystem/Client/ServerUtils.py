3064f1b9df0637b6d739aa7a65d5ed09e6ec65d8
Federico Stagni <federico.stagni@cern.ch>
2024-04-17 10:17:37 +0200

    sweep: #7572 fix: serverUtils: jit imports

--- src/DIRAC/WorkloadManagementSystem/Client/ServerUtils.py
+++ src/DIRAC/WorkloadManagementSystem/Client/ServerUtils.py
@@ -4,13 +4,13 @@ It always try to insert the records directly. In case of failure a WMS client is
 """
 
 from DIRAC.Core.Base.Client import Client
-from DIRAC.WorkloadManagementSystem.DB.PilotAgentsDB import gPilotAgentsDB
 
 
 def getPilotAgentsDB():
     try:
+        from DIRAC.WorkloadManagementSystem.DB.PilotAgentsDB import gPilotAgentsDB
+
         if gPilotAgentsDB and gPilotAgentsDB._connected:
             return gPilotAgentsDB
     except Exception:
-        pass
-    return Client(url="WorkloadManagement/PilotManager")
+        return Client(url="WorkloadManagement/PilotManager")
