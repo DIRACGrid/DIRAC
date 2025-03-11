872c2a21382aa7edd737321248a33fbd5e5e5b92
Federico Stagni <federico.stagni@cern.ch>
2025-03-11 16:34:22 +0100

    test: fix: added the pilot subs

--- tests/Jenkins/dirac-cfg-setup-diracx.py
+++ tests/Jenkins/dirac-cfg-setup-diracx.py
@@ -47,6 +47,7 @@ def main(url: str, disabled_vos: list[str]):
                         "adminusername": "e2cb28ec-1a1e-40ee-a56d-d899b79879ce",
                         "ciuser": "26dbe36e-cf5c-4c52-a834-29a1c904ef74",
                         "trialUser": "a95ab678-3fa4-41b9-b863-fe62ce8064ce",
+                        "pilot": "879fd4c9-339b-4cbb-ae13-fc87053a5014",
                     },
                     "Support": {
                         "Message": "Contact the help desk",
@@ -61,6 +62,7 @@ def main(url: str, disabled_vos: list[str]):
                         "adminusername": "26b14fc9-6d40-4ca5-b014-6234eaf0fb6e",
                         "ciuser": "d3adc733-6588-4d6f-8581-5986b02d0c87",
                         "trialUser": "ff2152ff-34f4-4739-b106-3def37e291e3",
+                        "pilot": "e8be5ed6-2ba1-43cb-a7dd-b13944283467",
                     },
                 },
             }
