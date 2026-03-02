3861d939864aed55e3b5b821422ba90f295ff54f
Jorge Lisa <64639359+AcquaDiGiorgio@users.noreply.github.com>
2026-03-02 15:24:53 +0100

    fix(PushJobAgent): Bug while obtaining job output in failed job wrappers

--- src/DIRAC/WorkloadManagementSystem/Agent/PushJobAgent.py
+++ src/DIRAC/WorkloadManagementSystem/Agent/PushJobAgent.py
@@ -814,6 +814,14 @@ class PushJobAgent(JobAgent):
                 self.log.exception("JobWrapper failed the initialization phase", jobID)
                 continue
 
+            if status == PilotStatus.FAILED:
+                job.jobReport.setJobStatus(
+                    status=JobStatus.FAILED, minorStatus="Payload failed", sendFlag=False
+                )
+                job.sendFailoverRequest()
+                job.sendJobAccounting(status=JobStatus.FAILED, minorStatus=JobMinorStatus.EXCEPTION_DURING_EXEC)
+                continue
+
             # Get the output of the job
             self.log.info(f"Getting the outputs of taskID {taskID} for {jobID}")
             if not (result := ce.getJobOutput(f"{taskID}:::{stamp}", job.jobIDPath))["OK"]:
