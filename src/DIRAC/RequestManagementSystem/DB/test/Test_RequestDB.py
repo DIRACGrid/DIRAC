628700c68564a723a147feb2e374fe5aa97832c4
Christophe Haen <christophe.haen@cern.ch>
2026-05-08 15:44:26 +0200

    feat: test

--- src/DIRAC/RequestManagementSystem/DB/test/Test_RequestDB.py
+++ src/DIRAC/RequestManagementSystem/DB/test/Test_RequestDB.py
@@ -12,6 +12,9 @@ from sqlalchemy.orm import sessionmaker
 from DIRAC import gLogger, S_OK
 
 from DIRAC.RequestManagementSystem.DB import RequestDB
+from DIRAC.RequestManagementSystem.Client.File import File
+from DIRAC.RequestManagementSystem.Client.Operation import Operation
+from DIRAC.RequestManagementSystem.Client.Request import Request
 
 from DIRAC.RequestManagementSystem.DB.test.RMSTestScenari import (  # pylint: disable=unused-import
     test_dirty,
@@ -39,3 +42,37 @@ def reqDB(mocker, request):
         db.createTables()
 
         yield db
+
+
+def test_web_queries_reject_unknown_attributes(reqDB):
+    request = Request({"RequestName": "web-summary"})
+    operation = Operation({"Type": "RemoveReplica", "TargetSE": "CERN-USER"})
+    operation += File({"LFN": "/lhcb/user/c/cibak/web-summary"})
+    request += operation
+
+    put = reqDB.putRequest(request)
+    assert put["OK"], put
+
+    summary = reqDB.getRequestSummaryWeb({"Type": "RemoveReplica"}, [("RequestID", "ASC")], 0, 10)
+    assert summary["OK"], summary
+    assert summary["Value"]["TotalRecords"] == 1, summary
+
+    counters = reqDB.getRequestCountersWeb("Type", {"Status": "Waiting"})
+    assert counters["OK"], counters
+    assert counters["Value"] == {"RemoveReplica": 1}, counters
+
+    distinct = reqDB.getDistinctValues("Operation", "Type")
+    assert distinct["OK"], distinct
+    assert distinct["Value"] == ["RemoveReplica"], distinct
+
+    invalid_summary = reqDB.getRequestSummaryWeb({"__class__": "Request"}, [], 0, 10)
+    assert not invalid_summary["OK"], invalid_summary
+    assert invalid_summary["Message"] == "Unknown Request attribute '__class__'"
+
+    invalid_counters = reqDB.getRequestCountersWeb("__class__", {})
+    assert not invalid_counters["OK"], invalid_counters
+    assert invalid_counters["Message"] == "Unknown Request attribute '__class__'"
+
+    invalid_distinct = reqDB.getDistinctValues("Request", "__class__")
+    assert not invalid_distinct["OK"], invalid_distinct
+    assert invalid_distinct["Message"] == "Unknown Request attribute '__class__'"
