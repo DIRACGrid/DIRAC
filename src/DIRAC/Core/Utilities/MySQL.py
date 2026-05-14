15a1da5cc6aaba476df5bdd529670418171ab1ef
Simon Fayer <sf105@ic.ac.uk>
2026-05-14 17:40:31 +0100

    feat: Add args parameter to allow for mysql parameterized queries

--- src/DIRAC/Core/Utilities/MySQL.py
+++ src/DIRAC/Core/Utilities/MySQL.py
@@ -703,7 +703,7 @@ class MySQL:
         return S_OK()
 
     @captureOptimizerTraces
-    def _query(self, cmd, *, conn=None, debug=True):
+    def _query(self, cmd, *, args=None, conn=None, debug=True):
         """
         execute MySQL query command
 
@@ -726,7 +726,7 @@ class MySQL:
 
         try:
             cursor = connection.cursor()
-            if cursor.execute(cmd):
+            if cursor.execute(cmd, args=args):
                 res = cursor.fetchall()
             else:
                 res = ()
