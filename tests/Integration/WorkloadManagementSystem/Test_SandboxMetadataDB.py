811423aa800d27b56b96adc1ea96ca79461a5e49
Ryunosuke O'Neil <r.oneil@cern.ch>
2026-06-16 16:51:19 +0200

    test(sandbox): cover transformation entity assign/unassign in SandboxMetadataDB

--- tests/Integration/WorkloadManagementSystem/Test_SandboxMetadataDB.py
+++ tests/Integration/WorkloadManagementSystem/Test_SandboxMetadataDB.py
@@ -44,3 +44,42 @@ def test_SandboxMetadataDB():
 
     res = smDB.getUnusedSandboxes()
     assert res["OK"], res["Message"]
+
+
+def test_SandboxMetadataDB_transformationAssignment():
+    smDB = SandboxMetadataDB()
+
+    owner = "adminusername"
+    ownerGroup = "dirac_admin"
+    VO = "vo"
+
+    sbSE = "ProductionSandboxSE"
+    sbPFN = "/sb/pfn/trans1.tar.bz2"
+
+    res = smDB.registerAndGetSandbox(owner, ownerGroup, VO, sbSE, sbPFN, 123)
+    assert res["OK"], res["Message"]
+    sbId, _newSandbox = res["Value"]
+
+    entityId = "Transformation:999"
+    assignTo = {entityId: [(f"SB:{sbSE}|{sbPFN}", "Input")]}
+
+    res = smDB.assignSandboxesToEntities(assignTo, owner, ownerGroup)
+    assert res["OK"], res["Message"]
+    assert res["Value"] == 1
+
+    # While the mapping exists the sandbox is reachable via the transformation entity
+    res = smDB.getSandboxesAssignedToEntity(entityId, owner, ownerGroup, VO)
+    assert res["OK"], res["Message"]
+    assert (sbSE, sbPFN, "Input") in res["Value"]
+
+    # Removing the mapping leaves no rows for that entity
+    res = smDB.unassignEntities([entityId])
+    assert res["OK"], res["Message"]
+
+    res = smDB.getSandboxesAssignedToEntity(entityId, owner, ownerGroup, VO)
+    assert res["OK"], res["Message"]
+    assert not res["Value"]
+
+    # cleanup
+    res = smDB.deleteSandboxes([sbId])
+    assert res["OK"], res["Message"]
