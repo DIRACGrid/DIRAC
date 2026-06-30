c35e0c5083c008c5d340f47a6678542882cab3de
Loxeris <30194187+Loxeris@users.noreply.github.com>
2026-06-30 10:29:02 +0200

    fix: add basename in url and file content in data

--- src/DIRAC/WorkloadManagementSystem/Agent/PilotSyncAgent.py
+++ src/DIRAC/WorkloadManagementSystem/Agent/PilotSyncAgent.py
@@ -109,7 +109,13 @@ class PilotSyncAgent(AgentModule):
             self.log.info("Attempting to upload", f"to {server}")
             if server.startswith("https://"):
                 for tf in allFiles:
-                    res = requests.put(server, data=tf, verify=self.casLocation, cert=self.certAndKeyLocation)
+                    res = requests.put(
+                        f"{server}/{os.path.basename(tf)}",
+                        data=open(tf, "rb"),
+                        verify=self.casLocation,
+                        cert=self.certAndKeyLocation,
+                        timeout=30,
+                    )
                     if res.status_code not in (200, 202):
                         self.log.error("Could not upload", f"to {server}: status {res.status_code}")
             else:  # Assumes this is a DIRAC SE
